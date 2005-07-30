cb09184460606efa20010dd9256569546b4f046f
kevins <kevins@localhost>
2005-07-30 17:37:43 +0000

    Add icon and MS Windows shortcut to the zip file build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 date "+%Y-%m-%d %X" > bin/version.txt
 
 rm ../eam.zip
-zip ../eam README LICENSE.jgraph
+zip ../eam README LICENSE.jgraph cmp.ico "e-Adaptive Management.lnk"
 zip -j ../eam third-party/jgraph/jgraph.jar
 zip -r ../eam icons -x CVS/
 zip -r ../eam translations -x CVS/
