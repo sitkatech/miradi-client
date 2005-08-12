03b192530d8ed367d4222526d45653803126cce6
kevins <kevins@localhost>
2005-08-12 19:21:34 +0000

    Move node inserting logic into base class to be shared by future doers

--- source/org/conservationmeasures/eam/views/diagram/InsertGoal.java
+++ source/org/conservationmeasures/eam/views/diagram/InsertGoal.java
@@ -7,22 +7,17 @@ package org.conservationmeasures.eam.views.diagram;
 
 import java.awt.Point;
 
-import org.conservationmeasures.eam.commands.Command;
-import org.conservationmeasures.eam.commands.CommandDiagramMove;
 import org.conservationmeasures.eam.commands.CommandInsertNode;
-import org.conservationmeasures.eam.commands.CommandSetNodeText;
 import org.conservationmeasures.eam.diagram.nodes.Node;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.main.BaseProject;
 import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.views.ProjectDoer;
 
-public class InsertGoal extends ProjectDoer
+public class InsertGoal extends InsertNode
 {
 	public InsertGoal(BaseProject project, Point invocationPoint)
 	{
-		super(project);
-		createAt = invocationPoint;
+		super(project, invocationPoint);
 	}
 
 	public boolean isAvailable()
@@ -35,22 +30,9 @@ public class InsertGoal extends ProjectDoer
 		doInsert(new CommandInsertNode(Node.TYPE_GOAL));
 	}
 
-	protected void doInsert(CommandInsertNode insertCommand) throws CommandFailedException
-	{
-		getProject().executeCommand(insertCommand);
-		int id = insertCommand.getId();
-	
-		Command setTextCommand = new CommandSetNodeText(id, getInitialText());
-		getProject().executeCommand(setTextCommand);
-
-		Command moveCommand = new CommandDiagramMove(createAt.x, createAt.y, new int[] {id});
-		getProject().executeCommand(moveCommand);
-	}
-	
 	public String getInitialText()
 	{
 		return EAM.text("Label|New Goal");
 	}
 
-	Point createAt;
 }
---
+++ source/org/conservationmeasures/eam/views/diagram/InsertNode.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2005, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.views.diagram;
+
+import java.awt.Point;
+
+import org.conservationmeasures.eam.commands.Command;
+import org.conservationmeasures.eam.commands.CommandDiagramMove;
+import org.conservationmeasures.eam.commands.CommandInsertNode;
+import org.conservationmeasures.eam.commands.CommandSetNodeText;
+import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.main.BaseProject;
+import org.conservationmeasures.eam.views.ProjectDoer;
+
+abstract public class InsertNode extends ProjectDoer
+{
+	public InsertNode(BaseProject project, Point invocationPoint)
+	{
+		super(project);
+		createAt = invocationPoint;
+	}
+	
+	abstract public String getInitialText();
+
+	protected void doInsert(CommandInsertNode insertCommand) throws CommandFailedException
+	{
+		getProject().executeCommand(insertCommand);
+		int id = insertCommand.getId();
+	
+		Command setTextCommand = new CommandSetNodeText(id, getInitialText());
+		getProject().executeCommand(setTextCommand);
+	
+		Command moveCommand = new CommandDiagramMove(createAt.x, createAt.y, new int[] {id});
+		getProject().executeCommand(moveCommand);
+	}
+
+	Point createAt;
+}
