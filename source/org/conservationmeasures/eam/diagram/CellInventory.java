db608d401f936d2148d45b10721cb70f649bbd8f
kevins <kevins@localhost>
2005-10-04 18:36:14 +0000

    Extract CellInventory to be its own java file

---
+++ source/org/conservationmeasures/eam/diagram/CellInventory.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2005, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.diagram;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.conservationmeasures.eam.diagram.nodes.EAMGraphCell;
+import org.conservationmeasures.eam.diagram.nodes.Linkage;
+import org.conservationmeasures.eam.diagram.nodes.Node;
+
+class CellInventory
+{
+	public CellInventory()
+	{
+		cells = new Vector();
+	}
+	
+	void add(EAMGraphCell cell, int id)
+	{
+		if(id == Node.INVALID_ID)
+		{
+			id = nextId++;
+		}
+		else
+		{
+			if(nextId < id+1)
+				nextId = id + 1;
+		}
+		
+		if(getById(id) != null)
+			throw new RuntimeException("Can't add over existing id " + id);
+		
+		cell.setId(id);
+		cells.add(cell);
+	}
+	
+	public int getIndex(EAMGraphCell cellToFind)
+	{
+		for (int index = 0; index < cells.size(); ++index) 
+		{
+			EAMGraphCell cell = (EAMGraphCell) cells.get(index);
+			if(cell.getId() == cellToFind.getId())
+				return index;
+		}
+		return -1;
+	}
+
+	public int size()
+	{
+		return cells.size();
+	}
+	
+	public EAMGraphCell getCellByIndex(int index)
+	{
+		return (EAMGraphCell)cells.get(index);
+	}
+	
+	public Node getNodeByIndex(int index)
+	{
+		int currentIndex = -1;
+		for(int i = 0; i < cells.size(); ++i)
+		{
+			EAMGraphCell cell = (EAMGraphCell)cells.get(i);
+			if(cell.isNode())
+				++currentIndex;
+			if(currentIndex == index)
+				return (Node)cell;
+		}
+		return null;
+	}
+	
+	public Linkage getLinkageByIndex(int index)
+	{
+		int currentIndex = -1;
+		for(int i = 0; i < cells.size(); ++i)
+		{
+			EAMGraphCell cell = (EAMGraphCell)cells.get(i);
+			if(cell.isLinkage())
+				++currentIndex;
+			if(currentIndex == index)
+				return (Linkage)cell;
+		}
+		return null;
+	}
+	
+	public EAMGraphCell getById(int id)
+	{
+		for (Iterator iter = cells.iterator(); iter.hasNext();) 
+		{
+			EAMGraphCell cell = (EAMGraphCell) iter.next();
+			if(cell.getId() == id)
+				return cell;
+		}
+		return null;
+	}
+	
+	public void remove(EAMGraphCell cell)
+	{
+		cells.remove(cell);
+	}
+	
+	public void clear()
+	{
+		nextId = 0;
+		cells.clear();
+	}
+
+	private int nextId;
+	Vector cells;
+}
--- source/org/conservationmeasures/eam/diagram/DiagramModel.java
+++ source/org/conservationmeasures/eam/diagram/DiagramModel.java
@@ -7,7 +7,6 @@ package org.conservationmeasures.eam.diagram;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -235,106 +234,5 @@ public class DiagramModel extends DefaultGraphModel
 
 	CellInventory cellInventory;
 	protected List diagramModelListenerList = new ArrayList();
-	
 }
 
-class CellInventory
-{
-	public CellInventory()
-	{
-		cells = new Vector();
-	}
-	
-	void add(EAMGraphCell cell, int id)
-	{
-		if(id == Node.INVALID_ID)
-		{
-			id = nextId++;
-		}
-		else
-		{
-			if(nextId < id+1)
-				nextId = id + 1;
-		}
-		
-		if(getById(id) != null)
-			throw new RuntimeException("Can't add over existing id " + id);
-		
-		cell.setId(id);
-		cells.add(cell);
-	}
-	
-	public int getIndex(EAMGraphCell cellToFind)
-	{
-		for (int index = 0; index < cells.size(); ++index) 
-		{
-			EAMGraphCell cell = (EAMGraphCell) cells.get(index);
-			if(cell.getId() == cellToFind.getId())
-				return index;
-		}
-		return -1;
-	}
-
-	public int size()
-	{
-		return cells.size();
-	}
-	
-	public EAMGraphCell getCellByIndex(int index)
-	{
-		return (EAMGraphCell)cells.get(index);
-	}
-	
-	public Node getNodeByIndex(int index)
-	{
-		int currentIndex = -1;
-		for(int i = 0; i < cells.size(); ++i)
-		{
-			EAMGraphCell cell = (EAMGraphCell)cells.get(i);
-			if(cell.isNode())
-				++currentIndex;
-			if(currentIndex == index)
-				return (Node)cell;
-		}
-		return null;
-	}
-	
-	public Linkage getLinkageByIndex(int index)
-	{
-		int currentIndex = -1;
-		for(int i = 0; i < cells.size(); ++i)
-		{
-			EAMGraphCell cell = (EAMGraphCell)cells.get(i);
-			if(cell.isLinkage())
-				++currentIndex;
-			if(currentIndex == index)
-				return (Linkage)cell;
-		}
-		return null;
-	}
-	
-	public EAMGraphCell getById(int id)
-	{
-		for (Iterator iter = cells.iterator(); iter.hasNext();) 
-		{
-			EAMGraphCell cell = (EAMGraphCell) iter.next();
-			if(cell.getId() == id)
-				return cell;
-		}
-		return null;
-	}
-	
-	public void remove(EAMGraphCell cell)
-	{
-		cells.remove(cell);
-	}
-	
-	public void clear()
-	{
-		nextId = 0;
-		cells.clear();
-	}
-
-	private int nextId;
-	Vector cells;
-}
