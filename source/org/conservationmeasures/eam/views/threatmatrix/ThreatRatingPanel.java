65f75ca4547144db421f2763fa929d985269c140
kevins <kevins@localhost>
2006-02-09 19:47:44 +0000

    Make ThreatRatingPanel look more like Nick's prototype

--- source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingPanel.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingPanel.java
@@ -5,40 +5,68 @@
  */
 package org.conservationmeasures.eam.views.threatmatrix;
 
+import java.awt.Color;
 import java.util.Random;
 
-import org.conservationmeasures.eam.icons.ThreatPriorityIcon;
+import javax.swing.Box;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
 import org.conservationmeasures.eam.main.NodePropertiesDialog;
 import org.conservationmeasures.eam.objects.ThreatPriority;
-import org.martus.swing.ParagraphLayout;
+import org.conservationmeasures.eam.project.Project;
 import org.martus.swing.UiComboBox;
 import org.martus.swing.UiLabel;
-import org.martus.swing.UiParagraphPanel;
-import org.martus.swing.UiTextField;
 
-public class ThreatRatingPanel extends UiParagraphPanel
+import com.jhlabs.awt.BasicGridLayout;
+
+public class ThreatRatingPanel extends JPanel
 {
 	public ThreatRatingPanel()
 	{
-//		add(new UiLabel("Threat:"), ParagraphLayout.NEW_PARAGRAPH);
-//		add(new UiLabel("Fishing"));
-//		add(new UiLabel("Target:"), ParagraphLayout.NEW_PARAGRAPH);
-//		add(new UiLabel("Coral reefs"));
-//		addBlankLine();
-		add(new UiLabel("Scope:"), ParagraphLayout.NEW_PARAGRAPH);
-		add(createRatingDropdown());
-		add(new UiLabel("Severity:"), ParagraphLayout.NEW_PARAGRAPH);
-		add(createRatingDropdown());
-		add(new UiLabel("Urgency:"), ParagraphLayout.NEW_PARAGRAPH);
-		add(createRatingDropdown());
-		add(new UiLabel("Rationale:"), ParagraphLayout.NEW_PARAGRAPH);
-		add(new UiTextField(15));
-		addBlankLine();
-		add(new UiLabel("Rating:"), ParagraphLayout.NEW_PARAGRAPH);
+		Box scopeCell = Box.createVerticalBox();
+		scopeCell.add(new UiLabel("Scope:"));
+		scopeCell.add(createRatingDropdown());
+		scopeCell.setBorder(new LineBorder(Color.BLACK, 3));
+		
+		Box severityCell = Box.createVerticalBox();
+		severityCell.add(new UiLabel("Severity:"));
+		severityCell.add(createRatingDropdown());
+		severityCell.setBorder(new LineBorder(Color.BLACK, 3));
+		
+		Box urgencyCell = Box.createVerticalBox();
+		urgencyCell.add(new UiLabel("Urgency:"));
+		urgencyCell.add(createRatingDropdown());
+		urgencyCell.setBorder(new LineBorder(Color.BLACK, 3));
+		
+		Box extraCell = Box.createVerticalBox();
+		extraCell.add(new UiLabel("Criterion 4:"));
+		extraCell.add(createRatingDropdown());
+		extraCell.setBorder(new LineBorder(Color.BLACK, 3));
+
+		
+		ThreatPriority priority = getRandomPriority();
 		UiLabel ratingLabel = new UiLabel();
-		ratingLabel.setText("High");
-		ratingLabel.setIcon(new ThreatPriorityIcon(ThreatPriority.createPriorityHigh()));
-		add(ratingLabel);
+		ratingLabel.setText(priority.toString());
+		ratingLabel.setBackground(Project.getPriorityColor(priority));
+		JPanel ratingPanel = new JPanel();
+		ratingPanel.add(ratingLabel);
+		ratingPanel.setBorder(new LineBorder(Color.BLACK, 3));
+		ratingPanel.setBackground(Project.getPriorityColor(priority));
+
+		JPanel top = new JPanel();
+		top.setLayout(new BasicGridLayout(2, 2));
+		top.add(scopeCell);
+		top.add(severityCell);
+		
+		top.add(urgencyCell);
+		top.add(extraCell);
+
+		Box vBox = Box.createVerticalBox();
+		vBox.add(top);
+		vBox.add(ratingPanel);
+		
+		add(vBox);
 	}
 
 	private UiComboBox createRatingDropdown()
@@ -47,5 +75,10 @@ public class ThreatRatingPanel extends UiParagraphPanel
 		int choice = new Random().nextInt(dropDown.getItemCount());
 		dropDown.setSelectedIndex(choice);
 		return dropDown;
+	}
+	
+	private ThreatPriority getRandomPriority()
+	{
+		return ThreatPriority.createFromInt(new Random().nextInt(4));
 	}
 }
