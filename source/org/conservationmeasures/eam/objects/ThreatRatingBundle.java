fe8aba6c25857f3536221e78b05fb5a46be43831
kevins <kevins@localhost>
2006-02-27 16:54:59 +0000

    Add json capabilities to ThreatRatingBundle

--- source/org/conservationmeasures/eam/objects/ThreatRatingBundle.java
+++ source/org/conservationmeasures/eam/objects/ThreatRatingBundle.java
@@ -6,15 +6,42 @@
 package org.conservationmeasures.eam.objects;
 
 import java.util.HashMap;
+import java.util.Iterator;
+
+import org.json.JSONObject;
 
 public class ThreatRatingBundle
 {
 	public ThreatRatingBundle(int threatIdToUse, int targetIdToUse, int defaultValueIdToUse)
 	{
+		this();
+		
 		threatId = threatIdToUse;
 		targetId = targetIdToUse;
-		defaultValueId = defaultValueIdToUse;
+		defaultValueId = defaultValueIdToUse;		
+	}
+	
+	public ThreatRatingBundle(JSONObject json)
+	{
+		this();
 		
+		threatId = json.getInt(TAG_THREAT_ID);
+		targetId = json.getInt(TAG_TARGET_ID);
+		defaultValueId = json.getInt(TAG_DEFAULT_VALUE_ID);
+		
+		JSONObject values = json.getJSONObject(TAG_VALUES);
+		Iterator iter = values.keys();
+		while(iter.hasNext())
+		{
+			String key = (String)iter.next();
+			int criterionId = Integer.parseInt(key);
+			int valueId = values.getInt(key);
+			setValueId(criterionId, valueId);
+		}
+	}
+	
+	ThreatRatingBundle()
+	{
 		map = new HashMap();
 	}
 	
@@ -40,6 +67,30 @@ public class ThreatRatingBundle
 	{
 		map.put(new Integer(criterionId), new Integer(valueId));
 	}
+	
+	public JSONObject toJson() throws Exception
+	{
+		JSONObject json = new JSONObject();
+		json.put(TAG_THREAT_ID, getThreatId());
+		json.put(TAG_TARGET_ID, getTargetId());
+		json.put(TAG_DEFAULT_VALUE_ID, defaultValueId);
+		
+		JSONObject values = new JSONObject();
+		Iterator iter = map.keySet().iterator();
+		while(iter.hasNext())
+		{
+			Integer criterion = (Integer)iter.next();
+			values.put(criterion.toString(), getValueId(criterion.intValue()));
+		}
+		json.put(TAG_VALUES, values);
+
+		return json;
+	}
+	
+	private static final String TAG_THREAT_ID = "ThreatId";
+	private static final String TAG_TARGET_ID = "TargetId";
+	private static final String TAG_DEFAULT_VALUE_ID = "DefaultValueId";
+	private static final String TAG_VALUES = "Values";
 	
 	int threatId;
 	int targetId;
--- tests/org/conservationmeasures/eam/objects/TestThreatRatingBundle.java
+++ tests/org/conservationmeasures/eam/objects/TestThreatRatingBundle.java
@@ -6,6 +6,7 @@
 package org.conservationmeasures.eam.objects;
 
 import org.conservationmeasures.eam.testall.EAMTestCase;
+import org.json.JSONObject;
 
 public class TestThreatRatingBundle extends EAMTestCase
 {
@@ -30,5 +31,27 @@ public class TestThreatRatingBundle extends EAMTestCase
 		int valueId = 12;
 		bundle.setValueId(criterionId, valueId);
 		assertEquals("didn't remember value?", valueId, bundle.getValueId(criterionId));
+	}
+	
+	public void testJson() throws Exception
+	{
+		int threatId = 5;
+		int targetId = 9;
+		int defaultValueId = 29;
+		int criterion1 = 32;
+		int rating1 = 1;
+		int criterion2 = 57;
+		int rating2 = 2;
+		
+		ThreatRatingBundle bundle = new ThreatRatingBundle(threatId, targetId, defaultValueId);
+		bundle.setValueId(criterion1, rating1);
+		bundle.setValueId(criterion2, rating2);
+		JSONObject json = bundle.toJson();
+		ThreatRatingBundle loaded = new ThreatRatingBundle(json);
+		assertEquals("Didn't load threat id?", bundle.getThreatId(), loaded.getThreatId());
+		assertEquals("Didn't load target id?", bundle.getTargetId(), loaded.getTargetId());
+		assertEquals("Didn't load default?", bundle.getValueId(-999), loaded.getValueId(-999));
+		assertEquals("Didn't load rating 1?", bundle.getValueId(criterion1), loaded.getValueId(criterion1));
+		assertEquals("Didn't load rating 2?", bundle.getValueId(criterion2), loaded.getValueId(criterion2));
 	}
 }
