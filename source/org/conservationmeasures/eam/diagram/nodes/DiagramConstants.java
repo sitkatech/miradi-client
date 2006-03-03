1107ed7c0235db5051d770477164c4024827e8fb
kevins <kevins@localhost>
2006-03-03 19:46:45 +0000

    Tweak indirect factor be less brown

--- source/org/conservationmeasures/eam/diagram/nodes/DiagramConstants.java
+++ source/org/conservationmeasures/eam/diagram/nodes/DiagramConstants.java
@@ -8,7 +8,7 @@ public class DiagramConstants
 	public static final Color COLOR_TARGET = new Color(153, 255, 153);
 	public static final Color COLOR_STRESS = new Color(150, 150, 255);
 	public static final Color COLOR_DIRECT_THREAT = new Color(255, 150, 150);
-	public static final Color COLOR_INDIRECT_FACTOR = new Color(230, 160, 0);
+	public static final Color COLOR_INDIRECT_FACTOR = new Color(255, 190, 0);
 	public static final Color COLOR_INTERVENTION = new Color(255, 255, 0);
 
 }
