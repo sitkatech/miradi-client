b5527619fbd5a5e55e221da3e0ea01dcb060ec99
kevins <kevins@localhost>
2006-03-11 00:01:10 +0000

    Refactor to remove duplicate code

--- source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingSummaryCell.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingSummaryCell.java
@@ -1,10 +1,7 @@
 package org.conservationmeasures.eam.views.threatmatrix;
 
-import java.util.HashSet;
-
 import javax.swing.JPanel;
 
-import org.conservationmeasures.eam.objects.ThreatRatingBundle;
 import org.conservationmeasures.eam.objects.ThreatRatingValueOption;
 import org.conservationmeasures.eam.project.ThreatRatingFramework;
 import org.martus.swing.UiLabel;
@@ -34,36 +31,16 @@ public class ThreatRatingSummaryCell extends JPanel
 	
 	public void dataHasChanged() throws Exception
 	{
+		ThreatRatingValueOption result = null;
+
 		ThreatRatingFramework framework = model.getProject().getThreatRatingFramework();
-		
-		HashSet bundleGroup = new HashSet();
-		
 		if(threatIndex >= 0)
-		{
-			for(int tempTargetIndex = 0; tempTargetIndex < model.getTargetCount(); ++tempTargetIndex)
-			{
-				if(model.isActiveCell(threatIndex, tempTargetIndex))
-				{
-					bundleGroup.add(framework.getBundle(model.getThreatId(threatIndex), model.getTargetId(tempTargetIndex)));
-				}
-			}
-		}
+			result = framework.getThreatThreatRatingValue(model.getThreatId(threatIndex));
 		else
-		{
-			for(int tempThreatIndex = 0; tempThreatIndex < model.getThreatCount(); ++tempThreatIndex)
-			{
-				if(model.isActiveCell(tempThreatIndex, targetIndex))
-				{
-					bundleGroup.add(framework.getBundle(model.getThreatId(tempThreatIndex), model.getTargetId(targetIndex)));
-				}
-			}
-		}
+			result = framework.getTargetThreatRatingValue(model.getTargetId(targetIndex));
 			
-		ThreatRatingBundle[] bundleArray = (ThreatRatingBundle[])bundleGroup.toArray(new ThreatRatingBundle[0]);
-		ThreatRatingValueOption result = framework.getSummaryOfBundles(bundleArray);
 		label.setText(result.getLabel());
 		setBackground(result.getColor());
-		
 	}
 	
 	ThreatMatrixTableModel model;
