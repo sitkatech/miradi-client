d905830e93f801d9ac4bbd615e3b36e34150d27d
kevins <kevins@localhost>
2006-03-20 23:53:36 +0000

    Minor text corrections

--- source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingWizardSeverityText.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingWizardSeverityText.java
@@ -16,8 +16,8 @@ public class ThreatRatingWizardSeverityText extends HtmlBuilder
 			tableRow(
 				tableCell(
 					heading("Rate the Severity of the Selected Threat") +
-					indent(paragraph("Using the rating criteria for the scope on the right, " +
-							"rate the " + anchorTag("DefineSeverity", "severity") + 
+					indent(paragraph("Using the scale shown on the right, rate the " + 
+							anchorTag("DefineSeverity", "severity") + 
 							" of the threat on the target.  " +
 							"You can click on the blue triangle to record any comments or assumptions.") +
 							paragraph(bold("What is the severity of the threat on the target?")) +
