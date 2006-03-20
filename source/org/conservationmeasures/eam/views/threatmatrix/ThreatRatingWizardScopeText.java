91b1f7b2a6d42d5e0571a17bd1359d83025ccd1c
kevins <kevins@localhost>
2006-03-20 23:44:56 +0000

    Update scope text per Nick's requests

--- source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingWizardScopeText.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingWizardScopeText.java
@@ -16,9 +16,10 @@ public class ThreatRatingWizardScopeText extends HtmlBuilder
 			tableRow(
 				tableCell(
 					heading("Rate the Scope of the Selected Threat") +
-					indent(paragraph("Using the rating criteria for the scope on the right, " +
-							"decide what the " + anchorTag("DefineScope", "scope") + 
-							" of the threat is on the target.") +
+					indent(paragraph("Using the scale shown on the right, rate the " + 
+							anchorTag("DefineScope", "scope") + 
+							" of the threat on the target.  " +
+							"You can click on the blue triangle to record any comments or assumptions.") +
 							paragraph(bold("What is the scope of the threat on the target?")) +
 					dropDown("value", optionLabels, currentValue) +
 					newline() +
@@ -34,22 +35,27 @@ public class ThreatRatingWizardScopeText extends HtmlBuilder
 					)
 				) + 
 				tableCell(
-					paragraph(bold("Scope - ") + "Most commonly defined spatially as the proportion " +
-							"of the overall area of a project site or target occurence likely " +
-							"to be affected by a threat under current circumstances (i.e. given " +
-							"the continuation of the existing situation) over the next ten years.") +
+					paragraph(bold("Scope - ") + "Most commonly defined spatially as " +
+							"the geographic scope of impact on the conservation target " +
+							"at the site that can reasonably be expected " +
+							"within ten years under current circumstances " +
+							"(i.e., given the continuation of the existing situation).") +
 					indent(paragraph(bold("Very High: ") + "The threat is likely to be " +
-							"very widespread or pervasive in its scope, and affect the " +
-							"conservation target throughout the target's occurences at the site.") +
-							paragraph(bold("High: ") + "The threat is likely to be widespread " +
-									"in its scope, and affect the conservation target at many " +
-									"of its locations at the site.") +
-							paragraph(bold("Medium: ") + "The threat is likely to be localized " +
-									"in its scope, and affect the conservation target at some " +
-									"of its locations at the site.") +
-							paragraph(bold("Low: ") + "The threat is likely to be very localized " +
-									"in its scope, and affect the conservation target at a " +
-									"limited portion of the target's location at the site.")
+								"very widespread or pervasive in its scope, " +
+								"and affect the conservation target " +
+								"throughout the target's occurrences at the site.") +
+							paragraph(bold("High: ") + "The threat is likely to be " +
+									"widespread in its scope, " +
+									"and affect the conservation target " +
+									"at many of its locations at the site.") +
+							paragraph(bold("Medium: ") + "The threat is likely to be " +
+									"localized in its scope, " +
+									"and affect the conservation target " +
+									"at some of the target's locations at the site.") +
+							paragraph(bold("Low: ") + "The threat is likely to be " +
+									"very localized in its scope, " +
+									"and affect the conservation target " +
+									"at a limited portion of the target's location at the site.")
 					)
 				)
 			)) 
