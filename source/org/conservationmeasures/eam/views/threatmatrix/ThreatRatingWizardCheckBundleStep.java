65b9c725e934471402fcce2556212fff12282a92
kevins <kevins@localhost>
2006-03-21 01:00:39 +0000

    Add link to description of rules for computing a bundle value

--- source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingWizardCheckBundleStep.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingWizardCheckBundleStep.java
@@ -5,11 +5,25 @@
  */
 package org.conservationmeasures.eam.views.threatmatrix;
 
-import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.utils.HtmlBuilder;
 import org.conservationmeasures.eam.utils.HtmlViewer;
 import org.conservationmeasures.eam.utils.HyperlinkHandler;
+import org.martus.swing.UiButton;
+import org.martus.swing.UiLabel;
+import org.martus.swing.Utilities;
 
 public class ThreatRatingWizardCheckBundleStep extends ThreatRatingWizardStep implements HyperlinkHandler
 {
@@ -37,53 +51,109 @@ public class ThreatRatingWizardCheckBundleStep extends ThreatRatingWizardStep im
 
 	public void linkClicked(String linkDescription)
 	{
+		if(linkDescription.equals(SHOW_RULES))
+		{
+			ShowBundleRulesDialog dlg = new ShowBundleRulesDialog();
+			dlg.show();
+		}
 	}
 
 	public void valueChanged(String widget, String newValue)
 	{
 	}
+	
+	static class CheckBundleText extends HtmlBuilder
+	{
+		public static String build()
+		{
+			return font("Arial", 
+				table(tableRow(
+					tableCell(
+							heading("Check Overall Threat Rating") + 
+							paragraph("Once you have completed your ratings for all three criteria, " +
+									"the software automatically calculates the " +
+									anchorTag("DefineOverallThreatRating", "overall threat rating") + 
+									" for this target and threat combination.  " +
+									"You should check whether you agree with this overall rating.  " +
+									"If it does not make sense, " +
+									"go back and adjust the ratings for the specific criteria.") +
+							newline() +
+							indent(table(
+								tableRow(
+									tableCell(button("Back", "&lt; Previous")) +
+									tableCell("&nbsp;") +
+									tableCell(button("Next", "Next &gt;")) 
+									)
+								)) + 
+							newline() +
+							"") +
+					tableCell(
+							paragraph(bold("Overall Threat Rating -- ") +
+									"The e-AM Software calculates threat ratings " +
+									"using a rule-based system for combining the scope, severity, " +
+									"and irreversibility criteria.  " +
+									"These procedures involve specifying rules " +
+									"as to how different parameters should be combined with one another. " +
+									anchorTag(SHOW_RULES, "Click here") + 
+									" for a detailed description of this system.")
+						)
+					))
+				);
+
+		}
+	}
+
+	public class ShowBundleRulesDialog extends JDialog implements ActionListener
+	{
+		public ShowBundleRulesDialog()
+		{
+			Container contents = getContentPane();
+			contents.setLayout(new BorderLayout());
+
+			contents.add(new JScrollPane(new UiLabel("<html>" +
+					"<p><h2>Explanation of How Overall Threat Ratings Are Calculated</h2></p>" +
+					"<p></p>" +
+					"The left-hand matrix shows the rule-based procedure for combining the rankings " +
+					"for the Scope and Severity variables to get a ranking of " +
+					"<a href=''>Threat Magnitude</a>.  <br></br>" +
+					"Under these rules, if a threat is rated “low” on either variable, " +
+					"then the magnitude is “low” overall.  " +
+					"Threat magnitude is then combined with Irreversibility ratings <br></br>" +
+					"using the right-hand matrix to get the summary threat rating." +
+					"</html>")), BorderLayout.BEFORE_FIRST_LINE);
+			ImageIcon image = new ImageIcon("images/BundleRules.jpg");
+			contents.add(new UiLabel("", image, SwingConstants.LEFT), BorderLayout.CENTER);
+			contents.add(createButtonBar(), BorderLayout.AFTER_LAST_LINE);
+
+			pack();
+			setResizable(true);
+			setModal(true);
+
+		}
+
+		private Box createButtonBar()
+		{
+			UiButton closeButton = new UiButton(EAM.text("Button|Close"));
+			closeButton.addActionListener(this);
+			getRootPane().setDefaultButton(closeButton);
+
+			Box buttonBar = Box.createHorizontalBox();
+			Component[] components = new Component[] {Box.createHorizontalGlue(), closeButton};
+			Utilities.addComponentsRespectingOrientation(buttonBar, components);
+			return buttonBar;
+		}
+
+		public void actionPerformed(ActionEvent event)
+		{
+			dispose();
+		}
+		
+	}
+
+	
+	static final String SHOW_RULES = "ShowRules";
 
 	HtmlViewer htmlViewer;
 
 }
 
-class CheckBundleText extends HtmlBuilder
-{
-	public static String build()
-	{
-		return font("Arial", 
-			table(tableRow(
-				tableCell(
-						heading("Check Overall Threat Rating") + 
-						paragraph("Once you have completed your ratings for all three criteria, " +
-								"the software automatically calculates the " +
-								anchorTag("DefineOverallThreatRating", "overall threat rating") + 
-								" for this target and threat combination.  " +
-								"You should check whether you agree with this overall rating.  " +
-								"If it does not make sense, " +
-								"go back and adjust the ratings for the specific criteria.") +
-						newline() +
-						indent(table(
-							tableRow(
-								tableCell(button("Back", "&lt; Previous")) +
-								tableCell("&nbsp;") +
-								tableCell(button("Next", "Next &gt;")) 
-								)
-							)) + 
-						newline() +
-						"") +
-				tableCell(
-						paragraph(bold("Overall Threat Rating -- ") +
-								"The e-AM Software calculates threat ratings " +
-								"using a rule-based system for combining the scope, severity, " +
-								"and irreversibility criteria.  " +
-								"These procedures involve specifying rules " +
-								"as to how different parameters should be combined with one another. " +
-								anchorTag("ShowRules", "Click here") + 
-								" for a detailed description of this system.")
-					)
-				))
-			);
-
-	}
-}
