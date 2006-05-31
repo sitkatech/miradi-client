73f376399b03f69c157522f29cabf0e7f9a9a392
kevins <kevins@localhost>
2006-05-31 17:24:47 +0000

    Confirm deleting in-use resources; clean up fully if they get deleted

--- source/org/conservationmeasures/eam/views/strategicplan/DeleteResource.java
+++ source/org/conservationmeasures/eam/views/strategicplan/DeleteResource.java
@@ -5,13 +5,20 @@
  */
 package org.conservationmeasures.eam.views.strategicplan;
 
+import java.text.ParseException;
+import java.util.Vector;
+
+import org.conservationmeasures.eam.annotations.TaskPool;
+import org.conservationmeasures.eam.commands.Command;
 import org.conservationmeasures.eam.commands.CommandBeginTransaction;
 import org.conservationmeasures.eam.commands.CommandDeleteObject;
 import org.conservationmeasures.eam.commands.CommandEndTransaction;
 import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.EAMObject;
 import org.conservationmeasures.eam.objects.ProjectResource;
+import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.views.ViewDoer;
 
 public class DeleteResource extends ViewDoer
@@ -40,15 +47,69 @@ public class DeleteResource extends ViewDoer
 			return;
 		
 		ProjectResource resource = getResourcePanel().getSelectedResource();
+		
+		int idToRemove = resource.getId();
+		Task[] tasksThatUseThisResource = findTasksThatUseThisResource(idToRemove);
+		if(tasksThatUseThisResource.length > 0)
+		{
+			String[] dialogText = {
+					"This resource is assigned to one or more tasks.", 
+					"Are you sure you want to delete it?", 
+					};
+			String[] buttons = {"Yes", "No", };
+			if(!EAM.confirmDialog("Delete Resource", dialogText, buttons))
+				return;
+		}
+		
+		Command[] removeFromTasks = createCommandsToRemoveResourceFromTasks(idToRemove, tasksThatUseThisResource);
+		
 		getProject().executeCommand(new CommandBeginTransaction());
+		for(int i = 0; i < removeFromTasks.length; ++i)
+		{
+			getProject().executeCommand(removeFromTasks[i]);
+		}
 		int type = resource.getType();
-		int id = resource.getId();
+		int id = idToRemove;
 		getProject().executeCommand(new CommandSetObjectData(type, id, EAMObject.TAG_LABEL, EAMObject.DEFAULT_LABEL));
 		getProject().executeCommand(new CommandSetObjectData(type, id, ProjectResource.TAG_INITIALS, ""));
 		getProject().executeCommand(new CommandSetObjectData(type, id, ProjectResource.TAG_NAME, ""));
 		getProject().executeCommand(new CommandSetObjectData(type, id, ProjectResource.TAG_POSITION, ""));
 		getProject().executeCommand(new CommandDeleteObject(type, id));
 		getProject().executeCommand(new CommandEndTransaction());
+	}
+
+	private Command[] createCommandsToRemoveResourceFromTasks(int idToRemove, Task[] tasksThatUseThisResource) throws CommandFailedException
+	{
+		Command[] removeFromTasks = new Command[tasksThatUseThisResource.length];
+		try
+		{
+			for(int i = 0; i < removeFromTasks.length; ++i)
+			{
+				String tag = Task.TAG_RESOURCE_IDS;
+				Task task = tasksThatUseThisResource[i];
+				removeFromTasks[i] = CommandSetObjectData.createRemoveIdCommand(task, tag, idToRemove);
+			}
+		}
+		catch (ParseException e)
+		{
+			throw new CommandFailedException(e);
+		}
+		return removeFromTasks;
+	}
+	
+	Task[] findTasksThatUseThisResource(int resourceId)
+	{
+		Vector foundTasks = new Vector();
+		TaskPool pool = getProject().getTaskPool();
+		int[] allTaskIds = pool.getIds();
+		for(int i = 0; i < allTaskIds.length; ++i)
+		{
+			Task task = pool.find(allTaskIds[i]);
+			if(task.getResourceIdList().contains(resourceId))
+				foundTasks.add(task);
+		}
+		
+		return (Task[])foundTasks.toArray(new Task[0]);
 	}
 
 	StrategicPlanView view;
