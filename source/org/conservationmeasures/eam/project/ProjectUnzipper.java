61629c8d7afb397d29bfbdb15f9ed4aff8bb310b
miker <miker@localhost>
2006-06-30 17:48:11 +0000

    recheck in

--- source/org/conservationmeasures/eam/project/ProjectUnzipper.java
+++ source/org/conservationmeasures/eam/project/ProjectUnzipper.java
@@ -17,7 +17,7 @@ public class ProjectUnzipper
 			if(entry == null)
 				break;
 			String name = entry.getName();
-			if(isTopLevelFile(name))
+			if(hasTopLevelFile(name))
 				return false;
 			if(hasLeadingSlash(name))
 				return false;
@@ -38,7 +38,7 @@ public class ProjectUnzipper
 		return name.indexOf('/');
 	}
 
-	private static boolean isTopLevelFile(String name)
+	private static boolean hasTopLevelFile(String name)
 	{
 		if(findSlash(name) < 0)
 			return true;
