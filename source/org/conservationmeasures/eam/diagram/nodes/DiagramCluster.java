7992ffec6b5fba19e4214da694815c6f33429aad
kevins <kevins@localhost>
2006-07-06 19:22:55 +0000

    Make groups opaque, which fixes several behavior problems

--- source/org/conservationmeasures/eam/diagram/nodes/DiagramCluster.java
+++ source/org/conservationmeasures/eam/diagram/nodes/DiagramCluster.java
@@ -9,12 +9,14 @@ import java.awt.Color;
 import java.awt.Rectangle;
 
 import org.conservationmeasures.eam.objects.ConceptualModelCluster;
+import org.jgraph.graph.GraphConstants;
 
 public class DiagramCluster extends DiagramNode
 {
 	public DiagramCluster(ConceptualModelCluster cmGroup)
 	{
 		super(cmGroup);
+		GraphConstants.setGroupOpaque(getAttributes(), true);
 	}
 
 	public Color getColor()
