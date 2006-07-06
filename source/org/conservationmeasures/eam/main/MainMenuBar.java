3eee8cb6b3821d6c7eb5da263730618ad1a92e02
kevins <kevins@localhost>
2006-07-06 17:58:43 +0000

    Remove bogus second copy of the InsertCluster menu item

--- source/org/conservationmeasures/eam/main/MainMenuBar.java
+++ source/org/conservationmeasures/eam/main/MainMenuBar.java
@@ -192,11 +192,10 @@ public class MainMenuBar extends JMenuBar
 		menu.add(createJMenuItemCenterLocation(actions.get(ActionInsertIndirectFactor.class)));
 		menu.add(createJMenuItemCenterLocation(actions.get(ActionInsertDirectThreat.class)));
 		menu.add(createJMenuItemCenterLocation(actions.get(ActionInsertTarget.class)));
-		menu.add(createJMenuItemCenterLocation(actions.get(ActionInsertCluster.class)));
 		menu.addSeparator();
 		menu.add(new JMenuItem(actions.get(ActionInsertConnection.class)));
 		menu.addSeparator();
-		menu.add(new JMenuItem(actions.get(ActionInsertCluster.class)));
+		menu.add(createJMenuItemCenterLocation(actions.get(ActionInsertCluster.class)));
 		return menu;
 	}
 	
