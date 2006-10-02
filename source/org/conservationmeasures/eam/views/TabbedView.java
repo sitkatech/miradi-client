f6a58754d2171918d8be7e1b2b462550c1bcbf59
kevins <kevins@localhost>
2006-10-02 16:41:38 +0000

    Extract TabbedView functionality out of MonitoringView so it can be reused

---
+++ source/org/conservationmeasures/eam/views/TabbedView.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.views;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.conservationmeasures.eam.commands.Command;
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
+import org.conservationmeasures.eam.main.CommandExecutedEvent;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.main.MainWindow;
+import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.ViewData;
+import org.conservationmeasures.eam.views.umbrella.UmbrellaView;
+
+abstract public class TabbedView extends UmbrellaView
+{
+	public TabbedView(MainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+
+		tabs = new JTabbedPane();
+		add(tabs, BorderLayout.CENTER);
+		tabs.addChangeListener(new TabChangeListener());
+	}
+
+	public abstract void createTabs() throws Exception;
+	public abstract void deleteTabs() throws Exception;
+	
+	public void becomeActive() throws Exception
+	{
+		ignoreTabChanges = true;
+		try
+		{
+			createTabs();
+			
+			tabs.setSelectedIndex(previousCurrentTab);
+		}
+		finally
+		{
+			ignoreTabChanges = false;
+		}
+	}
+
+	public void becomeInactive() throws Exception
+	{
+		previousCurrentTab = currentTab;
+
+		ignoreTabChanges = true;
+		try
+		{
+			tabs.removeAll();
+			deleteTabs();
+		}
+		finally
+		{
+			ignoreTabChanges = false;
+		}
+	}
+	
+	public void addTab(String name, Component contents)
+	{
+		tabs.add(name, contents);
+	}
+
+	public void setTab(int newTab)
+	{
+		tabs.setSelectedIndex(newTab);
+	}
+	
+	public void commandExecuted(CommandExecutedEvent event)
+	{
+		super.commandExecuted(event);
+		Command rawCommand = event.getCommand();
+		if(!rawCommand.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
+			return;
+		CommandSetObjectData cmd = (CommandSetObjectData)rawCommand;
+		try
+		{
+			if(cmd.getObjectType() != ObjectType.VIEW_DATA)
+				return;
+			if(!cmd.getObjectId().equals(getViewData().getId()))
+				return;
+			if(!cmd.getFieldTag().equals(ViewData.TAG_CURRENT_TAB))
+				return;
+			EAM.logVerbose("MonitoringView.commandExecuted: " + cmd.toString());
+			setTab(new Integer(cmd.getDataValue()).intValue());
+		}
+		catch(Exception e)
+		{
+			EAM.logException(e);
+		}
+		
+		
+	}
+
+	public void commandUndone(CommandExecutedEvent event)
+	{
+		super.commandUndone(event);
+		Command rawCommand = event.getCommand();
+		if(!rawCommand.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
+			return;
+		CommandSetObjectData cmd = (CommandSetObjectData)rawCommand;
+		try
+		{
+			if(cmd.getObjectType() != ObjectType.VIEW_DATA)
+				return;
+			if(!cmd.getObjectId().equals(getViewData().getId()))
+				return;
+			if(!cmd.getFieldTag().equals(ViewData.TAG_CURRENT_TAB))
+				return;
+			EAM.logVerbose("MonitoringView.commandUndone: " + cmd.toString());
+			setTab(new Integer(cmd.getPreviousDataValue()).intValue());
+		}
+		catch(Exception e)
+		{
+			EAM.logException(e);
+		}
+	}
+
+	class TabChangeListener implements ChangeListener
+	{
+		public void stateChanged(ChangeEvent event)
+		{
+			int newTab = tabs.getSelectedIndex();
+			if(!ignoreTabChanges)
+				recordTabChangeCommand(newTab);
+
+			currentTab = newTab;
+			getMainWindow().getActions().updateActionStates();
+		}
+
+		private void recordTabChangeCommand(int newTab)
+		{
+			EAM.logVerbose("TabChangeListener.stateChanged");
+			closeActivePropertiesDialog();
+			try
+			{
+				Command tabChangeCommand = createTabChangeCommand(newTab);
+				getViewData().setCurrentTab(newTab);
+				if(!getProject().isExecutingACommand())
+				{
+					getProject().recordCommand(tabChangeCommand);
+					EAM.logVerbose("TabChangeListener.stateChanged recorded command");
+				}
+			}
+			catch (Exception e)
+			{
+				EAM.logException(e);
+				EAM.errorDialog("Unexpected error");
+			}
+		}
+		
+		Command createTabChangeCommand(int newTab) throws Exception
+		{
+			CommandSetObjectData cmd = new CommandSetObjectData(ObjectType.VIEW_DATA, getViewData().getId(), ViewData.TAG_CURRENT_TAB, Integer.toString(newTab));
+			cmd.setPreviousDataValue(Integer.toString(currentTab));
+			return cmd;
+		}
+
+	}
+	
+	JTabbedPane tabs;
+	int currentTab;
+	int previousCurrentTab;
+	boolean ignoreTabChanges;
+	
+}
--- source/org/conservationmeasures/eam/views/monitoring/MonitoringView.java
+++ source/org/conservationmeasures/eam/views/monitoring/MonitoringView.java
@@ -5,36 +5,21 @@
  */
 package org.conservationmeasures.eam.views.monitoring;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JTabbedPane;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
 import org.conservationmeasures.eam.actions.ActionCreateIndicator;
 import org.conservationmeasures.eam.actions.ActionDeleteIndicator;
 import org.conservationmeasures.eam.actions.ActionModifyIndicator;
-import org.conservationmeasures.eam.commands.Command;
-import org.conservationmeasures.eam.commands.CommandSetObjectData;
-import org.conservationmeasures.eam.main.CommandExecutedEvent;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
-import org.conservationmeasures.eam.objecthelpers.ObjectType;
-import org.conservationmeasures.eam.objects.ViewData;
+import org.conservationmeasures.eam.views.TabbedView;
 import org.conservationmeasures.eam.views.umbrella.CreateIndicator;
-import org.conservationmeasures.eam.views.umbrella.UmbrellaView;
 
-public class MonitoringView extends UmbrellaView
+public class MonitoringView extends TabbedView
 {
 	public MonitoringView(MainWindow mainWindowToUse)
 	{
 		super(mainWindowToUse);
 		setToolBar(new MonitoringToolBar(mainWindowToUse.getActions()));
 
-		tabs = new JTabbedPane();
-		add(tabs, BorderLayout.CENTER);
-		tabs.addChangeListener(new TabChangeListener());
-		
 		addMonitoringPlanDoersToMap();
 	}
 
@@ -48,29 +33,15 @@ public class MonitoringView extends UmbrellaView
 		return "Monitoring Plan";
 	}
 
-	public void becomeActive() throws Exception
+	public void createTabs() throws Exception
 	{
-		int mostRecentTabIndex = currentTab;
-		
-		ignoreTabChanges = true;
-		try
-		{
-			tabs.removeAll();
-	
-			monitoringPanel = new MonitoringPanel(getProject());
-			indicatorManagementPanel = new IndicatorManagementPanel(this);
-			tabs.add(EAM.text("Monitoring Plan"), monitoringPanel);
-			tabs.add(EAM.text("Indicators"), indicatorManagementPanel);
-			
-			tabs.setSelectedIndex(mostRecentTabIndex);
-		}
-		finally
-		{
-			ignoreTabChanges = false;
-		}
+		monitoringPanel = new MonitoringPanel(getProject());
+		indicatorManagementPanel = new IndicatorManagementPanel(this);
+		addTab(EAM.text("Monitoring Plan"), monitoringPanel);
+		addTab(EAM.text("Indicators"), indicatorManagementPanel);
 	}
 
-	public void becomeInactive() throws Exception
+	public void deleteTabs() throws Exception
 	{
 		monitoringPanel = null;
 		indicatorManagementPanel = null;
@@ -86,110 +57,7 @@ public class MonitoringView extends UmbrellaView
 		addDoerToMap(ActionCreateIndicator.class, new CreateIndicator());
 		addDoerToMap(ActionModifyIndicator.class, new ModifyIndicator());
 		addDoerToMap(ActionDeleteIndicator.class, new DeleteIndicator());
-
-		
 	}
-	
-	private void setTab(int newTab)
-	{
-		tabs.setSelectedIndex(newTab);
-	}
-	
-	public void commandExecuted(CommandExecutedEvent event)
-	{
-		super.commandExecuted(event);
-		Command rawCommand = event.getCommand();
-		if(!rawCommand.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
-			return;
-		CommandSetObjectData cmd = (CommandSetObjectData)rawCommand;
-		try
-		{
-			if(cmd.getObjectType() != ObjectType.VIEW_DATA)
-				return;
-			if(!cmd.getObjectId().equals(getViewData().getId()))
-				return;
-			if(!cmd.getFieldTag().equals(ViewData.TAG_CURRENT_TAB))
-				return;
-			EAM.logVerbose("MonitoringView.commandExecuted: " + cmd.toString());
-			setTab(new Integer(cmd.getDataValue()).intValue());
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-		}
-		
-		
-	}
-
-	public void commandUndone(CommandExecutedEvent event)
-	{
-		super.commandUndone(event);
-		Command rawCommand = event.getCommand();
-		if(!rawCommand.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
-			return;
-		CommandSetObjectData cmd = (CommandSetObjectData)rawCommand;
-		try
-		{
-			if(cmd.getObjectType() != ObjectType.VIEW_DATA)
-				return;
-			if(!cmd.getObjectId().equals(getViewData().getId()))
-				return;
-			if(!cmd.getFieldTag().equals(ViewData.TAG_CURRENT_TAB))
-				return;
-			EAM.logVerbose("MonitoringView.commandUndone: " + cmd.toString());
-			setTab(new Integer(cmd.getPreviousDataValue()).intValue());
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-		}
-	}
-
-	class TabChangeListener implements ChangeListener
-	{
-		public void stateChanged(ChangeEvent event)
-		{
-			int newTab = tabs.getSelectedIndex();
-			if(!ignoreTabChanges)
-				recordTabChangeCommand(newTab);
-
-			currentTab = newTab;
-			getMainWindow().getActions().updateActionStates();
-		}
-
-		private void recordTabChangeCommand(int newTab)
-		{
-			EAM.logVerbose("TabChangeListener.stateChanged");
-			closeActivePropertiesDialog();
-			try
-			{
-				Command tabChangeCommand = createTabChangeCommand(newTab);
-				getViewData().setCurrentTab(newTab);
-				if(!getProject().isExecutingACommand())
-				{
-					getProject().recordCommand(tabChangeCommand);
-					EAM.logVerbose("TabChangeListener.stateChanged recorded command");
-				}
-			}
-			catch (Exception e)
-			{
-				EAM.logException(e);
-				EAM.errorDialog("Unexpected error");
-			}
-		}
-		
-		Command createTabChangeCommand(int newTab) throws Exception
-		{
-			CommandSetObjectData cmd = new CommandSetObjectData(ObjectType.VIEW_DATA, getViewData().getId(), ViewData.TAG_CURRENT_TAB, Integer.toString(newTab));
-			cmd.setPreviousDataValue(Integer.toString(currentTab));
-			return cmd;
-		}
-
-	}
-	
-	JTabbedPane tabs;
-	int currentTab;
-	boolean ignoreTabChanges;
 	
 	MonitoringPanel monitoringPanel;
 	IndicatorManagementPanel indicatorManagementPanel;
