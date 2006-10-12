b7eec876111db4b93ae862a2881413a4fc80698f
miker <miker@localhost>
2006-10-12 22:30:36 +0000

    html refactor

--- source/org/conservationmeasures/eam/views/summary/InterviewWizardDevelopObjectivesBStep.java
+++ source/org/conservationmeasures/eam/views/summary/InterviewWizardDevelopObjectivesBStep.java
@@ -5,7 +5,6 @@
  */
 package org.conservationmeasures.eam.views.summary;
 
-import org.conservationmeasures.eam.utils.HtmlBuilder;
 import org.conservationmeasures.eam.views.umbrella.WizardPanel;
 
 public class InterviewWizardDevelopObjectivesBStep extends InterviewWizardStep
@@ -16,55 +15,10 @@ public class InterviewWizardDevelopObjectivesBStep extends InterviewWizardStep
 		super(wizardToUse);
 	}
 	
-	public String getText()
+	public String getResourceFileName()
 	{
-		return DevelopObjectivesBText.build();
+		return HTML_FILENAME;
 	}
-
-}
-
-class DevelopObjectivesBText extends HtmlBuilder
-{
-	public static String build()
-	{
-		return font("Arial", 
-				wizardFrame(tableRow(
-				tableCell(
-						heading("Step 2.1.  Plan Your Actions") + 
-						paragraph(bold("Principle 2.1 A.  Develop clear goal and objectives")) +
-						paragraph(bold("Task 3. Develop Objectives")) +
-						newline() +
-						paragraph("The threat you have selected to address is: 'Cutting Trees'") +
-						paragraph("Write a draft " +
-			definition("Objective", "objective", "") +
-			" about how you hope to change this factor.  " +
-			"Then modify this draft objective to make sure it meets the criteria of being " +
-			definition("ImpactOriented", "impact oriented", "") +
-			", " +
-			definition("Measureable", "measureable", "") +
-			", " +
-			definition("Specific", "specific", "") +
-			", " +
-			definition("Realistic", "realistic", "") +
-			", " +
-			definition("TimeLimited", "time-limited", "") +
-			", and " +
-			definition("Achievable", "achievable", "") +
-			".") +
-			editor(8, 40) +
-						indent(table(
-							tableRow(
-								tableCell(button("Back", "&lt; Previous")) +
-								tableCell("&nbsp;") +
-								tableCell(button("Next", "Next &gt;")) 
-								)
-							)) + 
-						newline() +
-						"") +
-				tableCell(
-						smallHeading("Navigation Hints for the Interview") + 
-					""))
-			));
-
-	}
+	
+	String HTML_FILENAME = "DevelopObjectivesBStep.html";
 }
