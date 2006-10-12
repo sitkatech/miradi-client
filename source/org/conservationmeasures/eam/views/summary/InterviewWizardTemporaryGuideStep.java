df6a5118b9635d3850a97b169b97967250b6db29
miker <miker@localhost>
2006-10-12 22:39:22 +0000

    html refactor

--- source/org/conservationmeasures/eam/views/summary/InterviewWizardTemporaryGuideStep.java
+++ source/org/conservationmeasures/eam/views/summary/InterviewWizardTemporaryGuideStep.java
@@ -5,7 +5,6 @@
  */
 package org.conservationmeasures.eam.views.summary;
 
-import org.conservationmeasures.eam.utils.HtmlBuilder;
 import org.conservationmeasures.eam.views.umbrella.WizardPanel;
 
 public class InterviewWizardTemporaryGuideStep extends InterviewWizardStep
@@ -15,43 +14,10 @@ public class InterviewWizardTemporaryGuideStep extends InterviewWizardStep
 		super(wizardToUse);
 	}
 	
-	public String getText()
+	public String getResourceFileName()
 	{
-		return TemporaryGuide.build();
+		return HTML_FILENAME;
 	}
-
-}
-
-class TemporaryGuide extends HtmlBuilder
-{
-	public static String build()
-	{
-		return font("Arial", 
-			wizardFrame(tableRow(
-				tableCell(
-						heading("Interview (demo screen shots only)") + 
-						paragraph("The following interview screens will give you an idea of how the interview " +
-			"will, in future versions, walk you through developing your project.") +
-						paragraph(bold("To continue with the Interview demo, click on the 'Next' button.")) +
-						newline() +
-						indent(table(
-							tableRow(
-								tableCell(button("Back", "&lt; Previous")) +
-								tableCell("&nbsp;") +
-								tableCell(button("Next", "Next &gt;")) 
-								)
-							)) + 
-						newline() +
-						"") +
-				tableCell(
-						smallHeading("Navigation Hints for the Interview") + 
-						smallParagraph(("" + 
-			"If you want to start working with a project, " +
-			"switch to the Diagram View and start " +
-			"building a conceptual model of your project. ") + 
-					""))
-			))
-		);
-
-	}
+	
+	String HTML_FILENAME = "TemporaryGuideStep.html";
 }
