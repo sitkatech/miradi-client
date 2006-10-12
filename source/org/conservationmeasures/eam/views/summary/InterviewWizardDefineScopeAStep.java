7ffe2cb3407550193030ff0ccb19b623270aaceb
miker <miker@localhost>
2006-10-12 21:43:24 +0000

    html refactor

--- source/org/conservationmeasures/eam/views/summary/InterviewWizardDefineScopeAStep.java
+++ source/org/conservationmeasures/eam/views/summary/InterviewWizardDefineScopeAStep.java
@@ -5,7 +5,6 @@
  */
 package org.conservationmeasures.eam.views.summary;
 
-import org.conservationmeasures.eam.utils.HtmlBuilder;
 import org.conservationmeasures.eam.views.umbrella.WizardPanel;
 
 public class InterviewWizardDefineScopeAStep extends InterviewWizardStep
@@ -15,48 +14,11 @@ public class InterviewWizardDefineScopeAStep extends InterviewWizardStep
 	{
 		super(wizardToUse);
 	}
-
-	public String getText()
+	
+	public String getResourceFileName()
 	{
-		return DefineScopeAText.build();
+		return HTML_FILENAME;
 	}
-
-}
-
-class DefineScopeAText extends HtmlBuilder
-{
-	public static String build()
-	{
-		return font("Arial", 
-				wizardFrame(tableRow(
-				tableCell(
-						heading("Step 1.  Conceptualize") + 
-						paragraph(bold("Principle 1A.  Be clear and specific about the issue or problem")) +
-						paragraph(bold("Task 2. Define the scope of the area or theme")) +
-						newline() +
-						paragraph("Most conservation projects will focus on a defined geographic " +
-			definition("ProjectArea", "project area", "") + 
-			"that contains the biodiversity that is of interest.  " + 
-			"In a few cases, a conservation project may not focus on biodiversity in a specific area, " + 
-			"but instead will have a " +
-			definition("Theme", "theme", "") +
-			"that focuses on a population of wide-ranging animals, " + 
-			"such as migratory birds.") +
-						paragraph("Describe in a few sentences the project area or theme for your project:") +
-						editor(8, 40) +
-						indent(table(
-							tableRow(
-								tableCell(button("Back", "&lt; Previous")) +
-								tableCell("&nbsp;") +
-								tableCell(button("Next", "Next &gt;")) 
-								)
-							)) + 
-						newline() +
-						"") +
-				tableCell(
-						smallHeading("Navigation Hints for the Interview") + 
-					""))
-			));
-
-	}
+	
+	String HTML_FILENAME = "DefineScopeAStep.html";
 }
