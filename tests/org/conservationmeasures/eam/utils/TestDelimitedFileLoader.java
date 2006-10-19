3d8b8d5ac4662b929cca58717de46e5764d2923b
richardn <richardn@localhost>
2006-10-19 19:14:35 +0000

    Added additional test to test for multi rows and content checks

--- tests/org/conservationmeasures/eam/utils/TestDelimitedFileLoader.java
+++ tests/org/conservationmeasures/eam/utils/TestDelimitedFileLoader.java
@@ -56,6 +56,34 @@ public class TestDelimitedFileLoader extends EAMTestCase
 		assertEquals(2, ((Vector)myData.elementAt(0)).size());
 	}
 	
+	public void testElementContentWithTrim() throws IOException
+	{
+		BufferedReader reader = new BufferedReader( new StringReader("xxxx \t     \"abcd 123\"   ") );
+		Vector myData = DelimitedFileLoader.getDelimitedContents(reader);
+		String data = (String)((Vector)myData.elementAt(0)).get(1);
+		assertEquals("abcd 123", data);
+	}
+	
+
+	public void testElementTwoRows() throws IOException
+	{
+		BufferedReader reader = new BufferedReader( new StringReader("124 \t     \"abcd 234\"    \n 345 \t     \"efgc 678\"  ") );
+		Vector myData = DelimitedFileLoader.getDelimitedContents(reader);
+		assertEquals(2, myData.size());
+	}
+	
+	public void testElementTwoContent() throws IOException
+	{
+		BufferedReader reader = new BufferedReader( new StringReader("124 \t     \"abcd 234\"    \n 345 \t     \"efgc 678\"  ") );
+		Vector myData = DelimitedFileLoader.getDelimitedContents(reader);
+		String data = (String)((Vector)myData.elementAt(0)).get(1);
+		assertEquals("abcd 234", data);
+		String data2 = (String)((Vector)myData.elementAt(1)).get(1);
+		assertEquals("efgc 678", data2);
+	}
+	
+	
+	
 	public void testThreatFileInMain() throws Exception
 	{
 		Vector myData = DelimitedFileLoader.getDelimitedContents("ThreatTaxonomies.txt");
