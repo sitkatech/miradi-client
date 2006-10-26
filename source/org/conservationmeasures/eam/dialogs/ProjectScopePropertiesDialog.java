991fa99b6acf6d2debefcb2fb8c3478bacfde969
nimaa <nimaa@localhost>
2006-10-26 22:07:55 +0000

    "esc" now closes the dialog.

--- source/org/conservationmeasures/eam/dialogs/ProjectScopePropertiesDialog.java
+++ source/org/conservationmeasures/eam/dialogs/ProjectScopePropertiesDialog.java
@@ -2,14 +2,12 @@ package org.conservationmeasures.eam.dialogs;
 
 import java.awt.Container;
 
-import javax.swing.JDialog;
-
 import org.conservationmeasures.eam.diagram.EAMGraphCell;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 import org.conservationmeasures.eam.project.Project;
 
-public class ProjectScopePropertiesDialog extends JDialog
+public class ProjectScopePropertiesDialog extends ModelessDialogWithClose
 {
 	public ProjectScopePropertiesDialog(MainWindow parent, Project project, EAMGraphCell scope)
 	{
