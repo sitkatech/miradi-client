3711100d7e5592f73b25aae78fd2163a27ad2a25
kevins <kevins@localhost>
2006-10-26 01:31:05 +0000

    Pull standard wizard features up to WizardPanel

--- source/org/conservationmeasures/eam/views/diagram/wizard/DiagramWizardPanel.java
+++ source/org/conservationmeasures/eam/views/diagram/wizard/DiagramWizardPanel.java
@@ -12,53 +12,25 @@ import org.conservationmeasures.eam.actions.jump.ActionJumpIdentifyDirectThreats
 import org.conservationmeasures.eam.actions.jump.ActionJumpIdentifyIndirectThreats;
 import org.conservationmeasures.eam.actions.jump.ActionJumpIdentifyTargets;
 import org.conservationmeasures.eam.views.umbrella.WizardPanel;
-import org.conservationmeasures.eam.views.umbrella.WizardStep;
 
 public class DiagramWizardPanel extends WizardPanel
 {
 	public DiagramWizardPanel() throws Exception
 	{
-		steps = new WizardStep[STEP_COUNT];
-
-		steps[OVERVIEW] = new DiagramWizardOverviewStep(this);
-		steps[PROJECT_SCOPE] = new DiagramWizardProjectScopeStep(this);
-		steps[VISION] = new DiagramWizardVisionStep(this);
-		steps[CONSERVATION_TARGET] = new DiagramWizardDefineTargetsStep(this);
-		steps[REVIEW_AND_MODIFY_TARGETS] = new DiagramWizardReviewAndModifyTargetsStep(this);
-		steps[IDENTIFY_DIRECT_THREATS] = new DiagramWizardIdentifyDirectThreatStep(this);
-		steps[LINK_DIRECT_THREATS_TO_TARGETS] = new DiagramWizardLinkDirectThreatsToTargetsStep(this);
-		steps[IDENTIFY_INDIRECT_THREATS] = new DiagramWizardIdentifyIndirectThreatStep(this);
-		steps[CONSTRUCT_CHAINS_HOWTO] = new DiagramWizardConstructChainsStep(this);
-		steps[REVIEW_MODEL_AND_ADJUST] = new DiagramWizardReviewModelAndAdjustStep(this);
+		OVERVIEW = addStep(new DiagramWizardOverviewStep(this));
+		PROJECT_SCOPE = addStep(new DiagramWizardProjectScopeStep(this));
+		VISION = addStep(new DiagramWizardVisionStep(this));
+		CONSERVATION_TARGET = addStep(new DiagramWizardDefineTargetsStep(this));
+		REVIEW_AND_MODIFY_TARGETS = addStep(new DiagramWizardReviewAndModifyTargetsStep(this));
+		IDENTIFY_DIRECT_THREATS = addStep(new DiagramWizardIdentifyDirectThreatStep(this));
+		LINK_DIRECT_THREATS_TO_TARGETS = addStep(new DiagramWizardLinkDirectThreatsToTargetsStep(this));
+		IDENTIFY_INDIRECT_THREATS = addStep(new DiagramWizardIdentifyIndirectThreatStep(this));
+		CONSTRUCT_CHAINS_HOWTO = addStep(new DiagramWizardConstructChainsStep(this));
+		REVIEW_MODEL_AND_ADJUST = addStep(new DiagramWizardReviewModelAndAdjustStep(this));
 		
 		setStep(OVERVIEW);
 	}
 
-	public void next() throws Exception
-	{
-		int nextStep = currentStep + 1;
-		if(nextStep >= steps.length)
-			nextStep = 0;
-		
-		setStep(nextStep);
-	}
-	
-	public void previous() throws Exception
-	{
-		int nextStep = currentStep - 1;
-		if(nextStep < 0)
-			return;
-		
-		setStep(nextStep);
-	}
-	
-	public void setStep(int newStep) throws Exception
-	{
-		currentStep = newStep;
-		steps[currentStep].refresh();
-		setContents(steps[currentStep]);
-	}
-	
 	public void jump(Class stepMarker) throws Exception
 	{
 		if(stepMarker.equals(ActionJumpDefineScope.class))
@@ -77,27 +49,15 @@ public class DiagramWizardPanel extends WizardPanel
 			throw new RuntimeException("Step not in this view: " + stepMarker);
 	}
 	
-	public void refresh() throws Exception
-	{
-		for(int i = 0; i < steps.length; ++i)
-			steps[i].refresh();
-		validate();
-	}
-	
 
-	final static int OVERVIEW = 0;
-	final static int PROJECT_SCOPE = 1;
-	final static int VISION = 2;
-	final static int CONSERVATION_TARGET = 3;
-	final static int REVIEW_AND_MODIFY_TARGETS = 4;
-	final static int IDENTIFY_DIRECT_THREATS = 5;
-	final static int LINK_DIRECT_THREATS_TO_TARGETS = 6;
-	final static int IDENTIFY_INDIRECT_THREATS = 7;
-	final static int CONSTRUCT_CHAINS_HOWTO = 8;
-	final static int REVIEW_MODEL_AND_ADJUST = 9;
-	
-	final static int STEP_COUNT = 10;
-	
-	WizardStep[] steps;
-	int currentStep;
+	int OVERVIEW;
+	int PROJECT_SCOPE;
+	int VISION;
+	int CONSERVATION_TARGET;
+	int REVIEW_AND_MODIFY_TARGETS;
+	int IDENTIFY_DIRECT_THREATS;
+	int LINK_DIRECT_THREATS_TO_TARGETS;
+	int IDENTIFY_INDIRECT_THREATS;
+	int CONSTRUCT_CHAINS_HOWTO;
+	int REVIEW_MODEL_AND_ADJUST;
 }
--- source/org/conservationmeasures/eam/views/umbrella/WizardPanel.java
+++ source/org/conservationmeasures/eam/views/umbrella/WizardPanel.java
@@ -7,6 +7,9 @@ package org.conservationmeasures.eam.views.umbrella;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JPanel;
 
@@ -15,6 +18,8 @@ public class WizardPanel extends JPanel
 	public WizardPanel()
 	{
 		super(new BorderLayout());
+		
+		steps = new WizardStep[0];
 	}
 
 	public void setContents(JPanel contents)
@@ -28,17 +33,52 @@ public class WizardPanel extends JPanel
 	
 	public void next() throws Exception
 	{
+		int nextStep = currentStep + 1;
+		if(nextStep >= steps.length)
+			nextStep = 0;
+		
+		setStep(nextStep);
 	}
 	
 	public void previous() throws Exception
 	{
+		int nextStep = currentStep - 1;
+		if(nextStep < 0)
+			return;
+		
+		setStep(nextStep);
 	}
-
+	
 	private void allowSplitterToHideUsCompletely()
 	{
 		setMinimumSize(new Dimension(0, 0));
 	}
 	
+	public void setStep(int newStep) throws Exception
+	{
+		currentStep = newStep;
+		steps[currentStep].refresh();
+		setContents(steps[currentStep]);
+	}
+	
+	public void refresh() throws Exception
+	{
+		for(int i = 0; i < steps.length; ++i)
+			steps[i].refresh();
+		validate();
+	}
+	
+	public int addStep(WizardStep step)
+	{
+		List existingSteps = Arrays.asList(steps);
+		Vector newSteps = new Vector(existingSteps);
+		newSteps.add(step);
+		steps = (WizardStep[])newSteps.toArray(new WizardStep[0]);
+		return (steps.length - 1);
+	}
+	
 
+	WizardStep[] steps;
+	int currentStep;
 }
 
