92feb36b916e501731e98261eabef802d471c6c2
richardn <richardn@localhost>
2006-10-31 21:41:13 +0000

    Part fix of summary bundel panel

--- source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingPanel.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/ThreatRatingPanel.java
@@ -51,6 +51,7 @@ public class ThreatRatingPanel extends Box
 		ratingSummaryLabel = new UiLabel();
 		ratingSummaryLabel.setVerticalAlignment(JLabel.CENTER);
 		ratingSummaryLabel.setHorizontalAlignment(JLabel.CENTER);
+		ratingSummaryLabel.setPreferredSize(new Dimension(50,100));
 		
 		createSummaryPanel();
 
