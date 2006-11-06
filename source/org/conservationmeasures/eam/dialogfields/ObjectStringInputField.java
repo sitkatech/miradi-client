3890833780c594214ccd642b1190c73ce34667ba
richardn <richardn@localhost>
2006-11-06 18:27:35 +0000

    fixed size text field applied to String Field Input Object Class

--- source/org/conservationmeasures/eam/dialogfields/ObjectStringInputField.java
+++ source/org/conservationmeasures/eam/dialogfields/ObjectStringInputField.java
@@ -5,15 +5,23 @@
  */
 package org.conservationmeasures.eam.dialogfields;
 
+import javax.swing.JTextField;
+
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.project.Project;
 import org.martus.swing.UiTextArea;
+import org.martus.swing.UiTextField;
 
 
 public class ObjectStringInputField extends ObjectTextInputField
 {
 	public ObjectStringInputField(Project projectToUse, int objectType, BaseId objectId, String tag)
 	{
-		super(projectToUse, objectType, objectId, tag, new UiTextArea(1, 50));
+		super(projectToUse, objectType, objectId, tag, new UiTextField());
+		UiTextArea textArea = new UiTextArea(1,50);
+		JTextField jTextArea = (JTextField)getComponent();
+		jTextArea.setBorder(textArea.getBorder());
+		jTextArea.setFont(textArea.getFont());
+		jTextArea.setPreferredSize(textArea.getPreferredSize());
 	}
 }
