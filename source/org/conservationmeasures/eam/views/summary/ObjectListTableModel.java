a82e6bf441e087118a5ed3d6721f122c38e8a4e3
kevins <kevins@localhost>
2006-11-08 22:55:49 +0000

    Refactor extract ObjectListTableModel out of TeamModel

---
+++ source/org/conservationmeasures/eam/views/summary/ObjectListTableModel.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.views.summary;
+
+import java.text.ParseException;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.conservationmeasures.eam.ids.BaseId;
+import org.conservationmeasures.eam.ids.IdList;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objects.EAMObject;
+import org.conservationmeasures.eam.project.Project;
+
+public class ObjectListTableModel extends AbstractTableModel
+{
+	public ObjectListTableModel(Project projectToUse, int objectType, BaseId objectId, 
+			String idListFieldTag, int listedItemType, String tableColumnTag)
+	{
+		project = projectToUse;
+		type = objectType;
+		id = objectId;
+		tagOfIdList = idListFieldTag;
+		rowObjectType = listedItemType;
+		columnTag = tableColumnTag;
+	}
+	
+	public int getColumnCount()
+	{
+		return 1;
+	}
+
+	public String getColumnName(int column)
+	{
+		return EAM.fieldLabel(rowObjectType, columnTag);
+	}
+
+	public int getRowCount()
+	{
+		try
+		{
+			return getIdList().size();
+		}
+		catch(ParseException e)
+		{
+			EAM.logException(e);
+			throw new RuntimeException("Parse error reading IdList " + tagOfIdList);
+		}
+	}
+
+	private IdList getIdList() throws ParseException
+	{
+		return new IdList(getObject().getData(tagOfIdList));
+	}
+
+	private EAMObject getObject()
+	{
+		return project.findObject(type, id);
+	}
+
+	public Object getValueAt(int row, int column)
+	{
+		try
+		{
+			EAMObject rowObject;
+			rowObject = getObjectFromRow(row);
+			return rowObject.getData(columnTag);
+		}
+		catch(Exception e)
+		{
+			EAM.logException(e);
+			return "(Error)";
+		}
+	}
+
+	public EAMObject getObjectFromRow(int row) throws RuntimeException
+	{
+		try
+		{
+			BaseId rowObjectId = getIdList().get(row);
+			EAMObject rowObject = project.findObject(rowObjectType, rowObjectId);
+			return rowObject;
+		}
+		catch(Exception e)
+		{
+			EAM.logException(e);
+			throw new RuntimeException("TeamModel.getObjectFromRow error");
+		}
+	}
+
+	Project project;
+	int type;
+	BaseId id;
+	String tagOfIdList;
+	int rowObjectType;
+	String columnTag;
+}
--- source/org/conservationmeasures/eam/views/summary/TeamModel.java
+++ source/org/conservationmeasures/eam/views/summary/TeamModel.java
@@ -5,57 +5,16 @@
  */
 package org.conservationmeasures.eam.views.summary;
 
-import javax.swing.table.AbstractTableModel;
-
-import org.conservationmeasures.eam.ids.BaseId;
-import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.objects.ProjectResource;
 import org.conservationmeasures.eam.project.Project;
 
-public class TeamModel extends AbstractTableModel
+public class TeamModel extends ObjectListTableModel
 {
 	public TeamModel(Project projectToUse)
 	{
-		project = projectToUse;
+		super(projectToUse, ObjectType.PROJECT_METADATA, projectToUse.getMetadata().getId(), 
+				ProjectMetadata.TAG_TEAM_RESOURCE_IDS, ObjectType.PROJECT_RESOURCE, ProjectResource.TAG_NAME);
 	}
-	
-	public ProjectMetadata getMetadata()
-	{
-		return project.getMetadata();
-	}
-
-	public int getColumnCount()
-	{
-		return 1;
-	}
-
-	public String getColumnName(int column)
-	{
-		return EAM.text("Column|Name");
-	}
-
-	public int getRowCount()
-	{
-		return getMetadata().getTeamResourceIdList().size();
-	}
-
-	public Object getValueAt(int row, int column)
-	{
-		ProjectResource member = getObjectFromRow(row);
-		if(member == null)
-			return "(Error)";
-		
-		return member.getData(ProjectResource.TAG_NAME);
-	}
-
-	public ProjectResource getObjectFromRow(int row)
-	{
-		BaseId id = getMetadata().getTeamResourceIdList().get(row);
-		ProjectResource member = (ProjectResource)project.findObject(ObjectType.PROJECT_RESOURCE, id);
-		return member;
-	}
-
-	Project project;
 }
