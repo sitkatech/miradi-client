175b1a783127b4a9cefb7af7fbb35cf078c05d5b
nimaa <nimaa@localhost>
2006-11-10 00:15:03 +0000

    working on the monitoring view to convert to the new xxxyyyManagementPanel

---
+++ source/org/conservationmeasures/eam/views/monitoring/InidicatorPoolManagementPanel.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.views.monitoring;
+
+import org.conservationmeasures.eam.dialogs.ModelessDialogPanel;
+import org.conservationmeasures.eam.objects.EAMObject;
+
+public class InidicatorPoolManagementPanel extends ModelessDialogPanel
+{
+
+	public EAMObject getObject()
+	{
+		return null;
+	}
+
+	public String getPanelDescription()
+	{
+		return null;
+	}
+
+}
