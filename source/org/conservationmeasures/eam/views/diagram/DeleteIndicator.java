dff9015978506b6a981a841460921134d316c2fe
kevins <kevins@localhost>
2006-11-13 21:03:05 +0000

    Eliminate tons of duplicate code by extending new DeleteAnnotationsDoer

--- source/org/conservationmeasures/eam/views/diagram/DeleteIndicator.java
+++ source/org/conservationmeasures/eam/views/diagram/DeleteIndicator.java
@@ -5,83 +5,17 @@
  */
 package org.conservationmeasures.eam.views.diagram;
 
-import org.conservationmeasures.eam.commands.CommandBeginTransaction;
-import org.conservationmeasures.eam.commands.CommandDeleteObject;
-import org.conservationmeasures.eam.commands.CommandEndTransaction;
-import org.conservationmeasures.eam.commands.CommandSetObjectData;
-import org.conservationmeasures.eam.exceptions.CommandFailedException;
-import org.conservationmeasures.eam.ids.BaseId;
-import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.objecthelpers.ConceptualModelNodeSet;
-import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.ConceptualModelNode;
-import org.conservationmeasures.eam.objects.EAMObject;
-import org.conservationmeasures.eam.objects.Indicator;
-import org.conservationmeasures.eam.project.ChainManager;
-import org.conservationmeasures.eam.views.ObjectsDoer;
 
-public class DeleteIndicator extends ObjectsDoer
+public class DeleteIndicator extends DeleteAnnotationDoer
 {
-	public boolean isAvailable()
+	String[] getDialogText()
 	{
-		return (getObjects().length == 1);
+		return new String[] { "Are you sure you want to delete this Indicator?",};
 	}
 
-	public void doIt() throws CommandFailedException
+	String getAnnotationIdListTag()
 	{
-		if(!isAvailable())
-			return;
-		
-		ConceptualModelNode node = getSelectedNode();
-		Indicator indicator = (Indicator)getObjects()[0];
-		BaseId idToRemove = indicator.getId();
-		
-		String[] dialogText = {
-				"Are you sure you want to delete this Indicator?", 
-		};
-		String[] buttons = {"Delete", "Retain", };
-		if(!EAM.confirmDialog("Delete Indicator", dialogText, buttons))
-			return;
-
-		String tag = ConceptualModelNode.TAG_INDICATOR_IDS;
-		getProject().executeCommand(new CommandBeginTransaction());
-		try
-		{
-			getProject().executeCommand(CommandSetObjectData.createRemoveIdCommand(node, tag, idToRemove));
-			ConceptualModelNodeSet nodesThatUseThisIndicator = new ChainManager(getProject()).findNodesThatUseThisIndicator(idToRemove);
-			if(nodesThatUseThisIndicator.size() == 0)
-				deleteIndicator(indicator);
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-			throw new CommandFailedException(e);
-		}
-		finally
-		{
-			getProject().executeCommand(new CommandEndTransaction());
-		}
+		return ConceptualModelNode.TAG_INDICATOR_IDS;
 	}
-
-	
-	public ConceptualModelNode getSelectedNode()
-	{
-		EAMObject selected = getView().getSelectedObject();
-		if(selected == null)
-			return null;
-		
-		if(selected.getType() != ObjectType.MODEL_NODE)
-			return null;
-		
-		return (ConceptualModelNode)selected;
-	}
-	
-	void deleteIndicator(Indicator indicator) throws CommandFailedException
-	{
-		getProject().executeCommands(indicator.createCommandsToClear());
-		getProject().executeCommand(new CommandDeleteObject(indicator.getType(), indicator.getId()));
-		
-	
-	}
-
 }
--- source/org/conservationmeasures/eam/views/diagram/DeleteObjective.java
+++ source/org/conservationmeasures/eam/views/diagram/DeleteObjective.java
@@ -5,81 +5,18 @@
  */
 package org.conservationmeasures.eam.views.diagram;
 
-import org.conservationmeasures.eam.commands.CommandBeginTransaction;
-import org.conservationmeasures.eam.commands.CommandDeleteObject;
-import org.conservationmeasures.eam.commands.CommandEndTransaction;
-import org.conservationmeasures.eam.commands.CommandSetObjectData;
-import org.conservationmeasures.eam.exceptions.CommandFailedException;
-import org.conservationmeasures.eam.ids.BaseId;
-import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.objecthelpers.ConceptualModelNodeSet;
-import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.ConceptualModelNode;
-import org.conservationmeasures.eam.objects.EAMObject;
-import org.conservationmeasures.eam.objects.Objective;
-import org.conservationmeasures.eam.project.ChainManager;
-import org.conservationmeasures.eam.views.ObjectsDoer;
 
-public class DeleteObjective extends ObjectsDoer
+public class DeleteObjective extends DeleteAnnotationDoer
 {
-	public boolean isAvailable()
+	String[] getDialogText()
 	{
-		return (getObjects().length == 1);
+		return new String[] { "Are you sure you want to delete this Objective?",};
 	}
 
-	public void doIt() throws CommandFailedException
+	String getAnnotationIdListTag()
 	{
-		if(!isAvailable())
-			return;
-		
-		ConceptualModelNode node = getSelectedNode();
-		Objective objective = (Objective)getObjects()[0];
-		BaseId idToRemove = objective.getId();
-		
-		String[] dialogText = {
-				"Are you sure you want to delete this Objective?", 
-		};
-		String[] buttons = {"Delete", "Retain", };
-		if(!EAM.confirmDialog("Delete Objective", dialogText, buttons))
-			return;
-
-		String tag = ConceptualModelNode.TAG_OBJECTIVE_IDS;
-		getProject().executeCommand(new CommandBeginTransaction());
-		try
-		{
-			getProject().executeCommand(CommandSetObjectData.createRemoveIdCommand(node, tag, idToRemove));
-			ConceptualModelNodeSet nodesThatUseThisObjective = new ChainManager(getProject()).findNodesThatUseThisObjective(idToRemove);
-			if(nodesThatUseThisObjective.size() == 0)
-				deleteObjective(objective);
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-			throw new CommandFailedException(e);
-		}
-		finally
-		{
-			getProject().executeCommand(new CommandEndTransaction());
-		}
-	}
-
-	
-	public ConceptualModelNode getSelectedNode()
-	{
-		EAMObject selected = getView().getSelectedObject();
-		if(selected == null)
-			return null;
-		
-		if(selected.getType() != ObjectType.MODEL_NODE)
-			return null;
-		
-		return (ConceptualModelNode)selected;
-	}
-	
-	void deleteObjective(Objective objective) throws CommandFailedException
-	{
-		getProject().executeCommands(objective.createCommandsToClear());
-		getProject().executeCommand(new CommandDeleteObject(objective.getType(), objective.getId()));
+		return ConceptualModelNode.TAG_OBJECTIVE_IDS;
 	}
 
 }
