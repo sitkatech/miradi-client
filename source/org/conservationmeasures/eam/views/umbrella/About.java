932e6065854daec818fd78de56e92ab5ec911f83
kevins <kevins@localhost>
2006-11-18 17:36:16 +0000

    Tweak formatting and enclose in a scroll pane

--- source/org/conservationmeasures/eam/views/umbrella/About.java
+++ source/org/conservationmeasures/eam/views/umbrella/About.java
@@ -9,13 +9,14 @@ import info.clearthought.layout.TableLayout;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
 import org.conservationmeasures.eam.dialogs.EAMDialog;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
@@ -49,13 +50,10 @@ public class About extends Doer  implements HyperlinkHandler
 		dlg.setModal(true);
 		
 		HtmlViewer bodyComponent =  new HtmlViewer(body, this);
-		bodyComponent.setMargin(new Insets(0,15,0,15));
 		bodyComponent.setFont(Font.getFont("Arial"));
 		bodyComponent.setSize(new Dimension(750, Short.MAX_VALUE));
 		
 		JButton ok = new JButton(new OkAction(dlg));
-
-		JPanel panel = (JPanel)dlg.getContentPane();
 
 		// Seems like there should be an easier way to enforce a fixed width
 		// and allow an HTML text component to become as high as necessary.
@@ -64,13 +62,17 @@ public class About extends Doer  implements HyperlinkHandler
 		double[] rowSizes = {bodyComponent.getWidth()};
 		double[] columnSizes = {TableLayout.PREFERRED, TableLayout.PREFERRED};
 		double gridSizes[][] = { rowSizes, columnSizes };
+		JPanel panel = new JPanel(new TableLayout(gridSizes));
 		panel.setLayout(new TableLayout(gridSizes));
 		String columnZeroRowZero = "0, 0";
 		panel.add(bodyComponent, columnZeroRowZero);
 		String columnZeroRowOneCenteredCentered = "0, 1, c, c";
 		panel.add(ok, columnZeroRowOneCenteredCentered);
 		
+		panel.setBorder(new EmptyBorder(18,18,18,18));
 		panel.setBackground(bodyComponent.getBackground());
+		
+		dlg.getContentPane().add(new JScrollPane(panel));
 		dlg.pack();
 		dlg.setLocation(Utilities.center(dlg.getSize(), Utilities.getViewableRectangle()));
 		
@@ -102,13 +104,18 @@ public class About extends Doer  implements HyperlinkHandler
 		"and Beneficent Technology, Inc. (Benetech, at www.benetech.org)" +
 		"</p>" +
 
-		"<p align=\"left\">" +
+		"<p align='left'>" +
 		"This software program is being developed to assist conservation practitioners " +
 		"to go through the adaptive management process outlined in the CMP's Open Standards " +
 		"for the Practice of Conservation. To obtain a copy of the latest version of these standards, " +
 		"go to www.conservationmeasures.org. " +
 		"You can also find more information about this software program " +
-		"at http://www.conservationmeasures.org/CMP/Site_Page.cfm?PageID=22. " +
+		"at:" +
+		"</p>" +
+		"<p align='center'>" +
+		"http://www.conservationmeasures.org/CMP/Site_Page.cfm?PageID=22 " +
+		"</p>" +
+		"<p align='left'>" +
 		"If you have questions or suggestions, " +
 		"please contact Nick Salafsky at Nick@FOSonline.org or at 1-301-263-2784. " +
 		"</p>" +
@@ -117,7 +124,7 @@ public class About extends Doer  implements HyperlinkHandler
 		"</p>";
 
 	public static final String licenseText = 
-		"<p align=\"left\">"  +
+		"<p align='left'>"  +
 		"This pre-release version is intended for evaluation and feedback only. " +
 		"Please send suggestions and other feedback to e-AM@conservationmeasures.org. " +
 		"You are not allowed to redistribute this program without the express written " +
@@ -129,10 +136,7 @@ public class About extends Doer  implements HyperlinkHandler
 		"<h1>e-Adaptive Management Pre-Release Demo</h1>" +
 		"<font face='Arial'>" +
 		mainAboutText + 
-		"<p></p>" +
 		licenseText + 
-		"<p></p>" +
-		"<p></p>" +
 		"</font>" + 
 		"</td></tr>" +
 		"</table></html>";
