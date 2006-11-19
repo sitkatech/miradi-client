470eda0cadbefd257acb93c88f46e2975d4be6ab
richardn <richardn@localhost>
2006-11-19 04:03:39 +0000

    Objectives

--- source/org/conservationmeasures/eam/dialogs/ObjectiveListManagementPanel.java
+++ source/org/conservationmeasures/eam/dialogs/ObjectiveListManagementPanel.java
@@ -14,6 +14,7 @@ import org.conservationmeasures.eam.ids.ModelNodeId;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.EAMObject;
 import org.conservationmeasures.eam.project.Project;
+import org.martus.swing.UiScrollPane;
 
 public class ObjectiveListManagementPanel extends ModelessDialogPanel
 {
@@ -27,7 +28,7 @@ public class ObjectiveListManagementPanel extends ModelessDialogPanel
 		
 		propertiesPanel = new ObjectivePropertiesPanel(projectToUse, actions, invalidId);
 		listComponent.setPropertiesPanel(propertiesPanel);
-		add(propertiesPanel, BorderLayout.AFTER_LAST_LINE);
+		add(new UiScrollPane(propertiesPanel), BorderLayout.AFTER_LAST_LINE);
 	}
 	
 	public void dispose()
--- source/org/conservationmeasures/eam/dialogs/ObjectivePropertiesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/ObjectivePropertiesPanel.java
@@ -24,7 +24,7 @@ public class ObjectivePropertiesPanel extends ObjectDataInputPanel
 		super(projectToUse, ObjectType.OBJECTIVE, idToShow);
 		
 		addField(createStringField(Objective.TAG_SHORT_LABEL));
-		addField(createStringField(Objective.TAG_LABEL));
+		addField(createMultilineField(Objective.TAG_LABEL));
 		updateFieldsFromProject();
 	}
 
