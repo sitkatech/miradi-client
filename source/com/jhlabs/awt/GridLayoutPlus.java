d7a1453fe43ce23b2c59012b834bf4a11bb6801a
kevins <kevins@localhost>
2006-11-19 16:46:25 +0000

    Add license comment to the top of the page

--- source/com/jhlabs/awt/GridLayoutPlus.java
+++ source/com/jhlabs/awt/GridLayoutPlus.java
@@ -1,3 +1,23 @@
+/*
+ * This code was downloaded from: http://www.jhlabs.com/java/layout/index.html
+ * on 2006-11-18. According to that page, here is the license:
+ * 
+ * 
+ * License
+
+ The downloadable source code on this page is released under the Apache License. 
+ Basically, this means that you are free to do whatever you like with this code, 
+ including commercial use, but it's not my fault if your satellite/nuclear 
+ power station/missile system fails as a result.
+
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this code except in compliance with the License. 
+ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ Unless required by applicable law or agreed to in writing, software distributed under 
+ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS 
+ OF ANY KIND, either express or implied. See the License for the specific language 
+ governing permissions and limitations under the License.
+ */
 package com.jhlabs.awt;
 
 import java.awt.*;
