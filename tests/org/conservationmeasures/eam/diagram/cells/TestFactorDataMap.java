e1e68e00e3ebe500d835581554ba903cbd757a9f
kevins <kevins@localhost>
2006-11-24 17:12:29 +0000

    Combine tests and eliminate an unneeded test class

--- tests/org/conservationmeasures/eam/diagram/cells/TestFactorDataMap.java
+++ tests/org/conservationmeasures/eam/diagram/cells/TestFactorDataMap.java
@@ -5,6 +5,8 @@
  */
 package org.conservationmeasures.eam.diagram.cells;
 
+import java.awt.Point;
+
 import org.conservationmeasures.eam.diagram.cells.FactorDataMap;
 import org.conservationmeasures.eam.diagram.factortypes.FactorTypeCause;
 import org.conservationmeasures.eam.diagram.factortypes.FactorTypeCluster;
@@ -12,6 +14,9 @@ import org.conservationmeasures.eam.diagram.factortypes.FactorTypeContributingFa
 import org.conservationmeasures.eam.diagram.factortypes.FactorTypeDirectThreat;
 import org.conservationmeasures.eam.diagram.factortypes.FactorTypeStrategy;
 import org.conservationmeasures.eam.diagram.factortypes.FactorTypeTarget;
+import org.conservationmeasures.eam.ids.DiagramNodeId;
+import org.conservationmeasures.eam.ids.ModelNodeId;
+import org.conservationmeasures.eam.objects.Cause;
 import org.conservationmeasures.eam.testall.EAMTestCase;
 
 public class TestFactorDataMap extends EAMTestCase
@@ -37,5 +42,23 @@ public class TestFactorDataMap extends EAMTestCase
 		assertEquals(new FactorTypeCause(), FactorDataMap.convertIntToNodeType(FactorDataMap.INT_TYPE_INDIRECT_FACTOR));
 		assertEquals(new FactorTypeStrategy(), FactorDataMap.convertIntToNodeType(FactorDataMap.INT_TYPE_INTERVENTION));
 		assertEquals(new FactorTypeCluster(), FactorDataMap.convertIntToNodeType(FactorDataMap.INT_TYPE_CLUSTER));
+	}
+
+	public void testFactorDataMap() throws Exception
+	{
+		ModelNodeId wrappedId = new ModelNodeId(2);
+		Cause cmFactor = new Cause(wrappedId);
+		DiagramNodeId nodeId = new DiagramNodeId(44);
+		DiagramFactor nodeA = DiagramFactor.wrapConceptualModelObject(nodeId, cmFactor);
+		String nodeAText = "Node A";
+		nodeA.setLabel(nodeAText);
+		Point location = new Point(5,22);
+		nodeA.setLocation(location);
+		FactorDataMap nodeAData = nodeA.createNodeDataMap();
+		
+		assertEquals("Text incorrect", nodeAText, nodeAData.getString(DiagramFactor.TAG_VISIBLE_LABEL));
+		assertEquals("location incorrect", location, nodeAData.getPoint(DiagramFactor.TAG_LOCATION));
+		assertEquals("id incorrect", nodeId, nodeAData.getId(DiagramFactor.TAG_ID));
+		assertEquals("wrapped id incorrect", wrappedId, nodeAData.getId(DiagramFactor.TAG_WRAPPED_ID));
 	}
 }
