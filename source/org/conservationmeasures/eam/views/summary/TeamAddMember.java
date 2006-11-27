33e40c7b144d4ab92f85fe67e82315cdb91b12f8
nimaa <nimaa@localhost>
2006-11-27 20:09:07 +0000

    takes care of possible duplicate ids

--- source/org/conservationmeasures/eam/views/summary/TeamAddMember.java
+++ source/org/conservationmeasures/eam/views/summary/TeamAddMember.java
@@ -9,6 +9,7 @@ import org.conservationmeasures.eam.commands.Command;
 import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.ids.BaseId;
+import org.conservationmeasures.eam.ids.IdList;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.views.ObjectsDoer;
@@ -23,7 +24,7 @@ public class TeamAddMember extends ObjectsDoer
 		if (getSelectedIds().length > 1)
 			return true;
 		
-		ProjectMetadata metadata = getProject().getMetadata();
+		ProjectMetadata metadata = getMetaData();
 		if(metadata.getTeamResourceIdList().contains(getSelectedId()))
 			return false;
 		
@@ -37,8 +38,10 @@ public class TeamAddMember extends ObjectsDoer
 		
 		try
 		{
-			BaseId[] selectedIds = getSelectedIds();
-			tryToAddSelectedIds(selectedIds);
+			ProjectMetadata metadata = getMetaData();
+			IdList selectedIds = getUniqueSelectedIds();
+			Command cmd = CommandSetObjectData.createAppendIdsCommand(metadata, ProjectMetadata.TAG_TEAM_RESOURCE_IDS, selectedIds);
+			getProject().executeCommand(cmd);
 		}
 		catch (Exception e)
 		{
@@ -46,14 +49,21 @@ public class TeamAddMember extends ObjectsDoer
 			EAM.errorDialog(EAM.text("Text|Unknown error prevented adding this person to the team"));
 		}
 	}
-
-	private void tryToAddSelectedIds(BaseId[] selectedIds) throws Exception
+	
+	private IdList getUniqueSelectedIds()
 	{
-		ProjectMetadata metadata = getProject().getMetadata();
+		BaseId[] selectedIds = getSelectedIds();
+		IdList teamResourceIdList = getMetaData().getTeamResourceIdList();
+		IdList newList = new IdList();
 		for (int i = 0; i < selectedIds.length; i++)
-		{
-			Command cmd = CommandSetObjectData.createAppendIdCommand(metadata, ProjectMetadata.TAG_TEAM_RESOURCE_IDS, selectedIds[i]);
-			getProject().executeCommand(cmd);
-		}
+			if (!teamResourceIdList.contains(selectedIds[i]))
+				newList.add(selectedIds[i]);
+
+		return newList;		
+	}
+
+	private ProjectMetadata getMetaData()
+	{
+		return getProject().getMetadata();
 	}
 }
