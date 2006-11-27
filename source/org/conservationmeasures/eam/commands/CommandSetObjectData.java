f0095b56b0a71c11b6845117501f5a226006b5a0
nimaa <nimaa@localhost>
2006-11-27 20:07:12 +0000

    new append method takes in a IdList

--- source/org/conservationmeasures/eam/commands/CommandSetObjectData.java
+++ source/org/conservationmeasures/eam/commands/CommandSetObjectData.java
@@ -15,6 +15,14 @@ import org.conservationmeasures.eam.project.Project;
 
 public class CommandSetObjectData extends Command
 {
+	static public CommandSetObjectData createAppendIdsCommand(EAMObject object, String idListTag, IdList idsToAppend) throws ParseException
+	{
+		IdList newList = new IdList(object.getData(idListTag));
+		newList.addAll(idsToAppend);
+		
+		return new CommandSetObjectData(object.getType(), object.getId(), idListTag, newList.toString());
+	}
+	
 	static public CommandSetObjectData createAppendIdCommand(EAMObject object, String idListTag, BaseId idToAppend) throws ParseException
 	{
 		IdList newList = new IdList(object.getData(idListTag));
