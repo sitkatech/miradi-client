b99137650a5b51dac91b5baaa5798ceb9fbdb27d
nimaa <nimaa@localhost>
2006-11-28 03:34:11 +0000

    changed preferences dialog to extend ModelessDialogWithClose

---
+++ source/org/conservationmeasures/eam/dialogs/PreferencesDialog.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.dialogs;
+
+import org.conservationmeasures.eam.main.MainWindow;
+
+public class PreferencesDialog extends ModelessDialogWithClose
+{
+	public PreferencesDialog(MainWindow mainWindowToUse, DisposablePanel panel, String headingText)
+	{
+		super(mainWindowToUse, panel, headingText);
+	}
+
+}
