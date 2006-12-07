d4d7a7cb23cbeb1521ff5d9afcb4e506f6211775
richardn <richardn@localhost>
2006-12-07 15:34:05 +0000

    remve team list check

--- source/org/conservationmeasures/eam/views/umbrella/DeleteResource.java
+++ source/org/conservationmeasures/eam/views/umbrella/DeleteResource.java
@@ -15,7 +15,6 @@ import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.objects.ProjectResource;
 import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.views.ObjectsDoer;
@@ -42,27 +41,19 @@ public class DeleteResource extends ObjectsDoer
 		if(tasksThatUseThisResource.length > 0)
 			dialogText.add("This resource is assigned to one or more tasks.");
 
-		if(isTeamMember(idToRemove))
-			dialogText.add("This resource is a member of the project team.");
-
 		dialogText.add("\nAre you sure you want to delete this resource?");
 
 		String[] buttons = {"Yes", "No", };
 		if(!EAM.confirmDialog("Delete Resource", (String[])dialogText.toArray(new String[0]), buttons))
 			return;
 
-		
-		
 		try
 		{
 			Command[] removeFromTasks = createCommandsToRemoveResourceFromTasks(idToRemove, tasksThatUseThisResource);
-			Command[] removeFromTeam = createCommandsToRemoveResourceFromTeam(idToRemove);
 			
 			getProject().executeCommand(new CommandBeginTransaction());
 			for(int i = 0; i < removeFromTasks.length; ++i)
 				getProject().executeCommand(removeFromTasks[i]);
-			for(int i = 0; i < removeFromTeam.length; ++i)
-				getProject().executeCommand(removeFromTeam[i]);
 			int type = resource.getType();
 			BaseId id = idToRemove;
 			getProject().executeCommands(resource.createCommandsToClear());
@@ -80,11 +71,6 @@ public class DeleteResource extends ObjectsDoer
 		}
 	}
 
-	private boolean isTeamMember(BaseId idToRemove)
-	{
-		return getProject().getMetadata().getTeamResourceIdList().contains(idToRemove);
-	}
-
 	private Command[] createCommandsToRemoveResourceFromTasks(BaseId idToRemove, Task[] tasksThatUseThisResource) throws Exception
 	{
 		Command[] removeFromTasks = new Command[tasksThatUseThisResource.length];
@@ -95,14 +81,5 @@ public class DeleteResource extends ObjectsDoer
 			removeFromTasks[i] = CommandSetObjectData.createRemoveIdCommand(task, tag, idToRemove);
 		}
 		return removeFromTasks;
-	}
-	
-	private Command[] createCommandsToRemoveResourceFromTeam(BaseId idToRemove) throws Exception
-	{
-		if(!isTeamMember(idToRemove))
-			return new Command[0];
-		ProjectMetadata metadata = getProject().getMetadata();
-		Command cmd = CommandSetObjectData.createRemoveIdCommand(metadata, metadata.TAG_TEAM_RESOURCE_IDS, idToRemove);
-		return new Command[] {cmd};
 	}
 }
