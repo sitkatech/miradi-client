8d43e1adfac8c31288146349f514412736bfaefa
nimaa <nimaa@localhost>
2006-12-08 03:23:00 +0000

    changed dimension size to 900

--- source/org/conservationmeasures/eam/dialogfields/BudgetTableEditorComponent.java
+++ source/org/conservationmeasures/eam/dialogfields/BudgetTableEditorComponent.java
@@ -31,7 +31,7 @@ public class BudgetTableEditorComponent extends DisposablePanel
 		super(new BorderLayout());
 		
 		//FIXME budget code - remove, added just to display bigger columns
-		Dimension dimension = new Dimension(1200,400);
+		Dimension dimension = new Dimension(900,400);
 		setPreferredSize(dimension);
 		setMinimumSize(dimension);
 		
