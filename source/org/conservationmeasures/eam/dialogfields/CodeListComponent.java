ad4eefeeb6c85ede63dce3c8b1cc37945a5f4c02
richardn <richardn@localhost>
2006-12-09 06:26:18 +0000

    TeamCodes checkboxs

---
+++ source/org/conservationmeasures/eam/dialogfields/CodeListComponent.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.dialogfields;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.ratings.ResourceRoleQuestion;
+import org.conservationmeasures.eam.utils.CodeList;
+
+import com.jhlabs.awt.BasicGridLayout;
+
+public class CodeListComponent extends JPanel implements ItemListener
+{
+	public CodeListComponent(ResourceRoleQuestion questionToUse, ListSelectionListener listener)
+	{
+		super(new BasicGridLayout(0,3));
+		listSelectionListener = listener;
+		ChoiceItem[] choices = questionToUse.getChoices();
+		choiceItems = new ChoiceItem[choices.length];
+		checkBoxes = new JCheckBox[choices.length];
+		
+		for (int i=0; i<choices.length; ++i)
+		{
+			JCheckBox checkBox = new JCheckBox(choices[i].getLabel());
+			checkBox.addItemListener(this);
+			choiceItems[i] = choices[i];
+			checkBoxes[i] = checkBox;
+			add(checkBox);
+		}
+	}
+	
+	public void itemStateChanged(ItemEvent e) 
+	{
+	    if (e.getStateChange() == ItemEvent.SELECTED || 
+	    	e.getStateChange() == ItemEvent.DESELECTED)
+	    	valueChanged();
+	}
+	
+	public void valueChanged()
+	{
+		if (!skipNotice)
+		{
+			ListSelectionEvent event = new ListSelectionEvent("DUMMY EVENT",0,0, false);
+			listSelectionListener.valueChanged(event);
+		}
+	}
+	
+	public String getText()
+	{
+		CodeList codes = new CodeList();
+		for (int codeIndex = 0; codeIndex<checkBoxes.length; ++codeIndex )
+		{
+			JCheckBox checkBox = checkBoxes[codeIndex];
+			if (checkBox.isSelected())
+			{
+				ChoiceItem choiceItem = choiceItems[codeIndex];
+				codes.add(choiceItem.getCode());
+			}
+		}
+		return codes.toString();
+	}
+	
+	
+	public void setText(String codesToUse)
+	{
+		skipNotice=true;
+		clearCheckBoxes();
+		try
+		{
+			CodeList codes = new CodeList(codesToUse);
+			for(int codeIndex = 0; codeIndex < codes.size(); ++codeIndex)
+			{
+				for (int choiceIndex = 0; choiceIndex<choiceItems.length; ++choiceIndex)
+				{
+					ChoiceItem choiceItem = choiceItems[choiceIndex];
+					if (codes.get(codeIndex).equals(choiceItem.getCode()))
+						checkBoxes[choiceIndex].setSelected(true);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			EAM.errorDialog(EAM.text("Internal Error"));
+			EAM.logException(e);
+		}
+		finally
+		{
+			skipNotice=false;
+		}
+	}
+	
+	
+	public void updateEditableState(boolean isValidObject)
+	{
+		//FIXME: For some reason this code does not disable the check boxes even when isValidObject=false
+		for (int i=0; i<checkBoxes.length; ++i)
+			checkBoxes[i].enable(isValidObject);
+	}
+	
+	
+	public void clearCheckBoxes()
+	{
+		for (int i=0; i<checkBoxes.length; ++i)
+		{
+			checkBoxes[i].setSelected(false);
+		}
+	}
+	
+	private JCheckBox checkBoxes[];
+	private ChoiceItem choiceItems[];
+	private ListSelectionListener listSelectionListener;
+	private boolean skipNotice;
+}
--- source/org/conservationmeasures/eam/dialogfields/ObjectCodeListField.java
+++ source/org/conservationmeasures/eam/dialogfields/ObjectCodeListField.java
@@ -5,94 +5,48 @@
  */
 package org.conservationmeasures.eam.dialogfields;
 
-import java.awt.Color;
-
 import javax.swing.JComponent;
-import javax.swing.ListSelectionModel;
-import javax.swing.border.LineBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import org.conservationmeasures.eam.ids.BaseId;
-import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.ratings.ResourceRoleQuestion;
-import org.conservationmeasures.eam.utils.CodeList;
-import org.martus.swing.UiList;
 
-public class ObjectCodeListField extends ObjectDataInputField
+public class ObjectCodeListField extends ObjectDataInputField implements ListSelectionListener
 {
 	public ObjectCodeListField(Project projectToUse, int objectTypeToUse, BaseId objectIdToUse, ResourceRoleQuestion questionToUse)
 	{
 		super(projectToUse, objectTypeToUse, objectIdToUse, questionToUse.getTag());
-		list = new UiList(questionToUse.getChoices());
-		list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-		list.addListSelectionListener(new ComboChangeHandler());
-		list.setBorder(new LineBorder(Color.LIGHT_GRAY, 1));
-		addFocusListener();
+		component = new CodeListComponent(questionToUse,this);
 	}
 	
 	public JComponent getComponent()
 	{
-		return list;
+		return component;
 	}
 
 	public String getText()
 	{
-		Object[] selected = list.getSelectedValues();
-		if(selected.length==0)
-			return "";
-		CodeList codeList = new CodeList();
-		for (int i=0; i<selected.length; ++i)
-		{
-			codeList.add(((ChoiceItem)selected[i]).getCode());
-		}
-		return codeList.toString();
+		return component.getText();
 	}
 
 	public void setText(String codes)
 	{
-		try
-		{
-			list.clearSelection();
-			CodeList codeList = new CodeList(codes);
-			for(int i = 0; i < list.getModel().getSize(); ++i)
-			{
-				ChoiceItem choiceItem = (ChoiceItem)list.getModel().getElementAt(i);
-				if (codeList.contains(choiceItem.getCode()))
-					list.addSelectionInterval(i,i);
-			}
-		}
-		catch (Exception e)
-		{
-			EAM.logException(e);
-			EAM.errorDialog(EAM.text("Internal Error"));
-		}
-		
+		component.setText(codes);
 	}
 	
 	public void updateEditableState()
 	{
-		list.setEnabled(isValidObject());
-		if(isValidObject())
-		{
-			list.setForeground(EAM.EDITABLE_FOREGROUND_COLOR);
-			list.setBackground(EAM.EDITABLE_BACKGROUND_COLOR);
-		}
-		else
-		{
-			list.setForeground(EAM.READONLY_FOREGROUND_COLOR);
-			list.setBackground(EAM.READONLY_BACKGROUND_COLOR);
-		}
+		component.updateEditableState(isValidObject());
 	}
 	
-	class ComboChangeHandler implements ListSelectionListener
+	public void valueChanged(ListSelectionEvent arg0)
 	{
-		public void valueChanged(ListSelectionEvent arg0)
-		{
-			setNeedsSave();
-		}
+		setNeedsSave();
+		saveIfNeeded();
 	}
+	
+	CodeListComponent component;
 
-	UiList list;
 }
