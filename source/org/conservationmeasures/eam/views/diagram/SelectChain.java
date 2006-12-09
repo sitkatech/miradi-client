8f8202002907207ee9d1b4e23127d093687ead1e
kevins <kevins@localhost>
2006-12-09 18:20:53 +0000

    Now that links of selected sells highlight, we don't need to select the links as part of the chain

--- source/org/conservationmeasures/eam/views/diagram/SelectChain.java
+++ source/org/conservationmeasures/eam/views/diagram/SelectChain.java
@@ -12,11 +12,9 @@ import org.conservationmeasures.eam.diagram.cells.DiagramFactor;
 import org.conservationmeasures.eam.diagram.cells.LinkCell;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.ids.FactorId;
-import org.conservationmeasures.eam.ids.FactorLinkId;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.DiagramFactorLink;
 import org.conservationmeasures.eam.objects.Factor;
-import org.conservationmeasures.eam.objects.FactorLink;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.ViewDoer;
 
@@ -68,9 +66,7 @@ public class SelectChain extends ViewDoer
 			ChainObject chainObject = new ChainObject();
 			chainObject.buildNormalChain(model, selectedFactor.getUnderlyingObject());
 			Factor[] chainNodes = chainObject.getFactors().toNodeArray();
-			FactorLink[] linksInChain = chainObject.getFactorLinksArray();
 		
-			selectLinksInChain(diagram, linksInChain);
 			selectFactorsInChain(diagram, chainNodes);
 		}
 	}
@@ -93,16 +89,11 @@ public class SelectChain extends ViewDoer
 			Factor to = project.findNode(cell.getTo().getWrappedId());
 			downstreamChain.buildDownstreamChain(diagramModel, to);
 			
-			FactorLink[] downstreamLinks = downstreamChain.getFactorLinksArray();
-			FactorLink[] upstreamLinks = upstreamChain.getFactorLinksArray();
 			Factor[] upstreamFactors = upstreamChain.getFactorsArray();
 			Factor[] downstreamFactors = downstreamChain.getFactorsArray();
 			
 			selectFactorsInChain(diagram, upstreamFactors);
 			selectFactorsInChain(diagram, downstreamFactors);
-			
-			selectLinksInChain(diagram, upstreamLinks);
-			selectLinksInChain(diagram, downstreamLinks);
 		}
 	}
 
@@ -114,16 +105,6 @@ public class SelectChain extends ViewDoer
 			// convert CMNode to DiagramNode
 			DiagramFactor nodeToSelect = model.getDiagramFactorByWrappedId((FactorId)chainNodes[i].getId());
 			diagram.addSelectionCell(nodeToSelect);
-		}
-	}
-
-	private static void selectLinksInChain(DiagramComponent diagram, FactorLink[] linksInChain) throws Exception
-	{
-		DiagramModel model = diagram.getDiagramModel();
-		for (int i = 0 ; i < linksInChain.length; i++)
-		{
-			DiagramFactorLink linkToSelect = model.getDiagramFactorLinkbyWrappedId((FactorLinkId)linksInChain[i].getId());
-			diagram.addSelectionCell(model.findLinkCell(linkToSelect));
 		}
 	}
 
