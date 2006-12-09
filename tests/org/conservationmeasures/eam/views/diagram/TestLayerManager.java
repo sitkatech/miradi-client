21f16762e526d1b436dbbaa281e183988cbcf672
kevins <kevins@localhost>
2006-12-09 20:24:03 +0000

    Fix failing test (I broke it with my last checkin)

--- tests/org/conservationmeasures/eam/views/diagram/TestLayerManager.java
+++ tests/org/conservationmeasures/eam/views/diagram/TestLayerManager.java
@@ -75,8 +75,8 @@ public class TestLayerManager extends EAMTestCase
 		LayerManager manager = new LayerManager();
 		assertTrue("all nodes not visible to start?", manager.areAllNodesVisible());
 		IdList idsToHide = new IdList();
-		idsToHide.add(target.getDiagramFactorId());
-		idsToHide.add(factor.getDiagramFactorId());
+		idsToHide.add(target.getWrappedId());
+		idsToHide.add(factor.getWrappedId());
 		manager.setHiddenIds(idsToHide);
 		assertFalse("thinks all nodes are visible?", manager.areAllNodesVisible());
 		for(int i = 0; i < idsToHide.size(); ++i)
