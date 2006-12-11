f8d75a74c88b0b6039f0a322973be0f2f25cf7c5
richardn <richardn@localhost>
2006-12-11 18:11:49 +0000

    First part of legend button logic

--- source/org/conservationmeasures/eam/views/diagram/DiagramLegendPanel.java
+++ source/org/conservationmeasures/eam/views/diagram/DiagramLegendPanel.java
@@ -9,28 +9,37 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.AbstractButton;
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
-import org.conservationmeasures.eam.icons.FactorLinkIcon;
-import org.conservationmeasures.eam.icons.DirectThreatIcon;
+import org.conservationmeasures.eam.actions.ActionInsertContributingFactor;
+import org.conservationmeasures.eam.actions.ActionInsertDirectThreat;
+import org.conservationmeasures.eam.actions.ActionInsertFactorLink;
+import org.conservationmeasures.eam.actions.ActionInsertStrategy;
+import org.conservationmeasures.eam.actions.ActionInsertTarget;
+import org.conservationmeasures.eam.actions.Actions;
+import org.conservationmeasures.eam.actions.EAMAction;
 import org.conservationmeasures.eam.icons.GoalIcon;
 import org.conservationmeasures.eam.icons.IndicatorIcon;
-import org.conservationmeasures.eam.icons.ContributingFactorIcon;
-import org.conservationmeasures.eam.icons.StrategyIcon;
 import org.conservationmeasures.eam.icons.ObjectiveIcon;
 import org.conservationmeasures.eam.icons.ProjectScopeIcon;
 import org.conservationmeasures.eam.icons.StressIcon;
-import org.conservationmeasures.eam.icons.TargetIcon;
 import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.utils.LocationHolder;
+import org.martus.swing.UiButton;
 import org.martus.swing.UiLabel;
 
 import com.jhlabs.awt.BasicGridLayout;
 
 public class DiagramLegendPanel extends JPanel
 {
-	public DiagramLegendPanel()
+	public DiagramLegendPanel(Actions actions)
 	{
 		super(new BasicGridLayout(0, 1));
 		setBorder(new EmptyBorder(5,5,5,5));
@@ -40,19 +49,60 @@ public class DiagramLegendPanel extends JPanel
 		title.setBorder(new LineBorder(Color.BLACK, 2));
 		title.setHorizontalAlignment(UiLabel.CENTER);
 		add(title);
-		add(new UiLabel(EAM.text("Project Scope"), new ProjectScopeIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Target"), new TargetIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Direct Threat"), new DirectThreatIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Contributing Factor"), new ContributingFactorIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Strategy"), new StrategyIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Link"), new FactorLinkIcon(), UiLabel.LEADING));
+
 		
-		add(new UiLabel(EAM.text("Goal"), new GoalIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Objective"), new ObjectiveIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Indicator"), new IndicatorIcon(), UiLabel.LEADING));
-		add(new UiLabel(EAM.text("Stress"), new StressIcon(), UiLabel.LEADING));
-		
+		add(createLegendButtonPanel(actions));
+
 		setMinimumSize(new Dimension(0,0));
 	}
+	
+	private JPanel createLegendButtonPanel(Actions actions)
+	{
+		JPanel jpanel = new JPanel(new BasicGridLayout(0,3));
+		addButtonLine(jpanel, "Project Scope", new ProjectScopeIcon());
+		
+		addButtonLine(jpanel, "Target", actions.get(ActionInsertTarget.class));
+		addButtonLine(jpanel, "Direct Threat", actions.get(ActionInsertDirectThreat.class));
+		addButtonLine(jpanel, "Contributing Factor", actions.get(ActionInsertContributingFactor.class));
+		addButtonLine(jpanel, "Strategy", actions.get(ActionInsertStrategy.class));
+		addButtonLine(jpanel, "Link", actions.get(ActionInsertFactorLink.class));
+		
+		addButtonLine(jpanel, "Goal", new GoalIcon());
+		addButtonLine(jpanel, "Objective", new ObjectiveIcon());
+		addButtonLine(jpanel, "Indicator", new IndicatorIcon());
+		addButtonLine(jpanel, "Stress", new StressIcon());
+		
+		return jpanel;
+	}
+	
+	private void addButtonLine(JPanel jpanel, String text, EAMAction action)
+	{
+		JButton button = new LocationButton(action);
+		button.setText(null);
+		jpanel.add(button);
+		jpanel.add(new UiLabel(EAM.text(text)));
+		jpanel.add(new JCheckBox());
+	}
+	
+	private void addButtonLine(JPanel jpanel, String text, Icon icon)
+	{
+		JPanel centeringPanel = new JPanel();
+		centeringPanel.add(new UiLabel("",icon,AbstractButton.LEFT));
+		jpanel.add(centeringPanel);
+		jpanel.add(new UiLabel(EAM.text(text)));
+		jpanel.add(new UiLabel(""));
+	}
 
+	class LocationButton extends UiButton implements LocationHolder
+	{
+		LocationButton(Action action)
+		{
+			super(action);
+		}
+		
+		public boolean hasLocation()
+		{
+			return false;
+		}
+	}
 }
--- source/org/conservationmeasures/eam/views/diagram/DiagramView.java
+++ source/org/conservationmeasures/eam/views/diagram/DiagramView.java
@@ -90,7 +90,7 @@ public class DiagramView extends UmbrellaView implements CommandExecutedListener
 		super(mainWindowToUse);
 		mode = ViewData.MODE_DEFAULT;
 		diagram = new DiagramComponent(getMainWindow());
-		legendDialog = new DiagramLegendPanel();
+		legendDialog = new DiagramLegendPanel(getMainWindow().getActions());
 		getProject().setSelectionModel(diagram.getEAMGraphSelectionModel());
 		
 		addDiagramViewDoersToMap();
