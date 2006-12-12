ef39bdba37d75420bd7b508074243c6c349413e9
richardn <richardn@localhost>
2006-12-12 16:45:05 +0000

    Doer

--- source/org/conservationmeasures/eam/views/budget/ImportAccountingCodesDoer.java
+++ source/org/conservationmeasures/eam/views/budget/ImportAccountingCodesDoer.java
@@ -5,28 +5,44 @@
  */
 package org.conservationmeasures.eam.views.budget;
 
-import org.conservationmeasures.eam.commands.CommandCreateObject;
+import java.io.File;
+
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
-import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.views.ViewDoer;
+import org.martus.swing.UiFileChooser;
 
 public class ImportAccountingCodesDoer extends ViewDoer
 {
-	public boolean isAvailable()
+
+	public boolean isAvailable() 
 	{
 		return true;
 	}
 
-	public void doIt() throws CommandFailedException
+	public void doIt() throws CommandFailedException 
 	{
-		try
-		{
-			CommandCreateObject cmd = new CommandCreateObject(ObjectType.ACCOUNTING_CODE);
-			getProject().executeCommand(cmd);
-		}
-		catch (Exception e)
-		{
-			throw new CommandFailedException(e);
-		}
+		File startingDirectory = UiFileChooser.getHomeDirectoryFile();
+		String windowTitle = EAM.text("Import Accounting Codes");
+		UiFileChooser.FileDialogResults results = UiFileChooser.displayFileOpenDialog(
+				getMainWindow(), windowTitle, UiFileChooser.NO_FILE_SELECTED, startingDirectory, null, null);
+		
+		if (results.wasCancelChoosen())
+			return;
+		
+		File fileToImport = results.getChosenFile();
+		String projectName = withoutExtension(fileToImport.getName());
+		File finalProjectDirectory = new File(EAM.getHomeDirectory(), projectName);
+		finalProjectDirectory.canRead();
+		EAM.notifyDialog(EAM.text("Import Competed"));
 	}
+
+	private String withoutExtension(String fileName)
+	{
+		int lastDotAt = fileName.lastIndexOf('.');
+		if(lastDotAt < 0)
+			return fileName;
+		return fileName.substring(0, lastDotAt);
+	}
+
 }
