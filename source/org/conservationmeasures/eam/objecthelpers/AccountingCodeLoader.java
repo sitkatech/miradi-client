34115dd996bafdecc34132b999fda46126ae7e19
richardn <richardn@localhost>
2006-12-12 18:40:18 +0000

    Test and code for import of accounting codes

--- source/org/conservationmeasures/eam/objecthelpers/AccountingCodeLoader.java
+++ source/org/conservationmeasures/eam/objecthelpers/AccountingCodeLoader.java
@@ -8,45 +8,44 @@ package org.conservationmeasures.eam.objecthelpers;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringBufferInputStream;
 import java.util.Vector;
 
-import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.objects.AccountingCode;
 import org.conservationmeasures.eam.utils.DelimitedFileLoader;
 
 public class AccountingCodeLoader
 {
-	public static AccountingCode[] load(String resourceFileName) throws Exception
+	public static AccountingCodeData[] load(String data) throws Exception
 	{
 
-		InputStream is = EAM.class.getResourceAsStream(resourceFileName);
+		InputStream is = new StringBufferInputStream(data);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 		Vector fileVector = DelimitedFileLoader.getDelimitedContents(reader);
 		reader.close();
 
 		Vector accountingCodes = processVector(fileVector);
-		return (AccountingCode[]) accountingCodes.toArray(new AccountingCode[0]);
+		return (AccountingCodeData[]) accountingCodes.toArray(new AccountingCodeData[0]);
 	}
 
-	public static AccountingCode[] load(BufferedReader reader) throws Exception
+	public static AccountingCodeData[] load(BufferedReader reader) throws Exception
 	{
 		Vector fileVector = DelimitedFileLoader.getDelimitedContents(reader);
 
 		Vector accountingCodes = processVector(fileVector);
-		return (AccountingCode[]) accountingCodes.toArray(new AccountingCode[0]);
+		return (AccountingCodeData[]) accountingCodes.toArray(new AccountingCodeData[0]);
 	}
 	
 	private static Vector processVector(Vector fileVector)
 	{
-		Vector AccountingCodes = new Vector();
+		Vector AccountingCodeData = new Vector();
 		for(int vectorIndex = 0; vectorIndex < fileVector.size(); ++vectorIndex)
 		{
 			Vector row = (Vector) fileVector.get(vectorIndex);
 			String code = (String) row.get(0);
 			String label = (String) row.get(1);
-			AccountingCodes.add(new AccountingCodeData(code, label));
+			AccountingCodeData.add(new AccountingCodeData(code, label));
 		}
-		return AccountingCodes;
+		return AccountingCodeData;
 	}
 
 }
--- source/org/conservationmeasures/eam/views/budget/ImportAccountingCodesDoer.java
+++ source/org/conservationmeasures/eam/views/budget/ImportAccountingCodesDoer.java
@@ -5,16 +5,29 @@
  */
 package org.conservationmeasures.eam.views.budget;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.util.Vector;
 
+import org.conservationmeasures.eam.commands.CommandBeginTransaction;
+import org.conservationmeasures.eam.commands.CommandCreateObject;
+import org.conservationmeasures.eam.commands.CommandEndTransaction;
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.AccountingCodeData;
+import org.conservationmeasures.eam.objecthelpers.AccountingCodeLoader;
+import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.AccountingCode;
+import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.ViewDoer;
 import org.martus.swing.UiFileChooser;
 
 public class ImportAccountingCodesDoer extends ViewDoer
 {
-
 	public boolean isAvailable() 
 	{
 		return true;
@@ -31,18 +44,72 @@ public class ImportAccountingCodesDoer extends ViewDoer
 			return;
 		
 		File fileToImport = results.getChosenFile();
-		String projectName = withoutExtension(fileToImport.getName());
-		File finalProjectDirectory = new File(EAM.getHomeDirectory(), projectName);
-		finalProjectDirectory.canRead();
+		try 
+		{
+			Project project = getProject();
+			importCodes(new BufferedReader(new FileReader(fileToImport)), project);
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
+		
 		EAM.notifyDialog(EAM.text("Import Competed"));
 	}
 
-	private String withoutExtension(String fileName)
+
+	static public AccountingCode[] importCodes(String fileToImport, Project project) throws Exception
 	{
-		int lastDotAt = fileName.lastIndexOf('.');
-		if(lastDotAt < 0)
-			return fileName;
-		return fileName.substring(0, lastDotAt);
+		return importCodes(new BufferedReader(new StringReader(fileToImport)),project);
+	}
+	
+
+	static public AccountingCode[] importCodes(BufferedReader fileToImport, Project project) throws Exception
+	{
+		AccountingCodeData[] accountingCodes = new AccountingCodeData[0];
+		project.executeCommand(new CommandBeginTransaction());
+		try
+		{
+			accountingCodes = AccountingCodeLoader.load(fileToImport);
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
+		finally
+		{
+			project.executeCommand(new CommandEndTransaction());
+		}
+		return processData(project, accountingCodes);
 	}
 
+
+	private static AccountingCode[] processData(Project project, AccountingCodeData[] accountingCodes) throws Exception
+	{
+		Vector accountingCodeVector = new Vector();
+		for (int i=0; i<accountingCodes.length; ++i) 
+		{
+			AccountingCodeData AccountingCodeData = accountingCodes[i];
+			CommandCreateObject cmd = new CommandCreateObject(ObjectType.ACCOUNTING_CODE);
+			project.executeCommand(cmd);
+			BaseId baseId = cmd.getCreatedId();
+			AccountingCode accountingCode = new AccountingCode(baseId);
+			accountingCodeVector.add(accountingCode);
+
+			project.executeCommand(new CommandSetObjectData(
+					ObjectType.ACCOUNTING_CODE, baseId, 
+					AccountingCode.TAG_LABEL, AccountingCodeData.getLabel()));
+			
+			project.executeCommand(new CommandSetObjectData(
+					ObjectType.ACCOUNTING_CODE, baseId, 
+					AccountingCode.TAG_CODE, AccountingCodeData.getCode()));
+		}
+
+		AccountingCode[] toArray = (AccountingCode[])accountingCodeVector.toArray(new AccountingCode[0]);
+		return toArray;
+	}
+	
+	
+	
+	
 }
