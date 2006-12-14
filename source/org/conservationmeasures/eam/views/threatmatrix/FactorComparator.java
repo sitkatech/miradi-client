e00f17c7d4493cf73c53d1b21501fa416be3eda5
richardn <richardn@localhost>
2006-12-14 02:17:51 +0000

    sort fix for equal valued threats

--- source/org/conservationmeasures/eam/views/threatmatrix/FactorComparator.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/FactorComparator.java
@@ -8,6 +8,7 @@ package org.conservationmeasures.eam.views.threatmatrix;
 import java.util.Comparator;
 
 import org.conservationmeasures.eam.ids.FactorId;
+import org.conservationmeasures.eam.objects.EAMObject;
 import org.conservationmeasures.eam.objects.Factor;
 import org.conservationmeasures.eam.objects.ValueOption;
 import org.conservationmeasures.eam.project.ThreatRatingBundle;
@@ -46,6 +47,14 @@ public class FactorComparator implements  Comparator
 			Integer value1 = new Integer(valueOption1.getNumericValue());
 			Integer value2 = new Integer(valueOption2.getNumericValue());
 			
+			if (value1.compareTo(value2)==0)
+			{
+				EAMObject eamObject1 = ((EAMObject) object1);
+				EAMObject eamObject2 = ((EAMObject) object2);
+				String label1 = eamObject1.getData(Factor.TAG_LABEL);
+				String label2 = eamObject2.getData(Factor.TAG_LABEL);
+				return label1.compareToIgnoreCase(label2);
+			}
 			return value1.compareTo(value2);
 		}
 		catch(Exception e)
--- source/org/conservationmeasures/eam/views/threatmatrix/SummaryColumnComparator.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/SummaryColumnComparator.java
@@ -8,6 +8,8 @@ package org.conservationmeasures.eam.views.threatmatrix;
 import java.util.Comparator;
 
 import org.conservationmeasures.eam.ids.BaseId;
+import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.EAMObject;
 import org.conservationmeasures.eam.objects.Factor;
 import org.conservationmeasures.eam.objects.ValueOption;
 import org.conservationmeasures.eam.project.ThreatRatingFramework;
@@ -33,6 +35,14 @@ public class SummaryColumnComparator implements  Comparator
 			
 			Integer value1 = new Integer(valueOption1.getNumericValue());
 			Integer value2 = new Integer(valueOption2.getNumericValue());
+			if (value1.compareTo(value2)==0)
+			{
+				EAMObject eamObject1 = framework.getProject().findObject(ObjectType.FACTOR, baseId1);
+				EAMObject eamObject2 = framework.getProject().findObject(ObjectType.FACTOR, baseId2);
+				String label1 = eamObject1.getData(Factor.TAG_LABEL);
+				String label2 = eamObject2.getData(Factor.TAG_LABEL);
+				return label1.compareToIgnoreCase(label2);
+			}
 
 			return value1.compareTo(value2);
 		}
