4070ef70b94685ed47e40e45949cf4b6bdb8419a
kevins <kevins@localhost>
2006-12-14 23:03:17 +0000

    No longer extend DefaultTableCellRenderer so we can control paint completely

--- source/org/conservationmeasures/eam/views/threatmatrix/CustomTableCellRenderer.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/CustomTableCellRenderer.java
@@ -8,76 +8,77 @@ package org.conservationmeasures.eam.views.threatmatrix;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.Graphics;
 
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.ValueOption;
 import org.conservationmeasures.eam.project.ThreatRatingBundle;
 
-class CustomTableCellRenderer extends DefaultTableCellRenderer
+class CustomTableCellRenderer extends JComponent implements TableCellRenderer
 {
 	public CustomTableCellRenderer(ThreatGridPanel threatGridPanelToUse)
 	{
 		threatGridPanel = threatGridPanelToUse;
-		setHorizontalAlignment(CENTER);
+		font = new Font(null,Font.BOLD,12);
 	}
 
 	public Component getTableCellRendererComponent(JTable table, Object value,
 			boolean isSelected, boolean hasFocus, int row, int column)
 	{
-		Component cell = super.getTableCellRendererComponent(table, value,
-				isSelected, hasFocus, row, column);
-
-		cell.setBackground( ((ValueOption)value).getColor() );
-		cell.setFont(new Font(null,Font.BOLD,12));
-		cell.setForeground(Color.BLACK); 
+		valueOption = (ValueOption)value;
+		renderingRow = row;
+		renderingCol = column;
 		
 		setBorders(table, row, column);
 		
-		return cell;
+		return this;
 	}
 
 	private void setBorders(JTable table, int row, int column)
 	{
 		if (isOverallRatingCell(table, row, column))
 			setBorder(BorderFactory.createMatteBorder(5,5,1,1,Color.DARK_GRAY));
-		else
-		if (isSummaryRowCell(table, row, column))
+		else if (isSummaryRowCell(table, row, column))
 			setBorder(BorderFactory.createMatteBorder(5,1,1,1,Color.DARK_GRAY));
+		else if (isSummaryColumnCell(table, row, column))
+			setBorder(BorderFactory.createMatteBorder(1,5,1,1,Color.DARK_GRAY));
 		else 
-		if (isSummaryColumnCell(table, row, column))
-				setBorder(BorderFactory.createMatteBorder(1,5,1,1,Color.DARK_GRAY));
-		else 
-			setBorderForCell(row, column);
+			setBorderForNormalCell(row, column);
 	}
 
 
-	private void setBorderForCell(int row, int column)
+	private void setBorderForNormalCell(int row, int column)
 	{
 		try 
 		{
 			int indirectColumn = threatGridPanel.getThreatMatrixTable().convertColumnIndexToModel(column);
-			ThreatRatingBundle bundle = 
-				((NonEditableThreatMatrixTableModel)threatGridPanel.getThreatMatrixTable().getModel()).getBundle(row, indirectColumn);
+			ThreatRatingBundle bundle = getThreatTableModel().getBundle(row, indirectColumn);
 			if(bundle != null && threatGridPanel.getSelectedBundle()!= null)
+			{
 				if(threatGridPanel.getSelectedBundle().equals(bundle))
 				{
-					setBorder(BorderFactory.createCompoundBorder(BorderFactory
-							.createLineBorder(Color.BLUE, 3), getBorder()));
+					setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
 					return;
 				}
+			}
+
+			setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY,1));
 		}
 		catch (Exception e)
 		{
 			EAM.logException(e);
 		}
 		
-		setBorder(BorderFactory.createCompoundBorder(
-				BorderFactory.createLineBorder(Color.LIGHT_GRAY,1),getBorder()));
-		return;
+	}
+
+	private NonEditableThreatMatrixTableModel getThreatTableModel()
+	{
+		return (NonEditableThreatMatrixTableModel)threatGridPanel.getThreatMatrixTable().getModel();
 	}
 
 	private boolean isOverallRatingCell(JTable table, int row, int column)
@@ -100,5 +101,51 @@ class CustomTableCellRenderer extends DefaultTableCellRenderer
 	{
 		return arraySize-1;
 	}
+	
+	protected void paintComponent(Graphics g)
+	{
+		super.paintBorder(g);
+		
+		int width = getWidth();
+		int height = getHeight();
+
+		g.setColor(valueOption.getColor());
+		g.fillRect(0, 0, width, height);
+		
+		String label = valueOption.getLabel();
+		g.setFont(font);
+		int textHeight = g.getFontMetrics().getAscent();
+		int textWidth = g.getFontMetrics().stringWidth(label);
+		g.setColor(Color.BLACK);
+		g.drawString(label, (width-textWidth)/2, (height-textHeight)/2 + textHeight);
+	}
+
+
+	protected void firePropertyChange(String propertyName, Object oldValue, Object newValue)
+	{
+		//  Do nothing, as recommended in the javadocs for DefaultTableCellRenderer
+	}
+
+	public void revalidate()
+	{
+		//  Do nothing, as recommended in the javadocs for DefaultTableCellRenderer
+	}
+
+	public void validate()
+	{
+		//  Do nothing, as recommended in the javadocs for DefaultTableCellRenderer
+	}
+
+	public void repaint()
+	{
+		//  Do nothing, as recommended in the javadocs for DefaultTableCellRenderer
+	}
+
+
 	ThreatGridPanel threatGridPanel;
+	
+	ValueOption valueOption;
+	Font font;
+	int renderingRow;
+	int renderingCol;
 }
