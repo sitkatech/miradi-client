79cf557d923a2bf9ece5e9a5e8ebd6492188607b
richardn <richardn@localhost>
2006-12-15 03:24:01 +0000

    filter for Activity Pool Model to restrict to Strats

--- source/org/conservationmeasures/eam/dialogs/ActivityPoolTableModel.java
+++ source/org/conservationmeasures/eam/dialogs/ActivityPoolTableModel.java
@@ -5,7 +5,11 @@
  */
 package org.conservationmeasures.eam.dialogs;
 
+import org.conservationmeasures.eam.ids.BaseId;
+import org.conservationmeasures.eam.ids.IdList;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.Factor;
 import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.project.Project;
 
@@ -20,5 +24,26 @@ public class ActivityPoolTableModel extends ObjectPoolTableModel
 		Task.TAG_LABEL,
 		Task.PSEUDO_TAG_FACTOR_LABEL,
 	};
+	
+	public IdList getLatestIdListFromProject()
+	{
+		IdList filteredStrategy = new IdList();
+		
+		IdList indicator = super.getLatestIdListFromProject();
+		for (int i=0; i<indicator.size(); ++i)
+		{
+			BaseId baseId = indicator.get(i);
+			Factor factor = (Factor) project.findObject(ObjectType.FACTOR, baseId);
+			if (factor==null)
+			{
+				EAM.logError("ObjectType.FACTOR not found:" + baseId);
+				continue;
+			}
+			if (factor.isStrategy())
+				filteredStrategy.add(baseId);
+		}
+		return filteredStrategy;
+	}
+
 
 }
