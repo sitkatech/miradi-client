65881c55943a074a34671a59e7900a5d2f5aae83
kevins <kevins@localhost>
2006-12-16 02:54:56 +0000

    Change wording to "Very Good" for best Target Status choice

--- source/org/conservationmeasures/eam/questions/TargetStatusQuestion.java
+++ source/org/conservationmeasures/eam/questions/TargetStatusQuestion.java
@@ -24,7 +24,7 @@ public class TargetStatusQuestion extends ChoiceQuestion
 				new ChoiceItem("1", "Poor", COLOR_1_OF_4),
 				new ChoiceItem("2", "Fair", COLOR_2_OF_4),
 				new ChoiceItem("3", "Good", COLOR_3_OF_4),
-				new ChoiceItem("4", "Excellent", COLOR_4_OF_4),
+				new ChoiceItem("4", "Very Good", COLOR_4_OF_4),
 		};
 	}
 }
