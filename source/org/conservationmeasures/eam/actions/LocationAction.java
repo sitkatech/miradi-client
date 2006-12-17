164224e1ab2e6d3524130a5295da4e2348afd7a4
kevins <kevins@localhost>
2006-12-17 17:39:52 +0000

    Fix bug where inserted nodes were always going at 0,0

--- source/org/conservationmeasures/eam/actions/LocationAction.java
+++ source/org/conservationmeasures/eam/actions/LocationAction.java
@@ -6,7 +6,7 @@
 package org.conservationmeasures.eam.actions;
 
 import java.awt.Point;
-import java.awt.event.ActionEvent;
+import java.util.EventObject;
 
 import javax.swing.Icon;
 
@@ -36,7 +36,7 @@ public abstract class LocationAction extends MainWindowAction
 		createAt = location;
 	}
 
-	public void doAction(ActionEvent event) throws CommandFailedException
+	public void doAction(EventObject event) throws CommandFailedException
 	{
 		LocationHolder invoker = (LocationHolder)event.getSource();
 		if(!invoker.hasLocation())
