396a5e1a19c9aab6ff7365530fc9e7ecdfe3d7eb
richardn <richardn@localhost>
2006-12-18 18:13:03 +0000

    pop up modify property panel for team member when row has foucs and user hits return...note: currently unable to supress traveral on enter at this time.

--- source/org/conservationmeasures/eam/views/summary/TeamEditorComponent.java
+++ source/org/conservationmeasures/eam/views/summary/TeamEditorComponent.java
@@ -5,6 +5,9 @@
  */
 package org.conservationmeasures.eam.views.summary;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import org.conservationmeasures.eam.actions.ActionModifyResource;
 import org.conservationmeasures.eam.actions.ActionTeamCreateMember;
 import org.conservationmeasures.eam.actions.ActionTeamRemoveMember;
@@ -15,18 +18,21 @@ import org.conservationmeasures.eam.dialogs.ObjectPoolTable;
 import org.conservationmeasures.eam.dialogs.ObjectTableModel;
 import org.conservationmeasures.eam.dialogs.ObjectTablePanel;
 import org.conservationmeasures.eam.main.CommandExecutedEvent;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.ProjectResource;
 import org.conservationmeasures.eam.project.Project;
 import org.martus.swing.UiButton;
 
-public class TeamEditorComponent extends ObjectTablePanel
+public class TeamEditorComponent extends ObjectTablePanel implements KeyListener
 {
-	public TeamEditorComponent(Project projectToUse, Actions actions)
+	public TeamEditorComponent(Project projectToUse, Actions actionsToUse)
 	{
 		super(projectToUse, ObjectType.PROJECT_RESOURCE, new ObjectPoolTable(new TeamModel(projectToUse)));
-		addDoubleClickAction(actions.get(ActionModifyResource.class));
-		createButtonBar(actions);
+		addDoubleClickAction(actionsToUse.get(ActionModifyResource.class));
+		createButtonBar(actionsToUse);
+		getTable().addKeyListener(this);
+		actions = actionsToUse;
 	}
 	
 	public void dispose()
@@ -34,12 +40,12 @@ public class TeamEditorComponent extends ObjectTablePanel
 		super.dispose();
 	}
 	
-	void createButtonBar(Actions actions)
+	void createButtonBar(Actions actionsToUse)
 	{
-		addButton(new UiButton(actions.get(ActionTeamCreateMember.class)));
-		addButton(actions.getObjectsAction(ActionTeamRemoveMember.class));
-		addButton(actions.getObjectsAction(ActionModifyResource.class));
-		addButton(new UiButton(actions.get(ActionViewPossibleTeamMembers.class)));
+		addButton(new UiButton(actionsToUse.get(ActionTeamCreateMember.class)));
+		addButton(actionsToUse.getObjectsAction(ActionTeamRemoveMember.class));
+		addButton(actionsToUse.getObjectsAction(ActionModifyResource.class));
+		addButton(new UiButton(actionsToUse.get(ActionViewPossibleTeamMembers.class)));
 	}
 	
 	public void commandExecuted(CommandExecutedEvent event)
@@ -65,4 +71,31 @@ public class TeamEditorComponent extends ObjectTablePanel
 		if (((CommandSetObjectData) event.getCommand()).getFieldTag().equals(ProjectResource.TAG_ROLE_CODES))
 			((ObjectTableModel) getTable().getModel()).rowsWereAddedOrRemoved();
 	}
+
+	public void keyPressed(KeyEvent keyEvent)
+	{
+		if (keyEvent.getKeyCode() ==KeyEvent.VK_ENTER)
+		{
+			if (getTable().getSelectedRow()<0) 
+				return;
+			try
+			{
+				actions.get(ActionModifyResource.class).doAction();
+			}
+			catch (Exception e)
+			{
+				EAM.logException(e);
+			}
+		}
+	}
+
+	public void keyReleased(KeyEvent arg0)
+	{
+	}
+
+	public void keyTyped(KeyEvent arg0)
+	{
+	}
+	
+	Actions actions;
 }
