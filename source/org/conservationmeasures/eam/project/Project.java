b6732376c0f99c654d295d54dc38fb75b2bba73b
nimaa <nimaa@localhost>
2006-12-19 21:00:46 +0000

    name change Finances -> Financial

--- source/org/conservationmeasures/eam/project/Project.java
+++ source/org/conservationmeasures/eam/project/Project.java
@@ -953,7 +953,7 @@ public class Project
 	public static final String SCHEDULE_VIEW_NAME = "Schedule";
 	public static final String MAP_VIEW_NAME = "Map";
 	public static final String WORK_PLAN_VIEW_NAME = "Work Plan";
-	public static final String BUDGET_VIEW_NAME = "Finances";
+	public static final String BUDGET_VIEW_NAME = "Financial";
 	public static final String THREAT_MATRIX_VIEW_NAME = "ThreatMatrix";
 	public static final String NO_PROJECT_VIEW_NAME = "";
 	public static final String DIAGRAM_VIEW_NAME = "Diagram";
