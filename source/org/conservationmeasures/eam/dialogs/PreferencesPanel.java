521305ea1369c8d60a90f0cfb33d668e58e88053
nimaa <nimaa@localhost>
2006-12-21 17:39:02 +0000

    took out currency decimal setting

--- source/org/conservationmeasures/eam/dialogs/PreferencesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/PreferencesPanel.java
@@ -21,7 +21,6 @@ import javax.swing.ListCellRenderer;
 import org.conservationmeasures.eam.main.AppPreferences;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
-import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.utils.DialogGridPanel;
 import org.martus.swing.UiCheckBox;
 import org.martus.swing.UiComboBox;
@@ -34,7 +33,7 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 	{
 		super(mainWindowToUse.getProject());
 		mainWindow = mainWindowToUse;
-		
+
 		Box textBox = Box.createHorizontalBox();
 		textBox.add(Box.createHorizontalGlue());
 		JLabel bodyComponent = new JLabel(headerText);
@@ -45,7 +44,7 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 		Box buttonBox = Box.createHorizontalBox();
 		buttonBox.add(Box.createHorizontalGlue());
 		buttonBox.add(Box.createHorizontalGlue());
-		
+
 		Box box = Box.createVerticalBox();
 		box.add(textBox);
 		box.add(new UiLabel("Choose the colors that look best on your system"));
@@ -55,35 +54,35 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 		add(box);
 		setLocation(Utilities.center(getSize(), Utilities.getViewableRectangle()));		
 	}
-	
+
 	DialogGridPanel createColorPreferencesPanel()
 	{
 		DialogGridPanel panel = new DialogGridPanel();
-		
+
 		panel.add(new UiLabel("Strategy (Yellow)"));
 		interventionDropdown = createColorsDropdown(interventionColorChoices);
 		interventionDropdown.setSelectedItem(mainWindow.getColorPreference(AppPreferences.TAG_COLOR_STRATEGY));
 		interventionDropdown.addActionListener(this);
 		panel.add(interventionDropdown);
-		
+
 		panel.add(new UiLabel("Direct Threat (Pink)"));
 		directThreatDropdown = createColorsDropdown(directThreatColorChoices);
 		directThreatDropdown.setSelectedItem(mainWindow.getColorPreference(AppPreferences.TAG_COLOR_DIRECT_THREAT));
 		directThreatDropdown.addActionListener(this);
 		panel.add(directThreatDropdown);
-		
+
 		panel.add(new UiLabel("Contributing Factor (Orange)"));
 		indirectFactorDropdown = createColorsDropdown(indirectFactorColorChoices);
 		indirectFactorDropdown.setSelectedItem(mainWindow.getColorPreference(AppPreferences.TAG_COLOR_CONTRIBUTING_FACTOR));
 		indirectFactorDropdown.addActionListener(this);
 		panel.add(indirectFactorDropdown);
-		
+
 		panel.add(new UiLabel("Target (Light Green)"));
 		targetDropdown = createColorsDropdown(targetColorChoices);
 		targetDropdown.setSelectedItem(mainWindow.getColorPreference(AppPreferences.TAG_COLOR_TARGET));
 		targetDropdown.addActionListener(this);
 		panel.add(targetDropdown);
-		
+
 		panel.add(new UiLabel("Project Scope (Dark Green)"));
 		scopeDropdown = createColorsDropdown(scopeColorChoices);
 		scopeDropdown.setSelectedItem(mainWindow.getColorPreference(AppPreferences.TAG_COLOR_SCOPE));
@@ -95,47 +94,38 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 		gridVisibleCheckBox.setSelected(mainWindow.getBooleanPreference(AppPreferences.TAG_GRID_VISIBLE));
 		gridVisibleCheckBox.addActionListener(this);
 		panel.add(gridVisibleCheckBox);
-		
-		panel.add(new UiLabel("Currency Decimal Places"));
-		currencyDecimalDropDown = new UiComboBox(decimalPlacesChoices);
-		int currentDecimal = mainWindow.getProject().getMetadata().getCurrencyDecimalPlaces();
-		currencyDecimalDropDown.setSelectedItem(Integer.toString(currentDecimal));
-		currencyDecimalDropDown.addActionListener(this);
-		panel.add(currencyDecimalDropDown);
-		
+
 		return panel;
 	}
-	
+
 	private UiComboBox createColorsDropdown(Color[] colorChoices)
 	{
 		UiComboBox dropdown = new UiComboBox(colorChoices);
 		dropdown.setRenderer(new ColorItemRenderer());
 		return dropdown;
 	}
-	
+
 	void update()
 	{
+		Color interventionColor = (Color)interventionDropdown.getSelectedItem();
+		mainWindow.setColorPreference(AppPreferences.TAG_COLOR_STRATEGY, interventionColor);
+
+		Color indirectFactorColor = (Color)indirectFactorDropdown.getSelectedItem();
+		mainWindow.setColorPreference(AppPreferences.TAG_COLOR_CONTRIBUTING_FACTOR, indirectFactorColor);
+
+		Color directThreatColor = (Color)directThreatDropdown.getSelectedItem();
+		mainWindow.setColorPreference(AppPreferences.TAG_COLOR_DIRECT_THREAT, directThreatColor);
+
+		Color targetColor = (Color)targetDropdown.getSelectedItem();
+		mainWindow.setColorPreference(AppPreferences.TAG_COLOR_TARGET, targetColor);
+
+		Color scopeColor = (Color)scopeDropdown.getSelectedItem();
+		mainWindow.setColorPreference(AppPreferences.TAG_COLOR_SCOPE, scopeColor);
+
+		mainWindow.setBooleanPreference(AppPreferences.TAG_GRID_VISIBLE, gridVisibleCheckBox.isSelected());
+
 		try
 		{
-			Color interventionColor = (Color)interventionDropdown.getSelectedItem();
-			mainWindow.setColorPreference(AppPreferences.TAG_COLOR_STRATEGY, interventionColor);
-
-			Color indirectFactorColor = (Color)indirectFactorDropdown.getSelectedItem();
-			mainWindow.setColorPreference(AppPreferences.TAG_COLOR_CONTRIBUTING_FACTOR, indirectFactorColor);
-
-			Color directThreatColor = (Color)directThreatDropdown.getSelectedItem();
-			mainWindow.setColorPreference(AppPreferences.TAG_COLOR_DIRECT_THREAT, directThreatColor);
-
-			Color targetColor = (Color)targetDropdown.getSelectedItem();
-			mainWindow.setColorPreference(AppPreferences.TAG_COLOR_TARGET, targetColor);
-
-			Color scopeColor = (Color)scopeDropdown.getSelectedItem();
-			mainWindow.setColorPreference(AppPreferences.TAG_COLOR_SCOPE, scopeColor);
-
-			mainWindow.setBooleanPreference(AppPreferences.TAG_GRID_VISIBLE, gridVisibleCheckBox.isSelected());
-
-			String currencyDecimal = (String)currencyDecimalDropDown.getSelectedItem();
-			mainWindow.getProject().setMetadata(ProjectMetadata.TAG_CURRENCY_DECIMAL_PLACES, currencyDecimal);
 
 			mainWindow.savePreferences();
 		}
@@ -145,7 +135,7 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 			EAM.errorDialog("Unable to save preferences");
 		}
 	}
-	
+
 	static class ColorItemRenderer extends Component implements ListCellRenderer
 	{
 		public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
@@ -154,7 +144,7 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 			selected = isSelected;
 			return this;
 		}
-		
+
 		public void paint(Graphics g)
 		{
 			super.paint(g);
@@ -170,7 +160,7 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 		{
 			return new Dimension(48, 16);
 		}
-		
+
 		public Dimension getPreferredSize()
 		{
 			return getSize();
@@ -180,12 +170,12 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 		{
 			return getSize();
 		}
-		
+
 		public Dimension getMaximumSize()
 		{
 			return getSize();
 		}
-		
+
 		boolean selected;
 		Color color;
 	}
@@ -194,12 +184,12 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 	{
 		return EAM.text("Preferences");
 	}
-	
+
 	public void actionPerformed(ActionEvent e)
 	{
 		update();
 	}
-	
+
 	MainWindow mainWindow;
 	UiComboBox interventionDropdown;
 	UiComboBox directThreatDropdown;
@@ -207,16 +197,13 @@ public class PreferencesPanel extends DataInputPanel implements ActionListener
 	UiComboBox targetDropdown;
 	UiComboBox scopeDropdown;
 	UiCheckBox gridVisibleCheckBox; 
-	
+
 	static final Color[] interventionColorChoices = {new Color(255, 255, 0), new Color(240, 240, 0), new Color(255, 255, 128)};
 	static final Color[] directThreatColorChoices = {new Color(255, 150, 150), new Color(255, 128, 128), new Color(220, 150, 150), new Color(255, 200, 200)};
 	static final Color[] indirectFactorColorChoices = {new Color(255, 190, 0), new Color(255, 128, 0), new Color(200, 128, 0), new Color(255, 220, 0), new Color(255, 190, 64), new Color(255, 240, 200)};
 	static final Color[] targetColorChoices = {new Color(153, 255, 153), new Color(200, 255, 200), new Color(80, 255, 80), new Color(64, 220, 64)};
 	static final Color[] scopeColorChoices = {new Color(0, 255, 0), new Color(128, 255, 128), new Color(0, 220, 0), new Color(0, 180, 0), new Color(0, 128, 0)};
-	
+
 	static final String headerText = "<html><H2>e-Adaptive Management Preferences</H2></html>";
-	
-	private String[] decimalPlacesChoices = {"0", "1", "2", "3", };
-	private UiComboBox currencyDecimalDropDown;
 
 }
