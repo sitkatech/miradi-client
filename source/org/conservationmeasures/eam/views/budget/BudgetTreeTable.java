a09f187015294a70b229a6a4a80da25d55ecc239
nimaa <nimaa@localhost>
2006-12-26 22:04:37 +0000

    added font renderer to table columns in financial view treeTable.

--- source/org/conservationmeasures/eam/views/budget/BudgetTreeTable.java
+++ source/org/conservationmeasures/eam/views/budget/BudgetTreeTable.java
@@ -5,9 +5,23 @@
  */
 package org.conservationmeasures.eam.views.budget;
 
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.util.Map;
+
+import javax.swing.JTable;
+import javax.swing.JTree;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 
+import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.Factor;
+import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.views.TreeTableNode;
 import org.conservationmeasures.eam.views.TreeTableWithStateSaving;
 
 public class BudgetTreeTable extends TreeTableWithStateSaving
@@ -18,6 +32,90 @@ public class BudgetTreeTable extends TreeTableWithStateSaving
 		setModel(treeTableModelAdapter);
 		setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
 		getTree().setShowsRootHandles(true);
+		
+		setTableColumnRenderer();
+		
 	}
 
+	private void setTableColumnRenderer()
+	{
+		final int STARTING_TABLE_COLUMN = 1;
+		int columnCount = getColumnModel().getColumnCount();
+		
+		for (int i  = STARTING_TABLE_COLUMN; i < columnCount; i++)
+		{	
+			TableColumn tableColumn = getColumnModel().getColumn(i);
+			tableColumn.setCellRenderer(new FontRenderer(getTree()));
+		}
+	}
+	
+	protected static class FontRenderer extends DefaultTableCellRenderer
+	{
+		public FontRenderer(JTree treeToUse)
+		{
+			tree = treeToUse;
+		
+			defaultFont = getFont();
+			boldFont = defaultFont.deriveFont(Font.BOLD);
+			italicFont = defaultFont.deriveFont(Font.ITALIC);
+			
+			Map map = defaultFont.getAttributes();
+		    map.put(TextAttribute.SIZE, new Float(15.0));
+		    map.put(TextAttribute.WEIGHT, TextAttribute.WEIGHT_SEMIBOLD);
+		    customFont = new Font(map);
+		}
+		
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+		{
+			Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			TreePath path = tree.getPathForRow(row);
+			TreeTableNode node = (TreeTableNode)path.getLastPathComponent();
+			component.setFont(getNodeFont(node));
+			
+			return component;
+		}
+
+		private Font getNodeFont(TreeTableNode node)
+		{
+			Font fontToSet = defaultFont;
+			
+			if (node.getType() == ObjectType.FAKE)
+				fontToSet = customFont;
+			
+			else if(node.getType() == ObjectType.INDICATOR)
+				fontToSet = boldFont;
+			
+			else if(node.getType() == ObjectType.FACTOR)
+				fontToSet = getFactorFont((Factor)node.getObject());
+			
+			else if(node.getType() == ObjectType.TASK)
+				fontToSet = getTaskFont((Task)node.getObject());
+
+			return fontToSet;
+		}
+
+		private Font getFactorFont(Factor factor)
+		{
+			if (factor.isStrategy())
+				return boldFont;
+			
+			return defaultFont;
+		}
+
+		private Font getTaskFont(Task task)
+		{
+			if (task.isActivity())
+				return italicFont;
+			if (task.isMethod())
+				return italicFont;
+			
+			return defaultFont;
+		}
+		
+		Font defaultFont;
+		Font boldFont;
+		Font italicFont;
+		Font customFont;
+		JTree tree;
+	}
 }
