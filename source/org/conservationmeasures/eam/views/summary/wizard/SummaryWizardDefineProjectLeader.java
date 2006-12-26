04fc758a602241a3ebfeb43afc5d5bca38f3a472
richardn <richardn@localhost>
2006-12-26 22:03:06 +0000

    modifed to reflect button text

--- source/org/conservationmeasures/eam/views/summary/wizard/SummaryWizardDefineProjectLeader.java
+++ source/org/conservationmeasures/eam/views/summary/wizard/SummaryWizardDefineProjectLeader.java
@@ -5,6 +5,7 @@
  */
 package org.conservationmeasures.eam.views.summary.wizard;
 
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.views.umbrella.WizardPanel;
 import org.conservationmeasures.eam.views.umbrella.WizardStep;
 
@@ -15,6 +16,16 @@ public class SummaryWizardDefineProjectLeader extends WizardStep
 		super(panelToUse);
 	}
 
+	
+	public void linkClicked(String linkDescription)
+	{
+		if(linkDescription.equals("Definition:InitialProjectTeam"))
+		{
+			EAM.okDialog("Definition: Initial Project Team", new String[] {
+					"The people who conceive of and initiate the project."});
+		}
+	}
+	
 	public String getResourceFileName()
 	{
 		return HTML_FILENAME;
