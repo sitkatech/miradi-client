2c5401c41f0d7381f7d588fd13a18d7182f96666
richardn <richardn@localhost>
2007-01-02 19:18:32 +0000

    call browser for web page in about box in thread...note: use of existing runner interface supplied by Broswer Launcher class...also we allow the launcher to take the logger and error handle defaults by secifiying null to these parameters.

--- source/org/conservationmeasures/eam/views/umbrella/AboutDoer.java
+++ source/org/conservationmeasures/eam/views/umbrella/AboutDoer.java
@@ -19,8 +19,6 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
-import net.sf.wraplog.NoneLogger;
-
 import org.conservationmeasures.eam.dialogs.EAMDialog;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.main.EAM;
@@ -31,6 +29,7 @@ import org.martus.swing.HyperlinkHandler;
 import org.martus.swing.Utilities;
 
 import edu.stanford.ejalbert.BrowserLauncher;
+import edu.stanford.ejalbert.BrowserLauncherRunner;
 
 public class AboutDoer extends Doer  implements HyperlinkHandler
 {
@@ -151,15 +150,19 @@ public class AboutDoer extends Doer  implements HyperlinkHandler
 
 	public void linkClicked(String linkDescription)
 	{
-		try
-		{
-			BrowserLauncher browseLauncher = new BrowserLauncher(new NoneLogger());
-			browseLauncher.openURLinBrowser(linkDescription);
-		}
-		catch (Exception e)
-		{
-			EAM.logException(e);
-		}
+        try 
+        {
+            BrowserLauncherRunner runner = new BrowserLauncherRunner(
+            		new BrowserLauncher(null),
+                    "",
+                    linkDescription,
+                    null);
+            new Thread(runner).start();
+        }
+        catch (Exception e) 
+        {
+        	EAM.logException(e);
+        }
 	}
 
 	public JPopupMenu getRightClickMenu(String url)
