e7523d7c1fd5db0ef161ea716ebb46ac43a80ad4
richardn <richardn@localhost>
2007-01-04 15:11:13 +0000

    added Mthods defintion

--- source/org/conservationmeasures/eam/views/monitoring/wizard/MonitoringSelectMethodsStep.java
+++ source/org/conservationmeasures/eam/views/monitoring/wizard/MonitoringSelectMethodsStep.java
@@ -5,6 +5,7 @@
  */
 package org.conservationmeasures.eam.views.monitoring.wizard;
 
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.views.umbrella.WizardPanel;
 import org.conservationmeasures.eam.views.umbrella.WizardStep;
 
@@ -20,5 +21,16 @@ public class MonitoringSelectMethodsStep extends WizardStep
 		return HTML_FILENAME;
 	}
 	
+	public void linkClicked(String linkDescription)
+	{
+
+		if(linkDescription.equals("Definition:Method"))
+		{
+			EAM.okDialog("Definition: Method",new String[] { 
+					"A specific technique used to collect data to measure an indicator.  " +
+					"Methods vary in their accuracy and reliability, cost-effectiveness, " +
+					"feasibility, and appropriateness." });
+		}
+	}
 	String HTML_FILENAME = "MonitoringSelectMethodsStep.html";
 }
