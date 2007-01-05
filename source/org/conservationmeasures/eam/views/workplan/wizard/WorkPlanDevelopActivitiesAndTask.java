58cdc944388156d204d5dfe4192e22dbab34e305
richardn <richardn@localhost>
2007-01-05 19:31:00 +0000

    next step in work plan

---
+++ source/org/conservationmeasures/eam/views/workplan/wizard/WorkPlanDevelopActivitiesAndTask.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.views.workplan.wizard;
+
+import org.conservationmeasures.eam.views.umbrella.WizardPanel;
+import org.conservationmeasures.eam.views.umbrella.WizardStep;
+
+public class WorkPlanDevelopActivitiesAndTask extends WizardStep
+{
+	public WorkPlanDevelopActivitiesAndTask(WizardPanel wizardToUse)
+	{
+		super(wizardToUse);
+	}
+
+	public String getResourceFileName()
+	{
+		return HTML_FILENAME;
+	}
+
+	String HTML_FILENAME = "WorkPlanDevelopActivitiesAndTask.html";
+}
