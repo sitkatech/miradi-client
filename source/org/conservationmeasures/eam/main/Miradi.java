041780cb6ef005d714d690350eb171b262ea7a88
kevins <kevins@localhost>
2007-01-05 19:54:10 +0000

    Switch build and setup.exe from eAM to Miradi

---
+++ source/org/conservationmeasures/eam/main/Miradi.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.main;
+
+public class Miradi
+{
+	public static void main(String[] args)
+	{
+		EAM.main(args);
+	}
+}
