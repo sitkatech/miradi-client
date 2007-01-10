76a89441d7b4498632a4440b5e1ebc4d1f182bd8
richardn <richardn@localhost>
2007-01-10 18:00:21 +0000

    Images html  page

--- source/org/conservationmeasures/eam/views/images/ImagesView.java
+++ source/org/conservationmeasures/eam/views/images/ImagesView.java
@@ -6,15 +6,22 @@
 package org.conservationmeasures.eam.views.images;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.umbrella.UmbrellaView;
+import org.martus.swing.HtmlViewer;
 import org.martus.swing.ResourceImageIcon;
 import org.martus.swing.UiScrollPane;
+
+import com.jhlabs.awt.GridLayoutPlus;
 
 public class ImagesView extends UmbrellaView
 {
@@ -37,15 +44,53 @@ public class ImagesView extends UmbrellaView
 	public void becomeActive() throws Exception
 	{
 		super.becomeActive();
-		add(new UiScrollPane(new ImagesComponent()), BorderLayout.CENTER);
+		JPanel panel = new JPanel(new GridLayoutPlus(0,1));
+		panel.add(getIntroText());
+		panel.add(new ImagesComponent(), BorderLayout.CENTER);
+		add(new UiScrollPane(panel));
 	}
 
 	public void becomeInactive() throws Exception
 	{
 		super.becomeInactive();
 	}
+	
+	private HtmlViewer getIntroText()
+	{
+		HtmlViewer htmlViewer= new HtmlViewer("",null);
+		htmlViewer.setText(loadResourceFile(OVERVIEW_HTML));
+		
+		//TODO: Find a better way to calculate scroll bar width to subtract
+		setFixedWidth(htmlViewer,this.getSize().width - 20);
+
+		return htmlViewer;
+	}
+	
+	public static void setFixedWidth( Component component, int width )
+	{
+		component.setSize( new Dimension( width, Short.MAX_VALUE ) );
+		Dimension preferredSize = component.getPreferredSize();
+		component.setPreferredSize( new Dimension( width, preferredSize.height ) );
+	}
+	
+	
+	private String loadResourceFile(String htmlFile)
+	{
+		try
+		{
+			return EAM.loadResourceFile(this.getClass(), htmlFile);
+		}
+		catch (Exception e)
+		{
+			EAM.errorDialog("ERROR: file not found: " + this.getClass() + "/" + htmlFile );
+			return null;
+		}
+	}
+	
+	private String OVERVIEW_HTML = "Overview.html";
 
 }
+
 
 	class ImagesComponent extends JTabbedPane
 	{
