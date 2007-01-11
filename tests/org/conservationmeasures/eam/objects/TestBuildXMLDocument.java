c2d313a618ad4bb08e1e9dc300eeda3dcef4616c
richardn <richardn@localhost>
2007-01-11 04:16:05 +0000

    xmlBuild

---
+++ tests/org/conservationmeasures/eam/objects/TestBuildXMLDocument.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.objects;
+
+import java.io.File;
+
+import org.conservationmeasures.eam.database.ProjectServer;
+import org.conservationmeasures.eam.ids.BaseId;
+import org.conservationmeasures.eam.ids.IdList;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.main.EAMTestCase;
+import org.conservationmeasures.eam.objectdata.IdListData;
+import org.conservationmeasures.eam.objectdata.ObjectData;
+import org.conservationmeasures.eam.objectdata.RatingData;
+import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objectpools.EAMObjectPool;
+import org.conservationmeasures.eam.project.Project;
+
+public class TestBuildXMLDocument extends EAMTestCase
+{
+	public TestBuildXMLDocument(String name)
+	{
+		super(name);
+	}
+
+	public void testBuild() throws Exception
+	{
+		try
+		{
+			File projectFile = new File(EAM.getHomeDirectory(),"exportedProject");
+			processObjectPool(projectFile, ObjectType.FACTOR);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	private void processObjectPool(File projectFile, int objectType) throws Exception
+	{
+		
+		ProjectServer ps = new ProjectServer();
+		
+		try
+		{
+			ps.open(projectFile);
+			Project project = new Project(ps);
+			EAMObjectPool pool = project.getPool(objectType);
+			BaseId[] baseIds = pool.getIds();
+			System.out.println("HERE" + baseIds.length);
+			for(int i = 0; i < baseIds.length; ++i)
+			{
+				EAMBaseObject object = (EAMBaseObject) project.findObject(objectType, baseIds[i]);
+				String[] tags = object.getFieldTags();
+				for(int tagIdx = 0; tagIdx < tags.length; ++tagIdx)
+				{
+					String sampleData = getSampleData(object, tags[tagIdx]);
+					System.out.println(sampleData);
+				}
+			}
+		}
+		finally
+		{
+			ps.close();
+		}
+	}
+	
+	
+	private String getSampleData(EAMBaseObject object, String tag)
+	{
+		ObjectData field = object.getField(tag);
+		if(field instanceof IdListData)
+		{
+			IdList list = new IdList();
+			list.add(new BaseId(7));
+			return list.toString();
+		}
+		
+		if(field instanceof RatingData)
+		{
+			return "3";
+		}
+		
+		return tag + tag;
+	}
+}
+
