c6f9067eccc2fe23e1b73ea7e81975925610eb76
richardn <richardn@localhost>
2007-01-17 16:49:25 +0000

    For your review only...I believe we can elemeitate all the tricky code in the threat wizard

---
+++ source/org/conservationmeasures/eam/utils/EAMHtmlViewer.java
@@ -0,0 +1,288 @@
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
+package org.conservationmeasures.eam.utils;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Hashtable;
+
+import javax.swing.JComboBox;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+import javax.swing.text.html.FormView;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.ImageView;
+import javax.swing.text.html.StyleSheet;
+
+import org.martus.swing.HyperlinkHandler;
+import org.martus.swing.ResourceImageIcon;
+import org.martus.swing.UiEditorPane;
+
+
+public class EAMHtmlViewer extends UiEditorPane implements HyperlinkListener
+{
+	public EAMHtmlViewer(String htmlSource, HyperlinkHandler hyperLinkHandler)
+	{
+		linkHandler = hyperLinkHandler;
+		
+		setEditable(false);
+		setText(htmlSource);
+		addHyperlinkListener(this);
+	}
+	
+	public void setText(String text)
+	{
+		HTMLEditorKit htmlKit = new OurHtmlEditorKit(linkHandler);
+		StyleSheet style = htmlKit.getStyleSheet();
+		customizeStyleSheet(style);
+		htmlKit.setStyleSheet(style);
+		setEditorKit(htmlKit);
+
+		Document doc = htmlKit.createDefaultDocument();
+		setDocument(doc);
+		
+		super.setText(text);
+		setCaretPosition(0);
+	}
+
+	
+	public void setFixedWidth( Component component, int width )
+	{
+		component.setSize( new Dimension( width, Short.MAX_VALUE ) );
+		Dimension preferredSize = component.getPreferredSize();
+		component.setPreferredSize( new Dimension( width, preferredSize.height ) );
+	}
+	
+	protected void customizeStyleSheet(StyleSheet style)
+	{
+		style.addRule("body {background: #ffffff;}");
+	}
+
+	public void hyperlinkUpdate(HyperlinkEvent e)
+	{
+		if(e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
+		{
+			String clicked = e.getDescription();
+			linkHandler.linkClicked(clicked);
+		}
+
+	}
+	
+	class OurHtmlEditorKit extends HTMLEditorKit
+	{
+		public OurHtmlEditorKit(HyperlinkHandler handler)
+		{
+			factory = new OurHtmlViewFactory(handler);
+			ourStyleSheet = new StyleSheet();
+			ourStyleSheet.addStyleSheet(super.getStyleSheet());
+		}
+		
+		public ViewFactory getViewFactory()
+		{
+			return factory;
+		}
+
+		public StyleSheet getStyleSheet()
+		{
+			return ourStyleSheet;
+		}
+
+		public void setStyleSheet(StyleSheet s)
+		{
+			ourStyleSheet = s;
+		}
+
+		ViewFactory factory;
+		StyleSheet ourStyleSheet;
+	}
+	
+	class OurHtmlViewFactory extends HTMLEditorKit.HTMLFactory
+	{
+		public OurHtmlViewFactory(HyperlinkHandler handlerToUse)
+		{
+			handler = handlerToUse;
+		}
+		
+		public View create(Element elem)
+		{
+			if(elem.getName().equals("select"))
+			{
+				return new OurSelectView(elem, handler);
+			}
+			if(elem.getName().equals("input"))
+			{
+				AttributeSet attributes = elem.getAttributes();
+				Object typeAttribute = attributes.getAttribute(HTML.Attribute.TYPE);
+				if(typeAttribute.equals("submit"))
+				{
+					return new OurButtonView(elem, handler);
+				}
+				if(typeAttribute.equals("text"))
+				{
+					return new OurTexView(elem, handler);
+				}
+				if(typeAttribute.equals("textarea"))
+				{
+					return new OurTexView(elem, handler);
+				}
+			}
+			else if(elem.getName().equals("img"))
+			{
+				return new OurImageView(elem);
+			}
+			return super.create(elem);
+		}
+		
+		HyperlinkHandler handler;
+	}
+	
+	class OurButtonView extends FormView
+	{
+		public OurButtonView(Element elem, HyperlinkHandler handlerToUse)
+		{
+			super(elem);
+			handler = handlerToUse;
+		}
+
+		protected void submitData(String data)
+		{
+			String buttonName = (String)getElement().getAttributes().getAttribute(HTML.Attribute.NAME);
+			handler.buttonPressed(buttonName);
+		}
+		
+		HyperlinkHandler handler;
+	}
+	
+	class OurSelectView extends FormView implements ItemListener
+	{
+		public OurSelectView(Element elem, HyperlinkHandler handlerToUse)
+		{
+			super(elem);
+			handler = handlerToUse;
+		}
+
+		protected Component createComponent()
+		{
+			comboBox = (JComboBox)super.createComponent();
+			comboBox.addItemListener(this);
+			String elementName = (String)getElement().getAttributes().getAttribute(HTML.Attribute.NAME);
+			String key = handler.getClass().getSimpleName() + "." + elementName;
+			components.put(key,comboBox);
+			System.out.println("ADDING comboBox:" + key );
+			return comboBox;
+		}
+
+		public void itemStateChanged(ItemEvent e)
+		{
+			String name = (String)getElement().getAttributes().getAttribute(HTML.Attribute.NAME);
+			handler.valueChanged(name, comboBox.getSelectedItem().toString());
+		}
+		
+		HyperlinkHandler handler;
+		JComboBox comboBox;
+	}
+	
+	
+	class OurTexView extends FormView implements DocumentListener
+	{
+		public OurTexView(Element elem, HyperlinkHandler handlerToUse)
+		{
+			super(elem);
+			handler = handlerToUse;
+		}
+
+		protected Component createComponent()
+		{
+			textField = (JTextComponent)super.createComponent();
+			textField.getDocument().addDocumentListener(this);
+			String elementName = (String)getElement().getAttributes().getAttribute(HTML.Attribute.NAME);
+			String key = handler.getClass().getSimpleName() + "." + elementName;
+			components.put(key,textField);
+			System.out.println("ADDING textField:" + key );
+			return textField;
+		}
+
+		public void changedUpdate(DocumentEvent event) 
+		{
+			notifyHandler();
+		}
+
+
+		public void insertUpdate(DocumentEvent event) 
+		{
+			notifyHandler();
+		}
+
+		public void removeUpdate(DocumentEvent event) 
+		{
+			notifyHandler();
+		}
+		
+		private void notifyHandler() 
+		{
+			String name = (String)getElement().getAttributes().getAttribute(HTML.Attribute.NAME);
+			handler.valueChanged(name, textField.getText());
+		}
+		
+		protected void submitData(String data)
+		{
+		}
+		
+		HyperlinkHandler handler;
+		JTextComponent textField;
+
+	}
+
+	class OurImageView extends ImageView
+	{
+		public OurImageView(Element elem)
+		{
+			super(elem);
+			name = (String)elem.getAttributes().getAttribute(HTML.Attribute.SRC);
+		}
+
+		public Image getImage()
+		{
+			if(image == null)
+			{
+				try
+				{
+					ResourceImageIcon icon = new ResourceImageIcon(name);
+					image = icon.getImage();
+				}
+				catch(NullPointerException e)
+				{
+					throw new RuntimeException(name, e);
+				}
+			}
+			return image;
+		}
+		
+		String name;
+		Image image;
+	}
+	
+	HyperlinkHandler linkHandler;
+	//TODO: rather then making static the a method can be added to allow the handler to ask (getComponents method)for the table.
+	// This can be done also by pushing via a setComponents method on the handler interface ; also setComponent method can be used 
+	// bypassing the need for a table in the HtmlViewer all together. Note either of these solutions wouold also simplify the key 
+	// as only the element name attr would be needed.
+	// Side note: I tested if we can set the value of a component via the handler and yes you can at any time.
+	public static Hashtable components = new Hashtable();
+}
+
