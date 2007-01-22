83ad049d3012ada33130f8131b208cf4183425c5
richardn <richardn@localhost>
2007-01-22 16:22:34 +0000

    html panel resize fix

--- source/org/conservationmeasures/eam/views/umbrella/HtmlViewPanel.java
+++ source/org/conservationmeasures/eam/views/umbrella/HtmlViewPanel.java
@@ -1,30 +1,30 @@
-/* 
-* Copyright 2005-2007, Wildlife Conservation Society, 
-* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
-* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
-*/ 
+/*
+ * Copyright 2006, The Benetech Initiative
+ * 
+ * This file is confidential and proprietary
+ */
 package org.conservationmeasures.eam.views.umbrella;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
 
 import org.conservationmeasures.eam.dialogs.EAMDialog;
 import org.conservationmeasures.eam.main.EAM;
 import org.martus.swing.HtmlViewer;
 import org.martus.swing.HyperlinkHandler;
-import org.martus.swing.UiScrollPane;
 import org.martus.swing.Utilities;
-
-import com.jhlabs.awt.GridLayoutPlus;
 
 import edu.stanford.ejalbert.BrowserLauncher;
 import edu.stanford.ejalbert.BrowserLauncherRunner;
@@ -45,55 +45,51 @@ public class HtmlViewPanel implements HyperlinkHandler
 		String title = EAM.text("Title|" + viewTitle);
 		EAMDialog dlg = new EAMDialog(EAM.mainWindow, title);
 		dlg.setModal(true);
-		
+
 		String body = loadHtml();
 		if (body == null)
 			return;
 		HtmlViewer bodyComponent =  new HtmlViewer(body, this);
 		bodyComponent.setFont(Font.getFont("Arial"));
+		dlg.setBackground(bodyComponent.getBackground());
 
+		Container contents = dlg.getContentPane();
+		contents.setLayout(new BorderLayout());
+		contents.add(new JScrollPane(bodyComponent), BorderLayout.CENTER);
+		contents.add(createButtonBar(dlg), BorderLayout.AFTER_LAST_LINE);
 		
-		JButton close = new JButton(new CloseAction(dlg));
-
-		JPanel panel = new JPanel(new GridLayoutPlus(0,1));
-		bodyComponent.setFixedWidth(bodyComponent,750);
-		
-		String columnZeroRowZero = "0, 0";
-		panel.add(bodyComponent, columnZeroRowZero);
-		String columnZeroRowOneCenteredCentered = "0, 1, c, c";
-		panel.add(close, columnZeroRowOneCenteredCentered);
-		
-		panel.setBorder(new EmptyBorder(18,18,18,18));
-		panel.setBackground(bodyComponent.getBackground());
-		
-		UiScrollPane scrollPane = new UiScrollPane(panel);
-		scrollPane.getHorizontalScrollBar().setUnitIncrement(EAM.STANDARD_SCROLL_INCREMENT);
-		scrollPane.getVerticalScrollBar().setUnitIncrement(EAM.STANDARD_SCROLL_INCREMENT);
-		dlg.getContentPane().add(scrollPane);
-		dlg.pack();
-
-		fudgeSize(dlg);
-		
-		dlg.getRootPane().setDefaultButton(close);
-		close.requestFocus(true);
+		calculateHeight(dlg, contents, bodyComponent);
+		Utilities.centerDlg(dlg);
 		dlg.setVisible(true);
 	}
 
-	// TODO: There really has to be a better way to get a dialog to contain and center its self
-	private void fudgeSize(EAMDialog dlg)
+
+	private void calculateHeight(EAMDialog dlg, Container contents, HtmlViewer bodyComponent)
 	{
-		Dimension dimension = dlg.getSize();
+		final int buttonBarHeight = 40;
+		final int forcedWidth = 900;
+		
+		bodyComponent.setFixedWidth(bodyComponent, forcedWidth);
 		Rectangle rectangle = Utilities.getViewableRectangle();
-		if (rectangle.height < dimension.height)
-		{
-			dimension.height = dimension.height - 70;
-			dimension.width = dimension.width + 20;
-			dlg.setSize(dimension);
-			rectangle.height = rectangle.height - 20;
-		}
-		dlg.setLocation(Utilities.center(dimension, rectangle));
+		Dimension dim = contents.getPreferredSize();
+		
+		if (rectangle.height > dim.height)
+			rectangle.height = dim.height + buttonBarHeight;
+
+		dlg.setSize(forcedWidth, rectangle.height);
 	}
 
+
+	private Box createButtonBar(EAMDialog dlg)
+	{
+		JButton close = new JButton(new CloseAction(dlg));
+		close.requestFocus(true);
+		dlg.getRootPane().setDefaultButton(close);
+		Box buttonBar = Box.createHorizontalBox();
+		Component[] components = new Component[] {Box.createHorizontalGlue(), close};
+		Utilities.addComponentsRespectingOrientation(buttonBar, components);
+		return buttonBar;
+	}
 	
 	private String loadHtml()
 	{
