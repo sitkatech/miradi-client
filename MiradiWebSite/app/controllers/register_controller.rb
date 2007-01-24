0db16ba90d7e0b42dcd08dc6a1746648ee2e4853
kevins <kevins@localhost>
2007-01-24 20:54:38 +0000

    Require valid access code to register

--- MiradiWebSite/app/controllers/register_controller.rb
+++ MiradiWebSite/app/controllers/register_controller.rb
@@ -3,6 +3,16 @@ class RegisterController < ApplicationController
     if request.post?
   	  email = params[:user][:email]
   	  password = params[:user][:password]
+  	  access_code = params[:user][:access_code]
+  	  
+  	  ac = AccessCode.find(:first, :conditions=>["code = ?", access_code])
+  	  if !ac
+  	    session[:user] = nil
+  	    flash[:warning] = "Unknown Access Code"
+  	    redirect_to :action => 'index'
+  	    return
+  	  end
+  	  
       @user = User.new(@params[:user])
       if @user.save
         session[:user] = User.authenticate(email, password)
