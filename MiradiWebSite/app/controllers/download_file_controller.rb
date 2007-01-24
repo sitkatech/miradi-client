902802400e0537fa53196730712aed67334ef820
kevins <kevins@localhost>
2007-01-24 16:03:50 +0000

    Proof-of-concept for file downloading

---
+++ MiradiWebSite/app/controllers/download_file_controller.rb
@@ -0,0 +1,15 @@
+$available_files = ['README']
+
+class DownloadFileController < ApplicationController
+  def index
+	file = params[:file]
+	if(!$available_files.index(file))
+	  redirect_to :action => "download" 
+	end
+	type = 'application/octet-stream'
+	if(file == 'README')
+	  type = 'text'
+	end
+	send_file('private/' + file, {:stream => false, :type => type})
+  end
+end
