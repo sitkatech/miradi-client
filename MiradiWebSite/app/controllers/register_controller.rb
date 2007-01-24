d617cfc91d354f11d13a9594570111140843c6e7
kevins <kevins@localhost>
2007-01-24 20:45:24 +0000

    Show error on unsuccessful registration

--- MiradiWebSite/app/controllers/register_controller.rb
+++ MiradiWebSite/app/controllers/register_controller.rb
@@ -1,2 +1,19 @@
 class RegisterController < ApplicationController
+  def attempt
+    if request.post?
+  	  email = params[:user][:email]
+  	  password = params[:user][:password]
+      @user = User.new(@params[:user])
+      if @user.save
+        session[:user] = User.authenticate(email, password)
+        flash[:message] = "Registration successful"
+        redirect_to "/download"          
+      else
+        session[:user] = nil
+        flash[:warning] = "Registration unsuccessful"
+        redirect_to "/register"
+      end
+  
+    end
+  end
 end
