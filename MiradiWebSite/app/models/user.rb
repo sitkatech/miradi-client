684014c56d6d6788253c20e5ae11d5fb36ff2e32
kevins <kevins@localhost>
2007-01-24 19:33:51 +0000

    Fix typo in comment

--- MiradiWebSite/app/models/user.rb
+++ MiradiWebSite/app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   end
   
   def self.random_string(len)
-    #generat a random password consisting of strings and digits
+    #generate a random password consisting of strings and digits
     chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
     newpass = ""
     1.upto(len) { |i| newpass << chars[rand(chars.size-1)] }
