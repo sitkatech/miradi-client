a99f7dec3973ebac5d1860db565df3f78a8e21af
kevins <kevins@localhost>
2007-01-25 19:42:02 +0000

    Propagate to 1.0-Beta branch, and omit tmp directory

--- MiradiWebSite/push.sh
+++ MiradiWebSite/push.sh
@@ -1,3 +1,3 @@
-rsync -vHSzrlgoDC -r . cmp.benetech.org:/var/www/miradi.org/
+rsync -vHSzrlgoDC -r --exclude=tmp/ . cmp.benetech.org:/var/www/miradi.org/
 rsync -vHSzrlgoDC -r ../README cmp.benetech.org:/var/www/miradi.org/private/
 rsync -vHSzrlgoDC -r ../dist/MiradiSetup.exe cmp.benetech.org:/var/www/miradi.org/private/
