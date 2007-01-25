ed78bc6121a70b76b6c9112818fe03908b29809f
kevins <kevins@localhost>
2007-01-25 20:55:36 +0000

    Push Mac and Linux zips plus MarineExample to the new download site

--- MiradiWebSite/push.sh
+++ MiradiWebSite/push.sh
@@ -1,3 +1,6 @@
 rsync -vHSzrlgoDC -r --exclude=tmp/ . cmp.benetech.org:/var/www/miradi.org/
 rsync -vHSzrlgoDC -r ../README cmp.benetech.org:/var/www/miradi.org/private/
 rsync -vHSzrlgoDC -r ../dist/MiradiSetup.exe cmp.benetech.org:/var/www/miradi.org/private/
+rsync -vHSzrlgoDC -r ../dist/Miradi-Mac.zip cmp.benetech.org:/var/www/miradi.org/private/
+rsync -vHSzrlgoDC -r ../dist/Miradi-Linux.zip cmp.benetech.org:/var/www/miradi.org/private/
+rsync -vHSzrlgoDC -r ../dist/MarineExample.zip cmp.benetech.org:/var/www/miradi.org/private/
