8ab17f9b386573fcf144f8d5e627d686bf78167c
kevins <kevins@localhost>
2007-01-26 16:26:04 +0000

    Exclude log directory from push

--- MiradiWebSite/push.sh
+++ MiradiWebSite/push.sh
@@ -1,4 +1,4 @@
-rsync -vHSzrlgoDC -r --exclude=tmp/ . cmp.benetech.org:/var/www/miradi.org/
+rsync -vHSzrlgoDC -r --exclude=tmp/ --exclude=log/ . cmp.benetech.org:/var/www/miradi.org/
 rsync -vHSzrlgoDC -r ../README cmp.benetech.org:/var/www/miradi.org/private/
 rsync -vHSzrlgoDC -r ../dist/MiradiSetup.exe cmp.benetech.org:/var/www/miradi.org/private/
 rsync -vHSzrlgoDC -r ../dist/Miradi-Mac.zip cmp.benetech.org:/var/www/miradi.org/private/
