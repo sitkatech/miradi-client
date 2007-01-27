9198b5a5bb6a7bcb6bb4de4c87b303fd62cd8924
kevins <kevins@localhost>
2007-01-27 05:29:56 +0000

    Add user name field; add all fields to registration; fix created datetime

--- MiradiWebSite/db/schema.rb
+++ MiradiWebSite/db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 3) do
+ActiveRecord::Schema.define(:version => 5) do
 
   create_table "access_codes", :force => true do |t|
     t.column "organization", :string
@@ -10,15 +10,16 @@ ActiveRecord::Schema.define(:version => 3) do
   end
 
   create_table "users", :force => true do |t|
-    t.column "email",             :string,   :limit => 80
-    t.column "password_hash",     :string,   :limit => 80
-    t.column "password_salt",     :string,   :limit => 80
-    t.column "organization",      :string,   :limit => 80
-    t.column "position",          :string,   :limit => 80
-    t.column "created_timestamp", :datetime
-    t.column "access_code",       :string,   :limit => 80
-    t.column "notes",             :string,   :limit => 500
-    t.column "admin_flag",        :boolean
+    t.column "email",         :string,   :limit => 80
+    t.column "password_hash", :string,   :limit => 80
+    t.column "password_salt", :string,   :limit => 80
+    t.column "organization",  :string,   :limit => 80
+    t.column "position",      :string,   :limit => 80
+    t.column "access_code",   :string,   :limit => 80
+    t.column "notes",         :string,   :limit => 500
+    t.column "admin_flag",    :boolean
+    t.column "created_at",    :datetime
+    t.column "name",          :string,   :limit => 80
   end
 
 end
