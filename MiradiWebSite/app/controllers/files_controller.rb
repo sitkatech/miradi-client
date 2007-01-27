f2b3469237c4a895ff536f039532318f402f8902
kevins <kevins@localhost>
2007-01-27 17:36:34 +0000

    Display download file sizes (computed dynamically)

--- MiradiWebSite/app/controllers/files_controller.rb
+++ MiradiWebSite/app/controllers/files_controller.rb
@@ -2,13 +2,21 @@ class FilesController < ApplicationController
   before_filter :login_required
 
   def index
-    render :action => 'files'
+    redirect_to :action => 'files'
+  end
+  
+  def files
+    @sizeK = Hash.new
+	dir = get_private_directory
+	Dir.foreach(dir) do | f |
+	  @sizeK[f] = File.size(File.join(dir, f)) / 1024
+	end
   end
   
   def file
 	file = params[:file]
 
-	dir = File.join(RAILS_ROOT,'private')
+	dir = get_private_directory
 	
 	if(file.index('.') == 0 || !Dir.entries(dir).index(file))
       flash[:warning]  = "File not found"
@@ -26,5 +34,9 @@ class FilesController < ApplicationController
 	path = File.join(dir, file)
 	raise Exception, "Cannot read file #{path}" unless File.exist?(path)# and File.readable?(path)
 	send_file(path, {:stream => false, :type => type, :disposition => disposition})
+  end
+  
+  def get_private_directory
+	return File.join(RAILS_ROOT,'private')
   end
 end
