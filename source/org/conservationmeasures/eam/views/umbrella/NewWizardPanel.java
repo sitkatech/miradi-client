1fa30eeba9479b97a14339ec6592cb39cc029baa
richardn <richardn@localhost>
2007-01-30 18:07:25 +0000

    added TODO comment , unalbe to allow code to fall thouigh untill all wizards are converted.

--- source/org/conservationmeasures/eam/views/umbrella/NewWizardPanel.java
+++ source/org/conservationmeasures/eam/views/umbrella/NewWizardPanel.java
@@ -102,9 +102,9 @@ public class NewWizardPanel extends JPanel implements IWizardPanel
 		
 		if (!viewNameNew.equals(viewNameCur))
 		{
-			//TODO: should be in a single undo/redo transaction
 			mainWindow.getProject().executeCommand(new CommandSwitchView(viewNameNew));
 			EAM.logWarning("During wizard migration the target step may not be correclty opened:" + newStep);
+			//TODO: this line can be removed and allowed to fall into the else code when all wizards have been converted.
 			mainWindow.getCurrentView().jump(newStep);
 		}
 		else {
