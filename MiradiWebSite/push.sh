9e2649d1f56292c11d2f0e7dcec7743598d8e0a6
kevins <kevins@localhost>
2007-02-01 21:08:55 +0000

    Merge in changes from Beta-1.0 Release Branch

--- MiradiWebSite/push.sh
+++ MiradiWebSite/push.sh
@@ -1,6 +1,6 @@
 rsync -vHSzrlgoD -r --exclude=tmp/ --exclude=log/ --exclude=CVS/ . miradi.org:/var/www/miradi.org/
-rsync -vHSzrlgoD -r ../README miradi.org:/var/www/miradi.org/private/
-rsync -vHSzrlgoD -r ../dist/MiradiSetup.exe miradi.org:/var/www/miradi.org/private/
-rsync -vHSzrlgoD -r ../dist/Miradi-Mac.zip miradi.org:/var/www/miradi.org/private/
-rsync -vHSzrlgoD -r ../dist/Miradi-Linux.zip miradi.org:/var/www/miradi.org/private/
-rsync -vHSzrlgoD -r ../dist/MarineExample.zip miradi.org:/var/www/miradi.org/private/
+#rsync -vHSzrlgoD -r ../README miradi.org:/var/www/miradi.org/private/
+#rsync -vHSzrlgoD -r ../dist/MiradiSetup.exe miradi.org:/var/www/miradi.org/private/
+#rsync -vHSzrlgoD -r ../dist/Miradi-Mac.zip miradi.org:/var/www/miradi.org/private/
+#rsync -vHSzrlgoD -r ../dist/Miradi-Linux.zip miradi.org:/var/www/miradi.org/private/
+#rsync -vHSzrlgoD -r ../dist/MarineExample.zip miradi.org:/var/www/miradi.org/private/
