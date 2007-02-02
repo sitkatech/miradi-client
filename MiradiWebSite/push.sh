4ad26681696ebbaa483fc3abc1858f032158f5cb
kevins <kevins@localhost>
2007-02-02 16:28:18 +0000

    Remove commented-out code

--- MiradiWebSite/push.sh
+++ MiradiWebSite/push.sh
@@ -1,6 +1,1 @@
-rsync -vHSzrlgoD -r --exclude=tmp/ --exclude=log/ --exclude=CVS/ . miradi.org:/var/www/miradi.org/
-#rsync -vHSzrlgoD -r ../README miradi.org:/var/www/miradi.org/private/
-#rsync -vHSzrlgoD -r ../dist/MiradiSetup.exe miradi.org:/var/www/miradi.org/private/
-#rsync -vHSzrlgoD -r ../dist/Miradi-Mac.zip miradi.org:/var/www/miradi.org/private/
-#rsync -vHSzrlgoD -r ../dist/Miradi-Linux.zip miradi.org:/var/www/miradi.org/private/
-#rsync -vHSzrlgoD -r ../dist/MarineExample.zip miradi.org:/var/www/miradi.org/private/
+rsync --verbose --times --sparse --compress --recursive --exclude=tmp/ --exclude=log/ --exclude=CVS/ . miradi.org:/var/www/miradi.org/
