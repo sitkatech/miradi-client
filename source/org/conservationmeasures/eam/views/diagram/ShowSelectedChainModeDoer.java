b8c08717d6756601552d02702d9b1686f245ffd6
richardn <richardn@localhost>
2007-02-14 17:27:07 +0000

    modifed to add orphaned draft strats to brainstorm mode

--- source/org/conservationmeasures/eam/views/diagram/ShowSelectedChainModeDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/ShowSelectedChainModeDoer.java
@@ -5,16 +5,20 @@
 */ 
 package org.conservationmeasures.eam.views.diagram;
 
+import java.util.Vector;
+
 import org.conservationmeasures.eam.commands.CommandBeginTransaction;
 import org.conservationmeasures.eam.commands.CommandEndTransaction;
 import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.diagram.DiagramComponent;
+import org.conservationmeasures.eam.diagram.DiagramModel;
 import org.conservationmeasures.eam.diagram.cells.DiagramFactor;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.ids.IdList;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.Factor;
 import org.conservationmeasures.eam.objects.ViewData;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.ViewDoer;
@@ -56,17 +60,18 @@ public class ShowSelectedChainModeDoer extends ViewDoer
 			if (project.getOnlySelectedCells().length == 1)
 				SelectChain.selectAllChainsRelatedToAllSelectedCells(diagram);
 
-			BaseId viewId = getCurrentViewId();
-
+			DiagramFactor[] orphanedDaftStrats = getOrphanedDraftStrategies(project, view, diagram);
 			DiagramFactor[] selectedNodes = project.getOnlySelectedFactors();
-			IdList selectedNodeIds = new IdList();
-			for(int i = 0; i < selectedNodes.length; ++i)
-			{
-				selectedNodeIds.add(selectedNodes[i].getWrappedId());
-			}
+			
+			BaseId viewId = getCurrentViewId();
+			IdList nodeIdsToProcess = new IdList();
+			
+			addFactorsToList(selectedNodes, nodeIdsToProcess);
+			addFactorsToList(orphanedDaftStrats, nodeIdsToProcess);
+			
 			project.executeCommand(new CommandBeginTransaction());
 			project.executeCommand(new CommandSetObjectData(ObjectType.VIEW_DATA, viewId, 
-					ViewData.TAG_BRAINSTORM_NODE_IDS, selectedNodeIds.toString()));
+					ViewData.TAG_BRAINSTORM_NODE_IDS, nodeIdsToProcess.toString()));
 			
 			project.executeCommand(new CommandSetObjectData(ObjectType.VIEW_DATA, viewId, 
 					ViewData.TAG_CURRENT_MODE, ViewData.MODE_STRATEGY_BRAINSTORM));
@@ -79,6 +84,31 @@ public class ShowSelectedChainModeDoer extends ViewDoer
 		}
 	}
 
+	private void addFactorsToList(DiagramFactor[] orphanedDaftStrats, IdList selectedNodeIds)
+	{
+		for(int i = 0; i < orphanedDaftStrats.length; ++i)
+		{
+			selectedNodeIds.add(orphanedDaftStrats[i].getWrappedId());
+		}
+	}
+
+	private DiagramFactor[] getOrphanedDraftStrategies(Project project, DiagramView view, DiagramComponent diagram)
+	{
+		Vector diagramFactors = new Vector();
+		DiagramModel model = project.getDiagramModel();
+		Factor[] factors = project.getFactorPool().getStrategies();
+		for (int i=0; i<factors.length; ++i)
+		{
+			if (!factors[i].isStatusDraft())
+				continue;
+			DiagramFactor diagramFactor = model.getDiagramFactorByWrappedId(factors[i].getFactorId());
+			if (model.getFactorLinks(diagramFactor).size() > 0) 
+				continue;
+			diagramFactors.add(diagramFactor);
+		}
+		return (DiagramFactor[])diagramFactors.toArray(new DiagramFactor[0]);
+	}
+	
 	private BaseId getCurrentViewId() throws Exception
 	{
 		ViewData viewData = getProject().getCurrentViewData();
