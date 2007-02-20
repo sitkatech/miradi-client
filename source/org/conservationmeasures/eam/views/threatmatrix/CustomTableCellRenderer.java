707dc48a59b746c047cdd88b5268117d59f69741
richardn <richardn@localhost>
2007-02-20 13:56:08 +0000

    reating width increased to 12 from 10

--- source/org/conservationmeasures/eam/views/threatmatrix/CustomTableCellRenderer.java
+++ source/org/conservationmeasures/eam/views/threatmatrix/CustomTableCellRenderer.java
@@ -225,7 +225,7 @@ class CustomTableCellRenderer extends JComponent implements TableCellRenderer
 		//  Do nothing, as recommended in the javadocs for DefaultTableCellRenderer
 	}
 
-	private static final int INNER_CELL_RATING_BOX_WIDTH = 10;
+	private static final int INNER_CELL_RATING_BOX_WIDTH = 12;
 	ThreatGridPanel threatGridPanel;
 	ThreatRatingBundle bundle;
 	ValueOption valueOption;
