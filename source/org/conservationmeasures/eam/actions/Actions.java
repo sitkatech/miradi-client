8fbe12f5bb0a24b4a82acf75c34444613f73921e
richardn <richardn@localhost>
2007-03-09 16:37:37 +0000

    added action for indicator delte

--- source/org/conservationmeasures/eam/actions/Actions.java
+++ source/org/conservationmeasures/eam/actions/Actions.java
@@ -160,9 +160,12 @@ public class Actions
 		registerAction(new ActionDeleteObjective(mainWindow));
 		registerAction(new ActionCreateGoal(mainWindow));
 		registerAction(new ActionDeleteGoal(mainWindow));
+		
 		registerAction(new ActionCreateKeyEcologicalAttribute(mainWindow));
 		registerAction(new ActionDeleteKeyEcologicalAttribute(mainWindow));
 		registerAction(new ActionCreateKeyEcologicalAttributeIndicator(mainWindow));
+		registerAction(new ActionDeleteKeyEcologicalAttributeIndicator(mainWindow));
+	
 		registerAction(new ActionViewPossibleTeamMembers(mainWindow));
 		registerAction(new ActionTeamCreateMember(mainWindow));
 		registerAction(new ActionTeamRemoveMember(mainWindow));
