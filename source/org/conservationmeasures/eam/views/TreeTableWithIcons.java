262578e524aee4f81330b0333aa05f63414d859d
nimaa <nimaa@localhost>
2007-03-27 22:00:20 +0000

    fixed windows Jtable problem

--- source/org/conservationmeasures/eam/views/TreeTableWithIcons.java
+++ source/org/conservationmeasures/eam/views/TreeTableWithIcons.java
@@ -271,6 +271,10 @@ public class TreeTableWithIcons extends JTreeTable implements ObjectPicker
 	public void valueChanged(ListSelectionEvent e)
 	{
 		super.valueChanged(e);
+		
+		if(selectionListeners == null)
+			return;
+		
 		for(int i = 0; i < selectionListeners.size(); ++i)
 		{
 			SelectionChangeListener listener = (SelectionChangeListener)selectionListeners.get(i);
