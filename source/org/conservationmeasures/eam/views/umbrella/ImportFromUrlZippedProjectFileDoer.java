0f8105993a02b0f4631768c4c42cec658d01dd21
richardn <richardn@localhost>
2007-03-28 16:01:51 +0000

    fixed fixme...added try finally blocks to deal with file close

--- source/org/conservationmeasures/eam/views/umbrella/ImportFromUrlZippedProjectFileDoer.java
+++ source/org/conservationmeasures/eam/views/umbrella/ImportFromUrlZippedProjectFileDoer.java
@@ -8,6 +8,7 @@ package org.conservationmeasures.eam.views.umbrella;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -26,52 +27,49 @@ public class ImportFromUrlZippedProjectFileDoer
 	{
 		String remotePath = "https://miradi.org/MarineExample.zip";
 		
-		OutputStream outputStream = null;
-		InputStream inputStream = null;
 		File tempDir = null;
 		try
 		{
-			tempDir = File.createTempFile(TEMP_FILE_NAME, null);
 			URL remoteFile = new URL(remotePath);
 			String newName = Utility.getFileNameWithoutExtension(remoteFile.getFile());
 			newName = Project.makeProjectFilenameLegal(newName);
 			Project.validateNewProject(newName);
 			
-			outputStream = new BufferedOutputStream(new FileOutputStream(tempDir));
-			inputStream = remoteFile.openConnection().getInputStream();
-			Utility.copy(inputStream, outputStream);
-			ProjectUnzipper.unzipToProjectDirectory(tempDir, EAM.getHomeDirectory(), newName);
-			EAM.notifyDialog(EAM.text("Import Completed"));
-			
+			tempDir = File.createTempFile(TEMP_FILE_NAME, null);
+			OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(tempDir));
+			try
+			{
+				processCopy(outputStream, tempDir, remoteFile, newName);
+			}
+			finally 
+			{
+				outputStream.close();
+				DirectoryUtils.deleteEntireDirectoryTree(tempDir);
+			}
 		}
 		catch(Exception e)
 		{
 			String errorText = "Import Failed:" + e.getMessage();
 			EAM.notifyDialog(EAM.text(errorText));
 		}
-		finally
+	}
+
+
+	private static void processCopy(OutputStream outputStream, File tempDir, URL remoteFile, String newName) throws IOException, Exception
+	{
+		InputStream inputStream = remoteFile.openConnection().getInputStream();
+		try 
 		{
-			cleanUp(outputStream, inputStream, tempDir);
+			Utility.copy(inputStream, outputStream);
+			ProjectUnzipper.unzipToProjectDirectory(tempDir, EAM.getHomeDirectory(), newName);
+			EAM.notifyDialog(EAM.text("Import Completed"));
+		}
+		finally 
+		{
+			inputStream.close();
 		}
 	}
 	
-
-	//FIXME: can be coded better (Richard, with help from Kevin)
-	static private void cleanUp(OutputStream outputStream, InputStream inputStream, File tempDir)
-	{
-		try
-		{
-			if(inputStream != null)
-				inputStream.close();
-			if(outputStream != null)
-				outputStream.close();
-			DirectoryUtils.deleteEntireDirectoryTree(tempDir);
-		}
-		catch(Exception ioe)
-		{
-		}
-	}
-
 	static private String TEMP_FILE_NAME = "URLImport";
 	
 }
