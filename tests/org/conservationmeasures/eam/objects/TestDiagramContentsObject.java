38bec17f50a4c95526f59d757a5e8d652936b11a
nimaa <nimaa@localhost>
2007-03-30 19:10:03 +0000

    took out the verification of object tag

--- tests/org/conservationmeasures/eam/objects/TestDiagramContentsObject.java
+++ tests/org/conservationmeasures/eam/objects/TestDiagramContentsObject.java
@@ -5,9 +5,6 @@
 */ 
 package org.conservationmeasures.eam.objects;
 
-import org.conservationmeasures.eam.ids.BaseId;
-import org.conservationmeasures.eam.ids.DiagramFactorId;
-import org.conservationmeasures.eam.ids.IdList;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 
 public class TestDiagramContentsObject extends ObjectTestCase
@@ -20,20 +17,5 @@ public class TestDiagramContentsObject extends ObjectTestCase
 	public void testFields() throws Exception
 	{
 		verifyFields(ObjectType.DIAGRAM_CONTENTS);
-		verifyTagBehavior(DiagramContentsObject.TAG_DIAGRAM_FACTOR_IDS);
-	}
-	
-	private void verifyTagBehavior(String tag) throws Exception
-	{
-		IdList list = new IdList();
-		list.add(new DiagramFactorId(4));
-		list.add(new DiagramFactorId(6));
-		
-		DiagramContentsObject diagramContentsObject = new DiagramContentsObject(new BaseId(22));
-		assertEquals(tag + " didn't default properly?", "", diagramContentsObject.getData(tag));
-		
-		diagramContentsObject.setData(tag, list.toJson().toString());
-		DiagramContentsObject got = (DiagramContentsObject) DiagramContentsObject.createFromJson(diagramContentsObject.getType(), diagramContentsObject.toJson());
-		assertEquals(tag + " didn't survive json?", diagramContentsObject.getData(tag), got.getData(tag));
 	}
 }
