15f6fc690d29fed1d58faf43301c23a569e8417d
nimaa <nimaa@localhost>
2007-03-30 19:40:50 +0000

    tags are verified automatically.  took out the verification code.

--- tests/org/conservationmeasures/eam/objects/TestAccountingCode.java
+++ tests/org/conservationmeasures/eam/objects/TestAccountingCode.java
@@ -5,7 +5,6 @@
  */
 package org.conservationmeasures.eam.objects;
 
-import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 
 public class TestAccountingCode extends ObjectTestCase
@@ -18,18 +17,5 @@ public class TestAccountingCode extends ObjectTestCase
 	public void testFields() throws Exception
 	{
 		verifyFields(ObjectType.ACCOUNTING_CODE);
-		verifyTagBehavior(AccountingCode.TAG_LABEL);
-		verifyTagBehavior(AccountingCode.TAG_LABEL);
-		verifyTagBehavior(AccountingCode.TAG_COMMENTS);
-	}
-
-	private void verifyTagBehavior(String tag) throws Exception
-	{
-		String value = "ifislliefj";
-		AccountingCode accountingCode = new AccountingCode(new BaseId(22));
-		assertEquals(tag + " didn't default properly?", "", accountingCode.getData(tag));
-		accountingCode.setData(tag, value);
-		AccountingCode got = (AccountingCode)AccountingCode.createFromJson(accountingCode.getType(), accountingCode.toJson());
-		assertEquals(tag + " didn't survive json?", accountingCode.getData(tag), got.getData(tag));
 	}
 }
