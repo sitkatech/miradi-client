a14135d912c14cdab3e5cd8551ab05715772e995
nimaa <nimaa@localhost>
2007-04-03 16:43:06 +0000

    made target, strat, cuase have the same id as factor (4)

--- source/org/conservationmeasures/eam/objecthelpers/ObjectType.java
+++ source/org/conservationmeasures/eam/objecthelpers/ObjectType.java
@@ -26,8 +26,10 @@ public class ObjectType
 	public static final int KEY_ECOLOGICAL_ATTRIBUTE = 17;
 	public static final int DIAGRAM_FACTOR = 18;
 	public static final int DIAGRAM_CONTENTS = 19;
-	public static final int STRATEGY = 20;
-	public static final int CAUSE = 21;
-	public static final int TARGET = 22;
+	
+	//FIXME these should have unique ints after the refactor.
+	public static final int STRATEGY = 4;
+	public static final int CAUSE = 4;
+	public static final int TARGET = 4;
 }
 
