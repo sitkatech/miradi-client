040b995dac95252a97ead15898d62e8c3ce8d056
nimaa <nimaa@localhost>
2007-04-03 17:03:48 +0000

    changed Factor to a more specific new type (Target, Cuase, Strategy)

--- source/org/conservationmeasures/eam/dialogs/StrategyPoolTablePanel.java
+++ source/org/conservationmeasures/eam/dialogs/StrategyPoolTablePanel.java
@@ -12,6 +12,6 @@ public class StrategyPoolTablePanel extends ObjectPoolTablePanel
 {
 	public StrategyPoolTablePanel(Project projectToUse)
 	{
-		super(projectToUse, ObjectType.FACTOR, new StrategyPoolTableModel(projectToUse));
+		super(projectToUse, ObjectType.STRATEGY, new StrategyPoolTableModel(projectToUse));
 	}
 }
