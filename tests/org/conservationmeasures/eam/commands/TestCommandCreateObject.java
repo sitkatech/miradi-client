ab37f438708b2fb04ad972f45a698632a1714280
nimaa <nimaa@localhost>
2007-04-09 14:49:24 +0000

    refactor: removing references to CreateFactorParameter

--- tests/org/conservationmeasures/eam/commands/TestCommandCreateObject.java
+++ tests/org/conservationmeasures/eam/commands/TestCommandCreateObject.java
@@ -5,11 +5,9 @@
  */
 package org.conservationmeasures.eam.commands;
 
-import org.conservationmeasures.eam.diagram.factortypes.FactorTypeCause;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.ids.FactorId;
 import org.conservationmeasures.eam.main.EAMTestCase;
-import org.conservationmeasures.eam.objecthelpers.CreateFactorParameter;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.Factor;
 import org.conservationmeasures.eam.project.Project;
@@ -40,10 +38,8 @@ public class TestCommandCreateObject extends EAMTestCase
 		try
 		{
 			int type = ObjectType.CAUSE;
-			CreateFactorParameter parameter = new CreateFactorParameter(new FactorTypeCause());
-			CommandCreateObject cmd = new CommandCreateObject(type, parameter);
+			CommandCreateObject cmd = new CommandCreateObject(type);
 			assertEquals("already has an id?", BaseId.INVALID, cmd.getCreatedId());
-			assertEquals("didn't memorize extraInfo?", parameter, cmd.getParameter());
 			cmd.execute(project);
 			FactorId createdId = new FactorId(cmd.getCreatedId().asInt());
 			int highestId = project.getNodeIdAssigner().getHighestAssignedId();
