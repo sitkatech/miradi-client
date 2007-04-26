ea2bea123bbedf34cded184165c7e6e9e2ce3a1e
richardn <richardn@localhost>
2007-04-26 16:41:30 +0000

    text added before Report Dialog

--- source/org/conservationmeasures/eam/reports/MiradiReport.java
+++ source/org/conservationmeasures/eam/reports/MiradiReport.java
@@ -1,17 +1,21 @@
 package org.conservationmeasures.eam.reports;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Dimension;
 import java.io.InputStream;
 import java.util.HashMap;
 
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.view.JRViewer;
 
 import org.conservationmeasures.eam.project.Project;
+import org.martus.swing.UiLabel;
 
 public class MiradiReport
 {
@@ -27,15 +31,31 @@ public class MiradiReport
 		getReport(print);
 	}
 
+	
 	private void getReport(JasperPrint jPrint)
 	{
-        Dialog g = new JDialog();
-        g.setUndecorated(false);
-        g.add(new JRViewer(jPrint));
-        g.pack();
-        g.setSize(new Dimension(900,600));
-        g.setVisible(true);
+		Dialog g = new JDialog();
+		g.setUndecorated(false);
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(getBudgetComponent(),BorderLayout.BEFORE_FIRST_LINE);
+		panel.add(new JRViewer(jPrint));
+		g.add(panel);
+		g.pack();
+		g.setSize(new Dimension(900,600));
+		g.setVisible(true);
+	}
+	
+	private Component getBudgetComponent()
+	{
+		UiLabel label= new UiLabel();
+		label.setText("<html>" +
+		"Tools to enter actual expenditures <P></P> and to match these up to budget line items " +
+		"and report on them by programmatic objectives and activities, " +
+		"by accounting codes, or by funding sources, " +
+		"using a Quicken or Quickbooks style interface.</html>");
+		return label;
 	}
 	
 	Project project;
+	
 }
