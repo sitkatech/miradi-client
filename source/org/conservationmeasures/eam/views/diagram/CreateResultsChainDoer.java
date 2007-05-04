46c53e27f5e4600bfcfbb212075f4279f5dcf1a6
nimaa <nimaa@localhost>
2007-05-04 15:54:01 +0000

    set tab through command

--- source/org/conservationmeasures/eam/views/diagram/CreateResultsChainDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/CreateResultsChainDoer.java
@@ -7,12 +7,14 @@ package org.conservationmeasures.eam.views.diagram;
 
 import org.conservationmeasures.eam.commands.CommandBeginTransaction;
 import org.conservationmeasures.eam.commands.CommandEndTransaction;
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.BaseObject;
 import org.conservationmeasures.eam.objects.Factor;
+import org.conservationmeasures.eam.objects.ViewData;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.project.ResultsChainCreatorHelper;
 import org.conservationmeasures.eam.views.ViewDoer;
@@ -63,9 +65,10 @@ public class CreateResultsChainDoer extends ViewDoer
 
 		BaseId newResultsChainId = creatorHelper.createResultsChain();
 		int newTabIndex = diagramView.getTabIndex(new ORef(ObjectType.RESULTS_CHAIN_DIAGRAM, newResultsChainId));
-		//TODO RC can/should this be done by executing a command instead
-		//i dont think this should be done as a command, since undo will delete the results chain
-		diagramView.setTab(newTabIndex);
+		
+		ViewData viewData = project.getViewData(diagramView.cardName());
+		CommandSetObjectData setTabCommand = new CommandSetObjectData(ObjectType.VIEW_DATA, viewData.getId(), ViewData.TAG_CURRENT_TAB, Integer.toString(newTabIndex));
+		project.executeCommand(setTabCommand);
 	}	
 	
 	public static boolean atLeastOneStrategy(BaseObject[] selectedObjects)
