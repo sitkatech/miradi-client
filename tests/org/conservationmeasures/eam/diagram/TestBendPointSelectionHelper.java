cdec4da10228ef885abfc6904be7cf114ef715e5
nimaa <nimaa@localhost>
2007-05-24 16:46:36 +0000

    added test for MouseEvent manual construction

--- tests/org/conservationmeasures/eam/diagram/TestBendPointSelectionHelper.java
+++ tests/org/conservationmeasures/eam/diagram/TestBendPointSelectionHelper.java
@@ -5,6 +5,13 @@
 */ 
 package org.conservationmeasures.eam.diagram;
 
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+
 import org.conservationmeasures.eam.main.EAMTestCase;
 
 public class TestBendPointSelectionHelper extends EAMTestCase
@@ -16,5 +23,29 @@ public class TestBendPointSelectionHelper extends EAMTestCase
 
 	public void testMousePressed()
 	{
+		
+	}
+	
+	public void testMouseEvent()
+	{
+		MouseEvent mouseEvent = getMouseEvent(new Point(1, 1), InputEvent.SHIFT_DOWN_MASK);
+		assertEquals("shift not down?", true, mouseEvent.isShiftDown());
+		assertEquals("wrong click location?", new Point(1, 1), mouseEvent.getPoint());
+		assertEquals("control down?", false, mouseEvent.isControlDown());
+	}
+	
+	private MouseEvent getMouseEvent(Point point, int modifiersToUse)
+	{
+		Component source = new JButton();
+		int id = 0;
+		long when = 0;
+		int modifiers = modifiersToUse;
+        int x = point.x;
+        int y = point.y;
+        int clickCount = 1;
+        boolean popupTrigger = false;
+        int button = 0;
+
+		return new MouseEvent(source, id, when, modifiers, x, y, clickCount, popupTrigger, button);
 	}
 }
