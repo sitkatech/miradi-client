336c8b8c77e75473f6e1c11596898f599858b8aa
kevins <kevins@localhost>
2007-06-05 18:12:54 +0000

    Pull actual news from miradi.org and format it reasonably

--- source/org/conservationmeasures/eam/views/noproject/wizard/LeftSideTextPanelWithNews.java
+++ source/org/conservationmeasures/eam/views/noproject/wizard/LeftSideTextPanelWithNews.java
@@ -5,17 +5,14 @@
 */ 
 package org.conservationmeasures.eam.views.noproject.wizard;
 
-import java.awt.BorderLayout;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import javax.swing.BorderFactory;
-import javax.swing.border.TitledBorder;
+import javax.swing.border.Border;
 
-import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 import org.conservationmeasures.eam.wizard.WizardHtmlViewer;
 import org.martus.swing.HyperlinkHandler;
@@ -27,33 +24,22 @@ public class LeftSideTextPanelWithNews extends LeftSideTextPanel
 	{
 		super(mainWindow, htmlToUse, wizardToUse);
 		
-		newsViewer = new WizardHtmlViewer(mainWindow, wizardToUse);
-		newsViewer.setText(loadRemoteHtml());
-		
-		titleBorder = BorderFactory.createTitledBorder(EAM.text("Latest Miradi News"));
-		titleBorder.setTitleFont(mainWindow.getUserDataPanelFont());
-		newsViewer.setBorder(titleBorder);
-
-		add(newsViewer, BorderLayout.AFTER_LAST_LINE);
-	}
-	
-	public void refresh() throws Exception
-	{
-		super.refresh();
-		titleBorder.setTitleFont(newsViewer.getMainWindow().getUserDataPanelFont());
-		newsViewer.setText(newsViewer.getText());
+		viewer.setText(htmlToUse + 
+				"<br></br><table border='1'><tr><td>" + 
+				loadRemoteHtml() +
+				"</td></tr></table>");
 	}
 	
 	public String loadRemoteHtml()
 	{
 		try
 		{
-			URL url = new URL("https://miradi.org/robots.txt");
+			URL url = new URL("https://miradi.org/rest/latestnews");
 			return readContext((InputStream)url.getContent());
 		}
 		catch(Exception e)
 		{
-			return "news unavaiable";
+			return "Miradi news is not available";
 		}
 	}
 	
@@ -70,7 +56,7 @@ public class LeftSideTextPanelWithNews extends LeftSideTextPanel
 		return returnLine;
 	}
 	
-	TitledBorder titleBorder;
+	Border border;
 	WizardHtmlViewer newsViewer;
 }
 	
