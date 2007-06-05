621f2c55b2695ec3e2a2974dfb01f5b8df1adb6a
kevins <kevins@localhost>
2007-06-05 18:23:13 +0000

    Put news retrieval in a background thread to avoid stalling if offline

--- source/org/conservationmeasures/eam/views/noproject/wizard/LeftSideTextPanelWithNews.java
+++ source/org/conservationmeasures/eam/views/noproject/wizard/LeftSideTextPanelWithNews.java
@@ -11,10 +11,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import javax.swing.border.Border;
+import javax.swing.SwingUtilities;
 
 import org.conservationmeasures.eam.main.MainWindow;
-import org.conservationmeasures.eam.wizard.WizardHtmlViewer;
 import org.martus.swing.HyperlinkHandler;
 
 public class LeftSideTextPanelWithNews extends LeftSideTextPanel
@@ -23,41 +22,66 @@ public class LeftSideTextPanelWithNews extends LeftSideTextPanel
 	public LeftSideTextPanelWithNews(MainWindow mainWindow, String htmlToUse, HyperlinkHandler wizardToUse)
 	{
 		super(mainWindow, htmlToUse, wizardToUse);
+		originalHtml = htmlToUse;
+		new NewsRetriever().start();
+	}
+	
+	class NewsRetriever extends Thread
+	{
+
+		public void run()
+		{
+			newsHtml = loadRemoteHtml();
+			SwingUtilities.invokeLater(new NewsUpdater());
+		}
 		
-		viewer.setText(htmlToUse + 
+		private String loadRemoteHtml()
+		{
+			try
+			{
+				URL url = new URL("https://miradi.org/rest/latestnews");
+				return readContext((InputStream)url.getContent());
+			}
+			catch(Exception e)
+			{
+				return "Miradi news is not available";
+			}
+		}
+		
+		private String readContext(InputStream inputStream) throws IOException
+		{
+			InputStreamReader isr = new InputStreamReader(inputStream);
+			String returnLine = "";
+			String thisLine;
+			BufferedReader br = new BufferedReader(isr);
+			while((thisLine = br.readLine()) != null)
+			{
+				returnLine = returnLine + thisLine;
+			}
+			return returnLine;
+		}
+	}
+	
+	private class NewsUpdater implements Runnable
+	{
+		public void run()
+		{
+			updateText();
+		}
+		
+	}
+	
+	
+	private void updateText()
+	{
+		viewer.setText(originalHtml + 
 				"<br></br><table border='1'><tr><td>" + 
-				loadRemoteHtml() +
+				newsHtml +
 				"</td></tr></table>");
 	}
 	
-	public String loadRemoteHtml()
-	{
-		try
-		{
-			URL url = new URL("https://miradi.org/rest/latestnews");
-			return readContext((InputStream)url.getContent());
-		}
-		catch(Exception e)
-		{
-			return "Miradi news is not available";
-		}
-	}
-	
-	public String readContext(InputStream inputStream) throws IOException
-	{
-		InputStreamReader isr = new InputStreamReader(inputStream);
-		String returnLine = "";
-		String thisLine;
-		BufferedReader br = new BufferedReader(isr);
-		while((thisLine = br.readLine()) != null)
-		{
-			returnLine = returnLine + thisLine;
-		}
-		return returnLine;
-	}
-	
-	Border border;
-	WizardHtmlViewer newsViewer;
+	private String originalHtml;
+	private String newsHtml;
 }
 	
 	
