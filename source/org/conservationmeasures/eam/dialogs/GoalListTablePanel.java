a5a55849b5ff3f794a43eb16365116752bb545c3
richardn <richardn@localhost>
2007-06-07 23:56:45 +0000

    added clone button to goal panel...note non operatonal at present....

--- source/org/conservationmeasures/eam/dialogs/GoalListTablePanel.java
+++ source/org/conservationmeasures/eam/dialogs/GoalListTablePanel.java
@@ -5,11 +5,13 @@
 */ 
 package org.conservationmeasures.eam.dialogs;
 
+import org.conservationmeasures.eam.actions.ActionCloneGoal;
 import org.conservationmeasures.eam.actions.ActionCreateGoal;
 import org.conservationmeasures.eam.actions.ActionDeleteGoal;
 import org.conservationmeasures.eam.actions.Actions;
 import org.conservationmeasures.eam.actions.MainWindowAction;
 import org.conservationmeasures.eam.actions.ObjectsAction;
+import org.conservationmeasures.eam.dialogs.fieldComponents.PanelButton;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.project.Project;
@@ -22,6 +24,9 @@ public class GoalListTablePanel extends ObjectListTablePanel
 				new GoalListTableModel(projectToUse, nodeRef), 
 				(MainWindowAction)actions.get(ActionCreateGoal.class), 
 				(ObjectsAction)actions.get(ActionDeleteGoal.class));
+		//TODO: this class should be refactored to take a action list...EAMAction can have a isObjectAction and isMainWindowAction so it can be
+		// handeled in a loop at the parent level
+		addButton(new PanelButton(actions.get(ActionCloneGoal.class)));
 	}
 
 }
