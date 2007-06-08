960ab8f9befc5cddd3db719d04b03d896d546886
richardn <richardn@localhost>
2007-06-08 18:52:34 +0000

    fix missing EAM.text

--- source/org/conservationmeasures/eam/dialogs/AnnotationSelectionDlg.java
+++ source/org/conservationmeasures/eam/dialogs/AnnotationSelectionDlg.java
@@ -33,7 +33,7 @@ public class AnnotationSelectionDlg extends EAMDialog implements ListSelectionLi
 		list = poolTable;
 		list.getTable().addListSelectionListener(this);
 		Box box = Box.createVerticalBox();
-		box.add(new PanelTitleLabel("Please select which item should be cloned into this factor, then press the Clone but"));
+		box.add(new PanelTitleLabel(EAM.text("Please select which item should be cloned into this factor, then press the Clone but")));
 		box.add(list, BorderLayout.AFTER_LAST_LINE);
 		
 		setTitle(title);
