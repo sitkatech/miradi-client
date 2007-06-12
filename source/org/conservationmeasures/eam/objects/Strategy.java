050689c803632003f48d7b599da4867087b72458
richardn <richardn@localhost>
2007-06-12 17:23:20 +0000

    added note

--- source/org/conservationmeasures/eam/objects/Strategy.java
+++ source/org/conservationmeasures/eam/objects/Strategy.java
@@ -75,6 +75,7 @@ public class Strategy extends Factor
 		{
 			DiagramFactor diagramFactor = (DiagramFactor) getProject().findObject(diagramFactorList.get(i));
 			DiagramObject diagramObject = (DiagramObject)diagramFactor.getOwner();
+			//Note: Detection of ophaned DFs....do not remove
 			if (diagramObject==null)
 			{
 				EAM.logError("getResultsChains:Owner not found for diagram factor:" +diagramFactor.getRef());
