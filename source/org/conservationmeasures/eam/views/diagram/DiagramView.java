be14702ec4080bd3726abea3af503b690ee850fe
richardn <richardn@localhost>
2007-06-23 13:51:07 +0000

    removed un needed method call and added fixme for spliter postion

--- source/org/conservationmeasures/eam/views/diagram/DiagramView.java
+++ source/org/conservationmeasures/eam/views/diagram/DiagramView.java
@@ -144,7 +144,6 @@ public class DiagramView extends TabbedViewWithSidePanel implements CommandExecu
 	public void becomeInactive() throws Exception
 	{
 		slideShowPoolManagementPanel.dispose();
-		remove(slideShowPoolManagementPanel);
 		slideShowPoolManagementPanel = null;
 		super.becomeInactive();
 	}
@@ -152,7 +151,9 @@ public class DiagramView extends TabbedViewWithSidePanel implements CommandExecu
 	private SlideListManagementPanel createSlideShowPanel() throws Exception
 	{
 		ORef oref = createSlideShowIfNeeded().getRef(); 
+		//FIXME: need a different spliter then mainWindow.....
 		slideShowPoolManagementPanel =  new SlideListManagementPanel(getProject(), getMainWindow(), oref, getActions());
+		slideShowPoolManagementPanel.updateSplitterLocationToMiddle();
 		setSidePanel(slideShowPoolManagementPanel);
 		hideSidePanel();
 		return slideShowPoolManagementPanel;
