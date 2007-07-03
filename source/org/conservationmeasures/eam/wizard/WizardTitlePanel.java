15b2bd3516ceb364d37bf5c375b04149079a4ffa
kevins <kevins@localhost>
2007-07-03 20:39:04 +0000

    Add Miradi icon to wizard title panel

--- source/org/conservationmeasures/eam/wizard/WizardTitlePanel.java
+++ source/org/conservationmeasures/eam/wizard/WizardTitlePanel.java
@@ -6,13 +6,17 @@
 
 package org.conservationmeasures.eam.wizard;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import org.conservationmeasures.eam.main.AppPreferences;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
+import org.conservationmeasures.eam.utils.MiradiResourceImageIcon;
+import org.martus.swing.UiLabel;
 import org.martus.util.xml.XmlUtilities;
 
 import com.jhlabs.awt.BasicGridLayout;
@@ -21,7 +25,7 @@ public class WizardTitlePanel extends JPanel
 {
 	public WizardTitlePanel(MainWindow mainWindow)
 	{
-		super(new BasicGridLayout(2, 1));
+		super(new BorderLayout());
 		
 		stepTitle = new WizardTitleHtmlViewer(mainWindow);
 		stepTitle.setForeground(AppPreferences.WIZARD_TITLE_FOREGROUND);
@@ -29,8 +33,17 @@ public class WizardTitlePanel extends JPanel
 		screenTitle = new WizardTitleHtmlViewer(mainWindow);
 		screenTitle.setForeground(Color.BLACK);
 		screenTitle.setBackground(AppPreferences.WIZARD_TITLE_BACKGROUND);
-		add(stepTitle);
-		add(screenTitle);
+		
+		JPanel titlePanel = new JPanel(new BasicGridLayout(2,1));
+		titlePanel.add(stepTitle);
+		titlePanel.add(screenTitle);
+		titlePanel.setBackground(AppPreferences.WIZARD_TITLE_BACKGROUND);
+		
+		ImageIcon icon = new MiradiResourceImageIcon("images/miradi64.png");
+		UiLabel iconHolder = new UiLabel();
+		iconHolder.setIcon(icon);
+		add(iconHolder, BorderLayout.BEFORE_LINE_BEGINS);
+		add(titlePanel, BorderLayout.CENTER);
 		
 		setBackground(AppPreferences.WIZARD_TITLE_BACKGROUND);
 	}
