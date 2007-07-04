31f5293f2e308c4fbd0a505c373bf6e509050f2d
nimaa <nimaa@localhost>
2007-07-04 03:53:16 +0000

    added some functionality (but not working) to delete conceptual model doer

--- source/org/conservationmeasures/eam/views/diagram/DeleteConceptualModelDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/DeleteConceptualModelDoer.java
@@ -5,19 +5,56 @@
 */ 
 package org.conservationmeasures.eam.views.diagram;
 
+import org.conservationmeasures.eam.commands.CommandBeginTransaction;
+import org.conservationmeasures.eam.commands.CommandEndTransaction;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
-import org.conservationmeasures.eam.views.ViewDoer;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.views.ObjectsDoer;
 
-public class DeleteConceptualModelDoer extends ViewDoer
+public class DeleteConceptualModelDoer extends ObjectsDoer
 {
 	public boolean isAvailable()
 	{
-		return false;
+		if(!getProject().isOpen())
+			return false;
+		
+		if (!isDiagramView())
+			return false;
+
+		if (getDiagramView().isResultsChainTab())
+			return false;
+
+		if (getSelectedIds().length == 1)
+			return false;
+		
+		return true;
 	}
 	
 	public void doIt() throws CommandFailedException
 	{
 		if (! isAvailable())
 			return;
+		
+		Project project = getProject();
+		project.executeCommand(new CommandBeginTransaction());
+		try
+		{
+			deleteConceptualModel(project);
+		}
+		catch (Exception e)
+		{
+			EAM.errorDialog("Could not delete conceptual model");
+			EAM.logException(e);
+		}
+		finally
+		{
+			project.executeCommand(new CommandEndTransaction());
+		}
+	}
+
+	private void deleteConceptualModel(Project project)
+	{
+		//FIXME finish deleting conceptula model
 	}
 }
