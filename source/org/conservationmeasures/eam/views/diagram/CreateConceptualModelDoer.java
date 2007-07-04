f7369b83e79b0ce9157859a23b1a87590fca538e
nimaa <nimaa@localhost>
2007-07-04 03:43:56 +0000

    creating a conceptual model now works

--- source/org/conservationmeasures/eam/views/diagram/CreateConceptualModelDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/CreateConceptualModelDoer.java
@@ -5,19 +5,68 @@
 */ 
 package org.conservationmeasures.eam.views.diagram;
 
+import org.conservationmeasures.eam.commands.CommandBeginTransaction;
+import org.conservationmeasures.eam.commands.CommandCreateObject;
+import org.conservationmeasures.eam.commands.CommandEndTransaction;
+import org.conservationmeasures.eam.dialogs.DiagramPanel;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.DiagramObject;
+import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.ViewDoer;
 
 public class CreateConceptualModelDoer extends ViewDoer
 {
 	public boolean isAvailable()
 	{
-		return false;
+		if(!getProject().isOpen())
+			return false;
+		
+		if (!isDiagramView())
+			return false;
+
+		if (getDiagramView().isResultsChainTab())
+			return false;
+		
+		return true;
 	}
 	
 	public void doIt() throws CommandFailedException
 	{
 		if (! isAvailable())
 			return;
+		
+		Project project = getProject();
+		project.executeCommand(new CommandBeginTransaction());
+		try
+		{
+			createConceptualModelDiagram(project);
+		}
+		catch (Exception e)
+		{
+			EAM.errorDialog("Could not create Conceptual Model");
+			EAM.logException(e);
+		}
+		finally
+		{
+			project.executeCommand(new CommandEndTransaction());
+		}
+	}
+
+	private void createConceptualModelDiagram(Project project) throws Exception
+	{
+		CommandCreateObject createConcetualModel = new CommandCreateObject(ObjectType.CONCEPTUAL_MODEL_DIAGRAM);
+		project.executeCommand(createConcetualModel);
+
+		ORef createdDiagramObjectRef = new ORef(ObjectType.CONCEPTUAL_MODEL_DIAGRAM, createConcetualModel.getCreatedId());
+		DiagramObject diagramObject = (DiagramObject) project.findObject(createdDiagramObjectRef);
+		
+		DiagramSplitPane.createDiagram(getMainWindow(), diagramObject);
+		DiagramPanel diagramPanel = getDiagramView().getDiagramPanel();
+		DiagramSplitPane diagramSplitPane = diagramPanel.getDiagramSplitPane();
+		DiagramPageList diagramPageList = diagramSplitPane.getDiagramPageList();
+		diagramPageList.fillListWithSelectedDiagramObject(diagramObject);		
 	}
 }
