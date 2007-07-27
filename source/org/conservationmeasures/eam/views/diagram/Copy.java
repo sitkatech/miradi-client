5223341423c9efa814588ffb2102d7568c2abe7b
nimaa <nimaa@localhost>
2007-07-27 21:08:13 +0000

    removed EAM flavor code.  also remove unused constructors

--- source/org/conservationmeasures/eam/views/diagram/Copy.java
+++ source/org/conservationmeasures/eam/views/diagram/Copy.java
@@ -7,23 +7,11 @@ package org.conservationmeasures.eam.views.diagram;
 
 import org.conservationmeasures.eam.diagram.cells.EAMGraphCell;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
-import org.conservationmeasures.eam.main.TransferableEamList;
 import org.conservationmeasures.eam.main.TransferableMiradiList;
-import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.ViewDoer;
 
 public class Copy extends ViewDoer
 {
-	public Copy()
-	{
-		super();
-	}
-	
-	public Copy(Project project)
-	{
-		setProject(project);
-	}
-	
 	public boolean isAvailable()
 	{
 		if (! getProject().isOpen())
@@ -41,7 +29,6 @@ public class Copy extends ViewDoer
 		try
 		{
 			copySelectedItemsToMiradiClipboard();
-			copySelectedItemsToEAMClipboard();
 			getProject().getDiagramClipboard().incrementPasteCount();
 		}
 		catch (Exception e)
@@ -52,31 +39,11 @@ public class Copy extends ViewDoer
 
 	private void copySelectedItemsToMiradiClipboard() throws Exception
 	{
-		//FIXME temp swith beween transitions of two flavors
-		if (TransferableEamList.IS_EAM_FLAVOR)
-			return;
-		
 		EAMGraphCell[] selectedCells = getDiagramView().getDiagramPanel().getSelectedAndRelatedCells();
 		TransferableMiradiList miradiList = new TransferableMiradiList(getProject());
 		miradiList.storeData(selectedCells);
 		
 		DiagramClipboard clipboard = getProject().getDiagramClipboard();
 		clipboard.setContents(miradiList, getMainWindow());
-	}
-	
-	//TODO this method is duplicate and will go away after new miradi flavor takes over
-	public void copySelectedItemsToEAMClipboard()
-	{
-		//FIXME temp swith beween transitions of two flavors
-		if (! TransferableEamList.IS_EAM_FLAVOR)
-			return;
-		
-		EAMGraphCell[] selectedCells = getDiagramView().getDiagramPanel().getSelectedAndRelatedCells();
-		if(selectedCells.length == 0)
-			return;
-		TransferableEamList eamList = new TransferableEamList(getProject().getFilename(), selectedCells);
-		
-		DiagramClipboard clipboard = getProject().getDiagramClipboard();
-		clipboard.setContents(eamList, getMainWindow());
 	}
 }
