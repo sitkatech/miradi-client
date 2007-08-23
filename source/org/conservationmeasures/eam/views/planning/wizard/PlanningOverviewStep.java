74564876d9e81e42050c11269ed9ec9f7022afcd
nimaa <nimaa@localhost>
2007-08-23 18:09:58 +0000

    added overview step for planning view wizard

---
+++ source/org/conservationmeasures/eam/views/planning/wizard/PlanningOverviewStep.java
@@ -0,0 +1,30 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+
+package org.conservationmeasures.eam.views.planning.wizard;
+
+import org.conservationmeasures.eam.actions.views.ActionViewPlanning;
+import org.conservationmeasures.eam.views.planning.PlanningView;
+import org.conservationmeasures.eam.wizard.SplitWizardStep;
+import org.conservationmeasures.eam.wizard.WizardPanel;
+
+public class PlanningOverviewStep extends SplitWizardStep
+{
+	public PlanningOverviewStep(WizardPanel wizardToUse)
+	{
+		super(wizardToUse, PlanningView.getViewName());
+	}
+
+	public String getProcessStepTitle()
+	{
+		return "";
+	}
+
+	public Class getAssociatedActionClass()
+	{
+		return ActionViewPlanning.class;
+	}
+}
