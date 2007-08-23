9aaf95c8399b6475d16f8fba671a131f8a5533f2
nimaa <nimaa@localhost>
2007-08-23 17:12:03 +0000

    added wizard overview step

--- source/org/conservationmeasures/eam/project/Project.java
+++ source/org/conservationmeasures/eam/project/Project.java
@@ -980,7 +980,7 @@ public class Project
 	public static final String DIAGRAM_VIEW_NAME = "Diagram";
 	public static final String SUMMARY_VIEW_NAME = "Summary";
 	public static final String TARGET_VIABILITY_NAME = "Target Viability";
-	public static final String PLANNING_VIEW_NAME = "Planning View";
+	public static final String PLANNING_VIEW_NAME = "Planning";
 	
 	public static final String DEFAULT_VIEW_NAME = SUMMARY_VIEW_NAME;
 	
