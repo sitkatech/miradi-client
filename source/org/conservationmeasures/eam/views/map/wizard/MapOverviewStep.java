3b2c0fb81b52ee609b617a39a6dc560961a0ce1b
nimaa <nimaa@localhost>
2007-08-23 17:57:08 +0000

    fixing goofup that removed MapOverViewStep.html and MapOverviewStep.java

---
+++ source/org/conservationmeasures/eam/views/map/wizard/MapOverviewStep.java
@@ -0,0 +1,30 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+
+package org.conservationmeasures.eam.views.map.wizard;
+
+import org.conservationmeasures.eam.actions.views.ActionViewMap;
+import org.conservationmeasures.eam.views.map.MapView;
+import org.conservationmeasures.eam.wizard.SplitWizardStep;
+import org.conservationmeasures.eam.wizard.WizardPanel;
+
+public class MapOverviewStep extends SplitWizardStep
+{
+	public MapOverviewStep(WizardPanel wizardToUse)
+	{
+		super(wizardToUse, MapView.getViewName());
+	}
+
+	public String getProcessStepTitle()
+	{
+		return "";
+	}
+
+	public Class getAssociatedActionClass()
+	{
+		return ActionViewMap.class;
+	}
+}
