0c33a944178f9474dfe5b01551f9f637355e7029
nimaa <nimaa@localhost>
2007-08-24 14:48:34 +0000

    moved method out of StratPlanObjective and into ObjectManager

--- source/org/conservationmeasures/eam/views/strategicplan/StratPlanObjective.java
+++ source/org/conservationmeasures/eam/views/strategicplan/StratPlanObjective.java
@@ -5,19 +5,12 @@
 */ 
 package org.conservationmeasures.eam.views.strategicplan;
 
-import java.util.Arrays;
-import java.util.Vector;
-
-import org.conservationmeasures.eam.ids.ObjectiveId;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objects.BaseObject;
-import org.conservationmeasures.eam.objects.Factor;
 import org.conservationmeasures.eam.objects.Objective;
-import org.conservationmeasures.eam.objects.Strategy;
+import org.conservationmeasures.eam.project.ObjectManager;
 import org.conservationmeasures.eam.project.Project;
-import org.conservationmeasures.eam.project.ProjectChainObject;
-import org.conservationmeasures.eam.utils.IgnoreCaseStringComparator;
 import org.conservationmeasures.eam.views.TreeTableNode;
 
 public class StratPlanObjective extends TreeTableNode
@@ -71,39 +64,8 @@ public class StratPlanObjective extends TreeTableNode
 
 	public void rebuild()
 	{
-		strategies = getStrategyNodes(objective);
-	}
-
-	static public StratPlanStrategy[] getStrategyNodes(Objective objective)
-	{
-		Project project = objective.getProject();
-		ObjectiveId objectiveId = (ObjectiveId)objective.getId();
-
-		Factor[] strategyObjects = project.getStrategyPool().getNonDraftStrategies();
-		Vector strategyVector = new Vector();
-		for(int i = 0; i < strategyObjects.length; ++i)
-		{
-			Strategy strategy = (Strategy)strategyObjects[i];
-			if(doesChainContainObjective(strategy, objectiveId))
-				strategyVector.add(new StratPlanStrategy(project, strategy));
-		}
-		StratPlanStrategy[] strategies = (StratPlanStrategy[])strategyVector.toArray(new StratPlanStrategy[0]);
-		Arrays.sort(strategies, new IgnoreCaseStringComparator());
-		return strategies;
-	}
-
-	static private boolean doesChainContainObjective(Factor chainMember, ObjectiveId objectiveId)
-	{
-		ProjectChainObject chainObject = new ProjectChainObject();
-		chainObject.buildUpstreamDownstreamChain(chainMember);
-		Factor[] chainNodes = chainObject.getFactorsArray();
-		for(int i = 0; i < chainNodes.length; ++i)
-		{
-			if(chainNodes[i].getObjectives().contains(objectiveId))
-				return true;
-		}
-		
-		return false;
+		ObjectManager objectManager = project.getObjectManager();
+		strategies = objectManager.getStrategyNodes(objective);
 	}
 
 	Project project;
