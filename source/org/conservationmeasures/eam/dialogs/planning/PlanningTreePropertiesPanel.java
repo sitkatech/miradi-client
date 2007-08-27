ff746974608266ad45d3e37a3c17a75514e87f62
nimaa <nimaa@localhost>
2007-08-27 18:28:14 +0000

    setting up properies panel to handle multiple properties panels

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningTreePropertiesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningTreePropertiesPanel.java
@@ -5,6 +5,9 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
+import java.awt.CardLayout;
+import java.util.Vector;
+
 import org.conservationmeasures.eam.dialogs.ObjectDataInputPanel;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.project.Project;
@@ -14,12 +17,33 @@ public class PlanningTreePropertiesPanel extends ObjectDataInputPanel
 	public PlanningTreePropertiesPanel(Project projectToUse, ORef orefToUse)
 	{
 		super(projectToUse, orefToUse);
+		project = projectToUse;
+		setLayout(new CardLayout());
+		createPropertiesPanels();
 	}
 
+	private void createPropertiesPanels()
+	{
+		//FIXME create the properies panels here
+	}
+	
 	public String getPanelDescription()
 	{
 		return PANEL_DESCRIPTION;
 	}
 	
+	protected Vector getFields()
+	{
+		return super.getFields();
+	}
+
+	public void setObjectRefs(ORef[] orefsToUse)
+	{
+		super.setObjectRefs(orefsToUse);
+		
+	}
+	
 	public static final String PANEL_DESCRIPTION = "Planning Properties Panel";
+	
+	Project project;
 }
