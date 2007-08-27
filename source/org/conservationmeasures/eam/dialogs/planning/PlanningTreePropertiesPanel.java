b30734bdf23cde07bd94ae4faac5fb56971d377a
nimaa <nimaa@localhost>
2007-08-27 18:57:40 +0000

    added indicator properties panel (does not update fields yet)

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningTreePropertiesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningTreePropertiesPanel.java
@@ -8,23 +8,28 @@ package org.conservationmeasures.eam.dialogs.planning;
 import java.awt.CardLayout;
 import java.util.Vector;
 
+import org.conservationmeasures.eam.dialogs.IndicatorPropertiesPanel;
 import org.conservationmeasures.eam.dialogs.ObjectDataInputPanel;
 import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.Indicator;
 import org.conservationmeasures.eam.project.Project;
 
 public class PlanningTreePropertiesPanel extends ObjectDataInputPanel
 {
-	public PlanningTreePropertiesPanel(Project projectToUse, ORef orefToUse)
+	public PlanningTreePropertiesPanel(Project projectToUse, ORef orefToUse) throws Exception
 	{
 		super(projectToUse, orefToUse);
 		project = projectToUse;
-		setLayout(new CardLayout());
+		cardLayout = new CardLayout();
+		setLayout(cardLayout);
 		createPropertiesPanels();
 	}
 
-	private void createPropertiesPanels()
+	private void createPropertiesPanels() throws Exception
 	{
-		//FIXME create the properies panels here
+		indicatorPropertiesPanel = new IndicatorPropertiesPanel(project);
+		
+		add(indicatorPropertiesPanel, indicatorPropertiesPanel.getPanelDescription());
 	}
 	
 	public String getPanelDescription()
@@ -40,10 +45,22 @@ public class PlanningTreePropertiesPanel extends ObjectDataInputPanel
 	public void setObjectRefs(ORef[] orefsToUse)
 	{
 		super.setObjectRefs(orefsToUse);
-		
+		cardLayout.show(this, getDescription(orefsToUse));
 	}
 	
+	private String getDescription(ORef[] orefsToUse)
+	{
+		//FIXME should not use first ref
+		ORef firstRef = orefsToUse[0];
+		if (Indicator.getObjectType() == firstRef.getObjectType())
+			return indicatorPropertiesPanel.getPanelDescription();
+		
+		return "";
+	}
+
 	public static final String PANEL_DESCRIPTION = "Planning Properties Panel";
 	
 	Project project;
+	IndicatorPropertiesPanel indicatorPropertiesPanel;
+	CardLayout cardLayout;
 }
