5f06090c803b62dc006c77f784166462aa26603d
nimaa <nimaa@localhost>
2007-08-27 20:28:12 +0000

    moved to dialogs.planning package

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningViewLegendPanel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewLegendPanel.java
@@ -3,7 +3,7 @@
 * Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
 * Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
 */ 
-package org.conservationmeasures.eam.views.planning;
+package org.conservationmeasures.eam.dialogs.planning;
 
 import javax.swing.JLabel;
 
