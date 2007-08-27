3f78eb15a20df0a5a483f6de013ea169f93b4f53
nimaa <nimaa@localhost>
2007-08-27 21:11:34 +0000

    pulled up generic methods into LegendPanel,  preparing PlanningViewLegendPanel for useful functionality

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningViewLegendPanel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewLegendPanel.java
@@ -5,17 +5,46 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
-import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JCheckBox;
+
+import org.conservationmeasures.eam.objects.Objective;
+import org.conservationmeasures.eam.objects.ViewData;
+import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.utils.CodeList;
 import org.conservationmeasures.eam.views.umbrella.LegendPanel;
 
 import com.jhlabs.awt.BasicGridLayout;
 
-public class PlanningViewLegendPanel extends LegendPanel
+public class PlanningViewLegendPanel extends LegendPanel implements ActionListener
 {
-	public PlanningViewLegendPanel()
+	public PlanningViewLegendPanel(Project projectToUse)
 	{
-		super(new BasicGridLayout(0, 1));
-		add(new JLabel("legend construction area"));
+		super(projectToUse, new BasicGridLayout(0, 1));
+		project = projectToUse;
+		objectiveCheckBox = new JCheckBox("objective");
+		objectiveCheckBox.addActionListener(this);
+		add(objectiveCheckBox);
 	}
+
+	public void actionPerformed(ActionEvent event)
+	{	
+		JCheckBox checkBox = (JCheckBox)event.getSource();
+		String property = (String) checkBox.getClientProperty(LAYER);
+		updateProjectLegendSettings(property, ViewData.TAG_PLANNING_HIDDEN_TYPES);
+	}
+	
+	public CodeList getLegendSettings()
+	{
+		//FIXME planning - add all the types here
+		CodeList hiddenTypes = new CodeList();
+		hiddenTypes.add(Integer.toString(Objective.getObjectType()));
+		
+		return hiddenTypes;
+	}
+	
+	Project project;
+	JCheckBox objectiveCheckBox;
 }
