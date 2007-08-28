b42dc56d28c84ea22708834c7bc4a5223ab76ce5
nimaa <nimaa@localhost>
2007-08-28 17:39:27 +0000

    restore tree state after filtering the nodes

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeTablePanel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeTablePanel.java
@@ -44,6 +44,7 @@ public class PlanningTreeTablePanel extends TreeTablePanel
 			return;
 		
 		getPlanningModel().rebuildEntireTree();
+		restoreTreeExpansionState();
 	}
 	
 	PlanningTreeModel getPlanningModel()
