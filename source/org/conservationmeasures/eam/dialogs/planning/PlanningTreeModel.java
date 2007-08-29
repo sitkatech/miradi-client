5e10bbdd88b6ebabc24a5a2673a548d85afb49c9
nimaa <nimaa@localhost>
2007-08-29 18:54:18 +0000

    added fixme to change when field

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeModel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeModel.java
@@ -76,6 +76,7 @@ public class PlanningTreeModel extends GenericTreeTableModel
 		Indicator.TAG_PRIORITY, 
 		Indicator.PSEUDO_TAG_STATUS_VALUE, 
 		Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML,
+		//FIXME planning the "when" pseudo field isn't right....should be based on task assignment date ranges, not inidicator measurement date
 		Indicator.TAG_MEASUREMENT_DATE,
 		Task.PSEUDO_TAG_TASK_TOTAL, };	
 }
