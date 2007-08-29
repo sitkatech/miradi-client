c8fb22a8a11147834132f00b6c293412d8632b4d
nimaa <nimaa@localhost>
2007-08-29 20:37:49 +0000

    added renderer structure to planning tree table

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeTable.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeTable.java
@@ -5,6 +5,13 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.JTree;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
+
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.TreeTableWithStateSaving;
 
@@ -14,5 +21,31 @@ public class PlanningTreeTable extends TreeTableWithStateSaving
 	{
 		super(projectToUse, planningTreeModelToUse);
 		setShowGrid(true);
+		setTableColumnRenderer();
+	}
+	
+	private void setTableColumnRenderer()
+	{
+		final int STARTING_TABLE_COLUMN = 1;
+		int columnCount = getColumnModel().getColumnCount();
+		for (int i  = STARTING_TABLE_COLUMN; i < columnCount; ++i)
+		{	
+			TableColumn tableColumn = getColumnModel().getColumn(i);
+			tableColumn.setCellRenderer(new CustomRenderer(tree));
+		}
+	}
+	
+	protected static class CustomRenderer extends DefaultTableCellRenderer
+	{
+		public CustomRenderer(JTree treeToUse)
+		{
+		}
+		
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+		{
+			Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			//FIXME planning - add cell coloring here
+			return component;
+		}
 	}
 }
