3f5ff1b717b198584646b827a086a958951673bd
nimaa <nimaa@localhost>
2007-08-29 18:50:41 +0000

    refactor rename

--- source/org/conservationmeasures/eam/views/umbrella/LegendPanel.java
+++ source/org/conservationmeasures/eam/views/umbrella/LegendPanel.java
@@ -102,7 +102,7 @@ abstract public class LegendPanel extends JPanel implements ActionListener
 		}
 	}
 	
-	protected void selectAllCheckBoxs()
+	protected void selectAllCheckBoxes()
 	{
 		Object[] keys = checkBoxes.keySet().toArray();
 		for (int i = 0; i < keys.length; ++i)
