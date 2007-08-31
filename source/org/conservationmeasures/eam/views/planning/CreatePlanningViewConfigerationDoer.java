c233325388d3772c0b4e75b5d6d3ff711518f3d3
nimaa <nimaa@localhost>
2007-08-31 18:18:45 +0000

    empty doers to creat/delete planning view configeration

---
+++ source/org/conservationmeasures/eam/views/planning/CreatePlanningViewConfigerationDoer.java
@@ -0,0 +1,23 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.views.planning;
+
+import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.views.MainWindowDoer;
+
+public class CreatePlanningViewConfigerationDoer extends MainWindowDoer
+{
+	public boolean isAvailable()
+	{
+		return false;
+	}
+
+	public void doIt() throws CommandFailedException
+	{
+		if (! isAvailable())
+			return;
+	}
+}
