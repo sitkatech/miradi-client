f81148c6319c60c634940bfc09dd160f2be45f88
nimaa <nimaa@localhost>
2007-08-31 19:20:45 +0000

    added new control panel to hold all the legend panels

---
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewControlPanel.java
@@ -0,0 +1,61 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.main.MainWindow;
+import org.conservationmeasures.eam.project.Project;
+
+import com.jhlabs.awt.BasicGridLayout;
+
+public class PlanningViewControlPanel extends JPanel
+{
+	public PlanningViewControlPanel(MainWindow mainWindowToUse)
+	{
+		mainWindow = mainWindowToUse;
+		add(createScrollableLegendPanel());
+	}
+	
+	private JScrollPane createScrollableLegendPanel()
+	{
+		planningCustomizationPanel = new PlanningCustomizationPanel(getProject());
+		rowsLegendPanel = new PlanningViewRowsLegendPanel(getMainWindow());
+		columnsLegendPanel = new PlanningViewColumsLegendPanel(getMainWindow());
+		JPanel legendPanel = new JPanel(new BasicGridLayout(2, 1));
+		
+		legendPanel.add(rowsLegendPanel.createTitleBar(EAM.text("Control Bar")));
+		legendPanel.add(planningCustomizationPanel);
+		legendPanel.add(rowsLegendPanel);
+		legendPanel.add(columnsLegendPanel);
+			
+		return new JScrollPane(legendPanel);
+	}
+	
+	public void dispose()
+	{
+		getProject().removeCommandExecutedListener(rowsLegendPanel);
+		getProject().removeCommandExecutedListener(columnsLegendPanel);
+		getProject().removeCommandExecutedListener(planningCustomizationPanel);
+	}
+	
+	private MainWindow getMainWindow()
+	{
+		return mainWindow;
+	}
+	
+	private Project getProject()
+	{
+		return getMainWindow().getProject();
+	}
+
+	private MainWindow mainWindow;
+	private PlanningViewRowsLegendPanel rowsLegendPanel;
+	private PlanningCustomizationPanel planningCustomizationPanel;
+	private PlanningViewColumsLegendPanel columnsLegendPanel;
+}
