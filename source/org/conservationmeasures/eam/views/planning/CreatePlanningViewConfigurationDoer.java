c4c60a5eef44c5e893c0260ae4c2d72d571b3de0
nimaa <nimaa@localhost>
2007-09-03 19:42:24 +0000

    added functionality to create configuration doer

--- source/org/conservationmeasures/eam/views/planning/CreatePlanningViewConfigurationDoer.java
+++ source/org/conservationmeasures/eam/views/planning/CreatePlanningViewConfigurationDoer.java
@@ -5,19 +5,60 @@
 */ 
 package org.conservationmeasures.eam.views.planning;
 
+import org.conservationmeasures.eam.commands.CommandBeginTransaction;
+import org.conservationmeasures.eam.commands.CommandCreateObject;
+import org.conservationmeasures.eam.commands.CommandEndTransaction;
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
-import org.conservationmeasures.eam.views.MainWindowDoer;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.PlanningViewConfiguration;
+import org.conservationmeasures.eam.objects.ViewData;
+import org.conservationmeasures.eam.views.ViewDoer;
 
-public class CreatePlanningViewConfigurationDoer extends MainWindowDoer
+public class CreatePlanningViewConfigurationDoer extends ViewDoer
 {
 	public boolean isAvailable()
 	{
-		return false;
+		if (! isPlanningView())
+			return false;
+		
+		return true;
 	}
 
 	public void doIt() throws CommandFailedException
 	{
 		if (! isAvailable())
 			return;
+		
+		getProject().executeCommand(new CommandBeginTransaction());
+		try
+		{
+			createPlanningViewConfiguration();
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
+		finally
+		{
+			getProject().executeCommand(new CommandEndTransaction());
+		}
+	}
+
+	private void createPlanningViewConfiguration() throws Exception
+	{
+		ViewData viewData = getProject().getCurrentViewData();
+		String hiddenRowsAsString = viewData.getData(ViewData.TAG_PLANNING_HIDDEN_ROW_TYPES);
+		String hiddenColsAsString = viewData.getData(ViewData.TAG_PLANNING_HIDDEN_COL_TYPES);
+		
+		CommandCreateObject createConfiguration = new CommandCreateObject(PlanningViewConfiguration.getObjectType());
+		getProject().executeCommand(createConfiguration);
+		
+		ORef newConfigurationRef = createConfiguration.getObjectRef();
+		CommandSetObjectData setHiddenRowsCommand = new CommandSetObjectData(newConfigurationRef, PlanningViewConfiguration.TAG_ROW_CONFIGURATION, hiddenRowsAsString);
+		getProject().executeCommand(setHiddenRowsCommand);
+		
+		CommandSetObjectData setHiddenColsCommand = new CommandSetObjectData(newConfigurationRef, PlanningViewConfiguration.TAG_COL_CONFIGURATION, hiddenColsAsString);
+		getProject().executeCommand(setHiddenColsCommand);
 	}
 }
