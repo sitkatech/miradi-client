ed6808e588d05b6036cd48ebef1663a65c5a6d4d
nimaa <nimaa@localhost>
2007-09-06 20:09:30 +0000

    new empty table classes.  added tables to panel.

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningViewAssignmentEditorComponent.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewAssignmentEditorComponent.java
@@ -7,7 +7,13 @@ package org.conservationmeasures.eam.dialogs.planning;
 
 import java.awt.BorderLayout;
 
+import javax.swing.Box;
+import javax.swing.JScrollPane;
+
 import org.conservationmeasures.eam.dialogs.DisposablePanel;
+import org.conservationmeasures.eam.utils.MultiTableVerticalScrollController;
+import org.conservationmeasures.eam.utils.MultipleTableSelectionController;
+import org.conservationmeasures.eam.utils.TableWithHelperMethods;
 
 public class PlanningViewAssignmentEditorComponent extends DisposablePanel
 {
@@ -15,6 +21,49 @@ public class PlanningViewAssignmentEditorComponent extends DisposablePanel
 	{
 		super(new BorderLayout());
 		
-		
+		createTables();
+		addTables();
+		addTablesToSelectionController();
 	}
+
+	private void createTables()
+	{
+		selectionController = new MultipleTableSelectionController();
+		verticalController = new MultiTableVerticalScrollController();
+		
+		resourceTable = new PlanningViewResourceTable();
+		workplanTable = new PlanningViewWorkPlanTable();
+		budgetTable = new PlanningViewBudgetTable();
+	}
+	
+	private void addTables()
+	{
+		Box horizontalBox = Box.createHorizontalBox();
+		addVerticalScrollableControlledTable(horizontalBox, resourceTable);
+		addVerticalScrollableControlledTable(horizontalBox, workplanTable);
+		addVerticalScrollableControlledTable(horizontalBox, budgetTable);
+		
+		add(horizontalBox);
+	}
+
+	private void addVerticalScrollableControlledTable(Box horizontalBox, TableWithHelperMethods tableToAdd)
+	{
+		JScrollPane resourceScroller = new JScrollPane(tableToAdd);
+		verticalController.addTable(resourceScroller);
+		horizontalBox.add(resourceScroller);
+	}
+
+	private void addTablesToSelectionController()
+	{
+		selectionController.addTable(resourceTable);
+		selectionController.addTable(workplanTable);
+		selectionController.addTable(budgetTable);
+	}
+
+	private MultiTableVerticalScrollController verticalController;
+	private MultipleTableSelectionController selectionController;
+	
+	private PlanningViewResourceTable resourceTable;
+	private PlanningViewBudgetTable budgetTable;
+	private PlanningViewWorkPlanTable workplanTable;
 }
---
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewResourceTable.java
@@ -0,0 +1,13 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning;
+
+import org.conservationmeasures.eam.utils.TableWithHelperMethods;
+
+public class PlanningViewResourceTable extends TableWithHelperMethods
+{
+
+}
