b5604d759c442b75336743ff586872b22d3cf7eb
nimaa <nimaa@localhost>
2007-09-06 20:14:35 +0000

    empty models for the planning view assigment properties panel tables

---
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewResourceTableModel.java
@@ -0,0 +1,26 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning;
+
+import javax.swing.table.AbstractTableModel;
+
+public class PlanningViewResourceTableModel extends AbstractTableModel
+{
+	public int getColumnCount()
+	{
+		return 0;
+	}
+
+	public int getRowCount()
+	{
+		return 0;
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		return null;
+	}
+}
