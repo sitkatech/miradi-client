8bfd662a74d62c46772944aad85543eabb811ee3
nimaa <nimaa@localhost>
2007-09-07 15:27:47 +0000

    added radio button subclasses

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningViewRadioButton.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewRadioButton.java
@@ -5,13 +5,48 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
-import org.martus.swing.UiRadioButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PlanningViewRadioButton extends UiRadioButton
+import javax.swing.JRadioButton;
+
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.project.Project;
+
+//FIXME planning - this class should extend UiRadioButton but it cant since UiRadioButton has no constructor that takes in 0 args
+abstract public class PlanningViewRadioButton extends JRadioButton implements ActionListener
 {
-	public PlanningViewRadioButton(String text)
-	{
-		super(text);
+	public PlanningViewRadioButton(Project projectToUse)
+	{	
+		super();
+		project = projectToUse;
+		addActionListener(this);
 	}
 
+	public void actionPerformed(ActionEvent e)
+	{
+		saveRadioSelection();
+	}
+
+	private void saveRadioSelection()
+	{
+		try
+		{
+			//FIXME planning - uncomment and finish
+			//ViewData viewData = project.getCurrentViewData();
+			//String savedChoice = viewData.getData(ViewData.TAG_PLANNING_STYLE_CHOICE);
+		}
+		catch (Exception e) 
+		{
+			EAM.logException(e);
+			EAM.errorDialog(EAM.text("Error Occurred While Trying to Save Settings"));
+		}
+	}
+	
+	abstract public String[] getRowList();
+	abstract public String[] getColumnList();
+	abstract public String getPropertyName();
+	
+	//TODO planning - make private
+	Project project;
 }
