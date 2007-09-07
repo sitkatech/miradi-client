d300cb21d190aed5d926eabfbb47455e8f556858
nimaa <nimaa@localhost>
2007-09-07 16:11:40 +0000

    added functionality to save

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningViewRadioButton.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewRadioButton.java
@@ -10,8 +10,12 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JRadioButton;
 
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objects.ViewData;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.utils.CodeList;
+import org.conservationmeasures.eam.views.planning.PlanningView;
 
 //FIXME planning - this class should extend UiRadioButton but it cant since UiRadioButton has no constructor that takes in 0 args
 abstract public class PlanningViewRadioButton extends JRadioButton implements ActionListener
@@ -32,9 +36,9 @@ abstract public class PlanningViewRadioButton extends JRadioButton implements Ac
 	{
 		try
 		{
-			//FIXME planning - uncomment and finish
-			//ViewData viewData = project.getCurrentViewData();
-			//String savedChoice = viewData.getData(ViewData.TAG_PLANNING_STYLE_CHOICE);
+			saveCurrentRadioSelection();
+			saveCurrentColumnList();
+			saveCurrentRowList();
 		}
 		catch (Exception e) 
 		{
@@ -42,11 +46,45 @@ abstract public class PlanningViewRadioButton extends JRadioButton implements Ac
 			EAM.errorDialog(EAM.text("Error Occurred While Trying to Save Settings"));
 		}
 	}
+
+	private void saveCurrentRowList() throws Exception
+	{
+		String rowListAsString = getListToSave(PlanningView.getMasterRowList(), getRowList());
+		save(ViewData.TAG_PLANNING_HIDDEN_ROW_TYPES, rowListAsString);
+	}
+
+	private void saveCurrentColumnList() throws Exception
+	{
+		String columnListAsString = getListToSave(PlanningView.getMasterColumnList(), getColumnList());
+		save(ViewData.TAG_PLANNING_HIDDEN_COL_TYPES, columnListAsString);
+	}
+
+	private void saveCurrentRadioSelection() throws Exception
+	{
+		save(ViewData.TAG_PLANNING_STYLE_CHOICE, getPropertyName());
+	}
+
+	private void save(String tag, String newValue) throws Exception
+	{
+		ViewData viewData = project.getCurrentViewData();
+		String existingValue = viewData.getData(tag);
+		if (existingValue.equals(newValue))
+			return;
+
+		CommandSetObjectData setComboItem = new CommandSetObjectData(viewData.getRef(), tag, newValue);
+		project.executeCommand(setComboItem);
+	}
 	
+	private String getListToSave(CodeList masterList, String[] listAsString) throws Exception
+	{
+		CodeList listToSubtract = new CodeList(listAsString);
+		masterList.subtract(listToSubtract);
+		return masterList.toString();
+	}
+		
 	abstract public String[] getRowList();
 	abstract public String[] getColumnList();
 	abstract public String getPropertyName();
 	
-	//TODO planning - make private
-	Project project;
+	private	Project project;
 }
