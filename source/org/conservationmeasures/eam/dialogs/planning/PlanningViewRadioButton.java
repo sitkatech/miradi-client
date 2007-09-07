fd7e6b98bf59e150b963a2dc897a83e081e9322a
nimaa <nimaa@localhost>
2007-09-07 14:23:17 +0000

    added radio and combo box empty classes

---
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewRadioButton.java
@@ -0,0 +1,17 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning;
+
+import org.martus.swing.UiRadioButton;
+
+public class PlanningViewRadioButton extends UiRadioButton
+{
+	public PlanningViewRadioButton(String text)
+	{
+		super(text);
+	}
+
+}
