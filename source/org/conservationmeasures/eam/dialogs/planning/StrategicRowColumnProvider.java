bdc6217340a27b310230b403d76c8ba3f4441fe1
nimaa <nimaa@localhost>
2007-09-09 13:45:59 +0000

    row column providers

---
+++ source/org/conservationmeasures/eam/dialogs/planning/StrategicRowColumnProvider.java
@@ -0,0 +1,38 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning;
+
+import org.conservationmeasures.eam.objects.Goal;
+import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.objects.Objective;
+import org.conservationmeasures.eam.objects.Strategy;
+import org.conservationmeasures.eam.objects.Task;
+import org.conservationmeasures.eam.utils.CodeList;
+import org.conservationmeasures.eam.views.planning.PlanningView;
+
+public class StrategicRowColumnProvider implements RowColumnProvider
+{
+	public CodeList getColumnList()
+	{
+		return new CodeList(new String[] {Indicator.PSEUDO_TAG_STATUS_VALUE,			
+							 Indicator.TAG_PRIORITY,
+							 Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML,
+							 Task.PSEUDO_TAG_COMBINED_EFFORT_DATES,
+							 Task.PSEUDO_TAG_TASK_TOTAL, });
+	}
+
+	public CodeList getRowList()
+	{
+		return new CodeList(new String[] {Goal.OBJECT_NAME,
+							Objective.OBJECT_NAME,
+							Strategy.OBJECT_NAME,});
+	}
+	
+	public String getPropertyName()
+	{
+		return PlanningView.STRATEGIC_PLAN_RADIO_CHOICE;
+	}
+}
---
+++ source/org/conservationmeasures/eam/dialogs/planning/WorkPlanRowColumnProvider.java
@@ -0,0 +1,36 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning;
+
+import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.objects.Strategy;
+import org.conservationmeasures.eam.objects.Task;
+import org.conservationmeasures.eam.utils.CodeList;
+import org.conservationmeasures.eam.views.planning.PlanningView;
+
+public class WorkPlanRowColumnProvider implements RowColumnProvider
+{
+	public CodeList getColumnList()
+	{
+		return new CodeList(new String[] {Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML,
+							 Task.PSEUDO_TAG_COMBINED_EFFORT_DATES,
+							 Task.PSEUDO_TAG_TASK_TOTAL, });
+	}
+
+	public CodeList getRowList()
+	{
+		return new CodeList(new String[] {Strategy.OBJECT_NAME,
+							Task.ACTIVITY_NAME,
+							Indicator.OBJECT_NAME,
+							Task.METHOD_NAME,
+							Task.OBJECT_NAME, });
+	}
+	
+	public String getPropertyName()
+	{
+		return PlanningView.WORKPLAN_PLAN_RADIO_CHOICE;
+	}
+}
