1f4632e4fc2edfbc0ba0c3f59e85893258b9f892
nimaa <nimaa@localhost>
2007-09-12 17:41:39 +0000

    fixed nested transaction problems

---
+++ source/org/conservationmeasures/eam/questions/ObjectChoiceItem.java
@@ -0,0 +1,29 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.questions;
+
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.BaseObject;
+import org.conservationmeasures.eam.project.Project;
+
+public class ObjectChoiceItem extends ChoiceItem
+{
+	public ObjectChoiceItem(Project projectToUse, ORef wrappedRefToUse)
+	{
+		super(wrappedRefToUse.toString(), null);
+		project = projectToUse;
+		wrappedRef = wrappedRefToUse;
+	}
+	
+	public String getLabel()
+	{
+		BaseObject foundObject = project.findObject(wrappedRef);
+		return foundObject.getLabel();
+	}
+	
+	private Project project;
+	private ORef wrappedRef;
+}
