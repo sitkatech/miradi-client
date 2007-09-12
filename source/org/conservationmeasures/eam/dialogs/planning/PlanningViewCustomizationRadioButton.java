b9488329bd2144a922ab684a3d03a9a0f34fe7e1
nimaa <nimaa@localhost>
2007-09-12 02:03:32 +0000

    removed old unused code

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningViewCustomizationRadioButton.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningViewCustomizationRadioButton.java
@@ -5,11 +5,7 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
-import org.conservationmeasures.eam.objecthelpers.ORef;
-import org.conservationmeasures.eam.objects.PlanningViewConfiguration;
-import org.conservationmeasures.eam.objects.ViewData;
 import org.conservationmeasures.eam.project.Project;
-import org.conservationmeasures.eam.utils.CodeList;
 import org.conservationmeasures.eam.views.planning.PlanningView;
 
 public class PlanningViewCustomizationRadioButton extends PlanningViewRadioButton
@@ -17,26 +13,6 @@ public class PlanningViewCustomizationRadioButton extends PlanningViewRadioButto
 	public PlanningViewCustomizationRadioButton(Project projectToUse, RowColumnProvider rowColumnProvider)
 	{
 		super(projectToUse, rowColumnProvider);
-	}
-
-	public String[] getColumnList() throws Exception
-	{
-		return getList(PlanningViewConfiguration.TAG_COL_CONFIGURATION);
-	}
-
-	public String[] getRowList() throws Exception
-	{
-		return getList(PlanningViewConfiguration.TAG_ROW_CONFIGURATION);
-	}
-	
-	//TODO planning - this gets converted back to codeList maybe should return codeList
-	private String[] getList(String fieldTag) throws Exception
-	{
-		ViewData viewData = getProject().getCurrentViewData();
-		ORef configurationRef = viewData.getORef(ViewData.TAG_PLANNING_CUSTOM_PLAN_REF);
-		PlanningViewConfiguration configuration = (PlanningViewConfiguration) getProject().findObject(configurationRef);
-		
-		return new CodeList(configuration.getData(fieldTag)).toArray();
 	}
 	
 	public String getPropertyName()
