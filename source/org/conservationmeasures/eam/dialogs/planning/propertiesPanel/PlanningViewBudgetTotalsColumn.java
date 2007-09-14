6ff494f9d3ec3731f616501c69fa9bef6ac703ec
nimaa <nimaa@localhost>
2007-09-14 02:32:07 +0000

    totals tables for budget and work plan

---
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetTotalsColumn.java
@@ -0,0 +1,16 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning.propertiesPanel;
+
+import javax.swing.table.TableModel;
+
+public class PlanningViewBudgetTotalsColumn extends PlanningViewAbstractTable
+{
+	public PlanningViewBudgetTotalsColumn(TableModel model)
+	{
+		super(model);
+	}
+}
