0d3bfc8253ec2ee8ab7dd0e0249359e0252f17a0
kevins <kevins@localhost>
2007-09-14 18:47:25 +0000

    Planning View Create Activity and Method now work

---
+++ source/org/conservationmeasures/eam/views/planning/doers/TreeNodeCreateActivityDoer.java
@@ -0,0 +1,21 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.views.planning.doers;
+
+import org.conservationmeasures.eam.objects.BaseObject;
+import org.conservationmeasures.eam.objects.Strategy;
+
+public class TreeNodeCreateActivityDoer extends TreeNodeCreateTaskDoer
+{
+	boolean canOwnTask(BaseObject object)
+	{
+		if(object.getType() == Strategy.getObjectType())
+			return true;
+		
+		return false;
+	}
+
+}
