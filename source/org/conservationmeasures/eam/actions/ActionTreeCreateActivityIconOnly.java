9639aac530bac97c245100b4708e1029498c4a5c
kevins <kevins@localhost>
2007-09-14 18:47:25 +0000

    Planning View Create Activity and Method now work

---
+++ source/org/conservationmeasures/eam/actions/ActionTreeCreateActivityIconOnly.java
@@ -0,0 +1,17 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.actions;
+
+import org.conservationmeasures.eam.main.MainWindow;
+
+public class ActionTreeCreateActivityIconOnly extends ActionTreeCreateActivity
+{
+	public ActionTreeCreateActivityIconOnly(MainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse, null);
+	}
+
+}
