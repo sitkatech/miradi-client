e201d699a614e7fc49172ae818917382d85d74ef
nimaa <nimaa@localhost>
2007-09-16 21:42:30 +0000

    task label field is editable (TODO:tree has to get update)

--- source/org/conservationmeasures/eam/views/workplan/TaskPropertiesInputPanel.java
+++ source/org/conservationmeasures/eam/views/workplan/TaskPropertiesInputPanel.java
@@ -46,7 +46,7 @@ public class TaskPropertiesInputPanel extends ObjectDataInputPanel
 
 	private void addCommonFields()
 	{
-		addField(createReadonlyTextField(ObjectType.TASK, Task.TAG_LABEL));
+		addField(createStringField(ObjectType.TASK, Task.TAG_LABEL));
 		addField(createReadonlyTextField(Task.PSEUDO_TAG_TASK_TOTAL));
 		updateFieldsFromProject();
 	}
