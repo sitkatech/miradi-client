7db3b732ec972a16179a7e0460e0289c6c45ec98
nimaa <nimaa@localhost>
2007-09-17 01:30:10 +0000

    renderer for annuals budget column (table)

--- source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetAnnualTotalsTable.java
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetAnnualTotalsTable.java
@@ -5,8 +5,14 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning.propertiesPanel;
 
-import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
+
+import org.conservationmeasures.eam.main.AppPreferences;
 import org.conservationmeasures.eam.utils.TableWithHelperMethods;
 
 public class PlanningViewBudgetAnnualTotalsTable extends TableWithHelperMethods
@@ -14,6 +20,40 @@ public class PlanningViewBudgetAnnualTotalsTable extends TableWithHelperMethods
 	public PlanningViewBudgetAnnualTotalsTable(PlanningViewBudgetAnnualTotalTableModel model)
 	{
 		super(model);
+		setTableColumnRenderer();
 		setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 	}
+	
+	private void setTableColumnRenderer()
+	{
+		int columnCount = getColumnModel().getColumnCount();
+		for (int i = 0; i < columnCount; ++i)
+		{	
+			TableColumn tableColumn = getColumnModel().getColumn(i);
+			tableColumn.setCellRenderer(new CustomRenderer());
+		}
+	}
+	
+	public static class CustomRenderer extends DefaultTableCellRenderer
+	{
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+		{
+			Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			setBackground(getBackgroundColor(table.getColumnCount(), column));
+			
+			if (isSelected)
+				setBackground(table.getSelectionBackground());
+			
+			return component;
+		}
+
+		private Color getBackgroundColor(int columnCount, int column)
+		{
+			final int TOTALS_COLUMN = columnCount - 1;
+			if (column == TOTALS_COLUMN)
+				return AppPreferences.BUDGET_TOTAL_TABLE_BACKGROUND;
+			
+			return AppPreferences.BUDGET_TABLE_BACKGROUND;
+		}
+	}	
 }
