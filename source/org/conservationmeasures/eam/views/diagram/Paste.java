e55d1bf71fb2ed3bde9cb1b77b663a6453122fbe
nimaa <nimaa@localhost>
2007-09-17 15:15:34 +0000

    reverted earlier refactor rename

--- source/org/conservationmeasures/eam/views/diagram/Paste.java
+++ source/org/conservationmeasures/eam/views/diagram/Paste.java
@@ -82,7 +82,7 @@ public class Paste extends LocationDoer
 		if (! list.atleastOneFactorExists())
 			return AS_COPY_BUTTON;
 
-		if (isPastingInDiagramWithExistingEqualAlias(list))
+		if (isPastingInSameDiagramAsCopiedFrom(list))
 			return AS_COPY_BUTTON;
 		
 		String[] buttons = {AS_COPY_BUTTON, AS_ALIAS_BUTTON, CANCEL_BUTTON};
@@ -94,7 +94,7 @@ public class Paste extends LocationDoer
 		return EAM.choiceDialog(title, body, buttons);
 	}
 
-	private boolean isPastingInDiagramWithExistingEqualAlias(TransferableMiradiList list)
+	private boolean isPastingInSameDiagramAsCopiedFrom(TransferableMiradiList list)
 	{
 		ORef diagramObjecRefCopiedFrom = list.getDiagramObjectRefCopiedFrom();
 		ORef diagramObjectRefBeingPastedInto = getDiagramView().getDiagramPanel().getDiagramObject().getRef();
