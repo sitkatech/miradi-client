ee5334f72f8400f0a8607e485d783755a3a160b0
nimaa <nimaa@localhost>
2007-09-17 20:09:44 +0000

    column width are now as big as column header width

---
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewTableWithSizedColumns.java
@@ -0,0 +1,25 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning.propertiesPanel;
+
+import javax.swing.table.TableModel;
+
+public class PlanningViewTableWithSizedColumns extends PlanningViewAbstractTable
+{
+	public PlanningViewTableWithSizedColumns(TableModel modelToUse)
+	{
+		super(modelToUse);
+		addSizedColumnEditorsAndRenderers(modelToUse);
+	}
+		
+	private void addSizedColumnEditorsAndRenderers(TableModel modelToUse)
+	{
+		for (int i = 0; i < modelToUse.getColumnCount(); ++i)
+		{
+			setColumnWidthToHeaderWidth(i);
+		}
+	}
+}
