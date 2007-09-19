f666e09aeafccfe006f8a63809067de4eb9b1af5
nimaa <nimaa@localhost>
2007-09-19 14:23:17 +0000

    class to hold constant button names

---
+++ source/org/conservationmeasures/eam/utils/ConstantButtonNames.java
@@ -0,0 +1,16 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.utils;
+
+import org.conservationmeasures.eam.main.EAM;
+
+public class ConstantButtonNames
+{
+	//TODO there are others out there that should be using these constants
+	//do a find and replace them with these.
+	public static final String CANCEL = EAM.text("Cancel");
+	public static final String CREATE = EAM.text("Create");
+}
