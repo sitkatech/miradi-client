569f256936ea8cb9a62c19b1ec50b7198c2dd4a9
nimaa <nimaa@localhost>
2007-09-20 20:43:18 +0000

    single click auto select cell editor added to the tas properties workPlan table (orange table)

--- source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewWorkPlanTable.java
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewWorkPlanTable.java
@@ -5,15 +5,31 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning.propertiesPanel;
 
+import javax.swing.table.TableColumn;
+
+import org.conservationmeasures.eam.dialogs.fieldComponents.PanelTextField;
 import org.conservationmeasures.eam.main.AppPreferences;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.utils.SingleClickAutoSelectCellEditor;
 
 public class PlanningViewWorkPlanTable extends PlanningViewAbstractRightAlignedTable
 {
 	public PlanningViewWorkPlanTable(Project projectToUse, PlanningViewAbstractBudgetTableModel modelToUse) throws Exception
 	{
 		super(modelToUse);
-		setBackground(AppPreferences.WORKPLAN_TABLE_BACKGROUND);
+		setBackground(AppPreferences.WORKPLAN_TABLE_BACKGROUND);	
+		setSingleCellEditor();
+	}
+	
+	private void setSingleCellEditor()
+	{
+		int colCount = getColumnCount();
+		for (int i = 0; i < colCount; i++)
+		{
+			int modelColumn = convertColumnIndexToModel(i);
+			TableColumn column = getColumnModel().getColumn(modelColumn);
+			column.setCellEditor(new SingleClickAutoSelectCellEditor(new PanelTextField()));
+		}
 	}
 	
 	protected int getColumnWidth(int column)
