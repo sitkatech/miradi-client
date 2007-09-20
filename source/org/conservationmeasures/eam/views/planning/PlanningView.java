41443c0c016b7f3753357498074afab2942530d1
nimaa <nimaa@localhost>
2007-09-20 21:05:19 +0000

    show planning view first time notification dialog

--- source/org/conservationmeasures/eam/views/planning/PlanningView.java
+++ source/org/conservationmeasures/eam/views/planning/PlanningView.java
@@ -39,6 +39,7 @@ import org.conservationmeasures.eam.dialogs.planning.PlanningTreeTable;
 import org.conservationmeasures.eam.dialogs.planning.PlanningTreeTablePanel;
 import org.conservationmeasures.eam.dialogs.planning.legend.PlanningViewControlPanel;
 import org.conservationmeasures.eam.dialogs.planning.propertiesPanel.PlanningTreePropertiesPanel;
+import org.conservationmeasures.eam.main.AppPreferences;
 import org.conservationmeasures.eam.main.CommandExecutedEvent;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
@@ -82,6 +83,18 @@ public class PlanningView extends TabbedView
 		resourceManagementPanel.updateSplitterLocation();
 		accountingCodePoolManagementPanel.updateSplitterLocation();
 		fundingSourcePoolManagementPanel.updateSplitterLocation();
+		
+		showFirstTimeNotificationDialog();
+	}
+
+	private void showFirstTimeNotificationDialog()
+	{
+		String tag = getMainWindow().getTaggedString(AppPreferences.TAG_PLANNING_VIEW_FIRST_TIME_USE_BOOLEAN);
+		if (! tag.equals(""))
+			return;
+		
+		EAM.notifyDialog(EAM.text("This View is Still Under Construction"));
+		getMainWindow().setTaggedString(AppPreferences.TAG_PLANNING_VIEW_FIRST_TIME_USE_BOOLEAN, "false");
 	}
 	
 	public void createTabs() throws Exception
