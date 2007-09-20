308b9daea59e86c9d153f7ed7b720138e30c1079
nimaa <nimaa@localhost>
2007-09-20 16:04:48 +0000

    new border gets font set (same as pref)

---
+++ source/org/conservationmeasures/eam/dialogs/fieldComponents/PanelTitledBorder.java
@@ -0,0 +1,25 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.fieldComponents;
+
+import javax.swing.border.TitledBorder;
+
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.main.MainWindow;
+
+public class PanelTitledBorder extends TitledBorder
+{
+	public PanelTitledBorder(String title)
+	{
+		super(null, title, LEADING, TOP, getMainWindow().getUserDataPanelFont());
+	}
+	
+	//TODO: Richard: should not use static ref here
+	static private MainWindow getMainWindow()
+	{
+		return EAM.mainWindow;
+	}
+}
