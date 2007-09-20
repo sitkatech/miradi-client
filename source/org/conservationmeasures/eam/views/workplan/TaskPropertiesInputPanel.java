315f6505a1ca3c348af2c935373d7c49885f0665
nimaa <nimaa@localhost>
2007-09-20 18:25:19 +0000

    task calculation change.  and added a note if task has subtasks and assignments.

--- source/org/conservationmeasures/eam/views/workplan/TaskPropertiesInputPanel.java
+++ source/org/conservationmeasures/eam/views/workplan/TaskPropertiesInputPanel.java
@@ -10,13 +10,16 @@ import javax.swing.BorderFactory;
 
 import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.dialogs.ObjectDataInputPanel;
+import org.conservationmeasures.eam.dialogs.fieldComponents.PanelTitleLabel;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.main.CommandExecutedEvent;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.BaseObject;
 import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.project.Project;
+import org.martus.swing.UiLabel;
 
 public class TaskPropertiesInputPanel extends ObjectDataInputPanel
 {
@@ -36,6 +39,7 @@ public class TaskPropertiesInputPanel extends ObjectDataInputPanel
 		project = projectToUse;
 		setBorder(BorderFactory.createEtchedBorder());
 		
+		hasBothSubTaskAssignmentsWarningLabel = new PanelTitleLabel(EAM.text("NOTE: The budget total for this task is the sum of the budget totals of its subtasks. The resource assignments below are not included in this value."));
 		addCommonFields();
 	}
 	
@@ -48,12 +52,16 @@ public class TaskPropertiesInputPanel extends ObjectDataInputPanel
 	{
 		addField(createStringField(ObjectType.TASK, Task.TAG_LABEL));
 		addField(createReadonlyTextField(Task.PSEUDO_TAG_TASK_BUDGET_TOTAL));
+		addLabel(new UiLabel(""));
+		addLabel(hasBothSubTaskAssignmentsWarningLabel);
 		updateFieldsFromProject();
 	}
 
 	public void setObjectRefs(ORef[] orefsToUse)
 	{
 		super.setObjectRefs(orefsToUse);
+		updatedWarningMessageVisiblity(orefsToUse);
+		
 		if (editorComponent == null)
 			return;
 		if (orefsToUse.length==0)
@@ -82,8 +90,30 @@ public class TaskPropertiesInputPanel extends ObjectDataInputPanel
 		if (event.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
 			updateTable();
 	}
+	
+	private void updatedWarningMessageVisiblity(ORef[] orefsToUse)
+	{
+		hasBothSubTaskAssignmentsWarningLabel.setVisible(isVisible(orefsToUse));
+	}
+			
+	private boolean isVisible(ORef[] orefsToUse)
+	{
+		if (orefsToUse.length == 0)
+			return false;
 		
+		ORef firstRef = orefsToUse[0];
+		BaseObject foundObject = getProject().findObject(firstRef);
+		if (foundObject.getType() != Task.getObjectType())
+			return false;
+		
+		Task task = (Task) foundObject;
+		if (task.getSubtaskCount() == 0 || task.getAssignmentRefs().size() == 0)
+			return false;
+		
+		return true;
+	}
+	
 	Project project;
 	AssignmentEditorComponent editorComponent;
-	
+	PanelTitleLabel hasBothSubTaskAssignmentsWarningLabel;
 }
