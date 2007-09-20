2d0d2e8053e74a1f0821574e7be60283f292528b
kevins <kevins@localhost>
2007-09-20 14:50:55 +0000

    Make WorkPlan table color less vivid

--- source/org/conservationmeasures/eam/main/AppPreferences.java
+++ source/org/conservationmeasures/eam/main/AppPreferences.java
@@ -342,7 +342,7 @@ public class AppPreferences
 	public static final Color CONTROL_PANEL_BACKGROUND = new Color(0xe8, 0xee, 0xf7);
 	
 	public static final Color RESOURCE_TABLE_BACKGROUND = new Color(0x99, 0xcc, 0xff);
-	public static final Color WORKPLAN_TABLE_BACKGROUND = new Color(0xff, 0x99, 0x00);
+	public static final Color WORKPLAN_TABLE_BACKGROUND = new Color(0xff, 0xCC, 0x44);
 	public static final Color BUDGET_TABLE_BACKGROUND = new Color(0xcc, 0xff, 0xcc);
 	public static final Color BUDGET_TOTAL_TABLE_BACKGROUND = new Color(0x00, 0xff, 0x00);
 	
