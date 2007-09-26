73a268170632c6cd0385962442968807d2180ffe
nimaa <nimaa@localhost>
2007-09-26 21:10:09 +0000

    new TwoLevelQuestion

---
+++ source/org/conservationmeasures/eam/questions/TwoLevelQuestion.java
@@ -0,0 +1,14 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.questions;
+
+public class TwoLevelQuestion extends ChoiceQuestion
+{
+	public TwoLevelQuestion(String tagToUse, String labelToUse,	ChoiceItem[] choicesToUse)
+	{
+		super(tagToUse, labelToUse, choicesToUse);
+	}
+}
