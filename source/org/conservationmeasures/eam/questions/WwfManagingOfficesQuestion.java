76611c67751d3fe8deb7aa2be9ef0490d0e5ffec
nimaa <nimaa@localhost>
2007-09-27 16:33:17 +0000

    using new ManagingOfficeFileLoader

--- source/org/conservationmeasures/eam/questions/WwfManagingOfficesQuestion.java
+++ source/org/conservationmeasures/eam/questions/WwfManagingOfficesQuestion.java
@@ -5,7 +5,7 @@
 */ 
 package org.conservationmeasures.eam.questions;
 
-import org.conservationmeasures.eam.objecthelpers.TaxonomyFileLoader;
+import org.conservationmeasures.eam.objecthelpers.ManagingOfficeFileLoader;
 import org.conservationmeasures.eam.objecthelpers.TwoLevelFileLoader;
 
 
@@ -13,6 +13,6 @@ public class WwfManagingOfficesQuestion extends TwoLevelQuestion
 {
 	public WwfManagingOfficesQuestion(String tag)
 	{
-		super(tag, "WwfManagingOffices", new TaxonomyFileLoader(TwoLevelFileLoader.WWF_MANAGING_OFFICES_FILE));
+		super(tag, "WwfManagingOffices", new ManagingOfficeFileLoader(TwoLevelFileLoader.WWF_MANAGING_OFFICES_FILE));
 	}
 }
