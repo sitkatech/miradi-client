ed6dda546b7d225f96e6b457c17d10ff9a53cf64
nimaa <nimaa@localhost>
2007-09-27 15:43:45 +0000

    adding quetion as checkboxes

--- source/org/conservationmeasures/eam/views/summary/WWFSummaryPanel.java
+++ source/org/conservationmeasures/eam/views/summary/WWFSummaryPanel.java
@@ -25,7 +25,8 @@ public class WWFSummaryPanel extends ObjectDataInputPanel
 		addField(createStringField(ProjectMetadata.getObjectType(), ProjectMetadata.TAG_PROJECT_NAME));
 		addField(createStringField(WwfProjectData.getObjectType(), WwfProjectData.TAG_RELATED_PROJECTS));
 		addField(createStringField(WwfProjectData.getObjectType(), WwfProjectData.TAG_PROJECT_NUMBER, 20));
-		addField(createClassificationChoiceField(new WwfManagingOfficesQuestion(WwfProjectData.TAG_MANAGING_OFFICES)));
+		addField(createMultiCodeField(new WwfManagingOfficesQuestion(WwfProjectData.TAG_MANAGING_OFFICES)));
+		
 		
 		setObjectRefs(new ORef[] {metaDataToUse.getRef(), getWwfProjectDataRef()});
 	}
