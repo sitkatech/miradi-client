4a4b882226d29fa1704a1e21bb30c88c5ab7f25c
kevins <kevins@localhost>
2007-09-28 16:31:16 +0000

    Set reasonable column widths instead of being superwide

--- source/org/conservationmeasures/eam/dialogs/viability/ViabilityTreeModel.java
+++ source/org/conservationmeasures/eam/dialogs/viability/ViabilityTreeModel.java
@@ -6,7 +6,8 @@
 package org.conservationmeasures.eam.dialogs.viability;
 
 import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.objecthelpers.ObjectType;
+import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.objects.KeyEcologicalAttribute;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.views.GenericTreeTableModel;
 
@@ -23,12 +24,22 @@ public class ViabilityTreeModel extends GenericTreeTableModel
 		return columnTags.length;
 	}
 
+	public String getColumnTag(int column)
+	{
+		return columnTags[column];
+	}
+
 	public String getColumnName(int column)
 	{
-		return EAM.fieldLabel(ObjectType.KEY_ECOLOGICAL_ATTRIBUTE, columnTags[column]);
+		String tag = getColumnTag(column);
+		return EAM.fieldLabel(getObjectTypeForColumnLabel(tag), tag);
 	}
 	
-	public static String[] columnTags = {"Item", "Status", "Kea Type"};
+	private int getObjectTypeForColumnLabel(String tag)
+	{
+		return KeyEcologicalAttribute.getObjectType();
+	}
+	
+	public static String[] columnTags = {"Item", Indicator.TAG_STATUS, KeyEcologicalAttribute.TAG_KEY_ECOLOGICAL_ATTRIBUTE_TYPE};
 	Project project;
-
 }
