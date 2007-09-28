5fef4b857f7ade942dba7fc8f8d00e8e835b41f3
nimaa <nimaa@localhost>
2007-09-28 19:34:43 +0000

    including both RCs and CMs

--- source/org/conservationmeasures/eam/views/diagram/EntireDiagrams.java
+++ source/org/conservationmeasures/eam/views/diagram/EntireDiagrams.java
@@ -8,29 +8,38 @@ package org.conservationmeasures.eam.views.diagram;
 import java.util.Vector;
 
 import org.conservationmeasures.eam.objecthelpers.ORefList;
-import org.conservationmeasures.eam.objects.ConceptualModelDiagram;
 import org.conservationmeasures.eam.objects.DiagramFactor;
+import org.conservationmeasures.eam.objects.DiagramObject;
 import org.conservationmeasures.eam.project.Project;
 
 public class EntireDiagrams
 {
 	public static DiagramFactor[] getAllDiagramFactorsInAllDiagrams(Project project)
 	{
+		ORefList allDiagramRefs = getAllDiagramRefs(project);
 		Vector diagramFactors = new Vector();
-		ORefList conceptualModels = project.getConceptualModelDiagramPool().getORefList();
-		for (int i = 0; i < conceptualModels.size(); ++i)
+		for (int i = 0; i < allDiagramRefs.size(); ++i)
 		{
-			ConceptualModelDiagram conceptualModel = (ConceptualModelDiagram) project.findObject(conceptualModels.get(i));
-			diagramFactors.addAll(getAllDiagramFactors(project, conceptualModel));
+			DiagramObject diagramObject = (DiagramObject) project.findObject(allDiagramRefs.get(i));
+			diagramFactors.addAll(getAllDiagramFactors(project, diagramObject));
 		}
 		
 		return (DiagramFactor[]) diagramFactors.toArray(new DiagramFactor[0]);
 	}
+
+	private static ORefList getAllDiagramRefs(Project project)
+	{
+		ORefList allDiagramRefs = new ORefList();
+		allDiagramRefs.addAll(project.getConceptualModelDiagramPool().getORefList());
+		allDiagramRefs.addAll(project.getResultsChainDiagramPool().getORefList());
+
+		return allDiagramRefs;
+	}
 	
-	public static Vector getAllDiagramFactors(Project project, ConceptualModelDiagram conceptualModel)
+	public static Vector getAllDiagramFactors(Project project, DiagramObject diagramObject)
 	{
 		Vector diagramFactors = new Vector();
-		ORefList diagramFactorRefs = conceptualModel.getAllDiagramFactorRefs();
+		ORefList diagramFactorRefs = diagramObject.getAllDiagramFactorRefs();
 		for (int i = 0; i < diagramFactorRefs.size(); ++i)
 		{
 			DiagramFactor diagramFactor = (DiagramFactor) project.findObject(diagramFactorRefs.get(i));
