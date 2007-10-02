ef238c6384e6fd7020a7db82d61b1065321a495d
nimaa <nimaa@localhost>
2007-10-02 18:47:47 +0000

    added subHeadings

--- source/org/conservationmeasures/eam/views/diagram/wizard/DiagramWizardResultsChainStep.java
+++ source/org/conservationmeasures/eam/views/diagram/wizard/DiagramWizardResultsChainStep.java
@@ -6,6 +6,7 @@
 package org.conservationmeasures.eam.views.diagram.wizard;
 
 import org.conservationmeasures.eam.actions.jump.ActionJumpDiagramWizardResultsChainStep;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.ProcessSteps;
 import org.conservationmeasures.eam.wizard.DiagramWizardStep;
 import org.conservationmeasures.eam.wizard.WizardPanel;
@@ -25,5 +26,10 @@ public class DiagramWizardResultsChainStep extends DiagramWizardStep
 	public Class getAssociatedActionClass()
 	{
 		return ActionJumpDiagramWizardResultsChainStep.class;
+	}
+	
+	public String getSubHeading()
+	{
+		return EAM.text("Page 1");
 	}
 }
