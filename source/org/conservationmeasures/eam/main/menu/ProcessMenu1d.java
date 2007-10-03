6ff1fc5adc675b293ee509bd0c16838389c0ce06
kevins <kevins@localhost>
2007-10-03 17:35:03 +0000

    Tweak menu item mnemonics

--- source/org/conservationmeasures/eam/main/menu/ProcessMenu1d.java
+++ source/org/conservationmeasures/eam/main/menu/ProcessMenu1d.java
@@ -24,11 +24,11 @@ public class ProcessMenu1d extends MiradiMenu
 		setMnemonic(KeyEvent.VK_M);
 		
 		addMenuItem(ActionJumpDiagramWizardIdentifyIndirectThreatStep.class, KeyEvent.VK_I);
-		addMenuItem(ActionJumpAssessStakeholders.class, KeyEvent.VK_A);
-		addMenuItem(ActionJumpAnalyzeProjectCapacity.class, KeyEvent.VK_A);
+		addMenuItem(ActionJumpAssessStakeholders.class, KeyEvent.VK_S);
+		addMenuItem(ActionJumpAnalyzeProjectCapacity.class, KeyEvent.VK_C);
 		
 		addMenuItem(ActionJumpArticulateCoreAssumptions.class, KeyEvent.VK_A);
-		addMenuItem(ActionJumpDiagramWizardReviewModelAndAdjustStep.class, KeyEvent.VK_C);
+		addMenuItem(ActionJumpDiagramWizardReviewModelAndAdjustStep.class, KeyEvent.VK_R);
 		addMenuItem(ActionJumpGroundTruthRevise.class, KeyEvent.VK_G);
 	}
 
