4dd79e1d36b86e61f8de753b6a2553a5794d1668
kevins <kevins@localhost>
2007-10-03 19:29:39 +0000

    Update for OpenStandards 2.0

--- source/org/conservationmeasures/eam/main/menu/ProcessSteps.java
+++ source/org/conservationmeasures/eam/main/menu/ProcessSteps.java
@@ -9,7 +9,7 @@ package org.conservationmeasures.eam.main.menu;
 public class ProcessSteps
 {
 	public static final String PROCESS_STEP_1A = "1A. Define Initial Project Team";
-	public static final String PROCESS_STEP_1B = "1B. Define Clear and Common Purpose";
+	public static final String PROCESS_STEP_1B = "1B. Define Scope, Vision, and Targets";
 	public static final String PROCESS_STEP_1C = "1C. Identify Critical Threats";
 	public static final String PROCESS_STEP_1D = "1D. Analyze Project Situation";
 	public static final String PROCESS_STEP_2A = "2A. Develop Goals and Objectives";
