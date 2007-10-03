e9d1c6874afd9ef8e8489d2dc23aae2c56111686
kevins <kevins@localhost>
2007-10-03 17:13:47 +0000

    Start to create classes for each process menu

---
+++ source/org/conservationmeasures/eam/main/menu/MiradiMenu.java
@@ -0,0 +1,21 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.main.menu;
+
+import javax.swing.JMenu;
+
+import org.conservationmeasures.eam.actions.Actions;
+
+public abstract class MiradiMenu extends JMenu
+{
+	public MiradiMenu(String text, Actions actionsToUse)
+	{
+		super(text);
+		actions = actionsToUse;
+	}
+	
+	Actions actions;
+}
