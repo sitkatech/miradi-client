2da8bc0614c2ad4af966c69507415c38ff4c2fcc
nimaa <nimaa@localhost>
2007-10-04 21:05:03 +0000

    refactor move file to new planning package

--- source/org/conservationmeasures/eam/wizard/planning/StrategicPlanOverviewStep.java
+++ source/org/conservationmeasures/eam/wizard/planning/StrategicPlanOverviewStep.java
@@ -3,7 +3,7 @@
 * Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
 * Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
 */ 
-package org.conservationmeasures.eam.views.strategicplan.wizard;
+package org.conservationmeasures.eam.wizard.planning;
 
 import org.conservationmeasures.eam.actions.views.ActionViewStrategicPlan;
 import org.conservationmeasures.eam.wizard.StrategicPlanWizardStep;
