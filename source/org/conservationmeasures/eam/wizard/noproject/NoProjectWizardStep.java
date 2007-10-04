22fc57c80d85bc4e189be12c619aa5320e37ba0d
nimaa <nimaa@localhost>
2007-10-04 21:11:08 +0000

    noproject wizard package change

--- source/org/conservationmeasures/eam/wizard/noproject/NoProjectWizardStep.java
+++ source/org/conservationmeasures/eam/wizard/noproject/NoProjectWizardStep.java
@@ -3,7 +3,7 @@
 * Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
 * Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
 */ 
-package org.conservationmeasures.eam.views.noproject.wizard;
+package org.conservationmeasures.eam.wizard.noproject;
 
 import java.awt.BorderLayout;
 import java.awt.Cursor;
