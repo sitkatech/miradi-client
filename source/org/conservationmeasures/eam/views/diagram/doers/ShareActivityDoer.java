dc4489ebbc1b8db9f74f8100f65773f04c54a398
nimaa <nimaa@localhost>
2007-10-09 16:15:47 +0000

    new Action/Doer for share activity

---
+++ source/org/conservationmeasures/eam/views/diagram/doers/ShareActivityDoer.java
@@ -0,0 +1,20 @@
+package org.conservationmeasures.eam.views.diagram.doers;
+
+import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.views.ViewDoer;
+
+public class ShareActivityDoer extends ViewDoer
+{	
+	public boolean isAvailable()
+	{
+		return true;
+	}
+
+	public void doIt() throws CommandFailedException
+	{
+		if (! isAvailable())
+			return;
+		
+		
+	}
+}
