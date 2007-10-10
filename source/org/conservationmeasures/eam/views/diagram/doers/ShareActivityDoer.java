0049d699f7ac3afc8bab40c868d87edca47cffdc
nimaa <nimaa@localhost>
2007-10-10 02:37:02 +0000

    saving the selected task id to strategy's activityIdList

--- source/org/conservationmeasures/eam/views/diagram/doers/ShareActivityDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/ShareActivityDoer.java
@@ -1,11 +1,14 @@
 package org.conservationmeasures.eam.views.diagram.doers;
 
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.diagram.cells.EAMGraphCell;
 import org.conservationmeasures.eam.dialogs.ActivityPoolTablePanel;
 import org.conservationmeasures.eam.dialogs.diagram.ActivityShareSelectionDiaglog;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.DiagramFactor;
+import org.conservationmeasures.eam.objects.Strategy;
+import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.views.diagram.LocationDoer;
 
 public class ShareActivityDoer extends LocationDoer
@@ -29,6 +32,22 @@ public class ShareActivityDoer extends LocationDoer
 		
 		DiagramFactor diagramFactor = selected.getDiagramFactor();
 		ActivityShareSelectionDiaglog listDialog = new ActivityShareSelectionDiaglog(getMainWindow(), EAM.text("Share Activity"), new ActivityPoolTablePanel(getProject(), diagramFactor.getWrappedORef()));
-		listDialog.setVisible(true);	
+		listDialog.setVisible(true);
+		
+		appendSelectedActivity(diagramFactor, (Task) listDialog.getSelectedObject());
+	}
+
+	private void appendSelectedActivity(DiagramFactor diagramFactor, Task taskToShare) throws CommandFailedException
+	{
+		try
+		{
+			Strategy strategy = (Strategy) getProject().findObject(diagramFactor.getWrappedORef());
+			CommandSetObjectData appendActivityCommand = CommandSetObjectData.createAppendIdCommand(strategy, Strategy.TAG_ACTIVITY_IDS, taskToShare.getId());
+			getProject().executeCommand(appendActivityCommand);
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
 	}
 }
