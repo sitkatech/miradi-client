6c5722f381f8f51d0e06cb942bd4a2395042dffc
nimaa <nimaa@localhost>
2007-10-10 15:36:13 +0000

    refactor pull up method (will be used by isAvailable as soon as isAvailable is pulled up).  changed method visibility.

--- source/org/conservationmeasures/eam/views/planning/doers/AbstractTreeNodeCreateTaskDoer.java
+++ source/org/conservationmeasures/eam/views/planning/doers/AbstractTreeNodeCreateTaskDoer.java
@@ -1,7 +1,16 @@
 package org.conservationmeasures.eam.views.planning.doers;
 
+import org.conservationmeasures.eam.objects.BaseObject;
+import org.conservationmeasures.eam.objects.Task;
+
 
 abstract public class AbstractTreeNodeCreateTaskDoer extends AbstractTreeNodeDoer
 {
-
+	protected boolean canOwnTask(BaseObject object)
+	{
+		if(object.getType() == Task.getObjectType())
+			return true;
+		
+		return false;
+	}
 }
