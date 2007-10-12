0b1c08df0b12138b187fd7db003beac3a1868278
nimaa <nimaa@localhost>
2007-10-12 18:46:06 +0000

    using generic method to save shared object

--- source/org/conservationmeasures/eam/views/diagram/doers/ShareActivityDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/ShareActivityDoer.java
@@ -1,12 +1,10 @@
 package org.conservationmeasures.eam.views.diagram.doers;
 
-import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.diagram.cells.EAMGraphCell;
 import org.conservationmeasures.eam.dialogs.ShareableActivityPoolTablePanel;
 import org.conservationmeasures.eam.dialogs.diagram.ShareSelectionDialog;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.objects.BaseObject;
 import org.conservationmeasures.eam.objects.DiagramFactor;
 import org.conservationmeasures.eam.objects.Strategy;
 import org.conservationmeasures.eam.views.planning.doers.AbstractShareDoer;
@@ -30,27 +28,10 @@ public class ShareActivityDoer extends AbstractShareDoer
 		if (! selected.isFactor())
 			return;
 		
-		DiagramFactor diagramFactor = selected.getDiagramFactor();
-		ShareSelectionDialog listDialog = new ShareSelectionDialog(getMainWindow(), EAM.text("Share Activity"), new ShareableActivityPoolTablePanel(getProject(), diagramFactor.getWrappedORef()));
+		DiagramFactor selectedDiagramFactor = selected.getDiagramFactor();
+		ShareSelectionDialog listDialog = new ShareSelectionDialog(getMainWindow(), EAM.text("Share Activity"), new ShareableActivityPoolTablePanel(getProject(), selectedDiagramFactor.getWrappedORef()));
 		listDialog.setVisible(true);
 		
-		appendSelectedActivity(diagramFactor, listDialog.getSelectedObject());
-	}
-
-	private void appendSelectedActivity(DiagramFactor diagramFactor, BaseObject objectToShare) throws CommandFailedException
-	{
-		if (objectToShare == null)
-			return;
-		
-		try
-		{
-			Strategy strategy = (Strategy) getProject().findObject(diagramFactor.getWrappedORef());
-			CommandSetObjectData appendActivityCommand = CommandSetObjectData.createAppendIdCommand(strategy, Strategy.TAG_ACTIVITY_IDS, objectToShare.getId());
-			getProject().executeCommand(appendActivityCommand);
-		}
-		catch (Exception e)
-		{
-			throw new CommandFailedException(e);
-		}
+		appendSelectedObjectAsShared(selectedDiagramFactor.getWrappedORef(), Strategy.TAG_ACTIVITY_IDS, listDialog.getSelectedObject());
 	}
 }
