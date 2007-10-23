e35af20ef28ea24e4a0f44c1761980060959feb6
nimaa <nimaa@localhost>
2007-10-23 21:33:54 +0000

    added mouse listener to ColumnWidthSaver

--- source/org/conservationmeasures/eam/views/TreeTableWithColumnWidthSaving.java
+++ source/org/conservationmeasures/eam/views/TreeTableWithColumnWidthSaving.java
@@ -1,12 +1,13 @@
 package org.conservationmeasures.eam.views;
 
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.utils.ColumnWidthSaver;
 
 public class TreeTableWithColumnWidthSaving extends TreeTableWithStateSaving
 {
 	public TreeTableWithColumnWidthSaving(Project projectToUse, GenericTreeTableModel treeTableModel)
 	{
 		super(projectToUse, treeTableModel);
-		//FIXME add column saving here
+		getTableHeader().addMouseListener(new ColumnWidthSaver(this));
 	}
 }
