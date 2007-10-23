dba040e135ed58ce4f7bb0dbae44a5e11e2f9362
nimaa <nimaa@localhost>
2007-10-23 21:33:54 +0000

    added mouse listener to ColumnWidthSaver

--- source/org/conservationmeasures/eam/utils/ColumnWidthSaver.java
+++ source/org/conservationmeasures/eam/utils/ColumnWidthSaver.java
@@ -1,6 +1,29 @@
 package org.conservationmeasures.eam.utils;
 
-public class ColumnWidthSaver
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTable;
+
+public class ColumnWidthSaver extends MouseAdapter
 {
+	public ColumnWidthSaver(JTable tableToUse)
+	{
+		table = tableToUse;
+	}
+
+	public void mouseReleased(MouseEvent e)
+	{
+		saveColumnWidths();
+	}
 	
+	private void saveColumnWidths()
+	{
+		for (int i = 0; i < table.getColumnCount(); ++i)
+		{
+			//FIXME save each width
+		}
+	}
+
+	private JTable table;
 }
