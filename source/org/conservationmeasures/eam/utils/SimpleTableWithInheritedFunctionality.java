a5eae64f0c141b47dc5b164bc63cd9e4cbb44a7d
nimaa <nimaa@localhost>
2007-10-24 19:35:47 +0000

    table with column width saver is now abstract,  so it can no longer be instantiated,  had to create a simple leaf class that can be instantiated

---
+++ source/org/conservationmeasures/eam/utils/SimpleTableWithInheritedFunctionality.java
@@ -0,0 +1,11 @@
+package org.conservationmeasures.eam.utils;
+
+import javax.swing.table.TableModel;
+
+public class SimpleTableWithInheritedFunctionality extends TableWithColumnWidthSaver
+{
+	public SimpleTableWithInheritedFunctionality(TableModel model)
+	{	
+		super(model);
+	}
+}
--- source/org/conservationmeasures/eam/utils/TableWithColumnWidthSaver.java
+++ source/org/conservationmeasures/eam/utils/TableWithColumnWidthSaver.java
@@ -2,7 +2,7 @@ package org.conservationmeasures.eam.utils;
 
 import javax.swing.table.TableModel;
 
-public class TableWithColumnWidthSaver extends TableWithHelperMethods
+abstract public class TableWithColumnWidthSaver extends TableWithHelperMethods
 {
 	public TableWithColumnWidthSaver()
 	{
