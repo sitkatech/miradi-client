dabf343de510f544ed4959e9c5cb90d5fa81f751
nimaa <nimaa@localhost>
2007-10-25 21:45:58 +0000

    new KEA measurement node

---
+++ source/org/conservationmeasures/eam/dialogs/viability/KeyEcologicalAttributeMeasurementNode.java
@@ -0,0 +1,66 @@
+package org.conservationmeasures.eam.dialogs.viability;
+
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.BaseObject;
+import org.conservationmeasures.eam.objects.Measurement;
+import org.conservationmeasures.eam.views.TreeTableNode;
+
+public class KeyEcologicalAttributeMeasurementNode extends TreeTableNode
+{
+	public KeyEcologicalAttributeMeasurementNode(KeyEcologicalAttributeNode parent, Measurement measurementToUse)
+	{
+		measurement = measurementToUse;
+		keyEcologicalAttributesNode = parent;
+	}
+	
+	public BaseObject getObject()
+	{
+		return measurement;
+	}
+
+	public ORef getObjectReference()
+	{
+		return measurement.getRef();
+	}
+	
+	public int getType()
+	{
+		return measurement.getType();
+	}
+
+	public String toString()
+	{
+		return measurement.toString();
+	}
+
+	public int getChildCount()
+	{
+		return 0;
+	}
+
+	public TreeTableNode getChild(int index)
+	{
+		return null;
+	}
+	
+	public TreeTableNode getParentNode()
+	{
+		return keyEcologicalAttributesNode;
+	}
+
+	//TODO: this method could be pulled up to the supper
+	public Object getValueAt(int column)
+	{
+		String tag = COLUMN_TAGS[column];
+		return getObject().getData(tag);
+	}
+
+	public void rebuild() throws Exception
+	{
+	}
+	
+	public static final String[] COLUMN_TAGS = {Measurement.TAG_LABEL,};
+	
+	private Measurement measurement;
+	private KeyEcologicalAttributeNode keyEcologicalAttributesNode;
+}
