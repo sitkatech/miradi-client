36105993eefb7e7a5ea426eca48de192068fc39c
nimaa <nimaa@localhost>
2007-10-26 18:17:41 +0000

    took out the need for a project

--- tests/org/conservationmeasures/eam/project/TestProjectRepairer.java
+++ tests/org/conservationmeasures/eam/project/TestProjectRepairer.java
@@ -4,17 +4,17 @@ import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.ids.FactorId;
 import org.conservationmeasures.eam.ids.IdList;
 import org.conservationmeasures.eam.main.EAM;
-import org.conservationmeasures.eam.main.EAMTestCase;
+import org.conservationmeasures.eam.main.TestCaseWithProject;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.Factor;
 
-public class TestProjectRepairer extends EAMTestCase
+public class TestProjectRepairer extends TestCaseWithProject
 {
 	public TestProjectRepairer(String name)
 	{
 		super(name);
 	}
-
+	
 	public void testDeleteOrphanObjectives() throws Exception
 	{
 		int annotationType = ObjectType.OBJECTIVE;
@@ -44,28 +44,19 @@ public class TestProjectRepairer extends EAMTestCase
 
 	private void verifyDeleteOrphanAnnotations(int annotationType, int nodeType, String nodeTagForAnnotationList) throws Exception
 	{
-		Project project = new ProjectForTesting(getName());
-		try
-		{
-			//BaseId orphan = project.createObject(annotationType);
-			BaseId nonOrphan = project.createObjectAndReturnId(annotationType);
-			FactorId nodeId = (FactorId)project.createObject(nodeType, BaseId.INVALID);
-			IdList annotationIds = new IdList();
-			annotationIds.add(nonOrphan);
-			project.setObjectData(ObjectType.FACTOR, nodeId, nodeTagForAnnotationList, annotationIds.toString());
-			
-			EAM.setLogToString();
-			
-//			TODO: removed orphan deletion code until a solution can be found to general extentions of having annoations having annoations
-			ProjectRepairer.repairAnyProblems(project);
-			//assertContains("Deleting orphan", EAM.getLoggedString());
-			//assertNull("Didn't delete orphan?", project.findObject(annotationType, orphan));
-			assertEquals("Deleted non-orphan?", nonOrphan, project.findObject(annotationType, nonOrphan).getId());
-		}
-		finally
-		{
-			EAM.setLogToConsole();
-			project.close();
-		}
+		//BaseId orphan = project.createObject(annotationType);
+		BaseId nonOrphan = project.createObjectAndReturnId(annotationType);
+		FactorId nodeId = (FactorId)project.createObject(nodeType, BaseId.INVALID);
+		IdList annotationIds = new IdList();
+		annotationIds.add(nonOrphan);
+		project.setObjectData(ObjectType.FACTOR, nodeId, nodeTagForAnnotationList, annotationIds.toString());
+
+		EAM.setLogToString();
+
+//		TODO: removed orphan deletion code until a solution can be found to general extentions of having annoations having annoations
+		ProjectRepairer.repairAnyProblems(project);
+		//assertContains("Deleting orphan", EAM.getLoggedString());
+		//assertNull("Didn't delete orphan?", project.findObject(annotationType, orphan));
+		assertEquals("Deleted non-orphan?", nonOrphan, project.findObject(annotationType, nonOrphan).getId());
 	}
 }
