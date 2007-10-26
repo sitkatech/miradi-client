5b2977b6119496df11c20cbb39c652fccf647aa8
nimaa <nimaa@localhost>
2007-10-26 18:17:17 +0000

    parent test case with project

---
+++ tests/org/conservationmeasures/eam/main/TestCaseWithProject.java
@@ -0,0 +1,25 @@
+package org.conservationmeasures.eam.main;
+
+import org.conservationmeasures.eam.project.ProjectForTesting;
+
+public class TestCaseWithProject extends EAMTestCase
+{
+	public TestCaseWithProject(String name)
+	{
+		super(name);
+	}
+	
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		project = new ProjectForTesting(getName());
+	}
+
+	public void tearDown() throws Exception
+	{
+		super.tearDown();
+		project.close();
+	}
+
+	protected ProjectForTesting project;
+}
