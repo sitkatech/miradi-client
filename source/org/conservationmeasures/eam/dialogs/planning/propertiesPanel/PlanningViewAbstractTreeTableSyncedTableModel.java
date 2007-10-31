7e338db0233178825b97c34b28b3f6d9aaadf084
nimaa <nimaa@localhost>
2007-10-31 15:19:20 +0000

    new measurement table and model.  not added to panel yet. pulled out a common parent class

---
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewAbstractTreeTableSyncedTableModel.java
@@ -0,0 +1,40 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning.propertiesPanel;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.utils.ColumnTagProvider;
+
+import com.java.sun.jtreetable.TreeTableModelAdapter;
+
+abstract public class PlanningViewAbstractTreeTableSyncedTableModel extends AbstractTableModel  implements ColumnTagProvider
+{
+	public PlanningViewAbstractTreeTableSyncedTableModel(Project projectToUse, TreeTableModelAdapter adapterToUse) throws Exception
+	{
+		project = projectToUse;
+		adapter = adapterToUse;
+	}
+	
+	public int getRowCount()
+	{
+		return adapter.getRowCount();
+	}
+	
+	public String getColumnTag(int column)
+	{
+		return getColumnName(column);
+	}
+		
+	public Object getNodeForRow(int row)
+	{
+		return adapter.nodeForRow(row);
+	}
+		
+	protected Project project;
+	private TreeTableModelAdapter adapter;
+}
