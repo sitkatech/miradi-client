94877017367d9be853b629b7319fe6c6b6d2a22b
nimaa <nimaa@localhost>
2007-11-01 19:47:43 +0000

    took out measurement fields out of indicator

--- source/org/conservationmeasures/eam/objects/Indicator.java
+++ source/org/conservationmeasures/eam/objects/Indicator.java
@@ -25,9 +25,7 @@ import org.conservationmeasures.eam.project.ObjectManager;
 import org.conservationmeasures.eam.questions.IndicatorStatusRatingQuestion;
 import org.conservationmeasures.eam.questions.PriorityRatingQuestion;
 import org.conservationmeasures.eam.questions.RatingSourceQuestion;
-import org.conservationmeasures.eam.questions.StatusConfidenceQuestion;
 import org.conservationmeasures.eam.questions.StatusQuestion;
-import org.conservationmeasures.eam.questions.TrendQuestion;
 import org.conservationmeasures.eam.utils.EnhancedJsonObject;
 import org.conservationmeasures.eam.utils.StringMapData;
 
@@ -254,12 +252,6 @@ public class Indicator extends BaseObject
 		status = new ChoiceData();
 		taskIds = new IdListData();
 		indicatorThreshold = new StringMapData();
-		measurementTrend= new ChoiceData();
-		measurementStatus= new ChoiceData();
-		measurementDate= new DateData();;
-		measurementSummary= new StringData();
-		measurementDetail= new StringData();
-		measurementStatusConfidence = new ChoiceData();
 		ratingSource= new ChoiceData();
 		measurementRefs = new ORefListData();
 		
@@ -277,10 +269,7 @@ public class Indicator extends BaseObject
 		indicatorThresholdLabel = new PseudoStringData(TAG_INDICATOR_THRESHOLD);
 		priorityLabel = new PseudoQuestionData(new PriorityRatingQuestion(TAG_PRIORITY));
 		statusLabel = new PseudoQuestionData(new IndicatorStatusRatingQuestion(TAG_STATUS));
-		measurementTrendLabel = new PseudoQuestionData(new TrendQuestion(TAG_MEASUREMENT_TREND));
-		measurementStatusLabel = new PseudoQuestionData(new StatusQuestion(TAG_MEASUREMENT_STATUS));
 		ratingSourceLabel = new PseudoQuestionData(new RatingSourceQuestion(TAG_RATING_SOURCE));
-		measurementStatusConfidenceLabel = new PseudoQuestionData(new StatusConfidenceQuestion(TAG_MEASUREMENT_STATUS_CONFIDENCE));
 		latestMeasurement = new PseudoQuestionData(new StatusQuestion(Indicator.TAG_INDICATOR_THRESHOLD));
 		
 		futureStatusRatingLabel = new PseudoQuestionData(new StatusQuestion(TAG_FUTURE_STATUS_RATING));
@@ -291,12 +280,6 @@ public class Indicator extends BaseObject
 		addField(TAG_STATUS, status);
 		addField(TAG_TASK_IDS, taskIds);
 		addField(TAG_INDICATOR_THRESHOLD, indicatorThreshold);
-		addField(TAG_MEASUREMENT_TREND, measurementTrend);
-		addField(TAG_MEASUREMENT_STATUS, measurementStatus);
-		addField(TAG_MEASUREMENT_DATE, measurementDate);
-		addField(TAG_MEASUREMENT_SUMMARY, measurementSummary);
-		addField(TAG_MEASUREMENT_DETAIL, measurementDetail);
-		addField(TAG_MEASUREMENT_STATUS_CONFIDENCE, measurementStatusConfidence);
 		addField(TAG_RATING_SOURCE, ratingSource);
 		addField(TAG_MEASUREMENT_REFS, measurementRefs);
 		
@@ -314,10 +297,7 @@ public class Indicator extends BaseObject
 		addField(PSEUDO_TAG_METHODS, multiLineMethods);
 		addField(PSEUDO_TAG_PRIORITY_VALUE, priorityLabel);
 		addField(PSEUDO_TAG_STATUS_VALUE, statusLabel);
-		addField(PSEUDO_TAG_MEASUREMENT_TREND_VALUE, measurementTrendLabel);
-		addField(PSEUDO_TAG_MEASUREMENT_STATUS_VALUE, measurementStatusLabel);
 		addField(PSEUDO_TAG_RATING_SOURCE_VALUE, ratingSourceLabel);
-		addField(PSEUDO_TAG_MEASUREMENT_STATUS_CONFIDENCE_VALUE, measurementStatusConfidenceLabel);
 		
 		addField(PSEUDO_TAG_FUTURE_STATUS_RATING_VALUE, futureStatusRatingLabel);
 		addField(PSEUDO_TAG_LATEST_MEASUREMENT_REF, latestMeasurement);
@@ -328,12 +308,6 @@ public class Indicator extends BaseObject
 	public static final String TAG_STATUS = "Status";
 	public final static String TAG_TASK_IDS = "TaskIds";
 	public static final String TAG_INDICATOR_THRESHOLD = "IndicatorThresholds";
-	public static final String TAG_MEASUREMENT_TREND = "MeasurementTrend";
-	public static final String TAG_MEASUREMENT_STATUS  = "MeasurementStatus";
-	public static final String TAG_MEASUREMENT_DATE = "MeasurementDate";
-	public static final String TAG_MEASUREMENT_SUMMARY = "MeasurementSummary";
-	public static final String TAG_MEASUREMENT_DETAIL = "MeasurementDetail";
-	public static final String TAG_MEASUREMENT_STATUS_CONFIDENCE = "MeasurementStatusConfidence";
 	public static final String TAG_RATING_SOURCE = "RatingSource";
 	public static final String TAG_MEASUREMENT_REFS = "MeasurementRefs";
 	
@@ -350,12 +324,9 @@ public class Indicator extends BaseObject
 	public static final String PSEUDO_TAG_DIRECT_THREATS = "DirectThreats";
 	public static final String PSEUDO_TAG_STRATEGIES = "Strategies";
 	public static final String PSEUDO_TAG_METHODS = "Methods";
-	public static final String PSEUDO_TAG_MEASUREMENT_STATUS_VALUE  = "MeasurementStatusValue";
 	public static final String PSEUDO_TAG_INDICATOR_THRESHOLD_VALUE = "IndicatorThresholdValue";
 	public static final String PSEUDO_TAG_RATING_SOURCE_VALUE = "RatingSourceValue";
 	public static final String PSEUDO_TAG_PRIORITY_VALUE = "PriorityValue";
-	public static final String PSEUDO_TAG_MEASUREMENT_TREND_VALUE = "MeasurementTrendValue";
-	public static final String PSEUDO_TAG_MEASUREMENT_STATUS_CONFIDENCE_VALUE = "MeasurementStatusConfidenceValue";
 	public static final String PSEUDO_TAG_FUTURE_STATUS_RATING_VALUE  = "FutureStatusRatingValue";
 	public static final String PSEUDO_TAG_STATUS_VALUE  = "StatusValue";
 	public static final String PSEUDO_TAG_LATEST_MEASUREMENT_REF = "LatestMeasurementRef";
@@ -369,12 +340,6 @@ public class Indicator extends BaseObject
 	private ChoiceData status;
 	private IdListData taskIds;
 	private StringMapData indicatorThreshold;
-	private ChoiceData measurementTrend;
-	private ChoiceData measurementStatus;
-	private DateData measurementDate;
-	private StringData measurementSummary;
-	private StringData measurementDetail;
-	private ChoiceData measurementStatusConfidence;
 	private ChoiceData ratingSource;
 	private ORefListData measurementRefs;
 	
@@ -392,10 +357,7 @@ public class Indicator extends BaseObject
 	
 	private PseudoQuestionData priorityLabel;
 	private PseudoQuestionData statusLabel;
-	private PseudoQuestionData measurementTrendLabel;
-	private PseudoQuestionData measurementStatusLabel;
 	private PseudoQuestionData ratingSourceLabel;
-	private PseudoQuestionData measurementStatusConfidenceLabel;
 	
 	private PseudoQuestionData futureStatusRatingLabel;
 	private PseudoQuestionData latestMeasurement;
