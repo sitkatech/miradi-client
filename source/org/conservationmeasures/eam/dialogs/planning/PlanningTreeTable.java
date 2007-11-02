76c3f712998ae71a5f3fb623af8159870d23c78f
kevins <kevins@localhost>
2007-11-02 15:18:43 +0000

    Refactor extract inner class (and move to new package)

--- source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeTable.java
+++ source/org/conservationmeasures/eam/dialogs/planning/PlanningTreeTable.java
@@ -5,23 +5,14 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 
-import javax.swing.BorderFactory;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumn;
 
-import org.conservationmeasures.eam.main.AppPreferences;
-import org.conservationmeasures.eam.objects.BaseObject;
-import org.conservationmeasures.eam.objects.Indicator;
-import org.conservationmeasures.eam.objects.Task;
+import org.conservationmeasures.eam.dialogs.treetables.TreeTableCellRendererWithColor;
 import org.conservationmeasures.eam.project.Project;
-import org.conservationmeasures.eam.views.TreeTableNode;
 import org.conservationmeasures.eam.views.TreeTableWithColumnWidthSaving;
-import org.conservationmeasures.eam.views.TreeTableWithStateSaving;
 
 public class PlanningTreeTable extends TreeTableWithColumnWidthSaving
 {
@@ -59,59 +50,5 @@ public class PlanningTreeTable extends TreeTableWithColumnWidthSaving
 		return getPreferredSize();
 	}
 
-	static class TreeTableCellRendererWithColor extends DefaultTableCellRenderer
-	{
-		public TreeTableCellRendererWithColor(TreeTableWithStateSaving treeTableToUse, Project projectToUse)
-		{
-			treeTable = treeTableToUse;
-			project = projectToUse;
-		}
-		
-		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
-		{
-			Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-			PlanningTreeModel model = (PlanningTreeModel) treeTable.getTreeTableModel();
-			int modelColumn = table.convertColumnIndexToModel(column);
-			String columnTag = model.getColumnTag(modelColumn);
-			Color backgroundColor = getBackgroundColor(columnTag);
-			setBackground(backgroundColor);
-			if (model.getColumnTag(column).equals(BaseObject.PSEUDO_TAG_BUDGET_TOTAL))
-				setHorizontalAlignment(DefaultTableCellRenderer.RIGHT);
-			
-			setFont(getSharedTaskFont(((TreeTableNode) treeTable.getObjectForRow(row))));
-			setGrid();
-			
-			if (isSelected)
-				setBackground(table.getSelectionBackground());
-			
-			return component;
-		}
-
-		private void setGrid()
-		{
-			setBorder(BorderFactory.createMatteBorder(1, 1, 0, 0, Color.black));
-		}
-		
-		private Color getBackgroundColor(String columnTag)
-		{
-			if (columnTag.equals(Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML))
-				return AppPreferences.RESOURCE_TABLE_BACKGROUND;
-			
-			if (columnTag.equals(Indicator.PSEUDO_TAG_METHODS))
-				return AppPreferences.INDICATOR_COLOR;
-			
-			if(columnTag.equals(Task.PSEUDO_TAG_COMBINED_EFFORT_DATES))
-				return AppPreferences.WORKPLAN_TABLE_BACKGROUND;
-			
-			if(columnTag.equals(Task.PSEUDO_TAG_BUDGET_TOTAL))
-				return AppPreferences.BUDGET_TOTAL_TABLE_BACKGROUND;
-				
-			return Color.white;
-		}
-
-		TreeTableWithStateSaving treeTable;
-		Project project;
-	}
-	
 	public static final String UNIQUE_IDENTIFIER = "PlanningTreeTable";
 }
