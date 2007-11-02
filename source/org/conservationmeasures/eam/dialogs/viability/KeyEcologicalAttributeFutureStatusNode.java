8aa418961ebee08ca1117e0b9c873e4aa86a6c19
nimaa <nimaa@localhost>
2007-11-02 20:42:11 +0000

    added functionality to getvalueAt to return summary depending if column matches status

--- source/org/conservationmeasures/eam/dialogs/viability/KeyEcologicalAttributeFutureStatusNode.java
+++ source/org/conservationmeasures/eam/dialogs/viability/KeyEcologicalAttributeFutureStatusNode.java
@@ -10,6 +10,7 @@ import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objects.BaseObject;
 import org.conservationmeasures.eam.objects.Goal;
 import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.questions.StatusQuestion;
 
 public class KeyEcologicalAttributeFutureStatusNode extends TreeTableNode
 {
@@ -56,6 +57,21 @@ public class KeyEcologicalAttributeFutureStatusNode extends TreeTableNode
 
 	public Object getValueAt(int column)
 	{
+		String tag = COLUMN_TAGS[column];
+		String summaryData = parentNode.getObject().getData(Indicator.TAG_FUTURE_STATUS_SUMMARY);
+		String statusData = parentNode.getObject().getData(Indicator.TAG_FUTURE_STATUS_RATING);
+		if (tag.equals(StatusQuestion.POOR) && StatusQuestion.POOR.equals(statusData))
+			return summaryData;
+
+		if (tag.equals(StatusQuestion.FAIR) && StatusQuestion.FAIR.equals(statusData))
+			return summaryData;
+
+		if (tag.equals(StatusQuestion.GOOD) && StatusQuestion.GOOD.equals(statusData))
+			return summaryData;
+
+		if (tag.equals(StatusQuestion.VERY_GOOD) && StatusQuestion.VERY_GOOD.equals(statusData))
+			return summaryData;
+		
 		return "";
 	}
 
@@ -68,11 +84,11 @@ public class KeyEcologicalAttributeFutureStatusNode extends TreeTableNode
 		Goal.TAG_EMPTY, 
 		Goal.TAG_EMPTY,
 		
-		Goal.TAG_EMPTY,
-		Goal.TAG_EMPTY,
-		Goal.TAG_EMPTY,
-		Goal.TAG_EMPTY,
-		
+		StatusQuestion.POOR,
+		StatusQuestion.FAIR,
+		StatusQuestion.GOOD,
+	    StatusQuestion.VERY_GOOD,
+
 		Goal.TAG_EMPTY,
 	};
 	
