59fe788e61377ee504c65804bc86428222b20eca
kevins <kevins@localhost>
2007-11-09 04:08:44 +0000

    Brainstorm mode now has bottom panel with button to return to normal

--- source/org/conservationmeasures/eam/dialogs/diagram/ConceptualModelDiagramPanel.java
+++ source/org/conservationmeasures/eam/dialogs/diagram/ConceptualModelDiagramPanel.java
@@ -5,19 +5,40 @@
 */ 
 package org.conservationmeasures.eam.dialogs.diagram;
 
+import java.awt.BorderLayout;
+
+import org.conservationmeasures.eam.main.CommandExecutedEvent;
+import org.conservationmeasures.eam.main.CommandExecutedListener;
 import org.conservationmeasures.eam.main.MainWindow;
 import org.conservationmeasures.eam.views.diagram.ConceptualModelDiagramSplitPane;
 import org.conservationmeasures.eam.views.diagram.DiagramSplitPane;
 
-public class ConceptualModelDiagramPanel extends DiagramPanel
+public class ConceptualModelDiagramPanel extends DiagramPanel implements CommandExecutedListener
 {
 	public ConceptualModelDiagramPanel(MainWindow mainWindowToUse) throws Exception
 	{
 		super(mainWindowToUse);
+		brainstormModePanel = new BrainstormModePanel(mainWindowToUse.getActions());
+		add(brainstormModePanel, BorderLayout.AFTER_LAST_LINE);
+		mainWindowToUse.getProject().addCommandExecutedListener(this);
+	}
+	
+	public void dispose()
+	{
+		mainWindow.getProject().removeCommandExecutedListener(this);
+		super.dispose();
 	}
 
 	protected DiagramSplitPane createDiagramSplitter() throws Exception
 	{
 		return new ConceptualModelDiagramSplitPane(mainWindow);
-	}	
+	}
+	
+	public void commandExecuted(CommandExecutedEvent event)
+	{
+		brainstormModePanel.updateVisibility();
+	}
+
+	BrainstormModePanel brainstormModePanel;
+
 }
