66a7ae7f82008d6fe0f78a13b1ef7ae39cbbb790
kevins <kevins@localhost>
2007-11-09 05:05:19 +0000

    Start to extract reusable base class

---
+++ source/org/conservationmeasures/eam/dialogs/planning/legend/ControlPanel.java
@@ -0,0 +1,18 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.planning.legend;
+
+import org.conservationmeasures.eam.dialogs.base.DisposablePanel;
+
+import com.jhlabs.awt.BasicGridLayout;
+
+public class ControlPanel extends DisposablePanel
+{
+	public ControlPanel()
+	{
+		super(new BasicGridLayout(2, 1));
+	}
+}
