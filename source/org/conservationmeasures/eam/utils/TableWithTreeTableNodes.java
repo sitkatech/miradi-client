dff9b4db87ed29f7eb1f7594af3eb2c63ea00115
kevins <kevins@localhost>
2007-11-10 03:55:02 +0000

    Switch AstrickRenderer to use new TreeNodeForRowProvider interface

--- source/org/conservationmeasures/eam/utils/TableWithTreeTableNodes.java
+++ source/org/conservationmeasures/eam/utils/TableWithTreeTableNodes.java
@@ -5,14 +5,23 @@
 */ 
 package org.conservationmeasures.eam.utils;
 
+import org.conservationmeasures.eam.dialogs.fieldComponents.TreeNodeForRowProvider;
 import org.conservationmeasures.eam.dialogs.planning.propertiesPanel.PlanningViewAbstractTreeTableSyncedTableModel;
 import org.conservationmeasures.eam.dialogs.planning.propertiesPanel.PlanningViewFullSizeTable;
+import org.conservationmeasures.eam.dialogs.treetables.TreeTableNode;
 
-abstract public class TableWithTreeTableNodes extends PlanningViewFullSizeTable
+abstract public class TableWithTreeTableNodes extends PlanningViewFullSizeTable implements TreeNodeForRowProvider
 {
 	public TableWithTreeTableNodes(PlanningViewAbstractTreeTableSyncedTableModel modelToUse)
 	{
 		super(modelToUse);
+		nodeProvider = modelToUse;
 	}
 
+	public TreeTableNode getNodeForRow(int row)
+	{
+		return nodeProvider.getNodeForRow(row);
+	}
+
+	TreeNodeForRowProvider nodeProvider;
 }
