ca776be19493689e947f92675c35e7e6a974a694
kevins <kevins@localhost>
2007-11-11 04:17:51 +0000

    Convert to use new style renderer

--- source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetTotalsTable.java
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetTotalsTable.java
@@ -9,18 +9,27 @@ package org.conservationmeasures.eam.dialogs.planning.propertiesPanel;
 import java.awt.Color;
 
 import javax.swing.JLabel;
-import javax.swing.table.TableModel;
+import javax.swing.table.TableCellRenderer;
 
+import org.conservationmeasures.eam.dialogs.treetables.BasicTableCellRenderer;
+import org.conservationmeasures.eam.dialogs.treetables.NumericTableCellRenderer;
 import org.conservationmeasures.eam.main.AppPreferences;
 
 public class PlanningViewBudgetTotalsTable extends PlanningViewAbstractTableWithPreferredScrollableViewportSize
 {
-	public PlanningViewBudgetTotalsTable(TableModel model)
+	public PlanningViewBudgetTotalsTable(PlanningViewBudgetTotalsTableModel model)
 	{
 		super(model);
 		setBackground(getColumnBackGroundColor(0));
+		renderer = new NumericTableCellRenderer(model);
 	}
 
+	public TableCellRenderer getCellRenderer(int row, int column)
+	{
+		renderer.setCellBackgroundColor(getColumnBackGroundColor(column));
+		return renderer;	
+	}
+	
 	public Color getColumnBackGroundColor(int column)
 	{
 		return AppPreferences.BUDGET_TOTAL_TABLE_BACKGROUND;
@@ -47,4 +56,6 @@ public class PlanningViewBudgetTotalsTable extends PlanningViewAbstractTableWith
 	}
 	
 	public static final String UNIQUE_IDENTIFIER = "PlanningViewBudgetTotalsTable";
+
+	private BasicTableCellRenderer renderer;
 }
