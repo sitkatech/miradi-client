17f5f0888f67242ff3cb0ff22b80ee59f7847a2f
nimaa <nimaa@localhost>
2007-11-12 21:56:46 +0000

    added header comments

--- source/org/conservationmeasures/eam/dialogs/task/ShareableMethodPoolTablePanel.java
+++ source/org/conservationmeasures/eam/dialogs/task/ShareableMethodPoolTablePanel.java
@@ -1,3 +1,8 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/
 package org.conservationmeasures.eam.dialogs.task;
 
 import org.conservationmeasures.eam.dialogs.base.ObjectPoolTablePanel;
