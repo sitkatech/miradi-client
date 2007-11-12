bd6cbed2582b22472cc0f56852cf4d370c43aec6
kevins <kevins@localhost>
2007-11-12 01:28:09 +0000

    Add bold font to Planning View

--- source/org/conservationmeasures/eam/dialogs/tablerenderers/PlanningViewFontProvider.java
+++ source/org/conservationmeasures/eam/dialogs/tablerenderers/PlanningViewFontProvider.java
@@ -5,7 +5,34 @@
 */ 
 package org.conservationmeasures.eam.dialogs.tablerenderers;
 
+import java.awt.Font;
+
+import org.conservationmeasures.eam.objects.Goal;
+import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.objects.Objective;
+import org.conservationmeasures.eam.objects.Strategy;
+
 public class PlanningViewFontProvider extends FontForObjectTypeProvider
 {
+	public Font getFont(int objectType)
+	{
+		Font font = super.getFont(objectType);
+		if(shouldBeBold(objectType))
+			return font.deriveFont(Font.BOLD);
+		
+		return font;
+	}
 
+	private boolean shouldBeBold(int objectType)
+	{
+		if(objectType == Goal.getObjectType())
+			return true;
+		if(objectType == Objective.getObjectType())
+			return true;
+		if(objectType == Strategy.getObjectType())
+			return true;
+		if(objectType == Indicator.getObjectType())
+			return true;
+		return false;
+	}
 }
