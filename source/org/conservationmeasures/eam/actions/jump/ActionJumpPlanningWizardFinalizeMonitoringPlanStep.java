85fae8b706119dfda3b5a3691a19e9b396d16ec2
nimaa <nimaa@localhost>
2007-11-13 16:55:44 +0000

    new classes for finalize monitoring plan

---
+++ source/org/conservationmeasures/eam/actions/jump/ActionJumpPlanningWizardFinalizeMonitoringPlanStep.java
@@ -0,0 +1,23 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.actions.jump;
+
+import org.conservationmeasures.eam.actions.MainWindowAction;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.main.MainWindow;
+
+public class ActionJumpPlanningWizardFinalizeMonitoringPlanStep extends MainWindowAction
+{
+	public ActionJumpPlanningWizardFinalizeMonitoringPlanStep(MainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse, getLabel());
+	}
+
+	private static String getLabel()
+	{
+		return EAM.text("Finalize monitoring plan");
+	}
+}
