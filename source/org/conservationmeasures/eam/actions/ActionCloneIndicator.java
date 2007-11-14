b44b0d8064f0e123b3f62eb3647e4e2c93bf2ab3
nimaa <nimaa@localhost>
2007-11-14 04:26:12 +0000

    added clone indicator back into the factor properties dialog

--- source/org/conservationmeasures/eam/actions/ActionCloneIndicator.java
+++ source/org/conservationmeasures/eam/actions/ActionCloneIndicator.java
@@ -8,7 +8,7 @@ package org.conservationmeasures.eam.actions;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 
-public class ActionCloneIndicator extends MainWindowAction
+public class ActionCloneIndicator extends ObjectsAction
 {
 	public ActionCloneIndicator(MainWindow mainWindowToUse)
 	{
