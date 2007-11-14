bbe6b31bdfa81233d0eb76640cc16f661063d3d1
nimaa <nimaa@localhost>
2007-11-14 16:19:18 +0000

    repainting scroll pane if scrolled

--- source/org/conservationmeasures/eam/utils/FastScrollPane.java
+++ source/org/conservationmeasures/eam/utils/FastScrollPane.java
@@ -6,17 +6,23 @@
 package org.conservationmeasures.eam.utils;
 
 import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import org.martus.swing.UiScrollPane;
 
-public class FastScrollPane extends UiScrollPane
+public class FastScrollPane extends UiScrollPane implements MouseMotionListener, MouseWheelListener
 {
 	
 	public FastScrollPane(Component view)
 	{
 		super(view);
 		setUnitIncrements();
+		getVerticalScrollBar().addMouseMotionListener(this);
+		getHorizontalScrollBar().addMouseMotionListener(this);
+		addMouseWheelListener(this);
 	}
 	
 	private void setUnitIncrements()
@@ -29,6 +35,25 @@ public class FastScrollPane extends UiScrollPane
 	{
 		//note: overriding to change visibility
 		super.processMouseWheelEvent(e);
+	}
+
+	public void mouseDragged(MouseEvent e)
+	{
+		repaintDueToScrollRepaintIssues();
+	}
+
+	public void mouseMoved(MouseEvent e)
+	{
+	}
+
+	public void mouseWheelMoved(MouseWheelEvent e)
+	{
+		repaintDueToScrollRepaintIssues();
+	}
+	
+	private void repaintDueToScrollRepaintIssues()
+	{
+		repaint();
 	}
 	
 	public static final int SCROLL_UNIT_INCREMENT = 15;
