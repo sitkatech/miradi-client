088bb21e51209bc7815e22234bbd20529f8714f7
kevins <kevins@localhost>
2007-11-18 20:07:56 +0000

    Continue switch to unsequenced members

---
+++ source/org/miradi/datanet/RecordKeySet.java
@@ -0,0 +1,13 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.datanet;
+
+import java.util.HashSet;
+
+public class RecordKeySet extends HashSet<RecordKey>
+{
+
+}
