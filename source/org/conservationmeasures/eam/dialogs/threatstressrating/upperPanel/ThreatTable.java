9b4e523c30e4589a14ee25f9a23be98cc6d5349c
nimaa <nimaa@localhost>
2007-11-21 17:36:39 +0000

    added new ThreatTable.model (no functionality)

---
+++ source/org/conservationmeasures/eam/dialogs/threatstressrating/upperPanel/ThreatTable.java
@@ -0,0 +1,23 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.threatstressrating.upperPanel;
+
+import org.conservationmeasures.eam.utils.TableWithColumnWidthSaver;
+
+public class ThreatTable extends TableWithColumnWidthSaver
+{
+	public ThreatTable(ThreatTableModel tableModel)
+	{
+		super(tableModel);
+	}
+	
+	public String getUniqueTableIdentifier()
+	{
+		return UNIQUE_IDENTIFIER;
+	}
+
+	public static final String UNIQUE_IDENTIFIER = "ThreatsTable";
+}
