3b9734e6ed67eb5b5e3907d7a81e66ca69e9fad3
nimaa <nimaa@localhost>
2007-11-21 16:33:11 +0000

    moved class to new package

--- source/org/conservationmeasures/eam/dialogs/threatstressrating/management/ThreatStressRatingManagementPanel.java
+++ source/org/conservationmeasures/eam/dialogs/threatstressrating/management/ThreatStressRatingManagementPanel.java
@@ -3,11 +3,13 @@
 * Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
 * Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
 */ 
-package org.conservationmeasures.eam.dialogs.threatstressrating;
+package org.conservationmeasures.eam.dialogs.threatstressrating.management;
 
 import javax.swing.Icon;
 
 import org.conservationmeasures.eam.dialogs.base.ObjectManagementPanel;
+import org.conservationmeasures.eam.dialogs.threatstressrating.ThreatStressRatingListTablePanel;
+import org.conservationmeasures.eam.dialogs.threatstressrating.ThreatStressRatingPropertiesPanel;
 import org.conservationmeasures.eam.icons.StressIcon;
 import org.conservationmeasures.eam.utils.SplitterPositionSaverAndGetter;
 
