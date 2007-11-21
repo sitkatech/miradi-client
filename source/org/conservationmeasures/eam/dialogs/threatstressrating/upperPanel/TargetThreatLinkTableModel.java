8971dabbadc970673c4426e66ba32d69fed1ffcd
nimaa <nimaa@localhost>
2007-11-21 18:20:57 +0000

    added targetThreatLink table and model.

---
+++ source/org/conservationmeasures/eam/dialogs/threatstressrating/upperPanel/TargetThreatLinkTableModel.java
@@ -0,0 +1,40 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.threatstressrating.upperPanel;
+
+import org.conservationmeasures.eam.objects.Target;
+import org.conservationmeasures.eam.project.Project;
+
+public class TargetThreatLinkTableModel extends MainThreatTableModel
+{
+	public TargetThreatLinkTableModel(Project projectToUse)
+	{
+		super(projectToUse);
+		targets = getProject().getTargetPool().getTargets();
+	}
+	
+	public int getColumnCount()
+	{
+		return targets.length;
+	}
+	
+	public String getColumnName(int column)
+	{
+		return targets[column].toString();
+	}
+	
+	public Object getValueAt(int row, int column)
+	{
+		return "";
+	}
+	
+	public String getColumnTag(int column)
+	{
+		return targets[column].getLabel();
+	}
+	
+	private Target[] targets;
+}
