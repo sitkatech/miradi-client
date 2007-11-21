13186977411ac53c90f6c02b8d13ec2aed1e4ec9
nimaa <nimaa@localhost>
2007-11-21 02:45:46 +0000

    added correct text for labels.  clone stress pool table is colored

--- source/org/conservationmeasures/eam/dialogs/stress/StressPoolTableModel.java
+++ source/org/conservationmeasures/eam/dialogs/stress/StressPoolTableModel.java
@@ -8,6 +8,8 @@ package org.conservationmeasures.eam.dialogs.stress;
 import org.conservationmeasures.eam.dialogs.base.ObjectPoolTableModel;
 import org.conservationmeasures.eam.objects.Stress;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.questions.ChoiceQuestion;
+import org.conservationmeasures.eam.questions.PriorityRatingQuestion;
 
 public class StressPoolTableModel extends ObjectPoolTableModel
 {
@@ -16,8 +18,25 @@ public class StressPoolTableModel extends ObjectPoolTableModel
 		super(projectToUse, Stress.getObjectType(), COLUMN_TAGS);
 	}
 	
-	private static final String[] COLUMN_TAGS = new String[] {
-		Stress.TAG_LABEL, 
+	public ChoiceQuestion getColumnQuestion(int column)
+	{
+		return getPriorityQuestion(getColumnTag(column));
+	}
+
+	public static ChoiceQuestion getPriorityQuestion(String columnTag)
+	{
+		boolean isPriorityRatingQuestionColumn = columnTag.equals(Stress.TAG_SCOPE) ||
+		 columnTag.equals(Stress.TAG_SEVERITY) ||
+		 columnTag.equals(Stress.PSEUDO_STRESS_RATING);
+		if (isPriorityRatingQuestionColumn)
+			return new PriorityRatingQuestion(columnTag);
+		
+		return null;
+	}
+	
+	public static final String[] COLUMN_TAGS = new String[] {
+		Stress.TAG_LABEL,
+		Stress.TAG_SHORT_LABEL, 
 		Stress.TAG_SCOPE, 
 		Stress.TAG_SEVERITY, 
 		Stress.PSEUDO_STRESS_RATING
