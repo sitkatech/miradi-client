ee1b7a71ae697fb017cab57905a0e2ab91e815b6
kevins <kevins@localhost>
2007-11-22 07:26:12 +0000

    Avoid one more deprecated IdList constructor invocation

--- source/org/conservationmeasures/eam/commands/CommandSetObjectData.java
+++ source/org/conservationmeasures/eam/commands/CommandSetObjectData.java
@@ -66,7 +66,7 @@ public class CommandSetObjectData extends Command
 	//TODO find a better method name
 	static public CommandSetObjectData createAppendListCommand(BaseObject object, String idListTag, IdList listToAppend) throws ParseException
 	{
-		IdList newList = new IdList(object.getData(idListTag));
+		IdList newList = new IdList(listToAppend.getObjectType(), object.getData(idListTag));
 		newList.addAll(listToAppend);
 		return new CommandSetObjectData(object.getType(), object.getId(), idListTag, newList.toString());
 	}
