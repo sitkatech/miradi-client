ef8aa3d88d279417efdae9351187091abaa3bba9
nimaa <nimaa@localhost>
2007-11-26 20:41:59 +0000

    added some code for a drop downs (none functioning)

--- source/org/conservationmeasures/eam/dialogs/threatstressrating/properties/ThreatStressRatingTable.java
+++ source/org/conservationmeasures/eam/dialogs/threatstressrating/properties/ThreatStressRatingTable.java
@@ -7,18 +7,46 @@ package org.conservationmeasures.eam.dialogs.threatstressrating.properties;
 
 import org.conservationmeasures.eam.dialogs.base.EditableObjectTable;
 import org.conservationmeasures.eam.dialogs.threatstressrating.ThreatStressRatingTableModel;
+import org.conservationmeasures.eam.objects.ThreatStressRating;
 
 public class ThreatStressRatingTable extends EditableObjectTable
 {
 	public ThreatStressRatingTable(ThreatStressRatingTableModel threatStressRatingTableModel)
 	{
 		super(threatStressRatingTableModel);
+		model = threatStressRatingTableModel;
+		rebuildColumnEditorsAndRenderers();
+	}
+	
+	public ThreatStressRatingTableModel getThreatStressRatingTableModel()
+	{
+		return model;
+	}
+	
+	public void rebuildColumnEditorsAndRenderers()
+	{
+		for (int i = 0; i < getThreatStressRatingTableModel().getColumnCount(); ++i)
+		{
+			createContributionColumn(i);
+		}
 	}
 
+	private void createContributionColumn(int column)
+	{
+		if (! getThreatStressRatingTableModel().getColumnTag(column).equals(ThreatStressRating.TAG_CONTRIBUTION))
+			return;
+		//still in development
+		//FIXME use question instead of the threat list
+		//ThreatStressRating[] threatStressRatings = getObjectManager().getThreatStressRatingPool().getAllThreatStressRatings();
+		//ThreatStressRating invalidThreatStressRating = new ThreatStressRating(getObjectManager(), BaseId.INVALID);
+		//createComboColumn(threatStressRatings, column, invalidThreatStressRating);
+	}
+		
 	public String getUniqueTableIdentifier()
 	{
 		return UNIQUE_IDENTIFIER;
 	}
 
 	public static final String UNIQUE_IDENTIFIER = "ThreatStressRatingTable";
+	private ThreatStressRatingTableModel model;
 }
