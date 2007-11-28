cacab26f4c4d90c5da1d48386b9ebc554c2b0c07
nimaa <nimaa@localhost>
2007-11-28 15:55:21 +0000

    using extractbyType, remove static methods

--- source/org/conservationmeasures/eam/objects/BaseObject.java
+++ source/org/conservationmeasures/eam/objects/BaseObject.java
@@ -609,7 +609,7 @@ abstract public class BaseObject
 	
 	public ORefList findObjectsThatReferToUs(int objectType)
 	{
-		return ORefList.filterByType(findObjectsThatReferToUs(), objectType);
+		return findObjectsThatReferToUs().extractByType(objectType);
 	}
 	
 	
