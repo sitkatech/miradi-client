f838b2aa6b4788eb35d30fd317b6a05c82ca085e
nimaa <nimaa@localhost>
2007-11-29 00:01:49 +0000

    parent class with future choice item renderer

---
+++ source/org/conservationmeasures/eam/dialogs/threatstressrating/upperPanel/AbstractTableWithItemChoiceRenderer.java
@@ -0,0 +1,31 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.threatstressrating.upperPanel;
+
+import javax.swing.table.TableModel;
+
+import org.conservationmeasures.eam.utils.TableWithColumnWidthSaver;
+
+abstract public class AbstractTableWithItemChoiceRenderer extends TableWithColumnWidthSaver
+{
+	public AbstractTableWithItemChoiceRenderer(TableModel model)
+	{
+		super(model);
+		
+		setColumnRenderers();
+	}
+
+	private void setColumnRenderers()
+	{
+		//FIXME add renderer here and use this class for 3 upper threat rating tables
+		//ChoiceItemTableCellRenderer renderer = new ChoiceItemTableCellRenderer();
+		for (int i = 0; i < getColumnCount(); ++i)
+		{
+			//getColumnModel().getColumn(i).setCellRenderer();
+		}
+	}
+	
+}
