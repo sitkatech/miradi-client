ac1563a591a990d36ef592f726527cdcb60ecb62
nimaa <nimaa@localhost>
2007-12-05 19:22:42 +0000

    new question for threat rating mode

---
+++ source/org/conservationmeasures/eam/questions/ThreatRatingModeChoiceQuestion.java
@@ -0,0 +1,24 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.questions;
+
+import java.awt.Color;
+
+public class ThreatRatingModeChoiceQuestion extends StaticChoiceQuestion
+{
+	public ThreatRatingModeChoiceQuestion(String tagToUse)
+	{
+		super(tagToUse, "Threat Rating Mode", getChoiceItems());
+	}
+
+	static ChoiceItem[] getChoiceItems()
+	{
+		return new ChoiceItem[] {
+				new ChoiceItem("1", "Simple Threat Rating Mode", Color.WHITE),
+				new ChoiceItem("2", "Stress Based Threat Rating Mode", Color.yellow),
+		};
+	}
+}
