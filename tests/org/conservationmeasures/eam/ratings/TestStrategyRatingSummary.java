2b7fd337ae4ab0e5c7d30eb7da9039af59d97f3a
nimaa <nimaa@localhost>
2007-12-06 18:35:47 +0000

    testing new min method

--- tests/org/conservationmeasures/eam/ratings/TestStrategyRatingSummary.java
+++ tests/org/conservationmeasures/eam/ratings/TestStrategyRatingSummary.java
@@ -7,8 +7,6 @@ package org.conservationmeasures.eam.ratings;
 
 import org.conservationmeasures.eam.main.EAMTestCase;
 import org.conservationmeasures.eam.questions.ChoiceItem;
-import org.conservationmeasures.eam.questions.StrategyCostQuestion;
-import org.conservationmeasures.eam.questions.StrategyDurationQuestion;
 import org.conservationmeasures.eam.questions.StrategyFeasibilityQuestion;
 import org.conservationmeasures.eam.questions.StrategyImpactQuestion;
 import org.conservationmeasures.eam.questions.StrategyRatingSummaryQuestion;
@@ -22,31 +20,23 @@ public class TestStrategyRatingSummary extends EAMTestCase
 
 	public void testUnrated()
 	{
-		verifyGetResult("", "", "", "", "", "All unrated");
-		verifyGetResult("", "", "1", "1", "1", "Impact unrated");
-		verifyGetResult("", "1", "", "1", "1", "Duration unrated");
-		verifyGetResult("", "1", "", "1", "1", "Feasibility unrated");
-		verifyGetResult("", "1", "1", "1", "", "Cost unrated");
-		verifyGetResult("1", "1", "1", "1", "1", "All 1");
-		verifyGetResult("2", "2", "2", "2", "2", "All 2");
-		verifyGetResult("3", "3", "3", "3", "3", "All 3");
-		verifyGetResult("4", "4", "4", "4", "4", "All 4");
-		verifyGetResult("1", "1", "1", "1", "4", "1s and 4");
-		verifyGetResult("1", "4", "4", "4", "1", "4s and 1");
+		verifyGetResult("", "", "");
+		verifyGetResult("", "", "1");
+		verifyGetResult("", "1", "");
+		verifyGetResult("1", "1", "1");
+		verifyGetResult("1", "1", "2");
+		verifyGetResult("1", "2", "1");
 	}
 
-	private void verifyGetResult(String resultCode, String impactCode, String durationCode, String feasibilityCode, String costCode, String message)
+	private void verifyGetResult(String resultCode, String impactCode, String feasibilityCode)
 	{
 		StrategyImpactQuestion impactQuestion = new StrategyImpactQuestion("impact");
-		StrategyDurationQuestion durationQuestion = new StrategyDurationQuestion("impact");
 		StrategyFeasibilityQuestion feasibilityQuestion = new StrategyFeasibilityQuestion("feasibility");
-		StrategyCostQuestion costQuestion = new StrategyCostQuestion("feasibility");
-		StrategyRatingSummaryQuestion summary = new StrategyRatingSummaryQuestion("summary");
 		ChoiceItem impact = impactQuestion.findChoiceByCode(impactCode);
-		ChoiceItem duration = durationQuestion.findChoiceByCode(durationCode);
 		ChoiceItem feasibility = feasibilityQuestion.findChoiceByCode(feasibilityCode);
-		ChoiceItem cost = costQuestion.findChoiceByCode(costCode);
-		ChoiceItem result = summary.getResult(impact, duration, feasibility, cost);
-		assertEquals(message, resultCode, result.getCode());
+		
+		StrategyRatingSummaryQuestion summary = new StrategyRatingSummaryQuestion("summary");
+		ChoiceItem result = summary.getResult(impact, feasibility);
+		assertEquals("wrong result?", resultCode, result.getCode());
 	}
 }
