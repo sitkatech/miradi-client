821ba4167c1ea456e1283c6d15e0c82dabb75e75
nimaa <nimaa@localhost>
2007-12-06 16:32:08 +0000

    pulling up generic methods

--- source/org/conservationmeasures/eam/project/ThreatFormula.java
+++ source/org/conservationmeasures/eam/project/ThreatFormula.java
@@ -5,7 +5,47 @@
 */ 
 package org.conservationmeasures.eam.project;
 
+import java.util.HashMap;
+
 abstract public class ThreatFormula
 {
 
+	public HashMap<Integer, Integer> getBundleSummariesUsing357(int[] bundleValues)
+	{
+		int low = count(bundleValues, 1);
+		int medium = count(bundleValues, 2);
+		int high = count(bundleValues, 3);
+		int veryHigh = count(bundleValues, 4);
+		
+		// 3-5-7 rule
+		int newLow = low % 7;
+		medium += ((low - newLow) / 7);
+		low = newLow;
+		
+		int newMedium = medium % 5;
+		high += ((medium - newMedium) / 5);
+		medium = newMedium;
+		
+		int newHigh = high % 3;
+		veryHigh += ((high - newHigh) / 3);
+		high = newHigh;
+	
+		HashMap<Integer, Integer> computedValues = new HashMap();
+		computedValues.put(1, low);
+		computedValues.put(2, medium);
+		computedValues.put(3, high);
+		computedValues.put(4, veryHigh);
+		
+		return computedValues;
+	}
+	
+	private int count(int[] values, int lookFor)
+	{
+		int result = 0;
+		for(int i = 0; i < values.length; ++i)
+			if(values[i] == lookFor)
+				++result;
+		
+		return result;
+	}
 }
