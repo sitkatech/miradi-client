35e1250c44962ae412ec94a7b000c3b1a69c9e46
nimaa <nimaa@localhost>
2007-12-11 20:26:19 +0000

    split ViabilityTreeModel into 2, one for Viability, and one for indicators (since indicators have different columns).  there is also some commented code from colored headers.

--- source/org/conservationmeasures/eam/dialogs/viability/ViabilityTreeModel.java
+++ source/org/conservationmeasures/eam/dialogs/viability/ViabilityTreeModel.java
@@ -5,63 +5,23 @@
 */ 
 package org.conservationmeasures.eam.dialogs.viability;
 
-import org.conservationmeasures.eam.dialogs.treetables.GenericTreeTableModel;
-import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.Indicator;
 import org.conservationmeasures.eam.objects.KeyEcologicalAttribute;
 import org.conservationmeasures.eam.objects.Measurement;
 import org.conservationmeasures.eam.objects.Target;
-import org.conservationmeasures.eam.questions.ChoiceItem;
-import org.conservationmeasures.eam.questions.StatusQuestion;
 
-public class ViabilityTreeModel extends GenericTreeTableModel
+public class ViabilityTreeModel extends GenericViabilityTreeModel
 {
 	public ViabilityTreeModel(Object root)
 	{
 		super(root);
-		statusQuestion = new StatusQuestion("");
-
 	}
 
-	public int getColumnCount()
+	public String[] getColumnTags()
 	{
-		return columnTags.length;
-	}
-
-	public String getColumnTag(int column)
-	{
-		return columnTags[column];
-	}
-
-	public String getColumnName(int column)
-	{
-		String columnTag = getColumnTag(column);
-		if(isValueColumn(columnTag))
-			return getValueColumnChoice(columnTag).getLabel();
-		
-		return EAM.fieldLabel(getObjectTypeForColumnLabel(columnTag), columnTag);
+		return columnTags;
 	}
 	
-	private int getObjectTypeForColumnLabel(String tag)
-	{
-		if(tag.equals(Target.TAG_VIABILITY_MODE))
-			return Target.getObjectType();
-		else if(tag.equals(Measurement.TAG_STATUS_CONFIDENCE))
-			return Measurement.getObjectType();
-		return KeyEcologicalAttribute.getObjectType();
-	}
-	
-	boolean isValueColumn(String columnTag)
-	{
-		ChoiceItem choiceItem = statusQuestion.findChoiceByCode(columnTag);
-		return (choiceItem != null);
-	}
-
-	private ChoiceItem getValueColumnChoice(String columnTag)
-	{
-		return statusQuestion.findChoiceByCode(columnTag);	
-	}
-
 	public static String[] columnTags = {DEFAULT_COLUMN, 
 										 Target.TAG_VIABILITY_MODE,
 										 Indicator.TAG_STATUS, 
@@ -71,6 +31,4 @@ public class ViabilityTreeModel extends GenericTreeTableModel
 										 KeyEcologicalAttributeMeasurementNode.GOOD,
 										 KeyEcologicalAttributeMeasurementNode.VERY_GOOD,
 										 Measurement.TAG_STATUS_CONFIDENCE,};
-
-	private StatusQuestion statusQuestion;
 }
