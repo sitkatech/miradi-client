40479ee82c938130d3847d9cf59fa785d406ab31
nimaa <nimaa@localhost>
2007-12-12 14:37:23 +0000

    removed code that deals with creating bendpoints on nearby links

--- source/org/conservationmeasures/eam/views/diagram/BendPointCreator.java
+++ source/org/conservationmeasures/eam/views/diagram/BendPointCreator.java
@@ -6,9 +6,6 @@
 package org.conservationmeasures.eam.views.diagram;
 
 import java.awt.Point;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.util.Vector;
 
 import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.diagram.DiagramComponent;
@@ -17,7 +14,6 @@ import org.conservationmeasures.eam.diagram.cells.LinkCell;
 import org.conservationmeasures.eam.objects.DiagramLink;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.utils.PointList;
-import org.conservationmeasures.eam.utils.Utility;
 import org.jgraph.graph.GraphLayoutCache;
 
 public class BendPointCreator
@@ -54,72 +50,11 @@ public class BendPointCreator
 		linkCell.getBendPointSelectionHelper().addToSelection(snapped);
 	}
 	
-	public LinkCell[] getNearbyLinks(Point point, LinkCell selectedLinkCell)
-	{
-		LinkCell[] allCells = model.getAllFactorLinkCells();
-		Vector nearbyLinks = new Vector();
-		
-		for (int i = 0; i < allCells.length; ++i)
-		{
-			LinkCell linkCell = allCells[i];
-			if (! diagram.getBounds(linkCell).contains(point))
-				continue;
-		 
-			PointList pointList = getAllLinkPoints(linkCell);
-			if (isWithinRange(pointList, point))
-				nearbyLinks.add(linkCell);
-		}
-		
-		return (LinkCell[]) nearbyLinks.toArray(new LinkCell[0]);
-	}
-
-	private PointList getAllLinkPoints(LinkCell linkCell)
-	{
-		PointList pointList = new PointList();
-		pointList.add(linkCell.getSourceLocation(getCache()));
-		DiagramLink diagramLink = linkCell.getDiagramLink();
-		pointList.addAll(diagramLink.getBendPoints().getAllPoints());
-		pointList.add(linkCell.getTargetLocation(getCache()));
-		
-		return pointList;
-	}
-	
-	public boolean isWithinRange(PointList pointList, Point point)
-	{
-		Line2D.Double[] lineSegments = pointList.convertToLineSegments();
-		for (int i = 0; i < lineSegments.length; ++i)
-		{
-			Line2D.Double line = lineSegments[i];
-			Point2D point2D = Utility.convertToPoint2D(point);
-			double distance = line.ptSegDist(point2D);
-			if (distance < project.getGridSize())
-				return true;
-		}
-		
-		return false;
-	}
-	
 	private GraphLayoutCache getCache()
 	{
 		return diagram.getGraphLayoutCache();
 	}
 	
-	public void createBendPointOnNearbyLinks(DiagramLink link, Point pointToCreate) throws Exception
-	{
-		LinkCell linkCell = model.getDiagramFactorLink(link);
-		createBendPointOnNearbyLinks(linkCell, pointToCreate);
-	}
-	
-	public void createBendPointOnNearbyLinks(LinkCell linkCell, Point pointToCreate) throws Exception
-	{
-		LinkCell[] nearbyLinkCells = getNearbyLinks(pointToCreate, linkCell);
-		for (int i = 0; i < nearbyLinkCells.length; ++i)
-		{
-			LinkCell nearByLinkCell = nearbyLinkCells[i];
-			insertBendPointForLink(nearByLinkCell, pointToCreate);
-		}
-	}
-
 	private DiagramComponent diagram;
 	private DiagramModel model;
 	private Project project;
