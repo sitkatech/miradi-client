8bc7fb9eb3cc4699323a41047364ed6d365483f9
nimaa <nimaa@localhost>
2007-12-13 20:03:17 +0000

    removed duplicate code

--- source/org/conservationmeasures/eam/dialogs/base/ObjectTableWithParent.java
+++ source/org/conservationmeasures/eam/dialogs/base/ObjectTableWithParent.java
@@ -5,21 +5,11 @@
 */
 package org.conservationmeasures.eam.dialogs.base;
 
-import org.conservationmeasures.eam.objecthelpers.ORefList;
 
 public class ObjectTableWithParent extends ObjectListTable
 {
 	public ObjectTableWithParent(ObjectListTableModel modelToUse)
 	{
 		super(modelToUse);
-	}
-
-	public ORefList getSelectionHierarchy()
-	{
-		ORefList selectionHierarchyRefs = super.getSelectionHierarchy();
-		ObjectListTableModel listModel = (ObjectListTableModel) getObjectTableModel();
-		selectionHierarchyRefs.add(listModel.getContainingRef());
-		
-		return selectionHierarchyRefs;	
 	}
 }
