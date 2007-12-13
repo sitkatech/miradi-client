04b48d8eafcc75550cf6cfb26d7c212253741828
kevins <kevins@localhost>
2007-12-13 21:39:45 +0000

    Move Date to top of field list

--- source/org/conservationmeasures/eam/dialogs/planning/MeasurementPropertiesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/planning/MeasurementPropertiesPanel.java
@@ -21,9 +21,9 @@ public class MeasurementPropertiesPanel extends ObjectDataInputPanel
 	{
 		super(projectToUse, Measurement.getObjectType(), BaseId.INVALID);
 		
+		addField(createDateChooserField(ObjectType.MEASUREMENT, Measurement.TAG_DATE));
 		addField(createRatingChoiceField(ObjectType.MEASUREMENT, new StatusQuestion(Measurement.TAG_STATUS)));  
 		addField(createIconChoiceField(ObjectType.MEASUREMENT, new TrendQuestion(Measurement.TAG_TREND)));
-		addField(createDateChooserField(ObjectType.MEASUREMENT, Measurement.TAG_DATE));
 		addField(createStringField(ObjectType.MEASUREMENT, Measurement.TAG_SUMMARY,STD_SHORT));
 		addField(createMultilineField(ObjectType.MEASUREMENT, Measurement.TAG_DETAIL));
 		addField(createChoiceField(ObjectType.MEASUREMENT,  new StatusConfidenceQuestion(Measurement.TAG_STATUS_CONFIDENCE)));
