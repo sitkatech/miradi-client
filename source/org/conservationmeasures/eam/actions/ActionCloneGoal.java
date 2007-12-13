9d17050fb056b657aaf883fe789eedb07337b265
nimaa <nimaa@localhost>
2007-12-13 20:02:49 +0000

    are now objectActions

--- source/org/conservationmeasures/eam/actions/ActionCloneGoal.java
+++ source/org/conservationmeasures/eam/actions/ActionCloneGoal.java
@@ -8,7 +8,7 @@ package org.conservationmeasures.eam.actions;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 
-public class ActionCloneGoal extends MainWindowAction
+public class ActionCloneGoal extends ObjectsAction
 {
 	public ActionCloneGoal(MainWindow mainWindowToUse)
 	{
