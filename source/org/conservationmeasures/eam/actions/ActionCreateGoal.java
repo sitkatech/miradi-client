c0207b619e071706517bceab120db4c491d9b2ef
nimaa <nimaa@localhost>
2007-12-13 20:02:49 +0000

    are now objectActions

--- source/org/conservationmeasures/eam/actions/ActionCreateGoal.java
+++ source/org/conservationmeasures/eam/actions/ActionCreateGoal.java
@@ -8,7 +8,7 @@ package org.conservationmeasures.eam.actions;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 
-public class ActionCreateGoal extends MainWindowAction
+public class ActionCreateGoal extends ObjectsAction
 {
 	public ActionCreateGoal(MainWindow mainWindowToUse)
 	{
