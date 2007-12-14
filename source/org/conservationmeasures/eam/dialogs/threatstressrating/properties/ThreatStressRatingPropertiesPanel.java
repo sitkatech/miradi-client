5511c688155a0721f66d0341312b555ed23b7513
nimaa <nimaa@localhost>
2007-12-14 15:36:01 +0000

    updating the tsr list from the upper list table panel

--- source/org/conservationmeasures/eam/dialogs/threatstressrating/properties/ThreatStressRatingPropertiesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/threatstressrating/properties/ThreatStressRatingPropertiesPanel.java
@@ -7,16 +7,13 @@ package org.conservationmeasures.eam.dialogs.threatstressrating.properties;
 
 import java.awt.Component;
 
-import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.ids.BaseId;
-import org.conservationmeasures.eam.main.CommandExecutedEvent;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ORefList;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
-import org.conservationmeasures.eam.objects.FactorLink;
 import org.conservationmeasures.eam.views.umbrella.ObjectPicker;
 
 import com.jhlabs.awt.BasicGridLayout;
@@ -64,28 +61,6 @@ public class ThreatStressRatingPropertiesPanel extends ObjectDataInputPanel
 			setObjectRefs(new ORef[0]);
 		else
 			setObjectRefs(hierarchiesToSelectedRefs[0].toArray());
-	}
-	
-	public void commandExecuted(CommandExecutedEvent event)
-	{
-		super.commandExecuted(event);
-		if (event.isSetDataCommandWithThisTypeAndTag(FactorLink.getObjectType(), FactorLink.TAG_THREAT_STRESS_RATING_REFS))
-			handleExecutedSetDataCommand((CommandSetObjectData) event.getCommand());
-	}
-
-	private void handleExecutedSetDataCommand(CommandSetObjectData setCommand)
-	{
-		try
-		{
-			ORefList newThreatStressRatingRefsWithLinkRef = new ORefList(setCommand.getDataValue());
-			newThreatStressRatingRefsWithLinkRef.add(setCommand.getObjectORef());
-			setObjectRefs(new ORefList[] {newThreatStressRatingRefsWithLinkRef});
-		}
-		catch (Exception e)
-		{
-			EAM.logException(e);
-			EAM.errorDialog(EAM.text("Error Occured while updating Threat Stress Rating table."));
-		}
 	}
 	
 	public String getPanelDescription()
