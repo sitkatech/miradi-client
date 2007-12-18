4ce5ab14d7d5804edd9cbbfef7ad5d4723d8a6c4
nimaa <nimaa@localhost>
2007-12-18 19:46:32 +0000

    added isAvailable fucntionality

--- source/org/conservationmeasures/eam/views/diagram/doers/GroupBoxInsertFactorDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/GroupBoxInsertFactorDoer.java
@@ -5,19 +5,69 @@
 */ 
 package org.conservationmeasures.eam.views.diagram.doers;
 
+import org.conservationmeasures.eam.diagram.cells.EAMGraphCell;
+import org.conservationmeasures.eam.diagram.cells.FactorCell;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.objecthelpers.ORefList;
+import org.conservationmeasures.eam.objects.Cause;
+import org.conservationmeasures.eam.objects.GroupBox;
+import org.conservationmeasures.eam.objects.Strategy;
+import org.conservationmeasures.eam.objects.Target;
 import org.conservationmeasures.eam.views.diagram.LocationDoer;
 
 public class GroupBoxInsertFactorDoer extends LocationDoer
 {
 	public boolean isAvailable()
 	{
-		return false;
+		if (!isDiagramView())
+			return false;
+		
+		EAMGraphCell[] selected = getDiagramView().getDiagramPanel().getSelectedAndRelatedCells();
+		if (!containsOnlyOneGroupBox(selected))
+			return false;
+		
+		if (!containsAtleastOneFactor(selected))
+			return false;
+		
+		return true;
 	}
-
+	
 	public void doIt() throws CommandFailedException
 	{
 		if (!isAvailable())
 			return;
 	}
+
+	private boolean containsOnlyOneGroupBox(EAMGraphCell[] selected)
+	{
+		ORefList groupBoxRefs = new ORefList();
+		for (int i = 0; i < selected.length; ++i)
+		{
+			if (!selected[i].isFactor())
+				continue;
+			
+			FactorCell factorCell = (FactorCell) selected[i];
+			if (factorCell.getWrappedType() != GroupBox.getObjectType())
+				groupBoxRefs.add(factorCell.getWrappedORef());
+		}
+		
+		return groupBoxRefs.size() == 1;
+	}
+	
+	private boolean containsAtleastOneFactor(EAMGraphCell[] selected)
+	{
+		for (int i = 0; i < selected.length; ++i)
+		{
+			if (!selected[i].isFactor())
+				continue;
+			
+			FactorCell factorCell = (FactorCell) selected[i];
+			int type = factorCell.getWrappedType();
+			if (type == Target.getObjectType() || type == Cause.getObjectType() || type == Strategy.getObjectType())
+				return true;		
+		}
+		
+		return false;
+
+	}	
 }
