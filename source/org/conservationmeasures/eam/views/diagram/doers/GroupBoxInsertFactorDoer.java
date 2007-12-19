24115ab5157d2d04b14d1064ae0edd259eb144b9
nimaa <nimaa@localhost>
2007-12-19 00:20:40 +0000

    doIt inserts diagram factors into groupbox (no resizing of GB)

--- source/org/conservationmeasures/eam/views/diagram/doers/GroupBoxInsertFactorDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/GroupBoxInsertFactorDoer.java
@@ -5,11 +5,18 @@
 */ 
 package org.conservationmeasures.eam.views.diagram.doers;
 
+import java.text.ParseException;
+import java.util.Vector;
+
+import org.conservationmeasures.eam.commands.Command;
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.diagram.cells.EAMGraphCell;
 import org.conservationmeasures.eam.diagram.cells.FactorCell;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ORefList;
 import org.conservationmeasures.eam.objects.Cause;
+import org.conservationmeasures.eam.objects.DiagramFactor;
 import org.conservationmeasures.eam.objects.GroupBox;
 import org.conservationmeasures.eam.objects.Strategy;
 import org.conservationmeasures.eam.objects.Target;
@@ -35,27 +42,71 @@ public class GroupBoxInsertFactorDoer extends LocationDoer
 	public void doIt() throws CommandFailedException
 	{
 		if (!isAvailable())
-			return;
+			return;		
+		
+		try
+		{
+			Command[] commands = getCommandsToAppendGroupBoxChildren();
+			getProject().executeCommandsAsTransaction(commands);
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
 	}
 
+	private Command[] getCommandsToAppendGroupBoxChildren() throws ParseException
+	{
+		EAMGraphCell[] selected = getDiagramView().getDiagramPanel().getSelectedAndRelatedCells();
+		ORefList nonGroupBoxDiagramFactorRefs = extractNonGroupBoxDiagramFactors(selected);
+		DiagramFactor groupBoxDiagramFactor = getGroupBox(selected);
+		ORefList groupBoxChildrenRefs = groupBoxDiagramFactor.getGroupBoxChildrenRefs();
+		Vector<Command> commandsToAppend = new Vector();
+		for (int i = 0; i < nonGroupBoxDiagramFactorRefs.size(); ++i)
+		{
+			ORef diagramfactorRef = nonGroupBoxDiagramFactorRefs.get(i);
+			if (!groupBoxChildrenRefs.contains(diagramfactorRef))
+				commandsToAppend.add(CommandSetObjectData.createAppendORefCommand(groupBoxDiagramFactor, DiagramFactor.TAG_GROUP_BOX_CHILDERN_REFS, diagramfactorRef));
+		}
+		
+		return commandsToAppend.toArray(new Command[0]);
+	}
+	
 	private boolean containsOnlyOneGroupBox(EAMGraphCell[] selected)
 	{
-		ORefList groupBoxRefs = new ORefList();
+		return extractSelectedGroupBoxes(selected).size() == 1;		
+	}
+	
+	private boolean containsAtleastOneFactor(EAMGraphCell[] selected)
+	{
+		return extractNonGroupBoxDiagramFactors(selected).size() > 0;
+	}
+		
+	private DiagramFactor getGroupBox(EAMGraphCell[] selected)
+	{
+		final int FIRST_INDEX = 0;
+		return extractSelectedGroupBoxes(selected).get(FIRST_INDEX);
+	}
+	
+	private Vector<DiagramFactor>extractSelectedGroupBoxes(EAMGraphCell[] selected)
+	{
+		Vector<DiagramFactor> groupBoxDiagramFactors = new Vector();
 		for (int i = 0; i < selected.length; ++i)
 		{
 			if (!selected[i].isFactor())
 				continue;
 			
 			FactorCell factorCell = (FactorCell) selected[i];
-			if (factorCell.getWrappedType() != GroupBox.getObjectType())
-				groupBoxRefs.add(factorCell.getWrappedORef());
+			if (factorCell.getWrappedType() == GroupBox.getObjectType())
+				groupBoxDiagramFactors.add(factorCell.getDiagramFactor());
 		}
 		
-		return groupBoxRefs.size() == 1;
+		return groupBoxDiagramFactors;
 	}
 	
-	private boolean containsAtleastOneFactor(EAMGraphCell[] selected)
+	private ORefList extractNonGroupBoxDiagramFactors(EAMGraphCell[] selected)
 	{
+		ORefList nonGroupBoxDiagramFactorRefs = new ORefList();
 		for (int i = 0; i < selected.length; ++i)
 		{
 			if (!selected[i].isFactor())
@@ -64,10 +115,9 @@ public class GroupBoxInsertFactorDoer extends LocationDoer
 			FactorCell factorCell = (FactorCell) selected[i];
 			int type = factorCell.getWrappedType();
 			if (type == Target.getObjectType() || type == Cause.getObjectType() || type == Strategy.getObjectType())
-				return true;		
+				nonGroupBoxDiagramFactorRefs.add(factorCell.getDiagramFactorRef());		
 		}
 		
-		return false;
-
+		return nonGroupBoxDiagramFactorRefs;
 	}	
 }
