b6d2e1f34bbb7cfcfe6d0fde12012e89ced323d5
nimaa <nimaa@localhost>
2007-12-21 21:41:16 +0000

    took out extra test which is already done by isDiagramView

--- source/org/conservationmeasures/eam/views/diagram/InsertFactorLinkDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/InsertFactorLinkDoer.java
@@ -24,9 +24,6 @@ public class InsertFactorLinkDoer extends ViewDoer
 		if (!isDiagramView())
 			return false;
 		
-		if (getDiagramView().getDiagramModel() == null)
-			return false;
-				
 		return (getDiagramView().getDiagramModel().getFactorCount() >= 2);
 	}
 
