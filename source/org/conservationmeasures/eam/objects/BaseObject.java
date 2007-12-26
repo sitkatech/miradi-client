2a126b001770bec65ccd47156f485938e277480e
nimaa <nimaa@localhost>
2007-12-26 22:32:29 +0000

    refactor rename methods

--- source/org/conservationmeasures/eam/objects/BaseObject.java
+++ source/org/conservationmeasures/eam/objects/BaseObject.java
@@ -353,7 +353,7 @@ abstract public class BaseObject
 		id = newId;
 	}
 	
-	public String getBudgetCostRollupAsString()
+	public String getBudgetCostAsString()
 	{
 		return "";
 	}
@@ -874,7 +874,7 @@ abstract public class BaseObject
 	public String getPseudoData(String fieldTag)
 	{
 		if(fieldTag.equals(PSEUDO_TAG_BUDGET_TOTAL))
-			return getBudgetCostRollupAsString();
+			return getBudgetCostAsString();
 		
 		return getData(fieldTag);
 	}
