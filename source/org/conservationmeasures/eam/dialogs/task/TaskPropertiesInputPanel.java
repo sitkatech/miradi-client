4c9f5f18eabd127f9ffd64f76aa28c671c2ddc36
kevins <kevins@localhost>
2007-12-27 19:14:42 +0000

    Change budget rollup fields to readonly currency type

--- source/org/conservationmeasures/eam/dialogs/task/TaskPropertiesInputPanel.java
+++ source/org/conservationmeasures/eam/dialogs/task/TaskPropertiesInputPanel.java
@@ -48,7 +48,7 @@ public class TaskPropertiesInputPanel extends ObjectDataInputPanel
 	{
 		addField(createStringField(ObjectType.TASK, Task.TAG_LABEL));
 		addField(createChoiceField(Task.getObjectType(), new BudgetCostModeQuestion(Task.TAG_BUDGET_COST_MODE)));
-		addField(createReadonlyTextField(Task.PSEUDO_TAG_BUDGET_COST_ROLLUP));
+		addField(createReadonlyCurrencyField(Task.PSEUDO_TAG_BUDGET_COST_ROLLUP));
 		addField(createCurrencyField(Task.getObjectType(), Task.TAG_BUDGET_COST_OVERRIDE));
 		
 		addLabel(new UiLabel(""));
