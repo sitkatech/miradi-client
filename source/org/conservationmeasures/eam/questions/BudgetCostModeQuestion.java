6258ba8c456320b109dd4c2f71b3603d460c1a2d
kevins <kevins@localhost>
2007-12-27 21:52:50 +0000

    Extract public constant for ROLLUP code

--- source/org/conservationmeasures/eam/questions/BudgetCostModeQuestion.java
+++ source/org/conservationmeasures/eam/questions/BudgetCostModeQuestion.java
@@ -10,6 +10,7 @@ import org.conservationmeasures.eam.main.EAM;
 
 public class BudgetCostModeQuestion extends StaticChoiceQuestion
 {
+
 	public BudgetCostModeQuestion(String tagToUse)
 	{
 		super(tagToUse, "Budget Cost Mode", getChoiceItems());
@@ -18,10 +19,11 @@ public class BudgetCostModeQuestion extends StaticChoiceQuestion
 	public static ChoiceItem[] getChoiceItems()
 	{
 		return new ChoiceItem[] {
-				new ChoiceItem("", EAM.text("Rollup")),
+				new ChoiceItem(ROLLUP_MODE_CODE, EAM.text("Rollup")),
 				new ChoiceItem(OVERRIDE_MODE_CODE, EAM.text("High Level Est.")),
 		};
 	}
 	
+	public static final String ROLLUP_MODE_CODE = "";
 	public static final String OVERRIDE_MODE_CODE = "BudgetOverrideMode";
 }
