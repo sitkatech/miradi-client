9315166a925f62124d59070bccb34e6a856300c3
kevins <kevins@localhost>
2007-12-27 14:28:45 +0000

    Refactor rename class

--- source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetAnnualTotalsTable.java
+++ source/org/conservationmeasures/eam/dialogs/planning/propertiesPanel/PlanningViewBudgetAnnualTotalsTable.java
@@ -13,7 +13,7 @@ import javax.swing.table.TableCellRenderer;
 
 import org.conservationmeasures.eam.dialogs.tablerenderers.BasicTableCellRenderer;
 import org.conservationmeasures.eam.dialogs.tablerenderers.FontForObjectTypeProvider;
-import org.conservationmeasures.eam.dialogs.tablerenderers.PossiblyAllocatedPossiblyOverriddenNumericTableCellRenderer;
+import org.conservationmeasures.eam.dialogs.tablerenderers.BudgetCostTreeTableCellRenderer;
 import org.conservationmeasures.eam.dialogs.treetables.TreeTableNode;
 import org.conservationmeasures.eam.dialogs.treetables.TreeTableWithIcons;
 import org.conservationmeasures.eam.main.AppPreferences;
@@ -25,7 +25,7 @@ public class PlanningViewBudgetAnnualTotalsTable extends TableWithTreeTableNodes
 	public PlanningViewBudgetAnnualTotalsTable(PlanningViewBudgetAnnualTotalTableModel model, FontForObjectTypeProvider fontProvider)
 	{
 		super(model);
-		renderer = new PossiblyAllocatedPossiblyOverriddenNumericTableCellRenderer(model, getSyncedModel().getTreeTableModelAdapter(), fontProvider);
+		renderer = new BudgetCostTreeTableCellRenderer(model, getSyncedModel().getTreeTableModelAdapter(), fontProvider);
 	}
 	
 	public TableCellRenderer getCellRenderer(int row, int column)
