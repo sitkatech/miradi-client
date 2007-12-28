a5727966162c9353a7a8831005fb314bbdd1a17c
kevins <kevins@localhost>
2007-12-28 00:35:26 +0000

    Use base class to find constant

--- source/org/conservationmeasures/eam/dialogs/planning/StrategicRowColumnProvider.java
+++ source/org/conservationmeasures/eam/dialogs/planning/StrategicRowColumnProvider.java
@@ -5,13 +5,13 @@
 */ 
 package org.conservationmeasures.eam.dialogs.planning;
 
+import org.conservationmeasures.eam.objects.BaseObject;
 import org.conservationmeasures.eam.objects.ConceptualModelDiagram;
 import org.conservationmeasures.eam.objects.Desire;
 import org.conservationmeasures.eam.objects.Goal;
 import org.conservationmeasures.eam.objects.Objective;
 import org.conservationmeasures.eam.objects.ResultsChainDiagram;
 import org.conservationmeasures.eam.objects.Strategy;
-import org.conservationmeasures.eam.objects.Task;
 import org.conservationmeasures.eam.utils.CodeList;
 import org.conservationmeasures.eam.views.planning.PlanningView;
 
@@ -22,7 +22,7 @@ public class StrategicRowColumnProvider implements RowColumnProvider
 		return new CodeList(new String[] {
 							Strategy.PSEUDO_TAG_RATING_SUMMARY,
 							Desire.PSEUDO_TAG_FACTOR,
-							Task.PSEUDO_TAG_BUDGET_TOTAL,
+							BaseObject.PSEUDO_TAG_BUDGET_TOTAL,
 							Strategy.PSEUDO_TAG_TAXONOMY_CODE_VALUE,
 //							Strategy.PSEUDO_TAG_PROGRESS,
 //							Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML,
