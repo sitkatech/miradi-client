ef3ddca677a95935af27b4211f20232011689269
nimaa <nimaa@localhost>
2007-12-31 01:52:13 +0000

    abstract target threat table that will be used for right click popup

---
+++ source/org/conservationmeasures/eam/views/threatmatrix/AbstractDirectThreatTargetTableModel.java
@@ -0,0 +1,12 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.views.threatmatrix;
+
+import javax.swing.table.AbstractTableModel;
+
+abstract public class AbstractDirectThreatTargetTableModel extends AbstractTableModel
+{
+}
