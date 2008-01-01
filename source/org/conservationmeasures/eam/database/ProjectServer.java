2a6cc817111b7b007ab9fea1fb462d11f7358e30
kevins <kevins@localhost>
2008-01-01 22:59:45 +0000

    Migration just to notify user that currency type can now be specified

--- source/org/conservationmeasures/eam/database/ProjectServer.java
+++ source/org/conservationmeasures/eam/database/ProjectServer.java
@@ -267,7 +267,7 @@ abstract public class ProjectServer
 	static public String OBJECT_TYPE = "Type";
 	static public String TAG_VERSION = "Version";
 	static public String OBJECT_MANIFEST = "ObjectManifest";
-	static public int DATA_VERSION = 27;
+	static public int DATA_VERSION = 28;
 
 	File topDirectory;
 	String name;
