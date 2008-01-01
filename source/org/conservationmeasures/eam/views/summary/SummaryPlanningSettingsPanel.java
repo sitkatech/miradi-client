679e0601c5e2c7c9190084b5de9fd897be0a0971
kevins <kevins@localhost>
2008-01-01 22:30:42 +0000

    Add CurrencyType question, and add field to SummaryPlanningSettingsPanel

--- source/org/conservationmeasures/eam/views/summary/SummaryPlanningSettingsPanel.java
+++ source/org/conservationmeasures/eam/views/summary/SummaryPlanningSettingsPanel.java
@@ -5,15 +5,24 @@
 */ 
 package org.conservationmeasures.eam.views.summary;
 
+import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.questions.CurrencyTypeQuestion;
 
 public class SummaryPlanningSettingsPanel extends ObjectDataInputPanel
 {
 	public SummaryPlanningSettingsPanel(Project projectToUse)
 	{
 		super(projectToUse, projectToUse.getMetadata().getRef());
+		
+		ObjectDataInputField currency = createRatingChoiceField(new CurrencyTypeQuestion(ProjectMetadata.TAG_CURRENCY_TYPE));
+		
+		addField(currency);
+		
+		updateFieldsFromProject();
 	}
 
 	public String getPanelDescription()
