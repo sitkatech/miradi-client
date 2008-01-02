34f1440ed3118529381a612de5da159628236d6f
kevins <kevins@localhost>
2008-01-02 05:31:07 +0000

    Handy new layout and panel for two-column dialog panels

---
+++ source/org/conservationmeasures/eam/layout/TwoColumnGridLayout.java
@@ -0,0 +1,16 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.layout;
+
+import com.jhlabs.awt.GridLayoutPlus;
+
+public class TwoColumnGridLayout extends GridLayoutPlus
+{
+	public TwoColumnGridLayout()
+	{
+		super(0, 2);
+	}
+}
