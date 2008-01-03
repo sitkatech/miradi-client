f908912f13b1cab33ba2c347fc09319f183dafa1
nimaa <nimaa@localhost>
2008-01-03 18:07:57 +0000

    added content to summary location tab

--- source/org/conservationmeasures/eam/views/summary/SummaryLocationPanel.java
+++ source/org/conservationmeasures/eam/views/summary/SummaryLocationPanel.java
@@ -8,13 +8,27 @@ package org.conservationmeasures.eam.views.summary;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.questions.WwfCountriesQuestion;
 
 public class SummaryLocationPanel extends ObjectDataInputPanel
 {
 	public SummaryLocationPanel(Project projectToUse, ORef orefToUse)
 	{
 		super(projectToUse, orefToUse);
+		
+		addFieldWithCustomLabelAndHint(createNumericField(ProjectMetadata.TAG_PROJECT_LATITUDE, 10), "(Latitude must be -90.0000 to +90.0000)");
+		addFieldWithCustomLabelAndHint(createNumericField(ProjectMetadata.TAG_PROJECT_LONGITUDE, 10), "(Longitude must be -180.0000 to +180.0000)");
+		
+		addField(createMultiCodeField(ProjectMetadata.getObjectType(), new WwfCountriesQuestion(ProjectMetadata.TAG_COUNTRIES), 1));
+		addField(createStringField(ProjectMetadata.TAG_STATE_AND_PROVINCES));
+		addField(createStringField(ProjectMetadata.TAG_MUNICIPALITIES));
+		addField(createStringField(ProjectMetadata.TAG_LEGISLATIVE_DISTRICTS));
+		addField(createStringField(ProjectMetadata.TAG_LOCATION_DETAIL));
+		addField(createStringField(ProjectMetadata.TAG_LOCATION_COMMENTS));
+		
+		updateFieldsFromProject();
 	}
 
 	public String getPanelDescription()
