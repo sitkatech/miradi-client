c1695729a33b78114cc31d19dd49c522bb0b619b
nimaa <nimaa@localhost>
2008-01-03 17:40:14 +0000

    new Scope tab filled with fields

--- source/org/conservationmeasures/eam/views/summary/SummaryScopePanel.java
+++ source/org/conservationmeasures/eam/views/summary/SummaryScopePanel.java
@@ -5,9 +5,11 @@
 */ 
 package org.conservationmeasures.eam.views.summary;
 
+import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.project.Project;
 
 public class SummaryScopePanel extends ObjectDataInputPanel
@@ -15,6 +17,16 @@ public class SummaryScopePanel extends ObjectDataInputPanel
 	public SummaryScopePanel(Project projectToUse, ORef orefToUse)
 	{
 		super(projectToUse, orefToUse);
+		
+		addField(createStringField(ProjectMetadata.TAG_SHORT_PROJECT_SCOPE));
+		addField(createMultilineField(ProjectMetadata.TAG_PROJECT_SCOPE));
+		ObjectDataInputField projectAreaField = createMultilineField(ProjectMetadata.TAG_PROJECT_AREA);
+		ObjectDataInputField projectAreaNotesField = createMultilineField(ProjectMetadata.TAG_PROJECT_AREA_NOTES);
+		addFieldsOnOneLine(EAM.text("Label| Project Area(ha)"), new ObjectDataInputField[]{projectAreaField, projectAreaNotesField});
+		
+		addField(createStringField(ProjectMetadata.TAG_SHORT_PROJECT_VISION));
+		addField(createMultilineField(ProjectMetadata.TAG_PROJECT_VISION));
+		addField(createMultilineField(ProjectMetadata.TAG_SCOPE_COMMENTS));
 	}
 
 	public String getPanelDescription()
