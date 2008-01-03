f20d0ff75d47985e80208a74c64610097f5f17a3
kevins <kevins@localhost>
2008-01-03 16:43:06 +0000

    Add fancy question when user chooses to change budget time periods

--- source/org/conservationmeasures/eam/dialogfields/ObjectBudgetTimePeriodChoiceField.java
+++ source/org/conservationmeasures/eam/dialogfields/ObjectBudgetTimePeriodChoiceField.java
@@ -31,9 +31,29 @@ public class ObjectBudgetTimePeriodChoiceField extends ObjectChoiceField
 			String oldValue = getOldValue();
 			if(oldValue.equals(getText()))
 				return;
-			EAM.notifyDialog("This feature is not available yet");
+			
+			String discardButtonLabel = EAM.text("Discard");
+			String distributeButtonLabel = EAM.text("Distribute Evenly");
+			String cancelButtonLabel = EAM.getCancelButtonText();
+			String text = EAM.text("<html><p>If you have existing budget data, it can be migrated to the new time periods.</p>" +
+					"<p>" +
+					"<p>Please select how to handle existing budget data:</p>" +
+					"<p>" +
+					"<table>" +
+					"<tr><td>&lt;" + discardButtonLabel + "&gt;<td>Deletes all existing budget entries" +
+					"<tr><td>&lt;" + distributeButtonLabel + "&gt;<td>Consolidates or spreads out existing entries" +
+					"<tr><td>&lt;" + cancelButtonLabel + "&gt;<td>Revert to the existing time period with no data changes" +
+					"<p>" +
+					"");
+			String[] buttonLabels = {discardButtonLabel, distributeButtonLabel, cancelButtonLabel,};
+			
+			int result = EAM.confirmDialog(EAM.text("Change Budget Time Period"), text, buttonLabels);
+			System.out.println(result);
+			if(result != 2)
+				EAM.notifyDialog("Changing Budget Time Periods is not yet supported.");
 			setText(oldValue);
 //			super(event);
 		}
 	}
+	
 }
