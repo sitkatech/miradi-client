7765a42da90e75065b019f06f8bc1d40621a5e13
nimaa <nimaa@localhost>
2008-01-03 16:55:47 +0000

    created four tabs (empty tabs) to replace summary tab

---
+++ source/org/conservationmeasures/eam/views/summary/SummaryPlanningPanel.java
@@ -0,0 +1,24 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.views.summary;
+
+import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.project.Project;
+
+public class SummaryPlanningPanel extends ObjectDataInputPanel
+{
+	public SummaryPlanningPanel(Project projectToUse, ORef orefToUse)
+	{
+		super(projectToUse, orefToUse);
+	}
+
+	public String getPanelDescription()
+	{
+		return EAM.text("Planning");
+	}
+}
