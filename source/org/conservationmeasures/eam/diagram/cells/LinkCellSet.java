b448a0c6711b75cbb478a27da707dc77ad5a6220
nimaa <nimaa@localhost>
2008-01-04 16:37:41 +0000

    new sets for factorCell linkCell diagramLink and diagramFactor

---
+++ source/org/conservationmeasures/eam/diagram/cells/LinkCellSet.java
@@ -0,0 +1,12 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.diagram.cells;
+
+import java.util.HashSet;
+
+public class LinkCellSet extends HashSet
+{
+}
