5fa1c18d9f066a2c1b6a027b9d68a4f6700b1f70
nimaa <nimaa@localhost>
2008-01-07 23:40:49 +0000

    new abstract parent class for codelistComponent and soon refListcomponent

--- source/org/conservationmeasures/eam/dialogfields/CodeListComponent.java
+++ source/org/conservationmeasures/eam/dialogfields/CodeListComponent.java
@@ -5,64 +5,19 @@
 */ 
 package org.conservationmeasures.eam.dialogfields;
 
-import java.awt.Color;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-
 import javax.swing.JCheckBox;
-import javax.swing.JPanel;
-import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import org.conservationmeasures.eam.dialogs.fieldComponents.PanelCheckBox;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.questions.ChoiceItem;
 import org.conservationmeasures.eam.questions.ChoiceQuestion;
 import org.conservationmeasures.eam.utils.CodeList;
 
-import com.jhlabs.awt.BasicGridLayout;
-
-public class CodeListComponent extends JPanel implements ItemListener
+public class CodeListComponent extends AbstractListComponent
 {
-	public CodeListComponent(ChoiceQuestion questionToUse, ListSelectionListener listener)
-	{
-		this(questionToUse, 3, listener);
-	}
-	
 	public CodeListComponent(ChoiceQuestion questionToUse, int columnCount, ListSelectionListener listener)
 	{
-		setLayout(new BasicGridLayout(0,columnCount));
-		listSelectionListener = listener;
-		ChoiceItem[] choices = questionToUse.getChoices();
-		choiceItems = new ChoiceItem[choices.length];
-		checkBoxes = new PanelCheckBox[choices.length];
-		
-		for (int i=0; i<choices.length; ++i)
-		{
-			JCheckBox checkBox = new PanelCheckBox(choices[i].getLabel());
-			checkBox.addItemListener(this);
-			choiceItems[i] = choices[i];
-			checkBoxes[i] = checkBox;
-			add(checkBox);
-		}
-	}
-	
-	public void itemStateChanged(ItemEvent e) 
-	{
-	    if (e.getStateChange() == ItemEvent.SELECTED || 
-	    	e.getStateChange() == ItemEvent.DESELECTED)
-	    {
-	    	valueChanged();
-	    }
-	}
-	
-	public void valueChanged()
-	{
-		if (!skipNotice)
-		{
-			ListSelectionEvent event = new ListSelectionEvent("DUMMY EVENT",0,0, false);
-			listSelectionListener.valueChanged(event);
-		}
+		super(questionToUse, columnCount, listener);
 	}
 	
 	public String getText()
@@ -106,33 +61,4 @@ public class CodeListComponent extends JPanel implements ItemListener
 			skipNotice=false;
 		}
 	}
-	
-	public void setEnabled(boolean isValidObject)
-	{
-		super.setEnabled(isValidObject);
-		for (int checkBoxIndex = 0; checkBoxIndex<checkBoxes.length; ++checkBoxIndex)
-		{
-			updateEditableState(checkBoxes[checkBoxIndex],isValidObject);
-		}
-	}
-	
-	public void updateEditableState(JCheckBox checkBox, boolean isValidObject)
-	{
-			checkBox.setEnabled(isValidObject);
-			Color fg = EAM.EDITABLE_FOREGROUND_COLOR;
-			Color bg = EAM.EDITABLE_BACKGROUND_COLOR;
-			if(!isValidObject)
-			{
-				fg = EAM.READONLY_FOREGROUND_COLOR;
-				bg = EAM.READONLY_BACKGROUND_COLOR;
-			}
-			checkBox.setForeground(fg);
-			checkBox.setBackground(bg);
-
-	}
-
-	private JCheckBox checkBoxes[];
-	private ChoiceItem choiceItems[];
-	private ListSelectionListener listSelectionListener;
-	private boolean skipNotice;
 }
