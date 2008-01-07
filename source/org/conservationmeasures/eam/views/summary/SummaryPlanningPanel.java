1414245575c0d5e84a5b6845e13ebab80de2416f
kevins <kevins@localhost>
2008-01-07 23:27:32 +0000

    Make workplan panel a subpanel of the planning tab

--- source/org/conservationmeasures/eam/views/summary/SummaryPlanningPanel.java
+++ source/org/conservationmeasures/eam/views/summary/SummaryPlanningPanel.java
@@ -5,37 +5,28 @@
 */ 
 package org.conservationmeasures.eam.views.summary;
 
-import org.conservationmeasures.eam.dialogfields.ObjectBudgetTimePeriodChoiceField;
-import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
-import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
-import org.conservationmeasures.eam.ids.BaseId;
+import java.awt.Component;
+
+import javax.swing.BorderFactory;
+
+import org.conservationmeasures.eam.dialogs.base.AbstractObjectDataInputPanel;
+import org.conservationmeasures.eam.layout.OneColumnGridLayout;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
-import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.project.Project;
-import org.conservationmeasures.eam.questions.BudgetTimePeriodQuestion;
-import org.conservationmeasures.eam.questions.FiscalYearStartQuestion;
 
-public class SummaryPlanningPanel extends ObjectDataInputPanel
+public class SummaryPlanningPanel extends AbstractObjectDataInputPanel
 {
 	public SummaryPlanningPanel(Project projectToUse, ORef orefToUse)
 	{
 		super(projectToUse, orefToUse);
+		setLayout(new OneColumnGridLayout());
+
+		SummaryPlanningWorkPlanSubPanel workPlanSubPanel = new SummaryPlanningWorkPlanSubPanel(projectToUse, orefToUse);
+		workPlanSubPanel.setBorder(BorderFactory.createTitledBorder(EAM.text("Workplan")));
+		addSubPanel(workPlanSubPanel);
+		add(workPlanSubPanel);
 		
-		ObjectDataInputField startDate = createDateChooserField(ProjectMetadata.TAG_START_DATE);
-		ObjectDataInputField endDate = createDateChooserField(ProjectMetadata.TAG_EXPECTED_END_DATE);
-		ObjectDataInputField[] projectDateFields = new ObjectDataInputField[] {startDate, endDate, };
-		addFieldsOnOneLine(EAM.text("Label|Project Dates"), projectDateFields);
-		
-		ObjectDataInputField workPlanStartDate = createDateChooserField(ProjectMetadata.TAG_WORKPLAN_START_DATE);
-		ObjectDataInputField workPlanEndDate = createDateChooserField(ProjectMetadata.TAG_WORKPLAN_END_DATE);
-		ObjectDataInputField[] workPlanDateFields = new ObjectDataInputField[] {workPlanStartDate, workPlanEndDate, };
-		addFieldsOnOneLine(EAM.text("Label|Workplan Dates"), workPlanDateFields);
-		
-		ObjectBudgetTimePeriodChoiceField objectBudgetTimePeriodChoiceField = new ObjectBudgetTimePeriodChoiceField(getProject(), ProjectMetadata.getObjectType(), BaseId.INVALID, new BudgetTimePeriodQuestion(ProjectMetadata.TAG_WORKPLAN_TIME_UNIT));
-		addField(objectBudgetTimePeriodChoiceField);
-		addField(createChoiceField(ProjectMetadata.getObjectType(), new FiscalYearStartQuestion(ProjectMetadata.TAG_FISCAL_YEAR_START)));
-		addField(createMultilineField(ProjectMetadata.TAG_PLANNING_COMMENTS));
 		
 		updateFieldsFromProject();
 	}
@@ -43,5 +34,9 @@ public class SummaryPlanningPanel extends ObjectDataInputPanel
 	public String getPanelDescription()
 	{
 		return EAM.text("Planning");
+	}
+
+	public void addFieldComponent(Component component)
+	{
 	}
 }
---
+++ source/org/conservationmeasures/eam/views/summary/SummaryPlanningWorkPlanSubPanel.java
@@ -0,0 +1,48 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.views.summary;
+
+import org.conservationmeasures.eam.dialogfields.ObjectBudgetTimePeriodChoiceField;
+import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
+import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
+import org.conservationmeasures.eam.ids.BaseId;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.ProjectMetadata;
+import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.questions.BudgetTimePeriodQuestion;
+import org.conservationmeasures.eam.questions.FiscalYearStartQuestion;
+
+public class SummaryPlanningWorkPlanSubPanel extends ObjectDataInputPanel
+{
+	public SummaryPlanningWorkPlanSubPanel(Project projectToUse, ORef orefToUse)
+	{
+		super(projectToUse, orefToUse);
+		
+		ObjectDataInputField startDate = createDateChooserField(ProjectMetadata.TAG_START_DATE);
+		ObjectDataInputField endDate = createDateChooserField(ProjectMetadata.TAG_EXPECTED_END_DATE);
+		ObjectDataInputField[] projectDateFields = new ObjectDataInputField[] {startDate, endDate, };
+		addFieldsOnOneLine(EAM.text("Label|Project Dates"), projectDateFields);
+		
+		ObjectDataInputField workPlanStartDate = createDateChooserField(ProjectMetadata.TAG_WORKPLAN_START_DATE);
+		ObjectDataInputField workPlanEndDate = createDateChooserField(ProjectMetadata.TAG_WORKPLAN_END_DATE);
+		ObjectDataInputField[] workPlanDateFields = new ObjectDataInputField[] {workPlanStartDate, workPlanEndDate, };
+		addFieldsOnOneLine(EAM.text("Label|Workplan Dates"), workPlanDateFields);
+		
+		ObjectBudgetTimePeriodChoiceField objectBudgetTimePeriodChoiceField = new ObjectBudgetTimePeriodChoiceField(getProject(), ProjectMetadata.getObjectType(), BaseId.INVALID, new BudgetTimePeriodQuestion(ProjectMetadata.TAG_WORKPLAN_TIME_UNIT));
+		addField(objectBudgetTimePeriodChoiceField);
+		addField(createChoiceField(ProjectMetadata.getObjectType(), new FiscalYearStartQuestion(ProjectMetadata.TAG_FISCAL_YEAR_START)));
+		addField(createMultilineField(ProjectMetadata.TAG_PLANNING_COMMENTS));
+		
+		updateFieldsFromProject();
+	}
+
+	public String getPanelDescription()
+	{
+		return EAM.text("Planning: Work Plan");
+	}
+
+}
