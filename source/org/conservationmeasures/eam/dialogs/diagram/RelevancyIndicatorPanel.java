6e4254a98f03f0e1aabacb0944493f4f649e4ca8
nimaa <nimaa@localhost>
2008-01-07 23:29:34 +0000

    new relevancy indicator panel in modeless dialog

---
+++ source/org/conservationmeasures/eam/dialogs/diagram/RelevancyIndicatorPanel.java
@@ -0,0 +1,24 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.diagram;
+
+import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.project.Project;
+
+public class RelevancyIndicatorPanel extends ObjectDataInputPanel
+{
+	public RelevancyIndicatorPanel(Project projectToUse, ORef orefToUse)
+	{
+		super(projectToUse, orefToUse);
+	}
+
+	public String getPanelDescription()
+	{
+		return EAM.text("Indicator Relevancy Panel");
+	}
+}
