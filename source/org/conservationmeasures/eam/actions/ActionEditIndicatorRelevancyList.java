e10ab36cc037c5ca4a8e7d248afe4a98f57934a7
nimaa <nimaa@localhost>
2008-01-07 22:58:57 +0000

    changed doer and action to be objectsAction/doer.  added panel button for edit.  added action/doer to diagram view map

--- source/org/conservationmeasures/eam/actions/ActionEditIndicatorRelevancyList.java
+++ source/org/conservationmeasures/eam/actions/ActionEditIndicatorRelevancyList.java
@@ -8,7 +8,7 @@ package org.conservationmeasures.eam.actions;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 
-public class ActionEditIndicatorRelevancyList extends MainWindowAction
+public class ActionEditIndicatorRelevancyList extends ObjectsAction
 {
 	public ActionEditIndicatorRelevancyList(MainWindow mainWindow)
 	{
