eec5b55abc83be3eb1d6e1f2f1b40389aeb8507b
nimaa <nimaa@localhost>
2008-01-07 20:14:22 +0000

    updated RelevancyOverride/set/date and thier tests

--- tests/org/conservationmeasures/eam/main/MainTests.java
+++ tests/org/conservationmeasures/eam/main/MainTests.java
@@ -46,6 +46,7 @@ import org.conservationmeasures.eam.objecthelpers.TestORef;
 import org.conservationmeasures.eam.objecthelpers.TestORefList;
 import org.conservationmeasures.eam.objecthelpers.TestObjectDeepCopier;
 import org.conservationmeasures.eam.objecthelpers.TestRelevancyOverride;
+import org.conservationmeasures.eam.objecthelpers.TestRelevancyOverrideSet;
 import org.conservationmeasures.eam.objects.TestAccountingCode;
 import org.conservationmeasures.eam.objects.TestAssignment;
 import org.conservationmeasures.eam.objects.TestBaseObject;
@@ -235,6 +236,7 @@ public class MainTests extends TestCase
 		suite.addTest(new TestSuite(TestObjectDeepCopier.class));
 		suite.addTest(new TestSuite(TestDateRangeEffortList.class));
 		suite.addTest(new TestSuite(TestRelevancyOverride.class));
+		suite.addTest(new TestSuite(TestRelevancyOverrideSet.class));
 		
 		// objects package
 		suite.addTest(new TestSuite(TestStrategy.class));
--- tests/org/conservationmeasures/eam/objecthelpers/TestRelevancyOverride.java
+++ tests/org/conservationmeasures/eam/objecthelpers/TestRelevancyOverride.java
@@ -5,7 +5,10 @@
 */ 
 package org.conservationmeasures.eam.objecthelpers;
 
+import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.main.EAMTestCase;
+import org.conservationmeasures.eam.objects.Cause;
+import org.conservationmeasures.eam.utils.EnhancedJsonObject;
 
 public class TestRelevancyOverride extends EAMTestCase
 {
@@ -14,9 +17,39 @@ public class TestRelevancyOverride extends EAMTestCase
 		super(name);
 	}
 	
+	public void testRelevancyOverrideStringConstructor() throws Exception
+	{	
+		RelevancyOverride valid = new RelevancyOverride(getRandomRef(), true);
+		EnhancedJsonObject json = valid.toJson();
+		RelevancyOverride fromJson = new RelevancyOverride(json.toString());
+		assertEquals("not same ref?", getRandomRef(), fromJson.getRef());
+		assertEquals("not same flag?", true, fromJson.isOverride());
+	}
+
+	public void testRelevancyOverrideJsonConstructor() throws Exception
+	{
+		RelevancyOverride valid = new RelevancyOverride(getRandomRef(), true);
+		EnhancedJsonObject json = valid.toJson();
+		RelevancyOverride fromJson = new RelevancyOverride(json);
+		assertEquals("not same ref?", getRandomRef(), fromJson.getRef());
+		assertEquals("not same flag?", true, fromJson.isOverride());
+	}
+	
+	public void testEquals()
+	{
+		RelevancyOverride override1 = new RelevancyOverride(getRandomRef(), true);
+		RelevancyOverride override2 = new RelevancyOverride(getRandomRef(), true);
+		assertEquals("not the equals?", override1, override2);
+	}
+	
 	public void testIsOverride()
 	{
 		RelevancyOverride override = new RelevancyOverride(ORef.INVALID, true);
 		assertTrue(override.isOverride());
+	}
+	
+	private ORef getRandomRef()
+	{
+		return new ORef(Cause.getObjectType(), new BaseId(44));
 	}
 }
