9bb4422e2334330703f9596d3a8c1240fc9384f6
nimaa <nimaa@localhost>
2008-01-07 01:09:31 +0000

    field is now read only

--- source/org/conservationmeasures/eam/views/summary/TNCSummaryPanel.java
+++ source/org/conservationmeasures/eam/views/summary/TNCSummaryPanel.java
@@ -21,7 +21,7 @@ public class TNCSummaryPanel extends ObjectDataInputPanel
 		addField(createReadonlyTextField(metadata.TAG_TNC_DATABASE_DOWNLOAD_DATE));
 		addField(createMultilineField(metadata.TAG_TNC_LESSONS_LEARNED));
 		addField(createMultilineField(metadata.TAG_TNC_PLANNING_TEAM_COMMENT));
-		addField(createNumericField(metadata.TAG_TNC_SIZE_IN_HECTARES));
+		addField(createReadonlyTextField(metadata.TAG_TNC_SIZE_IN_HECTARES));
 		addField(createStringField(metadata.TAG_TNC_ECOREGION));
 		addField(createReadonlyTextField(metadata.TAG_TNC_COUNTRY));
 		addField(createStringField(metadata.TAG_TNC_OPERATING_UNITS));
