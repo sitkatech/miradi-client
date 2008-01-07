e42b94d57cec1f2fd3b3e0ef75c486661e6da551
nimaa <nimaa@localhost>
2008-01-07 05:01:17 +0000

    added new fields for team tab

---
+++ source/org/conservationmeasures/eam/questions/TeamRoleQuestion.java
@@ -0,0 +1,25 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.questions;
+
+public class TeamRoleQuestion extends StaticChoiceQuestion
+{
+	public TeamRoleQuestion(String tagToUse)
+	{
+		super(tagToUse, "Role", getRoleChoices());
+	}
+
+	static ChoiceItem[] getRoleChoices()
+	{
+		return new ChoiceItem[] {
+				new ChoiceItem("Contact", "Team Contact"),
+				new ChoiceItem("Leader", "Leader/Manager"),
+				new ChoiceItem("Facilitator", "Process Facilitator"),
+				new ChoiceItem("Advisor", "Project Advisor"),
+				new ChoiceItem("Stakeholder", "Stakeholder"),
+		};
+	}
+}
