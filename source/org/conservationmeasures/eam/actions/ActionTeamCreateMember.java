146626cbc499eab6b69dc72ba2301f0b51221179
nimaa <nimaa@localhost>
2008-01-07 16:20:27 +0000

    is now objectsAction

--- source/org/conservationmeasures/eam/actions/ActionTeamCreateMember.java
+++ source/org/conservationmeasures/eam/actions/ActionTeamCreateMember.java
@@ -8,7 +8,7 @@ package org.conservationmeasures.eam.actions;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.main.MainWindow;
 
-public class ActionTeamCreateMember extends ViewAction
+public class ActionTeamCreateMember extends ObjectsAction
 {
 		public ActionTeamCreateMember(MainWindow mainWindowToUse)
 		{
