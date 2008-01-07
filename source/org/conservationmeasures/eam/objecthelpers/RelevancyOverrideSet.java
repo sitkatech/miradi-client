dfea0a0a061b6d6c8fab8761da03a3266dc610b9
nimaa <nimaa@localhost>
2008-01-07 18:26:01 +0000

    relevancyOverride set

---
+++ source/org/conservationmeasures/eam/objecthelpers/RelevancyOverrideSet.java
@@ -0,0 +1,16 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.objecthelpers;
+
+import java.util.HashSet;
+
+public class RelevancyOverrideSet extends HashSet<RelevancyOverride>
+{
+	public RelevancyOverrideSet()
+	{
+		super();
+	}
+}
