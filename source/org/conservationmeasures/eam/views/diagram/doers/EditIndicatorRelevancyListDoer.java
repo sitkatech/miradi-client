b0d1fc4cf1429e027d17753277f90c287d69e923
nimaa <nimaa@localhost>
2008-01-08 21:17:55 +0000

    new titles for strat and indi relvant list dialog

--- source/org/conservationmeasures/eam/views/diagram/doers/EditIndicatorRelevancyListDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/EditIndicatorRelevancyListDoer.java
@@ -7,6 +7,7 @@ package org.conservationmeasures.eam.views.diagram.doers;
 
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.dialogs.diagram.RelevancyIndicatorPanel;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 
 public class EditIndicatorRelevancyListDoer extends AbstractEditRelevancyListDoer
@@ -14,5 +15,10 @@ public class EditIndicatorRelevancyListDoer extends AbstractEditRelevancyListDoe
 	protected ObjectDataInputPanel getRelevancyPanel(ORef objectiveRef)
 	{
 		return new RelevancyIndicatorPanel(getProject(), objectiveRef);
+	}
+
+	protected String getDialogTitle()
+	{
+		return EAM.text("Choose Relevant Indicator(s)");
 	}
 }
--- source/org/conservationmeasures/eam/views/diagram/doers/EditStrategyRelevancyListDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/EditStrategyRelevancyListDoer.java
@@ -7,6 +7,7 @@ package org.conservationmeasures.eam.views.diagram.doers;
 
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.dialogs.diagram.RelevancyStrategyPanel;
+import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 
 public class EditStrategyRelevancyListDoer extends AbstractEditRelevancyListDoer
@@ -15,5 +16,10 @@ public class EditStrategyRelevancyListDoer extends AbstractEditRelevancyListDoer
 	protected ObjectDataInputPanel getRelevancyPanel(ORef objectiveRef)
 	{
 		return new RelevancyStrategyPanel(getProject(), objectiveRef);
+	}
+	
+	protected String getDialogTitle()
+	{
+		return EAM.text("Choose Relevant Strategy(s)");
 	}
 }
