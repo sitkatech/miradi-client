601e3b0dbe26c1757db8f958fe7d6e24939591c8
nimaa <nimaa@localhost>
2008-01-08 00:28:02 +0000

    new objectQuestion that reads all objects from pool and indicator question

---
+++ source/org/conservationmeasures/eam/questions/IndicatorChoiceQuestion.java
@@ -0,0 +1,18 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.questions;
+
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.project.Project;
+
+public class IndicatorChoiceQuestion extends ObjectQuestion
+{
+	public IndicatorChoiceQuestion(Project project, String questionLabel)
+	{
+		super(project, Indicator.getObjectType(), EAM.text("Indicator list"));
+	}
+}
