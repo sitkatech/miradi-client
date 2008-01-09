9123c490d376d0ef193319c7a40e228f5b888c18
kevins <kevins@localhost>
2008-01-09 01:02:30 +0000

    First implementation of tree-based project list

---
+++ source/org/conservationmeasures/eam/wizard/noproject/FileSystemTreeNode.java
@@ -0,0 +1,97 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.wizard.noproject;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Vector;
+
+import org.conservationmeasures.eam.database.ProjectServer;
+import org.conservationmeasures.eam.dialogs.treetables.TreeTableNode;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.BaseObject;
+
+public class FileSystemTreeNode extends TreeTableNode
+{
+	public FileSystemTreeNode(File file) throws Exception
+	{
+		thisFile = file;
+		children = new Vector<FileSystemTreeNode>();
+		rebuild();
+	}
+	
+	public TreeTableNode getChild(int index)
+	{
+		return children.get(index);
+	}
+
+	public int getChildCount()
+	{
+		return children.size();
+	}
+
+	public BaseObject getObject()
+	{
+		throw new RuntimeException("Not an object-based tree");
+	}
+
+	public ORef getObjectReference()
+	{
+		throw new RuntimeException("Not an object-based tree");
+	}
+
+	public Object getValueAt(int column)
+	{
+		if(column == 0)
+		{
+			return thisFile;
+		}
+		if(column == 1)
+		{
+			if(!isProjectDirectory())
+				return null;
+			
+			long lastModifiedMillis = thisFile.lastModified();
+			Date date = new Date(lastModifiedMillis);
+			return date;
+		}
+		
+		throw new RuntimeException("Unknown column: " + column);
+	}
+
+	public void rebuild() throws Exception
+	{
+		children.clear();
+		if(isProjectDirectory())
+			return;
+		
+		File[] files = thisFile.listFiles();
+		for(int i = 0; i < files.length; ++i)
+		{
+			File file = files[i];
+			if(file.isDirectory())
+				children.add(new FileSystemTreeNode(file));
+		}
+	}
+
+	private boolean isProjectDirectory()
+	{
+		return ProjectServer.isExistingProject(thisFile);
+	}
+
+	public String toString()
+	{
+		return thisFile.getName();
+	}
+
+	public File getFile()
+	{
+		return thisFile;
+	}
+
+	private File thisFile;
+	private Vector<FileSystemTreeNode> children;
+}
---
+++ source/org/conservationmeasures/eam/wizard/noproject/ProjectListTreeTable.java
@@ -0,0 +1,260 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.wizard.noproject;
+
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.JPopupMenu;
+import javax.swing.ListSelectionModel;
+
+import org.conservationmeasures.eam.database.ProjectServer;
+import org.conservationmeasures.eam.dialogs.fieldComponents.PanelTreeTable;
+import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.views.noproject.CopyProject;
+import org.conservationmeasures.eam.views.noproject.DeleteProject;
+import org.conservationmeasures.eam.views.noproject.RenameProject;
+import org.conservationmeasures.eam.views.umbrella.ExportZippedProjectFileDoer;
+
+public class ProjectListTreeTable extends PanelTreeTable
+{
+	public ProjectListTreeTable(ProjectListTreeTableModel treeTableModel, NoProjectWizardStep handlerToUse)
+	{
+		super(treeTableModel);
+		tree.setRootVisible(false);
+		tree.setShowsRootHandles(true);
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		addMouseListener(new MouseHandler());
+	}
+
+	public String getUniqueTableIdentifier()
+	{
+		return EAM.text("Project List");
+	}
+	
+	private void doProjectOpen(Point point)
+	{
+		int row = rowAtPoint(point);
+		if(row < 0 || row > getRowCount())
+			return;
+		
+		FileSystemTreeNode node = (FileSystemTreeNode)getObjectForRow(row);
+		File file = node.getFile();
+		if(!isProjectDirectory(file))
+			return;
+		
+		Cursor cursor = getMainWindow().getCursor();
+		getMainWindow().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+		try
+		{
+			getMainWindow().createOrOpenProject(file);
+		}
+		finally
+		{
+			getMainWindow().setCursor(cursor);
+		}
+	}
+	
+	private void doContextMenu(Point point)
+	{
+		int row = rowAtPoint(point);
+		if(row < 0 || row > getRowCount())
+			return;
+		
+		getSelectionModel().setSelectionInterval(row, row);
+		FileSystemTreeNode node = (FileSystemTreeNode)getObjectForRow(row);
+		JPopupMenu menu = getRightClickMenu(node.getFile());
+		menu.show(this, point.x, point.y);
+
+	}
+
+	public JPopupMenu getRightClickMenu(File selectedFile)
+	{
+		JPopupMenu menu = new JPopupMenu();
+		menu.add(new ProjectListOpenAction(selectedFile));
+		menu.addSeparator();
+		menu.add(new ProjectListRenameAction(selectedFile)); 
+		menu.add(new ProjectListCopyToAction(selectedFile));
+		menu.add(new ProjectListExportAction(selectedFile));
+		menu.addSeparator();
+		menu.add(new ProjectListDeleteAction(selectedFile));
+		return menu;
+	}
+	
+	void refresh()
+	{
+		ProjectListTreeTableModel model = (ProjectListTreeTableModel)tree.getModel();
+		model.rebuildEntireTree();
+		repaint();
+	}
+	
+	private boolean isProjectDirectory(File file)
+	{
+		return ProjectServer.isExistingProject(file);
+	}
+
+
+	class MouseHandler extends MouseAdapter
+	{
+		public void mousePressed(MouseEvent e)
+		{
+			super.mousePressed(e);
+			if(e.isPopupTrigger())
+				doContextMenu(e.getPoint());
+		}
+		
+		public void mouseReleased(MouseEvent e)
+		{
+			super.mouseReleased(e);
+			if(e.isPopupTrigger())
+				doContextMenu(e.getPoint());
+		}
+		
+		public void mouseClicked(MouseEvent e)
+		{
+			super.mouseClicked(e);
+			if(e.getClickCount() == 2)
+				doProjectOpen(e.getPoint());
+		}
+	}
+	
+	abstract class ProjectListAction extends AbstractAction
+	{
+		public ProjectListAction(String string, File selectedFile)
+		{
+			super(string);
+			file = selectedFile;
+		}
+		
+		File getFile()
+		{
+			return file;
+		}
+		
+		private File file;
+	}
+
+	class ProjectListOpenAction extends ProjectListAction
+	{
+		public ProjectListOpenAction(File selectedFile)
+		{
+			super(EAM.text("Open"), selectedFile);
+		}
+
+		public void actionPerformed(ActionEvent event)
+		{
+			if(!isProjectDirectory(getFile()))
+				return;
+			
+			getMainWindow().createOrOpenProject(getFile());
+		}
+	}
+
+	class ProjectListRenameAction extends ProjectListAction
+	{
+		public ProjectListRenameAction(File selectedFile)
+		{
+			super(EAM.text("Rename..."), selectedFile);
+		}
+
+		public void actionPerformed(ActionEvent event)
+		{
+			try
+			{
+				if(!isProjectDirectory(getFile()))
+					return;
+				
+				RenameProject.doIt(getMainWindow(), getFile());
+			}
+			catch(Exception e)
+			{
+				EAM.logException(e);
+				EAM.errorDialog(EAM.text("Error renaming project: " + e.getMessage()));
+			}
+			refresh();
+		}
+	}
+
+	class ProjectListCopyToAction extends ProjectListAction
+	{
+		public ProjectListCopyToAction(File selectedFile)
+		{
+			super(EAM.text("Copy To..."), selectedFile);
+		}
+
+		public void actionPerformed(ActionEvent event)
+		{
+			try
+			{
+				if(!isProjectDirectory(getFile()))
+					return;
+				
+				CopyProject.doIt(getMainWindow(), getFile());
+			}
+			catch(Exception e)
+			{
+				EAM.logException(e);
+				EAM.errorDialog(EAM.text("Error copying project: " + e.getMessage()));
+			}
+			refresh();
+		}
+	}
+
+	class ProjectListExportAction extends ProjectListAction
+	{
+		public ProjectListExportAction(File selectedFile)
+		{
+			super(EAM.text("Export..."), selectedFile);
+		}
+
+		public void actionPerformed(ActionEvent event)
+		{
+			try
+			{
+				if(!isProjectDirectory(getFile()))
+					return;
+				
+				ExportZippedProjectFileDoer.perform(getMainWindow(), getFile());
+			}
+			catch(CommandFailedException e)
+			{
+				EAM.logException(e);
+				EAM.errorDialog(EAM.text("Error exporting project: " + e.getMessage()));
+			}
+		}
+	}
+
+	class ProjectListDeleteAction extends ProjectListAction
+	{
+		public ProjectListDeleteAction(File selectedFile)
+		{
+			super(EAM.text("Delete"), selectedFile);
+		}
+
+		public void actionPerformed(ActionEvent event)
+		{
+			try
+			{
+				if(!isProjectDirectory(getFile()))
+					return;
+				
+				DeleteProject.doIt(getMainWindow(), getFile());
+			}
+			catch(Exception e)
+			{
+				EAM.logException(e);
+				EAM.errorDialog(EAM.text("Error deleting project: " + e.getMessage()));
+			}
+			refresh();
+		}
+	}
+}
