68416b696cbf8e913255c0d93d45e140f234abca
nimaa <nimaa@localhost>
2008-01-09 15:40:15 +0000

    clear GB child list, then delete all referring GB links

--- source/org/conservationmeasures/eam/views/diagram/doers/DeleteGroupBoxDoer.java
+++ source/org/conservationmeasures/eam/views/diagram/doers/DeleteGroupBoxDoer.java
@@ -9,10 +9,15 @@ import java.util.HashSet;
 
 import org.conservationmeasures.eam.commands.CommandBeginTransaction;
 import org.conservationmeasures.eam.commands.CommandEndTransaction;
+import org.conservationmeasures.eam.commands.CommandSetObjectData;
 import org.conservationmeasures.eam.diagram.cells.FactorCell;
 import org.conservationmeasures.eam.exceptions.CommandFailedException;
 import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORefList;
+import org.conservationmeasures.eam.objects.DiagramFactor;
+import org.conservationmeasures.eam.objects.DiagramLink;
 import org.conservationmeasures.eam.project.FactorDeleteHelper;
+import org.conservationmeasures.eam.views.diagram.LinkDeletor;
 import org.conservationmeasures.eam.views.diagram.LocationDoer;
 
 public class DeleteGroupBoxDoer extends LocationDoer
@@ -33,10 +38,14 @@ public class DeleteGroupBoxDoer extends LocationDoer
 		getProject().executeCommand(new CommandBeginTransaction());
 		try
 		{
+			LinkDeletor linkDeletor = new LinkDeletor(getProject());
 			FactorDeleteHelper deleteHelper = new FactorDeleteHelper(getDiagramView().getDiagramModel());
 			HashSet<FactorCell> groupBoxCells = getDiagramView().getDiagramComponent().getOnlySelectedGroupBoxCells();
 			for(FactorCell groupBoxCell : groupBoxCells)
 			{
+				removeAllChildrenFromGroupBox(groupBoxCell);
+				ORefList referringGroupBoxDiagramLinks = groupBoxCell.getDiagramFactor().findObjectsThatReferToUs(DiagramLink.getObjectType());
+				linkDeletor.deleteDiagramLinks(referringGroupBoxDiagramLinks);
 				deleteHelper.deleteFactor(groupBoxCell.getDiagramFactor());	
 			}
 		}
@@ -51,6 +60,12 @@ public class DeleteGroupBoxDoer extends LocationDoer
 
 	}
 	
+	private void removeAllChildrenFromGroupBox(FactorCell groupBoxCell) throws Exception
+	{
+		CommandSetObjectData clearChildrenList = new CommandSetObjectData(groupBoxCell.getDiagramFactorRef(), DiagramFactor.TAG_GROUP_BOX_CHILDREN_REFS, new ORefList().toString());
+		getProject().executeCommand(clearChildrenList);
+	}
+
 	private boolean hasSelectedGroupBoxes()
 	{
 		try
