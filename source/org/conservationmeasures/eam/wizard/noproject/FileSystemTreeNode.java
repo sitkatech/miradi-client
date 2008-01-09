f1d6cd5b689d2d02866aecee9bf43fb838e92f94
kevins <kevins@localhost>
2008-01-09 18:26:06 +0000

    Fix crash if there was an empty folder

--- source/org/conservationmeasures/eam/wizard/noproject/FileSystemTreeNode.java
+++ source/org/conservationmeasures/eam/wizard/noproject/FileSystemTreeNode.java
@@ -71,6 +71,9 @@ public class FileSystemTreeNode extends TreeTableNode
 			return;
 		
 		File[] files = thisFile.listFiles();
+		if(files == null)
+			return;
+		
 		for(int i = 0; i < files.length; ++i)
 		{
 			File file = files[i];
