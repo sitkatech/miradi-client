c4d0aa65db77654e80a971a2ace504409bf43b93
nimaa <nimaa@localhost>
2008-01-10 00:23:20 +0000

    added terrestrial/marine/freshwater fields and questions to tnc tab

--- source/org/conservationmeasures/eam/views/summary/TNCSummaryPanel.java
+++ source/org/conservationmeasures/eam/views/summary/TNCSummaryPanel.java
@@ -9,7 +9,10 @@ import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.ProjectMetadata;
 import org.conservationmeasures.eam.project.Project;
+import org.conservationmeasures.eam.questions.TncFreshwaterEcoRegionQuestion;
+import org.conservationmeasures.eam.questions.TncMarineEcoRegionQuestion;
 import org.conservationmeasures.eam.questions.TncOperatingUnitsQuestion;
+import org.conservationmeasures.eam.questions.TncTerrestrialEcoRegionQuestion;
 
 public class TNCSummaryPanel extends ObjectDataInputPanel
 {
@@ -23,6 +26,10 @@ public class TNCSummaryPanel extends ObjectDataInputPanel
 		addField(createMultilineField(metadata.TAG_TNC_LESSONS_LEARNED));
 		addField(createMultilineField(metadata.TAG_TNC_PLANNING_TEAM_COMMENT));
 		addField(createStringField(metadata.TAG_TNC_ECOREGION));
+		addField(createMultiCodeField(ProjectMetadata.getObjectType(), new TncTerrestrialEcoRegionQuestion(ProjectMetadata.TAG_TNC_TERRESTRIAL_ECO_REGION), 1));
+		addField(createMultiCodeField(ProjectMetadata.getObjectType(), new TncMarineEcoRegionQuestion(ProjectMetadata.TAG_TNC_MARINE_ECO_REGION), 1));
+		addField(createMultiCodeField(ProjectMetadata.getObjectType(), new TncFreshwaterEcoRegionQuestion(ProjectMetadata.TAG_TNC_FRESHWATER_ECO_REGION), 1));
+		
 		addField(createReadonlyTextField(metadata.TAG_TNC_COUNTRY));
 		addField(createReadonlyTextField(metadata.LEGACY_TAG_TNC_OPERATING_UNITS));
 		addField(createMultiCodeField(ProjectMetadata.getObjectType(), new TncOperatingUnitsQuestion(ProjectMetadata.TAG_TNC_OPERATING_UNITS), 1));
