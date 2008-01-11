840f332eca35da7fdd4381b43c22b044db46c468
nimaa <nimaa@localhost>
2008-01-11 16:16:32 +0000

    added nested target icon to tab

---
+++ source/org/conservationmeasures/eam/icons/SubTargetIcon.java
@@ -0,0 +1,16 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.icons;
+
+import org.conservationmeasures.eam.main.EAMResourceImageIcon;
+
+public class SubTargetIcon extends EAMResourceImageIcon
+{
+	public SubTargetIcon()
+	{
+		super("images/SubTarget16.png");
+	}
+}
