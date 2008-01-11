a6202c37475ab74adebc13fd44acdb60db9edea2
kevins <kevins@localhost>
2008-01-11 01:49:50 +0000

    New pseudofield..not used but a nice example for future work

---
+++ source/org/conservationmeasures/eam/objectdata/pseudodata/PseudoTimestampData.java
@@ -0,0 +1,53 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.objectdata.pseudodata;
+
+import org.conservationmeasures.eam.objectdata.ObjectData;
+import org.conservationmeasures.eam.objectdata.StringData;
+import org.conservationmeasures.eam.objects.BaseObject;
+
+public class PseudoTimestampData extends ObjectData
+{
+	public PseudoTimestampData(BaseObject owningObject, String tag)
+	{
+		owner = owningObject;
+		pseudoTag = tag;
+	}
+
+	public boolean isPseudoField()
+	{
+		return true;
+	}
+	
+	public void set(String newValue) throws Exception
+	{
+		if (newValue.length()!=0)
+			throw new RuntimeException("Set not allowed in a pseuod field");
+	}
+
+	public String get()
+	{
+		return owner.getPseudoData(pseudoTag);
+	}
+	
+	public boolean equals(Object rawOther)
+	{
+		if(!(rawOther instanceof StringData))
+			return false;
+		
+		StringData other = (StringData)rawOther;
+		return get().equals(other.get());
+	}
+
+	public int hashCode()
+	{
+		return get().hashCode();
+	}
+
+	private BaseObject owner;
+	private String pseudoTag;
+
+}
