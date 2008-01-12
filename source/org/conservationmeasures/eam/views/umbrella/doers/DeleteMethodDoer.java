a85342c05e71f045d65fdd1ef4fe4c8c0b208138
kevins <kevins@localhost>
2008-01-12 04:49:10 +0000

    Implement Delete Method button in Edit Methods dialog

---
+++ source/org/conservationmeasures/eam/views/umbrella/doers/DeleteMethodDoer.java
@@ -0,0 +1,45 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.views.umbrella.doers;
+
+import org.conservationmeasures.eam.exceptions.CommandFailedException;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.Task;
+import org.conservationmeasures.eam.views.ObjectsDoer;
+import org.conservationmeasures.eam.views.umbrella.DeleteActivity;
+
+public class DeleteMethodDoer extends ObjectsDoer
+{
+	public boolean isAvailable()
+	{
+		if(getSelectedMethod() == null)
+			return false;
+		
+		return true;
+	}
+
+	public void doIt() throws CommandFailedException
+	{
+		if(!isAvailable())
+			return;
+		
+		DeleteActivity.deleteTaskWithUserConfirmation(getProject(), getSelectionHierarchy(), getSelectedMethod());
+
+	}
+
+	public Task getSelectedMethod()
+	{
+		if(getSelectedHierarchies().length != 1)
+			return null;
+		
+		ORef methodRef = getSelectedHierarchies()[0].getRefForType(Task.getObjectType());
+		if(methodRef == null || methodRef.isInvalid())
+			return null;
+		
+		return Task.find(getProject(), methodRef);
+	}
+
+}
