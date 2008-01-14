3ede0fc5fb1e029eabcaeca09daddf17776beece
nimaa <nimaa@localhost>
2008-01-14 00:02:16 +0000

    addd subpanels for indictor properties panel

---
+++ source/org/conservationmeasures/eam/dialogs/diagram/IndicatorSubPanel.java
@@ -0,0 +1,36 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.dialogs.diagram;
+
+import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
+import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
+import org.conservationmeasures.eam.icons.IndicatorIcon;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.project.Project;
+
+public class IndicatorSubPanel extends ObjectDataInputPanel
+{
+	public IndicatorSubPanel(Project projectToUse, ORef orefToUse)
+	{
+		super(projectToUse, orefToUse);
+
+		ObjectDataInputField shortLabelField = createStringField(Indicator.getObjectType(), Indicator.TAG_SHORT_LABEL,10);
+		ObjectDataInputField labelField = createStringField(Indicator.getObjectType(), Indicator.TAG_LABEL);
+		addFieldsOnOneLine(EAM.text("Indicator"), new IndicatorIcon(), new ObjectDataInputField[]{shortLabelField, labelField,});
+
+		addField(createMultilineField(Indicator.getObjectType(), Indicator.TAG_DETAIL));
+		addField(createMultilineField(Indicator.getObjectType(), Indicator.TAG_COMMENT));
+		
+		updateFieldsFromProject();
+	}
+
+	public String getPanelDescription()
+	{
+		return EAM.text("Indicator");
+	}
+}
--- source/org/conservationmeasures/eam/dialogs/indicator/IndicatorPropertiesPanel.java
+++ source/org/conservationmeasures/eam/dialogs/indicator/IndicatorPropertiesPanel.java
@@ -5,23 +5,17 @@
 */ 
 package org.conservationmeasures.eam.dialogs.indicator;
 
-import org.conservationmeasures.eam.actions.ActionEditMethods;
-import org.conservationmeasures.eam.actions.ObjectsAction;
-import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
-import org.conservationmeasures.eam.dialogs.fieldComponents.PanelButton;
-import org.conservationmeasures.eam.icons.IndicatorIcon;
+import org.conservationmeasures.eam.dialogs.diagram.IndicatorSubPanel;
+import org.conservationmeasures.eam.dialogs.viability.IndicatorMonitoringPlanSubPanel;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.ids.IndicatorId;
+import org.conservationmeasures.eam.layout.OneColumnGridLayout;
 import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.Indicator;
-import org.conservationmeasures.eam.objects.Measurement;
 import org.conservationmeasures.eam.project.Project;
-import org.conservationmeasures.eam.questions.IndicatorStatusRatingQuestion;
-import org.conservationmeasures.eam.questions.PriorityRatingQuestion;
-import org.conservationmeasures.eam.questions.TrendQuestion;
-import org.conservationmeasures.eam.utils.ObjectsActionButton;
 
 public class IndicatorPropertiesPanel extends ObjectDataInputPanel
 {
@@ -33,33 +27,10 @@ public class IndicatorPropertiesPanel extends ObjectDataInputPanel
 	public IndicatorPropertiesPanel(Project projectToUse, IndicatorId idToShow) throws Exception
 	{
 		super(projectToUse, ObjectType.INDICATOR, idToShow);
-
-		ObjectDataInputField shortLabelField = createStringField(Indicator.getObjectType(), Indicator.TAG_SHORT_LABEL,10);
-		ObjectDataInputField labelField = createStringField(Indicator.getObjectType(), Indicator.TAG_LABEL);
-		addFieldsOnOneLine(EAM.text("Indicator"), new IndicatorIcon(), new ObjectDataInputField[]{shortLabelField, labelField,});
-
-		addField(createMultilineField(Indicator.getObjectType(), Indicator.TAG_DETAIL));
-		addField(createMultilineField(Indicator.getObjectType(), Indicator.TAG_COMMENT));
+		setLayout(new OneColumnGridLayout());
 		
-		//FIXME finish layout
-		
-		
-		
-		addField(createRatingChoiceField(new PriorityRatingQuestion(Indicator.TAG_PRIORITY)));
-		addField(createRatingChoiceField(new IndicatorStatusRatingQuestion(Indicator.TAG_STATUS)));
-		
-		ObjectsAction editMethods = EAM.getMainWindow().getActions().getObjectsAction(ActionEditMethods.class);
-		PanelButton editMethodsButton = new ObjectsActionButton(editMethods, getPicker());
-		addFieldWithEditButton(EAM.text("Label|Methods"), createReadonlyTextField(Indicator.PSEUDO_TAG_METHODS), editMethodsButton);
-		addField(createReadonlyTextField(Indicator.PSEUDO_TAG_FACTOR));
-		addField(createReadonlyTextField(Indicator.PSEUDO_TAG_STRATEGIES));
-		addField(createReadonlyTextField(Indicator.PSEUDO_TAG_DIRECT_THREATS));
-		addField(createReadonlyTextField(Indicator.PSEUDO_TAG_TARGETS));
-		
-		addField(createDateChooserField(Measurement.getObjectType(), Measurement.TAG_DATE));
-		addField(createStringField(Measurement.getObjectType(), Measurement.TAG_SUMMARY));
-		addField(createMultilineField(Measurement.getObjectType(), Measurement.TAG_DETAIL));
-		addField(createIconChoiceField(Measurement.getObjectType(), new TrendQuestion(Measurement.TAG_TREND)));
+		addSubPanelWithTitledBorder(new IndicatorSubPanel(projectToUse, new ORef(Indicator.getObjectType(), idToShow)));
+		addSubPanelWithTitledBorder(new IndicatorMonitoringPlanSubPanel(projectToUse, new ORef(Indicator.getObjectType(), idToShow)));
 				
 		updateFieldsFromProject();
 	}
@@ -68,5 +39,4 @@ public class IndicatorPropertiesPanel extends ObjectDataInputPanel
 	{
 		return EAM.text("Title|Indicator Properties");
 	}
-
 }
