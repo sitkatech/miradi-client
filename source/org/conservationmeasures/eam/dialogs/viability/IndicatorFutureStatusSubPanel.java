080d61f1fdc9f5c6cd4c9958964e0b77e73ea734
nimaa <nimaa@localhost>
2008-01-17 23:13:45 +0000

    hiding status rating field for children node with non kea parents

--- source/org/conservationmeasures/eam/dialogs/viability/IndicatorFutureStatusSubPanel.java
+++ source/org/conservationmeasures/eam/dialogs/viability/IndicatorFutureStatusSubPanel.java
@@ -7,12 +7,15 @@ package org.conservationmeasures.eam.dialogs.viability;
 
 import org.conservationmeasures.eam.dialogfields.ObjectDataInputField;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
-import org.conservationmeasures.eam.icons.GoalIcon;
+import org.conservationmeasures.eam.dialogs.fieldComponents.PanelFieldLabel;
+import org.conservationmeasures.eam.dialogs.fieldComponents.PanelTitleLabel;
 import org.conservationmeasures.eam.ids.BaseId;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
+import org.conservationmeasures.eam.objecthelpers.ORefList;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.Indicator;
+import org.conservationmeasures.eam.objects.KeyEcologicalAttribute;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.questions.StatusQuestion;
 
@@ -28,18 +31,45 @@ public class IndicatorFutureStatusSubPanel extends ObjectDataInputPanel
 		super(projectToUse, orefToUse);
 
 		ObjectDataInputField futureStatusDateField = createDateChooserField(ObjectType.INDICATOR,  Indicator.TAG_FUTURE_STATUS_DATE);
+		PanelTitleLabel futureStatusDateLabelField = new PanelFieldLabel(futureStatusDateField.getObjectType(), futureStatusDateField.getTag());
+		
 		ObjectDataInputField futureStatusSummaryField = createStringField(ObjectType.INDICATOR,  Indicator.TAG_FUTURE_STATUS_SUMMARY);
-		ObjectDataInputField futureStatusRatingField = createRatingChoiceField(ObjectType.INDICATOR, new StatusQuestion(Indicator.TAG_FUTURE_STATUS_RATING));
-		addFieldsOnOneLine(EAM.text("Desired Status"), new GoalIcon(), new ObjectDataInputField[]{futureStatusDateField, futureStatusSummaryField, futureStatusRatingField});
+		PanelTitleLabel futureStatusSummaryLabelField = new PanelFieldLabel(futureStatusSummaryField.getObjectType(), futureStatusSummaryField.getTag());
+
+		futureStatusRatingField = createRatingChoiceField(ObjectType.INDICATOR, new StatusQuestion(Indicator.TAG_FUTURE_STATUS_RATING));
+		futureStatusRatingLabelField = new PanelFieldLabel(futureStatusRatingField.getObjectType(), futureStatusRatingField.getTag());
+		
+		addFieldsOnOneLine(EAM.text("Desired Status"), new Object[]{futureStatusDateField, futureStatusDateLabelField, 
+																	futureStatusSummaryField, futureStatusSummaryLabelField, 
+																	futureStatusRatingField, futureStatusRatingLabelField});
 
 		addField(createMultilineField(Indicator.getObjectType(), Indicator.TAG_FUTURE_STATUS_DETAIL));
 		addField(createMultilineField(Indicator.getObjectType(), Indicator.TAG_FUTURE_STATUS_COMMENT));
 		
 		updateFieldsFromProject();
 	}
+	
+	public void setObjectRefs(ORef[] orefsToUse)
+	{
+		super.setObjectRefs(orefsToUse);
+	
+		updateVisibility(true);
+		ORef foundRef = new ORefList(orefsToUse).getRefForType(KeyEcologicalAttribute.getObjectType());
+		if (foundRef.isInvalid())
+			updateVisibility(false);
+	}
+
+	private void updateVisibility(boolean isVisible)
+	{
+		futureStatusRatingLabelField.setVisible(isVisible);
+		futureStatusRatingField.setVisible(isVisible);
+	}
 
 	public String getPanelDescription()
 	{
 		return EAM.text("Desired Future Status");
 	}
+	
+	private ObjectDataInputField futureStatusRatingField;
+	private PanelTitleLabel futureStatusRatingLabelField ;
 }
