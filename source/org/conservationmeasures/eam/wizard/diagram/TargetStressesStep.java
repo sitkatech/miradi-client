6fde8b7443cb2c4dc3ae45fdd020198ae9285c06
kevins <kevins@localhost>
2008-01-17 22:36:33 +0000

    Insert new stub step for Target Stresses

---
+++ source/org/conservationmeasures/eam/wizard/diagram/TargetStressesStep.java
@@ -0,0 +1,18 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.wizard.diagram;
+
+import org.conservationmeasures.eam.wizard.DiagramWizardStep;
+import org.conservationmeasures.eam.wizard.WizardPanel;
+
+public class TargetStressesStep extends DiagramWizardStep
+{
+	public TargetStressesStep(WizardPanel wizardToUse)
+	{
+		super(wizardToUse);
+	}
+
+}
