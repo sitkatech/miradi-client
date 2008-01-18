65f26701b3584c4f9334925af079aa0b141dd7fe
kevins <kevins@localhost>
2008-01-18 21:19:52 +0000

    Update to new 2008 copyright with FoS instead of WCS

--- source/VerifyCopyrights.rb
+++ source/VerifyCopyrights.rb
@@ -1,9 +1,9 @@
 #!/bin/env ruby
 
 $COPYRIGHT = [
-' Copyright 2005-2007, Wildlife Conservation Society,',
-' Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and',
-' Beneficent Technology, Inc. ("Benetech"), Palo Alto, California.',
+'Copyright 2005-2008, Foundations of Success, Bethesda, Maryland',
+'(on behalf of the Conservation Measures Partnership, "CMP") and',
+'Beneficent Technology, Inc. ("Benetech"), Palo Alto, California.',
 ]
 
 def verify_tree(directory)
