621f24c8af71af3a8d99ec382e7cfd5fc603b3d1
kevins <kevins@localhost>
2008-01-19 23:57:34 +0000

    Set headings correctly for new wizard step

---
+++ source/org/conservationmeasures/eam/actions/jump/ActionJumpIdentifyStresses.java
@@ -0,0 +1,24 @@
+/* 
+* Copyright 2005-2007, Wildlife Conservation Society, 
+* Bronx, New York (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.actions.jump;
+
+import org.conservationmeasures.eam.actions.MainWindowAction;
+import org.conservationmeasures.eam.main.EAM;
+import org.conservationmeasures.eam.main.MainWindow;
+
+public class ActionJumpIdentifyStresses extends MainWindowAction
+{
+	public ActionJumpIdentifyStresses(MainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse, getLabel());
+	}
+	
+	private static String getLabel()
+	{
+		return EAM.text("Identify Stresses (optional)");
+	}
+
+}
