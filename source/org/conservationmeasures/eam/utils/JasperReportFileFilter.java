8425b4f6de2a40f2486add042d61c8dca05dc29b
nimaa <nimaa@localhost>
2008-01-29 16:49:46 +0000

    new cutom report button to bring up file chooser

---
+++ source/org/conservationmeasures/eam/utils/JasperReportFileFilter.java
@@ -0,0 +1,36 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.conservationmeasures.eam.utils;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+import org.conservationmeasures.eam.main.EAM;
+
+public class JasperReportFileFilter extends FileFilter
+{
+	//TODO,  this method should be pulled up since its dup code 
+	public boolean accept(File pathname)
+	{
+		if (pathname.isDirectory())
+			return true;
+		
+		return (pathname.getName().toLowerCase().endsWith(EXTENSION));
+	}
+
+	public String getDescription()
+	{
+		return EAM.text("FileFilter|JASPER (*.jasper)");
+	}
+	
+	public String getFileExtension()
+	{
+		return EXTENSION;
+	}
+
+	public static final String EXTENSION = ".jasper";
+}
