bae71e5238a839c0b288c33cd1ffc0085bdd4c74
nimaa <nimaa@localhost>
2008-02-06 22:51:13 +0000

    removed ununsed ratingsFieldTable

--- source/org/conservationmeasures/eam/dialogs/viability/IndicatorViabilityRatingsSubPanel.java
+++ source/org/conservationmeasures/eam/dialogs/viability/IndicatorViabilityRatingsSubPanel.java
@@ -8,15 +8,12 @@ package org.conservationmeasures.eam.dialogs.viability;
 import java.awt.Color;
 import java.awt.Font;
 
-import org.conservationmeasures.eam.dialogfields.ViabilityRatingsTableField;
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
 import org.conservationmeasures.eam.dialogs.fieldComponents.PanelTitleLabel;
-import org.conservationmeasures.eam.main.CommandExecutedEvent;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objecthelpers.ORef;
 import org.conservationmeasures.eam.objecthelpers.ObjectType;
 import org.conservationmeasures.eam.objects.Indicator;
-import org.conservationmeasures.eam.objects.Measurement;
 import org.conservationmeasures.eam.project.Project;
 import org.conservationmeasures.eam.questions.ChoiceQuestion;
 import org.conservationmeasures.eam.questions.RatingSourceQuestion;
@@ -48,33 +45,8 @@ public class IndicatorViabilityRatingsSubPanel extends ObjectDataInputPanel
 		addFieldWithCustomLabel(createStringMapField(Indicator.getObjectType(), Indicator.TAG_INDICATOR_THRESHOLD, code), label);
 	}
 
-	//FIXME remove field and class
-	public ViabilityRatingsTableField createViabilityRatingsTableField(int objectType, String tagToUse, ChoiceQuestion question)
-	{
-		return new ViabilityRatingsTableField(getProject(), objectType, getObjectIdForType(objectType), tagToUse, question);
-	}	
-
 	public String getPanelDescription()
 	{
 		return EAM.text("Viability Ratings");
 	}
-	
-	public void commandExecuted(CommandExecutedEvent event)
-	{
-		super.commandExecuted(event);
-		final boolean areIndicatorMeasurementFields = 
-			event.isSetDataCommandWithThisTypeAndTag(ObjectType.MEASUREMENT,Measurement.TAG_SUMMARY) ||
-			event.isSetDataCommandWithThisTypeAndTag(ObjectType.MEASUREMENT,Measurement.TAG_STATUS) ||
-			event.isSetDataCommandWithThisTypeAndTag(ObjectType.MEASUREMENT,Measurement.TAG_TREND) ||
-			
-			event.isSetDataCommandWithThisTypeAndTag(ObjectType.INDICATOR,Indicator.TAG_FUTURE_STATUS_SUMMARY) ||
-			event.isSetDataCommandWithThisTypeAndTag(ObjectType.INDICATOR,Indicator.TAG_FUTURE_STATUS_RATING);
-		
-		if (areIndicatorMeasurementFields)
-		{
-			ratingThresholdTable.dataHasChanged();
-		}
-	}
-	
-	private ViabilityRatingsTableField ratingThresholdTable;
 }
