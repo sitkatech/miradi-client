6d31bfa4909c79b88ea61bcb2cf6435a6b5ec74f
nimaa <nimaa@localhost>
2008-02-07 00:37:01 +0000

    added fields to wcpa project data and tab

--- source/org/conservationmeasures/eam/views/summary/WcpaSummaryPanel.java
+++ source/org/conservationmeasures/eam/views/summary/WcpaSummaryPanel.java
@@ -6,7 +6,6 @@
 package org.conservationmeasures.eam.views.summary;
 
 import org.conservationmeasures.eam.dialogs.base.ObjectDataInputPanel;
-import org.conservationmeasures.eam.layout.OneColumnGridLayout;
 import org.conservationmeasures.eam.main.EAM;
 import org.conservationmeasures.eam.objects.WcpaProjectData;
 import org.conservationmeasures.eam.project.Project;
@@ -17,8 +16,18 @@ public class WcpaSummaryPanel extends ObjectDataInputPanel
 	{
 		super(projectToUse, projectToUse.getSingletonObjectRef(WcpaProjectData.getObjectType()));
 		
-		setLayout(new OneColumnGridLayout());
-		
+		addField(createStringField(WcpaProjectData.TAG_LEGAL_STATUS));
+		addField(createStringField(WcpaProjectData.TAG_LEGISLATIVE));
+		addField(createStringField(WcpaProjectData.TAG_PHYSICAL_DESCRIPTION));
+		addField(createStringField(WcpaProjectData.TAG_BIOLOGICAL_DESCRIPTION));
+		addField(createStringField(WcpaProjectData.TAG_SOCIO_ECONOMIC_INFORMATION));
+		addField(createStringField(WcpaProjectData.TAG_HISTORICAL_DESCRIPTION));
+		addField(createStringField(WcpaProjectData.TAG_CULTURAL_DESCRIPTION));
+		addField(createStringField(WcpaProjectData.TAG_ACCESS_INFORMATION));
+		addField(createStringField(WcpaProjectData.TAG_VISITATION_INFORMATION));
+		addField(createStringField(WcpaProjectData.TAG_CURRENT_LAND_USES));
+		addField(createStringField(WcpaProjectData.TAG_MANAGEMENT_RESOURCES));				
+						
 		updateFieldsFromProject();
 	}
 
