1f03235572097ff3cb11241b3f05da78b681b586
kevins <kevins@localhost>
2008-02-15 22:36:58 +0000

    Rename WCPA to PAs, and add mirror of IUCN prot area codes

--- source/org/miradi/views/summary/WcpaSummaryPanel.java
+++ source/org/miradi/views/summary/WcpaSummaryPanel.java
@@ -5,16 +5,24 @@
 */ 
 package org.miradi.views.summary;
 
+import org.miradi.dialogfields.ObjectDataInputField;
 import org.miradi.dialogs.base.ObjectDataInputPanel;
 import org.miradi.main.EAM;
+import org.miradi.objects.ProjectMetadata;
 import org.miradi.objects.WcpaProjectData;
 import org.miradi.project.Project;
+import org.miradi.questions.ProtectedAreaCategoryQuestion;
 
 public class WcpaSummaryPanel extends ObjectDataInputPanel
 {
 	public WcpaSummaryPanel(Project projectToUse)
 	{
 		super(projectToUse, projectToUse.getSingletonObjectRef(WcpaProjectData.getObjectType()));
+
+		// NOTE: Mirror from Scope panel
+		ObjectDataInputField protectedAreaStatusField = createMultiCodeField(ProjectMetadata.getObjectType(), ProjectMetadata.TAG_PROTECTED_AREA_CATEGORIES, new ProtectedAreaCategoryQuestion(), 1);
+		ObjectDataInputField protectedAreaStatusNotesField = createMultilineField(ProjectMetadata.getObjectType(), ProjectMetadata.TAG_PROTECTED_AREA_CATEGORY_NOTES, 25);
+		addFieldsOnOneLine(EAM.text("Label|Protected Area Categories"), new ObjectDataInputField[]{protectedAreaStatusField, protectedAreaStatusNotesField});
 		
 		addField(createMultilineField(WcpaProjectData.TAG_LEGAL_STATUS));
 		addField(createMultilineField(WcpaProjectData.TAG_LEGISLATIVE));
@@ -33,6 +41,6 @@ public class WcpaSummaryPanel extends ObjectDataInputPanel
 
 	public String getPanelDescription()
 	{
-		return EAM.text("Label|WCPA");
+		return EAM.text("Label|PAs");
 	}
 }
