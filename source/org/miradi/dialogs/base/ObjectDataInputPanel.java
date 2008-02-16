7c046bc3383e5c11ffee1bfae32170379888f097
kevins <kevins@localhost>
2008-02-16 21:38:37 +0000

    Fix spacing of multi-field rows like FS date/parameter

--- source/org/miradi/dialogs/base/ObjectDataInputPanel.java
+++ source/org/miradi/dialogs/base/ObjectDataInputPanel.java
@@ -113,7 +113,8 @@ abstract public class ObjectDataInputPanel extends AbstractObjectDataInputPanel
 	// the label visibilty is changed outside of here.  
 	protected void addFieldsOnOneLine(String label, Object[] components)
 	{
-		JPanel fieldPanel = new OneRowPanel();
+		OneRowPanel fieldPanel = new OneRowPanel();
+		fieldPanel.setGaps(3);
 		fieldPanel.setBackground(AppPreferences.getDataPanelBackgroundColor());
 		for(int i = 0; i < components.length; i+=2)
 		{
