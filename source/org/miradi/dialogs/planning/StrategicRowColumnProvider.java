1838a0538bfe3169b240f326f78b48c9bf0fedbf
kevins <kevins@localhost>
2008-02-17 23:43:54 +0000

    Add Target row, remove priority and classification columns

--- source/org/miradi/dialogs/planning/StrategicRowColumnProvider.java
+++ source/org/miradi/dialogs/planning/StrategicRowColumnProvider.java
@@ -12,6 +12,7 @@ import org.miradi.objects.Goal;
 import org.miradi.objects.Objective;
 import org.miradi.objects.ResultsChainDiagram;
 import org.miradi.objects.Strategy;
+import org.miradi.objects.Target;
 import org.miradi.utils.CodeList;
 import org.miradi.views.planning.PlanningView;
 
@@ -20,10 +21,10 @@ public class StrategicRowColumnProvider implements RowColumnProvider
 	public CodeList getColumnListToShow()
 	{
 		return new CodeList(new String[] {
-							Strategy.PSEUDO_TAG_RATING_SUMMARY,
+//							Strategy.PSEUDO_TAG_RATING_SUMMARY,
 							Desire.PSEUDO_TAG_FACTOR,
 							BaseObject.PSEUDO_TAG_BUDGET_TOTAL,
-							Strategy.PSEUDO_TAG_TAXONOMY_CODE_VALUE,
+//							Strategy.PSEUDO_TAG_TAXONOMY_CODE_VALUE,
 //							Strategy.PSEUDO_TAG_PROGRESS,
 //							Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML,
 //							Task.PSEUDO_TAG_COMBINED_EFFORT_DATES,
@@ -36,6 +37,7 @@ public class StrategicRowColumnProvider implements RowColumnProvider
 		return new CodeList(new String[] {
 							ConceptualModelDiagram.OBJECT_NAME,
 							ResultsChainDiagram.OBJECT_NAME,
+							Target.OBJECT_NAME,
 							Goal.OBJECT_NAME,
 							Objective.OBJECT_NAME,
 							Strategy.OBJECT_NAME,});
