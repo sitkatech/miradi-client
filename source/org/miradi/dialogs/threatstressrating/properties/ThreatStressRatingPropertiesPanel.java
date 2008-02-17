9e86e0be11f6b74be2f52408ec31087cdf6bbbd0
kevins <kevins@localhost>
2008-02-17 21:59:13 +0000

    Stress link properties now uses Threat/Target icons and correct labels

--- source/org/miradi/dialogs/threatstressrating/properties/ThreatStressRatingPropertiesPanel.java
+++ source/org/miradi/dialogs/threatstressrating/properties/ThreatStressRatingPropertiesPanel.java
@@ -28,7 +28,7 @@ public class ThreatStressRatingPropertiesPanel extends ObjectDataInputPanel
 		super(mainWindowToUse.getProject(), ObjectType.THREAT_STRESS_RATING, BaseId.INVALID);
 		setLayout(new BasicGridLayout(2, 1));
 		
-		threatStressRatingFieldPanel = new ThreatStressRatingFieldPanel(mainWindowToUse.getProject(), ORef.INVALID); 
+		threatStressRatingFieldPanel = new ThreatStressRatingFieldPanel(mainWindowToUse.getProject(), objectPickerToUse); 
 		editorComponent = new ThreatStressRatingEditorComponent(mainWindowToUse, objectPickerToUse);
 		add(threatStressRatingFieldPanel);
 		add(editorComponent);
