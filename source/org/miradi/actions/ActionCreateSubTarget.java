34633880792f6e77943d156dad250a2016e1e94d
kevins <kevins@localhost>
2008-02-17 00:17:37 +0000

    Cleanup nested targets tab

--- source/org/miradi/actions/ActionCreateSubTarget.java
+++ source/org/miradi/actions/ActionCreateSubTarget.java
@@ -5,6 +5,7 @@
 */ 
 package org.miradi.actions;
 
+import org.miradi.icons.SubTargetIcon;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 
@@ -12,7 +13,7 @@ public class ActionCreateSubTarget extends ObjectsAction
 {
 	public ActionCreateSubTarget(MainWindow mainWindowToUse)
 	{
-		super(mainWindowToUse, getLabel());
+		super(mainWindowToUse, getLabel(), new SubTargetIcon());
 	}
 
 	private static String getLabel()
