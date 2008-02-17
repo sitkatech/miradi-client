60686cb570d715a0be8f8950fef15e1e411bdef6
kevins <kevins@localhost>
2008-02-17 23:13:09 +0000

    Keep Threat/Target fields properly updated as you click on different Threat View cells

--- source/org/miradi/dialogs/threatstressrating/properties/ThreatStressRatingFieldPanel.java
+++ source/org/miradi/dialogs/threatstressrating/properties/ThreatStressRatingFieldPanel.java
@@ -9,34 +9,28 @@ import org.miradi.diagram.factortypes.FactorType;
 import org.miradi.dialogfields.ObjectDataInputField;
 import org.miradi.dialogs.base.ObjectDataInputPanel;
 import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
+import org.miradi.main.EAM;
 import org.miradi.objectdata.BooleanData;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
+import org.miradi.objecthelpers.ObjectType;
 import org.miradi.objects.Factor;
 import org.miradi.objects.FactorLink;
 import org.miradi.project.Project;
 import org.miradi.questions.ThreatRatingQuestion;
-import org.miradi.views.umbrella.ObjectPicker;
 
 public class ThreatStressRatingFieldPanel extends ObjectDataInputPanel
 {
-	public ThreatStressRatingFieldPanel(Project projectToUse, ObjectPicker picker) throws Exception
+	public ThreatStressRatingFieldPanel(Project projectToUse) throws Exception
 	{
 		super(projectToUse, ORef.INVALID);
 		
-		ORefList[] selected = picker.getSelectedHierarchies();
-		System.out.println(selected[0]);
-		ORef linkRef = selected[0].getRefForType(FactorLink.getObjectType());
-		FactorLink link = FactorLink.find(getProject(), linkRef);
-
-		Factor fromFactor = Factor.findFactor(getProject(), link.getFromFactorRef());
-		ObjectDataInputField fromNameField = createReadonlyTextField(fromFactor.getType(), Factor.TAG_LABEL);
-		PanelTitleLabel fromLabel = new PanelTitleLabel(FactorType.getFactorTypeLabel(fromFactor), FactorType.getFactorIcon(fromFactor));
+		fromLabel = new PanelTitleLabel();
+		fromNameField = createReadonlyTextField(ObjectType.FAKE, Factor.TAG_LABEL);
 		addFieldWithCustomLabel(fromNameField, fromLabel);
 
-		Factor toFactor = Factor.findFactor(getProject(), link.getToFactorRef());
-		ObjectDataInputField toNameField = createReadonlyTextField(toFactor.getType(), Factor.TAG_LABEL);
-		PanelTitleLabel toLabel = new PanelTitleLabel(FactorType.getFactorTypeLabel(toFactor), FactorType.getFactorIcon(toFactor));
+		toLabel = new PanelTitleLabel();
+		toNameField = createReadonlyTextField(ObjectType.FAKE, Factor.TAG_LABEL);
 		addFieldWithCustomLabel(toNameField, toLabel);
 
 		addField(createCheckBoxField(FactorLink.getObjectType(), FactorLink.TAG_BIDIRECTIONAL_LINK, BooleanData.BOOLEAN_TRUE, BooleanData.BOOLEAN_FALSE));
@@ -47,9 +41,57 @@ public class ThreatStressRatingFieldPanel extends ObjectDataInputPanel
 
 		updateFieldsFromProject();
 	}
+	
+	@Override
+	public void setObjectRefs(ORef[] orefsToUse)
+	{
+		updateFieldLabels(orefsToUse);
+		super.setObjectRefs(orefsToUse);
+	}
+
+	private void updateFieldLabels(ORef[] orefsToUse)
+	{
+		ORefList refs = new ORefList(orefsToUse);
+		
+		ORef linkRef = refs.getRefForType(FactorLink.getObjectType());
+		if(linkRef.isInvalid())
+		{
+			fromLabel.setText("");
+			fromLabel.setIcon(null);
+			fromNameField.setObjectType(ObjectType.FAKE);
+			toLabel.setText("");
+			toLabel.setIcon(null);
+			toNameField.setObjectType(ObjectType.FAKE);
+			return;
+		}
+		
+		try
+		{
+			FactorLink link = FactorLink.find(getProject(), linkRef);
+
+			Factor fromFactor = Factor.findFactor(getProject(), link.getFromFactorRef());
+			fromLabel.setText(FactorType.getFactorTypeLabel(fromFactor));
+			fromLabel.setIcon(FactorType.getFactorIcon(fromFactor));
+			fromNameField.setObjectType(fromFactor.getType());
+
+			Factor toFactor = Factor.findFactor(getProject(), link.getToFactorRef());
+			toLabel.setText(FactorType.getFactorTypeLabel(toFactor));
+			toLabel.setIcon(FactorType.getFactorIcon(toFactor));
+			toNameField.setObjectType(toFactor.getType());
+		}
+		catch(Exception e)
+		{
+			EAM.panic(e);
+		}
+	}
 
 	public String getPanelDescription()
 	{
 		return "ThreatStressRatingFieldPanel";
 	}
+	
+	private PanelTitleLabel fromLabel;
+	private PanelTitleLabel toLabel;
+	ObjectDataInputField fromNameField;
+	ObjectDataInputField toNameField;
 }
