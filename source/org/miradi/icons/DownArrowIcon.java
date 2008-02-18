135f07047dd0b6b32226529c677ca41b7d850e78
kevins <kevins@localhost>
2008-02-18 16:43:41 +0000

    Add more icons to buttons

---
+++ source/org/miradi/icons/DownArrowIcon.java
@@ -0,0 +1,17 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.icons;
+
+import org.miradi.utils.MiradiResourceImageIcon;
+
+public class DownArrowIcon extends MiradiResourceImageIcon
+{
+	public DownArrowIcon()
+	{
+		super("icons/DownArrow.png");
+	}
+
+}
---
+++ source/org/miradi/icons/UpArrowIcon.java
@@ -0,0 +1,18 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.icons;
+
+import org.miradi.utils.MiradiResourceImageIcon;
+
+public class UpArrowIcon extends MiradiResourceImageIcon
+{
+	public UpArrowIcon()
+	{
+		super("icons/UpArrow.png");
+	}
+
+
+}
