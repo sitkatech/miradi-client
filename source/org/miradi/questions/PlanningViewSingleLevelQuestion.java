1b44bfdb9756f4929e87f40e26ad220ed351a782
kevins <kevins@localhost>
2008-02-21 16:18:28 +0000

    Remove -Only entries that aren't helpful

--- source/org/miradi/questions/PlanningViewSingleLevelQuestion.java
+++ source/org/miradi/questions/PlanningViewSingleLevelQuestion.java
@@ -6,15 +6,12 @@
 package org.miradi.questions;
 
 import org.miradi.main.EAM;
-import org.miradi.objects.Cause;
 import org.miradi.objects.Goal;
 import org.miradi.objects.Indicator;
 import org.miradi.objects.Measurement;
 import org.miradi.objects.Objective;
 import org.miradi.objects.Strategy;
-import org.miradi.objects.Target;
 import org.miradi.objects.Task;
-import org.miradi.objects.ThreatReductionResult;
 
 public class PlanningViewSingleLevelQuestion extends StaticChoiceQuestion
 {
@@ -26,16 +23,12 @@ public class PlanningViewSingleLevelQuestion extends StaticChoiceQuestion
 	private static ChoiceItem[] getSingleLevelChoices()
 	{
 		return new ChoiceItem[] {
-				new ChoiceItem(Target.OBJECT_NAME, EAM.text("Targets Only")),
 				new ChoiceItem(Goal.OBJECT_NAME, EAM.text("Goals Only")),
-				new ChoiceItem(Cause.OBJECT_NAME_THREAT, EAM.text("Direct Threats Only")),
-				new ChoiceItem(ThreatReductionResult.OBJECT_NAME, EAM.text("Threat Reduction Results Only")),
 				new ChoiceItem(Objective.OBJECT_NAME, EAM.text("Objectives Only")),
 				new ChoiceItem(Strategy.OBJECT_NAME, EAM.text("Strategies Only")),
 				new ChoiceItem(Task.ACTIVITY_NAME, EAM.text("Activities Only")),
 				new ChoiceItem(Indicator.OBJECT_NAME, EAM.text("Indicators Only")),
 				new ChoiceItem(Task.METHOD_NAME, EAM.text("Methods Only")),
-				new ChoiceItem(Task.OBJECT_NAME, EAM.text("Tasks Only")),
 				new ChoiceItem(Measurement.OBJECT_NAME, EAM.text("Measurements Only")),
 		};
 	}
