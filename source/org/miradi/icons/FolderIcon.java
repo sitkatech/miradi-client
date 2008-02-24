f5dee5827759202a94e8960a42c9dd18b9f61708
kevins <kevins@localhost>
2008-02-24 18:39:13 +0000

    Non-project empty folders now have folder icon instead of miradi icon

---
+++ source/org/miradi/icons/FolderIcon.java
@@ -0,0 +1,17 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.icons;
+
+import org.miradi.utils.MiradiResourceImageIcon;
+
+public class FolderIcon extends MiradiResourceImageIcon
+{
+	public FolderIcon()
+	{
+		super("icons/open.gif");
+	}
+
+}
