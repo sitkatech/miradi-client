80ab49c501fbb9a0df0622e263672f41e2fda1e5
kevins <kevins@localhost>
2008-02-28 18:03:43 +0000

    Switch Splash/About to use Java logo header instead of HTML

---
+++ source/org/miradi/views/umbrella/HelpAboutPanel.java
@@ -0,0 +1,27 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.views.umbrella;
+
+import javax.swing.JComponent;
+
+import org.miradi.main.EAM;
+import org.miradi.main.MainWindow;
+import org.miradi.main.MiradiLogoPanel;
+import org.miradi.utils.HtmlViewPanel;
+
+public class HelpAboutPanel extends HtmlViewPanel
+{
+	public HelpAboutPanel(MainWindow mainWindowToUse, String htmlBodyText)
+	{
+		super(mainWindowToUse, EAM.text("Title|About Miradi"), htmlBodyText, 900);
+	}
+	
+	@Override
+	protected JComponent createTopPanel()
+	{
+		return new MiradiLogoPanel();
+	}
+}
