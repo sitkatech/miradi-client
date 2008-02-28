2adf5ffdd77de99407beb3fc14df0bca6cd07c06
kevins <kevins@localhost>
2008-02-28 01:14:22 +0000

    Add icon, and add text above org table

--- source/org/miradi/dialogs/organization/OrganizationPoolTablePanel.java
+++ source/org/miradi/dialogs/organization/OrganizationPoolTablePanel.java
@@ -9,13 +9,19 @@ import org.miradi.actions.ActionCreateOrganization;
 import org.miradi.actions.ActionDeleteOrganization;
 import org.miradi.actions.Actions;
 import org.miradi.dialogs.base.ObjectTablePanelWithCreateAndDelete;
+import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
+import org.miradi.main.EAM;
 import org.miradi.project.Project;
+import org.miradi.resources.ResourcesHandler;
 
 public class OrganizationPoolTablePanel extends ObjectTablePanelWithCreateAndDelete
 {
-	public OrganizationPoolTablePanel(Project project, Actions actions)
+	public OrganizationPoolTablePanel(Project project, Actions actions) throws Exception
 	{
 		super(project, new OrganizationPoolTable(new OrganizationPoolTableModel(project)), actions, buttons);
+		
+		String html = EAM.loadResourceFile(ResourcesHandler.class, "OtherOrgOverview.html");
+		addAboveTable(new PanelTitleLabel(html));
 	}
 	
 	static Class[] buttons = new Class[] {
