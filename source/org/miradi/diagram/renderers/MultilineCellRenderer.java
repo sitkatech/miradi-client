164c75f00d997f8b3c4b1f98cf1531c9f83c3a62
kevins <kevins@localhost>
2008-02-29 19:10:28 +0000

    Remove new Scope box horns

--- source/org/miradi/diagram/renderers/MultilineCellRenderer.java
+++ source/org/miradi/diagram/renderers/MultilineCellRenderer.java
@@ -350,7 +350,7 @@ public class MultilineCellRenderer extends JComponent implements CellViewRendere
 	public static final int INDICATOR_WIDTH = 20;
 	public static final int INDICATOR_HEIGHT = 20;
 	public static final int ANNOTATIONS_HEIGHT = 20;
-	private static final int CORNER_SIZE = 20;
+	protected static final int CORNER_SIZE = 20;
 	private static final int HANDLE_SIZE = 4;
 
 	private static final Color LIGHT_BLUE = new Color(204,238,255);
