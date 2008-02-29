7172570dec484b531793b6e34d73f35bc2663eb1
kevins <kevins@localhost>
2008-02-29 00:00:41 +0000

    Change shape of scope icon

--- source/org/miradi/icons/ProjectScopeIcon.java
+++ source/org/miradi/icons/ProjectScopeIcon.java
@@ -6,21 +6,31 @@
 package org.miradi.icons;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
-import org.miradi.diagram.renderers.FactorRenderer;
-import org.miradi.diagram.renderers.RoundRectangleRenderer;
 import org.miradi.main.AppPreferences;
 import org.miradi.main.EAM;
 
-public class ProjectScopeIcon extends AbstractShapeIcon
+public class ProjectScopeIcon extends AbstractMiradiIcon
 {
-	FactorRenderer getRenderer()
+	public void paintIcon(Component c, Graphics g, int x, int y)
 	{
-		return new RoundRectangleRenderer();
-	}
-	
-	Color getIconColor()
-	{
-		return EAM.getMainWindow().getColorPreference(AppPreferences.TAG_COLOR_SCOPE);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		g.setColor(EAM.getMainWindow().getColorPreference(AppPreferences.TAG_COLOR_SCOPE));
+		int rectWidth = getIconWidth() * 4 / 5;
+		int rectHeight = getIconHeight() * 7 / 8;
+		int left = x + (getIconWidth() - rectWidth)/2;
+		int top = y + (getIconHeight() - rectHeight)/2;
+		int arcWidth = getIconWidth()/2;
+		int arcHeight = getIconHeight()/2;
+		g.fillRoundRect(left, top, rectWidth, rectHeight, arcWidth, arcHeight);
+		g.setColor(Color.BLACK);
+		g.drawRoundRect(left, top, rectWidth, rectHeight, arcWidth, arcHeight);
 	}
 }
