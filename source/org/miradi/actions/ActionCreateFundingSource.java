024f30dd9a8669b04bb33803520b9d59929ed268
kevins <kevins@localhost>
2008-02-29 00:41:21 +0000

    Add icons

--- source/org/miradi/actions/ActionCreateFundingSource.java
+++ source/org/miradi/actions/ActionCreateFundingSource.java
@@ -5,6 +5,7 @@
 */ 
 package org.miradi.actions;
 
+import org.miradi.icons.FundingSourceIcon;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 
@@ -12,7 +13,7 @@ public class ActionCreateFundingSource extends ObjectsAction
 {
 	public ActionCreateFundingSource(MainWindow mainWindowToUse)
 	{
-		super(mainWindowToUse, getLabel());
+		super(mainWindowToUse, getLabel(), new FundingSourceIcon());
 	}
 
 	private static String getLabel()
