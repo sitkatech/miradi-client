e1d94e583c14cf84d658ea66f86243cdead03f80
nimaa <nimaa@localhost>
2008-03-06 21:14:37 +0000

    split rollup and estimate into 2 sub panels

---
+++ source/org/miradi/dialogs/diagram/ForecastRollupSubPanel.java
@@ -0,0 +1,27 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.dialogs.diagram;
+
+import org.miradi.dialogs.base.ObjectDataInputPanel;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objects.BaseObject;
+import org.miradi.project.Project;
+
+public class ForecastRollupSubPanel extends ObjectDataInputPanel
+{
+	public ForecastRollupSubPanel(Project projectToUse, ORef initialRef)
+	{
+		super(projectToUse, initialRef);
+		
+		addField(createReadonlyCurrencyField(BaseObject.PSEUDO_TAG_BUDGET_COST_ROLLUP));
+	}
+
+	public String getPanelDescription()
+	{
+		return EAM.text("Rollup Panel");
+	}
+}
