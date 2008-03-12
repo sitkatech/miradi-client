cd1171899b84063177a5c92f3cd85fea74938cd7
nimaa <nimaa@localhost>
2008-03-12 20:10:00 +0000

    no longer setting the for/back ground colors when selected.  vista has white on grayish white.

--- source/org/miradi/dialogs/base/EditableObjectTable.java
+++ source/org/miradi/dialogs/base/EditableObjectTable.java
@@ -208,9 +208,7 @@ abstract public class EditableObjectTable extends TableWithColumnWidthSaver  imp
 
 	    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) 
 	    {
-	        if (isSelected) 
-	        	setColors(table.getSelectionBackground(), table.getSelectionForeground());
-	        else 
+	        if (!isSelected)  
 	        	setColors(table.getBackground(), table.getForeground());
 
 	        setSelectedItem(value);
