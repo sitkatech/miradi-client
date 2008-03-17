a38a5f29ec8d681081d86842a2db6177c5e6ec85
kevins <kevins@localhost>
2008-03-17 01:34:34 +0000

    Finish sectionizing Planning Strategy Properties panel

--- source/org/miradi/dialogs/diagram/StrategyPropertiesPanel.java
+++ source/org/miradi/dialogs/diagram/StrategyPropertiesPanel.java
@@ -8,6 +8,7 @@ package org.miradi.dialogs.diagram;
 
 import org.miradi.dialogs.base.ObjectDataInputPanel;
 import org.miradi.ids.BaseId;
+import org.miradi.layout.OneColumnGridLayout;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 import org.miradi.objecthelpers.ORef;
@@ -17,20 +18,17 @@ public class StrategyPropertiesPanel extends ObjectDataInputPanel
 {
 	public StrategyPropertiesPanel(MainWindow mainWindow)
 	{
-		super(mainWindow.getProject(), Strategy.getObjectType(), BaseId.INVALID);
+		super(mainWindow.getProject(), Strategy.getObjectType());
+		setLayout(new OneColumnGridLayout());
 		
-		addLabel("");
 		addSubPanelWithTitledBorder(new StrategyCoreSubpanel(getProject(), mainWindow.getActions(), Strategy.getObjectType()));
 		
 		ForecastSubPanel budgetSubPanel = new ForecastSubPanel(mainWindow, new ORef(Strategy.getObjectType(), BaseId.INVALID));
-		addSubPanel(budgetSubPanel);
+		addSubPanelWithTitledBorder(budgetSubPanel);
 
-		addLabel(EAM.text("Budget"));
-		addFieldComponent(budgetSubPanel);
-		
-		addLabel("");
 		addSubPanelWithTitledBorder(new RelatedItemsSubpanel(getProject(), Strategy.getObjectType()));
-		addField(createMultilineField(Strategy.TAG_COMMENT));
+		
+		addSubPanelWithTitledBorder(new FactorSummaryCommentsPanel(getProject(), mainWindow.getActions(), Strategy.getObjectType()));
 		
 		updateFieldsFromProject();
 	}
