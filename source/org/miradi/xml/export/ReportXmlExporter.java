cd02f2bf30c190c2597c59a617f64d4b63648cb1
nimaa <nimaa@localhost>
2008-04-01 18:13:03 +0000

    new classes non functioning xml export classes

---
+++ source/org/miradi/xml/export/ReportXmlExporter.java
@@ -0,0 +1,13 @@
+/* 
+* Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+* (on behalf of the Conservation Measures Partnership, "CMP") and 
+* Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+*/ 
+package org.miradi.xml.export;
+
+public class ReportXmlExporter
+{
+	public static void main(String[] args)
+	{	
+	}	
+}
