750a219465220a17c5ddcc52312cdf7aeab8bd50
nimaa <nimaa@localhost>
2008-04-14 18:17:38 +0000

    pulled up utility type methods into parent project class

--- tests/org/miradi/project/ProjectForTesting.java
+++ tests/org/miradi/project/ProjectForTesting.java
@@ -5,14 +5,9 @@
  */
 package org.miradi.project;
 
-import java.io.File;
-import java.util.Vector;
-
-import org.miradi.commands.Command;
 import org.miradi.commands.CommandCreateObject;
 import org.miradi.commands.CommandSetObjectData;
 import org.miradi.database.ProjectServer;
-import org.miradi.diagram.DiagramModel;
 import org.miradi.diagram.cells.FactorCell;
 import org.miradi.diagram.cells.LinkCell;
 import org.miradi.ids.AssignmentId;
@@ -22,18 +17,12 @@ import org.miradi.ids.FactorId;
 import org.miradi.ids.FactorLinkId;
 import org.miradi.ids.IdList;
 import org.miradi.ids.TaskId;
-import org.miradi.main.CommandExecutedEvent;
-import org.miradi.main.CommandExecutedListener;
-import org.miradi.main.EAM;
 import org.miradi.objectdata.BooleanData;
 import org.miradi.objecthelpers.CreateDiagramFactorLinkParameter;
 import org.miradi.objecthelpers.CreateFactorLinkParameter;
 import org.miradi.objecthelpers.ORef;
-import org.miradi.objecthelpers.ORefList;
 import org.miradi.objecthelpers.ObjectType;
-import org.miradi.objectpools.ConceptualModelDiagramPool;
 import org.miradi.objects.BaseObject;
-import org.miradi.objects.ConceptualModelDiagram;
 import org.miradi.objects.DiagramFactor;
 import org.miradi.objects.DiagramLink;
 import org.miradi.objects.DiagramObject;
@@ -41,11 +30,10 @@ import org.miradi.objects.FactorLink;
 import org.miradi.objects.Indicator;
 import org.miradi.objects.Task;
 import org.miradi.utils.PointList;
-import org.miradi.views.diagram.DiagramModelUpdater;
 
 
 
-public class ProjectForTesting extends ProjectWithHelpers implements CommandExecutedListener
+public class ProjectForTesting extends ProjectWithHelpers
 {
 	public ProjectForTesting(String testName) throws Exception
 	{
@@ -55,65 +43,6 @@ public class ProjectForTesting extends ProjectWithHelpers implements CommandExec
 	public ProjectForTesting(ProjectServer server) throws Exception
 	{
 		super(server);
-		String filename = getFilename();
-		addCommandExecutedListener(this);
-		diagramModel = new DiagramModel(this);
-		getTestDatabase().openMemoryDatabase(filename);
-		finishOpening();
-		commandStack = new Vector();
-	}
-	
-	public DiagramModel getDiagramModel()
-	{
-		return diagramModel;
-	}
-	
-	public DiagramObject getDiagramObject()
-	{
-		return getDiagramModel().getDiagramObject();
-	}
-	
-	protected void finishOpening() throws Exception
-	{
-		super.finishOpening();
-		loadDiagram();
-	}
-
-	public void close() throws Exception
-	{
-		super.close();
-		
-		diagramModel = null;
-	}
-
-	protected void setDefaultDiagramPage(int objectType)
-	{
-		//TODO overrode to be empty, so that tests dont fail
-	}
-	
-	//TODO this is fragile, should do a true close
-	public void closeAndReopen() throws Exception
-	{
-		File topDirectory = getDatabase().getTopDirectory();
-		getTestDatabase().closeAndDontDelete();
-		createOrOpen(topDirectory);
-	}
-
-	void fireCommandExecuted(Command command) 
-	{
-		super.fireCommandExecuted(command);
-		if(commandStack != null)
-			commandStack.add(command);
-	}
-
-	Command getLastCommand()
-	{
-		return (Command)commandStack.remove(commandStack.size()-1);
-	}
-	
-	public ProjectServerForTesting getTestDatabase()
-	{
-		return (ProjectServerForTesting)getDatabase();
 	}
 	
 	//TODO come up with a better name or eventualy all creates should return ref
@@ -236,47 +165,6 @@ public class ProjectForTesting extends ProjectWithHelpers implements CommandExec
 		return getDiagramModel().getFactorCellByWrappedId(insertedId);
 	}
 	
-	public void loadDiagram() throws Exception
-	{
-		ConceptualModelDiagramPool diagramContentsPool = (ConceptualModelDiagramPool) getPool(ObjectType.CONCEPTUAL_MODEL_DIAGRAM);
-		ORefList oRefs = diagramContentsPool.getORefList();
-		ConceptualModelDiagram diagramContentsObject = getDiagramContentsObject(oRefs);
-		getDiagramModel().fillFrom(diagramContentsObject);
-	}
-
-	private ConceptualModelDiagram getDiagramContentsObject(ORefList oRefs) throws Exception
-	{
-		if (oRefs.size() == 0)
-		{
-			BaseId id = createObjectAndReturnId(ObjectType.CONCEPTUAL_MODEL_DIAGRAM);
-			return (ConceptualModelDiagram) findObject(new ORef(ObjectType.CONCEPTUAL_MODEL_DIAGRAM, id));
-		}
-		if (oRefs.size() > 1)
-		{
-			EAM.logVerbose("Found more than one diagram contents inside pool");
-		}
-
-		ORef oRef = oRefs.get(0);
-		return (ConceptualModelDiagram) findObject(oRef);
-	}
-	
-	public void commandExecuted(CommandExecutedEvent event)
-	{
-		if (! event.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
-			return;
-
-		try
-		{
-			CommandSetObjectData setCommand = (CommandSetObjectData) event.getCommand();
-			DiagramModelUpdater modelUpdater = new DiagramModelUpdater(this, getDiagramModel());
-			modelUpdater.commandSetObjectDataWasExecuted(setCommand);
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-		}
-	}
-	
 	public LinkCell createLinkCell() throws Exception
 	{
 		BaseId diagramLinkId = createDiagramFactorLink();
@@ -375,7 +263,4 @@ public class ProjectForTesting extends ProjectWithHelpers implements CommandExec
 		
 		return factorLinkRef;
 	}
-
-	private DiagramModel diagramModel;
-	private Vector commandStack;
 }
--- tests/org/miradi/project/ProjectWithHelpers.java
+++ tests/org/miradi/project/ProjectWithHelpers.java
@@ -19,17 +19,136 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.project;
 
-import org.miradi.database.ProjectServer;
+import java.io.File;
+import java.util.Vector;
 
-public class ProjectWithHelpers extends Project
+import org.miradi.commands.Command;
+import org.miradi.commands.CommandSetObjectData;
+import org.miradi.database.ProjectServer;
+import org.miradi.diagram.DiagramModel;
+import org.miradi.ids.BaseId;
+import org.miradi.main.CommandExecutedEvent;
+import org.miradi.main.CommandExecutedListener;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.objecthelpers.ObjectType;
+import org.miradi.objectpools.ConceptualModelDiagramPool;
+import org.miradi.objects.ConceptualModelDiagram;
+import org.miradi.objects.DiagramObject;
+import org.miradi.views.diagram.DiagramModelUpdater;
+
+public class ProjectWithHelpers extends Project implements CommandExecutedListener
 {
-	public ProjectWithHelpers() throws Exception
+	public ProjectWithHelpers(String testName) throws Exception
 	{
-		super();
+		this(new ProjectServerForTesting());
 	}
 	
 	public ProjectWithHelpers(ProjectServer server) throws Exception
 	{
 		super(server);
+		String filename = getFilename();
+		addCommandExecutedListener(this);
+		diagramModel = new DiagramModel(this);
+		getTestDatabase().openMemoryDatabase(filename);
+		finishOpening();
+		commandStack = new Vector();
 	}
+	
+	public DiagramObject getDiagramObject()
+	{
+		return getDiagramModel().getDiagramObject();
+	}
+	
+	public DiagramModel getDiagramModel()
+	{
+		return diagramModel;
+	}
+
+	protected void finishOpening() throws Exception
+	{
+		super.finishOpening();
+		loadDiagram();
+	}
+
+	public void close() throws Exception
+	{
+		super.close();
+		
+		diagramModel = null;
+	}
+
+	protected void setDefaultDiagramPage(int objectType)
+	{
+		//TODO overrode to be empty, so that tests dont fail
+	}
+
+	public void closeAndReopen() throws Exception
+	{
+		File topDirectory = getDatabase().getTopDirectory();
+		getTestDatabase().closeAndDontDelete();
+		createOrOpen(topDirectory);
+	}
+
+	protected void fireCommandExecuted(Command command)
+	{
+		super.fireCommandExecuted(command);
+		if(commandStack != null)
+			commandStack.add(command);
+	}
+
+	protected Command getLastCommand()
+	{
+		return (Command)commandStack.remove(commandStack.size()-1);
+	}
+
+	public ProjectServerForTesting getTestDatabase()
+	{
+		return (ProjectServerForTesting)getDatabase();
+	}
+
+	public void loadDiagram() throws Exception
+	{
+		ConceptualModelDiagramPool diagramContentsPool = (ConceptualModelDiagramPool) getPool(ObjectType.CONCEPTUAL_MODEL_DIAGRAM);
+		ORefList oRefs = diagramContentsPool.getORefList();
+		ConceptualModelDiagram diagramContentsObject = getDiagramContentsObject(oRefs);
+		getDiagramModel().fillFrom(diagramContentsObject);
+	}
+	
+	ConceptualModelDiagram getDiagramContentsObject(ORefList oRefs) throws Exception
+	{
+		if (oRefs.size() == 0)
+		{
+			BaseId id = createObjectAndReturnId(ObjectType.CONCEPTUAL_MODEL_DIAGRAM);
+			return (ConceptualModelDiagram) findObject(new ORef(ObjectType.CONCEPTUAL_MODEL_DIAGRAM, id));
+		}
+		if (oRefs.size() > 1)
+		{
+			EAM.logVerbose("Found more than one diagram contents inside pool");
+		}
+
+		ORef oRef = oRefs.get(0);
+		return (ConceptualModelDiagram) findObject(oRef);
+	}
+	
+	public void commandExecuted(CommandExecutedEvent event)
+	{
+		if (! event.getCommandName().equals(CommandSetObjectData.COMMAND_NAME))
+			return;
+	
+		try
+		{
+			CommandSetObjectData setCommand = (CommandSetObjectData) event.getCommand();
+			DiagramModelUpdater modelUpdater = new DiagramModelUpdater(this, getDiagramModel());
+			modelUpdater.commandSetObjectDataWasExecuted(setCommand);
+		}
+		catch(Exception e)
+		{
+			EAM.logException(e);
+		}
+	}
+	
+	private DiagramModel diagramModel;
+	private Vector commandStack;
 }
