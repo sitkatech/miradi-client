32d73b8e7d4e303c581b0238cb2dfaab61f726fe
nimaa <nimaa@localhost>
2008-04-22 17:13:15 +0000

    refactor rename package

--- source/org/miradi/xml/conpro/exporter/ConProMiradiXmlValidator.java
+++ source/org/miradi/xml/conpro/exporter/ConProMiradiXmlValidator.java
@@ -3,7 +3,7 @@
 * (on behalf of the Conservation Measures Partnership, "CMP") and 
 * Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
 */ 
-package org.miradi.xml.conpro.export;
+package org.miradi.xml.conpro.exporter;
 
 import java.io.InputStream;
 import java.net.URL;
