8f5655cc745cfc9e752fc73b8d772ea3ec989137
nimaa <nimaa@localhost>
2008-04-30 15:51:25 +0000

    importing indicator measurement data

--- source/org/miradi/xml/conpro/exporter/ConproXmlExporter.java
+++ source/org/miradi/xml/conpro/exporter/ConproXmlExporter.java
@@ -332,7 +332,6 @@ public class ConproXmlExporter extends XmlExporter implements ConProMiradiXml
 		writeOptionalElement(out, CURRENT_RATING_DATE,  measurement, Measurement.TAG_DATE);
 		writeOptionalElement(out, CONFIDENE_CURRENT_RATING,  statusConfidenceToXmlValue(measurement.getData(Measurement.TAG_STATUS_CONFIDENCE)));
 		writeOptionalElement(out, CURRENT_RATING_COMMENT, measurement, Measurement.TAG_COMMENT);
-	
 	}
 
 	private void writeThreshold(UnicodeWriter out, String elementName, Indicator indicator, String threshold) throws Exception
