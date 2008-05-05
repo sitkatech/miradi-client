8a53035291b97526980f576fb2b4cd467d617352
nimaa <nimaa@localhost>
2008-05-05 21:38:16 +0000

    exporting project.xml as zip file

---
+++ source/org/miradi/utils/CpmzFileChooser.java
@@ -0,0 +1,62 @@
+/* 
+Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.utils;
+
+import javax.swing.filechooser.FileFilter;
+
+import org.miradi.main.MainWindow;
+
+public class CpmzFileChooser extends EAMFileSaveChooser
+{
+	public CpmzFileChooser(MainWindow mainWindow)
+	{
+		super(mainWindow);
+	}
+
+	public String getDialogApproveTitleText()
+	{
+		return "Title|Save Cpmz File";
+	}
+
+	public String getApproveButtonToolTipText()
+	{
+		return "TT|Save Cpmz File";
+	}
+
+	public String getDialogApprovelButtonText()
+	{
+		return "Save Cpmz";
+	}
+
+	public String getDialogOverwriteTitleText()
+	{
+		return "Title|Overwrite existing file?";
+	}
+
+	public String getDialogOverwriteBodyText()
+	{
+		return "This will replace the existing file.";
+	}
+
+	public FileFilter[] getFileFilter()
+	{
+		return new FileFilter[] {new CpmzFileFilter()};
+	}
+}
