f82193c1efea100fccf4ee069ad58e0cc3bf2d7e
nimaa <nimaa@localhost>
2008-05-06 20:54:54 +0000

    no longer exporting to a temp file

--- source/org/miradi/views/umbrella/CpmzExporter.java
+++ source/org/miradi/views/umbrella/CpmzExporter.java
@@ -19,14 +19,16 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.views.umbrella;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import org.martus.util.UnicodeStringWriter;
 import org.miradi.exceptions.CommandFailedException;
 import org.miradi.main.EAM;
 import org.miradi.utils.CpmzFileChooser;
@@ -64,52 +66,54 @@ public class CpmzExporter extends MainWindowDoer
 		}
 	}
 
-
 	private void createCpmzFile(File chosen) throws Exception
 	{
-		File projectXmlFile = createProjectXmlFileInSystemTemp();
-		ZipOutputStream out = new ZipOutputStream(new FileOutputStream(chosen));
+		ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(chosen));
 		try
 		{
-			new ConproXmlExporter(getProject()).export(projectXmlFile);
-			if (!new ConProMiradiXmlValidator().isValid(new FileInputStream(projectXmlFile)))
+			byte[] projectXmlInBytes = exportProjectXmlToBytes();
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(projectXmlInBytes);
+			if (!new ConProMiradiXmlValidator().isValid(inputStream))
+			{
 				throw new Exception("Exported file does not validate.");
-					
-			byte[] contents = readContent(projectXmlFile);
-			writeContent(projectXmlFile.getName(), out, contents);
+			}
+		
+			writeContent("project.xml", zipOut, projectXmlInBytes);
+		}
+		catch(Exception e)
+		{	
+			throw e;
 		}
 		finally
 		{
-			projectXmlFile.delete();
-			out.closeEntry();
-			out.close();
+			zipOut.close();
 		}
 	}
 
-	private void writeContent(String projectXmlName, ZipOutputStream out, byte[] contents) throws FileNotFoundException, IOException
+	private byte[] exportProjectXmlToBytes() throws IOException, Exception, UnsupportedEncodingException
+	{
+		UnicodeStringWriter writer = UnicodeStringWriter.create();
+		try
+		{
+			new ConproXmlExporter(getProject()).exportProject(writer);
+			writer.close();
+		}
+		finally
+		{
+			writer.close();
+		}
+		
+		return writer.toString().getBytes("UTF-8");
+	}
+
+	private void writeContent(String projectXmlName, ZipOutputStream out, byte[] bytes) throws FileNotFoundException, IOException
 	{
 		ZipEntry entry = new ZipEntry(projectXmlName);
-		entry.setSize(contents.length);
-		out.putNextEntry(entry);
-		out.write(contents);
+		entry.setSize(bytes.length);
+		out.putNextEntry(entry);	
+		out.write(bytes);
 	}
 
-	private byte[] readContent(File projectXmlFile) throws FileNotFoundException, IOException
-	{
-		int size = (int) projectXmlFile.length();
-		FileInputStream in = new FileInputStream(projectXmlFile);
-		try
-		{
-			byte[] contents = new byte[size];
-			in.read(contents);
-			return contents;
-		}
-		finally
-		{
-			in.close();
-		}
-	}
-	
 	public static File createProjectXmlFileInSystemTemp()
 	{
 		return new File(System.getProperty("java.io.tmpdir"), PROJECT_XML_FILE_NAME);
