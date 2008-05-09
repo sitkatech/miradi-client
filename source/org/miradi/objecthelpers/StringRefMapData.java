32fe9fe83597af9c4f8cb4e39d0c08654ffd327d
nimaa <nimaa@localhost>
2008-05-09 18:46:18 +0000

    new string ref map data class.  needs to finish implemention of getRefList (has fixme)

---
+++ source/org/miradi/objecthelpers/StringRefMapData.java
@@ -0,0 +1,68 @@
+/* 
+Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.objecthelpers;
+
+import org.miradi.objectdata.ObjectData;
+
+public class StringRefMapData extends ObjectData
+{
+	public StringRefMapData(String tagToUse)
+	{
+		super(tagToUse);
+		data = new StringRefMap();
+	}
+
+	public String get()
+	{
+		return getStringRefMap().toString();
+	}
+	
+	public StringRefMap getStringRefMap()
+	{
+		return data;
+	}
+
+	//FIXME this needs to be set correctly so that referrers works properly
+	public ORefList getRefList()
+	{
+		return new ORefList();
+	}
+
+	public void set(String newValue) throws Exception
+	{
+		data = new StringRefMap(newValue);
+	}
+
+	public boolean equals(Object rawOther)
+	{
+		if(!(rawOther instanceof StringRefMapData))
+			return false;
+
+		StringRefMapData other = (StringRefMapData) rawOther;
+		return other.data.equals(data);
+	}
+
+	public int hashCode()
+	{
+		return toString().hashCode();
+	}
+
+	private StringRefMap data;
+}
