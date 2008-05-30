b0890886ca1c5086289c7df74f02088f97cf77e0
nimaa <nimaa@localhost>
2008-05-30 16:59:57 +0000

    added functionality to incoming junction doer.  Pulled up code from outgoing junction to abstractJunction doer

--- source/org/miradi/views/diagram/doers/CreateOutGoingJunctionDoer.java
+++ source/org/miradi/views/diagram/doers/CreateOutGoingJunctionDoer.java
@@ -21,77 +21,27 @@ package org.miradi.views.diagram.doers;
 
 import java.awt.Dimension;
 import java.awt.Point;
-import java.util.Vector;
 
-import org.miradi.commands.CommandBeginTransaction;
-import org.miradi.commands.CommandEndTransaction;
-import org.miradi.commands.CommandSetObjectData;
-import org.miradi.exceptions.CommandFailedException;
 import org.miradi.objects.DiagramFactor;
-import org.miradi.objects.DiagramLink;
 import org.miradi.objects.FactorLink;
-import org.miradi.project.Project;
-import org.miradi.utils.PointList;
 
 public class CreateOutGoingJunctionDoer extends AbstractCreateJunctionDoer
 {
-	@Override
-	public void doIt() throws CommandFailedException
+	protected int getDirection()
 	{
-		if (!isAvailable())
-			return;
-		
-		getProject().executeCommand(new CommandBeginTransaction());
-		try
-		{
-			Vector<DiagramFactor> diagramFactors = getSelectedDiagramFactors();
-			for (int index = 0; index < diagramFactors.size(); ++index)
-			{
-				createOutgoingJunction(diagramFactors.get(index));
-			}		
-		}
-		finally
-		{
-			getProject().executeCommand(new CommandEndTransaction());
-		}
-	}
-
-	private void createOutgoingJunction(DiagramFactor diagramFactor) throws CommandFailedException
-	{
-		Vector<DiagramLink> outgoingDiagramLinks = getDiagramLinks(diagramFactor, FactorLink.FROM);
-		for (int index = 0; index < outgoingDiagramLinks.size(); ++index)
-		{
-			createJunction(diagramFactor, outgoingDiagramLinks.get(index));
-		}
-	}
-
-	private void createJunction(DiagramFactor diagramFactor, DiagramLink diagramLink) throws CommandFailedException
-	{
-		Point junctionPoint = getOutgoingJunctionPoint(diagramFactor);
-		PointList bendPoints = new PointList(diagramLink.getBendPoints());
-		if (bendPoints.contains(junctionPoint))
-			return; 
-		
-		if (bendPoints.size() > 0)
-			bendPoints.set(0, junctionPoint);
-		else
-			bendPoints.add(junctionPoint);
-		
-		CommandSetObjectData setBendPoints = new CommandSetObjectData(diagramLink, DiagramLink.TAG_BEND_POINTS, bendPoints.toString());
-		getProject().executeCommand(setBendPoints);
+		return FactorLink.FROM;
 	}
 	
-	private Point getOutgoingJunctionPoint(DiagramFactor diagramFactor)
+	protected Point getJunctionPoint(DiagramFactor diagramFactor)
 	{
 		Point location = diagramFactor.getLocation();
 		Dimension size = diagramFactor.getSize();
 		
 		int junctionX = location.x + size.width;
-		int middleOfHeigth = size.height / 2;
-		int junctionY = location.y + middleOfHeigth;
+		int junctionY = getYOfMiddleOfHeight(location, size);
 		
 		Point junctionPoint = new Point(junctionX, junctionY);
-		junctionPoint.translate(Project.DEFAULT_GRID_SIZE * 4, 0);
+		junctionPoint.translate(JUNCTION_DISTANCE_FROM_FACTOR, 0);
 		
 		return junctionPoint;
 	}
