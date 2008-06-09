34448dbcb882f78bf725a2c969fd6eb804b7fbb0
nimaa <nimaa@localhost>
2008-06-09 16:22:09 +0000

    fixed bug when exporting direct threats while in stress based mode

--- tests/org/miradi/xml/conpro/exporter/TestConproXmlExporter.java
+++ tests/org/miradi/xml/conpro/exporter/TestConproXmlExporter.java
@@ -21,9 +21,14 @@ package org.miradi.xml.conpro.exporter;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.martus.util.DirectoryUtils;
 import org.miradi.main.TestCaseWithProject;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objects.ProjectMetadata;
+import org.miradi.questions.ThreatRatingModeChoiceQuestion;
 
 public class TestConproXmlExporter extends TestCaseWithProject
 {
@@ -40,6 +45,19 @@ public class TestConproXmlExporter extends TestCaseWithProject
 	}
 	
 	public void testValidatedExport() throws Exception
+	{
+		verifyExport();
+	}
+
+	public void testValidatedExportInStressMode() throws Exception
+	{	
+		ORef metedataRef = getProject().getMetadata().getRef();
+		getProject().setObjectData(metedataRef, ProjectMetadata.TAG_THREAT_RATING_MODE, ThreatRatingModeChoiceQuestion.STRESS_BASED_CODE);
+		
+		verifyExport();
+	}
+	
+	private void verifyExport() throws IOException, Exception, FileNotFoundException
 	{
 		File tempXmlOutFile = createTempFileFromName("conpro.xml");
 		try
