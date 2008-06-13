adfe84bd78932897bb7b819ff450596276deb8d1
kevins <kevins@localhost>
2008-06-13 21:58:50 +0000

    When showing a stress, set a reasonable initial location

--- source/org/miradi/views/diagram/doers/ShowStressBubbleDoer.java
+++ source/org/miradi/views/diagram/doers/ShowStressBubbleDoer.java
@@ -19,11 +19,14 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.views.diagram.doers;
 
+import java.awt.Point;
+
 import org.miradi.diagram.DiagramModel;
 import org.miradi.ids.DiagramFactorId;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objects.DiagramFactor;
 import org.miradi.objects.DiagramObject;
+import org.miradi.objects.Target;
 import org.miradi.project.FactorCommandHelper;
 
 public class ShowStressBubbleDoer extends AbstractStressVisibilityDoer
@@ -35,12 +38,21 @@ public class ShowStressBubbleDoer extends AbstractStressVisibilityDoer
 	
 	protected void doWork() throws Exception
 	{
+		ORef targetDiagramFactorRef = getSelectedTargetDiagramFactorRef();
+
 		ORef selectedStressRef = getSelectedStress();
 		DiagramModel diagramModel = getDiagramView().getDiagramModel();
 		FactorCommandHelper helper = new FactorCommandHelper(getProject(), diagramModel);
 		DiagramObject diagramObject = diagramModel.getDiagramObject();
-		DiagramFactorId diagramFactorId = (DiagramFactorId) helper.createDiagramFactor(diagramObject, selectedStressRef).getCreatedId();
-		
-		helper.setDiagramFactorSize(diagramFactorId, DiagramFactor.DEFAULT_STRESS_SIZE);
+		DiagramFactorId stressDiagramFactorId = (DiagramFactorId) helper.createDiagramFactor(diagramObject, selectedStressRef).getCreatedId();
+
+		DiagramFactor targetDiagramFactor = DiagramFactor.find(getProject(), targetDiagramFactorRef);
+		Target target = (Target) targetDiagramFactor.getWrappedFactor();
+		int offset = target.getStressRefs().find(selectedStressRef);
+		Point stressLocation = new Point(targetDiagramFactor.getLocation());
+		stressLocation.x += (offset * getProject().getGridSize()); 
+		stressLocation.y += targetDiagramFactor.getSize().height;
+		helper.setDiagramFactorLocation(stressDiagramFactorId, stressLocation);
+		helper.setDiagramFactorSize(stressDiagramFactorId, DiagramFactor.DEFAULT_STRESS_SIZE);
 	}
 }
