d7d55e985f1174234ab4b605acb0bf490bb5e164
nimaa <nimaa@localhost>
2008-07-01 16:32:27 +0000

    bumped up version number,  still working on migration

--- source/org/miradi/database/ProjectServer.java
+++ source/org/miradi/database/ProjectServer.java
@@ -266,7 +266,7 @@ abstract public class ProjectServer
 	static public String OBJECT_TYPE = "Type";
 	static public String TAG_VERSION = "Version";
 	static public String OBJECT_MANIFEST = "ObjectManifest";
-	static public int DATA_VERSION = 34;
+	static public int DATA_VERSION = 35;
 
 	protected File topDirectory;
 	protected String name;
