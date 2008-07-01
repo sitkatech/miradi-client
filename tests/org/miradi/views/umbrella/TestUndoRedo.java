14b4adbcacdeb55378a468df22274f82a17a6dc0
nimaa <nimaa@localhost>
2008-07-01 18:58:29 +0000

    using Factor.findFactor instead of getProject.findXXX

--- tests/org/miradi/views/umbrella/TestUndoRedo.java
+++ tests/org/miradi/views/umbrella/TestUndoRedo.java
@@ -93,7 +93,7 @@ public class TestUndoRedo extends EAMTestCase
 		DiagramFactor node = diagramFactors[0];
 		assertTrue(project.getDiagramModel().doesDiagramFactorExist(node.getDiagramFactorId()));
 		
-		Factor factor = (Factor) project.findObject(node.getWrappedORef());
+		Factor factor = Factor.findFactor(project, node.getWrappedORef());
 		assertEquals("Incorrect label?", target1Text, factor.getLabel());
 		
 		undo.doIt();
