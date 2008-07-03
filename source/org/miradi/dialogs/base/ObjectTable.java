cd9a858ee056b52d177e2e0f11953c1c1e529799
nimaa <nimaa@localhost>
2008-07-03 16:40:43 +0000

    imporoved logic readability

--- source/org/miradi/dialogs/base/ObjectTable.java
+++ source/org/miradi/dialogs/base/ObjectTable.java
@@ -95,7 +95,7 @@ abstract public class ObjectTable extends UiTableWithAlternatingRows implements 
 	
 	public void scrollToAndSelectRow(int row)
 	{
-		if (getRowCount() <= row || row < 0)
+		if(row < 0 || row >= getRowCount())
 			return;
 		
 		Rectangle rect = getCellRect(row, 0, true);
