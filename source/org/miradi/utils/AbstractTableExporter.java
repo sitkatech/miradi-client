66009e8298a2ff9bf2efba886045721c2dd89fe5
nimaa <nimaa@localhost>
2008-08-01 14:49:37 +0000

    new abstract table exporter

---
+++ source/org/miradi/utils/AbstractTableExporter.java
@@ -0,0 +1,35 @@
+/* 
+Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.utils;
+
+import org.miradi.objects.BaseObject;
+
+public abstract class AbstractTableExporter
+{
+	abstract public int getMaxDepthCount();
+	abstract public int getDepth(int row);
+	abstract public int getColumnCount();
+	abstract public int getRowCount();
+	abstract public String getTextAt(int row, int column);
+	abstract public String getIconAt(int row, int column);
+	abstract public String getRowType(int row);
+	abstract public BaseObject getBaseObjectForRow(int row);
+	abstract public String getHeaderFor(int column);
+}
