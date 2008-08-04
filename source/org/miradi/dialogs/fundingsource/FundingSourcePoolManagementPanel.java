d6d7d18d4bfa4d7170cb5c07ceadf6b2060d5b92
nimaa <nimaa@localhost>
2008-08-04 17:57:47 +0000

    funding source table is now available for rtf exporting

--- source/org/miradi/dialogs/fundingsource/FundingSourcePoolManagementPanel.java
+++ source/org/miradi/dialogs/fundingsource/FundingSourcePoolManagementPanel.java
@@ -33,6 +33,7 @@ import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 import org.miradi.utils.BufferedImageFactory;
 import org.miradi.utils.AbstractTableExporter;
+import org.miradi.utils.RtfWriter;
 import org.miradi.utils.TableExporter;
 
 public class FundingSourcePoolManagementPanel extends ObjectPoolManagementPanel
@@ -94,6 +95,16 @@ public class FundingSourcePoolManagementPanel extends ObjectPoolManagementPanel
 	public JComponent getPrintableComponent() throws Exception
 	{
 		return createTable();
+	}
+	
+	public boolean isRtfExportable()
+	{
+		return true;
+	}
+		
+	public void exportRtf(RtfWriter writer) throws Exception
+	{
+		writer.writeRtfTable(getTableExporter());
 	}
 
 	private static String PANEL_DESCRIPTION = EAM.text("Title|Funding Sources"); 
