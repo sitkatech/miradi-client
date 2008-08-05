1c9093942cf11284d290bb0b4f11a1b412954385
kevins <kevins@localhost>
2008-08-05 17:38:21 +0000

    Add form for Summary View/Location Tab

---
+++ source/org/miradi/forms/summary/LocationTabForm.java
@@ -0,0 +1,44 @@
+/* 
+Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.forms.summary;
+
+import org.miradi.forms.FieldPanelSpec;
+import org.miradi.objects.ProjectMetadata;
+
+public class LocationTabForm extends FieldPanelSpec
+{
+	public LocationTabForm()
+	{
+		int type = ProjectMetadata.getObjectType();
+
+		addLabelAndField(type, ProjectMetadata.TAG_PROJECT_LATITUDE);
+		addLabelAndField(type, ProjectMetadata.TAG_PROJECT_LONGITUDE);
+		
+		addLabelAndField(type, ProjectMetadata.TAG_COUNTRIES);
+		
+		addLabelAndField(type, ProjectMetadata.TAG_STATE_AND_PROVINCES);
+		addLabelAndField(type, ProjectMetadata.TAG_MUNICIPALITIES);
+		addLabelAndField(type, ProjectMetadata.TAG_LEGISLATIVE_DISTRICTS);
+		addLabelAndField(type, ProjectMetadata.TAG_LOCATION_DETAIL);
+		addLabelAndField(type, ProjectMetadata.TAG_SITE_MAP_REFERENCE);
+		addLabelAndField(type, ProjectMetadata.TAG_LOCATION_COMMENTS);
+		
+	}
+}
