f0a30c3987a5ff14b42c7dd97aa083af20f33bf7
nimaa <nimaa@localhost>
2008-08-07 19:21:35 +0000

    added rows to Objective form

--- source/org/miradi/forms/objects/ObjectivePropertiesForm.java
+++ source/org/miradi/forms/objects/ObjectivePropertiesForm.java
@@ -20,10 +20,25 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.forms.objects;
 
 import org.miradi.forms.FieldPanelSpec;
+import org.miradi.main.EAM;
+import org.miradi.objects.Desire;
+import org.miradi.objects.Goal;
+import org.miradi.objects.Objective;
 
 public class ObjectivePropertiesForm extends FieldPanelSpec
 {
 	public ObjectivePropertiesForm()
 	{
+		int type = Objective.getObjectType();
+		addLabelAndFieldsWithLabels(EAM.text("Objective"), type, new String[]{Objective.TAG_SHORT_LABEL, Objective.TAG_LABEL});
+		addLabelAndField(type, Desire.TAG_FULL_TEXT);
+		addLabelAndField(type, Objective.PSEUDO_TAG_FACTOR);
+		addLabelAndField(type, Objective.PSEUDO_TAG_STRATEGIES);
+		addLabelAndField(type, Objective.PSEUDO_TAG_DIRECT_THREATS);
+		addLabelAndField(type, Objective.PSEUDO_TAG_TARGETS);
+		addLabelAndField(type, Objective.PSEUDO_RELEVANT_INDICATOR_REFS);
+		addLabelAndField(type, Objective.PSEUDO_RELEVANT_STRATEGY_REFS);
+		addLabelAndFieldsWithLabels(EAM.text("Progress Percents"), type, new String[]{Objective.TAG_PROGRESS_PERCENT_REFS});
+		addLabelAndField(type, Goal.TAG_COMMENTS);
 	}
 }
