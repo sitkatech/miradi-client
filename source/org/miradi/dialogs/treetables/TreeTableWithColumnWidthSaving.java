56f54ddee5573cc4d58e4e3613735dec5e95309b
kevins <kevins@localhost>
2008-08-08 15:35:47 +0000

    Extract treetable row height stuff to its own class

--- source/org/miradi/dialogs/treetables/TreeTableWithColumnWidthSaving.java
+++ source/org/miradi/dialogs/treetables/TreeTableWithColumnWidthSaving.java
@@ -19,19 +19,11 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.miradi.dialogs.treetables;
 
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-
-import javax.swing.JTable;
-
 import org.miradi.main.MainWindow;
 import org.miradi.utils.ColumnSequenceSaver;
 import org.miradi.utils.ColumnWidthSaver;
-import org.miradi.utils.RowHeightListener;
-import org.miradi.utils.TableRowHeightSaver;
-import org.miradi.utils.TableWithRowHeightManagement;
 
-abstract public class TreeTableWithColumnWidthSaving extends PanelTreeTable implements TableWithRowHeightManagement
+abstract public class TreeTableWithColumnWidthSaving extends TreeTableWithRowHeightSaver
 {
 	public TreeTableWithColumnWidthSaving(MainWindow mainWindowToUse, GenericTreeTableModel treeTableModel)
 	{
@@ -41,17 +33,6 @@ abstract public class TreeTableWithColumnWidthSaving extends PanelTreeTable impl
 
 		getTableHeader().addMouseListener(columnWidthSaver);
 		getTableHeader().addMouseListener(columnSequenceSaver);
-		
-		rowHeightSaver = new TableRowHeightSaver();
-		rowHeightSaver.manage(getMainWindow(), this, getUniqueTableIdentifier());
-		
-		addComponentListener(new ComponentEventHandler());
-
-	}
-	
-	public boolean allowUserToSetRowHeight()
-	{
-		return true;
 	}
 	
 	public void rebuildTableCompletely() throws Exception
@@ -59,86 +40,8 @@ abstract public class TreeTableWithColumnWidthSaving extends PanelTreeTable impl
 		super.rebuildTableCompletely();
 		columnWidthSaver.restoreColumnWidths();
 		columnSequenceSaver.restoreColumnSequences();
-		adjustRowHeights();
-
 	}
-
-	public void addRowHeightListener(RowHeightListener listener)
-	{
-		rowHeightSaver.addRowHeightListener(listener);
-	}
-	
-	@Override
-	public void setRowHeight(int rowHeight)
-	{
-		super.setRowHeight(rowHeight);
-		if(getMainWindow() == null)
-			return;
-		
-		if(getMainWindow().isRowHeightModeAutomatic())
-			return;
-		
-		if(rowHeightSaver == null)
-			return;
-		
-		rowHeightSaver.rowHeightChanged(rowHeight);
-	}
-	
-	@Override
-	public void setRowHeight(int row, int rowHeight)
-	{
-		super.setRowHeight(row, rowHeight);
-		if(rowHeightSaver == null)
-			return;
-		
-		rowHeightSaver.rowHeightChanged(row, rowHeight);
-	}
-	
-	public JTable asTable()
-	{
-		return this;
-	}
-	
-	private void adjustRowHeights()
-	{
-		if(getMainWindow().isRowHeightModeManual())
-			return;
-		
-		for(int row = 0; row < getRowCount(); ++row)
-		{
-			int height = getPreferredRowHeight(row);
-			setRowHeight(row, height);
-		}
-		getTree().setRowHeight(-1);
-	}
-
-	class ComponentEventHandler implements ComponentListener
-	{
-		public void componentShown(ComponentEvent e)
-		{
-			adjustRowHeights();
-		}
-		
-		public void componentHidden(ComponentEvent e)
-		{
-		}
-
-		public void componentMoved(ComponentEvent e)
-		{
-		}
-
-		public void componentResized(ComponentEvent e)
-		{
-			adjustRowHeights();
-		}
-
-	}
-	
-
-	
-	abstract public String getUniqueTableIdentifier();
 	
 	private ColumnWidthSaver columnWidthSaver;
 	private ColumnSequenceSaver columnSequenceSaver;
-	private TableRowHeightSaver rowHeightSaver;
 }
