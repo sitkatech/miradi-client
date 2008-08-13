9c02741c382c65002ac415d328657749b3ac6bc1
nimaa <nimaa@localhost>
2008-08-13 14:22:39 +0000

    moved exporting of CM and RC diagrams from diagramView to diagramViewRtfExporter

--- source/org/miradi/views/diagram/DiagramView.java
+++ source/org/miradi/views/diagram/DiagramView.java
@@ -147,7 +147,6 @@ import org.miradi.objects.BaseObject;
 import org.miradi.objects.ConceptualModelDiagram;
 import org.miradi.objects.DiagramFactor;
 import org.miradi.objects.DiagramLink;
-import org.miradi.objects.DiagramObject;
 import org.miradi.objects.Factor;
 import org.miradi.objects.ProjectMetadata;
 import org.miradi.objects.ResultsChainDiagram;
@@ -158,8 +157,6 @@ import org.miradi.objects.Target;
 import org.miradi.objects.Task;
 import org.miradi.objects.ViewData;
 import org.miradi.project.Project;
-import org.miradi.rtf.RtfWriter;
-import org.miradi.utils.BufferedImageFactory;
 import org.miradi.utils.DiagramCorruptionDetector;
 import org.miradi.utils.PointList;
 import org.miradi.views.TabbedView;
@@ -1059,23 +1056,6 @@ public class DiagramView extends TabbedView implements CommandExecutedListener
 		return getCurrentMode().equals(ViewData.MODE_STRATEGY_BRAINSTORM);
 	}
 	
-	public void exportRtf(RtfWriter writer) throws Exception
-	{
-		if (isResultsChainTab())
-			exportDiagrams(writer, getProject().getConceptualModelDiagramPool().getRefList());
-		else
-			exportDiagrams(writer, getProject().getResultsChainDiagramPool().getRefList());
-	}
-	
-	private void exportDiagrams(RtfWriter writer, ORefList diagramObjectRefs) throws Exception
-	{
-		for (int index = 0; index < diagramObjectRefs.size(); ++index)
-		{
-			DiagramObject diagramObject = DiagramObject.findDiagramObject(getProject(), diagramObjectRefs.get(index));
-			writer.writeImage(BufferedImageFactory.createImageFromDiagram(getMainWindow(), diagramObject));
-		}
-	}
-
 	private PropertiesDoer propertiesDoer;
 	private String mode;
 	
