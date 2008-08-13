8e7985b9ab2c65204fa1fc723cfcca72960b9ce1
nimaa <nimaa@localhost>
2008-08-13 19:37:21 +0000

    made sure codes are unique in question.  extracted codes as public static finals

--- source/org/miradi/questions/ReportTemplateContentQuestion.java
+++ source/org/miradi/questions/ReportTemplateContentQuestion.java
@@ -27,32 +27,53 @@ public class ReportTemplateContentQuestion extends StaticChoiceQuestion
 	}
 
 	static ChoiceItem[] getContentChoices()
-	{
+	{		
 		return new ChoiceItem[] {
-				new ChoiceItem("SummaryViewProjectTab", "Project"),
-				new ChoiceItem("SummaryViewTeamTab", "Team"),
-				new ChoiceItem("SummaryViewOrganizationTab", "Oranization"),
-				new ChoiceItem("SummaryViewScopeTab", "Scope"),
-				new ChoiceItem("SummaryViewLocationTab", "Location"),
-				new ChoiceItem("SummaryViewPlanningTab", "Planning"),
-				new ChoiceItem("SummaryViewTncTab", "TNC"),
-				new ChoiceItem("SummaryViewWwfTab", "WWF"),
-				new ChoiceItem("SummaryViewWcsTab", "WCS"),
-				new ChoiceItem("SummaryViewRareTab", "RARE"),
-				new ChoiceItem("SummaryViewFosTab", "FOS"),
+				new ChoiceItem(SUMMARY_VIEW_PROJECT_TAB_CODE, "Project"),
+				new ChoiceItem(SUMMARY_VIEW_TEAM_TAB_CODE, "Team"),
+				new ChoiceItem(SUMMARY_VIEW_ORGANIZATION_TAB_CODE, "Oranization"),
+				new ChoiceItem(SUMMARY_VIEW_SCOPE_TAB_CODE, "Scope"),
+				new ChoiceItem(SUMMARY_VIEW_LOCATION_TAB_CODE, "Location"),
+				new ChoiceItem(SUMMARY_VIEW_PLANNING_TAB_CODE, "Planning"),
+				new ChoiceItem(SUMMARY_VIEW_TNC_TAB_CODE, "TNC"),
+				new ChoiceItem(SUMMARY_VIEW_WWF_TAB_CODE, "WWF"),
+				new ChoiceItem(SUMMARY_VIEW_WCS_TAB_CODE, "WCS"),
+				new ChoiceItem(SUMMARY_VIEW_RARE_TAB_CODE, "RARE"),
+				new ChoiceItem(SUMMARY_VIEW_FOS_TAB_CODE, "FOS"),
 				
-				new ChoiceItem("DiagramView", "Conceptual Models"),
-				new ChoiceItem("DiagramView", "Results Chains"),
+				new ChoiceItem(DIAGRAM_VIEW_CONCEPTUAL_MODEL_TAB_CODE, "Conceptual Models"),
+				new ChoiceItem(DIAGRAM_VIEW_RESULTS_CHAINS_TAB_CODE, "Results Chains"),
 				
-				new ChoiceItem("ViabilityView", "Viability"),
+				new ChoiceItem(VIABILITY_VIEW_VIABILITY_TAB_CODE, "Viability"),
 				
-				new ChoiceItem("ThreatRatingsView", "Simple"),
-				new ChoiceItem("ThreatRatingsView", "Stress Based"),
+				new ChoiceItem(THREAT_RATING_VIEW_SIMPLE_CODE, "Simple"),
+				new ChoiceItem(THREAT_RATING_VIEW_STRESS_BASED_CODE, "Stress Based"),
 				
-				new ChoiceItem("PlanningView", "Planning"),
-				new ChoiceItem("PlanningView", "Resources"),
-				new ChoiceItem("PlanningView", "Accounting Codes"),
-				new ChoiceItem("PlanningView", "Funding Source"),
+				new ChoiceItem(PLANNING_VIEW_PLANNING_TAB_CODE, "Planning"),
+				new ChoiceItem(PLANNING_VIEW_RESOURCES_TAB_CODE, "Resources"),
+				new ChoiceItem(PLANNING_VIEW_ACCOUNTING_CODE_TAB_CODE, "Accounting Codes"),
+				new ChoiceItem(PLANNING_VIEW_FUNDING_SOURCE_TAB_CODE, "Funding Source"),
 		};
 	}
+	
+	public static final String SUMMARY_VIEW_PROJECT_TAB_CODE = "SummaryViewProjectTab";
+	public static final String SUMMARY_VIEW_TEAM_TAB_CODE = "SummaryViewTeamTab";
+	public static final String SUMMARY_VIEW_ORGANIZATION_TAB_CODE = "SummaryViewOrganizationTab";
+	public static final String SUMMARY_VIEW_SCOPE_TAB_CODE = "SummaryViewScopeTab";
+	public static final String SUMMARY_VIEW_LOCATION_TAB_CODE = "SummaryViewLocationTab";
+	public static final String SUMMARY_VIEW_PLANNING_TAB_CODE = "SummaryViewPlanningTab";
+	public static final String SUMMARY_VIEW_TNC_TAB_CODE = "SummaryViewTncTab";
+	public static final String SUMMARY_VIEW_WWF_TAB_CODE = "SummaryViewWwfTab";
+	public static final String SUMMARY_VIEW_WCS_TAB_CODE = "SummaryViewWcsTab";
+	public static final String SUMMARY_VIEW_RARE_TAB_CODE = "SummaryViewRareTab";
+	public static final String SUMMARY_VIEW_FOS_TAB_CODE = "SummaryViewFosTab";
+	public static final String DIAGRAM_VIEW_CONCEPTUAL_MODEL_TAB_CODE = "DiagramViewConceptualModelTab";
+	public static final String DIAGRAM_VIEW_RESULTS_CHAINS_TAB_CODE = "DiagramViewResultsChainTab";
+	public static final String VIABILITY_VIEW_VIABILITY_TAB_CODE = "ViabilityViewViabilityTab";
+	public static final String THREAT_RATING_VIEW_SIMPLE_CODE = "ThreatRatingsViewSimple";
+	public static final String THREAT_RATING_VIEW_STRESS_BASED_CODE = "ThreatRatingsViewStressBased";
+	public static final String PLANNING_VIEW_PLANNING_TAB_CODE = "PlanningViewPlanningTab";
+	public static final String PLANNING_VIEW_RESOURCES_TAB_CODE = "PlanningViewResourcesTab";
+	public static final String PLANNING_VIEW_ACCOUNTING_CODE_TAB_CODE = "PlanningViewAccountingCodesTab";
+	public static final String PLANNING_VIEW_FUNDING_SOURCE_TAB_CODE = "PlanningViewFundingSourceTab";
 }
