63893b7e02b10f4777f45c30c07b43ee02fcc7dc
nimaa <nimaa@localhost>
2008-08-14 16:49:57 +0000

    pulled up commong code to parent class

--- source/org/miradi/dialogs/planning/upperPanel/TreeTableModelExporter.java
+++ source/org/miradi/dialogs/planning/upperPanel/TreeTableModelExporter.java
@@ -19,22 +19,16 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.planning.upperPanel;
 
-import java.util.Vector;
-
-import javax.swing.Icon;
-
 import org.miradi.dialogs.treetables.GenericTreeTableModel;
 import org.miradi.dialogs.treetables.TreeTableNode;
-import org.miradi.icons.IconManager;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
 import org.miradi.objecthelpers.ObjectType;
 import org.miradi.objects.BaseObject;
 import org.miradi.project.Project;
-import org.miradi.utils.AbstractTableExporter;
+import org.miradi.utils.AbstractTreeTableOrModelExporter;
 
-//FIXME there is duplicate code between this class and TreeTableExporter.  Need a parent class for common code.
-public class TreeTableModelExporter extends AbstractTableExporter
+public class TreeTableModelExporter extends AbstractTreeTableOrModelExporter
 {
 	public TreeTableModelExporter(Project projectToUse, GenericTreeTableModel modelToUse) throws Exception
 	{
@@ -61,19 +55,6 @@ public class TreeTableModelExporter extends AbstractTableExporter
 		return  getModel().getPathOfNode(objectForRow.getRef()).getPath().length - ROOT_PLUS_TOPLEVEL_ADJUSTMENT;
 	}
 
-	public int getMaxDepthCount()
-	{
-		int maxRowDepth = 0;
-		int rowCount = getRowCount();
-		for (int row = 0; row < rowCount; ++row)
-		{
-			int rowDepth = getDepth(row);
-			maxRowDepth = Math.max(maxRowDepth, rowDepth);
-		}
-		
-		return maxRowDepth;
-	}
-
 	public String getHeaderFor(int column)
 	{
 		return getModel().getColumnName(column);
@@ -88,23 +69,6 @@ public class TreeTableModelExporter extends AbstractTableExporter
 	public int getColumnCount()
 	{
 		return model.getColumnCount();
-	}
-
-	@Override
-	public Icon getIconAt(int row, int column)
-	{
-		if (column == 0)
-		{
-			BaseObject baseObject = getBaseObjectForRow(row);
-			if (baseObject != null)
-				return IconManager.getImage(baseObject);
-			
-			int rowType = getRowType(row);
-			if (rowType != ObjectType.FAKE)
-				return IconManager.getImage(rowType);
-		}
-		//FIXME this needs to return correct cell icon
-		return null;
 	}
 
 	@Override
@@ -130,36 +94,6 @@ public class TreeTableModelExporter extends AbstractTableExporter
 		return getSafeValue(value);
 	}
 	
-	public ORefList getAllRefs(int objectType)
-	{
-		ORefList baseObjectsForType = new ORefList();
-		for (int row = 0; row < getRowCount(); ++row)
-		{
-			BaseObject baseObjectForRow = getBaseObjectForRow(row);
-			if (baseObjectForRow == null)
-				continue;
-			
-			if (baseObjectForRow.getType() == objectType)
-				baseObjectsForType.add(baseObjectForRow.getRef());
-		}
-		
-		return baseObjectsForType;
-	}
-
-	@Override
-	public Vector<Integer> getAllTypes()
-	{
-		Vector<Integer> rowTypes = new Vector<Integer>();
-		for (int row = 0; row < getRowCount(); ++row)
-		{
-			BaseObject baseObjectForRow = getBaseObjectForRow(row);
-			if (baseObjectForRow != null)
-				rowTypes.add(baseObjectForRow.getType());
-		}
-		
-		return rowTypes;
-	}
-	
 	private Project getProject()
 	{
 		return project;
@@ -173,5 +107,4 @@ public class TreeTableModelExporter extends AbstractTableExporter
 	private GenericTreeTableModel model;
 	private ORefList rowObjectRefs;
 	private Project project;
-	private static final int ROOT_PLUS_TOPLEVEL_ADJUSTMENT = 2;
 }
