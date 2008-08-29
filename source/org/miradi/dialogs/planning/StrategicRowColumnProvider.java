c58114ee872c21b333b680cd90e08a42f715117a
kevins <kevins@localhost>
2008-08-29 23:20:46 +0000

    Change StratPlan rows/columns as requested by Nick

--- source/org/miradi/dialogs/planning/StrategicRowColumnProvider.java
+++ source/org/miradi/dialogs/planning/StrategicRowColumnProvider.java
@@ -22,6 +22,7 @@ package org.miradi.dialogs.planning;
 import org.miradi.objects.BaseObject;
 import org.miradi.objects.ConceptualModelDiagram;
 import org.miradi.objects.Desire;
+import org.miradi.objects.Factor;
 import org.miradi.objects.Goal;
 import org.miradi.objects.Objective;
 import org.miradi.objects.ResultsChainDiagram;
@@ -35,26 +36,24 @@ public class StrategicRowColumnProvider implements RowColumnProvider
 	public CodeList getColumnListToShow()
 	{
 		return new CodeList(new String[] {
-//							Strategy.PSEUDO_TAG_RATING_SUMMARY,
-							Desire.PSEUDO_TAG_FACTOR,
-							BaseObject.PSEUDO_TAG_BUDGET_TOTAL,
-//							Strategy.PSEUDO_TAG_TAXONOMY_CODE_VALUE,
-//							Strategy.PSEUDO_TAG_PROGRESS,
-//							Task.PSEUDO_TAG_ASSIGNED_RESOURCES_HTML,
-//							Task.PSEUDO_TAG_COMBINED_EFFORT_DATES,
-//							Task.PSEUDO_TAG_TASK_TOTAL, 
-							});
+				Factor.TAG_TEXT,
+				Desire.PSEUDO_TAG_FACTOR,
+				BaseObject.PSEUDO_TAG_WHO_TOTAL,
+				BaseObject.PSEUDO_TAG_WHEN_TOTAL,
+				BaseObject.PSEUDO_TAG_BUDGET_TOTAL,
+				BaseObject.PSEUDO_TAG_LATEST_PROGRESS_REPORT_CODE,
+				});
 	}
 
 	public CodeList getRowListToShow()
 	{
 		return new CodeList(new String[] {
-							ConceptualModelDiagram.OBJECT_NAME,
-							ResultsChainDiagram.OBJECT_NAME,
-							Target.OBJECT_NAME,
-							Goal.OBJECT_NAME,
-							Objective.OBJECT_NAME,
-							Strategy.OBJECT_NAME,});
+				ResultsChainDiagram.OBJECT_NAME,
+				ConceptualModelDiagram.OBJECT_NAME,
+				Target.OBJECT_NAME,
+				Goal.OBJECT_NAME,
+				Objective.OBJECT_NAME,
+				Strategy.OBJECT_NAME,});
 	}
 	
 	public String getPropertyName()
