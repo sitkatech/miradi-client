e3f18fc6b6b4e869b0fc1a963817362de50a7418
kevins <kevins@localhost>
2008-09-03 17:28:55 +0000

    Various tweaks to the File menu: Sequence, shortcut keys, menu item wording, tooltip wording, separators

--- source/org/miradi/actions/ActionImportZippedConproProject.java
+++ source/org/miradi/actions/ActionImportZippedConproProject.java
@@ -31,7 +31,7 @@ public class ActionImportZippedConproProject extends MainWindowAction
 
 	public static String getLabel()
 	{
-		return EAM.text("Action|ConPro Project Zipfile");
+		return EAM.text("Action|ConPro/Miradi (CPMZ) File (BETA)");
 	}
 
 	public String getToolTipText()
