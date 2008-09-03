9f77abd71fdd4bdb74e237b6d845440f4d3e76c6
kevins <kevins@localhost>
2008-09-03 21:46:05 +0000

    New set of factor background colors from Nick

--- source/org/miradi/questions/DiagramFactorBackgroundQuestion.java
+++ source/org/miradi/questions/DiagramFactorBackgroundQuestion.java
@@ -34,19 +34,24 @@ public class DiagramFactorBackgroundQuestion extends StaticChoiceQuestion
 	{
 		return new ChoiceItem[] {
 			new ChoiceItem("", "Light Gray (Default)", DiagramConstants.TEXT_BOX_COLOR),
+			new ChoiceItem("White", "White", new Color(255, 255, 255)),
+			new ChoiceItem("Pink", "Pink", new Color(255, 0, 255)),
+			new ChoiceItem("Orange", "Light Orange", new Color(255, 128, 64)),
+			new ChoiceItem("LightYellow", "Light Yellow", new Color(255, 255, 205)),
+			new ChoiceItem("LightGreen", "Light Green", new Color(95, 251, 23)),
+			new ChoiceItem("LightBlue", "Light Blue", new Color(0, 204, 255)),
+			new ChoiceItem("LightPurple", "Light Purple", new Color(204, 153, 255)),
+			new ChoiceItem("Tan", "Tan", new Color(237, 226, 117)),
 			new ChoiceItem("Black", "Black", new Color(0, 0, 0)),
 			new ChoiceItem("DarkGray", "Dark Gray", new Color(78, 72, 72)),
-			new ChoiceItem("Brown", "Brown", new Color(200, 90, 23)),
-			new ChoiceItem("Tan", "Tan", new Color(237, 226, 117)),
-			new ChoiceItem("White", "White", new Color(255, 255, 255)),
-			new ChoiceItem("Red", "Red", new Color(255, 0, 0)),
-			new ChoiceItem("Pink", "Pink", new Color(255, 0, 255)),
-			new ChoiceItem("Orange", "Orange", new Color(255, 128, 64)),
-			new ChoiceItem("Yellow", "Yellow", new Color(255, 255, 0)),
-			new ChoiceItem("DarkGreen", "Dark Green", new Color(37, 65, 23)),
-			new ChoiceItem("LightGreen", "Light Green", new Color(95, 251, 23)),
+
+			new ChoiceItem("Red", "Dark Red", new Color(255, 0, 0)),
+			new ChoiceItem("DarkOrange", "Dark Orange", new Color(255, 0, 0)),
+			new ChoiceItem("DarkYellow", "Dark Yellow", new Color(255, 204, 0)),
+			new ChoiceItem("DarkGreen", "Dark Green", new Color(0, 128, 0)),
 			new ChoiceItem("DarkBlue", "Dark Blue", new Color(0, 0, 204)),
-			new ChoiceItem("LightBlue", "Light Blue", new Color(0, 204, 255)),
+			new ChoiceItem("DarkPurple", "Dark Purple", new Color(153, 0, 255)),
+			new ChoiceItem("Brown", "Brown", new Color(200, 90, 23)),
 		};
 	}
 }
