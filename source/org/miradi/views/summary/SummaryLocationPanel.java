a5835c200193718b2337f2ddc487296cccfef473
kevins <kevins@localhost>
2008-10-16 16:34:34 +0000

    Add missing EAM.text calls

--- source/org/miradi/views/summary/SummaryLocationPanel.java
+++ source/org/miradi/views/summary/SummaryLocationPanel.java
@@ -39,8 +39,8 @@ public class SummaryLocationPanel extends ObjectDataInputPanel
 	{
 		super(projectToUse, orefToUse);
 		
-		addFieldWithCustomLabelAndHint(createNumericField(ProjectMetadata.TAG_PROJECT_LATITUDE, 10), "(Latitude must be -90.0000 to +90.0000)");
-		addFieldWithCustomLabelAndHint(createNumericField(ProjectMetadata.TAG_PROJECT_LONGITUDE, 10), "(Longitude must be -180.0000 to +180.0000)");
+		addFieldWithCustomLabelAndHint(createNumericField(ProjectMetadata.TAG_PROJECT_LATITUDE, 10), EAM.text("(Latitude must be -90.0000 to +90.0000)"));
+		addFieldWithCustomLabelAndHint(createNumericField(ProjectMetadata.TAG_PROJECT_LONGITUDE, 10), EAM.text("(Longitude must be -180.0000 to +180.0000)"));
 		
 		addField(createCodeListField(ProjectMetadata.getObjectType(), ProjectMetadata.TAG_COUNTRIES, QuestionManager.getQuestion(CountriesQuestion.class), 1));
 		addField(createStringField(ProjectMetadata.TAG_STATE_AND_PROVINCES));
