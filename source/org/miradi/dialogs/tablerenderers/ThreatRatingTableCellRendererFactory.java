75683fd9939072eb4092409baed80a8f1d795bbd
kevins <kevins@localhost>
2008-11-03 19:48:39 +0000

    Implement showing detailed simple ratings in the table

--- source/org/miradi/dialogs/tablerenderers/ThreatRatingTableCellRendererFactory.java
+++ source/org/miradi/dialogs/tablerenderers/ThreatRatingTableCellRendererFactory.java
@@ -20,19 +20,89 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.tablerenderers;
 
 import java.awt.Color;
+import java.awt.Component;
 
-public class ThreatRatingTableCellRendererFactory extends ChoiceItemTableCellRendererFactory
+import javax.swing.Icon;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+
+import org.miradi.icons.BundleIcon;
+import org.miradi.icons.ColoredIcon;
+import org.miradi.main.AppPreferences;
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.FactorLink;
+import org.miradi.questions.ChoiceItem;
+
+public class ThreatRatingTableCellRendererFactory extends SingleLineObjectTableCellRendererFactory
 {
-	public ThreatRatingTableCellRendererFactory(RowColumnBaseObjectProvider providerToUse, FontForObjectTypeProvider fontProviderToUse)
+	public ThreatRatingTableCellRendererFactory(AppPreferences preferences, RowColumnBaseObjectProvider providerToUse, FontForObjectTypeProvider fontProviderToUse)
 	{
 		super(providerToUse, fontProviderToUse);
+		
+		stressBasedIcon = new ColoredIcon();
+		simpleIcon = new BundleIcon(preferences);
+		defaultBackgroundColor = Color.WHITE;
+	}
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int tableColumn)
+	{
+		JLabel renderer = (JLabel)super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, tableColumn);
+		String labelText = getLabelText(value);
+
+		if(!isSelected)
+			renderer.setBackground(getBackgroundColor(getChoiceItem(value)));
+
+		renderer.setText(labelText);
+
+		BaseObject object = getObjectProvider().getBaseObjectForRowColumn(row, tableColumn);
+		Icon configuredIcon = getConfiguredIcon(object, getChoiceItem(value));
+		renderer.setIcon(configuredIcon);
+		return renderer;
+	}
+
+	private String getLabelText(Object value)
+	{
+		ChoiceItem choice = getChoiceItem(value);
+		if(choice == null)
+			return "";
+		return choice.getLabel();
 	}
 
 	protected Color getBackgroundColor(Object value)
 	{
 		if(value == null)
 			return Color.GRAY.brighter();
-		return super.getBackgroundColor(getChoiceItem(value));
+		
+		ChoiceItem choice = getChoiceItem(value);
+		if(choice == null || choice.getColor() == null)
+			return defaultBackgroundColor;
+		
+		return choice.getColor();
 	}
 	
+	protected ChoiceItem getChoiceItem(Object value)
+	{
+		if(! (value instanceof ChoiceItem) )
+			return null;
+		return (ChoiceItem)value;
+	}
+
+	protected Icon getConfiguredIcon(BaseObject object, ChoiceItem choice)
+	{
+		if(choice == null || object == null || !FactorLink.is(object))
+			return null;
+		
+		if(object.getProject().isStressBaseMode())
+		{
+			stressBasedIcon.setColor(choice.getColor());
+			return stressBasedIcon;
+		}
+
+		simpleIcon.setLink((FactorLink)object);
+		return simpleIcon;
+	}
+	
+	private Color defaultBackgroundColor;
+	private ColoredIcon stressBasedIcon;
+	private BundleIcon simpleIcon;
 }
---
+++ source/org/miradi/icons/BundleIcon.java
@@ -0,0 +1,129 @@
+/* 
+Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.icons;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import org.miradi.main.AppPreferences;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objects.FactorLink;
+import org.miradi.project.Project;
+import org.miradi.project.SimpleThreatRatingFramework;
+import org.miradi.project.ThreatRatingBundle;
+import org.miradi.questions.ChoiceItem;
+import org.miradi.questions.ThreatRatingQuestion;
+
+public class BundleIcon extends AbstractMiradiIcon
+{
+	public BundleIcon(AppPreferences preferencesToUse)
+	{
+		preferences = preferencesToUse;
+	}
+	
+	public void setLink(FactorLink linkToUse)
+	{
+		link = linkToUse;
+	}
+
+	public void paintIcon(Component c, Graphics g, int x, int y)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+
+		int summaryX = 0;
+		if(preferences.getIsCellRatingsVisible())
+			summaryX = getSummaryX();
+		
+		ThreatRatingQuestion question = getThreatRatingQuestion();
+		try
+		{
+			int value = link.calculateThreatRatingBundleValue();
+			g.setColor(question.findChoiceByNumericValue(value).getColor());
+			g.fillRect(summaryX, 0, getIconWidth() - summaryX, getIconHeight());
+			
+			if(preferences.getIsCellRatingsVisible())
+				drawIndividualRatings(g);
+		}
+		catch(Exception e)
+		{
+			EAM.logException(e);
+		}
+	}
+
+	private ThreatRatingQuestion getThreatRatingQuestion()
+	{
+		return (ThreatRatingQuestion)getProject().getQuestion(ThreatRatingQuestion.class);
+	}
+
+	private void drawIndividualRatings(Graphics g) throws Exception
+	{
+		ThreatRatingQuestion question = getThreatRatingQuestion();
+		ThreatRatingBundle bundle = getBundle();
+		ChoiceItem scope = question.findChoiceByNumericValue(getFramework().getScopeNumericValue(bundle));
+		ChoiceItem severity = question.findChoiceByNumericValue(getFramework().getSeverityNumericValue(bundle));
+		ChoiceItem irreversibility = question.findChoiceByNumericValue(getFramework().getIrreversibilityNumericValue(bundle));
+
+		drawCriterionRectangle(g, 0, scope);
+		drawCriterionRectangle(g, 1, severity);
+		drawCriterionRectangle(g, 2, irreversibility);
+	}
+
+	private int getSummaryX()
+	{
+		return getIconWidth() / 3;
+	}
+
+	private void drawCriterionRectangle(Graphics g, int index, ChoiceItem choice)
+	{
+		int height = getIconHeight() / 3;
+		int top = height * index;
+		g.setColor(choice.getColor());
+		g.fillRect(0, top, getSummaryX(), height);
+		g.setColor(Color.BLACK);
+		g.drawRect(0, top, getSummaryX(), height);
+	}
+
+	private ThreatRatingBundle getBundle() throws Exception
+	{
+		ORef threatRef = link.getUpstreamThreatRef();
+		ORef targetRef = link.getDownstreamTargetRef();
+		return getFramework().getBundle(threatRef, targetRef);
+	}
+
+	private SimpleThreatRatingFramework getFramework()
+	{
+		return getProject().getSimpleThreatRatingFramework();
+	}
+	
+	private Project getProject()
+	{
+		return link.getProject();
+	}
+
+	private FactorLink link;
+	private AppPreferences preferences;
+}
