0251bc46189318c15990e6e319c318f0fc9065fc
nimaa <nimaa@localhost>
2008-11-11 20:38:52 +0000

    removed FIXME. FIXME was to combine this class with another one and remove the other one.

--- source/org/miradi/views/umbrella/doers/AbstractEditListDoer.java
+++ source/org/miradi/views/umbrella/doers/AbstractEditListDoer.java
@@ -26,8 +26,7 @@ import org.miradi.exceptions.CommandFailedException;
 import org.miradi.objects.BaseObject;
 import org.miradi.utils.ModalDialogWithCloseButNoMainScrollPane;
 import org.miradi.views.ObjectsDoer;
-
-//FIXME what is the difference between this class and AbstractEditListDoer?  notice the missing t in list 
+ 
 public abstract class AbstractEditListDoer extends ObjectsDoer
 {
 	public boolean isAvailable()
