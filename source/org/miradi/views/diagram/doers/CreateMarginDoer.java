4c4a363a981125ecec2938e9c39c79a26714da29
nimaa <nimaa@localhost>
2008-11-12 18:52:17 +0000

    added create margin doer functionality.

--- source/org/miradi/views/diagram/doers/CreateMarginDoer.java
+++ source/org/miradi/views/diagram/doers/CreateMarginDoer.java
@@ -19,15 +19,23 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.views.diagram.doers;
 
+import java.awt.Rectangle;
+import java.util.Vector;
+
+import org.miradi.diagram.cells.FactorCell;
 import org.miradi.exceptions.CommandFailedException;
 import org.miradi.views.ObjectsDoer;
+import org.miradi.views.diagram.NudgeDoer;
 
 public class CreateMarginDoer extends ObjectsDoer
 {
 	@Override
 	public boolean isAvailable()
 	{
-		return false;
+		if (!isInDiagram())
+			return false;
+		
+		return allowMargin();
 	}
 
 	@Override
@@ -35,5 +43,60 @@ public class CreateMarginDoer extends ObjectsDoer
 	{
 		if (!isAvailable())
 			return;
+		
+		try
+		{
+			Rectangle allDiagramFactorBounds = getDiagramFactorBounds();
+			int leftMarginToCreate = getCreateMarginAmount(allDiagramFactorBounds.x, MINIMUM_LEFT_MARGIN);
+			int topMarginToCreate = getCreateMarginAmount(allDiagramFactorBounds.y, MINIMUM_TOP_MARGIN);
+			
+			getDiagramView().getCurrentDiagramComponent().selectAll();
+			NudgeDoer.moveSelectedItems(getProject(), getDiagramView().getDiagramPanel(), leftMarginToCreate, topMarginToCreate);
+			getDiagramView().getCurrentDiagramComponent().clearSelection();
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
 	}
+	
+	private int getCreateMarginAmount(int currentPosition, int minimumMargin)
+	{
+		if (currentPosition < minimumMargin)
+			return CREATE_MARGIN_AMOUNT;
+		
+		return 0;
+	}
+	
+	private boolean allowMargin()
+	{
+		Rectangle diagramFactorBounds = getDiagramFactorBounds();
+		if (diagramFactorBounds.getX() < MINIMUM_LEFT_MARGIN)
+			return true;
+		
+		if (diagramFactorBounds.getY() < MINIMUM_TOP_MARGIN)
+			return true;
+		
+		return false;
+	}
+
+	private Rectangle getDiagramFactorBounds()
+	{
+		Vector<FactorCell> allFactorCells = getDiagramView().getDiagramModel().getAllFactorCells();
+		Rectangle allCellsBound = null;
+		for (int index = 0; index < allFactorCells.size(); ++index)
+		{
+			Rectangle factorCellBounds = (Rectangle) allFactorCells.get(index).getDiagramFactor().getBounds().clone();
+			if (allCellsBound == null)
+				allCellsBound = new Rectangle(factorCellBounds);
+			
+			allCellsBound = allCellsBound.union(factorCellBounds);			
+		}
+		
+		return allCellsBound;
+	}
+	
+	private static final int MINIMUM_TOP_MARGIN = 30;
+	private static final int MINIMUM_LEFT_MARGIN = 30;
+	private static final int CREATE_MARGIN_AMOUNT = 30;
 }
