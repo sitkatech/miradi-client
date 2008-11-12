5c1a40bd9901fa68c2ae6e99a17a92a1a6dae271
nimaa <nimaa@localhost>
2008-11-12 20:06:02 +0000

    refactor pulled up progress percent vars and methods from objective to desire. This is due to goals having progress percent as well.

--- source/org/miradi/objects/Objective.java
+++ source/org/miradi/objects/Objective.java
@@ -21,7 +21,6 @@ package org.miradi.objects;
 
 import org.miradi.ids.BaseId;
 import org.miradi.ids.ObjectiveId;
-import org.miradi.objectdata.ORefListData;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ObjectType;
 import org.miradi.project.ObjectManager;
@@ -63,55 +62,6 @@ public class Objective extends Desire
 		return false;
 	}
 	
-	public int getAnnotationType(String tag)
-	{
-		if (tag.equals(TAG_PROGRESS_PERCENT_REFS))
-			return ProgressPercent.getObjectType();
-				
-		return super.getAnnotationType(tag);
-	}
-	
-	public boolean isRefList(String tag)
-	{
-		if (tag.equals(TAG_PROGRESS_PERCENT_REFS))
-			return true;
-				
-		return super.isRefList(tag);
-	}
-
-	public String getPseudoData(String fieldTag)
-	{
-		if (fieldTag.equals(PSEUDO_TAG_LATEST_PROGRESS_PERCENT_COMPLETE))
-			return getLatestProgressPercentComplete();
-		
-		if (fieldTag.equals(PSEUDO_TAG_LATEST_PROGRESS_PERCENT_DETAILS))
-			return getLatestProgressPercentDetails();
-		
-		return super.getPseudoData(fieldTag);
-	}
-	
-	
-	private String getLatestProgressPercentDetails()
-	{
-		ProgressPercent latestProgressPercent = getLatestProgressPercent();
-		if(latestProgressPercent == null)
-			return "";
-		return latestProgressPercent.getData(ProgressPercent.TAG_PERCENT_COMPLETE_NOTES);
-	}
-
-	private String getLatestProgressPercentComplete()
-	{
-		ProgressPercent latestProgressPercent = getLatestProgressPercent();
-		if(latestProgressPercent == null)
-			return "";
-		return latestProgressPercent.getData(ProgressPercent.TAG_PERCENT_COMPLETE);
-	}
-	
-	private ProgressPercent getLatestProgressPercent()
-	{
-		return (ProgressPercent) getLatestObject(getObjectManager(), progressPercentRefs.getORefList(), ProgressPercent.TAG_DATE);
-	}
-
 	public static Objective find(ObjectManager objectManager, ORef objectiveRef)
 	{
 		return (Objective) objectManager.findObject(objectiveRef);
@@ -136,30 +86,6 @@ public class Objective extends Desire
 	{
 		return is(ref.getObjectType());
 	}
-
-	public void clear()
-	{
-		super.clear();
-		progressPercentRefs = new ORefListData(TAG_PROGRESS_PERCENT_REFS);
-		
-		latestProgressPercentComplete = new PseudoStringData(PSEUDO_TAG_LATEST_PROGRESS_PERCENT_COMPLETE);
-		latestProgressPercentDetails = new PseudoStringData(PSEUDO_TAG_LATEST_PROGRESS_PERCENT_DETAILS);
-		
-		addField(TAG_PROGRESS_PERCENT_REFS, progressPercentRefs);
-		
-		addField(PSEUDO_TAG_LATEST_PROGRESS_PERCENT_COMPLETE, latestProgressPercentComplete);
-		addField(PSEUDO_TAG_LATEST_PROGRESS_PERCENT_DETAILS, latestProgressPercentDetails);
-	}
 	
 	public static final String OBJECT_NAME = "Objective";
-	
-	public static final String TAG_PROGRESS_PERCENT_REFS = "ProgressPrecentRefs";
-	
-	public static final String PSEUDO_TAG_LATEST_PROGRESS_PERCENT_COMPLETE = "PseudoLatestProgressPercentComplete";
-	public static final String PSEUDO_TAG_LATEST_PROGRESS_PERCENT_DETAILS = "PseudoLatestProgressPercentDetails";
-	
-	private ORefListData progressPercentRefs;
-	
-	private PseudoStringData latestProgressPercentComplete;
-	private PseudoStringData latestProgressPercentDetails;
 }
