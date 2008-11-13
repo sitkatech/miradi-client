52c42590c9c6337afdcdef6b07776bb33d4b7ea4
nimaa <nimaa@localhost>
2008-11-13 15:08:03 +0000

    added ID (shortLabel) field to properties panel

--- source/org/miradi/dialogs/task/TaskDetailsPanel.java
+++ source/org/miradi/dialogs/task/TaskDetailsPanel.java
@@ -47,7 +47,8 @@ public class TaskDetailsPanel extends ObjectDataInputPanel
 		
 		taskNameLabel = new PanelTitleLabel("x");
 		ObjectDataInputField taskNameField = createExpandableField(ObjectType.TASK, Task.TAG_LABEL);
-		addFieldsOnOneLine(taskNameLabel, new ObjectDataInputField[] {taskNameField,} );
+		ObjectDataInputField taskIdField = createExpandableField(ObjectType.TASK, Task.TAG_SHORT_LABEL);
+		addFieldsOnOneLine(taskNameLabel, new ObjectDataInputField[] {taskNameField, taskIdField,} );
 		addField(createMultilineField(Task.getObjectType(), Task.TAG_DETAILS));
 
 		progressReportsLabel = new PanelTitleLabel(EAM.text("Progress Reports"));
