1c8748c964081c39323d821b8e1b822b3303fc8a
kevins <kevins@localhost>
2008-11-25 02:50:41 +0000

    Remove Measurements-only, add Targets and Threats-only

--- source/org/miradi/questions/PlanningViewSingleLevelQuestion.java
+++ source/org/miradi/questions/PlanningViewSingleLevelQuestion.java
@@ -20,11 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.questions;
 
 import org.miradi.main.EAM;
+import org.miradi.objects.Cause;
 import org.miradi.objects.Goal;
 import org.miradi.objects.Indicator;
-import org.miradi.objects.Measurement;
 import org.miradi.objects.Objective;
 import org.miradi.objects.Strategy;
+import org.miradi.objects.Target;
 import org.miradi.objects.Task;
 
 public class PlanningViewSingleLevelQuestion extends StaticChoiceQuestion
@@ -39,11 +40,12 @@ public class PlanningViewSingleLevelQuestion extends StaticChoiceQuestion
 		return new ChoiceItem[] {
 				new ChoiceItem(Goal.OBJECT_NAME, EAM.text("Goals Only")),
 				new ChoiceItem(Objective.OBJECT_NAME, EAM.text("Objectives Only")),
+				new ChoiceItem(Target.OBJECT_NAME, EAM.text("Targets Only")),
+				new ChoiceItem(Cause.OBJECT_NAME_THREAT, EAM.text("Direct Threats Only")),
 				new ChoiceItem(Strategy.OBJECT_NAME, EAM.text("Strategies Only")),
 				new ChoiceItem(Task.ACTIVITY_NAME, EAM.text("Activities Only")),
 				new ChoiceItem(Indicator.OBJECT_NAME, EAM.text("Indicators Only")),
 				new ChoiceItem(Task.METHOD_NAME, EAM.text("Methods Only")),
-				new ChoiceItem(Measurement.OBJECT_NAME, EAM.text("Measurements Only")),
 		};
 	}
 }
