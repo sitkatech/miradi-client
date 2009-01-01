bca0810cdc3076855d599055d856e91fafa6bd1a
kevins <kevins@localhost>
2009-01-01 18:41:35 +0000

    Make MultiTableModel more generic by pulling Planning-specific stuff into its own subclass

---
+++ source/org/miradi/dialogs/planning/upperPanel/PlanningTreeMultiTableModel.java
@@ -0,0 +1,48 @@
+/* 
+Copyright 2005-2008, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.planning.upperPanel;
+
+import javax.swing.table.TableModel;
+
+import org.miradi.utils.ColumnTagProvider;
+
+public class PlanningTreeMultiTableModel extends MultiTableModel implements ColumnTagProvider
+{
+	@Override
+	public void addModel(TableModel modelToAdd)
+	{
+		throw new RuntimeException(getClass().getName() + ".addModel: Model must be a ColumnTagProvider");
+	}
+	
+	public void addModel(TableModelWithColumnTagProvider modelToAdd)
+	{
+		// TODO Auto-generated method stub
+		super.addModel(modelToAdd);
+	}
+	
+	public String getColumnTag(int column)
+	{
+		final ColumnTagProvider provider = (ColumnTagProvider) findTable(column);
+		return provider.getColumnTag(findColumnWithinSubTable(column));
+	}
+
+
+}
