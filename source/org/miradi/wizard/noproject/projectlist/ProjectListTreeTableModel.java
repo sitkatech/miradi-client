43e44bf503f04a7b1b6413101d234eb366a23d60
nimaa <nimaa@localhost>
2009-01-09 13:53:31 +0000

    text change

--- source/org/miradi/wizard/noproject/projectlist/ProjectListTreeTableModel.java
+++ source/org/miradi/wizard/noproject/projectlist/ProjectListTreeTableModel.java
@@ -45,5 +45,5 @@ public class ProjectListTreeTableModel extends GenericTreeTableModel
 		return COLUMN_NAMES[column];
 	}
 
-	String[] COLUMN_NAMES = {EAM.text("Project"), EAM.text("Last Opened"), };
+	String[] COLUMN_NAMES = {EAM.text("Project"), EAM.text("Last Modified"), };
 }
