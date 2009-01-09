cf69cc1b467d9a29d569dfb0fbda085beb7fbf8c
nimaa <nimaa@localhost>
2009-01-09 20:53:50 +0000

    adding colored label to choiceItem

--- source/org/miradi/dialogs/viability/ViabilityTreeTableExporter.java
+++ source/org/miradi/dialogs/viability/ViabilityTreeTableExporter.java
@@ -21,6 +21,7 @@ package org.miradi.dialogs.viability;
 
 import javax.swing.Icon;
 
+import org.miradi.icons.ColoredIcon;
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.EmptyChoiceItem;
 import org.miradi.utils.TreeTableExporter;
@@ -58,7 +59,13 @@ public class ViabilityTreeTableExporter extends TreeTableExporter
 
 		int modelColumn = getViabilityTreeTable().convertColumnIndexToModel(column);
 		if (getViabilityTreeTable().isChoiceItemCell(row, modelColumn))
-			return (ChoiceItem) valueAt;	
+		{
+			ChoiceItem choiceItem = (ChoiceItem) valueAt;
+			ColoredIcon rowRatingIcon = new ColoredIcon(choiceItem.getColor());
+			choiceItem.setIcon(rowRatingIcon);
+						
+			return choiceItem;	
+		}
 		
 		return super.getChoiceItemAt(row, modelColumn);
 	}
