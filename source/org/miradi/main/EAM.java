caf9ffc8faf08c2feb66d501747cdf81a5f484bf
nimaa <nimaa@localhost>
2009-01-22 14:13:31 +0000

    converted method to check if file is inside other file.

--- source/org/miradi/main/EAM.java
+++ source/org/miradi/main/EAM.java
@@ -85,12 +85,15 @@ public class EAM
 		return defaultHomeDirectory;
 	}
 	
-	public static boolean isOneFileInsideTheOther(File containingDir, File dirToTest)
+	public static boolean isOneFileInsideTheOther(File file1, File file2)
 	{
-		String homeDirectory = containingDir.getAbsolutePath();
-		String parentOfChosen = dirToTest.getAbsolutePath();
-		//FIXME make sure this method goes the other way around as well
-		return parentOfChosen.startsWith(homeDirectory);
+		String file1AsString = file1.getAbsolutePath();
+		String file2AsString = file2.getAbsolutePath();
+		
+		if (file1AsString.length() <= file2AsString.length())
+			return file2AsString.startsWith(file1AsString);
+		
+		return file1AsString.startsWith(file2AsString);
 	}
 
 	private static void alertIfHomeIsNotOnC()
