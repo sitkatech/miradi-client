a965b4843d8fb971b76f78a00a7c01b7c95fda0e
kevins <kevins@localhost>
2009-01-23 18:42:13 +0000

    Revise Stress-based threat properties panel to be similar to new simple panel layout

--- source/org/miradi/dialogs/threatrating/properties/ThreatRatingCommonPropertiesSubpanel.java
+++ source/org/miradi/dialogs/threatrating/properties/ThreatRatingCommonPropertiesSubpanel.java
@@ -20,17 +20,9 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.threatrating.properties;
 
 import org.miradi.actions.Actions;
-import org.miradi.diagram.factortypes.FactorType;
-import org.miradi.dialogfields.ObjectDataInputField;
 import org.miradi.dialogs.base.ObjectDataInputPanel;
-import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
 import org.miradi.main.CommandExecutedEvent;
-import org.miradi.main.EAM;
-import org.miradi.objectdata.BooleanData;
 import org.miradi.objecthelpers.ORef;
-import org.miradi.objecthelpers.ORefList;
-import org.miradi.objecthelpers.ObjectType;
-import org.miradi.objects.Factor;
 import org.miradi.objects.FactorLink;
 import org.miradi.objects.ProjectMetadata;
 import org.miradi.project.Project;
@@ -42,21 +34,7 @@ public class ThreatRatingCommonPropertiesSubpanel extends ObjectDataInputPanel
 	{
 		super(projectToUse, ORef.INVALID);
 		
-		fromLabel = new PanelTitleLabel();
-		fromNameField = createReadonlyTextField(ObjectType.FAKE, Factor.TAG_LABEL);
-		addFieldWithCustomLabel(fromNameField, fromLabel);
-
-		toLabel = new PanelTitleLabel();
-		toNameField = createReadonlyTextField(ObjectType.FAKE, Factor.TAG_LABEL);
-		addFieldWithCustomLabel(toNameField, toLabel);
-
-		addField(createCheckBoxField(FactorLink.getObjectType(), FactorLink.TAG_BIDIRECTIONAL_LINK, BooleanData.BOOLEAN_TRUE, BooleanData.BOOLEAN_FALSE));
-
-		commentsField = createMultilineField(FactorLink.getObjectType(), FactorLink.getCommentTagForMode(getProject()));
-		addField(commentsField);
 		addField(createReadOnlyChoiceField(FactorLink.getObjectType(), FactorLink.PSEUDO_TAG_THREAT_RATING_BUNDLE_VALUE, new ThreatRatingQuestion()));
-
-		addBlankHorizontalLine();
 		
 		updateFieldsFromProject();
 	}
@@ -71,58 +49,9 @@ public class ThreatRatingCommonPropertiesSubpanel extends ObjectDataInputPanel
 		}
 	}
 	
-	@Override
-	public void setObjectRefs(ORef[] orefsToUse)
-	{
-		updateFieldLabels(orefsToUse);
-		super.setObjectRefs(orefsToUse);
-	}
-
-	private void updateFieldLabels(ORef[] orefsToUse)
-	{
-		ORefList refs = new ORefList(orefsToUse);
-		
-		ORef linkRef = refs.getRefForType(FactorLink.getObjectType());
-		if(linkRef.isInvalid())
-		{
-			fromLabel.setText("");
-			fromLabel.setIcon(null);
-			fromNameField.setObjectType(ObjectType.FAKE);
-			toLabel.setText("");
-			toLabel.setIcon(null);
-			toNameField.setObjectType(ObjectType.FAKE);
-			return;
-		}
-		
-		try
-		{
-			FactorLink link = FactorLink.find(getProject(), linkRef);
-
-			Factor fromFactor = Factor.findFactor(getProject(), link.getFromFactorRef());
-			fromLabel.setText(FactorType.getFactorTypeLabel(fromFactor));
-			fromLabel.setIcon(FactorType.getFactorIcon(fromFactor));
-			fromNameField.setObjectType(fromFactor.getType());
-
-			Factor toFactor = Factor.findFactor(getProject(), link.getToFactorRef());
-			toLabel.setText(FactorType.getFactorTypeLabel(toFactor));
-			toLabel.setIcon(FactorType.getFactorIcon(toFactor));
-			toNameField.setObjectType(toFactor.getType());
-			commentsField.setTag(FactorLink.getCommentTagForMode(getProject()));	
-		}
-		catch(Exception e)
-		{
-			EAM.panic(e);
-		}
-	}
-
 	public String getPanelDescription()
 	{
 		return "ThreatRatingCommonPropertiesSubpanel";
 	}
 	
-	private PanelTitleLabel fromLabel;
-	private PanelTitleLabel toLabel;
-	private ObjectDataInputField fromNameField;
-	private ObjectDataInputField toNameField;
-	private ObjectDataInputField commentsField;
 }
