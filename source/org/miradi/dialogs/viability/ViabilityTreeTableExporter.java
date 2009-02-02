3577457c025ac3e8bcddd048f8f5c32be28e7b3e
nimaa <nimaa@localhost>
2009-02-02 14:57:42 +0000

    removed duplicate code.  Same code appears in getChoiceItemAt

--- source/org/miradi/dialogs/viability/ViabilityTreeTableExporter.java
+++ source/org/miradi/dialogs/viability/ViabilityTreeTableExporter.java
@@ -19,8 +19,6 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.viability;
 
-import javax.swing.Icon;
-
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.EmptyChoiceItem;
 import org.miradi.utils.TreeTableExporter;
@@ -30,23 +28,6 @@ public class ViabilityTreeTableExporter extends TreeTableExporter
 	public ViabilityTreeTableExporter(TargetViabilityTreeTable viabilityTreeTable)
 	{
 		super(viabilityTreeTable);
-	}
-	
-	@Override
-	public Icon getIconAt(int row, int tableColumn)
-	{
-		Object valueAt = getViabilityTreeTable().getValueAt(row, tableColumn);
-		if (valueAt == null)
-			return null;
-
-		int modelColumn = getViabilityTreeTable().convertColumnIndexToModel(tableColumn);
-		if (getViabilityTreeTable().isChoiceItemCell(row, modelColumn))
-		{
-			ChoiceItem choiceItem = (ChoiceItem) valueAt;
-			return choiceItem.getIcon();	
-		}
-		
-		return super.getIconAt(row, modelColumn);
 	}
 	
 	@Override
