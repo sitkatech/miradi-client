66de62133d1d914323a5fb1efdeac774569adc17
nimaa <nimaa@localhost>
2009-02-10 16:17:26 +0000

    text change

--- source/org/miradi/views/noproject/DeleteProject.java
+++ source/org/miradi/views/noproject/DeleteProject.java
@@ -58,6 +58,6 @@ public class DeleteProject
 		if (ProjectServer.isExistingProject(projectToDelete))
 			return EAM.text("Are you sure you want to delete this project? ");
 		
-		return EAM.text("Are you sure you want to delete this directory and all of its content?");
+		return EAM.text("Are you sure you want to delete this directory and its contents, including all subdirectories?");
 	}
 }
