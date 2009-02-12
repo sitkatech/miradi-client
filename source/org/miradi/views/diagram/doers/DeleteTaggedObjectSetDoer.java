aefd621782a62571b8f3cd9fddd14eddf491c461
nimaa <nimaa@localhost>
2009-02-12 02:00:25 +0000

    bug fix.  removing tag from any diagram object tag selected list.

--- source/org/miradi/views/diagram/doers/DeleteTaggedObjectSetDoer.java
+++ source/org/miradi/views/diagram/doers/DeleteTaggedObjectSetDoer.java
@@ -19,7 +19,13 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.views.diagram.doers;
 
+import org.miradi.commands.CommandSetObjectData;
 import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.ConceptualModelDiagram;
+import org.miradi.objects.DiagramObject;
+import org.miradi.objects.ResultsChainDiagram;
 import org.miradi.views.umbrella.doers.DeletePoolObjectDoer;
 
 public class DeleteTaggedObjectSetDoer extends DeletePoolObjectDoer
@@ -28,5 +34,18 @@ public class DeleteTaggedObjectSetDoer extends DeletePoolObjectDoer
 	protected String getCustomText()
 	{
 		return EAM.text("Tag");
+	}
+	
+	@Override
+	protected void doWork(BaseObject objectToDelete) throws Exception
+	{
+		ORefList referringDiagramObjectRefs = objectToDelete.findObjectsThatReferToUs(ResultsChainDiagram.getObjectType());
+		referringDiagramObjectRefs.addAll(objectToDelete.findObjectsThatReferToUs(ConceptualModelDiagram.getObjectType()));
+		for (int index = 0; index < referringDiagramObjectRefs.size(); ++index)
+		{
+			DiagramObject diagramObject = DiagramObject.findDiagramObject(getProject(), referringDiagramObjectRefs.get(index));
+			CommandSetObjectData removeTagFromSelection =  CommandSetObjectData.createRemoveORefCommand(diagramObject, DiagramObject.TAG_SELECTED_TAGGED_OBJECT_SET_REFS, objectToDelete.getRef());
+			getProject().executeCommand(removeTagFromSelection);
+		}
 	}
 }
