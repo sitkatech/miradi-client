c8dbca5529f45efc81551d0b9acd6092c4e0705f
nimaa <nimaa@localhost>
2009-02-23 15:59:59 +0000

    text change

--- source/org/miradi/project/Project.java
+++ source/org/miradi/project/Project.java
@@ -1040,7 +1040,7 @@ public class Project
 		if(!isInCommandSideEffectMode())
 		{
 			EAM.friendlyInternalError("<html>" + 
-									  	EAM.text("A minor data adjustment has occurred.") + 
+									  	EAM.text("An unexpected error has occurred.") + 
 									  	EAM.text("<br> The operation completed successfully, but Undo/Redo may not work correctly.") +
 									  	EAM.text("<br> Please report this problem to the Miradi support team ") +
 									  	"(<a href=\"mailto:support@miradi.org\">support@miradi.org<a>)" + 
