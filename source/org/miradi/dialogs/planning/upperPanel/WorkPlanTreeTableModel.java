f033aa26c8118d84ae68ee7148b6e41a36293b74
nimaa <nimaa@localhost>
2009-04-01 21:38:19 +0000

    new work plan in operational view

---
+++ source/org/miradi/dialogs/planning/upperPanel/WorkPlanTreeTableModel.java
@@ -0,0 +1,37 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.dialogs.planning.upperPanel;
+
+import org.miradi.project.Project;
+import org.miradi.views.planning.ColumnManager;
+import org.miradi.views.planning.RowManager;
+
+public class WorkPlanTreeTableModel extends PlanningTreeTableModel
+{
+	public WorkPlanTreeTableModel(Project project) throws Exception
+	{
+		super(project, RowManager.getWorkPlanRows(), ColumnManager.getWorkPlanColumns());
+	}
+
+	@Override
+	public void updateColumnsToShow() throws Exception
+	{
+	}
+}
