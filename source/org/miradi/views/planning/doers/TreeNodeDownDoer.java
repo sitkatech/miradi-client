9d90d61e7c9f1c23337767efe4571f51d61ddc9e
nimaa <nimaa@localhost>
2009-04-08 19:55:54 +0000

    refactor rename class

--- source/org/miradi/views/planning/doers/TreeNodeDownDoer.java
+++ source/org/miradi/views/planning/doers/TreeNodeDownDoer.java
@@ -20,7 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.views.planning.doers;
 
-public class TreeNodeDownDoer extends AbstractTreeNodeMoveDoer
+public class TreeNodeDownDoer extends AbstractTreeTaskNodeMoveDoer
 {
 	protected int getDelta()
 	{
