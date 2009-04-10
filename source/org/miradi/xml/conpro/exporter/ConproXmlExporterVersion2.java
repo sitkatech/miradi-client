313d6c5d111ad29b8bbfb0c09b33e29ff60e37f1
nimaa <nimaa@localhost>
2009-04-10 14:02:30 +0000

    added sequence atrribute to progress percent element

--- source/org/miradi/xml/conpro/exporter/ConproXmlExporterVersion2.java
+++ source/org/miradi/xml/conpro/exporter/ConproXmlExporterVersion2.java
@@ -360,7 +360,7 @@ public class ConproXmlExporterVersion2 extends XmlExporter implements ConProMira
 		writeStartElement(out, PROGRESS_PERCENT_REPORTS);
 		for (int index = 0; index < progressPercentRefs.size(); ++index)
 		{
-			writeStartElement(out, PROGRESS_PERCENT_REPORT);
+			writeStartElementWithAttribute(out, PROGRESS_PERCENT_REPORT, SEQUENCE, index + 1);
 			
 			ProgressPercent progressPercent = ProgressPercent.find(getProject(), progressPercentRefs.get(index));
 			writeOptionalElement(out, PROGRESS_PERCENT_COMPLETE, progressPercent, ProgressPercent.TAG_PERCENT_COMPLETE);
