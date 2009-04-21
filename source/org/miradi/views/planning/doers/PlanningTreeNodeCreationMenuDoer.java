ba1d30c54957602cda09a4c68a3648ddec76a602
nimaa <nimaa@localhost>
2009-04-21 14:29:54 +0000

    refactor move class

--- source/org/miradi/views/planning/doers/PlanningTreeNodeCreationMenuDoer.java
+++ source/org/miradi/views/planning/doers/PlanningTreeNodeCreationMenuDoer.java
@@ -26,6 +26,7 @@ import org.miradi.actions.ActionTreeCreateObjective;
 import org.miradi.actions.ActionTreeCreateTask;
 import org.miradi.actions.ActionTreeShareActivity;
 import org.miradi.actions.ActionTreeShareMethod;
+import org.miradi.views.umbrella.doers.AbstractMenuDoer;
 
 public class PlanningTreeNodeCreationMenuDoer extends AbstractMenuDoer
 {
