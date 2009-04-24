5000ece4c4f75a8b2a9bcac7c64f7ca9d0293fb7
nimaa <nimaa@localhost>
2009-04-24 15:57:35 +0000

    refactor pull up all methods

--- source/org/miradi/dialogs/planning/propertiesPanel/PlanningViewWorkPlanTableModel.java
+++ source/org/miradi/dialogs/planning/propertiesPanel/PlanningViewWorkPlanTableModel.java
@@ -20,86 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.planning.propertiesPanel;
 
 
-import org.miradi.commands.Command;
-import org.miradi.commands.CommandSetObjectData;
-import org.miradi.main.EAM;
-import org.miradi.objecthelpers.DateRangeEffortList;
-import org.miradi.objects.Assignment;
 import org.miradi.project.Project;
-import org.miradi.project.ProjectCalendar;
-import org.miradi.utils.DateRange;
-import org.miradi.utils.DateRangeEffort;
 
 public class PlanningViewWorkPlanTableModel extends PlanningViewAbstractBudgetTableModel
 {
 	public PlanningViewWorkPlanTableModel(Project projectToUse) throws Exception
 	{
 		super(projectToUse);
-	}	
-	
-	public boolean isCellEditable(int row, int column)
-	{
-		DateRange dateRange = getDateRangeForColumn(column);
-		ProjectCalendar projectCalendar = getProject().getProjectCalendar();
-		return projectCalendar.isDateRangeEditable(dateRange);
-	}
-	
-	public Object getValueAt(int row, int column)
-	{
-		try
-		{
-			return getUnits(row, column);
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-		}
-
-		return "";
-	}
-	
-	public void setValueAt(Object value, int row, int column)
-	{
-		try
-		{
-			setUnitsForColumn(value, row, column);
-		}
-		catch (Exception e)
-		{
-			EAM.logException(e);
-		}
-	}
-	
-	public void setUnitsForColumn(Object value, int row, int column) throws Exception
-	{
-		DateRangeEffort effort = getDateRangeEffort(row, column);
-		double units = 0;
-		if (effort == null)
-			effort = new DateRangeEffort("", units, dateRanges[column]);
-
-		String valueAsString = value.toString().trim();
-		if (! valueAsString.equals(""))
-			units = Double.parseDouble(valueAsString);
-
-		Assignment assignment = getAssignment(row);
-		DateRangeEffortList effortList = getDateRangeEffortList(row);
-		setUnits(assignment, effortList, effort, units);
-	}
-	
-	public void setUnits(Assignment assignment, DateRangeEffortList effortList, DateRangeEffort effort, double units) throws Exception
-	{
-		effort.setUnitQuantity(units);
-		effortList.setDateRangeEffort(effort);
-		String newEffortListString = effortList.toString();
-		if(newEffortListString.equals(assignment.getData(assignment.TAG_DATERANGE_EFFORTS)))
-			return;
-		
-		Command command = new CommandSetObjectData(assignment.getType(), assignment.getId(), assignment.TAG_DATERANGE_EFFORTS, newEffortListString);
-		getProject().executeCommand(command);
-	}
-	
-	private DateRangeEffort getDateRangeEffort(int row, int column) throws Exception
-	{
-		return getDateRangeEffort(getAssignment(row), dateRanges[column]);
 	}
 }
