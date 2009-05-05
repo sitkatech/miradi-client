11d2d981fb137b07a882c3827843bcf851675434
nimaa <nimaa@localhost>
2009-05-05 21:36:49 +0000

    pulled up common code

--- source/org/miradi/dialogs/planning/propertiesPanel/ExpenseEditorComponent.java
+++ source/org/miradi/dialogs/planning/propertiesPanel/ExpenseEditorComponent.java
@@ -20,24 +20,18 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.planning.propertiesPanel;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
 import org.martus.swing.UiScrollPane;
 import org.miradi.actions.ActionAssignResource;
 import org.miradi.actions.ActionRemoveAssignment;
-import org.miradi.actions.Actions;
-import org.miradi.dialogs.treetables.MultiTreeTablePanel.ScrollPaneWithHideableScrollBar;
 import org.miradi.layout.OneRowPanel;
 import org.miradi.main.AppPreferences;
-import org.miradi.main.CommandExecutedEvent;
-import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
 import org.miradi.objects.BaseObject;
-import org.miradi.objects.TableSettings;
 import org.miradi.views.umbrella.ObjectPicker;
 
 //FIXME this class is duplicate and under constrcut
@@ -45,25 +39,18 @@ public class ExpenseEditorComponent extends AbstractMultiTablePanelEditorCompone
 {
 	public ExpenseEditorComponent(MainWindow mainWindowToUse, ObjectPicker objectPickerToUse) throws Exception
 	{
-		super(mainWindowToUse);
+		super(mainWindowToUse, objectPickerToUse);
 		setBackground(AppPreferences.getDataPanelBackgroundColor());
 		
-		mainWindow = mainWindowToUse;
-		objectPicker = objectPickerToUse;
 		createTables();
 		addTables();
 		addTablesToSelectionController();
-		
-		getProject().addCommandExecutedListener(this);
 	}
 	
 	@Override
 	public void dispose()
 	{
 		super.dispose();
-		becomeInactive();
-		getProject().removeCommandExecutedListener(this);
-		
 		resourceTable.dispose();
 		workUnitsTable.dispose();
 	}
@@ -113,10 +100,10 @@ public class ExpenseEditorComponent extends AbstractMultiTablePanelEditorCompone
 	private void createTables() throws Exception
 	{
 		resourceTableModel = new PlanningViewResourceTableModel(getProject());
-		resourceTable = new PlanningViewResourceTable(mainWindow, resourceTableModel);
+		resourceTable = new PlanningViewResourceTable(getMainWindow(), resourceTableModel);
 		
 		workUnitsTableModel = new WorkUnitsTableModel(getProject(), resourceTableModel);
-		workUnitsTable = new WorkUnitsTable(mainWindow, workUnitsTableModel);		
+		workUnitsTable = new WorkUnitsTable(getMainWindow(), workUnitsTableModel);		
 	}
 	
 	private void addTables()
@@ -130,42 +117,6 @@ public class ExpenseEditorComponent extends AbstractMultiTablePanelEditorCompone
 		add(createButtonBar(), BorderLayout.BEFORE_FIRST_LINE);
 	}
 	
-	static class AssignmentsComponentTableScrollPane extends ScrollPaneWithHideableScrollBar
-	{
-		public AssignmentsComponentTableScrollPane(AssignmentsComponentTable contents)
-		{
-			super(contents);
-			setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_ALWAYS);
-			setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_NEVER);
-			widthSetter = new PersistentWidthSetterComponent(contents.getMainWindow(), this, contents.getUniqueTableIdentifier(), getPreferredSize().width);
-		}
-		
-		public PersistentWidthSetterComponent getWidthSetterComponent()
-		{
-			return widthSetter;
-		}
-		
-		@Override
-		public Dimension getPreferredSize()
-		{
-			final Dimension size = super.getPreferredSize();
-			if(widthSetter != null)
-				size.width = widthSetter.getControlledWidth();
-			return size;
-		}
-		
-		@Override
-		public Dimension getSize()
-		{
-			final Dimension size = super.getSize();
-			if(widthSetter != null)
-				size.width = widthSetter.getControlledWidth();
-			return size;
-		}
-		
-		private PersistentWidthSetterComponent widthSetter;
-	}
-
 	private UiScrollPane addTableToPanel(OneRowPanel tables, AssignmentsComponentTable table)
 	{
 		addRowHeightControlledTable(table);
@@ -187,40 +138,18 @@ public class ExpenseEditorComponent extends AbstractMultiTablePanelEditorCompone
 		OneRowPanel box = new OneRowPanel();
 		box.setBackground(AppPreferences.getDataPanelBackgroundColor());
 		box.setGaps(3);
-		box.add(createObjectsActionButton(getActions().getObjectsAction(ActionAssignResource.class), objectPicker));
+		box.add(createObjectsActionButton(getActions().getObjectsAction(ActionAssignResource.class), getPicker()));
 		box.add(createObjectsActionButton(getActions().getObjectsAction(ActionRemoveAssignment.class), resourceTable));
 		
 		return box;
 	}
 	
-	private Actions getActions()
-	{
-		return mainWindow.getActions();
-	}
-	
-	public void commandExecuted(CommandExecutedEvent event)
-	{
-		try
-		{
-			if (event.isSetDataCommandWithThisTypeAndTag(TableSettings.getObjectType(), TableSettings.TAG_DATE_UNIT_LIST_DATA))
-				respondToExpandOrCollapseColumnEvent();
-
-			if (event.isSetDataCommand())
-				dataWasChanged();
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-			EAM.errorDialog(EAM.text("An unexpected error has occurred"));
-		}		
-	}
-
-	private void respondToExpandOrCollapseColumnEvent() throws Exception
+	protected void respondToExpandOrCollapseColumnEvent() throws Exception
 	{
 		workUnitsTableModel.restoreDateUnits();
 	}
 
-	private void dataWasChanged() throws Exception
+	protected void dataWasChanged() throws Exception
 	{
 		resourceTableModel.dataWasChanged();
 		
@@ -250,13 +179,9 @@ public class ExpenseEditorComponent extends AbstractMultiTablePanelEditorCompone
 		return resourceTable.getSelectedHierarchies();
 	}
 	
-	private MainWindow mainWindow;
-	
 	private PlanningViewResourceTable resourceTable;
 	private WorkUnitsTable workUnitsTable;
 	
 	private PlanningViewResourceTableModel resourceTableModel;
 	private WorkUnitsTableModel workUnitsTableModel;
-	
-	private ObjectPicker objectPicker;
 }
