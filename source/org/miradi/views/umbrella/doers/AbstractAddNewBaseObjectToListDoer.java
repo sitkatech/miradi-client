74c7ecacf6dc50336835f100e8aeea067cd6025c
nimaa <nimaa@localhost>
2009-05-06 16:47:59 +0000

    pulled out an abstract base class out of addAssignmentDoer

---
+++ source/org/miradi/views/umbrella/doers/AbstractAddNewBaseObjectToListDoer.java
@@ -0,0 +1,80 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.views.umbrella.doers;
+
+import org.miradi.commands.Command;
+import org.miradi.commands.CommandBeginTransaction;
+import org.miradi.commands.CommandCreateObject;
+import org.miradi.commands.CommandEndTransaction;
+import org.miradi.commands.CommandSetObjectData;
+import org.miradi.exceptions.CommandFailedException;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.objects.BaseObject;
+import org.miradi.views.ObjectsDoer;
+
+abstract public class AbstractAddNewBaseObjectToListDoer extends ObjectsDoer
+{
+	public boolean isAvailable()
+	{
+		if(!super.isAvailable())
+			return false;
+		
+		return getSelectionHierarchy().size() > 0;
+	}
+	
+	public void doIt() throws CommandFailedException
+	{
+		if (! isAvailable())
+			return;
+
+		try 
+		{
+			ORefList selectedRefs = getSelectionHierarchy();
+			BaseObject selectedBaseObject = BaseObject.find(getProject(), selectedRefs.get(0));			
+			createBaseObject(selectedBaseObject);
+		}
+		catch (Exception e)
+		{
+			EAM.logException(e);
+		}
+	}
+
+	private void createBaseObject(BaseObject selectedBaseObject) throws Exception
+	{
+		getProject().executeCommand(new CommandBeginTransaction());
+		try
+		{
+			CommandCreateObject createCommand = new CommandCreateObject(getTypeToCreate());
+			getProject().executeCommand(createCommand);
+
+			Command appendCommand = CommandSetObjectData.createAppendIdCommand(selectedBaseObject, getListTag(), createCommand.getCreatedId());
+			getProject().executeCommand(appendCommand);
+		}
+		finally 
+		{
+			getProject().executeCommand(new CommandEndTransaction());	
+		}
+	}
+
+	abstract protected String getListTag();
+
+	abstract protected int getTypeToCreate();
+}
