11ef5124198ec75e6b20cdde61a200e0c1f85f9a
nimaa <nimaa@localhost>
2009-05-06 15:39:54 +0000

    pulled out all functionality except for funding and accounting.

--- source/org/miradi/dialogs/planning/propertiesPanel/ExpenseResourceTableModel.java
+++ source/org/miradi/dialogs/planning/propertiesPanel/ExpenseResourceTableModel.java
@@ -19,6 +19,11 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.planning.propertiesPanel;
 
+import org.miradi.ids.BaseId;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objects.Assignment;
+import org.miradi.objects.ProjectResource;
 import org.miradi.project.Project;
 
 public class ExpenseResourceTableModel extends PlanningViewResourceTableModel
@@ -27,4 +32,114 @@ public class ExpenseResourceTableModel extends PlanningViewResourceTableModel
 	{
 		super(projectToUse);
 	}
+	
+	public String getColumnName(int column)
+	{
+		if (isResourceColumn(column))
+			return EAM.text("Name");
+		
+		return super.getColumnName(column);
+	}
+	
+	public int getColumnCount()
+	{
+		return COLUMN_COUNT;
+	}
+
+	public Object getValueAt(int row, int column)
+	{
+		return getCellValue(row, column);
+	}
+	
+	protected Object getCellValue(int row, int column)
+	{
+		if (isExpenseNameColumn(column))
+			return "DUMMY EXPENSE NAME";
+		
+		return super.getCellValue(row, column);
+	}
+
+	public void setValueAt(Object value, int row, int column)
+	{
+		if (value == null)
+			return;
+		
+		if (row < 0 || row >= getRowCount())
+		{
+			EAM.errorDialog(EAM.text("An error has occured while writing assignment data."));
+			EAM.logWarning("Row out of bounds in PlanningViewResourceTableModel.setValueAt value = "+ value + " row = " + row + " column = " + column);
+			return;
+		}
+		
+		ORef assignmentRefForRow = getAssignmentForRow(row);
+		setExpenseNameCell(value, assignmentRefForRow, column);
+		
+		super.setValueAt(value, row, column);
+	}
+	
+	private void setExpenseNameCell(Object value, ORef assignmentRefForRow, int column)
+	{
+		if (! isExpenseNameColumn(column))
+			return;
+
+		ProjectResource projectResource = (ProjectResource)value;
+		BaseId resourceId = projectResource.getId();
+		setValueUsingCommand(assignmentRefForRow, Assignment.TAG_ASSIGNMENT_RESOURCE_ID, resourceId);
+	}
+	
+	public boolean isResourceColumn(int column)
+	{
+		return false;
+	}
+	
+	public boolean isExpenseNameColumn(int column)
+	{
+		return getResourceColumn() == column;
+	}
+
+	public boolean isFundingSourceColumn(int column)
+	{
+		return getFundingSourceColumn() == column;
+	}
+
+	public boolean isAccountingCodeColumn(int column)
+	{
+		return getAccountingCodeColumn() == column;
+	}
+
+	public boolean isResourceCostColumn(int column)
+	{
+		return false;
+	}
+
+	public boolean isResourceCostPerUnitColumn(int column)
+	{
+		return false;
+	}
+
+	private int getAccountingCodeColumn()
+	{
+		return ACCOUNTING_CODE_COLUMN;
+	}
+	
+	private int getFundingSourceColumn()
+	{
+		return FUNDING_SOURCE_COLUMN;
+	}
+	
+	private int getResourceColumn()
+	{
+		return EXPENSE_NAME_COLUMN;
+	}
+						
+	public String getColumnTag(int column)
+	{
+		return getColumnName(column);
+	}
+	
+	private static final int COLUMN_COUNT = 3;
+	
+	private static final int EXPENSE_NAME_COLUMN = 0;
+	private static final int ACCOUNTING_CODE_COLUMN = 1;
+	private static final int FUNDING_SOURCE_COLUMN = 2;
 }
