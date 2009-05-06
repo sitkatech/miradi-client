97717bff219701c369bf62ef3c02b23820d0a9d9
nimaa <nimaa@localhost>
2009-05-06 14:28:57 +0000

    text changes

--- source/org/miradi/actions/ActionCreateExpense.java
+++ source/org/miradi/actions/ActionCreateExpense.java
@@ -31,11 +31,11 @@ public class ActionCreateExpense extends ObjectsAction
 	
 	public static String getLabel()
 	{
-		return EAM.text("Action|Assign Expense");
+		return EAM.text("Action|Create Expense");
 	}
 
 	public String getToolTipText()
 	{
-		return EAM.text("TT|Assign an expense");
+		return EAM.text("TT|Create an expense");
 	}
 }
