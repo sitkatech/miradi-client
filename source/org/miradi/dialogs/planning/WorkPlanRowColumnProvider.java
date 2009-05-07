e87c46e22eef7dd9fc3d658c8cd894ed7f41102f
nimaa <nimaa@localhost>
2009-05-07 17:28:29 +0000

    removed the resource/expenseassignment rows from operational plan view

--- source/org/miradi/dialogs/planning/WorkPlanRowColumnProvider.java
+++ source/org/miradi/dialogs/planning/WorkPlanRowColumnProvider.java
@@ -19,8 +19,6 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.planning;
 
-import org.miradi.objects.ExpenseAssignment;
-import org.miradi.objects.ResourceAssignment;
 import org.miradi.objects.BaseObject;
 import org.miradi.objects.Indicator;
 import org.miradi.objects.ResultsChainDiagram;
@@ -52,8 +50,6 @@ public class WorkPlanRowColumnProvider implements RowColumnProvider
 				Indicator.OBJECT_NAME,
 				Task.METHOD_NAME,
 				Task.OBJECT_NAME,
-				ResourceAssignment.OBJECT_NAME,
-				ExpenseAssignment.OBJECT_NAME, 
 				});
 	}
 }
