b46fc0b5b7a43dcfbafca99ab0634548640472e0
nimaa <nimaa@localhost>
2009-05-27 16:20:36 +0000

    no longer saving conpro imported when overrides into field but added fixme to save it by creating a new resourceAssingnment

--- source/org/miradi/xml/conpro/importer/ConproXmlImporterVersion2.java
+++ source/org/miradi/xml/conpro/importer/ConproXmlImporterVersion2.java
@@ -33,7 +33,6 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.martus.util.MultiCalendar;
 import org.martus.util.inputstreamwithseek.InputStreamWithSeek;
 import org.miradi.commands.CommandCreateObject;
 import org.miradi.commands.CommandSetObjectData;
@@ -101,7 +100,6 @@ import org.miradi.questions.TncMarineEcoRegionQuestion;
 import org.miradi.questions.TncTerrestrialEcoRegionQuestion;
 import org.miradi.questions.ViabilityModeQuestion;
 import org.miradi.utils.CodeList;
-import org.miradi.utils.DateRange;
 import org.miradi.utils.EnhancedJsonObject;
 import org.miradi.xml.conpro.ConProMiradiCodeMapHelperVersion2;
 import org.miradi.xml.conpro.ConProMiradiXmlVersion2;
@@ -261,10 +259,10 @@ public class ConproXmlImporterVersion2 implements ConProMiradiXmlVersion2
 		String endDateAsString = getNodeContent(activityNode, ACTIVITY_END_DATE);
 		if (startDateAsString.length() > 0 && endDateAsString.length() > 0)
 		{
-			MultiCalendar startDate = MultiCalendar.createFromIsoDateString(startDateAsString);
-			MultiCalendar endDate = MultiCalendar.createFromIsoDateString(endDateAsString);
-			DateRange dateRange = new DateRange(startDate, endDate);
-			setData(activityRef, Task.TAG_WHEN_OVERRIDE, dateRange.toJson().toString());
+			//FIXME,  no longer saving when override in a field.  need to create a resourceAssignment
+			//MultiCalendar startDate = MultiCalendar.createFromIsoDateString(startDateAsString);
+			//MultiCalendar endDate = MultiCalendar.createFromIsoDateString(endDateAsString);
+			//DateRange dateRange = new DateRange(startDate, endDate);
 		}
 	}
 	
