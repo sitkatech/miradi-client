eba194d2bf63eceaca94e8cd971e1c4fcca5627a
nimaa <nimaa@localhost>
2009-05-27 16:41:07 +0000

    removed references to who override refs tag

--- tests/org/miradi/objects/TestBaseObject.java
+++ tests/org/miradi/objects/TestBaseObject.java
@@ -74,20 +74,6 @@ public class TestBaseObject extends EAMTestCase
 	   	assertEquals("incorrect owned object count?", 2, allOwnedObjects.size());
 	}
 	
-	public void testIsRefList() throws Exception
-	{
-		ORef taskRef = project.createObject(Task.getObjectType());
-		Task task = Task.find(project, taskRef);
-		assertTrue("is tag refList?", task.isRefList(BaseObject.TAG_WHO_OVERRIDE_REFS));	
-	}
-
-	public void testGetAnnotationType() throws Exception
-	{
-		ORef taskRef = project.createObject(Task.getObjectType());
-		Task task = Task.find(project, taskRef);
-		assertEquals("wrong annotation type?", ProjectResource.getObjectType(), task.getAnnotationType(BaseObject.TAG_WHO_OVERRIDE_REFS));	
-	}
-	
 	public void testGetReferredObjects() throws Exception
 	{
 		ORef taskRef = project.createObject(Task.getObjectType());
