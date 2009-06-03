0bef2422f0650727e20779e6f5f9af1a7fbd7200
nimaa <nimaa@localhost>
2009-06-03 15:18:19 +0000

    no longer adding question that is not referred to

--- source/org/miradi/questions/StaticQuestionManager.java
+++ source/org/miradi/questions/StaticQuestionManager.java
@@ -41,7 +41,6 @@ public class StaticQuestionManager
 		addQuestion(new FiscalYearStartQuestion());
 		addQuestion(new FontFamiliyQuestion());
 		addQuestion(new FontSizeQuestion());
-		addQuestion(new IndicatorStatusRatingQuestion());
 		addQuestion(new KeyEcologicalAttributeTypeQuestion());
 		addQuestion(new LanguageQuestion());
 		addQuestion(new PriorityRatingQuestion());
