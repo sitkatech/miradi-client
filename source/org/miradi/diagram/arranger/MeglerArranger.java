1034eebbf5f775e92b8dc334cb0a8c9c4e656f7b
kevins <kevins@localhost>
2009-06-10 15:33:45 +0000

    Megler now puts unlinked factors in column 0

--- source/org/miradi/diagram/arranger/MeglerArranger.java
+++ source/org/miradi/diagram/arranger/MeglerArranger.java
@@ -26,6 +26,7 @@ import java.util.Vector;
 import org.miradi.objecthelpers.ORefList;
 import org.miradi.objects.Cause;
 import org.miradi.objects.DiagramFactor;
+import org.miradi.objects.DiagramLink;
 import org.miradi.objects.DiagramObject;
 import org.miradi.objects.Factor;
 import org.miradi.objects.Strategy;
@@ -44,12 +45,35 @@ public class MeglerArranger
 	public void arrange() throws Exception
 	{
 		extractFactorsOfInterest();
-
+		segregateUnlinkedFactors();
 		setLocations();
+	}
+
+	private void segregateUnlinkedFactors()
+	{
+		unlinked = new Vector<DiagramFactor>();
+		
+		unlinked.addAll(extractUnlinkedDiagramFactors(strategies));
+		unlinked.addAll(extractUnlinkedDiagramFactors(threats));
+		unlinked.addAll(extractUnlinkedDiagramFactors(targets));
+	}
+
+	private Vector<DiagramFactor> extractUnlinkedDiagramFactors(Vector<DiagramFactor> candidates)
+	{
+		Vector<DiagramFactor> unlinkedDiagramFactors = new Vector<DiagramFactor>();
+		for(DiagramFactor diagramFactor : candidates)
+		{
+			ORefList linkRefs = diagramFactor.findObjectsThatReferToUs(DiagramLink.getObjectType());
+			if(linkRefs.size() == 0)
+				unlinkedDiagramFactors.add(diagramFactor);
+		}
+		candidates.removeAll(unlinkedDiagramFactors);
+		return unlinkedDiagramFactors;
 	}
 
 	private void setLocations() throws Exception
 	{
+		moveFactorsToFinalLocations(unlinked, UNLINKED_COLUMN_X, TOP_Y);
 		moveFactorsToFinalLocations(strategies, STRATEGY_COLUMN_X, TOP_Y);
 		moveFactorsToFinalLocations(threats, THREAT_COLUMN_X, TOP_Y);
 		moveFactorsToFinalLocations(targets, TARGET_COLUMN_X, TOP_Y);
@@ -94,9 +118,10 @@ public class MeglerArranger
 		}
 	}
 
-	private static final int STRATEGY_COLUMN_X = 30;
-	private static final int THREAT_COLUMN_X = 180;
-	private static final int TARGET_COLUMN_X = 330;
+	private static final int UNLINKED_COLUMN_X = 30;
+	private static final int STRATEGY_COLUMN_X = 180;
+	private static final int THREAT_COLUMN_X = 330;
+	private static final int TARGET_COLUMN_X = 480;
 	
 	private static final int TOP_Y = 30;
 	private static final int DELTA_Y = 90;
@@ -105,5 +130,6 @@ public class MeglerArranger
 	private Vector<DiagramFactor> strategies;
 	private Vector<DiagramFactor> threats;
 	private Vector<DiagramFactor> targets;
+	private Vector<DiagramFactor> unlinked;
 	
 }
--- tests/org/miradi/diagram/arranger/TestMeglerArranger.java
+++ tests/org/miradi/diagram/arranger/TestMeglerArranger.java
@@ -21,6 +21,8 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.diagram.arranger;
 
 import org.miradi.commands.CommandSetObjectData;
+import org.miradi.exceptions.CommandFailedException;
+import org.miradi.exceptions.UnexpectedNonSideEffectException;
 import org.miradi.main.TestCaseWithProject;
 import org.miradi.objectdata.BooleanData;
 import org.miradi.objecthelpers.ORef;
@@ -39,27 +41,29 @@ public class TestMeglerArranger extends TestCaseWithProject
 	public void testHorizontalColumns() throws Exception
 	{
 		DiagramFactor targetDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Target.getObjectType());
-		DiagramFactor threatDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Cause.getObjectType());
-		ORef threatRef = threatDiagramFactor.getWrappedORef();
-		CommandSetObjectData toThreat = new CommandSetObjectData(threatRef, Cause.TAG_IS_DIRECT_THREAT, BooleanData.BOOLEAN_TRUE);
-		getProject().executeCommand(toThreat);
+		DiagramFactor threatDiagramFactor = createThreat();
 		DiagramFactor strategyDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
+		
+		getProject().createDiagramFactorLink(strategyDiagramFactor, threatDiagramFactor);
+		getProject().createDiagramFactorLink(threatDiagramFactor, targetDiagramFactor);
+
 		MeglerArranger arranger = new MeglerArranger(getProject().getMainDiagramObject());
 		arranger.arrange();
 		
-		assertEquals("Strategy not in column 0?", 30, strategyDiagramFactor.getLocation().x);
-		assertEquals("Threat not in column 1?", 180, threatDiagramFactor.getLocation().x);
-		assertEquals("Target not in column 2?", 330, targetDiagramFactor.getLocation().x);
+		assertEquals("Strategy not in column 1?", 180, strategyDiagramFactor.getLocation().x);
+		assertEquals("Threat not in column 2?", 330, threatDiagramFactor.getLocation().x);
+		assertEquals("Target not in column 3?", 480, targetDiagramFactor.getLocation().x);
 		
 		assertEquals("Strategy not at top?", 30, strategyDiagramFactor.getLocation().y);
 		assertEquals("Threat not at top?", 30, threatDiagramFactor.getLocation().y);
 		assertEquals("Target not at top?", 30, targetDiagramFactor.getLocation().y);
 	}
-	
+
 	public void testVerticalSpacing() throws Exception
 	{
 		DiagramFactor strategyDiagramFactor1 = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
 		DiagramFactor strategyDiagramFactor2 = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
+		
 		MeglerArranger arranger = new MeglerArranger(getProject().getMainDiagramObject());
 		arranger.arrange();
 		
@@ -67,5 +71,35 @@ public class TestMeglerArranger extends TestCaseWithProject
 		int y2 = strategyDiagramFactor2.getLocation().y;
 		assertEquals("Strategies not spaced vertically?", 90, Math.abs(y1 - y2));
 	}
+	
+	public void testLinklessAtFarLeft() throws Exception
+	{
+		DiagramFactor unlinkedTargetDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Target.getObjectType());
+		DiagramFactor unlinkedThreatDiagramFactor = createThreat();
+		DiagramFactor unlinkedStrategyDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
+		
+		DiagramFactor linkedTargetDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Target.getObjectType());
+		DiagramFactor linkedThreatDiagramFactor = createThreat();
+		DiagramFactor linkedStrategyDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
+		
+		getProject().createDiagramFactorLink(linkedStrategyDiagramFactor, linkedThreatDiagramFactor);
+		getProject().createDiagramFactorLink(linkedThreatDiagramFactor, linkedTargetDiagramFactor);
+		
+		MeglerArranger arranger = new MeglerArranger(getProject().getMainDiagramObject());
+		arranger.arrange();
+		
+		assertEquals("unlinked strategy not in column 0?", 30, unlinkedStrategyDiagramFactor.getLocation().x);
+		assertEquals("unlinked threat not in column 0?", 30, unlinkedThreatDiagramFactor.getLocation().x);
+		assertEquals("unlinked target not in column 0?", 30, unlinkedTargetDiagramFactor.getLocation().x);
+	}
 
+	private DiagramFactor createThreat() throws Exception, UnexpectedNonSideEffectException, CommandFailedException
+	{
+		DiagramFactor threatDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Cause.getObjectType());
+		ORef threatRef = threatDiagramFactor.getWrappedORef();
+		CommandSetObjectData toThreat = new CommandSetObjectData(threatRef, Cause.TAG_IS_DIRECT_THREAT, BooleanData.BOOLEAN_TRUE);
+		getProject().executeCommand(toThreat);
+		return threatDiagramFactor;
+	}
+	
 }
