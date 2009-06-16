817482aa76246d286891ff9b76141743c563c482
nimaa <nimaa@localhost>
2009-06-16 20:49:27 +0000

    create or delete resource assignments

--- source/org/miradi/dialogfields/StandAloneCodeListComponent.java
+++ source/org/miradi/dialogfields/StandAloneCodeListComponent.java
@@ -20,9 +20,23 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogfields;
 
+import java.util.Vector;
+
+import org.miradi.commands.Command;
+import org.miradi.commands.CommandBeginTransaction;
+import org.miradi.commands.CommandCreateObject;
+import org.miradi.commands.CommandEndTransaction;
+import org.miradi.commands.CommandSetObjectData;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
 import org.miradi.objects.BaseObject;
+import org.miradi.objects.ResourceAssignment;
+import org.miradi.project.Project;
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.ChoiceQuestion;
+import org.miradi.utils.CodeList;
+import org.miradi.views.diagram.CreateAnnotationDoer;
+import org.miradi.views.planning.doers.TreeNodeDeleteDoer;
 //FIXME urgent - this class is still under construction,  needs to add and remove assignments from parentObject
 public class StandAloneCodeListComponent extends AbstractCodeListComponent
 {
@@ -37,10 +51,72 @@ public class StandAloneCodeListComponent extends AbstractCodeListComponent
 	@Override
 	public void valueChanged(ChoiceItem choiceItem, boolean isSelected)	throws Exception
 	{
-		enableSkipNotice();
-		super.valueChanged(choiceItem, isSelected);
+		CodeList currentCodes = getParentObject().getWhoTotalCodes();
+		if (currentCodes.contains(choiceItem.getCode()) && !isSelected)
+			deleteMatchingResourceAssignments(choiceItem);  
+		else if (!currentCodes.contains(choiceItem.getCode()))
+			createResourceExpense(ORef.createFromString(choiceItem.getCode()));
+	}
+
+	private void createResourceExpense(ORef resourceRef) throws Exception
+	{
+		getProject().executeCommand(new CommandBeginTransaction());
+		try
+		{
+			CommandCreateObject createCommand = new CommandCreateObject(ResourceAssignment.getObjectType());
+			getProject().executeCommand(createCommand);
+
+			ORef newResourceAssignmentRef = createCommand.getObjectRef();
+			cloneFromExistingObject(resourceRef, newResourceAssignmentRef);
+
+			Command appendCommand = CreateAnnotationDoer.createAppendCommand(getParentObject(), newResourceAssignmentRef, BaseObject.TAG_RESOURCE_ASSIGNMENT_IDS);
+			getProject().executeCommand(appendCommand);
+		}
+		finally
+		{
+			getProject().executeCommand(new CommandEndTransaction());
+		}
+	}
+
+	private void cloneFromExistingObject(ORef resourceRef, ORef newResourceAssignmentRef)	throws Exception
+	{
+		ORefList existingResourceAssignmentRefs = getParentObject().getRefList(BaseObject.TAG_RESOURCE_ASSIGNMENT_IDS);
+		ORef existingResourceAssignmentRef = existingResourceAssignmentRefs.getRefForType(ResourceAssignment.getObjectType());
+		if (existingResourceAssignmentRef.isValid())
+		{
+			ResourceAssignment existingResourceAssignment = ResourceAssignment.find(getProject(), existingResourceAssignmentRef);
+			getProject().executeCommandsWithoutTransaction(existingResourceAssignment.createCommandsToClone(newResourceAssignmentRef.getObjectId()));
+		}
+		
+		CommandSetObjectData setResouce = new CommandSetObjectData(newResourceAssignmentRef, ResourceAssignment.TAG_RESOURCE_ID, resourceRef.getObjectId().toString());
+		getProject().executeCommand(setResouce);
+	}
+
+	private BaseObject getParentObject()
+	{
+		return parentObject;
+	}
+
+	private void deleteMatchingResourceAssignments(ChoiceItem choiceItem) throws Exception
+	{
+		Vector<Command> commands = new Vector();
+		ORefList resourceAssignmentRefs = getParentObject().getRefList(BaseObject.TAG_RESOURCE_ASSIGNMENT_IDS);
+		for (int index = 0; index < resourceAssignmentRefs.size(); ++index)
+		{
+			ResourceAssignment resourceAssignment = ResourceAssignment.find(getProject(), resourceAssignmentRefs.get(index));
+			ORef resourceRef = resourceAssignment.getResourceRef();
+			if (resourceRef.equals(ORef.createFromString(choiceItem.getCode())))
+				commands.addAll(TreeNodeDeleteDoer.buildCommandsToDeleteAnnotation(getProject(), resourceAssignment, BaseObject.TAG_RESOURCE_ASSIGNMENT_IDS));
+		}
+		
+		getProject().executeCommandsAsTransaction(commands);
+	}
+	
+	private Project getProject()
+	{
+		return getParentObject().getProject();
 	}
 	
 	private static final int LAYOUT_COLUMN_COUNT = 1;
-	BaseObject parentObject;
+	private BaseObject parentObject;
 }
