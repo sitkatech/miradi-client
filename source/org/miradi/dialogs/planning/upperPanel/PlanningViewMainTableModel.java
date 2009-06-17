4d048af7c5d4cc65986dc1353a161a7aa8c6d77f
kevins <kevins@localhost>
2009-06-17 20:10:14 +0000

    Editing Who in upper work plan is mostly working

--- source/org/miradi/dialogs/planning/upperPanel/PlanningViewMainTableModel.java
+++ source/org/miradi/dialogs/planning/upperPanel/PlanningViewMainTableModel.java
@@ -205,7 +205,7 @@ public class PlanningViewMainTableModel extends PlanningViewAbstractTreeTableSyn
 			isFirstIteration = false;	
 		}
 		
-		return new TaglessChoiceItem(appendedResources);
+		return new ChoiceItem(codeList.toString(), appendedResources);
 	}
 	
 	private String getWhoName(ORef resourceRef)
