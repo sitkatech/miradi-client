1dcdf94ec83bb0fa9fe45a30735b8420a50af310
nimaa <nimaa@localhost>
2009-07-03 12:44:57 +0000

    moved isCellEditable and helper out of table and into model

--- source/org/miradi/dialogs/planning/upperPanel/PlanningUpperMultiTable.java
+++ source/org/miradi/dialogs/planning/upperPanel/PlanningUpperMultiTable.java
@@ -35,7 +35,6 @@ import org.miradi.actions.Actions;
 import org.miradi.dialogs.planning.MultiTableCollapseColumnAction;
 import org.miradi.dialogs.planning.RightClickActionProvider;
 import org.miradi.dialogs.planning.TableWithExpandableColumnsInterface;
-import org.miradi.dialogs.planning.propertiesPanel.AssignmentDateUnitsTableModel;
 import org.miradi.dialogs.planning.propertiesPanel.MultiTableExpandColumnAction;
 import org.miradi.dialogs.planning.propertiesPanel.PlanningRightClickHandler;
 import org.miradi.dialogs.tablerenderers.BasicTableCellRendererFactory;
@@ -45,20 +44,12 @@ import org.miradi.dialogs.tablerenderers.FontForObjectTypeProvider;
 import org.miradi.dialogs.tablerenderers.MultiLineObjectTableCellRendererFactory;
 import org.miradi.dialogs.tablerenderers.NumericTableCellRendererFactory;
 import org.miradi.dialogs.tablerenderers.ProgressTableCellRendererFactory;
-import org.miradi.dialogs.tablerenderers.WhoColumnTableCellEditorFactory;
 import org.miradi.dialogs.tablerenderers.RowColumnBaseObjectProvider;
-import org.miradi.main.EAM;
-import org.miradi.objecthelpers.DateUnit;
-import org.miradi.objecthelpers.ORefList;
-import org.miradi.objecthelpers.TimePeriodCosts;
-import org.miradi.objecthelpers.TimePeriodCostsMap;
+import org.miradi.dialogs.tablerenderers.WhoColumnTableCellEditorFactory;
 import org.miradi.objects.BaseObject;
-import org.miradi.objects.ResourceAssignment;
 import org.miradi.project.CurrencyFormat;
 import org.miradi.project.Project;
 import org.miradi.questions.EmptyChoiceItem;
-import org.miradi.utils.DateUnitEffortList;
-import org.miradi.utils.OptionalDouble;
 import org.miradi.utils.TableWithColumnWidthAndSequenceSaver;
 
 public class PlanningUpperMultiTable extends TableWithColumnWidthAndSequenceSaver implements RowColumnBaseObjectProvider, RightClickActionProvider, TableWithExpandableColumnsInterface
@@ -82,65 +73,6 @@ public class PlanningUpperMultiTable extends TableWithColumnWidthAndSequenceSave
 		addMouseListener(new PlanningRightClickHandler(getMainWindow(), this, this));
 	}
 	
-	@Override
-	public boolean isCellEditable(int row, int column)
-	{
-		int modelColumn = convertColumnIndexToModel(column);
-		String columnTag = getCastedModel().getColumnTag(modelColumn);
-		if (columnTag.equals(BaseObject.PSEUDO_TAG_WHO_TOTAL))
-			return isWhoCellEditable(row, modelColumn);
-		
-		return getCastedModel().isCellEditable(row, modelColumn);
-	}
-	
-	private boolean isWhoCellEditable(int row, int modelColumn)
-	{
-		try
-		{
-			BaseObject baseObjectForRow = getBaseObjectForRowColumn(row, modelColumn);
-			if (!AssignmentDateUnitsTableModel.canReferToAssignments(baseObjectForRow.getRef()))
-				return false;
-
-			if (doAnySubtasksHaveAnyWorkUnitData(baseObjectForRow))
-				return false;
-
-			return doAllResourceAssignmentsHaveIdenticalWorkUnits(row, modelColumn);
-		}
-		catch (Exception e)
-		{
-			EAM.logException(e);
-			return false;		
-		}
-	}
-
-	private boolean doAnySubtasksHaveAnyWorkUnitData(BaseObject baseObjectForRow) throws Exception
-	{
-		TimePeriodCostsMap timePeriodCostsMap = baseObjectForRow.getTotalTimePeriodCostsMapForSubTasks(baseObjectForRow.getSubTaskRefs(), BaseObject.TAG_RESOURCE_ASSIGNMENT_IDS);
-		TimePeriodCosts wholeProjectTimePeriodCosts = timePeriodCostsMap.calculateTimePeriodCosts(new DateUnit());
-		OptionalDouble totalSubtaskWorkUnitsForAllTimePeriods = wholeProjectTimePeriodCosts.calculateResourcesTotalUnits();
-
-		return totalSubtaskWorkUnitsForAllTimePeriods.hasValue();
-	}
-	
-	private boolean doAllResourceAssignmentsHaveIdenticalWorkUnits(int row, int modelColumn) throws Exception
-	{
-			BaseObject baseObjectForRow = getBaseObjectForRowColumn(row, modelColumn);
-			ORefList resourceAssignments = baseObjectForRow.getResourceAssignmentRefs();
-			DateUnitEffortList expectedDateUnitEffortList = null;
-			for (int index = 0; index < resourceAssignments.size(); ++index)
-			{
-				ResourceAssignment resourceAssignment = ResourceAssignment.find(getProject(), resourceAssignments.get(index));
-				DateUnitEffortList thisDateUnitEffortList = resourceAssignment.getDateUnitEffortList();
-				if (expectedDateUnitEffortList == null)
-					expectedDateUnitEffortList = thisDateUnitEffortList;
-				
-				if (!expectedDateUnitEffortList.equals(thisDateUnitEffortList))
-					return false;
-			}
-			
-			return true;
-	}
-
 	@Override
 	public TableCellEditor getCellEditor(int row, int tableColumn)
 	{
