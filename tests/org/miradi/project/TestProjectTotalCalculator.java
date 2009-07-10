d9301cee6ed5f85eccc4bdc19dfee3a4ce02fdd8
nimaa <nimaa@localhost>
2009-07-10 19:14:48 +0000

    assert text changes.  extract helper method

--- tests/org/miradi/project/TestProjectTotalCalculator.java
+++ tests/org/miradi/project/TestProjectTotalCalculator.java
@@ -26,6 +26,7 @@ import org.miradi.objecthelpers.DateUnit;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.TimePeriodCosts;
 import org.miradi.objecthelpers.TimePeriodCostsMap;
+import org.miradi.objects.BaseObject;
 import org.miradi.objects.DiagramFactor;
 import org.miradi.objects.Indicator;
 import org.miradi.objects.ProjectResource;
@@ -48,6 +49,8 @@ public class TestProjectTotalCalculator extends TestCaseWithProject
 		ResultsChainDiagram resultsChain = ResultsChainDiagram.find(getProject(), resultsChainRef);
 		resultsChainDiagramModel = new PersistentDiagramModel(getProject());
 		resultsChainDiagramModel.fillFrom(resultsChain);
+		
+		fred = getProject().createAndPopulateProjectResource();
 	}
 	
 	public void testProjectTotalCalculation() throws Exception
@@ -55,42 +58,47 @@ public class TestProjectTotalCalculator extends TestCaseWithProject
 		
 		assertEquals("no results chains created?", 1, getProject().getResultsChainDiagramPool().size());
 		ProjectTotalCalculator calculator = new ProjectTotalCalculator(getProject());
-		assertEquals("empty project should not have totals data?", 0, calculator.calculateProjectTotals().size());
+		assertEquals("Empty project had non-zero totals data?", 0, calculator.calculateProjectTotals().size());
 		
-		ProjectResource fred = getProject().createAndPopulateProjectResource();
 		DiagramFactor conceptualModelStrategy = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
 		getProject().addResourceAssignment(conceptualModelStrategy.getWrappedFactor(), fred, 10, YEAR_2008, YEAR_2009);
-		assertEquals("conceptualModel strategies should not affect project totals?", 0, calculator.calculateProjectTotals().size());
+		assertEquals("ConceptualModel Strategy included in project totals?", 0, calculator.calculateProjectTotals().size());
 		
 		DiagramFactor conceptualModelDraftStrategy = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
 		getProject().turnOnDraft((Strategy)conceptualModelDraftStrategy.getWrappedFactor());
 		getProject().addResourceAssignment(conceptualModelDraftStrategy.getWrappedFactor(), fred, 10, YEAR_2008, YEAR_2009);
-		assertEquals("conceptualModel strategies should not affect project totals?", 0, calculator.calculateProjectTotals().size());
+		assertEquals("ConceptualModel Draft Strategy included in project totals?", 0, calculator.calculateProjectTotals().size());
 		
 		Indicator indicatorWithResourceAssignment = getProject().createAndPopulateIndicator();
-		getProject().addResourceAssignment(indicatorWithResourceAssignment, fred, 10, YEAR_2008, YEAR_2009);
+		addResourceAssignment(indicatorWithResourceAssignment);
 		TimePeriodCostsMap totalsWithIndicator = calculator.calculateProjectTotals();
 		assertEquals("did not include indicator in totals?", 1, totalsWithIndicator.size());
 		DateUnit dateUnit = getProject().createDateUnit(YEAR_2008, YEAR_2009);
 		TimePeriodCosts indictorTimePeriodCosts = totalsWithIndicator.getTimePeriodCostsForSpecificDateUnit(dateUnit);
-		assertEquals("wrong resources total units calculation with single indicator?", 10.0, indictorTimePeriodCosts.calculateResourcesTotalUnits().getValue());
+		assertEquals("wrong resources total units calculation with indicator?", 10.0, indictorTimePeriodCosts.calculateResourcesTotalUnits().getValue());
 		
 		DiagramFactor resultsChainNonDraftStrategy = getProject().createAndAddFactorToDiagram(resultsChainDiagramModel.getDiagramObject(), Strategy.getObjectType());
-		getProject().addResourceAssignment(resultsChainNonDraftStrategy.getWrappedFactor(), fred, 10, YEAR_2008, YEAR_2009);
+		addResourceAssignment(resultsChainNonDraftStrategy.getWrappedFactor());
 		TimePeriodCostsMap totalsWithNonDraftStrategy = calculator.calculateProjectTotals();
 		TimePeriodCosts strategyTimePeriodCosts = totalsWithNonDraftStrategy.getTimePeriodCostsForSpecificDateUnit(dateUnit);
-		assertEquals("did not include non draft strategy inside results chain?", 20.0, strategyTimePeriodCosts.calculateResourcesTotalUnits().getValue());
+		assertEquals("did not include strategy inside results chain?", 20.0, strategyTimePeriodCosts.calculateResourcesTotalUnits().getValue());
 		
 		DiagramFactor resultsChainDraftStrategy = getProject().createAndAddFactorToDiagram(resultsChainDiagramModel.getDiagramObject(), Strategy.getObjectType());
 		getProject().turnOnDraft((Strategy)conceptualModelDraftStrategy.getWrappedFactor());
 		TimePeriodCostsMap totalsWithDraftStrategy = calculator.calculateProjectTotals();
 		TimePeriodCosts draftStrategyTimePeriodCosts = totalsWithDraftStrategy.getTimePeriodCostsForSpecificDateUnit(dateUnit);
-		getProject().addResourceAssignment(resultsChainDraftStrategy.getWrappedFactor(), fred, 10, YEAR_2008, YEAR_2009);
-		assertEquals("wrong total after adding draft strategy inside results chain?", 20.0, draftStrategyTimePeriodCosts.calculateResourcesTotalUnits().getValue());
+		addResourceAssignment(resultsChainDraftStrategy.getWrappedFactor());
+		assertEquals("Results chain Draft Strategy included in project totals?", 20.0, draftStrategyTimePeriodCosts.calculateResourcesTotalUnits().getValue());
+	}
+
+	private void addResourceAssignment(BaseObject wrappedFactor) throws Exception
+	{
+		getProject().addResourceAssignment(wrappedFactor, fred, 10, YEAR_2008, YEAR_2009);
 	}
 	
 	private static final int YEAR_2008 = 2008;
 	private static final int YEAR_2009 = 2009;
 	
 	private PersistentDiagramModel resultsChainDiagramModel;
+	private ProjectResource fred; 
 }
