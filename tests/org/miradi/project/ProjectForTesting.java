ae0ec99171875a6265113eacac2241912cc50017
nimaa <nimaa@localhost>
2009-08-21 13:25:07 +0000

    changed field value and refactored vars

--- tests/org/miradi/project/ProjectForTesting.java
+++ tests/org/miradi/project/ProjectForTesting.java
@@ -161,7 +161,7 @@ public class ProjectForTesting extends ProjectWithHelpers
 		projectTypes.add(TncProjectPlaceTypeQuestion.MULTI_PLACE_BASED_PROJECT_CODE);
 		projectTypes.add(TncProjectPlaceTypeQuestion.NON_PLACE_BASED_PROJECT_CODE);
 		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_PROJECT_PLACE_TYPES, projectTypes.toString());
-		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_PARENT_CHILD, "Some Parent Child Value");
+		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_CON_PRO_PARENT_CHILD_PROJECT_TEXT, "Some Parent Child Value");
 	}
 
 	public void setSingleYearProjectDate(int singleYear) throws Exception
