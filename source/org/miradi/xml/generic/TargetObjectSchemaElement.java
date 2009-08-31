4ce54455e87a0aab4f0eb9bb471f610787f897f0
kevins <kevins@localhost>
2009-08-31 00:55:38 +0000

    Schema creator: Export skeleton Target and Cause elements

---
+++ source/org/miradi/xml/generic/TargetObjectSchemaElement.java
@@ -0,0 +1,37 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.generic;
+
+
+abstract public class TargetObjectSchemaElement extends FactorObjectSchemaElement
+{
+	public TargetObjectSchemaElement(String objectTypeNameToUse)
+	{
+		super(objectTypeNameToUse);
+//		TAG_TARGET_STATUS
+//		TAG_VIABILITY_MODE
+//		TAG_CURRENT_STATUS_JUSTIFICATION
+//		TAG_SUB_TARGET_REFS
+//		TAG_GOAL_IDS
+//		TAG_KEY_ECOLOGICAL_ATTRIBUTE_IDS
+//		createTextField(ProjectMetadata.TAG_PROJECT_NAME);
+	}
+}
