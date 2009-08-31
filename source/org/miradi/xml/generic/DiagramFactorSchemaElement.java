eee1375c5acc3b667735927edb592da5c82989b1
kevins <kevins@localhost>
2009-08-31 01:42:38 +0000

    Schema creator: add DiagramFactor skeleton with location/size

---
+++ source/org/miradi/xml/generic/DiagramFactorSchemaElement.java
@@ -0,0 +1,41 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.generic;
+
+import org.miradi.objects.DiagramFactor;
+
+public class DiagramFactorSchemaElement extends BaseObjectSchemaElement
+{
+	public DiagramFactorSchemaElement()
+	{
+		super("DiagramFactor");
+//		TAG_WRAPPED_REF
+		createDiagramPointField(DiagramFactor.TAG_LOCATION);
+		createDiagramSizeField(DiagramFactor.TAG_SIZE);
+//		TAG_FONT_SIZE
+//		TAG_FOREGROUND_COLOR
+//		TAG_FONT_STYLE
+//		TAG_GROUP_BOX_CHILDREN_REFS
+//		TAG_BACKGROUND_COLOR
+//		TAG_TEXT_BOX_Z_ORDER_CODE
+	}
+
+}
