a3d08aaf1d9d18acb4c1541c5691239504b86f39
nimaa <nimaa@localhost>
2009-09-04 16:23:22 +0000

    removed references to field that will be removed

--- tests/org/miradi/project/ProjectForTesting.java
+++ tests/org/miradi/project/ProjectForTesting.java
@@ -154,8 +154,7 @@ public class ProjectForTesting extends ProjectWithHelpers
 		projectTypes.add(TncProjectPlaceTypeQuestion.MULTI_PLACE_BASED_PROJECT_CODE);
 		projectTypes.add(TncProjectPlaceTypeQuestion.NON_PLACE_BASED_PROJECT_CODE);
 		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_PROJECT_PLACE_TYPES, projectTypes.toString());
-		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_CON_PRO_PARENT_CHILD_PROJECT_TEXT, "Some Parent Child Value");
-		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_CLASSIFICATIONS, "12\tSomeName\tSomeCategory\n13\tAnotherName\tAnotherCategory");		
+		fillObjectUsingCommand(tncProjectDataRef, TncProjectData.TAG_CON_PRO_PARENT_CHILD_PROJECT_TEXT, "Some Parent Child Value");		
 	}
 
 	public void setSingleYearProjectDate(int singleYear) throws Exception
