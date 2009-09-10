2854f5a891536e83129ddc1028bc5ee51f9fcaba
nimaa <nimaa@localhost>
2009-09-10 21:30:42 +0000

    Using existing target threat rating label.

--- source/org/miradi/utils/SimpleThreatRatingDetailsTableExporter.java
+++ source/org/miradi/utils/SimpleThreatRatingDetailsTableExporter.java
@@ -20,6 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.utils;
 
+import org.miradi.dialogs.threatrating.properties.ThreatRatingCommonPropertiesSubpanel;
 import org.miradi.main.EAM;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
@@ -58,7 +59,7 @@ public class SimpleThreatRatingDetailsTableExporter extends AbstractThreatRating
 			return getThreatColumnName();
 		
 		if (getColumnTag(modelColumn).equals(Stress.PSEUDO_STRESS_RATING))
-			return EAM.text("Summary Target-Threat Rating"); 
+			return ThreatRatingCommonPropertiesSubpanel.getTargetThreatRatingLabel(); 
 
 		return EAM.fieldLabel(Stress.getObjectType(), getColumnTag(modelColumn));
 	}
