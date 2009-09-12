eb42090b541dd58ca2861158ff969390796de983
nimaa <nimaa@localhost>
2009-09-12 00:21:40 +0000

    3 new exportable table model classes that only override columnName to provide a more descriptive name. The classes provide the same name regardless of column index.  We assume that only total column is exported to rtf.

---
+++ source/org/miradi/dialogs/planning/propertiesPanel/ExpenseAmountsExportableTableModel.java
@@ -0,0 +1,39 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.planning.propertiesPanel;
+
+import org.miradi.dialogs.tablerenderers.RowColumnBaseObjectProvider;
+import org.miradi.main.EAM;
+import org.miradi.project.Project;
+
+public class ExpenseAmountsExportableTableModel extends	ExpenseAmountsTableModel
+{
+	public ExpenseAmountsExportableTableModel(Project projectToUse,	RowColumnBaseObjectProvider providerToUse, String treeModelIdentifierAsTagToUse) throws Exception
+	{
+		super(projectToUse, providerToUse, treeModelIdentifierAsTagToUse);
+	}
+
+	@Override
+	public String getColumnName(int col)
+	{
+		return EAM.text("Total\nExpenses");
+	}
+}
