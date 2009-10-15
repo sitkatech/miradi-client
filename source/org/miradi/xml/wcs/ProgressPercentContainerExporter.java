ce6b01be0dfe25238c39fcd023296eb65bad1b39
nimaa <nimaa@localhost>
2009-10-15 23:11:24 +0000

    Exporting progress percent container

--- source/org/miradi/xml/wcs/ProgressPercentContainerExporter.java
+++ source/org/miradi/xml/wcs/ProgressPercentContainerExporter.java
@@ -18,19 +18,26 @@ You should have received a copy of the GNU General Public License
 along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
 */ 
 
-package org.miradi.xml.generic;
+package org.miradi.xml.wcs;
 
+import org.martus.util.UnicodeWriter;
+import org.miradi.objects.BaseObject;
 import org.miradi.objects.ProgressPercent;
-import org.miradi.xml.wcs.WcsXmlConstants;
 
-public class ProgressPercentObjectSchemaElement extends ObjectSchemaElement
+public class ProgressPercentContainerExporter extends ObjectContainerExporter
 {
-	public ProgressPercentObjectSchemaElement()
+	public ProgressPercentContainerExporter(WcsXmlExporter wcsXmlExporterToUse)
 	{
-		super(WcsXmlConstants.PROGRESS_PERCENT);
+		super(wcsXmlExporterToUse, PROGRESS_PERCENT, ProgressPercent.getObjectType());
+	}
+	
+	@Override
+	protected void exportFields(UnicodeWriter writer, BaseObject baseObject) throws Exception
+	{
+		super.exportFields(writer, baseObject);
 		
-		createOptionalDateField(ProgressPercent.TAG_DATE);
-		createOptionalNumericField(ProgressPercent.TAG_PERCENT_COMPLETE);
-		createOptionalTextField(ProgressPercent.TAG_PERCENT_COMPLETE_NOTES);
+		writeElementWithSameTag(baseObject, ProgressPercent.TAG_DATE);
+		writeElementWithSameTag(baseObject, ProgressPercent.TAG_PERCENT_COMPLETE);
+		writeCodeElementSameAsTag(baseObject, ProgressPercent.TAG_PERCENT_COMPLETE_NOTES);
 	}
 }
