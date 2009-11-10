f745ed50d2897f7c3e00fc73a383cd98a3583ded
nimaa <nimaa@localhost>
2009-11-10 15:57:44 +0000

    Rename method

--- source/org/miradi/main/CommandExecutedEvent.java
+++ source/org/miradi/main/CommandExecutedEvent.java
@@ -114,7 +114,7 @@ public class CommandExecutedEvent
 		return (Factor.isFactor(cmd.getObjectType()) && cmd.getFieldTag().equals(tag));
 	}
 	
-	public boolean isSetDataCommandFor(ORef otherRef)
+	public boolean isSetObjectDataCommandFor(ORef otherRef)
 	{
 		if (isSetDataCommand())
 			return getCastedSetObjectDataCommand().getObjectORef().equals(otherRef);
