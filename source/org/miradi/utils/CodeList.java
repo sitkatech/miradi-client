ae1338f9ff6b0130b7b6ccaecdd68c7867e31364
nimaa <nimaa@localhost>
2009-11-19 18:46:54 +0000

    Pulled up methods

--- source/org/miradi/utils/CodeList.java
+++ source/org/miradi/utils/CodeList.java
@@ -20,153 +20,40 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.utils;
 
 import java.text.ParseException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
 public class CodeList extends StringList
 {
 	public CodeList()
 	{
-		this(new Vector());
+		super();
 	}
 	
 	public CodeList(String[] codes)
 	{
-		this(new Vector(Arrays.asList(codes)));
+		super(codes);
 	}
 	
 	public CodeList(CodeList copyFrom)
 	{
-		this(new Vector(copyFrom.data));
+		super(copyFrom);
 	}
-	
 	
 	public CodeList(EnhancedJsonObject json)
 	{
-		this();
-		EnhancedJsonArray array = json.optJsonArray(getJsonTag());
-		if(array == null)
-			array = new EnhancedJsonArray();
-		for(int i = 0; i < array.length(); ++i)
-			add((String)array.get(i));
-		
+		super(json);
 	}
 	
 	public CodeList(String listAsJsonString) throws ParseException
 	{
-		this(new EnhancedJsonObject(listAsJsonString));
+		super(listAsJsonString);
 	}
 	
-	private CodeList(List dataToUse)
+	public CodeList(List dataToUse)
 	{
-		data = new Vector(dataToUse);
+		super(dataToUse);
 	}
-	
-	public int size()
-	{
-		return data.size();
-	}
-
-	
-	public void add(String code)
-	{
-		data.add(code);
-	}
-	
-	public void addAll(CodeList listToAdd)
-	{
-		data.addAll(Arrays.asList(listToAdd.toArray()));
-	}
-		
-	public String get(int index)
-	{
-		return data.get(index);
-	}
-	
-	public boolean contains(String code)
-	{
-		return data.contains(code);
-	}
-	
-	public int find(String code)
-	{
-		return data.indexOf(code);
-	}
-	
-	public void removeCode(String code)
-	{
-		if(!data.contains(code))
-			throw new RuntimeException("Attempted to remove non-existant code: " + code + " from: " + toString());
-		data.remove(code);
-	}
-	
-	public void subtract(CodeList other)
-	{
-		for(int i = 0; i < other.size(); ++i)
-		{
-			String code = other.get(i);
-			if(contains(code))
-				removeCode(code);
-		}
-	}
-	
-	public void retainAll(CodeList other)
-	{
-		data.retainAll(other.toVector());
-	}
-	
-	public Vector<String> toVector()
-	{
-		return new Vector(data);
-	}
-	
-	public String[] toArray()
-	{
-		return data.toArray(new String[0]);
-	}
-	
-	public EnhancedJsonObject toJson()
-	{
-		EnhancedJsonObject json = new EnhancedJsonObject();
-		EnhancedJsonArray array = new EnhancedJsonArray();
-		for(int i = 0; i < size(); ++i)
-			array.appendString(get(i));
-		json.put(getJsonTag(), array);
-		return json;
-	}
-	
-	public String toString()
-	{
-		if(size() == 0)
-			return "";
-		return toJson().toString();
-	}
-	
-	public boolean equals(Object rawOther)
-	{
-		if(! (rawOther instanceof CodeList))
-			return false;
-		
-		CodeList other = (CodeList)rawOther;
-		return data.equals(other.data);
-	}
-	
-	public int hashCode()
-	{
-		return data.hashCode();
-	}
-	
-	public void removeCodeAt(int index)
-	{
-		data.removeElementAt(index);
-	}
-	
-	public void insertElementAt(String code, int index)
-	{
-		data.insertElementAt(code, index);
-	}
-	
+								
 	public CodeList withoutDuplicates()
 	{
 		CodeList withoutDuplicates = new CodeList();
@@ -180,18 +67,11 @@ public class CodeList extends StringList
 		return withoutDuplicates;
 	}
 	
-	public boolean isEmpty()
-	{
-		return size() == 0;
-	}
-	
-	private String getJsonTag()
+	protected String getJsonTag()
 	{
 		return TAG_IDS;
 	}
 		
 	private static final String TAG_IDS = "Codes";
-
-	private Vector<String> data;
 }
 
