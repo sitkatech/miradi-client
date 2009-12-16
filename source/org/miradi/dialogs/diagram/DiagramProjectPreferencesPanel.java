e021d45cd327b11904e11953a5604eb9a5000a08
nimaa <nimaa@localhost>
2009-12-16 20:50:44 +0000

    Getting project from main window. Will no longer pass in project (next commit)

--- source/org/miradi/dialogs/diagram/DiagramProjectPreferencesPanel.java
+++ source/org/miradi/dialogs/diagram/DiagramProjectPreferencesPanel.java
@@ -38,7 +38,7 @@ public class DiagramProjectPreferencesPanel extends ObjectDataInputPanel
 {
 	public DiagramProjectPreferencesPanel(MainWindow mainWindowToUse, Project projectToUse, ProjectInfo projectInfo)
 	{
-		super(projectToUse, ProjectMetadata.getObjectType(), projectInfo.getMetadataId());
+		super(mainWindowToUse.getProject(), ProjectMetadata.getObjectType(), projectInfo.getMetadataId());
 		
 		mainWindow = mainWindowToUse;
 		addField(createChoiceField(ProjectMetadata.getObjectType(), ProjectMetadata.TAG_DIAGRAM_FONT_SIZE, new FontSizeQuestion()));
