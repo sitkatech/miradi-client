632b312509549e48e986fa08a15b0338b57b7d98
nimaa <nimaa@localhost>
2009-12-30 18:43:08 +0000

    Wrapped text in EAM.text()

--- source/org/miradi/questions/ScopeThreatRatingQuestion.java
+++ source/org/miradi/questions/ScopeThreatRatingQuestion.java
@@ -32,25 +32,25 @@ public class ScopeThreatRatingQuestion extends ThreatRatingQuestion
 	@Override
 	protected String getLowRatingChoiceItemDescription()
 	{
-		return "Low: The threat is likely to be very narrow in its scope, affecting the target across a small proportion (1-10%) of its occurrence/population.";
+		return EAM.text("Low: The threat is likely to be very narrow in its scope, affecting the target across a small proportion (1-10%) of its occurrence/population.");
 	}
 	
 	@Override
 	protected String getMediumRatingChoiceItemDescription()
 	{
-		return "Medium: The threat is likely to be restricted in its scope, affecting the target across some (11-30%) of its occurrence/population.";
+		return EAM.text("Medium: The threat is likely to be restricted in its scope, affecting the target across some (11-30%) of its occurrence/population.");
 	}
 
 	@Override
 	protected String getHighRatingChoiceItemDescription()
 	{
-		return "High: The threat is likely to be widespread in its scope, affecting the target across much (31-70%) of its occurrence/population.";
+		return EAM.text("High: The threat is likely to be widespread in its scope, affecting the target across much (31-70%) of its occurrence/population.");
 	}
 
 	@Override
 	protected String getVeryHighRatingChoiceItemDescription()
 	{
-		return "Very High: The threat is likely to be pervasive in its scope, affecting the target across all or most (71-100%) of its occurrence/population.";
+		return EAM.text("Very High: The threat is likely to be pervasive in its scope, affecting the target across all or most (71-100%) of its occurrence/population.");
 	}
 	
 	private static String getDescription()
