2df5b836c3ce70e465c5682535b0bcaf0d2234ad
nimaa <nimaa@localhost>
2010-01-06 13:25:14 +0000

    Pulled up Duplicated methods

--- source/org/miradi/dialogs/iucnRedlistSpecies/IucnRedlistSpeciesEditablePoolTableModel.java
+++ source/org/miradi/dialogs/iucnRedlistSpecies/IucnRedlistSpeciesEditablePoolTableModel.java
@@ -21,13 +21,8 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.iucnRedlistSpecies;
 
 import org.miradi.dialogs.base.EditableObjectPoolRefsTableModel;
-import org.miradi.objecthelpers.ORef;
-import org.miradi.objecthelpers.ORefList;
-import org.miradi.objects.BaseObject;
 import org.miradi.objects.IucnRedlistSpecies;
 import org.miradi.project.Project;
-import org.miradi.questions.EmptyChoiceItem;
-import org.miradi.questions.TaglessChoiceItem;
 
 public class IucnRedlistSpeciesEditablePoolTableModel extends EditableObjectPoolRefsTableModel
 {
@@ -43,46 +38,11 @@ public class IucnRedlistSpeciesEditablePoolTableModel extends EditableObjectPool
 	}
 
 	@Override
-	protected ORefList extractOutEditableRefs(ORef[] hierarchyToSelectedRef)
-	{	
-		return getProject().getPool(getObjectType()).getRefList();
-	}
-
-	@Override
 	protected int getObjectType()
 	{
 		return IucnRedlistSpecies.getObjectType();
 	}
 
-	public Object getValueAt(int rowIndex, int columnIndex)
-	{
-		BaseObject iucnRedlistSpecies = getBaseObjectForRowColumn(rowIndex, columnIndex);
-		if (isLabelColumn(columnIndex))
-			return new TaglessChoiceItem(iucnRedlistSpecies.getData(getColumnTags()[columnIndex]));
-		
-		return new EmptyChoiceItem();
-	}
-
-	@Override
-	public void setValueAt(Object value, int row, int column)
-	{
-		if (value == null)
-			return;
-		
-		ORef ref = getBaseObjectForRowColumn(row, column).getRef();
-		setDataValue(ref, column, value.toString());
-	}
-	
-	private void setDataValue(ORef ref, int column, String value)
-	{
-		setValueUsingCommand(ref, getColumnTag(column), value);
-	}
-	
-	public boolean isLabelColumn(int modelColumn)
-	{
-		return isColumnForTag(modelColumn, getColumnTags()[modelColumn]);
-	}
-	
 	@Override
 	protected String[] getColumnTags()
 	{
--- source/org/miradi/dialogs/otherNotableSpecies/OtherNotableSpeciesEditablePoolTableModel.java
+++ source/org/miradi/dialogs/otherNotableSpecies/OtherNotableSpeciesEditablePoolTableModel.java
@@ -21,13 +21,8 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.otherNotableSpecies;
 
 import org.miradi.dialogs.base.EditableObjectPoolRefsTableModel;
-import org.miradi.objecthelpers.ORef;
-import org.miradi.objecthelpers.ORefList;
-import org.miradi.objects.BaseObject;
 import org.miradi.objects.OtherNotableSpecies;
 import org.miradi.project.Project;
-import org.miradi.questions.EmptyChoiceItem;
-import org.miradi.questions.TaglessChoiceItem;
 
 public class OtherNotableSpeciesEditablePoolTableModel extends EditableObjectPoolRefsTableModel
 {
@@ -43,46 +38,11 @@ public class OtherNotableSpeciesEditablePoolTableModel extends EditableObjectPoo
 	}
 
 	@Override
-	protected ORefList extractOutEditableRefs(ORef[] hierarchyToSelectedRef)
-	{	
-		return getProject().getPool(getObjectType()).getRefList();
-	}
-
-	@Override
 	protected int getObjectType()
 	{
 		return OtherNotableSpecies.getObjectType();
 	}
 
-	public Object getValueAt(int rowIndex, int columnIndex)
-	{
-		BaseObject otherNotableSpecies = getBaseObjectForRowColumn(rowIndex, columnIndex);
-		if (isLabelColumn(columnIndex))
-			return new TaglessChoiceItem(otherNotableSpecies.getData(OtherNotableSpecies.TAG_LABEL));
-		
-		return new EmptyChoiceItem();
-	}
-
-	@Override
-	public void setValueAt(Object value, int row, int column)
-	{
-		if (value == null)
-			return;
-		
-		ORef ref = getBaseObjectForRowColumn(row, column).getRef();
-		setDataValue(ref, column, value.toString());
-	}
-	
-	private void setDataValue(ORef ref, int column, String value)
-	{
-		setValueUsingCommand(ref, getColumnTag(column), value);
-	}
-	
-	public boolean isLabelColumn(int modelColumn)
-	{
-		return isColumnForTag(modelColumn, OtherNotableSpecies.TAG_LABEL);
-	}
-	
 	@Override
 	protected String[] getColumnTags()
 	{
