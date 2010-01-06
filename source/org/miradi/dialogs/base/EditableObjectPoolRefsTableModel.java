823768b9313cec1939e2aae3a2bb10782381ab20
nimaa <nimaa@localhost>
2010-01-06 13:25:14 +0000

    Pulled up Duplicated methods

--- source/org/miradi/dialogs/base/EditableObjectPoolRefsTableModel.java
+++ source/org/miradi/dialogs/base/EditableObjectPoolRefsTableModel.java
@@ -20,12 +20,52 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.base;
 
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.objects.BaseObject;
 import org.miradi.project.Project;
+import org.miradi.questions.EmptyChoiceItem;
+import org.miradi.questions.TaglessChoiceItem;
 
 abstract public class EditableObjectPoolRefsTableModel extends EditableObjectRefsTableModel
 {
 	public EditableObjectPoolRefsTableModel(Project projectToUse)
 	{
 		super(projectToUse);
+	}
+	
+	@Override
+	protected ORefList extractOutEditableRefs(ORef[] hierarchyToSelectedRef)
+	{	
+		return getProject().getPool(getObjectType()).getRefList();
+	}
+	
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		BaseObject iucnRedlistSpecies = getBaseObjectForRowColumn(rowIndex, columnIndex);
+		if (isLabelColumn(columnIndex))
+			return new TaglessChoiceItem(iucnRedlistSpecies.getData(getColumnTags()[columnIndex]));
+		
+		return new EmptyChoiceItem();
+	}
+	
+	@Override
+	public void setValueAt(Object value, int row, int column)
+	{
+		if (value == null)
+			return;
+		
+		ORef ref = getBaseObjectForRowColumn(row, column).getRef();
+		setDataValue(ref, column, value.toString());
+	}
+	
+	private void setDataValue(ORef ref, int column, String value)
+	{
+		setValueUsingCommand(ref, getColumnTag(column), value);
+	}
+	
+	public boolean isLabelColumn(int modelColumn)
+	{
+		return isColumnForTag(modelColumn, getColumnTags()[modelColumn]);
 	}
 }
