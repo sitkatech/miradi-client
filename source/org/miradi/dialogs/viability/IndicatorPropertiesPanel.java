2797670820e7d3017f9135fa16ae23eaf7754a46
nimaa <nimaa@localhost>
2010-01-08 10:15:07 +0000

    Pulled up all methods

--- source/org/miradi/dialogs/viability/IndicatorPropertiesPanel.java
+++ source/org/miradi/dialogs/viability/IndicatorPropertiesPanel.java
@@ -19,65 +19,13 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.viability;
 
-import org.miradi.dialogs.assignment.AssignmentsPropertiesPanel;
-import org.miradi.dialogs.diagram.IndicatorSubPanel;
-import org.miradi.dialogs.expense.ExpensesPropertiesPanel;
-import org.miradi.dialogs.progressReport.ProgressReportSubPanel;
-import org.miradi.ids.BaseId;
-import org.miradi.ids.FactorId;
-import org.miradi.layout.OneColumnGridLayout;
-import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
-import org.miradi.objecthelpers.ORef;
-import org.miradi.objecthelpers.ORefList;
-import org.miradi.objecthelpers.ObjectType;
-import org.miradi.objects.Indicator;
-import org.miradi.objects.KeyEcologicalAttribute;
-import org.miradi.objects.Task;
 import org.miradi.views.umbrella.ObjectPicker;
 
 public class IndicatorPropertiesPanel extends AbstractIndicatorPropertiesPanel
 {
 	public IndicatorPropertiesPanel(MainWindow mainWindow, ObjectPicker picker) throws Exception
 	{
-		super(mainWindow.getProject(), Indicator.getObjectType());
-		
-		setLayout(new OneColumnGridLayout());
-		addSubPanelWithTitledBorder(new IndicatorSubPanel(getProject(), getInvalidTargetRef()));
-		
-		viabilityRatingsSubPanel = new IndicatorViabilityRatingsSubPanel(getProject(), getInvalidTargetRef());
-		addSubPanelWithTitledBorder(viabilityRatingsSubPanel);
-		addSubPanelWithTitledBorder(new IndicatorMonitoringPlanSubPanel(getProject(), getInvalidTargetRef()));
-		addSubPanelWithTitledBorder(new ProgressReportSubPanel(getMainWindow(), getPicker()));
-		addSubPanelWithTitledBorder(new IndicatorFutureStatusSubPanel(getProject(), getInvalidTargetRef()));
-		addBudgetSubPanels(picker);
-		
-		updateFieldsFromProject();
+		super(mainWindow.getProject(), picker);
 	}
-
-	private void addBudgetSubPanels(ObjectPicker picker) throws Exception
-	{
-		addSubPanelWithTitledBorder(new AssignmentsPropertiesPanel(getMainWindow(), Task.getObjectType(), picker));
-		addSubPanelWithTitledBorder(new ExpensesPropertiesPanel(getMainWindow(), Task.getObjectType(), picker));
-	}
-
-	public void setObjectRefs(ORef[] orefsToUse)
-	{
-		super.setObjectRefs(orefsToUse);
-		
-		ORef foundRef = new ORefList(orefsToUse).getRefForType(KeyEcologicalAttribute.getObjectType());
-		setTabEnabled(viabilityRatingsSubPanel.getPanelDescription(), foundRef.isValid());
-	}
-	
-	private static ORef getInvalidTargetRef()
-	{
-		return new ORef(ObjectType.TARGET, new FactorId(BaseId.INVALID.asInt()));
-	}
-	
-	public String getPanelDescription()
-	{
-		return EAM.text("Title|Indicator Properties");
-	}
-	
-	private IndicatorViabilityRatingsSubPanel viabilityRatingsSubPanel;
 }
