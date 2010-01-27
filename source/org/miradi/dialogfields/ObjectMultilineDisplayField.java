2a7fb65a4892c4b85bfaa7eddb64c7a47dc5ea78
kevins <kevins@localhost>
2010-01-27 18:33:07 +0000

    Reduce visibility of a constructor to private

--- source/org/miradi/dialogfields/ObjectMultilineDisplayField.java
+++ source/org/miradi/dialogfields/ObjectMultilineDisplayField.java
@@ -31,7 +31,7 @@ public class ObjectMultilineDisplayField extends ObjectTextInputField
 		this(projectToUse, objectTypeToUse, objectIdToUse, tagToUse, 50);
 	}
 	
-	public ObjectMultilineDisplayField(Project projectToUse, int objectTypeToUse, BaseId objectIdToUse, String tagToUse, int columnCount)
+	private ObjectMultilineDisplayField(Project projectToUse, int objectTypeToUse, BaseId objectIdToUse, String tagToUse, int columnCount)
 	{
 		super(projectToUse, objectTypeToUse, objectIdToUse, tagToUse, new PanelTextArea(1, columnCount));
 		((UiTextArea)getComponent()).setWrapStyleWord(true);
