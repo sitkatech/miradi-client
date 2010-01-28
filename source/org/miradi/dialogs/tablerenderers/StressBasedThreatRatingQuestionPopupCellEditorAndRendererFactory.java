26543363b19f4545173e3821ad9d821003a9fbb8
nimaa <nimaa@localhost>
2010-01-28 20:57:30 +0000

    When the getEditor is called, force a click on the invoke popup editor button.

--- source/org/miradi/dialogs/tablerenderers/StressBasedThreatRatingQuestionPopupCellEditorAndRendererFactory.java
+++ source/org/miradi/dialogs/tablerenderers/StressBasedThreatRatingQuestionPopupCellEditorAndRendererFactory.java
@@ -48,6 +48,7 @@ public class StressBasedThreatRatingQuestionPopupCellEditorAndRendererFactory ex
 	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column)
 	{
 		updateEditor(table, value, row, column);
+		questionEditor.forceInvokePopupButtonClick();
 		
 		return questionEditor;
 	}
