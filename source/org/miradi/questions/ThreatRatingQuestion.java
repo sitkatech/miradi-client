4e0900618c2ee70b7f36f36cd6de2bc6437e139a
nimaa <nimaa@localhost>
2010-01-29 14:30:33 +0000

    Pullup method

--- source/org/miradi/questions/ThreatRatingQuestion.java
+++ source/org/miradi/questions/ThreatRatingQuestion.java
@@ -20,7 +20,6 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.questions;
 
 import java.awt.Color;
-import java.util.Comparator;
 
 import org.miradi.main.EAM;
 
@@ -68,12 +67,6 @@ public class ThreatRatingQuestion extends StaticChoiceQuestionSortableByCode
 		return "";
 	}
 	
-	@Override
-	public Comparator getComparator()
-	{
-		return new ChoiceItemCodeComparator();
-	}
-
 	public static final String HIGH_RATING_CODE = "3";
 	public static final String VERY_HIGH_RATING_CODE = "4";
 }
