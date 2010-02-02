01c757c96e26d38e8f15dd25c53aea85f2a0dbc1
nimaa <nimaa@localhost>
2010-02-02 18:07:05 +0000

    Extracted out a dialog that closes when selection changes.

---
+++ source/org/miradi/utils/DialogWithCloseAfterSelectionHandler.java
@@ -0,0 +1,41 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.utils;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import org.miradi.dialogs.base.UndecoratedModelessDialogWithClose;
+import org.miradi.main.MainWindow;
+
+public class DialogWithCloseAfterSelectionHandler extends UndecoratedModelessDialogWithClose implements ListSelectionListener
+{
+	public DialogWithCloseAfterSelectionHandler(MainWindow parent, String title)
+	{
+		super(parent, title);
+	}
+	
+	public void valueChanged(ListSelectionEvent event)
+	{
+		setVisible(false);
+		dispose();
+	}
+}
