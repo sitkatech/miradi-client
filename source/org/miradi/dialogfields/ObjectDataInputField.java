b10b58fbec66420fc327a03d0cc2aab2fb7bdaf3
nimaa <nimaa@localhost>
2010-02-03 22:02:22 +0000

    Field now executes single command inside a transaction.

--- source/org/miradi/dialogfields/ObjectDataInputField.java
+++ source/org/miradi/dialogfields/ObjectDataInputField.java
@@ -180,7 +180,7 @@ abstract public class ObjectDataInputField extends SavableField
 		CommandSetObjectData cmd = new CommandSetObjectData(objectType, objectId, tag, newValue);
 		try
 		{
-			project.executeCommand(cmd);
+			project.executeCommandAsTransaction(cmd);
 			updateFromObject();
 		}
 		catch(CommandFailedException e)
