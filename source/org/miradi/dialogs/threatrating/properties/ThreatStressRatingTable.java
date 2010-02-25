49389989ecf11eba50703bca5160be61c2c467eb
nimaa <nimaa@localhost>
2010-02-25 16:37:15 +0000

    Cleanup

--- source/org/miradi/dialogs/threatrating/properties/ThreatStressRatingTable.java
+++ source/org/miradi/dialogs/threatrating/properties/ThreatStressRatingTable.java
@@ -51,6 +51,7 @@ public class ThreatStressRatingTable extends EditableObjectTable
 		return (ThreatStressRatingTableModel) getModel();
 	}
 	
+	@Override
 	public void rebuildColumnEditorsAndRenderers() throws Exception
 	{
 		ThreatStressRatingTableModel threatStressRatingTableModel = getThreatStressRatingTableModel();
