3b62cbcf12f646d2c5aa8e8e4a1eb4f0e1df4555
nimaa <nimaa@localhost>
2010-03-03 20:32:24 +0000

    New question for quarter column visibility

---
+++ source/org/miradi/questions/QuarterColumnsVisibilityQuestion.java
@@ -0,0 +1,43 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.questions;
+
+import org.miradi.main.EAM;
+
+public class QuarterColumnsVisibilityQuestion extends StaticChoiceQuestion
+{
+	public QuarterColumnsVisibilityQuestion()
+	{
+		super(getChoiceItems());
+	}
+
+	private static ChoiceItem[] getChoiceItems()
+	{
+		return new ChoiceItem[] {
+				new ChoiceItem(SHOW_QUARTER_COLUMNS_CODE, EAM.text("Show Quearter Columns")),
+				new ChoiceItem(HIDE_QUARTER_COLUMNS_CODE, EAM.text("Hide Quarter Columns")),
+		};
+	}
+	
+	private static final String SHOW_QUARTER_COLUMNS_CODE = "";
+	private static final String HIDE_QUARTER_COLUMNS_CODE = "HideQuarterColumns";
+
+}
