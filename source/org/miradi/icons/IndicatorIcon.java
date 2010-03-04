b1b0930ee9b0dd13399df8acc51ac75eee404af0
nimaa <nimaa@localhost>
2010-03-04 15:22:30 +0000

    Pulled up a paret class for indicator icon.  Created a new warning icon

--- source/org/miradi/icons/IndicatorIcon.java
+++ source/org/miradi/icons/IndicatorIcon.java
@@ -20,39 +20,13 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.icons;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Polygon;
-import java.awt.Rectangle;
 
-import javax.swing.Icon;
-
-import org.miradi.diagram.renderers.TriangleRenderer;
 import org.miradi.main.AppPreferences;
 
-public class IndicatorIcon implements Icon
+public class IndicatorIcon extends AbstractTriangleIcon
 {
-	public int getIconHeight()
+	protected Color getFillColor()
 	{
-		return HEIGHT;
+		return AppPreferences.INDICATOR_COLOR;
 	}
-
-	public int getIconWidth()
-	{
-		return WIDTH;
-	}
-
-	public void paintIcon(Component c, Graphics g, int x, int y)
-	{
-		Rectangle rect = new Rectangle(x+1, y+1, WIDTH-2, HEIGHT-2);
-		Polygon triangle = TriangleRenderer.buildTriangle(rect);
-		g.setColor(AppPreferences.INDICATOR_COLOR);
-		g.fillPolygon(triangle);
-		g.setColor(Color.BLACK);
-		g.drawPolygon(triangle);
-	}
-	
-	static final int WIDTH = 16;
-	static final int HEIGHT = 16;
-
 }
