7f8f67a28cd97a45a248f553fcf215275aeb1a87
nimaa <nimaa@localhost>
2010-03-05 21:41:18 +0000

    Cleanup

--- source/org/miradi/views/planning/doers/TreeNodeCreateActivityDoer.java
+++ source/org/miradi/views/planning/doers/TreeNodeCreateActivityDoer.java
@@ -24,6 +24,7 @@ import org.miradi.objects.Strategy;
 
 public class TreeNodeCreateActivityDoer extends TreeNodeCreateTaskDoer
 {
+	@Override
 	protected boolean canBeParentOfTask(BaseObject selectedObject) throws Exception
 	{
 		if(Strategy.is(selectedObject))
