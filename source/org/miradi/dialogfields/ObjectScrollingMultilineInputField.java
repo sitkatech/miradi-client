804b06d985d6d72c32bdf614b851349241961d98
nimaa <nimaa@localhost>
2010-03-10 01:41:06 +0000

    mrd-3659:Added Popup button to bring up a bigger text field inside a dialog.

--- source/org/miradi/dialogfields/ObjectScrollingMultilineInputField.java
+++ source/org/miradi/dialogfields/ObjectScrollingMultilineInputField.java
@@ -19,9 +19,23 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogfields;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JComponent;
 
+import org.martus.swing.Utilities;
+import org.miradi.dialogs.base.MiradiPanel;
+import org.miradi.dialogs.base.ModalDialogWithClose;
+import org.miradi.dialogs.fieldComponents.PanelButton;
+import org.miradi.icons.PopupEditorIcon;
 import org.miradi.ids.BaseId;
+import org.miradi.layout.OneColumnPanel;
+import org.miradi.main.AppPreferences;
+import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 import org.miradi.utils.MiradiScrollPane;
 
@@ -32,15 +46,31 @@ public class ObjectScrollingMultilineInputField extends ObjectMultilineInputFiel
 		super(mainWindow, objectTypeToUse, objectIdToUse, tagToUse, INITIAL_MULTI_LINE_TEXT_AREA_ROW_COUNT, columnsToUse);
 	}
 
+	private void createTextFieldWithPopupButtonPanel()
+	{
+		editButton = new PanelButton(new PopupEditorIcon());
+		editButton.addActionListener(new PopupButtonHandler());
+
+		OneColumnPanel buttonPanel = new OneColumnPanel();
+		buttonPanel.setBackground(AppPreferences.getDataPanelBackgroundColor());
+		buttonPanel.add(editButton);
+
+		scrollPane = new MiradiScrollPane(getTextField());
+		scrollPane.setVerticalScrollBarPolicy(scrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+		mainPanel = new MiradiPanel(new BorderLayout());
+		mainPanel.add(scrollPane, BorderLayout.CENTER);
+		mainPanel.add(buttonPanel, BorderLayout.AFTER_LINE_ENDS);
+		setDefaultFieldBorder();
+	}
+
 	@Override
 	public JComponent getComponent()
 	{
-		if(scrollPane == null)
-		{
-			scrollPane = new MiradiScrollPane(getTextField());
-			scrollPane.setVerticalScrollBarPolicy(scrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		}
-		return scrollPane;
+		if (mainPanel == null)
+			createTextFieldWithPopupButtonPanel();
+		
+		return mainPanel;
 	}
 
 	@Override
@@ -50,6 +80,34 @@ public class ObjectScrollingMultilineInputField extends ObjectMultilineInputFiel
 		getTextField().addFocusListener(this);
 	}
 
+	private class PopupButtonHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			ModalDialogWithClose dialog = new ModalDialogWithClose(getMainWindow(), EAM.text("Edit..."));
+			dialog.addWindowListener(new WindowCloseSaveHandler());
+			popupTextField = new MiradiTextPane(getMainWindow(), 100, 20);
+			new TextAreaRightClickMouseHandler(getMainWindow().getActions(), popupTextField);
+			popupTextField.setText(getTextField().getText());
+			dialog.add(new MiradiScrollPane(popupTextField));
+			Utilities.centerDlg(dialog);
+			dialog.setVisible(true);
+		}
+	}
+	
+	private class WindowCloseSaveHandler extends WindowAdapter
+	{
+		@Override
+		public void windowDeactivated(WindowEvent e)
+		{
+			setText(popupTextField.getText());
+			saveIfNeeded();
+		}
+	}
+
+	private MiradiTextPane popupTextField; 
+	private PanelButton editButton;
+	private MiradiPanel mainPanel;
 	private MiradiScrollPane scrollPane;
 	public static final int INITIAL_MULTI_LINE_TEXT_AREA_ROW_COUNT = 4;
 }
