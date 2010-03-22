5619a712aa6d95b67b1fb822be7631c8c7d8c6c5
nimaa <nimaa@localhost>
2010-03-22 22:17:03 +0000

    Pulled up all of workPlanProjectNode into parent

--- source/org/miradi/dialogs/planning/treenodes/AbstractProjectNode.java
+++ source/org/miradi/dialogs/planning/treenodes/AbstractProjectNode.java
@@ -20,13 +20,80 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.planning.treenodes;
 
+import java.util.Vector;
+
+import org.miradi.icons.MiradiApplicationIcon;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.ProjectMetadata;
 import org.miradi.project.Project;
+import org.miradi.questions.ChoiceItem;
 import org.miradi.utils.CodeList;
 
 abstract public class AbstractProjectNode extends AbstractPlanningTreeNode
 {
-	public AbstractProjectNode(Project projectToUse, CodeList visibleRowsToUse)
+	public AbstractProjectNode(Project projectToUse, CodeList visibleRowsToUse) throws Exception
 	{
 		super(projectToUse, visibleRowsToUse);
+		
+		rebuild();
+	}
+	
+	@Override
+	public int getType()
+	{
+		return ProjectMetadata.getObjectType();
+	}
+
+	@Override
+	public BaseObject getObject()
+	{
+		return project.getMetadata();
+	}
+
+	@Override
+	public Object getValueAt(int column)
+	{
+		if (column == 0)
+			return getObject().toString();
+		
+		return new ChoiceItem("", "", new MiradiApplicationIcon());
+	}
+
+	public boolean attemptToAdd(ORef refToAdd)
+	{
+		return false;
+	}
+
+	@Override
+	public void rebuild() throws Exception
+	{
+		children = new Vector();
+		if (getProject().getCurrentViewData().shouldIncludeConceptualModelPage())
+			addConceptualModel();
+
+		if (getProject().getCurrentViewData().shouldIncludeResultsChain())
+			addResultsChainDiagrams();
+		
+		pruneUnwantedLayers(visibleRows);
+	}
+
+	@Override
+	public String toRawString()
+	{
+		return getProject().getFilename();
+	}
+
+	private void addConceptualModel() throws Exception
+	{
+		ORefList conceptualModelRefs = project.getConceptualModelDiagramPool().getORefList();
+		createAndAddChildren(conceptualModelRefs, null);
+	}
+
+	private void addResultsChainDiagrams() throws Exception
+	{
+		ORefList resultsChainRefs = project.getResultsChainDiagramPool().getORefList();
+		createAndAddChildren(resultsChainRefs, null);
 	}
 }
--- source/org/miradi/dialogs/planning/treenodes/WorkPlanProjectNode.java
+++ source/org/miradi/dialogs/planning/treenodes/WorkPlanProjectNode.java
@@ -19,15 +19,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.planning.treenodes;
 
-import java.util.Vector;
-
-import org.miradi.icons.MiradiApplicationIcon;
-import org.miradi.objecthelpers.ORef;
-import org.miradi.objecthelpers.ORefList;
-import org.miradi.objects.BaseObject;
-import org.miradi.objects.ProjectMetadata;
 import org.miradi.project.Project;
-import org.miradi.questions.ChoiceItem;
 import org.miradi.utils.CodeList;
 
 public class WorkPlanProjectNode extends AbstractProjectNode
@@ -35,63 +27,5 @@ public class WorkPlanProjectNode extends AbstractProjectNode
 	public WorkPlanProjectNode(Project projectToUse, CodeList visibleRowsToUse) throws Exception
 	{
 		super(projectToUse, visibleRowsToUse);
-		rebuild();
-	}
-	
-	@Override
-	public int getType()
-	{
-		return ProjectMetadata.getObjectType();
-	}
-
-	@Override
-	public BaseObject getObject()
-	{
-		return project.getMetadata();
-	}
-
-	@Override
-	public Object getValueAt(int column)
-	{
-		if (column == 0)
-			return getObject().toString();
-		
-		return new ChoiceItem("", "", new MiradiApplicationIcon());
-	}
-
-	public boolean attemptToAdd(ORef refToAdd)
-	{
-		return false;
-	}
-
-	@Override
-	public void rebuild() throws Exception
-	{
-		children = new Vector();
-		if (getProject().getCurrentViewData().shouldIncludeConceptualModelPage())
-			addConceptualModel();
-
-		if (getProject().getCurrentViewData().shouldIncludeResultsChain())
-			addResultsChainDiagrams();
-		
-		pruneUnwantedLayers(visibleRows);
-	}
-
-	@Override
-	public String toRawString()
-	{
-		return getProject().getFilename();
-	}
-
-	private void addConceptualModel() throws Exception
-	{
-		ORefList conceptualModelRefs = project.getConceptualModelDiagramPool().getORefList();
-		createAndAddChildren(conceptualModelRefs, null);
-	}
-
-	private void addResultsChainDiagrams() throws Exception
-	{
-		ORefList resultsChainRefs = project.getResultsChainDiagramPool().getORefList();
-		createAndAddChildren(resultsChainRefs, null);
-	}
+	}	
 }
