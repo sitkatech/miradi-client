29f2330a4821811c7fee7c07fa8c924b66f7e29e
nimaa <nimaa@localhost>
2010-03-26 15:27:55 +0000

    Cleanup

--- source/org/miradi/actions/ActionCreateActivity.java
+++ source/org/miradi/actions/ActionCreateActivity.java
@@ -35,6 +35,7 @@ public class ActionCreateActivity extends ObjectsAction
 		return EAM.text("Action|Manage|Create Activity");
 	}
 
+	@Override
 	public String getToolTipText()
 	{
 		return EAM.text("TT|Create a new Activity");
