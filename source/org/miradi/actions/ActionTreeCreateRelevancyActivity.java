6a7ecce220209c2a6387a214d23bb3e77da9e572
nimaa <nimaa@localhost>
2010-03-26 15:43:52 +0000

    Changed parent class. Removed unused constructor

--- source/org/miradi/actions/ActionTreeCreateRelevancyActivity.java
+++ source/org/miradi/actions/ActionTreeCreateRelevancyActivity.java
@@ -20,25 +20,14 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.actions;
 
-import org.miradi.icons.ActivityIcon;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 
-public class ActionTreeCreateRelevancyActivity extends ObjectsAction
+public class ActionTreeCreateRelevancyActivity extends AbstractActionCreateActivity
 {
 	public ActionTreeCreateRelevancyActivity(MainWindow mainWindowToUse)
 	{
-		this(mainWindowToUse, getLabel());
-	}
-
-	public ActionTreeCreateRelevancyActivity(MainWindow mainWindowToUse, String label)
-	{
-		super(mainWindowToUse, label, new ActivityIcon());
-	}
-
-	private static String getLabel()
-	{
-		return EAM.text("Action|Manage|Create Activity");
+		super(mainWindowToUse);
 	}
 
 	@Override
