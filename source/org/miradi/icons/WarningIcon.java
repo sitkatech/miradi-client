5b1f6624ae697b0fcb6efdb8e81f052a6bf72ed3
nimaa <nimaa@localhost>
2010-04-01 19:31:11 +0000

    Antialiasing warning icon

--- source/org/miradi/icons/WarningIcon.java
+++ source/org/miradi/icons/WarningIcon.java
@@ -23,12 +23,16 @@ package org.miradi.icons;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class WarningIcon extends AbstractTriangleIcon
 {
 	@Override
 	public void paintIcon(Component c, Graphics g, int x, int y)
 	{
+		turnOnAntialiasing(g);
+		
 		super.paintIcon(c, g, x, y);
 		
 		int exclamationMarkX = (getIconWidth() / 2) + x;
@@ -40,6 +44,20 @@ public class WarningIcon extends AbstractTriangleIcon
 		
 		int exclamantionPointY = y + 12;
 		g.fillRect(exclamationMarkX, exclamantionPointY, THICKNESS, THICKNESS);
+		
+		turnOffAntialiasing(g);
+	}
+
+	private void turnOnAntialiasing(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	}
+	
+	private void turnOffAntialiasing(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
 	}
 	
 	@Override
