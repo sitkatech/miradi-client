614b4ea6f4f6415fe20eb949b9fc58a92cd346b4
nimaa <nimaa@localhost>
2010-04-06 18:36:27 +0000

    Enabled all when cards

--- source/org/miradi/dialogfields/editors/WhenEditorComponent.java
+++ source/org/miradi/dialogfields/editors/WhenEditorComponent.java
@@ -35,8 +35,7 @@ public class WhenEditorComponent extends DisposablePanel
 	{
 		setLayout(new OneColumnGridLayout());
 
-		//FIXME urgen - temporarly disabled
-		String[] choices = new String[]{NONE_ITEM, PROJECT_TOTAL_ITEM, };//YEAR_ITEM, QUARTER_ITEM, MONTH_ITEM, DAY_ITEM,};
+		String[] choices = new String[]{NONE_ITEM, PROJECT_TOTAL_ITEM, YEAR_ITEM, QUARTER_ITEM, MONTH_ITEM, DAY_ITEM,};
 		dateUnitTypeCombo = new UiComboBox(choices);
 		dateUnitTypeCombo.addItemListener(new ChangeHandler());
 
