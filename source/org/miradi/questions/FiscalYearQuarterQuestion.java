f938f583aeb2508f837f9688f7397afe03218a4a
nimaa <nimaa@localhost>
2010-04-07 18:43:37 +0000

    Text changes.

--- source/org/miradi/questions/FiscalYearQuarterQuestion.java
+++ source/org/miradi/questions/FiscalYearQuarterQuestion.java
@@ -28,10 +28,10 @@ public class FiscalYearQuarterQuestion extends StaticChoiceQuestion
 	public ChoiceItem[] getChoices()
 	{
 		return new ChoiceItem[]{
-				new ChoiceItem("1", EAM.text("FY Q1")),
-				new ChoiceItem("2", EAM.text("FY Q2")),
-				new ChoiceItem("3", EAM.text("FY Q3")),
-				new ChoiceItem("4", EAM.text("FY Q4")),
+				new ChoiceItem("1", EAM.text("First Fiscal Quarter")),
+				new ChoiceItem("2", EAM.text("Second Fiscal Quarter")),
+				new ChoiceItem("3", EAM.text("Third Fiscal Quarter")),
+				new ChoiceItem("4", EAM.text("Fourth Fiscal Quarter")),
 		};
 	}
 }
