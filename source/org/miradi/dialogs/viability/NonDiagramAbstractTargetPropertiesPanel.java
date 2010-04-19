a0df4bb652b191f64ec47f0acac23e3418ebf1b3
nimaa <nimaa@localhost>
2010-04-19 22:00:55 +0000

    MRD-3983: Depending on target's mode display the correct status sub panel.  If target is in simple mode,  status drop down is editable.  If target is in viability mode, status is readonly.

--- source/org/miradi/dialogs/viability/NonDiagramAbstractTargetPropertiesPanel.java
+++ source/org/miradi/dialogs/viability/NonDiagramAbstractTargetPropertiesPanel.java
@@ -20,11 +20,14 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.viability;
 
-import org.miradi.dialogfields.ObjectDataInputField;
+import org.miradi.dialogs.base.ObjectDataInputPanel;
 import org.miradi.dialogs.base.ObjectDataInputPanelWithSections;
 import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
 import org.miradi.objects.AbstractTarget;
 import org.miradi.objects.HumanWelfareTarget;
+import org.miradi.objects.Target;
 import org.miradi.project.Project;
 import org.miradi.questions.StatusQuestion;
 
@@ -39,8 +42,12 @@ public class NonDiagramAbstractTargetPropertiesPanel extends ObjectDataInputPane
 		TargetCoreSubPanel targetCoreSubPanel = new TargetCoreSubPanel(getProject(), targetType);
 		addSubPanelWithTitledBorder(targetCoreSubPanel);
 		
+		simpleModeStatusSubPanel = new SimpleModeStatusSubPanel(getProject(), ORef.createInvalidWithType(targetType));
+		viabilityModeStatusSubPanel = new ViabilityModeStatusSubPanel(getProject(), ORef.createInvalidWithType(targetType));
+		
 		createSingleSection(EAM.text("Status"));
-		addFieldsOnOneLine("", new ObjectDataInputField[]{createReadOnlyStatusField()});
+		addSubPanelField(simpleModeStatusSubPanel);
+		addSubPanelField(viabilityModeStatusSubPanel);
 		
 		ModelessTargetSubPanel modelessTargetSubPanel = new ModelessTargetSubPanel(getProject(), targetType);
 		addSubPanelWithTitledBorder(modelessTargetSubPanel);
@@ -48,10 +55,28 @@ public class NonDiagramAbstractTargetPropertiesPanel extends ObjectDataInputPane
 		updateFieldsFromProject();
 	}
 	
-	private ObjectDataInputField createReadOnlyStatusField()
+	@Override
+	public void setObjectRefs(ORef[] orefsToUse)
 	{
-		return createReadOnlyChoiceField(targetType, AbstractTarget.PSEUDO_TAG_TARGET_VIABILITY,getProject().getQuestion(StatusQuestion.class));
-	} 
+		super.setObjectRefs(orefsToUse);
+		
+		ORef abstractTargetRef = extractTargetRef(new ORefList(orefsToUse));
+		if (abstractTargetRef.isValid())
+		{
+			AbstractTarget abstractTarget = AbstractTarget.findTarget(getProject(), abstractTargetRef);
+			simpleModeStatusSubPanel.setVisible(!abstractTarget.isViabilityModeTNC());
+			viabilityModeStatusSubPanel.setVisible(abstractTarget.isViabilityModeTNC());
+		}
+	}
+	
+	private ORef extractTargetRef(ORefList refs)
+	{
+		ORef targetRef = refs.getRefForType(Target.getObjectType());
+		if (targetRef.isValid())
+			return targetRef;
+		
+		return refs.getRefForType(HumanWelfareTarget.getObjectType());
+	}
 	
 	@Override
 	public String getPanelDescription()
@@ -62,5 +87,39 @@ public class NonDiagramAbstractTargetPropertiesPanel extends ObjectDataInputPane
 		return EAM.text("Title|Target Properties");
 	}
 	
-	private int targetType;
+	private class SimpleModeStatusSubPanel extends ObjectDataInputPanel
+	{
+		public SimpleModeStatusSubPanel(Project projectToUse, ORef refToUse)
+		{
+			super(projectToUse, refToUse);
+			
+			addField(createRatingChoiceField(targetType, Target.TAG_TARGET_STATUS, getProject().getQuestion(StatusQuestion.class)));
+		}
+
+		@Override
+		public String getPanelDescription()
+		{
+			return "SimpleModeStatusSubPanel";
+		}
+	}
+	
+	private class ViabilityModeStatusSubPanel extends ObjectDataInputPanel
+	{
+		public ViabilityModeStatusSubPanel(Project projectToUse, ORef refToUse)
+		{
+			super(projectToUse, refToUse);
+			
+			addField(createReadOnlyChoiceField(targetType, AbstractTarget.PSEUDO_TAG_TARGET_VIABILITY, getProject().getQuestion(StatusQuestion.class)));
+		}
+
+		@Override
+		public String getPanelDescription()
+		{
+			return "ViabilityModeStatusSubPanel";
+		}
+	}
+	
+	private int targetType;	
+	private SimpleModeStatusSubPanel simpleModeStatusSubPanel;
+	private ViabilityModeStatusSubPanel viabilityModeStatusSubPanel;
 }
