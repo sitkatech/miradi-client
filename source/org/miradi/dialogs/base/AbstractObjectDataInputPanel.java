634cbcfc637ae9f518115ae11baebc8dbd4e3d1b
nimaa <nimaa@localhost>
2010-05-12 16:48:39 +0000

    Reversed if logic and return value

--- source/org/miradi/dialogs/base/AbstractObjectDataInputPanel.java
+++ source/org/miradi/dialogs/base/AbstractObjectDataInputPanel.java
@@ -719,10 +719,10 @@ abstract public class AbstractObjectDataInputPanel extends ModelessDialogPanel i
 			return false;
 		
 		CommandDeleteObject cmd = (CommandDeleteObject)event.getCommand();
-		if(!cmd.getObjectRef().equals(getRefForType(cmd.getObjectType())))
-			return false;
+		if(cmd.getObjectRef().equals(getRefForType(cmd.getObjectType())))
+			return true;
 		
-		return true;
+		return false;
 	}
 
 	@Override
