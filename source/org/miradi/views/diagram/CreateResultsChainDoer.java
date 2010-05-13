35ab6c8af46faa9d90f84c5d2eb436b2503f6250
kevins <kevins@localhost>
2010-05-13 11:49:50 +0000

    MRD-4043: Create RC from RC tab when no RC's exist give error

--- source/org/miradi/views/diagram/CreateResultsChainDoer.java
+++ source/org/miradi/views/diagram/CreateResultsChainDoer.java
@@ -85,7 +85,7 @@ public class CreateResultsChainDoer extends ViewDoer
 		ORef newResultsChainRef = creatorHelper.createResultsChain();
 		selectResultsChain(project, diagramView, newResultsChainRef);
 		
-		if (diagramModel.isConceptualModelDiagram() && wereAnyAnnotationsTransferred(project, newResultsChainRef))
+		if (diagramModel != null && diagramModel.isConceptualModelDiagram() && wereAnyAnnotationsTransferred(project, newResultsChainRef))
 			EAM.notifyDialog(EAM.text("Any Indicators or Objectives that were associated to Contributing Factors or Threats \n" +
 									"have been moved to the corresponding Threat Reduction Result or Intermediate Result"));
 	}
