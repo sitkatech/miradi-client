6f1edd43d963e5f755b811c6af3c36bd60bbe65a
nimaa <nimaa@localhost>
2010-05-26 21:23:46 +0000

    Pulled out duplicated code to loop nodes and create objects.

--- source/org/miradi/xml/xmpz/ProjectResourceImporter.java
+++ source/org/miradi/xml/xmpz/ProjectResourceImporter.java
@@ -20,14 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml.xmpz;
 
-import org.miradi.ids.BaseId;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objects.ProjectResource;
 import org.miradi.questions.ResourceTypeQuestion;
 import org.miradi.xml.AbstractXmpzObjectImporter;
 import org.miradi.xml.wcs.WcsXmlConstants;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 
 public class ProjectResourceImporter extends AbstractXmpzObjectImporter
@@ -40,36 +38,33 @@ public class ProjectResourceImporter extends AbstractXmpzObjectImporter
 	@Override
 	public void importElement() throws Exception
 	{
-		String poolName = WcsXmlConstants.PROJECT_RESOURCE  + WcsXmlConstants.POOL_ELEMENT_TAG;
-		NodeList nodes = getImporter().getNodes(getImporter().getRootNode(), new String[]{poolName, WcsXmlConstants.PROJECT_RESOURCE, });
-		for (int index = 0; index < nodes.getLength(); ++index)
-		{
-			Node projectResourceNode = nodes.item(index);
-			String intIdAsString = getImporter().getAttributeValue(projectResourceNode, WcsXmlConstants.ID);
-			ORef projectResourceRef = getProject().createObject(ProjectResource.getObjectType(), new BaseId(intIdAsString));
-			importCodeField(projectResourceNode, WcsXmlConstants.PROJECT_RESOURCE, projectResourceRef, ProjectResource.TAG_RESOURCE_TYPE, new ResourceTypeQuestion());
-			
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_GIVEN_NAME);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_SUR_NAME);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_INITIALS);
-			importCodeListField(projectResourceNode, WcsXmlConstants.PROJECT_RESOURCE, projectResourceRef, ProjectResource.TAG_ROLE_CODES);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_ORGANIZATION);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_POSITION);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_LOCATION);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_PHONE_NUMBER);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_PHONE_NUMBER_MOBILE);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_PHONE_NUMBER_HOME);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_PHONE_NUMBER_OTHER);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_EMAIL);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_ALTERNATIVE_EMAIL);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_IM_ADDRESS);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_IM_SERVICE);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_DATE_UPDATED);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_COST_PER_UNIT);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_COMMENTS);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_CUSTOM_FIELD_1);
-			importSummaryField(projectResourceNode, projectResourceRef, ProjectResource.TAG_CUSTOM_FIELD_2);
-		}
+		importObject(ProjectResource.getObjectType());
+	}
+	
+	@Override
+	protected void importFields(Node node, ORef ref) throws Exception
+	{
+		importCodeField(node, WcsXmlConstants.PROJECT_RESOURCE, ref, ProjectResource.TAG_RESOURCE_TYPE, new ResourceTypeQuestion());			
+		importSummaryField(node, ref, ProjectResource.TAG_GIVEN_NAME);
+		importSummaryField(node, ref, ProjectResource.TAG_SUR_NAME);
+		importSummaryField(node, ref, ProjectResource.TAG_INITIALS);
+		importCodeListField(node, WcsXmlConstants.PROJECT_RESOURCE, ref, ProjectResource.TAG_ROLE_CODES);
+		importSummaryField(node, ref, ProjectResource.TAG_ORGANIZATION);
+		importSummaryField(node, ref, ProjectResource.TAG_POSITION);
+		importSummaryField(node, ref, ProjectResource.TAG_LOCATION);
+		importSummaryField(node, ref, ProjectResource.TAG_PHONE_NUMBER);
+		importSummaryField(node, ref, ProjectResource.TAG_PHONE_NUMBER_MOBILE);
+		importSummaryField(node, ref, ProjectResource.TAG_PHONE_NUMBER_HOME);
+		importSummaryField(node, ref, ProjectResource.TAG_PHONE_NUMBER_OTHER);
+		importSummaryField(node, ref, ProjectResource.TAG_EMAIL);
+		importSummaryField(node, ref, ProjectResource.TAG_ALTERNATIVE_EMAIL);
+		importSummaryField(node, ref, ProjectResource.TAG_IM_ADDRESS);
+		importSummaryField(node, ref, ProjectResource.TAG_IM_SERVICE);
+		importSummaryField(node, ref, ProjectResource.TAG_DATE_UPDATED);
+		importSummaryField(node, ref, ProjectResource.TAG_COST_PER_UNIT);
+		importSummaryField(node, ref, ProjectResource.TAG_COMMENTS);
+		importSummaryField(node, ref, ProjectResource.TAG_CUSTOM_FIELD_1);
+		importSummaryField(node, ref, ProjectResource.TAG_CUSTOM_FIELD_2);
 	}
 	
 	private void importSummaryField(Node projectSumaryNode,	ORef ref, String tag) throws Exception
