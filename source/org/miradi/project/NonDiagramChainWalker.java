d220e433f7c6e4407b7064cb0ff960a179f2fa11
kevins <kevins@localhost>
2010-06-22 23:08:05 +0000

    Fix compile error in previous commit (method needs to be public instead of private)

--- source/org/miradi/project/NonDiagramChainWalker.java
+++ source/org/miradi/project/NonDiagramChainWalker.java
@@ -102,7 +102,7 @@ public class NonDiagramChainWalker
 		return newFactorIfAny;
 	}
 	
-	private void clearCaches()
+	public void clearCaches()
 	{
 		cachedUpstreamChain = new HashMap();
 		cachedDownstreamChain = new HashMap();
