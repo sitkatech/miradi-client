756e45e4bd5c89df5c55dc44fa369f1fc099fdbf
kevins <kevins@localhost>
2010-06-23 00:33:44 +0000

    Rewrite NonDiagramChainWalker to just call DiagramChainWalker for each diagram this factor is on

--- source/org/miradi/project/NonDiagramChainWalker.java
+++ source/org/miradi/project/NonDiagramChainWalker.java
@@ -19,156 +19,45 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.project;
 
-import java.util.HashMap;
-import java.util.Vector;
-
+import org.miradi.diagram.DiagramChainWalker;
 import org.miradi.objecthelpers.FactorSet;
-import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
+import org.miradi.objects.DiagramFactor;
+import org.miradi.objects.DiagramObject;
 import org.miradi.objects.Factor;
-import org.miradi.objects.FactorLink;
 
 
 public class NonDiagramChainWalker
 {	
-	public NonDiagramChainWalker()
+	public NonDiagramChainWalker(Project projectToUse)
 	{
-		clearCaches();
+		project = projectToUse;
 	}
 
 	public FactorSet buildNormalChainAndGetFactors(Factor factor)
 	{
-		buildNormalChain(factor);
-		return getFactors();
-	}
-	
-	private FactorSet getFactors()
-	{
-		return factorSet;
-	}
-
-	private FactorSet getAllUpstreamFactors()
-	{
-		return getAllLinkedFactors(FactorLink.TO);
-	}
-	
-	private FactorSet getAllDownstreamFactors()
-	{
-		return getAllLinkedFactors(FactorLink.FROM);
+		FactorSet factorsOnAllDiagrams = new FactorSet();
+		DiagramChainWalker realWalker = new DiagramChainWalker();
+		ORefList diagramFactorRefs = factor.findObjectsThatReferToUs(DiagramFactor.getObjectType());
+		for(int i = 0; i < diagramFactorRefs.size(); ++i)
+		{
+			DiagramFactor df = DiagramFactor.find(getProject(), diagramFactorRefs.get(i));
+			ORefList diagramRefs = df.findDiagramsThatReferToUs();
+			if(diagramRefs.size() != 1)
+				throw new RuntimeException("DF " + df.getRef() + " is in multiple diagrams: " + diagramRefs);
+			
+			DiagramObject diagram = DiagramObject.findDiagramObject(getProject(), diagramRefs.getFirstElement());
+			FactorSet factorsOnThisDiagram = realWalker.buildNormalChainAndGetFactors(diagram, df);
+			factorsOnAllDiagrams.attemptToAddAll(factorsOnThisDiagram);
+		}
+		
+		return factorsOnAllDiagrams;
 	}
 	
 	private Project getProject()
 	{
-		return startingFactor.getProject();
+		return project;
 	}
 	
-	private void attemptToAdd(FactorLink thisLinkage)
-	{
-		if (!processedLinks.contains(thisLinkage))
-			processedLinks.add(thisLinkage);
-	}
-	
-	private FactorSet processLink(Factor thisFactor, FactorLink thisLink, int direction)
-	{
-		FactorSet newFactorIfAny = new FactorSet();
-		if(thisLink.getFactorRef(direction).equals(thisFactor.getRef()))
-		{
-			attemptToAdd(thisLink);
-			Factor linkedNode = (Factor) getProject().findObject(thisLink.getOppositeFactorRef(direction));
-			newFactorIfAny.attemptToAdd(linkedNode);
-			return newFactorIfAny;
-		}
-		
-		if (!thisLink.isBidirectional())
-			return newFactorIfAny;
-		
-		if(thisLink.getOppositeFactorRef(direction).equals(thisFactor.getRef()))
-		{
-			attemptToAdd(thisLink);
-			Factor linkedNode = (Factor) getProject().findObject(thisLink.getFactorRef(direction));
-			newFactorIfAny.attemptToAdd(linkedNode);
-		}
-		
-		return newFactorIfAny;
-	}
-	
-	public void clearCaches()
-	{
-		cachedUpstreamChain = new HashMap();
-		cachedDownstreamChain = new HashMap();
-	}
-
-	private void buildNormalChain(Factor factor)
-	{
-		initializeChain(factor);
-		factorSet.attemptToAddAll(getAllDownstreamFactors());
-		factorSet.attemptToAddAll(getAllUpstreamFactors());
-	}
-
-	private FactorSet getAllLinkedFactors(int direction)
-	{
-		HashMap<ORef, FactorSet> cache = getCache(direction);
-		if(cache.containsKey(startingFactor.getRef()))
-			return cache.get(startingFactor.getRef());
-		FactorSet linkedFactors = new FactorSet();
-		FactorSet unprocessedFactors = new FactorSet();
-		linkedFactors.attemptToAdd(startingFactor);
-		
-		ORefList factorLinkRefs = getAllFactorLinkRefs();		
-		unprocessedFactors.attemptToAddAll(getFactorsToProcess(direction, factorLinkRefs, startingFactor));
-		
-		while(unprocessedFactors.size() > 0)
-		{
-			Factor thisFactor = (Factor)unprocessedFactors.toArray()[0];
-			if (!linkedFactors.contains(thisFactor))
-			{
-				linkedFactors.attemptToAdd(thisFactor);
-				unprocessedFactors.attemptToAddAll(getFactorsToProcess(direction, factorLinkRefs, thisFactor));
-			}
-			unprocessedFactors.remove(thisFactor);
-		}
-		
-		cache.put(startingFactor.getRef(), linkedFactors);
-		return linkedFactors;
-	}
-
-	private FactorSet getFactorsToProcess(int direction, ORefList allFactorLinkRefs, Factor factorToProcess)
-	{
-		FactorSet unprocessedFactors = new FactorSet();
-		for(int index = 0; index < allFactorLinkRefs.size(); ++index)
-		{
-			FactorLink factorLink = FactorLink.find(getProject(), allFactorLinkRefs.get(index));	
-			unprocessedFactors.attemptToAddAll(processLink(factorToProcess, factorLink, direction));
-		}
-		
-		return unprocessedFactors;
-	}
-	private HashMap<ORef, FactorSet> getCache(int direction)
-	{
-		switch(direction)
-		{
-			case FactorLink.FROM:
-				return cachedDownstreamChain;
-			case FactorLink.TO:
-				return cachedUpstreamChain;
-		}
-		
-		throw new RuntimeException("Unknown direction: " + direction);
-	}
-	private void initializeChain(Factor factor)
-	{
-		this.startingFactor = factor;
-		factorSet = new FactorSet();
-		processedLinks = new Vector();
-	}
-	private ORefList getAllFactorLinkRefs()
-	{
-		return getProject().getFactorLinkPool().getFactorLinkRefs();
-	}
-
-	private FactorSet factorSet;
-	private Vector processedLinks;
-	private Factor startingFactor;
-	private HashMap<ORef, FactorSet> cachedUpstreamChain;
-	private HashMap<ORef, FactorSet> cachedDownstreamChain;
+	private Project project;
 }
