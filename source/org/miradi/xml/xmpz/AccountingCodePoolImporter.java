0ae7691d3ca031c5b1c742a86fa16dba494784f6
nimaa <nimaa@localhost>
2010-06-23 17:23:54 +0000

    Extracted out a generic parent class

--- source/org/miradi/xml/xmpz/AccountingCodePoolImporter.java
+++ source/org/miradi/xml/xmpz/AccountingCodePoolImporter.java
@@ -20,24 +20,13 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml.xmpz;
 
-import org.miradi.objecthelpers.ORef;
 import org.miradi.objects.AccountingCode;
 import org.miradi.xml.wcs.WcsXmlConstants;
-import org.w3c.dom.Node;
 
-public class AccountingCodePoolImporter extends AbstractBaseObjectPoolImporter
+public class AccountingCodePoolImporter extends AbstractBudgetCategoryPoolImporter
 {
 	public AccountingCodePoolImporter(XmpzXmlImporter importerToUse)
 	{
 		super(importerToUse, WcsXmlConstants.ACCOUNTING_CODE, AccountingCode.getObjectType());
-	}
-	
-	@Override
-	protected void importFields(Node node, ORef destinationRef)	throws Exception
-	{
-		super.importFields(node, destinationRef);
-		
-		importField(node, destinationRef, AccountingCode.TAG_CODE);
-		importField(node, destinationRef, AccountingCode.TAG_COMMENTS);
-	}
+	}	
 }
