fcd3ec59e77a50cecaff56737c502a8bba3e4c30
nimaa <nimaa@localhost>
2010-07-06 16:41:00 +0000

    Rollup reports metadata root node displays static name

--- source/org/miradi/dialogs/planning/treenodes/BaseObjectTreeRootNodeWithUnspecifiedNode.java
+++ source/org/miradi/dialogs/planning/treenodes/BaseObjectTreeRootNodeWithUnspecifiedNode.java
@@ -44,6 +44,11 @@ public class BaseObjectTreeRootNodeWithUnspecifiedNode extends BaseObjectTreeRoo
 	@Override
 	public String toRawString()
 	{
+		return getProjectTotalsName();
+	}
+
+	public static String getProjectTotalsName()
+	{
 		return EAM.text("Project Totals");
 	}
 }
