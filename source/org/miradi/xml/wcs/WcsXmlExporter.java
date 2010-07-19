e9a347bfcd12691ba6faa08a27c6a4032e9bcf1c
nimaa <nimaa@localhost>
2010-07-19 20:08:53 +0000

    Class rename

--- source/org/miradi/xml/wcs/WcsXmlExporter.java
+++ source/org/miradi/xml/wcs/WcsXmlExporter.java
@@ -104,7 +104,7 @@ public class WcsXmlExporter extends XmlExporter implements WcsXmlConstants
 		new AccountingCodePoolExporter(this).exportXml();
 		new FundingSourcePoolExporter(this).exportXml();
 		new BudgetCategoryOnePoolExporter(this).exportXml();
-		new CategoryTwoPoolExporter(this).exportXml();
+		new BudgetCategoryTwoPoolExporter(this).exportXml();
 		new ExpenseAssignmentPoolExporter(this).exportXml();
 		new ResourceAssignmentPoolExporter(this).exportXml();
 		new ThreatTargetThreatRatingElementExporter(this).exportXml();
