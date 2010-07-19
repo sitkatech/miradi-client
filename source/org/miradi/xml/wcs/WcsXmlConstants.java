ce655a7363c0383c0bdcbc25d4a798873b245354
nimaa <nimaa@localhost>
2010-07-19 20:04:57 +0000

    Renamed var

--- source/org/miradi/xml/wcs/WcsXmlConstants.java
+++ source/org/miradi/xml/wcs/WcsXmlConstants.java
@@ -95,7 +95,7 @@ public interface WcsXmlConstants
 	public static final String MEASUREMENT_IDS = "MeasurementIds";
 	public static final String ACCOUNTING_CODE = "AccountingCode";
 	public static final String FUNDING_SOURCE = "FundingSource";
-	public static final String CATEGORY_ONE = "BudgetCategoryOne";
+	public static final String BUDGET_CATEGORY_ONE = "BudgetCategoryOne";
 	public static final String CATEGORY_TWO = "BudgetCategoryTwo";
 	public static final String PROJECT_LOCATION = "ProjectLocation";
 	public static final String GEOSPATIAL_LOCATION = "GeospatialLocation";
