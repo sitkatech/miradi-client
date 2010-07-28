46d953fd815de940c974b1fee92de265fa9b28ad
nimaa <nimaa@localhost>
2010-07-28 17:30:11 +0000

    MRD-4208 using root node that has both RC and CM children for all trees and using a custom root node for configurable which depends on user input if it contains CM or RC nodes.

--- source/org/miradi/dialogs/planning/upperPanel/PlanningTreeTableModel.java
+++ source/org/miradi/dialogs/planning/upperPanel/PlanningTreeTableModel.java
@@ -20,8 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.planning.upperPanel;
 
 import org.miradi.dialogs.planning.treenodes.AbstractPlanningTreeNode;
-import org.miradi.dialogs.planning.treenodes.HiddenProjectRootNode;
-import org.miradi.dialogs.planning.treenodes.PlanningTreeErrorNode;
+import org.miradi.dialogs.planning.treenodes.ObjectsOnlyRootNode;
 import org.miradi.dialogs.treetables.GenericTreeTableModel;
 import org.miradi.dialogs.treetables.TreeTableNode;
 import org.miradi.main.EAM;
@@ -48,16 +47,7 @@ abstract public class PlanningTreeTableModel extends GenericTreeTableModel
 
 	private static TreeTableNode createPlanningTreeRootNode(Project projectToUse, CodeList visibleRowCodesToUse) throws Exception
 	{
-		try
-		{
-			return new HiddenProjectRootNode(projectToUse, visibleRowCodesToUse);
-		}
-		catch(Exception e)
-		{
-			EAM.logException(e);
-			EAM.errorDialog(EAM.text("<HTML>A severe error has prevented this Planning Subview from being shown.<BR>Please contact Miradi Support (<a href=\"mailto:support@miradi.org\">support@miradi.org<a>) for assistance</HTML>"));
-			return new PlanningTreeErrorNode(projectToUse, projectToUse.getMetadata().getRef()); 
-		}
+		return new ObjectsOnlyRootNode(projectToUse, visibleRowCodesToUse);
 	}
 	
 	protected void updateColumnsToShow(CodeList visibleColumnCodes)
