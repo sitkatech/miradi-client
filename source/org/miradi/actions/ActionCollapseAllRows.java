3fb244d5f87746c35e6992f71463a747f930a43c
nimaa <nimaa@localhost>
2010-08-05 21:22:51 +0000

    MRD-4253: new collapse/expand all icons added to buttons

--- source/org/miradi/actions/ActionCollapseAllRows.java
+++ source/org/miradi/actions/ActionCollapseAllRows.java
@@ -19,6 +19,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.actions;
 
+import org.miradi.icons.CollapseAllIcon;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 
@@ -26,7 +27,7 @@ public class ActionCollapseAllRows extends ObjectsAction
 {
 	public ActionCollapseAllRows(MainWindow mainWindow)
 	{
-		super(mainWindow, getLabel());
+		super(mainWindow, getLabel(), new CollapseAllIcon());
 	}
 
 	private static String getLabel()
