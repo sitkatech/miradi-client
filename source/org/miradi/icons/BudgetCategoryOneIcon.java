6bd22d15d7d9776bb5ffb4da1f75bd33622157dd
nimaa <nimaa@localhost>
2010-08-05 23:12:26 +0000

    mrd-4170: Changed BudgetCategoryOne/Two icons to load a png icon

--- source/org/miradi/icons/BudgetCategoryOneIcon.java
+++ source/org/miradi/icons/BudgetCategoryOneIcon.java
@@ -20,30 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.icons;
 
-import java.awt.Color;
+import org.miradi.utils.MiradiResourceImageIcon;
 
-import javax.swing.Icon;
-
-import org.miradi.diagram.renderers.FactorRenderer;
-import org.miradi.diagram.renderers.RectangleRenderer;
-
-public class BudgetCategoryOneIcon extends AbstractShapeIcon implements Icon
+public class BudgetCategoryOneIcon extends MiradiResourceImageIcon
 {
-	@Override
-	FactorRenderer getRenderer() 
+	public BudgetCategoryOneIcon()
 	{
-		return new RectangleRenderer();
-	}
-
-	@Override
-	Color getIconColor() 
-	{
-		return Color.RED;
-	}
-	
-	@Override
-	public int getIconHeight()
-	{
-		return 10;
+		super("icons/budgetCategoryOne.png");
 	}
 }
