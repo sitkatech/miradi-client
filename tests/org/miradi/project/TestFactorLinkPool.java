0c068aa9dc081d6aa93eb7168372d863bea1c0f7
kevins <kevins@localhost>
2010-08-17 16:32:16 +0000

    Remove methods that were only called by a test, and remove that test code

--- tests/org/miradi/project/TestFactorLinkPool.java
+++ tests/org/miradi/project/TestFactorLinkPool.java
@@ -20,14 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.project;
 
 import org.miradi.ids.FactorId;
-import org.miradi.ids.FactorLinkId;
-import org.miradi.ids.IdAssigner;
 import org.miradi.main.TestCaseWithProject;
-import org.miradi.objecthelpers.ObjectType;
-import org.miradi.objectpools.FactorLinkPool;
-import org.miradi.objects.Factor;
-import org.miradi.objects.FactorLink;
-import org.miradi.project.FactorLinkListener;
 
 public class TestFactorLinkPool extends TestCaseWithProject
 {
@@ -39,25 +32,8 @@ public class TestFactorLinkPool extends TestCaseWithProject
 	public void setUp() throws Exception
 	{
 		super.setUp();
-		idAssigner = new IdAssigner();
 	}
 
-	public void testHasLinkage() throws Exception
-	{
-		FactorLinkPool pool = new FactorLinkPool(idAssigner);
-		Factor node1 = Factor.createConceptualModelObject(getObjectManager(), takeNextModelNodeId(), ObjectType.CAUSE);
-		Factor node2 = Factor.createConceptualModelObject(getObjectManager(), takeNextModelNodeId(), ObjectType.CAUSE);
-		Factor node3 = Factor.createConceptualModelObject(getObjectManager(), takeNextModelNodeId(), ObjectType.CAUSE);
-		
-		FactorLinkId linkageId = new FactorLinkId(idAssigner.takeNextId().asInt());
-		FactorLink linkage = new FactorLink(getObjectManager(), linkageId, node1.getRef(), node2.getRef());
-		pool.put(linkage);
-		
-		assertTrue("Didn't find link 1->2?", pool.isLinked(linkage.getFromFactorRef(), linkage.getToFactorRef()));
-		assertTrue("Didn't find link 2->1?", pool.isLinked(linkage.getToFactorRef(), linkage.getFromFactorRef()));
-		assertFalse("Found link 1->3?", pool.isLinked(node1.getRef(), node3.getRef()));
-	}
-	
 	class LinkageMonitor implements FactorLinkListener
 	{
 		public void factorLinkWasCreated(FactorId linkFromId, FactorId linkToId)
@@ -69,11 +45,5 @@ public class TestFactorLinkPool extends TestCaseWithProject
 		}		
 	}
 	
-	private FactorId takeNextModelNodeId()
-	{
-		return new FactorId(idAssigner.takeNextId().asInt());
-	}
-	
 
-	private IdAssigner idAssigner;
 }
