351cbcc7235610d72aed8cf6c08c6e0a84ac5023
nimaa <nimaa@localhost>
2010-08-19 21:50:26 +0000

    mrd-3804: org table can now be exported as image

--- source/org/miradi/dialogs/organization/OrganizationManagementPanel.java
+++ source/org/miradi/dialogs/organization/OrganizationManagementPanel.java
@@ -19,6 +19,8 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.organization;
 
+import java.awt.image.BufferedImage;
+
 import javax.swing.Icon;
 import javax.swing.JComponent;
 
@@ -28,6 +30,8 @@ import org.miradi.icons.OrganizationIcon;
 import org.miradi.ids.BaseId;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
+import org.miradi.utils.BufferedImageFactory;
+import org.miradi.utils.ImageTooLargeException;
 import org.miradi.utils.ObjectTableModelExporter;
 import org.miradi.utils.TableExporter;
 
@@ -80,6 +84,18 @@ public class OrganizationManagementPanel extends ObjectPoolManagementPanel
 	public JComponent getPrintableComponent() throws Exception
 	{
 		return createTable();
+	}
+	
+	@Override
+	public boolean isImageAvailable()
+	{
+		return true;
+	}
+	
+	@Override
+	public BufferedImage getImage() throws ImageTooLargeException
+	{
+		return BufferedImageFactory.createImageFromTable(createTable());
 	}
 		
 	@Override
