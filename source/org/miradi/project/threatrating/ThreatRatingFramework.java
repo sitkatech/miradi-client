0c1d22bee16de115554a258f0b86e1be0f19c07a
Nima A. <nimaa@benetech.org>
2010-09-16 18:48:20 -0400

    Pulled up methods.  Extracted out abstract methods out of the pulled up methods.  No longer overriding get2Prime method

--- source/org/miradi/project/threatrating/ThreatRatingFramework.java
+++ source/org/miradi/project/threatrating/ThreatRatingFramework.java
@@ -19,12 +19,19 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.project.threatrating;
 
+import java.util.Vector;
+
 import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefSet;
+import org.miradi.objecthelpers.ThreatTargetVirtualLinkHelper;
+import org.miradi.objects.Cause;
 import org.miradi.objects.Factor;
+import org.miradi.objects.Target;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.ChoiceQuestion;
 import org.miradi.questions.ThreatRatingQuestion;
+import org.miradi.utils.Utility;
 
 abstract public class ThreatRatingFramework
 {
@@ -63,11 +70,62 @@ abstract public class ThreatRatingFramework
 		return question.findChoiceByCode(code);
 	}
 	
+	public int get2PrimeSummaryRatingValue(Factor factor) throws Exception
+	{
+		return getThreatFormula().getSummaryOfBundlesWithTwoPrimeRule(calculateSummaryRatingValues(factor));
+	}
+		
+	protected int[] calculateSummaryRatingValues(Factor factor) throws Exception
+	{
+		if (factor.isDirectThreat())
+			return calculateSummaryRatingValue((Cause) factor);
+		
+		if (factor.isTarget())
+			return calculateSummaryRatingValue((Target) factor);
+		
+		return new int[0];
+	}
+	
+	private int[] calculateSummaryRatingValue(Target target) throws Exception
+	{
+		ORefSet upstreamThreatRefs = getUpstreamThreatRefs(target);
+
+		return calculateSummaryRatingValue(upstreamThreatRefs, new ORefSet(target));
+	}
+
+	private int[] calculateSummaryRatingValue(Cause threat) throws Exception
+	{
+		ORefSet downStreamTargets = getDownstreamTargetRefs(threat);
+		
+		return calculateSummaryRatingValue(new ORefSet(threat), downStreamTargets);
+	}
+
+	private int[] calculateSummaryRatingValue(ORefSet upstreamThreats, ORefSet downstreamTargets) throws Exception
+	{
+		if (upstreamThreats.size() > 1 && downstreamTargets.size() > 1)
+			throw new RuntimeException("Method should only be used to calculate rating from a sinlge factor to multiple up/downstream factors");
+		
+		Vector<Integer> calculatedSummaryRatingValues = new Vector<Integer>();
+		ThreatTargetVirtualLinkHelper threatTargetVirtualLink = new ThreatTargetVirtualLinkHelper(getProject());
+		for (ORef threatRef : upstreamThreats)
+		{
+			for(ORef targetRef : downstreamTargets)
+			{
+				int threatRatingBundleValue = threatTargetVirtualLink.calculateThreatRatingBundleValue(threatRef, targetRef);
+				calculatedSummaryRatingValues.add(threatRatingBundleValue);
+			}
+		}
+		
+		return Utility.convertToIntArray(calculatedSummaryRatingValues);
+	}
+	
 	abstract protected ThreatFormula getThreatFormula();
 
 	abstract public ChoiceItem getThreatThreatRatingValue(ORef threatRef) throws Exception;
 	
-	abstract public int get2PrimeSummaryRatingValue(Factor factor) throws Exception;
+	abstract protected ORefSet getUpstreamThreatRefs(Target target);
 	
+	abstract protected ORefSet getDownstreamTargetRefs(Cause threat);
+		
 	protected Project project;
 }
