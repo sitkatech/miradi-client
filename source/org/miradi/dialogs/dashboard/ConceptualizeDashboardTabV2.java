bd17c6c341eacd5ff23e4775d2a3091b6c306d6c
Nima A. <nimaa@benetech.org>
2010-09-30 19:49:35 -0400

    pulled up vars/inner classes.

--- source/org/miradi/dialogs/dashboard/ConceptualizeDashboardTabV2.java
+++ source/org/miradi/dialogs/dashboard/ConceptualizeDashboardTabV2.java
@@ -22,14 +22,10 @@ package org.miradi.dialogs.dashboard;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.HashMap;
-import java.util.Set;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.JComponent;
 
 import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
 import org.miradi.layout.TwoColumnPanel;
@@ -38,16 +34,12 @@ import org.miradi.objecthelpers.ORef;
 import org.miradi.objects.Dashboard;
 import org.miradi.project.Project;
 import org.miradi.utils.FillerLabel;
-import org.miradi.views.umbrella.PersistentNonPercentageHorizontalSplitPane;
 
 public class ConceptualizeDashboardTabV2 extends AbstractDashboardTag
 {
 	public ConceptualizeDashboardTabV2(Project projectToUse) throws Exception
 	{
 		super(projectToUse, Dashboard.getObjectType());
-		
-		clickableComponentToContentsFileNameMap = new HashMap<SelectableRow, String>();
-		splitPane = new PersistentNonPercentageHorizontalSplitPane(getMainWindow(), getMainWindow(), getPanelDescription());		
 		
 		addLeftPanel(createLeftPanel());
 		
@@ -253,88 +245,6 @@ public class ConceptualizeDashboardTabV2 extends AbstractDashboardTag
 		return "Conceptualize";
 	}
 	
-	private class ClickHandler extends MouseAdapter
-	{
-		public ClickHandler(SelectableRow clickableComponentToUse)
-		{
-			selectableComponent = clickableComponentToUse;
-		}
-		
-		@Override
-		public void mouseClicked(MouseEvent e)
-		{
-			super.mouseClicked(e);
-			
-			try
-			{
-				clearSelection();
-				selectableComponent.selectRow();
-				
-				String resourceFileName = clickableComponentToContentsFileNameMap.get(selectableComponent);
-				rightSideDescriptionPanel.setRightSidePanelContent(resourceFileName);
-			}
-			catch (Exception exception)
-			{
-				EAM.logException(exception);
-				EAM.unexpectedErrorDialog(exception);
-			}
-		}
-	
-		private void clearSelection() throws Exception
-		{
-			Set<SelectableRow> selectableRows = clickableComponentToContentsFileNameMap.keySet();
-			for(SelectableRow selectableRow : selectableRows)
-			{
-				selectableRow.clearSelection();
-			}
-		}
-		
-		private SelectableRow selectableComponent;
-	}
-	
-	private class SelectableRow
-	{
-		protected SelectableRow(JComponent leftSideToUse, JComponent rightSideToUse)
-		{
-			leftSide = leftSideToUse;
-			rightSide = rightSideToUse;
-			
-			leftSide.addMouseListener(new ClickHandler(this));
-			rightSide.addMouseListener(new ClickHandler(this));
-		}
-		
-		protected void selectRow()
-		{
-			leftSide.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
-			rightSide.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
-		}
-		
-		protected void clearSelection()
-		{
-			leftSide.setBorder(BorderFactory.createEtchedBorder());
-			rightSide.setBorder(BorderFactory.createEtchedBorder());
-		}
-		
-		protected void setBackgroundColor(Color backgroundColor)
-		{
-			setBackgroundColor(leftSide, backgroundColor);
-			setBackgroundColor(rightSide, backgroundColor);
-		}
-		
-		private void setBackgroundColor(JComponent component, Color backgroundColor)
-		{
-			component.setOpaque(true);
-			component.setBackground(backgroundColor);
-		}
-		
-		private JComponent leftSide;
-		private JComponent rightSide;
-	}
-	
-	private HashMap<SelectableRow, String> clickableComponentToContentsFileNameMap;
-	private DashboardRightSideDescriptionPanel rightSideDescriptionPanel;
-	private PersistentNonPercentageHorizontalSplitPane splitPane;
-	private static final int INDENT_PER_LEVEL = 20;
 	private static final String TEAM_RIGHT_PANEL_FILE_NAME = "dashboard/1A.html";
 	private static final String SCOPE_AND_VISION_RIGHT_PANEL_FILE_NAME = "dashboard/1B.html";
 	private static final String CRITICAL_THREATS_RIGHT_PANEL_FILE_NAME = "dashboard/1C.html";
