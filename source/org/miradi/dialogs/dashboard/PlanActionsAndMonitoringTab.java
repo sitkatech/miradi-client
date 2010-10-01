b06a7972350cd25a4d5fdb54fd3fcf5ab9eb101b
Nima A. <nimaa@benetech.org>
2010-10-01 11:39:12 -0400

    Added new Rows to PlanActions and monitoring dashboard tab

--- source/org/miradi/dialogs/dashboard/PlanActionsAndMonitoringTab.java
+++ source/org/miradi/dialogs/dashboard/PlanActionsAndMonitoringTab.java
@@ -20,8 +20,14 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.dashboard;
 
+import java.awt.Color;
+import java.util.HashMap;
+
+import javax.swing.BorderFactory;
+
 import org.miradi.layout.TwoColumnPanel;
 import org.miradi.main.EAM;
+import org.miradi.objects.Dashboard;
 import org.miradi.project.Project;
 
 public class PlanActionsAndMonitoringTab extends AbstractDashboardTab
@@ -41,10 +47,80 @@ public class PlanActionsAndMonitoringTab extends AbstractDashboardTab
 	protected TwoColumnPanel createLeftPanel()
 	{
 		TwoColumnPanel leftMainPanel = new TwoColumnPanel();
-		
+		leftMainPanel.setBorder(BorderFactory.createEtchedBorder(Color.red, Color.red));
 		createHeaderRow(leftMainPanel, EAM.text("2. Plan Actions and Monitoring"), "", getMainDescriptionFileName());
 		
+		addDevelopStrategicPlanRow(leftMainPanel);
+		
 		return leftMainPanel;
+	}
+
+	private void addDevelopStrategicPlanRow(TwoColumnPanel leftMainPanel)
+	{
+		addSubHeaderRow(leftMainPanel, EAM.text("2A. Develop Strategic Plan"), DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
+		
+		createTargetsWithGoalRow(leftMainPanel);
+		createGoalCountRow(leftMainPanel);
+		createDraftStrategiesCountRow(leftMainPanel);
+		createRankedDraftStrategiesCountRow(leftMainPanel);
+		createStrategyCountRow(leftMainPanel);
+		createStrategyWithTaxonomyCountRow(leftMainPanel);
+	}
+
+	private void createStrategyWithTaxonomyCountRow(TwoColumnPanel leftMainPanel)
+	{
+		String leftColumnTranslatedText = EAM.text("Strategies with taxonomy assingments");
+		HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+		tokenReplacementMap.put("%strategyWithTaxonomyCount", getDashboardData(Dashboard.PSEUDO_STRATEGY__WITH_TAXONOMY_COUNT));
+		String rightColumnTranslatedText = EAM.substitute(EAM.text("%strategyWithTaxonomyCount with taxonomy assignments"), tokenReplacementMap);
+
+		createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
+	}
+
+	private void createStrategyCountRow(TwoColumnPanel leftMainPanel)
+	{
+		String leftColumnTranslatedText = EAM.text("Create Strategies");
+		HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+		tokenReplacementMap.put("%strategyCount", getDashboardData(Dashboard.PSEUDO_STRATEGY_COUNT));
+		String rightColumnTranslatedText = EAM.substitute(EAM.text("%strategyCount Strategies Created"), tokenReplacementMap);
+
+		createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
+	}
+
+	private void createRankedDraftStrategiesCountRow(TwoColumnPanel leftMainPanel)
+	{
+		String leftColumnTranslatedText = EAM.text("Rank Draft Strategies");
+		HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+		tokenReplacementMap.put("%rankedDraftStrategiesCount", getDashboardData(Dashboard.PSEUDO_RANKED_DRAFT_STRATEGY_COUNT));
+		String rightColumnTranslatedText = EAM.substitute(EAM.text("%rankedDraftStrategiesCount Draft Strategies Ranked"), tokenReplacementMap);
+
+		createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
+	}
+
+	private void createDraftStrategiesCountRow(TwoColumnPanel leftMainPanel)
+	{
+		String leftColumnTranslatedText = EAM.text("Create Draft Strategies");
+		HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+		tokenReplacementMap.put("%draftStrategiesCount", getDashboardData(Dashboard.PSEUDO_DRAFT_STRATEGY_COUNT));
+		String rightColumnTranslatedText = EAM.substitute(EAM.text("%draftStrategiesCount Draft Strategies Created"), tokenReplacementMap);
+
+		createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
+	}
+
+	private void createGoalCountRow(TwoColumnPanel leftMainPanel)
+	{
+		createDataRow(leftMainPanel, EAM.text("Total Goals Created:"), getDashboardData(Dashboard.PSEUDO_GOAL_COUNT), DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
+	}
+
+	protected void createTargetsWithGoalRow(TwoColumnPanel leftMainPanel)
+	{
+		String leftColumnTranslatedText = EAM.text("Develop Goals for Each Target:");
+		HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+		tokenReplacementMap.put("%targetWithGoalCount", getDashboardData(Dashboard.PSEUDO_TARGET_COUNT));
+		tokenReplacementMap.put("%targetCount", getDashboardData(Dashboard.PSEUDO_TARGET_COUNT));
+		String rightColumnTranslatedText = EAM.substitute(EAM.text("%targetWithGoalCount of %targetCount Targets have Goals"), tokenReplacementMap);
+
+		createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME);
 	}
 
 	@Override
@@ -52,4 +128,6 @@ public class PlanActionsAndMonitoringTab extends AbstractDashboardTab
 	{
 		return EAM.text("Plan Actions and Monitoring");
 	}
+	
+	private static final String DEVELOP_STRATEGIC_PLAN_RIGHT_SIDE_FILENAME = "dashboard/2A.html";
 }
