301033b8466af44861d81007c1b72bc8855955b9
Kevin <kevins@benetech.org>
2010-10-13 17:20:01 -0400

    MRD-4304: Avoid data corruption in Indicator Create From...

--- source/org/miradi/objects/BaseObject.java
+++ source/org/miradi/objects/BaseObject.java
@@ -886,7 +886,7 @@ abstract public class BaseObject
 				continue;
 			if(isPseudoField(tag))
 				continue;
-			if(isIdListField(tag))
+			if(isIdListTag(tag))
 				continue;
 			if(isRefList(tag))
 				continue;
@@ -896,12 +896,6 @@ abstract public class BaseObject
 		return (CommandSetObjectData[])commands.toArray(new CommandSetObjectData[0]);
 	}
 
-	private boolean isIdListField(String tag)
-	{
-		return tag.indexOf("_IDS")>0;
-	}
-	
-	
 	public EnhancedJsonObject toJson()
 	{
 		EnhancedJsonObject json = new EnhancedJsonObject();
