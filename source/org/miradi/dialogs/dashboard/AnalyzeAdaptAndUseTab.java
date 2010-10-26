fd5d909a4c6f4446f8f21f4f652307a80fa34970
Nima A. <nimaa@benetech.org>
2010-10-26 16:00:55 -0400

    Using wizard step name instead of view name to switch view.

--- source/org/miradi/dialogs/dashboard/AnalyzeAdaptAndUseTab.java
+++ source/org/miradi/dialogs/dashboard/AnalyzeAdaptAndUseTab.java
@@ -23,7 +23,6 @@ package org.miradi.dialogs.dashboard;
 import org.miradi.layout.TwoColumnPanel;
 import org.miradi.main.EAM;
 import org.miradi.project.Project;
-import org.miradi.views.summary.SummaryView;
 
 public class AnalyzeAdaptAndUseTab extends AbstractDashboardTab
 {
@@ -42,10 +41,10 @@ public class AnalyzeAdaptAndUseTab extends AbstractDashboardTab
 	protected TwoColumnPanel createLeftPanel()
 	{
 		TwoColumnPanel leftMainPanel = new TwoColumnPanel();
-		createHeaderRow(leftMainPanel, EAM.text("4. Analyze, Use and Adapt"), "", getMainDescriptionFileName(), SummaryView.getViewName());
-		createSubHeaderRow(leftMainPanel, EAM.text("4A. Prepare Data for Analysis"), EAM.text("Develop Systems for Recording, Storing, Processing and Backing Up Project Data"), PREPARE_DATA_FOR_ANALYSIS_RIGTH_SIDE_FILENAME, "");
-		createSubHeaderRow(leftMainPanel, EAM.text("4B. Analyze Results"), EAM.text("Analyze Project Results and Assumptions Analyze Operational and Financial Data Document Discussions and Decisions"), ANALYZE_RESULTS_RIGTH_SIDE_FILENAME, "");
-		createSubHeaderRow(leftMainPanel, EAM.text("4C. Adapt Project Plan"), EAM.text("Revise Project Plan: Strategic Monitoring Operational Work Plan"), ADAPT_PROJECT_PLAN_RIGTH_SIDE_FILENAME, "");
+		createHeaderRow(leftMainPanel, EAM.text("4. Analyze, Use and Adapt"), "", getMainDescriptionFileName(), getSummaryOverviewStepName());
+		createSubHeaderRow(leftMainPanel, EAM.text("4A. Prepare Data for Analysis"), EAM.text("Develop Systems for Recording, Storing, Processing and Backing Up Project Data"), PREPARE_DATA_FOR_ANALYSIS_RIGTH_SIDE_FILENAME, getSummaryOverviewStepName());
+		createSubHeaderRow(leftMainPanel, EAM.text("4B. Analyze Results"), EAM.text("Analyze Project Results and Assumptions Analyze Operational and Financial Data Document Discussions and Decisions"), ANALYZE_RESULTS_RIGTH_SIDE_FILENAME, getSummaryOverviewStepName());
+		createSubHeaderRow(leftMainPanel, EAM.text("4C. Adapt Project Plan"), EAM.text("Revise Project Plan: Strategic Monitoring Operational Work Plan"), ADAPT_PROJECT_PLAN_RIGTH_SIDE_FILENAME, getSummaryOverviewStepName());
 		
 		return leftMainPanel;
 	}
--- source/org/miradi/dialogs/dashboard/ConceptualizeDashboardTab.java
+++ source/org/miradi/dialogs/dashboard/ConceptualizeDashboardTab.java
@@ -26,7 +26,6 @@ import org.miradi.layout.TwoColumnPanel;
 import org.miradi.main.EAM;
 import org.miradi.objects.Dashboard;
 import org.miradi.project.Project;
-import org.miradi.views.summary.SummaryView;
 
 public class ConceptualizeDashboardTab extends AbstractDashboardTab
 {
@@ -45,7 +44,7 @@ public class ConceptualizeDashboardTab extends AbstractDashboardTab
 	protected TwoColumnPanel createLeftPanel()
 	{
 		TwoColumnPanel leftMainPanel = new TwoColumnPanel();
-		createHeaderRow(leftMainPanel, EAM.text("1. Conceptualize"), "", getMainDescriptionFileName(), SummaryView.getViewName());
+		createHeaderRow(leftMainPanel, EAM.text("1. Conceptualize"), "", getMainDescriptionFileName(), getSummaryOverviewStepName());
 
 		addTeamMembersRow(leftMainPanel);
 		addScopeVisionAndTargetsRow(leftMainPanel);
@@ -155,12 +154,12 @@ public class ConceptualizeDashboardTab extends AbstractDashboardTab
 	
 	private SelectableRow createDataRow(TwoColumnPanel leftMainPanel, String leftColumnTranslatedText, String rightColumnTranslatedText, String descriptionFileName)
 	{
-		return createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, descriptionFileName, SummaryView.getViewName());
+		return createDataRow(leftMainPanel, leftColumnTranslatedText, rightColumnTranslatedText, descriptionFileName, getSummaryOverviewStepName());
 	}
 	
 	private void createSubHeaderRow(TwoColumnPanel leftMainPanel, String leftColumnTranslatedText, String rightPanelHtmlFileName)
 	{
-		createSubHeaderRow(leftMainPanel, leftColumnTranslatedText, rightPanelHtmlFileName, SummaryView.getViewName());
+		createSubHeaderRow(leftMainPanel, leftColumnTranslatedText, rightPanelHtmlFileName, getSummaryOverviewStepName());
 	}
 	
 	@Override
