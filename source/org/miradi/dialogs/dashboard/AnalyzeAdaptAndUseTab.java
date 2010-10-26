869198e678030498cbf4c3cdd03eab1df038c6ab
Nima A. <nimaa@benetech.org>
2010-10-26 15:28:45 -0400

    Passing empty view name for now

--- source/org/miradi/dialogs/dashboard/AnalyzeAdaptAndUseTab.java
+++ source/org/miradi/dialogs/dashboard/AnalyzeAdaptAndUseTab.java
@@ -43,9 +43,9 @@ public class AnalyzeAdaptAndUseTab extends AbstractDashboardTab
 	{
 		TwoColumnPanel leftMainPanel = new TwoColumnPanel();
 		createHeaderRow(leftMainPanel, EAM.text("4. Analyze, Use and Adapt"), "", getMainDescriptionFileName(), SummaryView.getViewName());
-		createSubHeaderRow(leftMainPanel, EAM.text("4A. Prepare Data for Analysis"), EAM.text("Develop Systems for Recording, Storing, Processing and Backing Up Project Data"), PREPARE_DATA_FOR_ANALYSIS_RIGTH_SIDE_FILENAME);
-		createSubHeaderRow(leftMainPanel, EAM.text("4B. Analyze Results"), EAM.text("Analyze Project Results and Assumptions Analyze Operational and Financial Data Document Discussions and Decisions"), ANALYZE_RESULTS_RIGTH_SIDE_FILENAME);
-		createSubHeaderRow(leftMainPanel, EAM.text("4C. Adapt Project Plan"), EAM.text("Revise Project Plan: Strategic Monitoring Operational Work Plan"), ADAPT_PROJECT_PLAN_RIGTH_SIDE_FILENAME);
+		createSubHeaderRow(leftMainPanel, EAM.text("4A. Prepare Data for Analysis"), EAM.text("Develop Systems for Recording, Storing, Processing and Backing Up Project Data"), PREPARE_DATA_FOR_ANALYSIS_RIGTH_SIDE_FILENAME, "");
+		createSubHeaderRow(leftMainPanel, EAM.text("4B. Analyze Results"), EAM.text("Analyze Project Results and Assumptions Analyze Operational and Financial Data Document Discussions and Decisions"), ANALYZE_RESULTS_RIGTH_SIDE_FILENAME, "");
+		createSubHeaderRow(leftMainPanel, EAM.text("4C. Adapt Project Plan"), EAM.text("Revise Project Plan: Strategic Monitoring Operational Work Plan"), ADAPT_PROJECT_PLAN_RIGTH_SIDE_FILENAME, "");
 		
 		return leftMainPanel;
 	}
