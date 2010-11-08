30f7f2e8a9538a63b4a0097ada448b9b57d052aa
Nima A. <nimaa@benetech.org>
2010-11-08 11:00:42 -0500

    FIXME fixed. Pulled up duplicate methods.

--- source/org/miradi/dialogfields/AbstractEditableCodeListField.java
+++ source/org/miradi/dialogfields/AbstractEditableCodeListField.java
@@ -20,7 +20,19 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogfields;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComponent;
+
+import org.martus.swing.Utilities;
 import org.miradi.dialogs.base.DisposablePanel;
+import org.miradi.dialogs.base.MiradiPanel;
+import org.miradi.dialogs.base.ModalDialogWithClose;
+import org.miradi.dialogs.fieldComponents.PanelButton;
+import org.miradi.layout.OneColumnPanel;
+import org.miradi.main.EAM;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceQuestion;
@@ -30,7 +42,73 @@ abstract public class AbstractEditableCodeListField extends ObjectDataInputField
 	public AbstractEditableCodeListField(Project projectToUse, ORef refToUse, String tagToUse, ChoiceQuestion questionToUse, int columnCount)
 	{
 		super(projectToUse, refToUse, tagToUse);
-	}	
+		
+		question = questionToUse;
+		component = new MiradiPanel(new BorderLayout());
+		codeListComponent = new ReadOnlyCodeListComponent(questionToUse.getChoices(), columnCount);
+		component.add(codeListComponent, BorderLayout.CENTER);
+		
+		selectButton = new PanelButton(EAM.text("Select..."));
+
+		selectButton.addActionListener(new SelectButtonHandler());
+		OneColumnPanel buttonPanel = new OneColumnPanel();
+		buttonPanel.setBackground(EAM.READONLY_BACKGROUND_COLOR);
+		buttonPanel.setForeground(EAM.READONLY_FOREGROUND_COLOR);
+		buttonPanel.add(selectButton);
+		setDefaultFieldBorder();
+		component.add(buttonPanel, BorderLayout.AFTER_LINE_ENDS);
+	}
+	
+	@Override
+	public void updateEditableState()
+	{
+		super.updateEditableState();
+		
+		selectButton.setEnabled(isValidObject());			
+	}
+	
+	@Override
+	public JComponent getComponent()
+	{
+		return component;
+	}
+
+	@Override
+	public String getText()
+	{
+		return codeListComponent.getText();
+	}
+
+	@Override
+	public void setText(String newValue)
+	{
+		codeListComponent.setText(newValue);
+	}
+	
+	public class SelectButtonHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			try
+			{
+				DisposablePanel editorPanel = createEditorPanel();
+				ModalDialogWithClose dialog = new ModalDialogWithClose(EAM.getMainWindow(), EAM.text("Selection Dialog"));
+				dialog.setScrollableMainPanel(editorPanel);
+				Utilities.centerDlg(dialog);
+				dialog.setVisible(true);
+			}
+			catch (Exception e)
+			{
+				EAM.logException(e);
+				EAM.unexpectedErrorDialog(e);
+			}
+		}
+	}
 	
 	abstract protected DisposablePanel createEditorPanel() throws Exception;
+	
+	protected ChoiceQuestion question;
+	private MiradiPanel component;
+	private ReadOnlyCodeListComponent codeListComponent;
+	private PanelButton selectButton;
 }
--- source/org/miradi/dialogfields/CodeListPopupWithDescriptionPanelField.java
+++ source/org/miradi/dialogfields/CodeListPopupWithDescriptionPanelField.java
@@ -20,27 +20,14 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogfields;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JComponent;
-
-import org.martus.swing.Utilities;
 import org.miradi.dialogfields.editors.QuestionWithDescriptionEditorPanel;
 import org.miradi.dialogs.base.DisposablePanel;
-import org.miradi.dialogs.base.MiradiPanel;
-import org.miradi.dialogs.base.ModalDialogWithClose;
 import org.miradi.dialogs.base.OneFieldObjectDataInputPanel;
 import org.miradi.dialogs.dashboard.QuestionBasedLeftSideEditorComponent;
-import org.miradi.dialogs.fieldComponents.PanelButton;
-import org.miradi.layout.OneColumnPanel;
-import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.questions.ChoiceQuestion;
 
-//FIXME urgent - this class has been duplicated from EditableCodeListField
 public class CodeListPopupWithDescriptionPanelField extends	AbstractEditableCodeListField
 {
 	public CodeListPopupWithDescriptionPanelField(MainWindow mainWindowToUse, ORef refToUse, String tagToUse, ChoiceQuestion questionToUse)
@@ -48,46 +35,6 @@ public class CodeListPopupWithDescriptionPanelField extends	AbstractEditableCode
 		super(mainWindowToUse.getProject(), refToUse, tagToUse, questionToUse, 1);
 		
 		mainWindow = mainWindowToUse;
-		question = questionToUse;
-		component = new MiradiPanel(new BorderLayout());
-		codeListComponent = new ReadOnlyCodeListComponent(questionToUse.getChoices(), 1);
-		component.add(codeListComponent, BorderLayout.CENTER);
-		
-		selectButton = new PanelButton(EAM.text("Select..."));
-
-		selectButton.addActionListener(new SelectButtonHandler());
-		OneColumnPanel buttonPanel = new OneColumnPanel();
-		buttonPanel.setBackground(EAM.READONLY_BACKGROUND_COLOR);
-		buttonPanel.setForeground(EAM.READONLY_FOREGROUND_COLOR);
-		buttonPanel.add(selectButton);
-		setDefaultFieldBorder();
-		component.add(buttonPanel, BorderLayout.AFTER_LINE_ENDS);
-	}
-	
-	@Override
-	public void updateEditableState()
-	{
-		super.updateEditableState();
-		
-		selectButton.setEnabled(isValidObject());			
-	}
-	
-	@Override
-	public JComponent getComponent()
-	{
-		return component;
-	}
-
-	@Override
-	public String getText()
-	{
-		return codeListComponent.getText();
-	}
-
-	@Override
-	public void setText(String newValue)
-	{
-		codeListComponent.setText(newValue);
 	}
 	
 	protected DisposablePanel createEditorPanel() throws Exception
@@ -99,29 +46,5 @@ public class CodeListPopupWithDescriptionPanelField extends	AbstractEditableCode
 		return new QuestionWithDescriptionEditorPanel(mainWindow, question, leftPanel);
 	}
 	
-	public class SelectButtonHandler implements ActionListener
-	{
-		public void actionPerformed(ActionEvent event)
-		{
-			try
-			{
-				DisposablePanel editorPanel = createEditorPanel();
-				ModalDialogWithClose dialog = new ModalDialogWithClose(EAM.getMainWindow(), EAM.text("Selection Dialog"));
-				dialog.setScrollableMainPanel(editorPanel);
-				Utilities.centerDlg(dialog);
-				dialog.setVisible(true);
-			}
-			catch (Exception e)
-			{
-				EAM.logException(e);
-				EAM.unexpectedErrorDialog(e);
-			}
-		}
-	}
-
 	private MainWindow mainWindow;
-	private ChoiceQuestion question;
-	private MiradiPanel component;
-	private ReadOnlyCodeListComponent codeListComponent;
-	private PanelButton selectButton;
 }
