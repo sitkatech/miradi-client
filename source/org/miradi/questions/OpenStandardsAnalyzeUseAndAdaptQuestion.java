5df29038c95a8cc98a84a2402142c0e3dd41eb80
Nima A. <nimaa@benetech.org>
2010-11-24 15:00:26 -0500

    Added actions for step by steps parent menu items that didnt not have actions.

--- source/org/miradi/questions/OpenStandardsAnalyzeUseAndAdaptQuestion.java
+++ source/org/miradi/questions/OpenStandardsAnalyzeUseAndAdaptQuestion.java
@@ -29,23 +29,48 @@ public class OpenStandardsAnalyzeUseAndAdaptQuestion extends DynamicChoiceWithRo
 	protected ChoiceItemWithChildren createHeaderChoiceItem() throws Exception
 	{
 		StaticLongDescriptionProvider provider = new StaticLongDescriptionProvider();
-		ChoiceItemWithChildren headerChoiceItem = new ChoiceItemWithChildren("AnalyzeUseAndAdapt", EAM.text("Menu|4. Analyze, Use, Adapt"), provider);
+		ChoiceItemWithChildren headerChoiceItem = new ChoiceItemWithChildren(ANALYZE_USE_AND_ADAPT_HEADER_CODE, getHeaderLabel(), provider);
 		
-		ChoiceItemWithChildren processStep4a = new ChoiceItemWithChildren("ProcessStep4", EAM.text("ProcessStep|4A. Prepare Data for Analysis"), EAM.text(""), provider);
+		
+		ChoiceItemWithChildren processStep4a = new ChoiceItemWithChildren(PROCESS_STEP_4A_CODE, getProcessStep4a(), EAM.text(""), provider);
 		headerChoiceItem.addChild(processStep4a);
 		processStep4a.addChild(new ChoiceItem("DevelopSystemsForRecordingStoringProcessingAndBackingUpProjectData", EAM.text("Develop systems for recording, storing, processing and backing up project data")));
 		
-		ChoiceItemWithChildren processStep4bChoiceItem = new ChoiceItemWithChildren("ProcessStep4B", EAM.text("ProcessStep|4B. Analyze Results"), EAM.text(""), provider);
+		ChoiceItemWithChildren processStep4bChoiceItem = new ChoiceItemWithChildren(PROCESS_STEP_4B_CODE, getProcessStep4b(), EAM.text(""), provider);
 		headerChoiceItem.addChild(processStep4bChoiceItem);
 		processStep4bChoiceItem.addChild(new ChoiceItem("AnalyzeProjectResultsSndSssumptions", (EAM.text("Analyze project results and assumptions"))));
 		processStep4bChoiceItem.addChild(new ChoiceItem("AnalyzeOperationalAndFinancialData", (EAM.text("Analyze operational and financial data"))));
 		processStep4bChoiceItem.addChild(new ChoiceItem("DocumentDiscussionsAndDecisions", (EAM.text("Document discussions and decisions"))));
 
-		
-		ChoiceItemWithChildren processStep4cChoiceItem = new ChoiceItemWithChildren("ProcessStep4C", EAM.text("ProcessStep|4C. Adapt Project Plan"), EAM.text(""), provider);
+		ChoiceItemWithChildren processStep4cChoiceItem = new ChoiceItemWithChildren(PROCESS_STEP_4C_CODE, getProcessStep4c(), EAM.text(""), provider);
 		headerChoiceItem.addChild(processStep4cChoiceItem);
 		processStep4cChoiceItem.addChild(new ChoiceItem("ReviseProjectPlanStrategicMonitoringOperationalAndWorkPlans", EAM.text("Revise project plan: strategic, monitoring, operational, and work plans")));
 		
 		return headerChoiceItem;
 	}
+
+	public static String getHeaderLabel()
+	{
+		return EAM.text("Menu|4. Analyze, Use, Adapt");
+	}
+	
+	public static String getProcessStep4a()
+	{
+		return EAM.text("ProcessStep|4A. Prepare Data for Analysis");
+	}
+	
+	public static String getProcessStep4b()
+	{
+		return EAM.text("ProcessStep|4B. Analyze Results");
+	}
+	
+	public static String getProcessStep4c()
+	{
+		return EAM.text("ProcessStep|4C. Adapt Project Plan");
+	}
+
+	public static final String ANALYZE_USE_AND_ADAPT_HEADER_CODE = "AnalyzeUseAndAdapt";
+	public static final String PROCESS_STEP_4A_CODE = "ProcessStep4";
+	public static final String PROCESS_STEP_4B_CODE = "ProcessStep4B";
+	public static final String PROCESS_STEP_4C_CODE = "ProcessStep4C";
 }
