7f750fe54611696450337db6d9b8842cc9ff4dff
Nima A. <nimaa@benetech.org>
2010-11-29 15:37:04 -0500

    Added second level rows to dashboard panel

--- source/org/miradi/dialogs/base/AbstractOpenStandardsQuestionPanel.java
+++ source/org/miradi/dialogs/base/AbstractOpenStandardsQuestionPanel.java
@@ -20,6 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.base;
 
+import java.awt.Font;
+import java.util.Vector;
+
+import javax.swing.Box;
+import javax.swing.JComponent;
+
 import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
 import org.miradi.layout.TwoColumnGridLayout;
 import org.miradi.objecthelpers.ORef;
@@ -37,9 +43,79 @@ abstract public class AbstractOpenStandardsQuestionPanel extends AbstractObjectD
 		setLayout(new TwoColumnGridLayout());
 		question = questionToUse;
 		
-		ChoiceItem headerChoiceItem = question.getHeaderChoiceItem();
-		add(new PanelTitleLabel(headerChoiceItem.getLabel()));
+		addFirstLevelRow(question.getHeaderChoiceItem());
+		addSecondLevelRows(question.getHeaderChoiceItem().getChildren());
+	}
+
+	private void addFirstLevelRow(ChoiceItem choiceItem) throws Exception
+	{
+		final int HEADER_INDENT_COUNT = 0;
+		
+		addRow(choiceItem, HEADER_INDENT_COUNT, createFirstLevelFont());
+	}
+
+	private void addSecondLevelRows(Vector<ChoiceItem> children) throws Exception
+	{
+		for(ChoiceItem firstLevelChild : children)
+		{
+			addThirdLevelRows(firstLevelChild);
+		}
+	}
+	
+	private void addThirdLevelRows(ChoiceItem choiceItem) throws Exception
+	{
+		final int SUBHEADER_INDENT_COUNT = 1;
+		
+		addRow(choiceItem, SUBHEADER_INDENT_COUNT, createSecondLevelFont());
+	}
+
+	private void addRow(ChoiceItem choiceItem, final int indentCount, Font font)
+	{
+		JComponent leftComponent = new PanelTitleLabel(choiceItem.getLabel());
+		leftComponent.setFont(font);
+		
+		PanelTitleLabel rightComponent = new PanelTitleLabel(choiceItem.getAdditionalText());
+		rightComponent.setFont(font);
+		
+		Box box = createHorizontalBoxWithIndents(indentCount);
+		box.add(leftComponent);
+		add(box);
+		add(rightComponent);
+	}
+	
+	private Box createHorizontalBoxWithIndents(int indentCount)
+	{
+		Box box = Box.createHorizontalBox();
+		for (int index = 0; index < indentCount; ++index)
+		{
+			box.add(Box.createHorizontalStrut(INDENT_PER_LEVEL));
+		}
+		
+		return box;
+	}
+	
+	private Font createFirstLevelFont()
+	{
+		Font font = getRawFont();
+		font = font.deriveFont(Font.BOLD);
+		font = font.deriveFont((float)(font.getSize() * 1.5));
+		
+		return font;
+	}
+	
+	private Font createSecondLevelFont()
+	{
+		Font font = getRawFont();
+		font = font.deriveFont(Font.BOLD);
+		
+		return font;
+	}
+	
+	private Font getRawFont()
+	{
+		return new PanelTitleLabel().getFont();
 	}
 
 	private DynamicChoiceWithRootChoiceItem question;
+	protected static final int INDENT_PER_LEVEL = 25;
 }
