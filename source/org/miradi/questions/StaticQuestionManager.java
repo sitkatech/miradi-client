0f57d271af87c72fd89d97be6cc439ba0a80858f
Nima A. <nimaa@benetech.org>
2010-12-08 11:11:05 -0500

    MRD-4412: first commit of many to come.  Added static icon to each row.

--- source/org/miradi/questions/StaticQuestionManager.java
+++ source/org/miradi/questions/StaticQuestionManager.java
@@ -95,6 +95,7 @@ public class StaticQuestionManager
 		addQuestion(new DiagramObjectDataInclusionQuestion());
 		addQuestion(new PlanningTreeTargetPositionQuestion());
 		addQuestion(new WorkPlanCategoryTypesQuestion());
+		addQuestion(new OpenStandardsProgessQuestion());
 	}
 	
 	private static void addQuestion(ChoiceQuestion question)
