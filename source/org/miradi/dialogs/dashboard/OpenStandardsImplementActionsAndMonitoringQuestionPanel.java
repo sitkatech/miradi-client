97ec3528e057f5568aaa372de6b329eeab6bbe15
nimaa <nimaa@localhost>
2010-12-10 14:26:25 -0500

    Converted row creation into row definition and using duplicated code to create the rows.

--- source/org/miradi/dialogs/dashboard/OpenStandardsImplementActionsAndMonitoringQuestionPanel.java
+++ source/org/miradi/dialogs/dashboard/OpenStandardsImplementActionsAndMonitoringQuestionPanel.java
@@ -20,10 +20,11 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.dashboard;
 
+import java.util.HashMap;
+import java.util.Vector;
+
 import org.miradi.actions.AbstractJumpMenuAction;
 import org.miradi.dialogs.base.AbstractOpenStandardsQuestionPanel;
-import org.miradi.main.EAM;
-import org.miradi.objects.Dashboard;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.OpenStandardsImplementActionsAndMonitoringQuestion;
@@ -37,78 +38,36 @@ public class OpenStandardsImplementActionsAndMonitoringQuestionPanel extends Abs
 	
 	@Override
 	protected void addRow(ChoiceItem choiceItem, int level) throws Exception
-	{
+	{		
 		super.addRow(choiceItem, level);
 		
-		String code = choiceItem.getCode();
-		AbstractLongDescriptionProvider longDescriptionProvider = choiceItem.getLongDescriptionProvider();
-		AbstractJumpMenuAction action = getMainWindow().getActions().getJumpMenuAction(code);
-		String stepName = getMainWindow().getWizardManager().stripJumpPrefix(action.getClass());
-		longDescriptionProvider.setWizardStepName(stepName);
+		Vector<DashboardRowDefinition> rowDefinitions = getDashboardRowDefinitionManager().getRowDefinitions(choiceItem.getCode());
 		
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.DETAIL_ACTIVITIES_TASKS_AND_RESPONSIBILITIES_CODE))
-			addDetailActivitiesTasksAndResponsibilitiesRow(longDescriptionProvider, level);
-	
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.DETAIL_METHODS_TASKS_AND_RESPONSIBILITIES_CODE))
-			addDetailMethodsTasksAndResponsibilitiesRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.DEVELOP_PROJECT_TIMELINE_OR_CALENDAR_CODE))
-			addDevelopProjectTimelineOrCalendarRow(longDescriptionProvider, level);
-		
-		
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.ESTIMATE_COSTS_FOR_ACTIVITIES_AND_MONITORING_CODE))
-			addEstimateCostsForActivitiesAndMonitoringrow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.DEVELOP_AND_SUBMIT_FUNDING_PROPOSALS_CODE))
-			addDevelopAndSubmitFundingProposalsrow(longDescriptionProvider, level);
-		
-		
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.IMPLEMENT_STRATEGIC_AND_MONITORING_PLANS))
-			addImplementStrategicAndMonitoringPlansrow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsImplementActionsAndMonitoringQuestion.IMPLEMENT_WORK_PLAN_CODE))
-			addImplementWorkPlanrow(longDescriptionProvider, level);
-	}
-	
-	private void addDetailActivitiesTasksAndResponsibilitiesRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X of %Y Strategies have at least 1 Activity."), longDescriptionProvider, indentCount, Dashboard.PSEUDO_STRATEGIES_WITH_ACTIVITIES_COUNT, Dashboard.PSEUDO_STRATEGY_COUNT);
-		addRowWithTemplateAndPseudoField(EAM.text("%X Total activities created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_ACTIVITIES_COUNT);
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%Y of %X Activities and tasks have assignments"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_ACTIVITIES_AND_TASKS_COUNT, Dashboard.PSEUDO_ACTIVITIES_AND_TASKS_WITH_ASSIGNMENTS_COUNT);
-	}
+		for (DashboardRowDefinition rowDefinition: rowDefinitions)
+		{
+			Vector<String> pseudoTags = rowDefinition.getPseudoTags();
+			HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+			//FIXME urgent dashboard panel - when all other tabs are converted using the RowDefinitionManager, convert these ifs into
+			// a loop using %1, %2... (looping through pseudoTags
+			if (pseudoTags.size() == 1)
+			{
+				tokenReplacementMap.put("%X", getDashboardData(pseudoTags.get(0)));
+			}
+			
+			if (pseudoTags.size() == 2)
+			{
+				tokenReplacementMap.put("%X", getDashboardData(pseudoTags.get(0)));
+				tokenReplacementMap.put("%Y", getDashboardData(pseudoTags.get(1)));
+			}
 
-	private void addDetailMethodsTasksAndResponsibilitiesRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X of %Y Indicators have Methods."), longDescriptionProvider, indentCount, Dashboard.PSEUDO_INDICATORS_WITH_METHODS_COUNT, Dashboard.PSEUDO_INDICATORS_COUNT);
-		addRowWithTemplateAndPseudoField(EAM.text("%X Total methods created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_METHODS_COUNT);
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X of %Y Methods and Tasks have assignments"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_METHODS_AND_TASKS_WITH_ASSIGNMENT_COUNT, Dashboard.PSEUDO_METHODS_AND_TASKS_COUNT);
-	}
+			String code = choiceItem.getCode();
+			AbstractLongDescriptionProvider longDescriptionProvider = choiceItem.getLongDescriptionProvider();
+			AbstractJumpMenuAction action = getMainWindow().getActions().getJumpMenuAction(code);
+			String stepName = getMainWindow().getWizardManager().stripJumpPrefix(action.getClass());
+			longDescriptionProvider.setWizardStepName(stepName);
 
-	private void addDevelopProjectTimelineOrCalendarRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X - %Y"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_WORK_PLAN_START_DATE, Dashboard.PSEUDO_WORK_PLAN_END_DATE);
-	}
-
-	private void addEstimateCostsForActivitiesAndMonitoringrow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("Work Costs: %X %Y"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_CURRENCY_SYMBOL, Dashboard.PSEUDO_TOTAL_PROJECT_RESOURCES_COSTS);
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("Expenses: %X %Y"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_CURRENCY_SYMBOL, Dashboard.PSEUDO_TOTAL_PROJECT_EXPENSES);
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("Project Budget: %X %Y"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_CURRENCY_SYMBOL, Dashboard.PSEUDO_PROJECT_BUDGET);
-	}
-
-	private void addDevelopAndSubmitFundingProposalsrow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("Total Budget for Funding: %  %X Budget Secured"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_BUDGET_SECURED_PERCENT);
-	}
-
-	private void addImplementStrategicAndMonitoringPlansrow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X Strategies/activities (% %Y) have progress reports"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_STRATEGIES_AND_ACTIVITIES_WITH_PROGRESS_REPORT_COUNT, Dashboard.PSEUDO_STRATEGIES_AND_ACTIVITIES_WITH_PROGRESS_REPORT_PERCENT);
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X Indicators/methods (% %Y) have progress reports"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_INDICATORS_AND_METHODS_WITH_PROGRESS_REPORT_COUNT, Dashboard.PSEUDO_INDICATORS_AND_METHODS_WITH_PROGRESS_REPORT_PERCENT);
-	}
-
-	private void addImplementWorkPlanrow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount)
-	{
+			addRow(EMPTY_LEFT_COLUMN_TEXT, rowDefinition.getRightColumnTemplate(), tokenReplacementMap, longDescriptionProvider, choiceItem.getCode(), level);
+		}
 	}
 	
 	@Override
