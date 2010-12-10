4a915eadaeee30e63e1f3ad007ac3e1e8c3e43e6
nimaa <nimaa@localhost>
2010-12-10 14:35:29 -0500

    Pulled up duplicate methods.

--- source/org/miradi/dialogs/base/OpenStandardsPlanActionsAndMonitoringQuestionPanel.java
+++ source/org/miradi/dialogs/base/OpenStandardsPlanActionsAndMonitoringQuestionPanel.java
@@ -20,14 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.base;
 
-import java.util.HashMap;
-import java.util.Vector;
-
-import org.miradi.actions.AbstractJumpMenuAction;
-import org.miradi.dialogs.dashboard.AbstractLongDescriptionProvider;
-import org.miradi.dialogs.dashboard.DashboardRowDefinition;
 import org.miradi.project.Project;
-import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.OpenStandardsPlanActionsAndMonitoringQuestion;
 
 public class OpenStandardsPlanActionsAndMonitoringQuestionPanel extends AbstractOpenStandardsQuestionPanel
@@ -37,40 +30,6 @@ public class OpenStandardsPlanActionsAndMonitoringQuestionPanel extends Abstract
 		super(projectToUse, new OpenStandardsPlanActionsAndMonitoringQuestion());
 	}
 	
-	@Override
-	protected void addRow(ChoiceItem choiceItem, int level) throws Exception
-	{		
-		super.addRow(choiceItem, level);
-		
-		Vector<DashboardRowDefinition> rowDefinitions = getDashboardRowDefinitionManager().getRowDefinitions(choiceItem.getCode());
-		
-		for (DashboardRowDefinition rowDefinition: rowDefinitions)
-		{
-			Vector<String> pseudoTags = rowDefinition.getPseudoTags();
-			HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
-			//FIXME urgent dashboard panel - when all other tabs are converted using the RowDefinitionManager, convert these ifs into
-			// a loop using %1, %2... (looping through pseudoTags
-			if (pseudoTags.size() == 1)
-			{
-				tokenReplacementMap.put("%X", getDashboardData(pseudoTags.get(0)));
-			}
-			
-			if (pseudoTags.size() == 2)
-			{
-				tokenReplacementMap.put("%X", getDashboardData(pseudoTags.get(0)));
-				tokenReplacementMap.put("%Y", getDashboardData(pseudoTags.get(1)));
-			}
-
-			String code = choiceItem.getCode();
-			AbstractLongDescriptionProvider longDescriptionProvider = choiceItem.getLongDescriptionProvider();
-			AbstractJumpMenuAction action = getMainWindow().getActions().getJumpMenuAction(code);
-			String stepName = getMainWindow().getWizardManager().stripJumpPrefix(action.getClass());
-			longDescriptionProvider.setWizardStepName(stepName);
-
-			addRow(EMPTY_LEFT_COLUMN_TEXT, rowDefinition.getRightColumnTemplate(), tokenReplacementMap, longDescriptionProvider, choiceItem.getCode(), level);
-		}
-	}
-
 	@Override
 	public String getPanelDescription()
 	{
