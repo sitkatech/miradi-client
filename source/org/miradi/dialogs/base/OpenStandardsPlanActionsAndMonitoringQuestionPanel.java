89a47b0d613e44814a6ea5bda38c4bbd3fe881a4
nimaa <nimaa@localhost>
2010-12-10 14:16:28 -0500

    Converted row creation into row definition and using duplicated code to create the rows.

--- source/org/miradi/dialogs/base/OpenStandardsPlanActionsAndMonitoringQuestionPanel.java
+++ source/org/miradi/dialogs/base/OpenStandardsPlanActionsAndMonitoringQuestionPanel.java
@@ -20,10 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.base;
 
+import java.util.HashMap;
+import java.util.Vector;
+
 import org.miradi.actions.AbstractJumpMenuAction;
 import org.miradi.dialogs.dashboard.AbstractLongDescriptionProvider;
-import org.miradi.main.EAM;
-import org.miradi.objects.Dashboard;
+import org.miradi.dialogs.dashboard.DashboardRowDefinition;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.OpenStandardsPlanActionsAndMonitoringQuestion;
@@ -37,114 +39,36 @@ public class OpenStandardsPlanActionsAndMonitoringQuestionPanel extends Abstract
 	
 	@Override
 	protected void addRow(ChoiceItem choiceItem, int level) throws Exception
-	{
+	{		
 		super.addRow(choiceItem, level);
 		
-		String code = choiceItem.getCode();
-		AbstractLongDescriptionProvider longDescriptionProvider = choiceItem.getLongDescriptionProvider();
-		AbstractJumpMenuAction action = getMainWindow().getActions().getJumpMenuAction(code);
-		String stepName = getMainWindow().getWizardManager().stripJumpPrefix(action.getClass());
-		longDescriptionProvider.setWizardStepName(stepName);
+		Vector<DashboardRowDefinition> rowDefinitions = getDashboardRowDefinitionManager().getRowDefinitions(choiceItem.getCode());
 		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEVELOP_GOALS_FOR_EACH_TARGET_CODE))
-			addDevelopGoalsForEachTargetRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.IDENTIFY_KEY_FACTORS_AND_DRAFT_STRATEGIES_CODE))
-			addIdentifyKeyFactorsAndDraftStrategiesRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.RANK_DRAFT_STRATEGIES_CODE))
-			addRankDraftStrategiesrow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.CREATE_RESULTS_CHAINS_SHOWING_ASSUMPTIONS_CODE))
-			addCreateResultsChainsShowingAssumptionsRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEVELOP_OBJECTIVES_CODE))
-			addDevelopObjectivesRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.FINALIZE_STRATEGIC_PLAN_CODE))
-			addFinalizeStrategicPlanRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.FINALIZE_MONITORING_PLAN_CODE))
-			addFinalizeMonitoringPlanRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEFINE_AUDIENCES_AND_INFORMATION_NEEDS_CODE))
-			addDefineAudiencesAndInformationNeedsRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEFINE_INDICATORS_CODE))
-			addDefineIndicatorsRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.ASSESS_HUMAN_FINANCIAL_AND_OTHER_RESOURCES_CODE))
-			addAssessHumanFinacialAndOtherResourcesRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.ASSESS_RISKS_CODE))
-			addAssessRisksRow(longDescriptionProvider, level);
-		
-		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.PLAN_PROJECT_LIFESPAN_AND_EXIT_STRATEGY_CODE))
-			addPlanProjectLifespanAndExitStrategyRow(longDescriptionProvider, level);
-	}
+		for (DashboardRowDefinition rowDefinition: rowDefinitions)
+		{
+			Vector<String> pseudoTags = rowDefinition.getPseudoTags();
+			HashMap<String, String> tokenReplacementMap = new HashMap<String, String>();
+			//FIXME urgent dashboard panel - when all other tabs are converted using the RowDefinitionManager, convert these ifs into
+			// a loop using %1, %2... (looping through pseudoTags
+			if (pseudoTags.size() == 1)
+			{
+				tokenReplacementMap.put("%X", getDashboardData(pseudoTags.get(0)));
+			}
+			
+			if (pseudoTags.size() == 2)
+			{
+				tokenReplacementMap.put("%X", getDashboardData(pseudoTags.get(0)));
+				tokenReplacementMap.put("%Y", getDashboardData(pseudoTags.get(1)));
+			}
 
-	private void addDevelopGoalsForEachTargetRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%X of %Y Targets have Goals"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_TARGETS_WITH_GOALS_COUNT, Dashboard.PSEUDO_TARGET_COUNT);
-		addRowWithTemplateAndPseudoField(EAM.text("%X goals Created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_GOAL_COUNT);
-	}
+			String code = choiceItem.getCode();
+			AbstractLongDescriptionProvider longDescriptionProvider = choiceItem.getLongDescriptionProvider();
+			AbstractJumpMenuAction action = getMainWindow().getActions().getJumpMenuAction(code);
+			String stepName = getMainWindow().getWizardManager().stripJumpPrefix(action.getClass());
+			longDescriptionProvider.setWizardStepName(stepName);
 
-	private void addIdentifyKeyFactorsAndDraftStrategiesRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X Draft Strategies Created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_DRAFT_STRATEGY_COUNT);
-	}
-
-	private void addRankDraftStrategiesrow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X Draft Strategies Ranked"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_RANKED_DRAFT_STRATEGY_COUNT);
-	}
-
-	private void addCreateResultsChainsShowingAssumptionsRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X Strategies Created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_STRATEGY_COUNT);
-		addRowWithTemplateAndPseudoField(EAM.text("%X with taxonomy assignments"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_STRATEGY__WITH_TAXONOMY_COUNT);
-		addRowWithTemplateAndPseudoField(EAM.text("%X Results Chains Created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_RESULTS_CHAIN_COUNT);
-	}
-
-	private void addDevelopObjectivesRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X Objectives Created"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_OBJECTIVE_COUNT);
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%Y of %X RCs have at least 1 objective"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_STRATEGY__WITH_TAXONOMY_COUNT, Dashboard.PSEUDO_RESULTS_CHAIN_WITH_OBJECTIVE_COUNT);
-	}
-
-	private void addFinalizeStrategicPlanRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X % of Objectives relevant to a Strategy"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_OBJECTIVES_RELEVANT_TO_STRATEGIES_PERCENTAGE);		
-		addRowWithTemplateAndPseudoField(EAM.text("%X Strategies that do not contribute to an Objective"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_IRRELEVANT_STRATEGIES_TO_OBJECTIVES_COUNT);
-	}
-
-	private void addFinalizeMonitoringPlanRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X % of objectives with relevant indicators"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_OBJECTIVES_RELEVANT_TO_INDICATORS_PERCENTAGE);
-	}
-
-	private void addDefineAudiencesAndInformationNeedsRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithNoPseudoTag(EAM.text("Use Comments Field?"), longDescriptionProvider, indentCount);
-	}
-
-	private void addDefineIndicatorsRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndPseudoField(EAM.text("%X Indicators associated to KEA's"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_KEA_INDICATORS_COUNT);
-		addRowWithTemplateAndPseudoField(EAM.text("%X indicators associate to Factors"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_FACTOR_INDICATORS_COUNT);
-	}
-
-	private void addAssessHumanFinacialAndOtherResourcesRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount)
-	{
-	}
-
-	private void addAssessRisksRow(AbstractLongDescriptionProvider longDescriptionProvider,	int indentCount)
-	{
-	}
-
-	private void addPlanProjectLifespanAndExitStrategyRow(AbstractLongDescriptionProvider longDescriptionProvider, int indentCount) throws Exception
-	{
-		addRowWithTemplateAndTwoPseudoFields(EAM.text("%Y - %X"), longDescriptionProvider, indentCount, Dashboard.PSEUDO_PROJECT_PLANNING_START_DATE, Dashboard.PSEUDO_PROJECT_PLANNING_END_DATE);
+			addRow(EMPTY_LEFT_COLUMN_TEXT, rowDefinition.getRightColumnTemplate(), tokenReplacementMap, longDescriptionProvider, choiceItem.getCode(), level);
+		}
 	}
 
 	@Override
--- source/org/miradi/dialogs/dashboard/DashboardRowDefinitionManager.java
+++ source/org/miradi/dialogs/dashboard/DashboardRowDefinitionManager.java
@@ -24,20 +24,103 @@ import java.util.Vector;
 
 import org.miradi.main.EAM;
 import org.miradi.objects.Dashboard;
-import org.miradi.questions.ChoiceQuestion;
 import org.miradi.questions.OpenStandardsConceptualizeQuestion;
+import org.miradi.questions.OpenStandardsPlanActionsAndMonitoringQuestion;
 import org.miradi.utils.CodeList;
 
 public class DashboardRowDefinitionManager
 {
 	public CodeList getThirdLevelCodes()
 	{
-		ChoiceQuestion question = new OpenStandardsConceptualizeQuestion();
-		return question.getAllCodes();
+		CodeList allCodes = new CodeList();
+		allCodes.addAll(new OpenStandardsConceptualizeQuestion().getAllCodes());
+		allCodes.addAll(new OpenStandardsPlanActionsAndMonitoringQuestion().getAllCodes());
+		
+		return allCodes;
 	}
 	
 	public Vector<DashboardRowDefinition> getRowDefinitions(String code) throws Exception
 	{	
+		Vector<DashboardRowDefinition> rowDefinitions = new Vector<DashboardRowDefinition>();
+		rowDefinitions.addAll(addConceptualizeRowDefinitions(code));
+		rowDefinitions.addAll(addPlanActionsAndMonitoringDefinitions(code));
+		
+		return rowDefinitions;
+	}
+
+	private Vector<DashboardRowDefinition> addPlanActionsAndMonitoringDefinitions(String code)
+	{
+		Vector<DashboardRowDefinition> rowDefinitions = new Vector<DashboardRowDefinition>();
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEVELOP_GOALS_FOR_EACH_TARGET_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X of %Y Targets have Goals"), Dashboard.PSEUDO_TARGETS_WITH_GOALS_COUNT, Dashboard.PSEUDO_TARGET_COUNT));
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X goals Created"), Dashboard.PSEUDO_GOAL_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.IDENTIFY_KEY_FACTORS_AND_DRAFT_STRATEGIES_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Draft Strategies Created"), Dashboard.PSEUDO_DRAFT_STRATEGY_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.RANK_DRAFT_STRATEGIES_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Draft Strategies Ranked"), Dashboard.PSEUDO_RANKED_DRAFT_STRATEGY_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.CREATE_RESULTS_CHAINS_SHOWING_ASSUMPTIONS_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Strategies Created"), Dashboard.PSEUDO_STRATEGY_COUNT));
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X with taxonomy assignments"), Dashboard.PSEUDO_STRATEGY__WITH_TAXONOMY_COUNT));
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Results Chains Created"), Dashboard.PSEUDO_RESULTS_CHAIN_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEVELOP_OBJECTIVES_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Objectives Created"), Dashboard.PSEUDO_OBJECTIVE_COUNT));
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%Y of %X RCs have at least 1 objective"), Dashboard.PSEUDO_STRATEGY__WITH_TAXONOMY_COUNT, Dashboard.PSEUDO_RESULTS_CHAIN_WITH_OBJECTIVE_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.FINALIZE_STRATEGIC_PLAN_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X % of Objectives relevant to a Strategy"), Dashboard.PSEUDO_OBJECTIVES_RELEVANT_TO_STRATEGIES_PERCENTAGE));		
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Strategies that do not contribute to an Objective"), Dashboard.PSEUDO_IRRELEVANT_STRATEGIES_TO_OBJECTIVES_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.FINALIZE_MONITORING_PLAN_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X % of objectives with relevant indicators"), Dashboard.PSEUDO_OBJECTIVES_RELEVANT_TO_INDICATORS_PERCENTAGE));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEFINE_AUDIENCES_AND_INFORMATION_NEEDS_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("Use Comments Field?")));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.DEFINE_INDICATORS_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X Indicators associated to KEA's"), Dashboard.PSEUDO_KEA_INDICATORS_COUNT));
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%X indicators associate to Factors"), Dashboard.PSEUDO_FACTOR_INDICATORS_COUNT));
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.ASSESS_HUMAN_FINANCIAL_AND_OTHER_RESOURCES_CODE))
+		{
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.ASSESS_RISKS_CODE))
+		{
+		}
+		
+		if (code.equals(OpenStandardsPlanActionsAndMonitoringQuestion.PLAN_PROJECT_LIFESPAN_AND_EXIT_STRATEGY_CODE))
+		{
+			rowDefinitions.add(new DashboardRowDefinition(EAM.text("%Y - %X"), Dashboard.PSEUDO_PROJECT_PLANNING_START_DATE, Dashboard.PSEUDO_PROJECT_PLANNING_END_DATE));
+		}
+		
+		return rowDefinitions;
+	}
+
+	private Vector<DashboardRowDefinition> addConceptualizeRowDefinitions(String code)
+	{
 		Vector<DashboardRowDefinition> rowDefinitions = new Vector<DashboardRowDefinition>();
 		if (code.equals(OpenStandardsConceptualizeQuestion.SELECT_INTIAL_TEAM_MEMBERS_CODE))
 		{
