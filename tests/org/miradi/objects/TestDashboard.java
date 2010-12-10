8bd270e284bc4c4590f92f299688d3cc346cf0e1
nimaa <nimaa@localhost>
2010-12-10 13:52:30 -0500

    Added test for method that returns effective status codes

--- tests/org/miradi/objects/TestDashboard.java
+++ tests/org/miradi/objects/TestDashboard.java
@@ -20,7 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.objects;
 
+import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ObjectType;
+import org.miradi.objecthelpers.StringChoiceMap;
+import org.miradi.questions.OpenStandardsConceptualizeQuestion;
+import org.miradi.questions.OpenStandardsProgressQuestion;
+import org.miradi.utils.CodeList;
 
 public class TestDashboard extends ObjectTestCase
 {
@@ -32,5 +37,50 @@ public class TestDashboard extends ObjectTestCase
 	public void testFields() throws Exception
 	{
 		verifyFields(ObjectType.DASHBOARD);
+	}
+	
+	public void testGetEffectiveStatusMapWithNoData() throws Exception
+	{
+		 assertEquals("empty dashboard should not have status?",createdEmptyStringChoiceMap() , getEffectiveStatusMap());
+	}
+
+	public void testGetEffectiveStatusMapWithData() throws Exception
+	{
+		verifyProgressCode(OpenStandardsProgressQuestion.NOT_SPECIFIED_CODE);
+		
+		getProject().createProjectResource();
+		verifyProgressCode(OpenStandardsProgressQuestion.IN_PROGRESS_CODE);
+	}
+
+	private void verifyProgressCode(String expectedCode) throws Exception
+	{
+		StringChoiceMap mapAsString = getEffectiveStatusMap();
+		StringChoiceMap map = new StringChoiceMap(mapAsString);
+		String targetStatusCount = map.get(OpenStandardsConceptualizeQuestion.SELECT_INTIAL_TEAM_MEMBERS_CODE);
+
+		assertEquals("Incorrect status count?", expectedCode, targetStatusCount);
+	}
+	
+	private StringChoiceMap createdEmptyStringChoiceMap()
+	{
+		CodeList allThirdLEvelCodes = getDashboard().getDashboardRowDefinitionManager().getThirdLevelCodes();
+		StringChoiceMap emptyMap = new StringChoiceMap();
+		for (int index = 0; index < allThirdLEvelCodes.size(); ++index)
+		{
+			emptyMap.add(allThirdLEvelCodes.get(index), OpenStandardsProgressQuestion.NOT_SPECIFIED_CODE);
+		}
+		
+		return emptyMap;
+	}
+
+	private StringChoiceMap getEffectiveStatusMap() throws Exception
+	{
+		return getDashboard().getEffectiveStatusMap();
+	}
+	
+	private Dashboard getDashboard()
+	{
+		ORef dashboardRef = getProject().getSingletonObjectRef(Dashboard.getObjectType());
+		return Dashboard.find(getProject(), dashboardRef);
 	}
 }
