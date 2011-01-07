2c0cfa91526cc587a30bd95a0f56c3ba211bf2a5
Kevin <kevins@benetech.org>
2011-01-07 09:40:27 -0500

    Introduce new OpenStandardsDashboardTab parent class (no functionality yet)

--- source/org/miradi/dialogs/dashboard/OpenStandardsImplementActionsAndMonitoringTab.java
+++ source/org/miradi/dialogs/dashboard/OpenStandardsImplementActionsAndMonitoringTab.java
@@ -24,7 +24,7 @@ import org.miradi.dialogs.base.AbstractOpenStandardsQuestionPanel;
 import org.miradi.main.MainWindow;
 import org.miradi.questions.OpenStandardsImplementActionsAndMonitoringQuestion;
 
-public class OpenStandardsImplementActionsAndMonitoringTab extends SplitterPanelWithRightSideTextPanel
+public class OpenStandardsImplementActionsAndMonitoringTab extends OpenStandardsDashboardTab
 {
 	private OpenStandardsImplementActionsAndMonitoringTab(MainWindow mainWindowToUse, AbstractOpenStandardsQuestionPanel leftPanelToUse) throws Exception
 	{
