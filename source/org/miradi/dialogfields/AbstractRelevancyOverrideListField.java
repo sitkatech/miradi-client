a65b0065811c5e7f08a7f725f576d27b117cf5be
nimaa <nimaa@localhost>
2011-01-20 15:36:43 -0500

    Pulled up duplicate methods

--- source/org/miradi/dialogfields/AbstractRelevancyOverrideListField.java
+++ source/org/miradi/dialogfields/AbstractRelevancyOverrideListField.java
@@ -20,14 +20,57 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogfields;
 
+import org.miradi.main.EAM;
 import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.objecthelpers.RelevancyOverrideSet;
+import org.miradi.objects.Strategy;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceQuestion;
 
-public class AbstractRelevancyOverrideListField extends RefListEditorField
+abstract public class AbstractRelevancyOverrideListField extends RefListEditorField
 {
 	public AbstractRelevancyOverrideListField(Project projectToUse,	ORef refToUse, String tagToUse, ChoiceQuestion questionToUse)
 	{
 		super(projectToUse, refToUse, tagToUse, questionToUse);
 	}
+	
+	@Override
+	public String getText()
+	{
+		try
+		{
+			Strategy strategy = Strategy.find(getProject(), getORef());
+			ORefList all = new ORefList(refListEditor.getText());
+			return getCalculatedRelevantOverrides(strategy, all).toString();
+		}
+		catch(Exception e)
+		{
+			EAM.unexpectedErrorDialog(e);
+			EAM.logException(e);
+			return "";
+		}
+	}
+
+	
+
+	@Override
+	public void setText(String codes)
+	{
+		try
+		{
+			Strategy strategy = Strategy.find(getProject(), getORef());
+			ORefList relevantRefList = getRelevantRefs(strategy);
+			refListEditor.setText(relevantRefList.toString());
+		}
+		catch(Exception e)
+		{
+			EAM.unexpectedErrorDialog(e);
+			EAM.logException(e);
+		}	
+	}
+	
+	abstract protected RelevancyOverrideSet getCalculatedRelevantOverrides(Strategy strategy, ORefList all) throws Exception;
+
+	abstract protected ORefList getRelevantRefs(Strategy strategy) throws Exception;
 }
