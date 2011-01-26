ad235e94a53b45b0b86cfea7f403828c961ce019
nimaa <nimaa@localhost>
2011-01-26 09:39:31 -0500

    Method removed

--- source/org/miradi/objects/Strategy.java
+++ source/org/miradi/objects/Strategy.java
@@ -205,16 +205,6 @@ public class Strategy extends Factor
 		return calculateRelevantRefList(relevantGoals, relevantOverrides);
 	}
 	
-	public RelevancyOverrideSet getCalculatedRelevantGoalOverrides(ORefList all) throws Exception
-	{
-		RelevancyOverrideSet relevantOverrides = new RelevancyOverrideSet();
-		ORefList defaultRelevantRefList = getGoalRefs();
-		relevantOverrides.addAll(computeRelevancyOverrides(all, defaultRelevantRefList, true));
-		relevantOverrides.addAll(computeRelevancyOverrides(defaultRelevantRefList, all , false));	
-	
-		return relevantOverrides;
-	}
-
 	public String getTaxonomyCode()
 	{
 		return taxonomyCode.get();
