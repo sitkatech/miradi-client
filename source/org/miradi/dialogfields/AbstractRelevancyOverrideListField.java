f816e795a6349e7fed60db89586718d14d042715
nimaa <nimaa@localhost>
2011-01-27 09:40:54 -0500

    Method rename

--- source/org/miradi/dialogfields/AbstractRelevancyOverrideListField.java
+++ source/org/miradi/dialogfields/AbstractRelevancyOverrideListField.java
@@ -91,7 +91,7 @@ abstract public class AbstractRelevancyOverrideListField extends ObjectDataField
 	{
 		Desire desire = Desire.findDesire(getProject(), desireRef);
 		if (selectedDesireRefs.contains(desireRef))
-			return  desire.createCommandsToEnsureFactorIsRelevant(getORef());
+			return  desire.createCommandsToEnsureStrategyOrActivityIsRelevant(getORef());
 		
 		return  desire.createCommandsToEnsureStrategyOrActivityIsIrrelevant(getORef());
 	}
