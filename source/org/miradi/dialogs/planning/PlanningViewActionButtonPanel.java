cd2b96d82cdd2908d738fffbec9130faac42b7b4
nimaa <nimaa@localhost>
2011-02-01 14:02:14 -0500

    Added action tree configuration choice.  Only has one choice right now.

---
+++ source/org/miradi/dialogs/planning/PlanningViewActionButtonPanel.java
@@ -0,0 +1,45 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.planning;
+
+import org.miradi.dialogs.base.ObjectDataInputPanel;
+import org.miradi.objects.ViewData;
+import org.miradi.project.Project;
+import org.miradi.questions.ActionTreeConfigurationQuestion;
+
+public class PlanningViewActionButtonPanel extends ObjectDataInputPanel
+{
+	public PlanningViewActionButtonPanel(Project projectToUse) throws Exception
+	{
+		super(projectToUse, ViewData.getObjectType());
+		
+		addField(createChoiceField(ViewData.getObjectType(), ViewData.TAG_ACTION_TREE_CONFIGURATION_CHOICE, new ActionTreeConfigurationQuestion()));
+		
+		setObjectRef(getProject().getCurrentViewData().getRef());
+		updateFieldsFromProject();
+	}
+
+	@Override
+	public String getPanelDescription()
+	{
+		return "PlanningViewActionButtonPanel";
+	}
+}
---
+++ source/org/miradi/questions/ActionTreeConfigurationQuestion.java
@@ -0,0 +1,40 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.questions;
+
+import org.miradi.main.EAM;
+
+public class ActionTreeConfigurationQuestion extends StaticChoiceQuestion
+{
+	public ActionTreeConfigurationQuestion()
+	{
+		super(getStaticChoices());
+	}
+
+	private static ChoiceItem[] getStaticChoices()
+	{
+		return new ChoiceItem[] {
+			new ChoiceItem(NOT_SPECIFIED_CODE, EAM.text("Not Specified")),		
+		};
+	}
+	
+	private static final String NOT_SPECIFIED_CODE = "";
+}
