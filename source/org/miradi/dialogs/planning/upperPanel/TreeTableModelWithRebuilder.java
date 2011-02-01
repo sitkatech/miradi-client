0eaf39a3fb99757052a51836013a3be1f4b7ef17
nimaa <nimaa@localhost>
2011-02-01 15:38:19 -0500

    New TreeTableModel with rebuilding method

---
+++ source/org/miradi/dialogs/planning/upperPanel/TreeTableModelWithRebuilder.java
@@ -0,0 +1,50 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.planning.upperPanel;
+
+import org.miradi.dialogs.planning.treenodes.NewPlanningRootNode;
+import org.miradi.dialogs.planning.upperPanel.rebuilder.TreeRebuilder;
+import org.miradi.dialogs.treetables.TreeTableNode;
+import org.miradi.main.EAM;
+import org.miradi.objects.PlanningTreeConfiguration;
+import org.miradi.project.Project;
+
+public class TreeTableModelWithRebuilder extends ExportablePlanningTreeTableModel
+{
+	public TreeTableModelWithRebuilder(Project projectToUse, TreeTableNode rootNode, PlanningTreeConfiguration rowColumnProvider, String uniqueTreeTableModelIdentifierToUse) throws Exception
+	{
+		super(projectToUse, rootNode, rowColumnProvider, uniqueTreeTableModelIdentifierToUse);
+	}
+
+	@Override
+	protected void rebuildNode()
+	{
+		try
+		{
+			TreeRebuilder treeRebuilder = new TreeRebuilder(getProject(), getRowColumnProvider());
+			treeRebuilder.rebuildTree((NewPlanningRootNode)getRootNode());
+		}
+		catch(Exception e)
+		{
+			EAM.panic(e);
+		}
+	}
+}
