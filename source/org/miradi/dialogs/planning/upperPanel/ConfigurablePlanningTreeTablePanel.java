c416d32fdb8c81e1c286cf5e2edd68e4c62db09c
nimaa <nimaa@localhost>
2011-02-01 11:25:28 -0500

    Method removed

--- source/org/miradi/dialogs/planning/upperPanel/ConfigurablePlanningTreeTablePanel.java
+++ source/org/miradi/dialogs/planning/upperPanel/ConfigurablePlanningTreeTablePanel.java
@@ -19,17 +19,6 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.planning.upperPanel;
 
-import org.miradi.actions.ActionCollapseAllRows;
-import org.miradi.actions.ActionCreatePlanningViewConfigurationMenu;
-import org.miradi.actions.ActionDeletePlanningViewConfiguration;
-import org.miradi.actions.ActionDeletePlanningViewTreeNode;
-import org.miradi.actions.ActionExpandAllRows;
-import org.miradi.actions.ActionPlanningCreationMenu;
-import org.miradi.actions.ActionPlanningCustomizeDialogPopup;
-import org.miradi.actions.ActionRenamePlanningViewConfiguration;
-import org.miradi.actions.ActionTreeNodeDown;
-import org.miradi.actions.ActionTreeNodeUp;
-import org.miradi.dialogs.planning.ConfigurableRowColumnProvider;
 import org.miradi.dialogs.planning.PlanningViewConfigurableControlPanel;
 import org.miradi.main.MainWindow;
 import org.miradi.objects.PlanningTreeConfiguration;
@@ -46,32 +35,6 @@ public class ConfigurablePlanningTreeTablePanel extends PlanningTreeTablePanel
 
 		customizationPanel = new PlanningViewConfigurableControlPanel(getProject());
 		addComponentAsFirst(customizationPanel);
-	}
-
-	public static PlanningTreeTablePanel createPlanningTreeTablePanel(MainWindow mainWindowToUse) throws Exception
-	{
-		PlanningTreeTableModel model = new ConfigurablePlanningTreeTableModel(mainWindowToUse.getProject());
-		PlanningTreeTable treeTable = new PlanningTreeTable(mainWindowToUse, model);
-		PlanningTreeConfiguration rowColumnProvider = new ConfigurableRowColumnProvider(mainWindowToUse.getProject());
-		return new ConfigurablePlanningTreeTablePanel(mainWindowToUse, treeTable, model, getButtonActions(), rowColumnProvider);
-	}
-		
-	private static Class[] getButtonActions()
-	{
-		return new Class[] {
-			ActionPlanningCustomizeDialogPopup.class,	
-			ActionDeletePlanningViewConfiguration.class,
-			ActionCreatePlanningViewConfigurationMenu.class,
-			ActionRenamePlanningViewConfiguration.class,
-			null,
-				
-			ActionExpandAllRows.class,
-			ActionCollapseAllRows.class,
-			ActionTreeNodeUp.class,
-			ActionTreeNodeDown.class,
-			ActionPlanningCreationMenu.class,
-			ActionDeletePlanningViewTreeNode.class,
-		};
 	}
 	
 	@Override
