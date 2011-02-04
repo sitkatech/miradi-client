8c2324438a694ec24454ab6bbcf9e67410337668
Kevin <kevins@benetech.org>
2011-02-04 12:04:24 -0500

    Cleanup to add @Override annotations

--- source/org/miradi/xml/generic/OptionalCodeFieldSchemaElement.java
+++ source/org/miradi/xml/generic/OptionalCodeFieldSchemaElement.java
@@ -29,6 +29,7 @@ public class OptionalCodeFieldSchemaElement extends AbstractCodeFieldSchemaEleme
 		super(objectTypeNameToUse, fieldNameToUse, vocabularyNameToUse);
 	}
 
+	@Override
 	public void output(SchemaWriter writer) throws IOException
 	{
 		super.output(writer);
