f6be86e891d0894e81bce044abcce871462d265b
nimaa <nimaa@localhost>
2011-02-10 13:58:28 -0500

    Constructors deleted

--- source/org/miradi/exceptions/UnknownCommandException.java
+++ source/org/miradi/exceptions/UnknownCommandException.java
@@ -25,20 +25,4 @@ public class UnknownCommandException extends Exception
 	{
 		super();
 	}
-
-	public UnknownCommandException(String arg0)
-	{
-		super(arg0);
-	}
-
-	public UnknownCommandException(Throwable arg0)
-	{
-		super(arg0);
-	}
-
-	public UnknownCommandException(String arg0, Throwable arg1)
-	{
-		super(arg0, arg1);
-	}
-
 }
