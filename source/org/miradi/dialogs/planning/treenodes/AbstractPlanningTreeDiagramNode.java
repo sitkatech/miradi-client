5e26fc3690fc7ae049cee32d46836921cc91828a
nimaa <nimaa@localhost>
2011-02-14 11:47:27 -0500

    Import Cleanup

--- source/org/miradi/dialogs/planning/treenodes/AbstractPlanningTreeDiagramNode.java
+++ source/org/miradi/dialogs/planning/treenodes/AbstractPlanningTreeDiagramNode.java
@@ -19,15 +19,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.dialogs.planning.treenodes;
 
-import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
 import org.miradi.objects.AbstractTarget;
 import org.miradi.objects.BaseObject;
 import org.miradi.objects.DiagramFactor;
 import org.miradi.objects.DiagramObject;
 import org.miradi.objects.Factor;
-import org.miradi.objects.Goal;
-import org.miradi.objects.Objective;
 import org.miradi.project.Project;
 import org.miradi.utils.CodeList;
 
