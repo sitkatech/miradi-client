b5287dda881f9a590273b6d0d18cd342237eb67e
nimaa <nimaa@localhost>
2011-03-08 16:00:36 -0500

    Creating a vector of importers, looping them and calling import on each.

--- source/org/miradi/xml/xmpz/XmpzXmlImporter.java
+++ source/org/miradi/xml/xmpz/XmpzXmlImporter.java
@@ -20,12 +20,15 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml.xmpz;
 
+import java.util.Vector;
+
 import javax.xml.namespace.NamespaceContext;
 
 import org.miradi.objecthelpers.ThreatStressRatingEnsurer;
 import org.miradi.project.Project;
 import org.miradi.utils.ProgressInterface;
 import org.miradi.xml.AbstractXmlImporter;
+import org.miradi.xml.AbstractXmpzObjectImporter;
 import org.miradi.xml.wcs.XmpzXmlConstants;
 import org.w3c.dom.Node;
 
@@ -41,56 +44,64 @@ public class XmpzXmlImporter extends AbstractXmlImporter implements XmpzXmlConst
 	@Override
 	protected void importXml() throws Exception
 	{
-		new ProjectSummaryImporter(this).importElement();
-		new ProjectResourceImporter(this).importElement();
-		new OrganizationImporter(this).importElement();
-		new ProjectSummaryScopeImporter(this).importElement();
-		new ProjectSummaryLocationImporter(this).importElement();
-		new ProjectSummaryPlanningImporter(this).importElement();
-		new TncProjectDataImporter(this).importElement();
-		new WwfProjectDataImporter(this).importElement();
-		new WcsProjectDataImporter(this).importElement();
-		new RareProjetDataImporter(this).importElement();
-		new FosProjectDataImporter(this).importElement();
+		Vector<AbstractXmpzObjectImporter> importers = new Vector<AbstractXmpzObjectImporter>();
 		
-		new ConceptualModelPoolImporter(this).importElement();
-		new ResultsChainDiagramPoolImporter(this).importElement();
+		importers.add(new ProjectSummaryImporter(this));
+		importers.add(new ProjectResourceImporter(this));
+		importers.add(new OrganizationImporter(this));
+		importers.add(new ProjectSummaryScopeImporter(this));
+		importers.add(new ProjectSummaryLocationImporter(this));
+		importers.add(new ProjectSummaryPlanningImporter(this));
+		importers.add(new TncProjectDataImporter(this));
+		importers.add(new WwfProjectDataImporter(this));
+		importers.add(new WcsProjectDataImporter(this));
+		importers.add(new RareProjetDataImporter(this));
+		importers.add(new FosProjectDataImporter(this));
 		
-		new CausePoolImporter(this).importElement();
-		new BiodiversityTargetPoolImporter(this).importElement();
-		new HumanWelfareTargetPoolImporter(this).importElement();
-		new StrategyPoolImporter(this).importElement();
-		new ThreatReductionResultsPoolImporter(this).importElement();
-		new IntermediateResultPoolImporter(this).importElement();
-		new StressPoolImporter(this).importElement();
-		new ScopeBoxPoolImporter(this).importElement();
-		new TextBoxPoolImporter(this).importElement();
-		new GroupBoxPoolImporter(this).importElement();
-		new TaskPoolImporter(this).importElement();		
+		importers.add(new ConceptualModelPoolImporter(this));
+		importers.add(new ResultsChainDiagramPoolImporter(this));
 		
-		new DiagramFactorPoolImporter(this).importElement();
-		new DiagramLinkPoolImporter(this).importElement();
+		importers.add(new CausePoolImporter(this));
+		importers.add(new BiodiversityTargetPoolImporter(this));
+		importers.add(new HumanWelfareTargetPoolImporter(this));
+		importers.add(new StrategyPoolImporter(this));
+		importers.add(new ThreatReductionResultsPoolImporter(this));
+		importers.add(new IntermediateResultPoolImporter(this));
+		importers.add(new StressPoolImporter(this));
+		importers.add(new ScopeBoxPoolImporter(this));
+		importers.add(new TextBoxPoolImporter(this));
+		importers.add(new GroupBoxPoolImporter(this));
+		importers.add(new TaskPoolImporter(this));		
 		
-		new KeyEcologicalAttributePoolImporter(this).importElement();
-		new IndicatorPoolImporter(this).importElement();
-		new GoalPoolImporter(this).importElement();
-		new ObjectivePoolImporter(this).importElement();
-		new MeasurementPoolImporter(this).importElement();
-		new SubTargetPoolImporter(this).importElement();
-		new ProgressReportPoolImporter(this).importElement();
-		new ProgressPercentPoolImporter(this).importElement();
-		new AccountingCodePoolImporter(this).importElement();
-		new FundingSourcePoolImporter(this).importElement();
-		new BudgetCategoryOnePoolImporter(this).importElement();
-		new BudgetCategoryTwoPoolImporter(this).importElement();
-		new IucnRedListspeciesPoolImporter(this).importElement();
-		new OtherNotableSpeciesPoolImporter(this).importElement();
-		new AudiencePoolImporter(this).importElement();
-		new ObjectTreeTableConfigurationPoolImporter(this).importElement();
-		new ExpenseAssignmentPoolImporter(this).importElement();
-		new ResourceAssignmentPoolImporter(this).importElement();
-		new DashboardPoolImporter(this).importElement();
-		new TaggedObjectSetPoolImporter(this).importElement();
+		importers.add(new DiagramFactorPoolImporter(this));
+		importers.add(new DiagramLinkPoolImporter(this));
+		
+		importers.add(new KeyEcologicalAttributePoolImporter(this));
+		importers.add(new IndicatorPoolImporter(this));
+		importers.add(new GoalPoolImporter(this));
+		importers.add(new ObjectivePoolImporter(this));
+		importers.add(new MeasurementPoolImporter(this));
+		importers.add(new SubTargetPoolImporter(this));
+		importers.add(new ProgressReportPoolImporter(this));
+		importers.add(new ProgressPercentPoolImporter(this));
+		importers.add(new AccountingCodePoolImporter(this));
+		importers.add(new FundingSourcePoolImporter(this));
+		importers.add(new BudgetCategoryOnePoolImporter(this));
+		importers.add(new BudgetCategoryTwoPoolImporter(this));
+		importers.add(new IucnRedListspeciesPoolImporter(this));
+		importers.add(new OtherNotableSpeciesPoolImporter(this));
+		importers.add(new AudiencePoolImporter(this));
+		importers.add(new ObjectTreeTableConfigurationPoolImporter(this));
+		importers.add(new ExpenseAssignmentPoolImporter(this));
+		importers.add(new ResourceAssignmentPoolImporter(this));
+		importers.add(new DashboardPoolImporter(this));
+		importers.add(new TaggedObjectSetPoolImporter(this));
+		
+		for (AbstractXmpzObjectImporter importer : importers)
+		{
+			importer.importElement();
+		}
+		
 		importThreatStressRatings();
 		importDeletedOrphanText();
 	}
