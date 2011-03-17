a11b9de34b3adb57bb5324e799dc98b894edb736
nimaa <nimaa@localhost>
2011-03-17 15:36:36 -0400

    Changed label,  Description text now appears to the right of the check box

--- source/org/miradi/dialogs/summary/TeamMemberPropertiesPanel.java
+++ source/org/miradi/dialogs/summary/TeamMemberPropertiesPanel.java
@@ -75,8 +75,7 @@ public class TeamMemberPropertiesPanel extends ObjectDataInputPanel
 
 		addField(createMultilineField(ProjectResource.TAG_COMMENTS));
 		
-		ObjectDataInputField checkBox = createCheckBoxField(ProjectResource.TAG_IS_AFFILIATED_TO_CONSERVATION_COACHES_NETWORK);
-		addFieldsOnOneLine(EAM.text("This person is a coach within the Conservation Coaches Network"), new ObjectDataInputField[]{checkBox, });
+		addFieldWithCustomLabelAndHint(createCheckBoxField(ProjectResource.TAG_IS_AFFILIATED_TO_CONSERVATION_COACHES_NETWORK), "This person is a coach within the Conservation Coaches Network");
 		
 		addField(createStringField(ProjectResource.TAG_CUSTOM_FIELD_1));
 		addField(createStringField(ProjectResource.TAG_CUSTOM_FIELD_2));
