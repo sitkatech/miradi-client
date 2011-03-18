f47ab2613288e727b930a14247c4523a1744dad4
nimaa <nimaa@localhost>
2011-03-18 13:09:47 -0400

    Method rename

--- tests/org/miradi/objects/TestDashboardStatusMapsCache.java
+++ tests/org/miradi/objects/TestDashboardStatusMapsCache.java
@@ -61,7 +61,7 @@ public class TestDashboardStatusMapsCache extends TestCaseWithProject
 		AbstractStringKeyMap cachedMap = getProject().getCachedDashboardEffectiveMap();
 		
 		getDashboardStatusMapsCache().invalidateAllCachedMaps();
-		AbstractStringKeyMap nonCachedMap = getDashboardStatusMapsCache().getEffectiveStatusMap();
+		AbstractStringKeyMap nonCachedMap = getDashboardStatusMapsCache().getEffectiveMap();
 		assertEquals("Cached map is not in sync with dashbard's effective status map?", nonCachedMap, cachedMap);
 	}
 
@@ -125,7 +125,7 @@ public class TestDashboardStatusMapsCache extends TestCaseWithProject
 	{
 		getDashboardStatusMapsCache().invalidateAllCachedMaps();
 
-		return getDashboardStatusMapsCache().getEffectiveStatusMap();
+		return getDashboardStatusMapsCache().getEffectiveMap();
 	}
 	
 	private StringChoiceMap createStringChoiceMapForEmptyProject()
