9833dccde72c15e1ce8c59c102be01b6f04e5837
nimaa <nimaa@localhost>
2011-04-14 09:52:19 -0400

    Method extract. Made into static, manually.

--- source/org/miradi/xml/wcs/BaseObjectPoolExporter.java
+++ source/org/miradi/xml/wcs/BaseObjectPoolExporter.java
@@ -163,7 +163,7 @@ abstract public class BaseObjectPoolExporter extends ObjectPoolExporter
 	private void writeExpenseAssignmentTimePeriodCosts(HashMap<DateUnit, TimePeriodCosts> dateUnitTimePeriodCostsMap) throws Exception
 	{
 		final AbstractTimePeriodCostsWriter timePeriodCostsWriter = new ExpenseEntryWriter(getWcsXmlExporter());
-		getWcsXmlExporter().writeStartElement(CALCULATED_EXPENSE_ENTRIES);
+		getWcsXmlExporter().writeStartElement(getExpenseEntriesName());
 		for (DateUnit dateUnit : dateUnitTimePeriodCostsMap.keySet())
 		{
 			TimePeriodCosts timePeriodCosts = dateUnitTimePeriodCostsMap.get(dateUnit);
@@ -171,7 +171,12 @@ abstract public class BaseObjectPoolExporter extends ObjectPoolExporter
 			writeCategorizedQuantaties(dateUnit, categorizedQuantaties, EXPENSE_ENTRY, timePeriodCostsWriter);
 		}
 		
-		getWcsXmlExporter().writeEndElement(CALCULATED_EXPENSE_ENTRIES);
+		getWcsXmlExporter().writeEndElement(getExpenseEntriesName());
+	}
+
+	public static String getExpenseEntriesName()
+	{
+		return CALCULATED_EXPENSE_ENTRIES;
 	}
 
 	private void writeResourceAssignmentTimePeriodCosts(HashMap<DateUnit, TimePeriodCosts> dateUnitTimePeriodCostsMap) throws Exception
