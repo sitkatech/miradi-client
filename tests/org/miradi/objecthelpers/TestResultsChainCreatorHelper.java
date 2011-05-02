2298cf7eb0e84233082ac60c3cfb086637ad06cc
nimaa <nimaa@localhost>
2011-05-02 17:40:19 -0400

    MRD-4613: Bug fixed.  Failing test uncommented.  We are skipping cloning GB child if the child is already in the selection list.

--- tests/org/miradi/objecthelpers/TestResultsChainCreatorHelper.java
+++ tests/org/miradi/objecthelpers/TestResultsChainCreatorHelper.java
@@ -20,11 +20,16 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.objecthelpers;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 import org.miradi.diagram.cells.FactorCell;
 import org.miradi.main.TestCaseWithProject;
 import org.miradi.objects.Cause;
+import org.miradi.objects.DiagramFactor;
 import org.miradi.objects.Factor;
 import org.miradi.objects.IntermediateResult;
+import org.miradi.objects.Strategy;
 import org.miradi.project.ResultsChainCreatorHelper;
 
 public class TestResultsChainCreatorHelper extends TestCaseWithProject
@@ -34,33 +39,31 @@ public class TestResultsChainCreatorHelper extends TestCaseWithProject
 		super(name);
 	}
 	
-//FIXME urgent - uncomment this and fix the related bug.
-// The RCCH.cloneDiagramFactors will require a visibility increase
-//	public void testTransferAnnotationsToNewlyCreatedFactorCoveredByGroupBox() throws Exception
-//	{
-//		DiagramFactor causeDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Cause.getObjectType());
-//		getProject().addObjective(causeDiagramFactor.getWrappedFactor());
-//		DiagramFactor groupBoxDiagramFactor = getProject().createAndPopulateDiagramFactorGroupBox(causeDiagramFactor);
-//		DiagramFactor strategyDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
-//		getProject().createDiagramLinkAndAddToDiagram(strategyDiagramFactor, groupBoxDiagramFactor);
-//		
-//		HashSet<DiagramFactor> diagramFactorToClone = new HashSet<DiagramFactor>();
-//		diagramFactorToClone.add(strategyDiagramFactor);
-//		diagramFactorToClone.add(causeDiagramFactor);
-//		diagramFactorToClone.add(groupBoxDiagramFactor);
-//		
-//		ResultsChainCreatorHelper helper = new ResultsChainCreatorHelper(getProject(), getDiagramModel(), new FactorCell[0]);
-//		HashMap<DiagramFactor, DiagramFactor> originalToClonedMap = helper.cloneDiagramFactors(diagramFactorToClone);
-//		
-//		assertEquals("incorrect number of cloned diagramFactors?", 3, originalToClonedMap.size());
-//		ORefList intermediateResultRefs = getProject().getIntermediateResultPool().getRefList();
-//		assertEquals("Incorrect number of intermediate results created?", 1, intermediateResultRefs.size());
-//		assertEquals("incorrect number of objectives?", 1, getProject().getObjectivePool().size());
-//		
-//		DiagramFactor intermediateResultDiagramFactor = originalToClonedMap.get(causeDiagramFactor);
-//		final Factor intermediateResult = intermediateResultDiagramFactor.getWrappedFactor();
-//		assertEquals("the objective was not transferred?", 1, intermediateResult.getObjectiveIds().size());
-//	}
+	public void testTransferAnnotationsToNewlyCreatedFactorCoveredByGroupBox() throws Exception
+	{
+		DiagramFactor causeDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Cause.getObjectType());
+		getProject().addObjective(causeDiagramFactor.getWrappedFactor());
+		DiagramFactor groupBoxDiagramFactor = getProject().createAndPopulateDiagramFactorGroupBox(causeDiagramFactor);
+		DiagramFactor strategyDiagramFactor = getProject().createDiagramFactorAndAddToDiagram(Strategy.getObjectType());
+		getProject().createDiagramLinkAndAddToDiagram(strategyDiagramFactor, groupBoxDiagramFactor);
+		
+		HashSet<DiagramFactor> diagramFactorToClone = new HashSet<DiagramFactor>();
+		diagramFactorToClone.add(strategyDiagramFactor);
+		diagramFactorToClone.add(causeDiagramFactor);
+		diagramFactorToClone.add(groupBoxDiagramFactor);
+		
+		ResultsChainCreatorHelper helper = new ResultsChainCreatorHelper(getProject(), getDiagramModel(), new FactorCell[0]);
+		HashMap<DiagramFactor, DiagramFactor> originalToClonedMap = helper.cloneDiagramFactors(diagramFactorToClone);
+		
+		assertEquals("incorrect number of cloned diagramFactors?", 3, originalToClonedMap.size());
+		ORefList intermediateResultRefs = getProject().getIntermediateResultPool().getRefList();
+		assertEquals("Incorrect number of intermediate results created?", 1, intermediateResultRefs.size());
+		assertEquals("incorrect number of objectives?", 1, getProject().getObjectivePool().size());
+		
+		DiagramFactor intermediateResultDiagramFactor = originalToClonedMap.get(causeDiagramFactor);
+		final Factor intermediateResult = intermediateResultDiagramFactor.getWrappedFactor();
+		assertEquals("the objective was not transferred?", 1, intermediateResult.getObjectiveIds().size());
+	}
 	
 	public void testTransferAnnotationsToNewlyCreatedFactor() throws Exception
 	{
