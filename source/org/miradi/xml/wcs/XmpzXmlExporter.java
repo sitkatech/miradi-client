afd411cd1650a9e062782c88cedb06dcf7718a1c
nimaa <nimaa@localhost>
2011-08-31 10:35:14 -0400

    MRD-4690: Only add obsolete code if the list does not contain it already.

--- source/org/miradi/xml/wcs/XmpzXmlExporter.java
+++ source/org/miradi/xml/wcs/XmpzXmlExporter.java
@@ -225,15 +225,23 @@ public class XmpzXmlExporter extends XmlExporter implements XmpzXmlConstants
 
 	public void exportTncOperatingUnits() throws Exception
 	{
-		CodeList codes = getMetadata().getCodeList(ProjectMetadata.TAG_TNC_OPERATING_UNITS);
+		CodeList codes = getOpertingUnitsWithoutLegacyCode(getMetadata());
+		
+		writeOptionalCodeListElement(TNC_PROJECT_DATA, XmlSchemaCreator.TNC_OPERATING_UNITS, codes);
+	}
+
+	public static CodeList getOpertingUnitsWithoutLegacyCode(ProjectMetadata projectMetadata) throws Exception
+	{
+		CodeList codes = projectMetadata.getCodeList(ProjectMetadata.TAG_TNC_OPERATING_UNITS);
 		final String LEGACY_CODE_TO_BE_REMOVED = "PACIF";
 		if (codes.contains(LEGACY_CODE_TO_BE_REMOVED))
 		{
 			codes.removeCode(LEGACY_CODE_TO_BE_REMOVED);
-			codes.add(TncOperatingUnitsQuestion.TNC_SUPERSEDED_OU_CODE);
+			if (!codes.contains(TncOperatingUnitsQuestion.TNC_SUPERSEDED_OU_CODE))
+				codes.add(TncOperatingUnitsQuestion.TNC_SUPERSEDED_OU_CODE);
 		}
 		
-		writeOptionalCodeListElement(TNC_PROJECT_DATA, XmlSchemaCreator.TNC_OPERATING_UNITS, codes);
+		return codes;
 	}
 
 	private void writeShareOutsideOrganizationElement() throws Exception
