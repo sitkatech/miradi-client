c159701eb56e5432de93d1eac1ec5d51fde2d2e5
nimaa <nimaa@localhost>
2011-10-11 10:12:19 -0400

    Method extracted

--- tests/org/miradi/objects/TestObjectManager.java
+++ tests/org/miradi/objects/TestObjectManager.java
@@ -215,6 +215,12 @@ public class TestObjectManager extends MiradiTestCase
 	private void verifyBasicObjectLifecycle(int type) throws Exception, IOException, ParseException
 	{
 		BaseId createdId = manager.createObject(type, BaseId.INVALID, null);
+		verifyBasicObjectLifecycle(type, createdId);
+	}
+
+	public void verifyBasicObjectLifecycle(int type, BaseId createdId)
+			throws Exception
+	{
 		assertNotEquals(type + " Created with invalid id", BaseId.INVALID, createdId);
 		db.readObject(manager, type, createdId);
 		assertTrue(db.readObjectManifest(type).has(createdId));
