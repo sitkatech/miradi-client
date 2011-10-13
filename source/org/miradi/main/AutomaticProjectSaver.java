133b84320646deea66ce0d1cb282bc0e8e632789
kevins <kevins@localhost>
2011-10-13 16:46:17 -0400

    New automatic saver

---
+++ source/org/miradi/main/AutomaticProjectSaver.java
@@ -0,0 +1,74 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.main;
+
+import java.io.File;
+
+import org.martus.util.UnicodeStringWriter;
+import org.martus.util.UnicodeWriter;
+import org.miradi.database.ProjectServer;
+import org.miradi.project.Project;
+import org.miradi.project.ProjectSaver;
+
+public class AutomaticProjectSaver implements CommandExecutedListener
+{
+	public void startSaving(ProjectServer databaseToUse, String projectName, Project projectToUse)
+	{
+		if(!databaseToUse.isLocalProject())
+			throw new RuntimeException("Remote storage no longer supported");
+		
+		projectFile = new File(databaseToUse.getCurrentLocalProjectDirectory().getAbsolutePath()+".Miradi");
+		project = projectToUse;
+		project.addCommandExecutedListener(this);
+	}
+	
+	public void stopSaving()
+	{
+		project.removeCommandExecutedListener(this);
+	}
+	
+	public void commandExecuted(CommandExecutedEvent event)
+	{
+		if(project.isInTransaction())
+			return;
+		
+		try
+		{
+			long startedAt = System.currentTimeMillis();
+			UnicodeStringWriter stringWriter = UnicodeStringWriter.create();
+			ProjectSaver.saveProject(project, stringWriter);
+			
+			// TODO: Need safe writing here
+			UnicodeWriter fileWriter = new UnicodeWriter(projectFile);
+			fileWriter.write(stringWriter.toString());
+			fileWriter.close();
+			long endedAt = System.currentTimeMillis();
+			EAM.logDebug("Saved project: " + (endedAt - startedAt) + "ms");
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private File projectFile;
+	private Project project;
+}
