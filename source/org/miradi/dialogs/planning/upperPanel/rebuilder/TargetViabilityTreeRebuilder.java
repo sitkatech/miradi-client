3d763516805f7344078309bffa85d0230b3762f0
nimaa <nimaa@localhost>
2011-10-19 10:35:18 -0400

    Adding children nodes to parent node.

--- source/org/miradi/dialogs/planning/upperPanel/rebuilder/TargetViabilityTreeRebuilder.java
+++ source/org/miradi/dialogs/planning/upperPanel/rebuilder/TargetViabilityTreeRebuilder.java
@@ -23,7 +23,10 @@ package org.miradi.dialogs.planning.upperPanel.rebuilder;
 import org.miradi.main.EAM;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
+import org.miradi.objects.AbstractTarget;
 import org.miradi.objects.DiagramObject;
+import org.miradi.objects.Indicator;
+import org.miradi.objects.KeyEcologicalAttribute;
 import org.miradi.objects.PlanningTreeRowColumnProvider;
 import org.miradi.objects.ProjectMetadata;
 import org.miradi.project.Project;
@@ -42,10 +45,47 @@ public class TargetViabilityTreeRebuilder extends AbstractTreeRebuilder
 		if(ProjectMetadata.is(parentRef))
 			return getChildrenOfProjectNode(parentRef);
 		
+		if(AbstractTarget.isAbstractTarget(parentRef))
+			return getChildrenOfAbstractTargetNode(parentRef);
+		
+		if(KeyEcologicalAttribute.is(parentRef))
+			return getChildrenOfKea(parentRef);
+		
+		if(Indicator.is(parentRef))
+			return getChildrenOfIndicator(parentRef);
+		
 		EAM.logDebug("Don't know how to get children of " + parentRef);
 		return new ORefList();
 	}
 	
+	private ORefList getChildrenOfKea(ORef parentRef)
+	{
+		ORefList childRefs = new ORefList();
+		KeyEcologicalAttribute kea = KeyEcologicalAttribute.find(getProject(), parentRef);
+		childRefs.addAll(kea.getIndicatorRefs());
+		
+		return childRefs;
+	}
+
+	private ORefList getChildrenOfIndicator(ORef parentRef)
+	{
+		ORefList childRefs = new ORefList();
+		Indicator indicator = Indicator.find(getProject(), parentRef);
+		childRefs.addAll(indicator.getMeasurementRefs());
+		//FIXME urgent - must add goal node here (Future status)
+		
+		return childRefs;
+	}
+
+	private ORefList getChildrenOfAbstractTargetNode(ORef parentRef)
+	{
+		ORefList childRefs = new ORefList();
+		AbstractTarget abtractTarget = AbstractTarget.findTarget(getProject(), parentRef);
+		childRefs.addAll(abtractTarget.getKeyEcologicalAttributeRefs());
+		
+		return childRefs;
+	}
+
 	private ORefList getChildrenOfProjectNode(ORef parentRef) throws Exception
 	{
 		ORefList childRefs = new ORefList();
