688d8d41abb3341cdced7a5c8f235d68505ce1ff
kevins <kevins@localhost>
2011-10-20 00:15:58 -0400

    Command line MpzToDotMiradiConverter (with some fixes)

--- source/org/miradi/project/MpzToDotMiradiConverter.java
+++ source/org/miradi/project/MpzToDotMiradiConverter.java
@@ -20,28 +20,90 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.project;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.martus.util.UnicodeStringWriter;
+import org.martus.util.UnicodeWriter;
 import org.miradi.database.ProjectServer;
 import org.miradi.ids.BaseId;
+import org.miradi.main.EAM;
 import org.miradi.objectdata.ObjectData;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objects.BaseObject;
+import org.miradi.objects.Factor;
 import org.miradi.utils.EnhancedJsonArray;
 import org.miradi.utils.EnhancedJsonObject;
+import org.miradi.utils.Translation;
 
 public class MpzToDotMiradiConverter extends AbstractMiradiProjectSaver
 {
+	public static void main(String[] args) throws Exception
+	{
+		Translation.initialize();
+		
+		File homeDirectory = EAM.getHomeDirectory();
+		String mpzPath = "";
+
+		System.out.println("Converts a .MPZ file to a .Miradi file");
+		System.out.println(" The .Miradi file will be created in " + homeDirectory);
+		if(args.length == 0)
+		{
+			System.out.print("Enter the path to the MPZ file: ");
+			System.out.flush();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			mpzPath = reader.readLine();
+		}
+		else if(args.length == 1)
+		{
+			mpzPath = args[0];
+		}
+		else
+		{
+			System.out.println("Must specify an MPZ file to convert.");
+			System.exit(1);
+		}
+		System.out.println("Converting " + mpzPath);
+		
+		if(!mpzPath.endsWith(".mpz"))
+			throw new RuntimeException("Not an MPZ file: " + mpzPath);
+
+		File mpzFile = new File(mpzPath);
+		if(!mpzFile.exists())
+			throw new RuntimeException("MPZ doesn't exist: " + mpzFile.getAbsolutePath());
+
+		String newName = mpzFile.getName().replaceAll(".mpz", ".Miradi");
+		File destination = new File(homeDirectory, newName);
+		if(destination.exists())
+			throw new RuntimeException(".Miradi file already exists: " + destination.getAbsolutePath());
+		
+		InputStream mpzInput = new FileInputStream(mpzFile);
+		String converted = convert(mpzInput);
+		UnicodeWriter writer = new UnicodeWriter(destination);
+		writer.write(converted);
+		writer.close();
+		System.out.println("Converted");
+	}
+	
 	public MpzToDotMiradiConverter(InputStream inputStream, UnicodeStringWriter writerToUse) throws Exception
 	{
 		super(writerToUse);
 		
 		zipInputStream = new ZipInputStream(inputStream);
+	}
+	
+	public static final String convert(InputStream mpzInputStream) throws Exception
+	{
+		UnicodeStringWriter writer = UnicodeStringWriter.create();
+		convert(mpzInputStream, writer);
+		return writer.toString();
 	}
 	
 	public static final void convert(InputStream inputStream, UnicodeStringWriter writer) throws Exception
@@ -125,11 +187,6 @@ public class MpzToDotMiradiConverter extends AbstractMiradiProjectSaver
 		String[] typeId = splittedPath[1].split("/");
 		ORef ref = new ORef(Integer.parseInt(typeId[0]), new BaseId(typeId[1]));
 		writeValue(CREATE_OBJECT_CODE, createSimpleRefString(ref));
-		writeUpdateObjectLines(fileContent, ref);
-	}
-
-	public void writeUpdateObjectLines(String fileContent, ORef ref) throws Exception
-	{
 		final Project tempProject = new Project();
 		EnhancedJsonObject json = new EnhancedJsonObject(fileContent);
 		BaseObject baseObject = BaseObject.createFromJson(tempProject.getObjectManager(), ref.getObjectType(), json);
@@ -137,18 +194,36 @@ public class MpzToDotMiradiConverter extends AbstractMiradiProjectSaver
 		while (iterator.hasNext())
 		{
 			String tag = (String)iterator.next();
-			if (!tag.equals(BaseObject.TAG_TIME_STAMP_MODIFIED))
+			if (!shouldSkipTag(ref, tag))
 			{
 				String data = json.get(tag).toString();	
 				ObjectData dataField = baseObject.getField(tag);
 				if (dataField != null && dataField.isUserText())
-				{
 					data = xmlNewLineEncode(data);
-				}
-
-				writeRefTagValue(UPDATE_OBJECT_CODE, ref, tag, data);
+		
+				if(data.length() > 0)
+					writeRefTagValue(UPDATE_OBJECT_CODE, ref, tag, data);
 			}
 		}
+	}
+
+	private boolean shouldSkipTag(ORef ref, String tag)
+	{
+		if(tag.equals(BaseObject.TAG_TIME_STAMP_MODIFIED))
+			return true;
+		if(tag.equals("BudgetCostOverride"))
+			return true;
+		if(tag.equals("WhoOverrideRefs"))
+			return true;
+		if(tag.equals("WhenOverride"))
+			return true;
+		if(tag.equals("CostUnit"))
+			return true;
+		
+		if(Factor.isFactor(ref) && tag.equals("Type"))
+			return true;
+
+		return false;
 	}
 
 	private void writeSimpleThreatFramework(String jsonContent) throws Exception
