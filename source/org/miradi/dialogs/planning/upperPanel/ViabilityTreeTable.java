e416932a09ad405a4e01ac2d7c37b333c63b9feb
nimaa <nimaa@localhost>
2011-10-21 17:28:37 -0400

    Overriding getRelevantActions to include actions on a popup menu

--- source/org/miradi/dialogs/planning/upperPanel/ViabilityTreeTable.java
+++ source/org/miradi/dialogs/planning/upperPanel/ViabilityTreeTable.java
@@ -20,6 +20,16 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.planning.upperPanel;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.miradi.actions.ActionExpandToGoal;
+import org.miradi.actions.ActionExpandToHumanWelfareTarget;
+import org.miradi.actions.ActionExpandToIndicator;
+import org.miradi.actions.ActionExpandToKeyEcologicalAttribute;
+import org.miradi.actions.ActionExpandToMeasurement;
+import org.miradi.actions.ActionExpandToMenu;
+import org.miradi.actions.ActionExpandToTarget;
 import org.miradi.dialogs.treetables.GenericTreeTableModel;
 import org.miradi.main.MainWindow;
 
@@ -34,5 +44,21 @@ public class ViabilityTreeTable extends PlanningTreeTable
 	protected boolean isRootVisible()
 	{
 		return true;
+	}
+	
+	@Override
+	protected Set<Class> getRelevantActions()
+	{
+		HashSet<Class> relevantActions = new HashSet<Class>();
+		relevantActions.addAll(super.getRelevantActions());
+		relevantActions.add(ActionExpandToMenu.class);
+		relevantActions.add(ActionExpandToTarget.class);
+		relevantActions.add(ActionExpandToHumanWelfareTarget.class);
+		relevantActions.add(ActionExpandToKeyEcologicalAttribute.class);
+		relevantActions.add(ActionExpandToIndicator.class);
+		relevantActions.add(ActionExpandToGoal.class);
+		relevantActions.add(ActionExpandToMeasurement.class);
+		
+		return relevantActions;
 	}
 }
