1e2a1a9fddeabbcb407a3a11dcd62d72072635ac
kevins <kevins@localhost>
2011-11-05 11:56:47 +0700

    Refactor rename constant (and its string contents) for consistency

--- source/org/miradi/wizard/noproject/WelcomeImportStep.java
+++ source/org/miradi/wizard/noproject/WelcomeImportStep.java
@@ -112,7 +112,7 @@ public class WelcomeImportStep extends NoProjectWizardStep
 			{
 				EAM.notifyDialog("Import MPZ is not supported");
 			}
-			if (buttonName.equals(CONTROL_IMPORT_CONPRO))
+			if (buttonName.equals(CONTROL_IMPORT_CPMZ))
 			{
 				MiradiAction action = getMainWindow().getActions().get(ActionImportCpmz.class);
 				action.doAction();
@@ -129,6 +129,6 @@ public class WelcomeImportStep extends NoProjectWizardStep
 		}
 	}
 
-	public static final String CONTROL_IMPORT_CONPRO = "Import ConPro Project";
+	public static final String CONTROL_IMPORT_CPMZ = "Import ConPro (CPMZ) Project";
 	public static final String CONTROL_IMPORT_MPZ = "Import MPZ Project";
 }
