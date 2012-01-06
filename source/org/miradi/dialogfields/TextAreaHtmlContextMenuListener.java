8912fcb2f45fa6f1f07a12e05af569d5a740bfa8
nimaa <nimaa@localhost>
2012-01-06 10:16:35 -0500

    mrd-585: Html editor pane is hooked into multi line text fields.

---
+++ source/org/miradi/dialogfields/TextAreaHtmlContextMenuListener.java
@@ -0,0 +1,56 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogfields;
+
+import javax.swing.AbstractAction;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.text.JTextComponent;
+
+import org.bushe.swing.action.ActionList;
+import org.miradi.actions.Actions;
+
+public class TextAreaHtmlContextMenuListener extends TextAreaContextMenuListener
+{
+	public TextAreaHtmlContextMenuListener(JPopupMenu menuToUse, JTextComponent textFieldToUse, Actions actionsToUse, ActionList actionListToUse)
+	{
+		super(menuToUse, textFieldToUse, actionsToUse);
+		
+		actionList = actionListToUse;
+	}
+	
+	@Override
+	public void popupMenuWillBecomeVisible(PopupMenuEvent ev)
+	{
+		super.popupMenuWillBecomeVisible(ev);
+		
+		getMenu().addSeparator();
+		
+		for (int index = 0; index < actionList.size(); ++index)
+		{
+			AbstractAction action = (AbstractAction) actionList.get(index);
+			getMenu().add(new JMenuItem(action));
+		}
+	}
+	
+	private ActionList actionList;
+}
--- source/org/miradi/utils/EditableHtmlPane.java
+++ source/org/miradi/utils/EditableHtmlPane.java
@@ -20,109 +20,53 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.utils;
 
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.StringReader;
 
-import javax.swing.JEditorPane;
-import javax.swing.JPopupMenu;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
 import javax.swing.text.Document;
-import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import javax.swing.undo.UndoManager;
 
 import net.atlanticbb.tantlinger.ui.text.CompoundUndoManager;
 import net.atlanticbb.tantlinger.ui.text.HTMLUtils;
 import net.atlanticbb.tantlinger.ui.text.WysiwygHTMLEditorKit;
-import net.atlanticbb.tantlinger.ui.text.actions.HTMLEditorActionFactory;
-import net.atlanticbb.tantlinger.ui.text.actions.HTMLTextEditAction;
 
-import org.bushe.swing.action.ActionList;
-import org.bushe.swing.action.ActionUIFactory;
+import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
 
 public class EditableHtmlPane extends MiradiTextPane
 {
-	private static final long serialVersionUID = 1L;
-	private static final String INVALID_TAGS[] = {"html", "head", "body", "title"};
-
-	private JEditorPane focusedEditor;
-	private JPopupMenu wysPopupMenu;
-	private ActionList actionList;
-
-	private FocusListener focusHandler = new FocusHandler(); 
-	private CaretListener caretHandler = new CaretHandler();
-	private MouseListener popupHandler = new PopupHandler();
-
 	public EditableHtmlPane(MainWindow mainWindow, int fixedApproximateColumnCount, int initialApproximateRowCount) throws Exception
 	{
 		super(mainWindow, fixedApproximateColumnCount, initialApproximateRowCount);
 		
-		createWysiwygEditor();
-		createEditorActions();
-	}
-
-	private void createEditorActions()
-	{        
-		actionList = new ActionList("editor-actions");
-		actionList.addAll(HTMLEditorActionFactory.createFontSizeActionList());        
-		actionList.addAll(HTMLEditorActionFactory.createInlineActionList());
-		actionList.addAll(HTMLEditorActionFactory.createBlockElementActionList());
-		actionList.addAll(HTMLEditorActionFactory.createListElementActionList());
-		actionList.addAll(HTMLEditorActionFactory.createAlignActionList());        
-		actionList.addAll(HTMLEditorActionFactory.createInsertTableElementActionList());
-		actionList.addAll(HTMLEditorActionFactory.createDeleteTableElementActionList());
-		
-		wysPopupMenu = ActionUIFactory.getInstance().createPopupMenu(actionList);
-	}
-
-	
-	private void createWysiwygEditor()
-	{
 		setEditorKitForContentType("text/html", new WysiwygHTMLEditorKit());
 		setContentType("text/html"); 
-		insertHTML("", 0);        
-
-		addCaretListener(caretHandler);
-		addFocusListener(focusHandler);
-		addMouseListener(popupHandler);
-
-
-		HTMLDocument document = (HTMLDocument)getDocument();
-		CompoundUndoManager cuh = new CompoundUndoManager(document, new UndoManager());
-		document.addUndoableEditListener(cuh);
+		insertHtml("", 0);        
 	}
 
-	private void insertHTML(String html, int location) 
+	private void insertHtml(String html, int location) 
 	{       
 		try 
 		{
-			HTMLEditorKit kit = (HTMLEditorKit) getEditorKit();
-			Document doc = getDocument();
-			System.out.println("BEFORE=" + html);
-			final String jEditorPaneizeHTML = HTMLUtils.jEditorPaneizeHTML(html);
-			System.out.println("AFTERR=" + jEditorPaneizeHTML);
-			StringReader reader = new StringReader(jEditorPaneizeHTML);
-			kit.read(reader, doc, location);
+			HTMLEditorKit htmlEditorKit = (HTMLEditorKit) getEditorKit();
+			Document document = getDocument();
+			final String jEditorPaneizeHtml = HTMLUtils.jEditorPaneizeHTML(html);
+			StringReader stringReader = new StringReader(jEditorPaneizeHtml);
+			htmlEditorKit.read(stringReader, document, location);
 		} 
-		catch (Exception ex)
+		catch (Exception e)
 		{
-			ex.printStackTrace();
+			EAM.unexpectedErrorDialog(e);
+			EAM.logException(e);
 		}
 	}
 
 	@Override
 	public void setText(String text)
 	{
-		//`System.out.println("setttingText=" +  text);
-		String topText = removeInvalidTags(text);  
+		text = HtmlUtilities.removeNonHtmlNewLines(text);
+		String topText = removeInvalidTags(text);
 		super.setText("");
-		insertHTML(topText, 0);            
+		insertHtml(topText, 0);            
 		CompoundUndoManager.discardAllEdits(getDocument());
 	}
 
@@ -134,6 +78,7 @@ public class EditableHtmlPane extends MiradiTextPane
 
 	private String removeInvalidTags(String html)
 	{
+		final String INVALID_TAGS[] = {"html", "head", "body", "title"};
 		for(int i = 0; i < INVALID_TAGS.length; i++)
 		{
 			html = deleteOccurance(html, '<' + INVALID_TAGS[i] + '>');
@@ -145,74 +90,13 @@ public class EditableHtmlPane extends MiradiTextPane
 
 	private String deleteOccurance(String text, String word)
 	{
-		StringBuffer sb = new StringBuffer(text);       
-		int p;
-		while((p = sb.toString().toLowerCase().indexOf(word.toLowerCase())) != -1)
+		StringBuffer stringBuffer = new StringBuffer(text);       
+		int index;
+		while((index = stringBuffer.toString().toLowerCase().indexOf(word.toLowerCase())) != -1)
 		{           
-			sb.delete(p, p + word.length());            
+			stringBuffer.delete(index, index + word.length());            
 		}
-		return sb.toString();
+		
+		return stringBuffer.toString();
 	}
-
-	private void updateState()
-	{
-		actionList.putContextValueForAll(HTMLTextEditAction.EDITOR, focusedEditor);
-		actionList.updateEnabledForAll();
-	}
-
-	private class CaretHandler implements CaretListener
-	{
-		public void caretUpdate(CaretEvent e)
-		{            
-			updateState();
-		}        
-	}
-
-	private class PopupHandler extends MouseAdapter
-	{
-		@Override
-		public void mousePressed(MouseEvent e)
-		{ 
-			checkForPopupTrigger(e); 
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent e)
-		{ 
-			checkForPopupTrigger(e); 
-		}
-
-		private void checkForPopupTrigger(MouseEvent e)
-		{
-			if(e.isPopupTrigger())
-			{                    
-				wysPopupMenu.show(e.getComponent(), e.getX(), e.getY());
-			}
-		}
-	}
-
-	private class FocusHandler implements FocusListener
-	{
-		public void focusGained(FocusEvent e)
-		{
-			if(e.getComponent() instanceof JEditorPane)
-			{
-				JEditorPane ed = (JEditorPane)e.getComponent();
-				CompoundUndoManager.updateUndo(ed.getDocument());
-				focusedEditor = ed;
-
-				updateState();
-			}
-		}
-
-		public void focusLost(FocusEvent e)
-		{
-			if(e.getComponent() instanceof JEditorPane)
-			{
-				//focusedEditor = null;
-				//wysiwygUpdated();
-			}
-		}
-	}
-
 }
