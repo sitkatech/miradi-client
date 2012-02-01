7d0f0ab91377bc1b08a2aefa7326b023bd467b34
nimaa <nimaa@localhost>
2012-02-01 14:24:17 -0500

    Class renamed

--- tests/org/miradi/main/MainTestSuite.java
+++ tests/org/miradi/main/MainTestSuite.java
@@ -204,7 +204,7 @@ import org.miradi.utils.TestPointList;
 import org.miradi.utils.TestRtfWriter;
 import org.miradi.utils.TestCodeChoiceMap;
 import org.miradi.utils.TestCodeCodeListMap;
-import org.miradi.utils.TestStringMap;
+import org.miradi.utils.TestCodeStringMap;
 import org.miradi.utils.TestStringUtilities;
 import org.miradi.utils.TestTaxonomyFileLoader;
 import org.miradi.utils.TestThreatStressRatingHelper;
@@ -375,7 +375,7 @@ public class MainTestSuite extends TestSuite
 		addTest(new TestSuite(TestIdList.class));
 		addTest(new TestSuite(TestBaseId.class));
 		addTest(new TestSuite(TestCodeList.class));
-		addTest(new TestSuite(TestStringMap.class));
+		addTest(new TestSuite(TestCodeStringMap.class));
 		addTest(new TestSuite(TestCodeChoiceMap.class));
 		addTest(new TestSuite(TestCodeCodeListMap.class));
 		addTest(new TestSuite(TestIndicator.class));
