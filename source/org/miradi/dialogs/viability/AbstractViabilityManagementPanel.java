99d0c662e41191b8bc90acb8164bd66160bbeca0
nimaa <nimaa@localhost>
2012-02-03 13:47:05 -0500

    Pulled up generic methods and class member and its setter/getter.

--- source/org/miradi/dialogs/viability/AbstractViabilityManagementPanel.java
+++ source/org/miradi/dialogs/viability/AbstractViabilityManagementPanel.java
@@ -20,14 +20,95 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.viability;
 
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
+
 import org.miradi.dialogs.base.ObjectListManagementPanel;
+import org.miradi.dialogs.planning.treenodes.PlanningTreeRootNodeAlwaysExpanded;
 import org.miradi.dialogs.planning.upperPanel.PlanningTreeTablePanel;
+import org.miradi.dialogs.planning.upperPanel.TreeTableModelWithRebuilder;
+import org.miradi.dialogs.planning.upperPanel.ViabilityTreeTableModel;
 import org.miradi.main.MainWindow;
+import org.miradi.objects.PlanningTreeRowColumnProvider;
+import org.miradi.utils.BufferedImageFactory;
+import org.miradi.utils.TableExporter;
 
 abstract public class AbstractViabilityManagementPanel extends ObjectListManagementPanel
 {
 	protected AbstractViabilityManagementPanel(MainWindow mainWindowToUse, PlanningTreeTablePanel tablePanelToUse, TargetViabilityMultiPropertiesPanel propertiesPanel) throws Exception
 	{
 		super(mainWindowToUse, tablePanelToUse, propertiesPanel);
+		
+		setTreeTablePanel(tablePanelToUse);
 	}	
+	
+	@Override
+	public boolean isImageAvailable()
+	{
+		return true;
+	}
+	
+	@Override
+	public BufferedImage getImage(int scale) throws Exception
+	{
+		JComponent panel = getPrintableComponent();
+		BufferedImage image = BufferedImageFactory.createImageFromComponent(panel);
+		return image;
+	}
+
+	@Override
+	public boolean isExportableTableAvailable()
+	{
+		return true;
+	}
+	
+	@Override
+	public TableExporter getTableExporter() throws Exception
+	{
+		PlanningTreeTablePanel panel = createPlanningTreeTablePanelForExport();
+		TableExporter table = panel.getTableForExporting();
+		panel.dispose();
+		
+		return table;
+	}
+
+	private PlanningTreeTablePanel createPlanningTreeTablePanel(String uniqueTreeTableModelIdentifier, PlanningTreeRowColumnProvider rowColumnProvider) throws Exception
+	{
+		PlanningTreeRootNodeAlwaysExpanded rootNode = new PlanningTreeRootNodeAlwaysExpanded(getMainWindow().getProject());
+		TreeTableModelWithRebuilder model = new ViabilityTreeTableModel(getMainWindow().getProject(), rootNode, rowColumnProvider);
+		PlanningTreeTablePanel treeTablePanel = TargetViabilityTreeTablePanel.createTreeTablePanel(getMainWindow(), model, rowColumnProvider);
+		
+		return treeTablePanel;
+	}
+
+	@Override
+	public JComponent getPrintableComponent() throws Exception
+	{
+		PlanningTreeTablePanel panel = createPlanningTreeTablePanelForExport();
+		
+		return PlanningTreeTablePanel.createReformattedPrintablePlanningTreeTablePanel(panel);
+	}
+	
+	private PlanningTreeTablePanel createPlanningTreeTablePanelForExport() throws Exception
+	{
+		String uniqueTreeTableModelIdentifier = getPlanningTreeTablePanel().getTree().getTreeTableModel().getUniqueTreeTableModelIdentifier();
+		PlanningTreeRowColumnProvider rowColumnProvider = getPlanningTreeTablePanel().getRowColumnProvider();
+		PlanningTreeTablePanel panel = createPlanningTreeTablePanel(uniqueTreeTableModelIdentifier,	rowColumnProvider);
+		panel.becomeActive();
+		
+		return panel;
+	}
+	
+	private void setTreeTablePanel(PlanningTreeTablePanel planningTreeTablePanelToUse)
+	{
+		planningTreeTablePanel = planningTreeTablePanelToUse;
+	}
+
+	private PlanningTreeTablePanel getPlanningTreeTablePanel()
+	{
+		return planningTreeTablePanel;
+	}
+	
+	private PlanningTreeTablePanel planningTreeTablePanel;
 }
--- source/org/miradi/dialogs/viability/TargetViabilityManagementPanel.java
+++ source/org/miradi/dialogs/viability/TargetViabilityManagementPanel.java
@@ -20,10 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogs.viability;
 
-import java.awt.image.BufferedImage;
-
 import javax.swing.Icon;
-import javax.swing.JComponent;
 
 import org.miradi.actions.jump.ActionJumpTargetViabilityMethodChoiceStep;
 import org.miradi.dialogs.planning.AbstractPlanningTreeRowColumnProvider;
@@ -34,17 +31,12 @@ import org.miradi.dialogs.planning.upperPanel.ViabilityTreeTableModel;
 import org.miradi.icons.IconManager;
 import org.miradi.main.EAM;
 import org.miradi.main.MainWindow;
-import org.miradi.objects.PlanningTreeRowColumnProvider;
-import org.miradi.utils.BufferedImageFactory;
-import org.miradi.utils.TableExporter;
 
 public class TargetViabilityManagementPanel extends AbstractViabilityManagementPanel
 {
 	protected TargetViabilityManagementPanel(MainWindow mainWindowToUse, PlanningTreeTablePanel tablePanelToUse, TargetViabilityMultiPropertiesPanel propertiesPanel) throws Exception
 	{
 		super(mainWindowToUse, tablePanelToUse, propertiesPanel);
-		
-		setTreeTablePanel(tablePanelToUse);
 	}
 	
 	public static TargetViabilityManagementPanel createManagementPanel(MainWindow mainWindowToUse) throws Exception
@@ -59,73 +51,6 @@ public class TargetViabilityManagementPanel extends AbstractViabilityManagementP
 		return new TargetViabilityManagementPanel(mainWindowToUse, treeTablePanel, propertiesPanel);
 	}
 	
-	@Override
-	public boolean isImageAvailable()
-	{
-		return true;
-	}
-	
-	@Override
-	public BufferedImage getImage(int scale) throws Exception
-	{
-		JComponent panel = getPrintableComponent();
-		BufferedImage image = BufferedImageFactory.createImageFromComponent(panel);
-		return image;
-	}
-
-	@Override
-	public boolean isExportableTableAvailable()
-	{
-		return true;
-	}
-	
-	@Override
-	public TableExporter getTableExporter() throws Exception
-	{
-		PlanningTreeTablePanel panel = createPlanningTreeTablePanelForExport();
-		TableExporter table = panel.getTableForExporting();
-		panel.dispose();
-		
-		return table;
-	}
-
-	private PlanningTreeTablePanel createPlanningTreeTablePanel(String uniqueTreeTableModelIdentifier, PlanningTreeRowColumnProvider rowColumnProvider) throws Exception
-	{
-		PlanningTreeRootNodeAlwaysExpanded rootNode = new PlanningTreeRootNodeAlwaysExpanded(getMainWindow().getProject());
-		TreeTableModelWithRebuilder model = new ViabilityTreeTableModel(getMainWindow().getProject(), rootNode, rowColumnProvider);
-		PlanningTreeTablePanel treeTablePanel = TargetViabilityTreeTablePanel.createTreeTablePanel(getMainWindow(), model, rowColumnProvider);
-		
-		return treeTablePanel;
-	}
-
-	@Override
-	public JComponent getPrintableComponent() throws Exception
-	{
-		PlanningTreeTablePanel panel = createPlanningTreeTablePanelForExport();
-		
-		return PlanningTreeTablePanel.createReformattedPrintablePlanningTreeTablePanel(panel);
-	}
-	
-	private PlanningTreeTablePanel createPlanningTreeTablePanelForExport() throws Exception
-	{
-		String uniqueTreeTableModelIdentifier = getPlanningTreeTablePanel().getTree().getTreeTableModel().getUniqueTreeTableModelIdentifier();
-		PlanningTreeRowColumnProvider rowColumnProvider = getPlanningTreeTablePanel().getRowColumnProvider();
-		PlanningTreeTablePanel panel = createPlanningTreeTablePanel(uniqueTreeTableModelIdentifier,	rowColumnProvider);
-		panel.becomeActive();
-		
-		return panel;
-	}
-	
-	private void setTreeTablePanel(PlanningTreeTablePanel planningTreeTablePanelToUse)
-	{
-		planningTreeTablePanel = planningTreeTablePanelToUse;
-	}
-
-	private PlanningTreeTablePanel getPlanningTreeTablePanel()
-	{
-		return planningTreeTablePanel;
-	}
-
 	@Override
 	public Icon getIcon()
 	{
@@ -151,6 +76,4 @@ public class TargetViabilityManagementPanel extends AbstractViabilityManagementP
 	}
 
 	private static String PANEL_DESCRIPTION_VIABILITY = EAM.text("Tab|Viability");
-	
-	private PlanningTreeTablePanel planningTreeTablePanel;
 }
