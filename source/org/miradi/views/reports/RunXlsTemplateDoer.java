c7c67e71a87f6955a79c934f55f5f40d53a1f4bc
nimaa <nimaa@localhost>
2012-02-08 14:51:31 -0500

    Copied run xsl + xml transformation functionality from prototype to doer.

--- source/org/miradi/views/reports/RunXlsTemplateDoer.java
+++ source/org/miradi/views/reports/RunXlsTemplateDoer.java
@@ -20,13 +20,85 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.views.reports;
 
-import org.miradi.views.ObjectsDoer;
+import java.io.File;
+import java.io.FileOutputStream;
 
-//FIXME urgent - add run functionality to this doer and make it available
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.martus.util.UnicodeStringReader;
+import org.martus.util.UnicodeStringWriter;
+import org.miradi.dialogs.reportTemplate.XsltReportPanel;
+import org.miradi.main.EAM;
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.XslTemplate;
+import org.miradi.utils.XmlUtilities2;
+import org.miradi.views.ObjectsDoer;
+import org.miradi.xml.wcs.XmpzXmlExporter;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
 public class RunXlsTemplateDoer extends ObjectsDoer
 {
 	@Override
+	public boolean isAvailable()
+	{
+		if(!super.isAvailable())
+			return false;
+		
+		return getSingleSelectedObject() != null;
+	}
+	
+	@Override
 	protected void doIt() throws Exception
 	{
+		if (!isAvailable())
+			return;
+		
+		BaseObject selectedObject = getSingleSelectedObject();
+		String xlsTemplate = selectedObject.getData(XslTemplate.TAG_XSL_TEMPLATE);
+		xlsTemplate = XmlUtilities2.getXmlDecoded(xlsTemplate);
+		transform(xlsTemplate);
+	}
+	
+	private void transform(final String xslTemplate) throws Exception 
+	{
+		final InputSource projectXmlInputSource = getExportedProjectXmlAsString(); 
+
+		DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
+		dfactory.setNamespaceAware(true);
+		Document doc = dfactory.newDocumentBuilder().parse(projectXmlInputSource);
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		final StreamSource xslStreamSource = new StreamSource(new UnicodeStringReader(xslTemplate));
+		Transformer transformer = transformerFactory.newTransformer(xslStreamSource);
+
+		final File outputFile = XsltReportPanel.getUserChosenFile(getMainWindow(), EAM.text("Select Output File"), EAM.text("Save"));
+		if (outputFile != null)
+		{				
+			transformer.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(outputFile)));
+			EAM.notifyDialog(EAM.text("Transformation Completed!"));
+		}
+	}
+	
+	private InputSource getExportedProjectXmlAsString() throws Exception
+	{
+		UnicodeStringWriter projectWriter = UnicodeStringWriter.create();
+		try
+		{
+			new XmpzXmlExporter(getProject()).exportProject(projectWriter);
+			projectWriter.flush();
+			final String projectXmlAsString = projectWriter.toString();
+			UnicodeStringReader reader = new UnicodeStringReader(projectXmlAsString);
+			
+			return new InputSource(reader);
+		}
+		finally
+		{
+			projectWriter.close();
+		}
 	}
 }
