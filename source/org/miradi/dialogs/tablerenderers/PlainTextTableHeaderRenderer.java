6c12e28f9274d7a51eed64ef1396dd5f2933d988
nimaa <nimaa@localhost>
2012-03-21 13:30:22 -0400

    MRD-4841: added plain text header renderer. Didnt add html renderer because we dont want new lines in our headers

---
+++ source/org/miradi/dialogs/tablerenderers/PlainTextTableHeaderRenderer.java
@@ -0,0 +1,39 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.tablerenderers;
+
+import java.awt.Component;
+
+import javax.swing.JTable;
+
+import org.miradi.utils.HtmlUtilities;
+
+import sun.swing.table.DefaultTableCellHeaderRenderer;
+
+public class PlainTextTableHeaderRenderer extends DefaultTableCellHeaderRenderer
+{
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	{
+		final Object valueAsString = HtmlUtilities.convertHtmlToPlainText(value.toString());
+		return super.getTableCellRendererComponent(table, valueAsString, isSelected, hasFocus, row, column);
+	}
+}
