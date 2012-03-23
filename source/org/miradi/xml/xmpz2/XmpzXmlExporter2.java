cbb2a2b9e6c3905bb21ee69efdc9bd977f4fb712
nimaa <nimaa@localhost>
2012-03-23 15:41:39 -0400

    New exporter shell inside new package.

---
+++ source/org/miradi/xml/xmpz2/XmpzXmlExporter2.java
@@ -0,0 +1,39 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.xmpz2;
+
+import org.martus.util.UnicodeWriter;
+import org.miradi.project.Project;
+import org.miradi.xml.XmlExporter;
+
+public class XmpzXmlExporter2 extends XmlExporter
+{
+	public XmpzXmlExporter2(Project projectToExport)
+	{
+		super(projectToExport);
+	}
+
+	@Override
+	public void exportProject(UnicodeWriter out) throws Exception
+	{
+		
+	}
+}
