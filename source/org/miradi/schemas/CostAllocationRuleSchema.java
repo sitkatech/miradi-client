51d6157fe33053416c8735052975b1a8990494c2
nimaa <nimaa@localhost>
2012-03-27 13:29:55 -0400

    Using new schema class to construct objects.

---
+++ source/org/miradi/schemas/CostAllocationRuleSchema.java
@@ -0,0 +1,41 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.schemas;
+
+import org.miradi.objects.CostAllocationRule;
+
+public class CostAllocationRuleSchema extends BaseObjectSchema
+{
+	public CostAllocationRuleSchema()
+	{
+		super();
+	}
+	
+	@Override
+	protected void fillFieldSchemas()
+	{
+		super.fillFieldSchemas();
+		
+		createFieldSchemaRef(CostAllocationRule.TAG_PARENT_REF);
+		createFieldSchemaRef(CostAllocationRule.TAG_CHILD_REF);
+		createFieldSchemaIntegerField(CostAllocationRule.TAG_COST_PERCENTAGE);
+	}
+}
