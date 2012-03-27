4df5f518c19a70f90b3d8903787f3e3cff802e5e
nimaa <nimaa@localhost>
2012-03-27 17:13:45 -0400

    Using new schema class to construct object

---
+++ source/org/miradi/schemas/RareProjectDataSchema.java
@@ -0,0 +1,49 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.schemas;
+
+import org.miradi.objects.RareProjectData;
+
+public class RareProjectDataSchema extends BaseObjectSchema
+{
+	public RareProjectDataSchema()
+	{
+		super();
+	}
+	
+	@Override
+	protected void fillFieldSchemas()
+	{
+		super.fillFieldSchemas();
+		
+		createFieldSchemaSingleLineUserText(RareProjectData.TAG_FLAGSHIP_SPECIES_COMMON_NAME);
+		createFieldSchemaSingleLineUserText(RareProjectData.TAG_FLAGSHIP_SPECIES_SCIENTIFIC_NAME);
+		createFieldSchemaMultiLineUserText(RareProjectData.TAG_FLAGSHIP_SPECIES_DETAIL);
+		createFieldSchemaMultiLineUserText(RareProjectData.TAG_CAMPAIGN_SLOGAN);
+		createFieldSchemaMultiLineUserText(RareProjectData.TAG_CAMPAIGN_THEORY_OF_CHANGE);
+		createFieldSchemaMultiLineUserText(RareProjectData.TAG_SUMMARY_OF_KEY_MESSAGES);
+		createFieldSchemaSingleLineUserText(RareProjectData.TAG_BIODIVERSITY_HOTSPOTS);
+		createFieldSchemaSingleLineUserText(RareProjectData.TAG_COHORT);
+		createFieldSchemaIntegerField(RareProjectData.TAG_NUMBER_OF_COMMUNITIES_IN_CAMPAIGN_AREA);
+		createFieldSchemaMultiLineUserText(RareProjectData.LEGACY_TAG_THREATS_ADDRESSED_NOTES);
+		createFieldSchemaMultiLineUserText(RareProjectData.TAG_MAIN_ACTIVITIES_NOTES);
+	}
+}
