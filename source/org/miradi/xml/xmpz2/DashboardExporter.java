ead0a6d5467d98ec69aec378dfa3d7f464fa5500
nimaa <nimaa@localhost>
2012-04-05 12:29:27 -0400

    Moved writing of custom field to DashboardExporter

--- source/org/miradi/xml/xmpz2/DashboardExporter.java
+++ source/org/miradi/xml/xmpz2/DashboardExporter.java
@@ -20,9 +20,14 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml.xmpz2;
 
+import org.miradi.dialogs.dashboard.DashboardRowDefinitionManager;
+import org.miradi.objecthelpers.CodeToChoiceMap;
+import org.miradi.objecthelpers.CodeToCodeListMap;
+import org.miradi.objecthelpers.CodeToUserStringMap;
 import org.miradi.objects.BaseObject;
 import org.miradi.objects.Dashboard;
 import org.miradi.schemas.BaseObjectSchema;
+import org.miradi.utils.CodeList;
 
 public class DashboardExporter extends BaseObjectExporter
 {
@@ -34,6 +39,36 @@ public class DashboardExporter extends BaseObjectExporter
 	@Override
 	protected void writeFields(final BaseObject baseObject,	BaseObjectSchema baseObjectSchema) throws Exception
 	{
-		getWriter().writeDashboardSchemaElement((Dashboard)baseObject);
+		writeDashboardSchemaElement((Dashboard)baseObject);
+	}
+	
+	private void writeDashboardSchemaElement(Dashboard dashboard) throws Exception
+	{
+		final CodeToUserStringMap commentsMap = dashboard.getCommentsMap();
+		final CodeToChoiceMap progressMap = dashboard.getProgressChoiceMap();
+		final CodeToCodeListMap flagsMap = dashboard.getFlagsMap();
+
+		DashboardRowDefinitionManager manager = new DashboardRowDefinitionManager();
+		final CodeList allThirdLevelRowCodes = manager.getThirdLevelCodes();
+		final String parentElementName = getWriter().appendParentNameToChildName(DASHBOARD, DASHBOARD_STATUS_ENTRIES);
+		getWriter().writeStartElement(parentElementName);
+		for (String thirdLevelCode : allThirdLevelRowCodes)
+		{
+			final boolean hasCommentsValue = commentsMap.contains(thirdLevelCode);
+			final boolean hasProgressValue = progressMap.contains(thirdLevelCode);
+			final boolean hasFlagValue = flagsMap.contains(thirdLevelCode);
+			if (hasCommentsValue || hasProgressValue || hasFlagValue)
+			{
+				getWriter().writeStartElementWithAttribute(DASHBOARD_STATUS_ENTRY, KEY_ATTRIBUTE_NAME, thirdLevelCode);
+
+				getWriter().writeElement(DASHBOARD_PROGRESS, progressMap.getChoiceCode(thirdLevelCode));
+				getWriter().writeCodeListElement(getWriter().appendParentNameToChildName(DASHBOARD, DASHBOARD_FLAGS), flagsMap.getCodeList(thirdLevelCode));
+				getWriter().writeElement(DASHBOARD_COMMENTS, commentsMap.getUserString(thirdLevelCode));
+
+				getWriter().writeEndElement(DASHBOARD_STATUS_ENTRY);
+			}
+		}
+
+		getWriter().writeEndElement(parentElementName);
 	}
 }
