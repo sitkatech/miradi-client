863ddae97e02fe9a8800868f392beb00bb9562a9
nimaa <nimaa@localhost>
2012-04-05 10:53:37 -0400

    Exporting DateUniteffors.  Increased helper method visibility in writer

---
+++ source/org/miradi/xml/xmpz2/ResourceAssignmentExporter.java
@@ -0,0 +1,86 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.xmpz2;
+
+import org.miradi.objects.Assignment;
+import org.miradi.xml.wcs.XmpzXmlConstants;
+
+public class ResourceAssignmentExporter extends AssignmentExporter
+{
+	public ResourceAssignmentExporter(Xmpz2XmlUnicodeWriter writerToUse)
+	{
+		super(writerToUse);
+	}
+
+	@Override
+	protected String getDateUnitElementName()
+	{
+		return XmpzXmlConstants.WORK_UNITS_DATE_UNIT;
+	}
+	
+	@Override
+	protected String getDayElementName()
+	{
+		return WORK_UNITS_DAY;
+	}
+	
+	@Override
+	protected String getMonthElementName()
+	{
+		return WORK_UNITS_MONTH;
+	}
+	
+	@Override
+	protected String getQuarterElementName()
+	{
+		return WORK_UNITS_QUARTER;
+	}
+	
+	@Override
+	protected String getYearElementName()
+	{
+		return WORK_UNITS_YEAR;
+	}
+	
+	@Override
+	protected String getFullProjectTimespanElementName()
+	{
+		return WORK_UNITS_FULL_PROJECT_TIMESPAN;
+	}
+	
+	@Override
+	protected String getQuantatityElementName()
+	{
+		return XmpzXmlConstants.WORK_UNITS;
+	}
+
+	@Override
+	protected String getDateUnitsElementName()
+	{
+		return Assignment.TAG_DATEUNIT_EFFORTS;
+	}
+	
+	@Override
+	protected String getPoolName()
+	{
+		return EXPENSE_ASSIGNMENT;
+	}
+}
