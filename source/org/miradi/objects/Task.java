4a1f7b2fc5d22bf949d0308d1afc0102d057ab07
nimaa <nimaa@localhost>
2012-04-06 14:34:20 -0400

    Moved more specifiec field answers to subclasses.  getRefListdata and getidListdata can handle when a field does not exist in object.

--- source/org/miradi/objects/Task.java
+++ source/org/miradi/objects/Task.java
@@ -104,12 +104,21 @@ public class Task extends Factor
 		if (tag.equals(TAG_SUBTASK_IDS))
 			return true;
 		
+		if (tag.equals(TAG_RESOURCE_ASSIGNMENT_IDS))
+			return true;
+		
 		return super.isIdListTag(tag);
 	}
 
 	@Override
 	public boolean isRefList(String tag)
 	{
+		if (tag.equals(TAG_EXPENSE_ASSIGNMENT_REFS))
+			return true;
+		
+		if (tag.equals(TAG_PROGRESS_REPORT_REFS))
+			return true;
+
 		return super.isRefList(tag);
 	}
 
