357bfe455c6032b435d5b2e5b7b63b4a6ed6e953
nimaa <nimaa@localhost>
2012-04-11 15:03:12 -0400

    Removed asking if type is exportable or not by adding exporters for all exportable types.

--- source/org/miradi/xml/xmpz2/Xmpz2XmlExporter.java
+++ source/org/miradi/xml/xmpz2/Xmpz2XmlExporter.java
@@ -28,39 +28,47 @@ import org.miradi.objecthelpers.ORefList;
 import org.miradi.objecthelpers.ObjectType;
 import org.miradi.objectpools.EAMObjectPool;
 import org.miradi.objects.BaseObject;
-import org.miradi.objects.ProjectMetadata;
-import org.miradi.objects.TableSettings;
-import org.miradi.objects.ThreatRatingCommentsData;
-import org.miradi.objects.ThreatStressRating;
-import org.miradi.objects.ViewData;
-import org.miradi.objects.Xenodata;
 import org.miradi.project.Project;
+import org.miradi.schemas.AccountingCodeSchema;
+import org.miradi.schemas.AudienceSchema;
 import org.miradi.schemas.BudgetCategoryOneSchema;
 import org.miradi.schemas.BudgetCategoryTwoSchema;
+import org.miradi.schemas.CauseSchema;
 import org.miradi.schemas.ConceptualModelDiagramSchema;
+import org.miradi.schemas.CostAllocationRuleSchema;
 import org.miradi.schemas.DashboardSchema;
 import org.miradi.schemas.DiagramFactorSchema;
 import org.miradi.schemas.DiagramLinkSchema;
 import org.miradi.schemas.ExpenseAssignmentSchema;
-import org.miradi.schemas.FactorLinkSchema;
-import org.miradi.schemas.FosProjectDataSchema;
+import org.miradi.schemas.FundingSourceSchema;
 import org.miradi.schemas.GoalSchema;
+import org.miradi.schemas.GroupBoxSchema;
 import org.miradi.schemas.HumanWelfareTargetSchema;
 import org.miradi.schemas.IndicatorSchema;
+import org.miradi.schemas.IntermediateResultSchema;
 import org.miradi.schemas.IucnRedlistSpeciesSchema;
+import org.miradi.schemas.KeyEcologicalAttributeSchema;
+import org.miradi.schemas.MeasurementSchema;
+import org.miradi.schemas.ObjectTreeTableConfigurationSchema;
 import org.miradi.schemas.ObjectiveSchema;
+import org.miradi.schemas.OrganizationSchema;
+import org.miradi.schemas.OtherNotableSpeciesSchema;
+import org.miradi.schemas.ProgressPercentSchema;
+import org.miradi.schemas.ProgressReportSchema;
 import org.miradi.schemas.ProjectResourceSchema;
-import org.miradi.schemas.RareProjectDataSchema;
+import org.miradi.schemas.ReportTemplateSchema;
 import org.miradi.schemas.ResourceAssignmentSchema;
 import org.miradi.schemas.ResultsChainDiagramSchema;
+import org.miradi.schemas.ScopeBoxSchema;
 import org.miradi.schemas.StrategySchema;
+import org.miradi.schemas.StressSchema;
+import org.miradi.schemas.SubTargetSchema;
 import org.miradi.schemas.TaggedObjectSetSchema;
 import org.miradi.schemas.TargetSchema;
 import org.miradi.schemas.TaskSchema;
-import org.miradi.schemas.TncProjectDataSchema;
-import org.miradi.schemas.WcpaProjectDataSchema;
-import org.miradi.schemas.WcsProjectDataSchema;
-import org.miradi.schemas.WwfProjectDataSchema;
+import org.miradi.schemas.TextBoxSchema;
+import org.miradi.schemas.ThreatReductionResultSchema;
+import org.miradi.schemas.XslTemplateSchema;
 import org.miradi.xml.XmlExporter;
 import org.miradi.xml.wcs.XmpzXmlConstants;
 
@@ -91,69 +99,19 @@ public class Xmpz2XmlExporter extends XmlExporter implements XmpzXmlConstants
 	{
 		for(int objectType = ObjectType.FIRST_OBJECT_TYPE; objectType < ObjectType.OBJECT_TYPE_COUNT; ++objectType)
 		{
-			EAMObjectPool pool = getBaseObjectPoolToExport(objectType);
-			if(pool != null)
-			{
-				final BaseObjectExporter baseObjectExporter = getBaseObjectExporter(objectType);
-				final String containerName = baseObjectExporter.getExporterContainerName(objectType);
-				final String poolName = getWriter().createPoolElementName(containerName);
-				ORefList sortedRefList = pool.getSortedRefList();
-				if (sortedRefList.hasRefs())
-					exportBaseObjects(poolName, sortedRefList, baseObjectExporter);
-			}
+			EAMObjectPool pool = getProject().getPool(objectType);
+			final BaseObjectExporter baseObjectExporter = getBaseObjectExporter(objectType);
+			if (baseObjectExporter == null)
+				continue;
+			
+			final String containerName = baseObjectExporter.getExporterContainerName(objectType);
+			final String poolName = getWriter().createPoolElementName(containerName);
+			ORefList sortedRefList = pool.getSortedRefList();
+			if (sortedRefList.hasRefs())
+				exportBaseObjects(poolName, sortedRefList, baseObjectExporter);
 		}
 	}
 
-	private EAMObjectPool getBaseObjectPoolToExport(final int objectType)
-	{
-		if (objectType == ObjectType.RATING_CRITERION)
-			return null;
-		
-		if (objectType ==  ObjectType.VALUE_OPTION)
-			return null;
-		
-		if (TableSettings.is(objectType))
-			return null;
-		
-		if (ThreatRatingCommentsData.is(objectType))
-			return null;
-		
-		if (ViewData.is(objectType))
-			return null;
-		
-		if (ProjectMetadata.is(objectType))
-			return null;
-
-		if (TncProjectDataSchema.getObjectType() == objectType)
-			return null;
-		
-		if (WwfProjectDataSchema.getObjectType() == objectType)
-			return null;
-		
-		if (WcsProjectDataSchema.getObjectType() == objectType)
-			return null;
-		
-		if (RareProjectDataSchema.getObjectType() == objectType)
-			return null;
-
-		if (FosProjectDataSchema.getObjectType() == objectType)
-			return null;
-		
-		if (WcpaProjectDataSchema.getObjectType() == objectType)
-			return null;
-		
-		if (FactorLinkSchema.getObjectType() == objectType)
-			return null;
-		
-		if (ThreatStressRating.is(objectType))
-			return null;
-		
-		if (Xenodata.is(objectType))
-			return null;
-		
-		return getProject().getPool(objectType);
-	}
-	
 	private void exportBaseObjects(final String poolName, ORefList sortedRefList, final BaseObjectExporter baseObjectExporter) throws Exception
 	{
 		getWriter().writeStartElement(poolName);
@@ -187,14 +145,33 @@ public class Xmpz2XmlExporter extends XmlExporter implements XmpzXmlConstants
 		objectTypeToExporterMap.put(IucnRedlistSpeciesSchema.getObjectType(), new IucnRedlistSpeciesExporter(getWriter()));
 		objectTypeToExporterMap.put(BudgetCategoryOneSchema.getObjectType(), new BudgetCategoryOneExporter(getWriter()));
 		objectTypeToExporterMap.put(BudgetCategoryTwoSchema.getObjectType(), new BudgetCategoryTwoExporter(getWriter()));
+		
+		objectTypeToExporterMap.put(AccountingCodeSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(FundingSourceSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(KeyEcologicalAttributeSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(CauseSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(IntermediateResultSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(ThreatReductionResultSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(TextBoxSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(ObjectTreeTableConfigurationSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(CostAllocationRuleSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(MeasurementSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(StressSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(GroupBoxSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(SubTargetSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(ProgressReportSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(OrganizationSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(ProgressPercentSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(ReportTemplateSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(ScopeBoxSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(OtherNotableSpeciesSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(AudienceSchema.getObjectType(), new BaseObjectExporter(getWriter()));
+		objectTypeToExporterMap.put(XslTemplateSchema.getObjectType(), new BaseObjectExporter(getWriter()));
 	}
 	
 	private BaseObjectExporter getBaseObjectExporter(final int objectType)
 	{
-		if (objectTypeToExporterMap.containsKey(objectType))
-			return objectTypeToExporterMap.get(objectType);
-		
-		return new BaseObjectExporter(getWriter());
+		return objectTypeToExporterMap.get(objectType);
 	}
 	
 	private Xmpz2XmlUnicodeWriter getWriter()
