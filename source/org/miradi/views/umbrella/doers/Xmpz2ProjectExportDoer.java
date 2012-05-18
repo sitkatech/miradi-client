964349d5ab49eab3dc2ea0a8f07ad0bb1591cd0e
nimaa <nimaa@localhost>
2012-05-18 12:14:35 -0400

    MRD-4909 Ui parts but not working (need to do some refactoring before finishing doer)

---
+++ source/org/miradi/views/umbrella/doers/Xmpz2ProjectExportDoer.java
@@ -0,0 +1,46 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.views.umbrella.doers;
+
+import org.miradi.utils.MiradiFileSaveChooser;
+import org.miradi.utils.Xmpz2ZipFileChooser;
+
+//FIXME under construction
+public class Xmpz2ProjectExportDoer extends AbstractExportProjectXmlZipDoer
+{
+	@Override
+	protected MiradiFileSaveChooser createFileChooser()
+	{
+		return new Xmpz2ZipFileChooser(getMainWindow());
+	}
+
+//	@Override
+//	protected XmlExporter createExporter() throws Exception
+//	{
+//		return new Xmpz2XmlExporter(getProject(), createWriter());
+//	}
+//	
+//	@Override
+//	protected boolean isValidXml(ByteArrayInputStream inputStream) throws Exception
+//	{
+//		return new WcsMiradiXmlValidator().isValid(inputStream);
+//	}
+}
