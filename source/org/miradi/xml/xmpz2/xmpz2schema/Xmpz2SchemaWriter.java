b74121e4c7c2540dd54ce3e60d5cab1c4765c51b
nimaa <nimaa@localhost>
2012-06-12 20:43:37 -0400

    Using existing method to create schema element.

--- source/org/miradi/xml/xmpz2/xmpz2schema/Xmpz2SchemaWriter.java
+++ source/org/miradi/xml/xmpz2/xmpz2schema/Xmpz2SchemaWriter.java
@@ -97,6 +97,6 @@ public class Xmpz2SchemaWriter extends SchemaWriter implements Xmpz2XmlConstants
 		
 	public String createSchemaElement(String parentElementName, final String elementName)
 	{
-		return ELEMENT_NAME + PREFIX + parentElementName + " { " + elementName + "* }";
+		return createSchemaElement5(parentElementName, elementName + "*");
 	}
 }
