93ca1faf727204c1620bf07443773f14e2afc472
nimaa <nimaa@localhost>
2012-06-13 14:53:07 -0400

    Using existing var

--- source/org/miradi/xml/xmpz2/xmpz2schema/Xmpz2XmlSchemaCreator.java
+++ source/org/miradi/xml/xmpz2/xmpz2schema/Xmpz2XmlSchemaCreator.java
@@ -736,7 +736,7 @@ public class Xmpz2XmlSchemaCreator implements Xmpz2XmlConstants
 	
 	private void defineElement(String elementName, Vector<String> subElements)
 	{
-		getSchemaWriter().defineAlias(createElementName(elementName), "element miradi:" + elementName);
+		getSchemaWriter().defineAlias(createElementName(elementName), "element " + PREFIX + elementName);
 		getSchemaWriter().startBlock();
 		getSchemaWriter().defineElements(subElements);
 		getSchemaWriter().endBlock();
