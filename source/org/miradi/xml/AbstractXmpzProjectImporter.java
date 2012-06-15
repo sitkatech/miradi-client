d9ca62bda0828e448b1b22ac12a211d5b03506a3
nimaa <nimaa@localhost>
2012-06-15 11:40:20 -0400

    Pulled up duplicate methods.

--- source/org/miradi/xml/AbstractXmpzProjectImporter.java
+++ source/org/miradi/xml/AbstractXmpzProjectImporter.java
@@ -20,7 +20,16 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+import org.miradi.exceptions.ValidationException;
 import org.miradi.main.MainWindow;
+import org.miradi.project.Project;
+import org.miradi.project.ProjectSaver;
+import org.miradi.utils.ProgressInterface;
 import org.miradi.views.umbrella.AbstractZippedXmlImporter;
 
 abstract public class AbstractXmpzProjectImporter extends AbstractZippedXmlImporter
@@ -28,5 +37,31 @@ abstract public class AbstractXmpzProjectImporter extends AbstractZippedXmlImpor
 	public AbstractXmpzProjectImporter(MainWindow mainWindowToUse)
 	{
 		super(mainWindowToUse);
+	}
+	
+	@Override
+	protected void createOrOpenProject(Project projectToFill, File projectFile) throws Exception
+	{
+		projectToFill.rawCreateorOpen();
+	}
+	
+	@Override
+	protected void createProject(File importFile, File homeDirectory, File newProjectFile, ProgressInterface progressIndicator) throws Exception
+	{
+		Project project = importProject(importFile, progressIndicator);
+		ProjectSaver.saveProject(project, newProjectFile);
+	}
+	
+	private Project importProject(File zipFileToImport, ProgressInterface progressIndicator) throws ZipException, IOException, Exception, ValidationException
+	{
+		ZipFile zipFile = new ZipFile(zipFileToImport);
+		try
+		{
+			return importProjectFromXmlEntry(zipFile, progressIndicator);
+		}
+		finally
+		{
+			zipFile.close();
+		}
 	}
 }
