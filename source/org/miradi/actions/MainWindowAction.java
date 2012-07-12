3dcd8784690cc987ab0bcfc5042a69933e1c8298
nimaa <nimaa@localhost>
2012-07-12 15:58:42 -0400

    Removed empty space.

--- source/org/miradi/actions/MainWindowAction.java
+++ source/org/miradi/actions/MainWindowAction.java
@@ -84,10 +84,9 @@ public abstract class MainWindowAction extends MiradiAction
 			if(lastCommand == null)
 				EAM.friendlyInternalError("Attempted to execute command as side effect before any command had been executed ");
 
-			EAM.friendlyInternalError(
-					"Attempt to execute command while in side effect mode:\n" +
-							" tried  " + e.getCommand().toString() + "\n" +
-							"  within " + lastCommand.toString()
+			EAM.friendlyInternalError("Attempt to execute command while in side effect mode:\n" +
+									  " tried  " + e.getCommand().toString() + "\n" +
+									  " within " + lastCommand.toString()
 			);
 		}
 		catch (Exception e)
