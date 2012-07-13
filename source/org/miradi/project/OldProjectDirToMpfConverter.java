ce155ad908b4de68cb7746a5a4b75de3ea80d148
nimaa <nimaa@localhost>
2012-07-13 12:17:33 -0400

    No longer passing ProjectName to compare method. Using directories name instead.  Unit test has to ignore  "__MACOSX" dir due to mac specific zipping issues.

--- source/org/miradi/project/OldProjectDirToMpfConverter.java
+++ source/org/miradi/project/OldProjectDirToMpfConverter.java
@@ -34,7 +34,7 @@ public class OldProjectDirToMpfConverter
 	public static File convert(MainWindow mainWindow, File oldProjectDirectory) throws Exception
 	{
 		File oldProjectZipped = ZipUtilities.createZipFromDirectory(oldProjectDirectory);
-		if (!ZipUtilities.doesProjectZipContainAllProjectFiles(new ZipFile(oldProjectZipped), oldProjectDirectory, oldProjectDirectory.getName()))
+		if (!ZipUtilities.doesProjectZipContainAllProjectFiles(new ZipFile(oldProjectZipped), oldProjectDirectory))
 		{
 			final String errorMessage = EAM.text("Mpz to Mpf data conversion failed");
 			EAM.errorDialog(errorMessage);
