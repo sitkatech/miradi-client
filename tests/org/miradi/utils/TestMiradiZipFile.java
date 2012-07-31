69d7d097fb8312e4fe9440fd0d102c47a6abcea4
nimaa <nimaa@localhost>
2012-07-31 17:26:36 -0400

    Changed methods to be static and added unit tests for the methods.

---
+++ tests/org/miradi/utils/TestMiradiZipFile.java
@@ -0,0 +1,53 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.utils;
+
+import org.miradi.main.MiradiTestCase;
+
+public class TestMiradiZipFile extends MiradiTestCase
+{
+	public TestMiradiZipFile(String name)
+	{
+		super(name);
+	}
+	
+	public void testAttemptUsingReversedPathSeparator()
+	{
+		verifyAttemptUsingReversedPathSeparator("/", "\\");
+		verifyAttemptUsingReversedPathSeparator("\\", "/");
+	}
+	
+	private void verifyAttemptUsingReversedPathSeparator(String expected, String actual)
+	{
+		assertEquals("incorrect reversed path seperator", expected, MiradiZipFile.attemptUsingReversedPathSeparator(actual));
+	}
+
+	public void testReplaceWithOtherPossibleLeadingChar()
+	{
+		verifyReplaceWithOtherPossibleLeadingChar("/", "");
+		verifyReplaceWithOtherPossibleLeadingChar("", "/");
+	}
+
+	private void verifyReplaceWithOtherPossibleLeadingChar(String otherExpectedLeadingChar, String leadingChar)
+	{
+		assertEquals("leading char was not replaced with other option?", otherExpectedLeadingChar, MiradiZipFile.replaceWithOtherPossibleLeadingChar(leadingChar));
+	}
+}
