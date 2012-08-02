d412983c433dd0414ff333a979d8288dd1efe791
nimaa <nimaa@localhost>
2012-08-02 16:10:01 -0400

    Var renamed

--- source/org/miradi/utils/FileUtilities.java
+++ source/org/miradi/utils/FileUtilities.java
@@ -180,5 +180,5 @@ public class FileUtilities
 	
 	public static final String SEPARATOR = "/";
 	public static final String BACKWARD_SLASH = "\\";
-	public static final String DOUBLE_BACKWARD_SLASH = "\\\\";
+	public static final String REGULAR_EXPRESSION_BACKSLASH = "\\\\";
 }
