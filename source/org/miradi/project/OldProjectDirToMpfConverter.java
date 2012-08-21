36c87210f43742da4ad98f396bfa55165acdc5f0
kevins <kevins@localhost>
2012-08-21 14:18:48 -0400

    MRD-4967: Notify user before migrating projects to 4.0 format

--- source/org/miradi/project/OldProjectDirToMpfConverter.java
+++ source/org/miradi/project/OldProjectDirToMpfConverter.java
@@ -38,8 +38,28 @@ public class OldProjectDirToMpfConverter
 	public static File convert(MainWindow mainWindow, File oldProjectDirectory) throws Exception
 	{
 		File backupFolder = new File(EAM.getHomeDirectory(), FileSystemTreeNode.BACKUP_FOLDER_NAME);
-		backupFolder.mkdirs();
+		String body = EAM.text(
+			"The selected project was created with an older version of Miradi.<br/>" + 
+			"In order to use it with this version of Miradi, the data will have <br/>" +
+			"to be migrated. This is safe and automatic, but after the conversion <br/>" +
+			"has been done, Miradi 3.X will no longer be able to use this file.<br/>" +
+			"<br/>" +
+			"If you continue, you will still be able to export the project as <br/>" +
+			"a Miradi Project Zip (MPZ) file, which can be imported by Miradi 3.3. <br/>" +
+			"<br/>" +
+			"In the unlikely event that you need to revert to Miradi 3.3, a <br/>" +
+			"backup copy of the project, in MPZ format, will be created in the a <br/>" +
+			"directory named: %DIRECTORY%<br/>" +
+			"<br/>" +
+			"Do you want to go ahead and migrate and open this project?"
+		);
+		body = EAM.substitute(body, "%DIRECTORY%", backupFolder.getAbsolutePath());
+		String[] buttons = new String[] { EAM.text("Button|Migrate"), EAM.text("Button|Cancel")};
+		int result = EAM.confirmDialog(EAM.text("Title|Migrate Project"), "<html>" + body, buttons);
+		if(result != 0)
+			return null;
 		
+		backupFolder.mkdirs();
 		File oldProjectZippedAsBackup = new File(backupFolder, "v3-Backup-" + oldProjectDirectory.getName() + "-" + Calendar.getInstance().getTimeInMillis() + MpzFileFilter.EXTENSION);
 		if (oldProjectZippedAsBackup.exists())
 			throw new Exception("Attempted to override an existing backup file when converting old project dir to new project format:" + oldProjectZippedAsBackup.getAbsolutePath());
