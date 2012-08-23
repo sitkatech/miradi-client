0c293d870045ad8c5e1f4f7b7cf6977e36850f12
nimaa <nimaa@localhost>
2012-08-23 15:01:28 -0400

    MRD-5000: Setting the file filter everytime we add a file filter.  THis change is needed due to java 7.

--- source/org/miradi/utils/AbstractFileChooser.java
+++ source/org/miradi/utils/AbstractFileChooser.java
@@ -71,10 +71,7 @@ abstract public class AbstractFileChooser
 	private void addFileFilters(JFileChooser dialog)
 	{
 		FileFilter[] filters = getFileFilter();
-		for (int index = 0; index < filters.length; ++index)
-		{
-			dialog.addChoosableFileFilter(filters[index]);
-		}
+		addFileFilters(dialog, filters);
 	}
 	
 	public static File getFileWithExtension(JFileChooser fileChooser, File file)
