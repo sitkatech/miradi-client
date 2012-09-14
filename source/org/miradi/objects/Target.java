a564bec3112a6fc9c34813d7e40029c4dac662d3
nimaa <nimaa@localhost>
2012-09-14 10:02:38 -0400

    Bug fix: target did now own subtargets/stresses.  Added failing test.  Targets now own stresses and subtargets.

--- source/org/miradi/objects/Target.java
+++ source/org/miradi/objects/Target.java
@@ -23,6 +23,7 @@ package org.miradi.objects;
 import org.miradi.ids.FactorId;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
+import org.miradi.objecthelpers.ObjectType;
 import org.miradi.project.ObjectManager;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceItem;
@@ -75,6 +76,19 @@ public class Target extends AbstractTarget
 		}
 		
 		return appendedChoiceValues.toString();
+	}
+	
+	@Override
+	public ORefList getOwnedObjects(int objectType)
+	{
+		ORefList list = super.getOwnedObjects(objectType);
+		switch(objectType)
+		{
+			case ObjectType.STRESS:
+				list.addAll(getStressRefs());
+		}
+		
+		return list;
 	}
 	
 	public ORefList getStressRefs()
