cff75b70b985ce128266feb2c5357756c264e68c
kevins <kevins@localhost>
2012-09-19 16:40:58 -0400

    Bump language pack version number to 4.0

--- source/org/miradi/main/Miradi.java
+++ source/org/miradi/main/Miradi.java
@@ -300,7 +300,7 @@ public class Miradi
 		private String[] commandLineArgs;
 	}
 
-	public static final String MAIN_VERSION = "3.3";
+	public static final String MAIN_VERSION = "4.0";
 	public static final String LANGUAGE_PACK_PREFIX = "MiradiContent-" + MAIN_VERSION + "-";
 	private static final String ALPHA_TESTER_MODE_ON_SWITCH = "--tester";
 	
