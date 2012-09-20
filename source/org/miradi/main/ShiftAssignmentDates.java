444db765a50087a4ea27cda896684ca272bd9267
kevins <kevins@localhost>
2012-09-20 11:24:13 -0400

    Update ShiftAssignmentDates utility so it works with new .Miradi file format

--- source/org/miradi/main/ShiftAssignmentDates.java
+++ source/org/miradi/main/ShiftAssignmentDates.java
@@ -20,15 +20,18 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.main;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.util.Vector;
 
+import org.martus.util.MultiCalendar;
 import org.miradi.objecthelpers.DateUnit;
 import org.miradi.objectpools.EAMObjectPool;
 import org.miradi.objects.Assignment;
 import org.miradi.objects.BaseObject;
 import org.miradi.project.Project;
 import org.miradi.project.ProjectLoader;
+import org.miradi.project.ProjectSaver;
 import org.miradi.schemas.ExpenseAssignmentSchema;
 import org.miradi.schemas.ResourceAssignmentSchema;
 import org.miradi.utils.DateUnitEffort;
@@ -61,9 +64,13 @@ public class ShiftAssignmentDates
 		showAllAssignmentDates(project, ResourceAssignmentSchema.getObjectType());
 		showAllAssignmentDates(project, ExpenseAssignmentSchema.getObjectType());
 		
-// Methods that write to the file are commented out for safety. They do work.
-//		shiftAssignmentDatesOneYearLater(project, ResourceAssignment.getObjectType());
-//		shiftAssignmentDatesOneYearLater(project, ExpenseAssignment.getObjectType());
+		shiftAssignmentDatesOneYearLater(project, ResourceAssignmentSchema.getObjectType());
+		shiftAssignmentDatesOneYearLater(project, ExpenseAssignmentSchema.getObjectType());
+
+		String newProjectName = "Shifted" + projectFile.getName();
+		File newProjectFile = new File(projectFile.getParentFile(), newProjectName);
+		ProjectSaver.saveProject(project, newProjectFile);
+		System.out.println("Saved as " + newProjectName);
 	}
 
 	private static void showAllAssignmentDates(Project project, int objectType) throws Exception
@@ -78,18 +85,18 @@ public class ShiftAssignmentDates
 		}
 	}
 
-//	private static void shiftAssignmentDatesOneYearLater(Project project, int objectType) throws Exception
-//	{
-//		EAMObjectPool pool = project.getPool(objectType);
-//		Vector<BaseObject> objects = pool.getAllObjects();
-//		for(BaseObject object : objects)
-//		{
-//			Assignment assignment = (Assignment) object;
-//			DateUnitEffortList duel = assignment.getDateUnitEffortList();
-//			DateUnitEffortList shifted = getShiftedOneYearLater(duel);
-//			project.setObjectData(assignment, Assignment.TAG_DATEUNIT_EFFORTS, shifted.toString());
-//		}
-//	}
+	private static void shiftAssignmentDatesOneYearLater(Project project, int objectType) throws Exception
+	{
+		EAMObjectPool pool = project.getPool(objectType);
+		Vector<BaseObject> objects = pool.getAllObjects();
+		for(BaseObject object : objects)
+		{
+			Assignment assignment = (Assignment) object;
+			DateUnitEffortList duel = assignment.getDateUnitEffortList();
+			DateUnitEffortList shifted = getShiftedOneYearLater(duel);
+			project.setObjectData(assignment, Assignment.TAG_DATEUNIT_EFFORTS, shifted.toString());
+		}
+	}
 
 	private static void showDates(DateUnitEffortList duel)
 	{
@@ -104,19 +111,19 @@ public class ShiftAssignmentDates
 		}
 	}
 
-//	private static DateUnitEffortList getShiftedOneYearLater(DateUnitEffortList duel)
-//	{
-//		DateUnitEffortList shifted = new DateUnitEffortList();
-//		for(int index = 0; index < duel.size(); ++index)
-//		{
-//			DateUnitEffort due = duel.getDateUnitEffort(index);
-//			DateUnit du = due.getDateUnit();
-//			DateUnit shiftedDateUnit = getShifted(du);
-//			shifted.add(new DateUnitEffort(shiftedDateUnit, due.getQuantity()));
-//		}
-//		
-//		return shifted;
-//	}
+	private static DateUnitEffortList getShiftedOneYearLater(DateUnitEffortList duel)
+	{
+		DateUnitEffortList shifted = new DateUnitEffortList();
+		for(int index = 0; index < duel.size(); ++index)
+		{
+			DateUnitEffort due = duel.getDateUnitEffort(index);
+			DateUnit du = due.getDateUnit();
+			DateUnit shiftedDateUnit = getShifted(du);
+			shifted.add(new DateUnitEffort(shiftedDateUnit, due.getQuantity()));
+		}
+		
+		return shifted;
+	}
 
 	private static DateUnit getShifted(DateUnit du)
 	{
@@ -135,6 +142,23 @@ public class ShiftAssignmentDates
 			String quarter = code.substring(4);
 			year = Integer.toString(new Integer(year) + 1);
 			return new DateUnit(year + quarter);
+		}
+		if(du.isMonth())
+		{
+			int year = du.getYear();
+			int month = du.getMonth();
+			++year;
+			MultiCalendar cal = MultiCalendar.createFromGregorianYearMonthDay(year, month, 1);
+			return DateUnit.createMonthDateUnit(cal.toIsoDateString());
+		}
+		if(du.isDay())
+		{
+			int year = du.getYear();
+			int month = du.getMonth();
+			int day = du.getDay();
+			++year;
+			MultiCalendar cal = MultiCalendar.createFromGregorianYearMonthDay(year, month, day);
+			return DateUnit.createDayDateUnit(cal.toIsoDateString());
 		}
 		return null;
 	}
