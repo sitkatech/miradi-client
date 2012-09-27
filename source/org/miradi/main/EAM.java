2910dd3e1b641c60a8a0a85f61d073ad70dd09f8
kevins <kevins@localhost>
2012-09-27 13:20:22 -0400

    MRD-5067: Export to MPZ doesn't log/save exceptions (wasn't forcing immediate project save)

--- source/org/miradi/main/EAM.java
+++ source/org/miradi/main/EAM.java
@@ -278,6 +278,7 @@ public class EAM
 		try
 		{	
 			project.appendToExceptionLog(convertExceptionToString(exceptionToAppend));
+			mainWindow.getProjectSaver().safeSave();
 		}
 		catch (Exception e)
 		{
