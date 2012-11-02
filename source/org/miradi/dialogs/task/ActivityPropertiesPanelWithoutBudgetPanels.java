c1af197946ed1b715d896a9447b18c78cdacdbd1
nimaa <nimaa@localhost>
2012-11-02 14:39:05 -0400

    Backed out of changeset 29149:12c03970eae1

---
+++ source/org/miradi/dialogs/task/ActivityPropertiesPanelWithoutBudgetPanels.java
@@ -0,0 +1,52 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.task;
+
+import org.miradi.dialogs.base.ObjectDataInputPanel;
+import org.miradi.main.EAM;
+import org.miradi.main.MainWindow;
+import org.miradi.views.umbrella.ObjectPicker;
+
+public class ActivityPropertiesPanelWithoutBudgetPanels extends TaskPropertiesPanelWithoutBudgetPanels
+{
+	public ActivityPropertiesPanelWithoutBudgetPanels(MainWindow mainWindow, ObjectPicker objectPickerToUse) throws Exception
+	{
+		super(mainWindow, objectPickerToUse);
+	}
+	
+	@Override
+	protected ObjectDataInputPanel createDetailsPanel(MainWindow mainWindow) throws Exception
+	{
+		return new ActivityDetailsPanelWithRelevancyOverrideFields(getProject(), mainWindow.getActions());
+	}
+	
+	@Override
+	protected boolean shouldHaveVisibilityPanel()
+	{
+		return true;
+	}
+	
+	@Override
+	public String getPanelDescription()
+	{
+		return EAM.text("Title|Activity Properties");
+	}
+}
---
+++ source/org/miradi/xml/xmpz2/objectExporters/IndicatorExporter.java
@@ -0,0 +1,97 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.xmpz2.objectExporters;
+
+import org.miradi.objecthelpers.CodeToUserStringMap;
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.Indicator;
+import org.miradi.questions.ChoiceQuestion;
+import org.miradi.questions.StatusQuestion;
+import org.miradi.schemas.BaseObjectSchema;
+import org.miradi.schemas.IndicatorSchema;
+import org.miradi.utils.CodeList;
+import org.miradi.xml.xmpz2.BaseObjectExporter;
+import org.miradi.xml.xmpz2.Xmpz2XmlWriter;
+
+public class IndicatorExporter extends BaseObjectExporter
+{
+	public IndicatorExporter(Xmpz2XmlWriter writerToUse)
+	{
+		super(writerToUse, IndicatorSchema.getObjectType());
+	}
+
+	@Override
+	protected void writeFields(final BaseObject baseObject,	BaseObjectSchema baseObjectSchema) throws Exception
+	{
+		super.writeFields(baseObject, baseObjectSchema);
+		
+		final Indicator indicator = (Indicator) baseObject;
+		writeMethodRefs(baseObjectSchema, indicator);
+		writeThreshold(indicator);
+	}
+
+	private void writeMethodRefs(BaseObjectSchema baseObjectSchema, final Indicator indicator) throws Exception
+	{
+		getWriter().writeReflist(baseObjectSchema.getObjectName() + METHOD_IDS, METHOD, indicator.getMethodRefs());
+	}
+
+	@Override
+	protected boolean doesFieldRequireSpecialHandling(final String tag)
+	{
+		if (tag.equals(Indicator.TAG_THRESHOLDS_MAP))
+			return true;
+		
+		if (tag.equals(Indicator.TAG_THRESHOLD_DETAILS_MAP))
+			return true;
+		
+		if (tag.equals(Indicator.TAG_METHOD_IDS))
+			return true;
+		
+		return super.doesFieldRequireSpecialHandling(tag);
+	}
+	
+	private void writeThreshold(Indicator indicator) throws Exception
+	{
+		CodeToUserStringMap thresholdValues = indicator.getThresholdsMap().getCodeToUserStringMap();
+		CodeToUserStringMap thresholdDetails = indicator.getThresholdDetailsMap();
+		if (thresholdValues.size() == 0 && thresholdDetails.size() == 0)
+			return;
+		
+		final String elementName = getWriter().appendChildNameToParentName(INDICATOR, THRESHOLDS);
+		getWriter().writeStartElement(elementName);
+		ChoiceQuestion question = getWriter().getProject().getQuestion(StatusQuestion.class);
+		CodeList allCodes = question.getAllCodes();
+		for(int index = 0; index < allCodes.size(); ++index)
+		{
+			String code = allCodes.get(index);
+			if (code.equals(StatusQuestion.UNSPECIFIED))
+				continue;
+			
+			getWriter().writeStartElement(THRESHOLD);
+			getWriter().writeElement(STATUS_CODE, code);
+			getWriter().writeElement(THRESHOLD_VALUE, thresholdValues.getUserString(code));
+			getWriter().writeElement(THRESHOLD_DETAILS, thresholdDetails.getUserString(code));
+			getWriter().writeEndElement(THRESHOLD);			
+		}
+		
+		getWriter().writeEndElement(elementName);
+	}
+}
