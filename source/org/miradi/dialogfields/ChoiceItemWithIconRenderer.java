cc7bb1dca65f93d90f1205bdb51cb0a6e00eb0fa
nimaa <nimaa@localhost>
2012-11-21 15:04:15 -0500

    Pulled up duplicate method, removed duplicated method.

--- source/org/miradi/dialogfields/ChoiceItemWithIconRenderer.java
+++ source/org/miradi/dialogfields/ChoiceItemWithIconRenderer.java
@@ -20,25 +20,12 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.dialogfields;
 
-import java.awt.Component;
-
 import javax.swing.Icon;
-import javax.swing.JList;
 
 import org.miradi.questions.ChoiceItem;
 
 public class ChoiceItemWithIconRenderer extends AbstractChoiceItemRenderer
 {
-	@Override
-	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) 
-	{
-		Component cell = super.getListCellRendererComponent(list, value, index, isSelected,	cellHasFocus);
-		ChoiceItem thisOption = (ChoiceItem)value;
-		if (value!=null)
-			setIcon(getIcon(thisOption));
-		return cell;
-	}
-
 	@Override
 	public Icon getIcon(ChoiceItem thisOption)
 	{
