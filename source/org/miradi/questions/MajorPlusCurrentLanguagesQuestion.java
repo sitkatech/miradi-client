1680fb4557e1b5cde957ffc1fb67d5248359d5eb
nimaa <nimaa@localhost>
2012-12-04 13:58:58 -0500

    No longer including UI language code,  since not all machines have the same codes.

--- source/org/miradi/questions/MajorPlusCurrentLanguagesQuestion.java
+++ source/org/miradi/questions/MajorPlusCurrentLanguagesQuestion.java
@@ -1,25 +1,5 @@
 package org.miradi.questions;
 
-import org.miradi.utils.Translation;
-/* 
-Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
-(on behalf of the Conservation Measures Partnership, "CMP") and 
-Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
-
-This file is part of Miradi
-
-Miradi is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3, 
-as published by the Free Software Foundation.
-
-Miradi is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
- */
 
 public class MajorPlusCurrentLanguagesQuestion extends AbstractLanguagesQuestion
 {
@@ -27,9 +7,6 @@ public class MajorPlusCurrentLanguagesQuestion extends AbstractLanguagesQuestion
 	protected boolean shouldIncludeLanguage(String threeLetterCode, String oldThreeLetterCode, String twoLetterCode)
 	{
 		if(oldThreeLetterCode.length() > 0)
-			return true;
-		
-		if(threeLetterCode.equals(Translation.getCurrentLanguageCode()))
 			return true;
 		
 		return false;
