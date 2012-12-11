9dbc670ef20eb4d2e0731151337ed6bd29316a54
nimaa <nimaa@localhost>
2012-12-11 14:53:17 -0500

    MRD-5182: Combined first dialog text with new text and used it in one poup dialog.

--- source/org/miradi/views/umbrella/XmlExporterDoer.java
+++ source/org/miradi/views/umbrella/XmlExporterDoer.java
@@ -49,9 +49,15 @@ abstract public class XmlExporterDoer extends AbstractFileSaverDoer
 	{
 		String title = EAM.text("Export Project (BETA)");
 		String[] body = new String[] {
-			EAM.text("This feature is not yet fully supported. " +
-			"It exports all the project data, but the XML schema is still in flux. " +
-			"Future versions of Miradi will export the data in different formats."),
+			EAM.text("The XMPZ format is only supported for compatibility with Miradi 3.3. " +
+					"It does not support some of the newer features of Miradi 4.0, so exporting " +
+					"your project to the XMPZ format may lose data. If you just " +
+					"want a backup copy of your project, or if you want to send a copy to " +
+					"someone who uses Miradi 4.0, you should either export to the XMPZ2 format, " +
+					"or just use the .Miradi project file directly.  " +
+					"NOTE: This data format can be for sending to other systems or creating reports. " +
+					"It cannot be used to transfer data from one copy or version of Miradi to another.\n" +
+					"Do you want to continue with XMPZ export that may lose data?"),
 		};
 
 		String[] buttons = new String[] {
