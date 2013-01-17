fd36f007a1f5934999fa769eb48e2e7d7b213c9a
nimaa <nimaa@localhost>
2013-01-17 14:48:17 -0500

    Pushed down  variable

--- source/org/miradi/main/TransferableMiradiList.java
+++ source/org/miradi/main/TransferableMiradiList.java
@@ -311,8 +311,6 @@ abstract public class TransferableMiradiList implements Transferable, Serializab
 	
 	abstract public DataFlavor[] getTransferDataFlavors();
 		
-	public static DataFlavor miradiListDataFlavor = new DataFlavor(TransferableMiradiList.class, "Miradi Objects");
-
 	private Project project;
 	
 	private ORef diagramObjectRefCopiedFrom;
