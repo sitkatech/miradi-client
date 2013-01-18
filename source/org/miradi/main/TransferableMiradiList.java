bb9c1c9a0500bc550f8a9e55d26ff493e3514127
nimaa <nimaa@localhost>
2013-01-18 18:24:53 -0500

    Backed out of changeset 29662:b346e19306a9

--- source/org/miradi/main/TransferableMiradiList.java
+++ source/org/miradi/main/TransferableMiradiList.java
@@ -306,8 +306,6 @@ public class TransferableMiradiList implements Transferable, Serializable
 	}
 
 	// TODO: Is there a way to avoid suppressing warnings here?
-	//try new Vector((Vector<String>) objectIn.readObject());
-	//but it needs testing since it would no longer pass by reference
 	@SuppressWarnings("unchecked")
 	private Vector<String> readStringVector(ObjectInputStream objectIn)
 			throws IOException, ClassNotFoundException
