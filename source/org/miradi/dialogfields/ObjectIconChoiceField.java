b1113a857e9701f18fa099f8ee65d46b1b6999fc
nimaa <nimaa@localhost>
2013-01-21 10:50:45 -0500

    Backed out of changeset 29668:121297a9f7fd

---
+++ source/org/miradi/dialogfields/ObjectIconChoiceField.java
@@ -0,0 +1,36 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.dialogfields;
+
+
+
+import org.miradi.ids.BaseId;
+import org.miradi.project.Project;
+import org.miradi.questions.ChoiceQuestion;
+
+public class ObjectIconChoiceField extends ObjectChoiceField
+{
+	public ObjectIconChoiceField(Project projectToUse, int objectType, BaseId objectId, String tagToUse, ChoiceQuestion questionToUse)
+	{
+		super(projectToUse, objectType, objectId, tagToUse, questionToUse);
+		
+		combo.setRenderer(new ChoiceItemWithIconRenderer());
+	}
+}
---
+++ source/org/miradi/utils/ReadonlyHtmlPane.java
@@ -0,0 +1,43 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.utils;
+
+import javax.swing.text.html.StyleSheet;
+
+import org.miradi.main.EAM;
+import org.miradi.main.MainWindow;
+
+public class ReadonlyHtmlPane extends AbstractHtmlPane
+{
+	public ReadonlyHtmlPane(MainWindow mainWindow, int fixedApproximateColumnCount, int initialApproximateRowCount)
+	{
+		super(mainWindow, fixedApproximateColumnCount, initialApproximateRowCount);
+	}
+	
+	@Override
+	protected void customizeStyleSheet(StyleSheet style)
+	{
+		final int fontSize = getMainWindow().getWizardFontSize();
+		HtmlUtilities.addRuleFontSize(style, getFont().getSize(), fontSize);
+		HtmlUtilities.addRuleFontFamily(style, getMainWindow().getDataPanelFontFamily());
+		HtmlUtilities.addFontColor(style, EAM.READONLY_FOREGROUND_COLOR);
+	}
+}
