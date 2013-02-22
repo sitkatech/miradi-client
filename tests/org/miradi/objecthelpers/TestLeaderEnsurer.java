7eb91e7d6563db5105fd9d12e1d836016662d4d4
nimaa <nimaa@localhost>
2013-02-22 17:27:33 -0500

    Updated leader ensurer to handle case:  when removing ResourceAssignment from its referrer.

--- tests/org/miradi/objecthelpers/TestLeaderEnsurer.java
+++ tests/org/miradi/objecthelpers/TestLeaderEnsurer.java
@@ -20,6 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.objecthelpers;
 
+import org.miradi.commands.CommandSetObjectData;
 import org.miradi.main.TestCaseWithProject;
 import org.miradi.objects.ResourceAssignment;
 import org.miradi.objects.Strategy;
@@ -38,6 +39,16 @@ public class TestLeaderEnsurer extends TestCaseWithProject
 		getProject().fillObjectUsingCommand(strategy, Strategy.TAG_LEADER_RESOURCE, resourceAssignment.getResourceRef());
 		assertEquals("leader not set?", resourceAssignment.getResourceRef(), strategy.getLeaderResourceRef());
 		getProject().fillObjectUsingCommand(resourceAssignment, ResourceAssignment.TAG_RESOURCE_ID, "");
+		assertEquals("leader not removed", ORef.INVALID, strategy.getLeaderResourceRef());
+	}
+	
+	public void testResourceLeaderIsUpdatedWhenResourceAssignmentIsDeleted() throws Exception
+	{
+		Strategy strategy = getProject().createStrategy();
+		ResourceAssignment resourceAssignment = getProject().addResourceAssignment(strategy);
+		getProject().fillObjectUsingCommand(strategy, Strategy.TAG_LEADER_RESOURCE, resourceAssignment.getResourceRef());
+		assertEquals("leader not set?", resourceAssignment.getResourceRef(), strategy.getLeaderResourceRef());
+		getProject().executeCommand(CommandSetObjectData.createRemoveIdCommand(strategy, Strategy.TAG_RESOURCE_ASSIGNMENT_IDS, resourceAssignment.getId()));
 		assertEquals("leader not removed", ORef.INVALID, strategy.getLeaderResourceRef());
 	}
 }
