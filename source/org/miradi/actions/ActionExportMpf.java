2d10c563ee784800805bdb1db486f2c4ee27307a
nimaa <nimaa@localhost>
2013-03-04 12:03:42 -0500

    MRD-5331: Text change

--- source/org/miradi/actions/ActionExportMpf.java
+++ source/org/miradi/actions/ActionExportMpf.java
@@ -32,7 +32,7 @@ public class ActionExportMpf extends MainWindowAction
 
 	public static String getLabel()
 	{
-		return EAM.text("Action|Miradi Project");
+		return EAM.text("Action|Miradi (.Miradi) Project File");
 	}
 
 	@Override
