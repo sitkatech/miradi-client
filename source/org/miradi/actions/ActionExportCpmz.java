4cc077eee4f03cfe5cdcf8064fef64867cc0c317
nimaa <nimaa@localhost>
2013-03-04 14:52:34 -0500

    MRD-5332: text changes

--- source/org/miradi/actions/ActionExportCpmz.java
+++ source/org/miradi/actions/ActionExportCpmz.java
@@ -31,7 +31,7 @@ public class ActionExportCpmz extends MainWindowAction
 
 	public static String getLabel()
 	{
-		return EAM.text("Action|Export|ConPro (CPMZ) File");
+		return EAM.text("Action|Export|ConPro File (.cpmz)");
 	}
 
 	@Override
