b519057898c0a5fdb1c6bc35c1bc1494b3325163
nimaa <nimaa@localhost>
2013-03-15 12:51:49 -0400

    MRD-4695: warn user when saving as and the undo stack is not empty

--- source/org/miradi/views/umbrella/doers/SaveProjectAsDoer.java
+++ source/org/miradi/views/umbrella/doers/SaveProjectAsDoer.java
@@ -42,6 +42,24 @@ public class SaveProjectAsDoer extends MainWindowDoer
 		if (!isAvailable())
 			return;
 		
+		if (!isUndoStackEmpty())
+		{
+			String[] buttons = new String[2];
+			final int CANCEL_CHOICE = 0;
+			buttons[CANCEL_CHOICE] = EAM.text("Button|Cancel");
+			buttons[1] = EAM.text("Button|Continue");
+			
+			String body = EAM.text("WARNING: The changes caused by the most recent Undo commands have already been saved. \n" +
+									"If you go ahead with Save As right now, both the existing project and the new project \n" +
+									"will contain identical data, and those recent Undo commands will not be able to be reverted \n" +
+									"using Redo. This is different behavior from most applications.\n\n" +
+
+									"Continue with Save As?");
+			
+			if (EAM.confirmDialog(EAM.text("Confirmation"), body, buttons) == CANCEL_CHOICE)
+				return;
+		}
+		
 		final File projectFile = getProject().getProjectFile();
 		CreateProjectDialog saveDialog = new CreateProjectDialog(getMainWindow(), EAM.text("Save As..."), projectFile.getParentFile(), projectFile.getName());
 		if(!saveDialog.showSaveAsDialog())
@@ -59,6 +77,11 @@ public class SaveProjectAsDoer extends MainWindowDoer
 			EAM.logException(e);
 			throw new CommandFailedException("Unexpected error during Save As: " + e);
 		}
+	}
+
+	private boolean isUndoStackEmpty()
+	{
+		return getProject().getLastExecutedCommand() == null;
 	}
 
 	private void saveAs(File chosenFile) throws Exception
