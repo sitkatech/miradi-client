8418f0a63ded9e982c74d3e031fede5902a34ea2
nimaa <nimaa@localhost>
2013-05-16 11:53:34 -0400

    Removed FIXME, we cant pass any args to question, since question is dyanmic.

--- source/org/miradi/schemas/MiradiShareTaxonomySchema.java
+++ source/org/miradi/schemas/MiradiShareTaxonomySchema.java
@@ -38,7 +38,6 @@ public class MiradiShareTaxonomySchema extends BaseObjectSchema
 		createFieldSchemaCode(TAG_TAXONOMY_CODE);
 		createFieldSchemaSingleLineUserText(TAG_TAXONOMY_VERSION);
 		createTaxonomyElementList(TAG_TAXONOMY_ELEMENTS);
-		//FIXME urgent - Question below needs to be take better args. 
 		createFieldSchemaCodeList(TAG_TAXONOMY_TOP_LEVEL_ELEMENT_CODES, new MiradiShareTaxonomyQuestion(null, null));
 	}
 	
