8ce4f07c803ed313055287508efdd6ca819b5b37
jenkins <jenkins@localhost.localdomain>
2013-05-16 16:17:00 -0500

    Updated PO files: 2013-05-16 16:16:25

---
+++ source/org/miradi/dialogs/planning/upperPanel/rebuilder/FactorNoderSorter.java
@@ -0,0 +1,55 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.planning.upperPanel.rebuilder;
+
+import org.miradi.objects.Cause;
+import org.miradi.schemas.GroupBoxSchema;
+import org.miradi.schemas.HumanWelfareTargetSchema;
+import org.miradi.schemas.IntermediateResultSchema;
+import org.miradi.schemas.ScopeBoxSchema;
+import org.miradi.schemas.StrategySchema;
+import org.miradi.schemas.StressSchema;
+import org.miradi.schemas.TargetSchema;
+import org.miradi.schemas.TaskSchema;
+import org.miradi.schemas.TextBoxSchema;
+import org.miradi.schemas.ThreatReductionResultSchema;
+
+public class FactorNoderSorter extends NodeSorter
+{
+	@Override
+	protected String[] getNodeSortOrder()
+	{
+		return new String[] {
+			ScopeBoxSchema.OBJECT_NAME,
+			TargetSchema.OBJECT_NAME,
+			HumanWelfareTargetSchema.OBJECT_NAME,
+			StressSchema.OBJECT_NAME,
+			Cause.OBJECT_NAME_THREAT,
+			Cause.OBJECT_NAME_CONTRIBUTING_FACTOR,
+			ThreatReductionResultSchema.OBJECT_NAME,
+			IntermediateResultSchema.OBJECT_NAME,
+			StrategySchema.OBJECT_NAME,
+			TaskSchema.ACTIVITY_NAME,
+			GroupBoxSchema.OBJECT_NAME,
+			TextBoxSchema.OBJECT_NAME,
+		};
+	}
+}
---
+++ source/org/miradi/objecthelpers/TaxonomyHelper.java
@@ -0,0 +1,60 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.objecthelpers;
+
+import org.miradi.objectpools.TaxonomyAssociationPool;
+import org.miradi.objects.MiradiShareTaxonomy;
+import org.miradi.objects.TaxonomyAssociation;
+import org.miradi.project.Project;
+import org.miradi.schemas.MiradiShareTaxonomySchema;
+
+public class TaxonomyHelper
+{
+	public static TaxonomyAssociation findTaxonomyAssociation(Project project, String taxonomyAssociationCode)
+	{
+		TaxonomyAssociationPool taxonomyAssociationPool = project.getTaxonomyAssociationPool();
+		ORefList taxonomyAssociationsForType = taxonomyAssociationPool.getRefList();
+		for(ORef taxonomyAssociationRef : taxonomyAssociationsForType)
+		{
+			TaxonomyAssociation taxonomyAssociation = TaxonomyAssociation.find(project, taxonomyAssociationRef);
+			if (taxonomyAssociation.getTaxonomyAssociationCode().equals(taxonomyAssociationCode))
+				return taxonomyAssociation;
+		}
+
+		return null;
+	}
+	
+	public static MiradiShareTaxonomy getTaxonomyElementList(final TaxonomyAssociation taxonomyAssociation) throws Exception
+	{
+		String taxonomyCode = taxonomyAssociation.getTaxonomyCode();
+		final Project projectToUse = taxonomyAssociation.getProject();
+		ORefList miradiShareTaxonomyRefs = projectToUse.getPool(MiradiShareTaxonomySchema.getObjectType()).getORefList();
+		for(ORef miradiShareTaxonomyRef : miradiShareTaxonomyRefs)
+		{
+			MiradiShareTaxonomy miradiShareTaxonomy = MiradiShareTaxonomy.find(projectToUse, miradiShareTaxonomyRef);
+			final String thisTaxonomyCode = miradiShareTaxonomy.getData(MiradiShareTaxonomySchema.TAG_TAXONOMY_CODE);
+			if (thisTaxonomyCode.equals(taxonomyCode))
+				return miradiShareTaxonomy;
+		}
+		
+		throw new Exception("Taxonomy object could not be found for code:" + taxonomyCode);
+	}
+}
