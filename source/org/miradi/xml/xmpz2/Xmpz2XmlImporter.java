88611f43ea03be3190d37eafc9dbce62c4815eaa
nimaa <nimaa@localhost>
2013-05-27 15:21:21 -0400

    Inlined method

--- source/org/miradi/xml/xmpz2/Xmpz2XmlImporter.java
+++ source/org/miradi/xml/xmpz2/Xmpz2XmlImporter.java
@@ -413,11 +413,6 @@ public class Xmpz2XmlImporter extends AbstractXmlImporter implements Xmpz2XmlCon
 		importField(destinationRef, destinationTag, internalCode);
 	}
 	
-	public void importDateField(Node node, ORef destinationRefToUse, BaseObjectSchema baseObjectSchema, AbstractFieldSchema fieldSchema) throws Exception
-	{
-		importStringField(node, baseObjectSchema.getXmpz2ElementName(), destinationRefToUse, fieldSchema.getTag());
-	}
-
 	public int getObjectTypeOfNode(Node typedIdNode)
 	{
 		String nodeName = typedIdNode.getNodeName();
