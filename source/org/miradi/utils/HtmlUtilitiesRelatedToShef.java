37e97c5d7f7bf821004ab71bef9508fa59b36efe
nimaa <nimaa@localhost>
2013-06-07 18:30:32 -0400

    Method renamed

--- source/org/miradi/utils/HtmlUtilitiesRelatedToShef.java
+++ source/org/miradi/utils/HtmlUtilitiesRelatedToShef.java
@@ -48,7 +48,7 @@ public class HtmlUtilitiesRelatedToShef
 		trimmedText = HtmlUtilities.removeNonHtmlNewLines(trimmedText);
 		trimmedText = HtmlUtilities.appendNewlineToEndDivTags(trimmedText);
 		trimmedText = HtmlUtilities.removeAllExcept(trimmedText, allowedHtmlTags);
-		trimmedText = HtmlUtilities.fixAnchorElementsSaftley(trimmedText, allowedHtmlTags);
+		trimmedText = HtmlUtilities.fixAnchorElementsSafely(trimmedText, allowedHtmlTags);
 		trimmedText = HtmlUtilities.stripAttributesFromNonAnchorElements(trimmedText);
 		trimmedText = trimmedText.replaceAll("\\t", " ");
 		trimmedText = trimmedText.replaceAll(" +", " ");
