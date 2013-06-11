b5b10eb81f55ca09f1d5e5b0ba0d18210ab843c5
nimaa <nimaa@localhost>
2013-06-11 17:34:42 -0400

    Uncommented future status fields in form. Created new choice and date form items. added labels.

---
+++ source/org/miradi/views/umbrella/Xmpz2ProjectImporter.java
@@ -0,0 +1,50 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.views.umbrella;
+
+import org.miradi.main.MainWindow;
+import org.miradi.project.Project;
+import org.miradi.utils.GenericMiradiFileFilter;
+import org.miradi.utils.ProgressInterface;
+import org.miradi.utils.Xmpz2FileFilter;
+import org.miradi.xml.AbstractXmlImporter;
+import org.miradi.xml.AbstractXmpzProjectImporter;
+import org.miradi.xml.xmpz2.Xmpz2XmlImporter;
+
+public class Xmpz2ProjectImporter extends AbstractXmpzProjectImporter
+{
+	public Xmpz2ProjectImporter(MainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+
+	@Override
+	protected AbstractXmlImporter createXmpzXmlImporter(Project projectToFill,	ProgressInterface progressIndicator) throws Exception
+	{
+		return new Xmpz2XmlImporter(projectToFill, progressIndicator);
+	}
+
+	@Override
+	protected GenericMiradiFileFilter createFileFilter()
+	{
+		return new Xmpz2FileFilter();
+	}
+}
---
+++ source/org/miradi/wizard/planning/PlanningOverviewStep.java
@@ -0,0 +1,47 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.wizard.planning;
+
+import org.miradi.actions.views.ActionViewPlanning;
+import org.miradi.views.planning.PlanningView;
+import org.miradi.wizard.SplitWizardStep;
+import org.miradi.wizard.WizardPanel;
+
+public class PlanningOverviewStep extends SplitWizardStep
+{
+	public PlanningOverviewStep(WizardPanel wizardToUse)
+	{
+		super(wizardToUse, PlanningView.getViewName());
+	}
+
+	@Override
+	public String getProcessStepTitle()
+	{
+		return "";
+	}
+
+	@Override
+	public Class getAssociatedActionClass()
+	{
+		return ActionViewPlanning.class;
+	}
+	
+}
