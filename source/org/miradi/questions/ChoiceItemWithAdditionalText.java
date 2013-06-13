cdcd4c20e8942e6bce2f5a1224d4f7bb97cc034c
jenkins <jenkins@localhost.localdomain>
2013-06-13 13:32:04 -0500

    Updated PO files: 2013-06-13 13:31:24

---
+++ source/org/miradi/questions/ChoiceItemWithAdditionalText.java
@@ -0,0 +1,61 @@
+/* 
+Copyright 2005-2010, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.questions;
+
+import java.awt.Color;
+
+public class ChoiceItemWithAdditionalText extends ChoiceItem
+{
+	public ChoiceItemWithAdditionalText(String codeToUse)
+	{
+		this(codeToUse, "", "");
+	}
+	
+	public ChoiceItemWithAdditionalText(String codeToUse, String additionalTextToUse)
+	{
+		this(codeToUse, additionalTextToUse, "");
+	}
+	
+	public ChoiceItemWithAdditionalText(String codeToUse, String leftLabelToUse, String additionalTextToUse)
+	{
+		this(codeToUse, leftLabelToUse, additionalTextToUse, (Color)null);
+	}
+	
+	public ChoiceItemWithAdditionalText(String codeToUse, String leftLabelToUse, String additionalTextToUse, Color colorToUse)
+	{
+		super(codeToUse, leftLabelToUse, colorToUse);
+		
+		additionalText = additionalTextToUse;
+	}
+
+	public void setRightLabel(String additionalTextToUse)
+	{
+		additionalText = additionalTextToUse;
+	}
+	
+	@Override
+	public String getAdditionalText()
+	{
+		return additionalText;
+	}
+
+	private String additionalText;
+}
---
+++ source/org/miradi/xml/xmpz2/objectImporters/TaxonomyImporter.java
@@ -0,0 +1,53 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.xmpz2.objectImporters;
+
+import org.miradi.objecthelpers.ORef;
+import org.miradi.schemas.MiradiShareTaxonomySchema;
+import org.miradi.xml.xmpz2.Xmpz2XmlImporter;
+import org.w3c.dom.Node;
+
+public class TaxonomyImporter extends BaseObjectImporter
+{
+	public TaxonomyImporter(Xmpz2XmlImporter importerToUse)
+	{
+		super(importerToUse, new MiradiShareTaxonomySchema());
+	}
+	
+	@Override
+	protected boolean isCustomImportField(String tag)
+	{
+		if (tag.equals(MiradiShareTaxonomySchema.TAG_TAXONOMY_CODE))
+			return true;
+		
+		return false;
+	}
+	
+	@Override
+	public ORef createBaseObject(final BaseObjectImporter importer,	Node baseObjectNode) throws Exception
+	{
+		ORef newTaxonomyRef = getProject().createObject(MiradiShareTaxonomySchema.getObjectType());
+		String taxonomyCode = getImporter().getAttributeValue(baseObjectNode, TAXONOMY_CODE);
+		getImporter().setData(newTaxonomyRef, MiradiShareTaxonomySchema.TAG_TAXONOMY_CODE, taxonomyCode);
+		
+		return newTaxonomyRef;
+	}
+}
