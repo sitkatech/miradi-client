d1fee6d182dfbff8649aede2635f9fed53aa8b3a
nimaa <nimaa@localhost>
2013-06-21 19:46:38 -0400

    Removed the adding of goal as future status node.

--- source/org/miradi/dialogs/planning/upperPanel/rebuilder/ViabilityTreeRebuilder.java
+++ source/org/miradi/dialogs/planning/upperPanel/rebuilder/ViabilityTreeRebuilder.java
@@ -26,7 +26,6 @@ import java.util.Comparator;
 import java.util.Vector;
 
 import org.miradi.dialogs.planning.treenodes.AbstractPlanningTreeNode;
-import org.miradi.dialogs.planning.treenodes.UnspecifiedBaseObject;
 import org.miradi.main.EAM;
 import org.miradi.objecthelpers.ORef;
 import org.miradi.objecthelpers.ORefList;
@@ -41,7 +40,6 @@ import org.miradi.objects.Measurement;
 import org.miradi.objects.PlanningTreeRowColumnProvider;
 import org.miradi.objects.ProjectMetadata;
 import org.miradi.project.Project;
-import org.miradi.schemas.GoalSchema;
 import org.miradi.schemas.IndicatorSchema;
 import org.miradi.utils.CodeList;
 
@@ -77,9 +75,6 @@ public class ViabilityTreeRebuilder extends AbstractTreeRebuilder
 		if (FutureStatus.is(parentRef))
 			return noChildren;
 		
-		if(Goal.is(parentRef))
-			return noChildren;
-		
 		if(parentRef.isInvalid())
 			throw new RuntimeException("Attempted to getChildRefs for null parent: " + parentRef.getObjectType());
 		
@@ -109,7 +104,6 @@ public class ViabilityTreeRebuilder extends AbstractTreeRebuilder
 		Indicator indicator = Indicator.find(getProject(), parentRef);
 		childRefs.addAll(getSortedByDateMeasurementRefs(indicator));
 		childRefs.addAll(indicator.getFutureStatusRefs());
-		childRefs.add(new UnspecifiedBaseObject(getProject().getObjectManager(), GoalSchema.getObjectType(), GoalSchema.OBJECT_NAME));
 		
 		return childRefs;
 	}
