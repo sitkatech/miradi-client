22be801ab8aac64afadc342876ff8bc3abd65622
nimaa <nimaa@localhost>
2013-06-23 15:46:59 -0400

    Using hard coded indicator future status field names for xmpz1's indicator schema.

--- source/org/miradi/xml/generic/IndicatorObjectSchemaElement.java
+++ source/org/miradi/xml/generic/IndicatorObjectSchemaElement.java
@@ -20,6 +20,7 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml.generic;
 
+import org.miradi.mpfMigrations.IndicatorFutureStatusTagsToFutureStatusTagsMap;
 import org.miradi.objects.Indicator;
 import org.miradi.xml.wcs.Xmpz1XmlConstants;
 
@@ -33,11 +34,11 @@ public class IndicatorObjectSchemaElement extends BaseObjectSchemaElementWithLab
 		createOptionalTextField(Indicator.TAG_DETAIL);
 		createOptionalTextField(Indicator.TAG_COMMENTS);
 		createCodeField(Indicator.TAG_PRIORITY, XmlSchemaCreator.VOCABULARY_PRIORITY_RATING_CODE);
-		createOptionalDateField(Indicator.TAG_FUTURE_STATUS_DATE);
-		createOptionalTextField(Indicator.TAG_FUTURE_STATUS_SUMMARY);
-		createCodeField(Indicator.TAG_FUTURE_STATUS_RATING, XmlSchemaCreator.VOCABULARY_MEASUREMENT_STATUS);
-		createOptionalTextField(Indicator.TAG_FUTURE_STATUS_DETAIL);
-		createOptionalTextField(Indicator.TAG_FUTURE_STATUS_COMMENTS);	
+		createOptionalDateField(IndicatorFutureStatusTagsToFutureStatusTagsMap.TAG_INDICATOR_FUTURE_STATUS_DATE);
+		createOptionalTextField(IndicatorFutureStatusTagsToFutureStatusTagsMap.TAG_INDICATOR_FUTURE_STATUS_SUMMARY);
+		createCodeField(IndicatorFutureStatusTagsToFutureStatusTagsMap.TAG_INDICATOR_FUTURE_STATUS_RATING, XmlSchemaCreator.VOCABULARY_MEASUREMENT_STATUS);
+		createOptionalTextField(IndicatorFutureStatusTagsToFutureStatusTagsMap.TAG_INDICATOR_FUTURE_STATUS_DETAIL);
+		createOptionalTextField(IndicatorFutureStatusTagsToFutureStatusTagsMap.TAG_INDICATOR_FUTURE_STATUS_COMMENTS);	
 		createOptionalIdListField(Xmpz1XmlConstants.PROGRESS_REPORT_IDS, XmlSchemaCreator.PROGRESS_REPORT_ID_ELEMENT_NAME);
 		createOptionalIdListField(Xmpz1XmlConstants.EXPENSE_IDS, XmlSchemaCreator.EXPENSE_ASSIGNMENT_ID_ELEMENT_NAME);
 		createOptionalIdListField(Indicator.TAG_RESOURCE_ASSIGNMENT_IDS, XmlSchemaCreator.RESOURCE_ASSIGNMENT_ID_ELEMENT_NAME);
