38a681aa5a0c395eaace526e48c78ac94a70f399
jenkins <jenkins@localhost.localdomain>
2013-06-28 16:47:16 -0500

    Updated PO files: 2013-06-28 16:46:36

---
+++ source/org/miradi/objectdata/SingleLineUserTextData.java
@@ -0,0 +1,45 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.objectdata;
+
+import org.miradi.schemas.AbstractFieldSchema;
+import org.miradi.schemas.BaseObjectSchema;
+import org.miradi.xml.xmpz2.xmpz2schema.Xmpz2XmlSchemaCreator;
+
+public class SingleLineUserTextData extends UserTextData
+{
+	public SingleLineUserTextData(String tagToUse)
+	{
+		super(tagToUse);
+	}
+
+	@Override
+	public boolean isSingleLineUserText()
+	{
+		return true;
+	}
+	
+	@Override
+	public String createXmpz2SchemaElementString(Xmpz2XmlSchemaCreator creator, BaseObjectSchema baseObjectSchema, AbstractFieldSchema fieldSchema) throws Exception
+	{
+		return creator.createStringSchemaElement(baseObjectSchema, fieldSchema);
+	}
+}
---
+++ tests/org/miradi/xml/wcs/TestAbstractSchemaCreator.java
@@ -0,0 +1,57 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.wcs;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.miradi.main.ResourcesHandler;
+import org.miradi.main.TestCaseWithProject;
+
+abstract public class TestAbstractSchemaCreator extends TestCaseWithProject
+{
+	public TestAbstractSchemaCreator(String name)
+	{
+		super(name);
+	}
+	
+	public String getExpectedLines() throws Exception
+	{
+		URL resourceURL = ResourcesHandler.getEnglishResourceURL(getStaticSchemaPath());
+		FileInputStream fileInputStream = new FileInputStream(resourceURL.getFile());
+		DataInputStream in = new DataInputStream(fileInputStream);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+		StringBuffer allLines = new StringBuffer();
+		String expectedLine;
+		while ((expectedLine = bufferedReader.readLine()) != null)   
+		{
+			allLines.append(expectedLine);
+			allLines.append("\n");
+		}
+
+		return allLines.toString();
+	}
+
+	abstract public String getStaticSchemaPath();
+}
