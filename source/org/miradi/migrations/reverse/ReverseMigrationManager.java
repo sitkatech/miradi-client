089002e9e67f432fb532e6d4f3959ae9747047e9
nimaa <nimaa@localhost>
2013-07-18 16:25:33 -0400

    Package renamed

--- source/org/miradi/migrations/reverse/ReverseMigrationManager.java
+++ source/org/miradi/migrations/reverse/ReverseMigrationManager.java
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
 */ 
 
-package org.miradi.mpfMigrations.reverseMigrations;
+package org.miradi.migrations.reverse;
 
 import org.miradi.migrations.AbstractMigrationManager;
 
