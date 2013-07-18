8401908cc1532e9577ef4af224f2b1f0ddd24884
nimaa <nimaa@localhost>
2013-07-18 16:24:38 -0400

    Package renamed

--- source/org/miradi/migrations/forward/MigrationTo5.java
+++ source/org/miradi/migrations/forward/MigrationTo5.java
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
 */ 
 
-package org.miradi.mpfMigrations.forwardMigrations;
+package org.miradi.migrations.forward;
 
 import org.miradi.migrations.AbstractForwardMigration;
 import org.miradi.migrations.RawObject;
