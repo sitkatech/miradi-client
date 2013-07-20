97c35a674f6fe91b048762bbbcdf0beb91ee2bf3
jenkins <jenkins@localhost.localdomain>
2013-07-19 19:25:37 -0500

    Updated PO files: 2013-07-19 19:25:10

---
+++ source/org/miradi/migrations/AbstractMigration.java
@@ -0,0 +1,85 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.migrations;
+
+import java.util.Vector;
+
+
+abstract public class AbstractMigration
+{
+	public AbstractMigration(RawProject rawProjectToUse)
+	{
+		rawProject = rawProjectToUse;
+	}
+
+	public boolean canMigrateThisVersion(VersionRange versionRange) throws Exception
+	{
+		return getMigratableVersionRange().doesContainHigh(versionRange.getHighVersion());
+	}
+
+	protected RawProject getRawProject()
+	{
+		return rawProject;
+	}
+	
+	public Vector<RawObjectVisitor> createRawObjectForwardMigrationVisitors()
+	{
+		return new Vector<RawObjectVisitor>();
+	}
+	
+	public Vector<RawObjectVisitor> createRawObjectReverseMigrationVisitors()
+	{
+		return new Vector<RawObjectVisitor>();
+	}
+	
+	public void forwardMigrateIfPossible() throws Exception
+	{
+		final Vector<RawObjectVisitor> rawObjectForwardMigrationVisitors = createRawObjectForwardMigrationVisitors();
+		final VersionRange postMigrationVersionRange = getPostForwardMigrationVersionRange();
+		
+		migrateIfPossible(rawObjectForwardMigrationVisitors, postMigrationVersionRange);
+	}
+	
+	public void reverseMigrateIfPossible() throws Exception
+	{
+		final Vector<RawObjectVisitor> rawObjectReverseMigrationVisitors = createRawObjectReverseMigrationVisitors();
+		final VersionRange postMigrationVersionRange = getPostReverseMigrationVersionRange();
+		
+		migrateIfPossible(rawObjectReverseMigrationVisitors, postMigrationVersionRange);
+	}
+
+	private void migrateIfPossible(final Vector<RawObjectVisitor> rawObjectVisitors, final VersionRange postMigrationVersionRange) throws Exception
+	{
+		if (canMigrateThisVersion(getRawProject().getCurrentVersionRange()))
+		{
+			getRawProject().visitAllObjectsInPool(rawObjectVisitors);
+			getRawProject().setCurrentVersionRange(postMigrationVersionRange);
+		}
+	}
+	
+	abstract public VersionRange getMigratableVersionRange() throws Exception;
+	
+	abstract public VersionRange getPostForwardMigrationVersionRange() throws Exception;
+	
+	abstract public VersionRange getPostReverseMigrationVersionRange() throws Exception;
+
+	private RawProject rawProject;
+}
---
+++ tests/org/miradi/migrations/AbstractTestMigration.java
@@ -0,0 +1,50 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.migrations;
+
+import java.io.IOException;
+
+import org.martus.util.UnicodeStringReader;
+import org.miradi.main.TestCaseWithProject;
+import org.miradi.migrations.forward.MigrationManager;
+import org.miradi.project.ProjectForTesting;
+import org.miradi.project.ProjectLoader;
+import org.miradi.project.ProjectSaverForTesting;
+
+public class AbstractTestMigration extends TestCaseWithProject
+{
+	public AbstractTestMigration(String name)
+	{
+		super(name);
+	}
+	
+	protected ProjectForTesting migrateProject(final VersionRange versionRangeToUse) throws Exception, IOException
+	{
+		MigrationManager migrationManager = new MigrationManager();
+		String projectAsString = ProjectSaverForTesting.createSnapShot(getProject(), versionRangeToUse);
+		String migratedMpfFile = migrationManager.migrateForward(projectAsString);
+		
+		ProjectForTesting migratedProject = ProjectForTesting.createProjectWithoutDefaultObjects("MigratedProject");
+		ProjectLoader.loadProject(new UnicodeStringReader(migratedMpfFile), migratedProject);
+		
+		return migratedProject;
+	}	
+}
