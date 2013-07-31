92a70ed154187051d59710864452cfc4e51a5bc4
nimaa <nimaa@localhost>
2013-07-31 18:22:57 -0400

    MRD-55675: enabled mpz import

--- source/org/miradi/main/menu/MainMenuBar.java
+++ source/org/miradi/main/menu/MainMenuBar.java
@@ -123,8 +123,7 @@ public class MainMenuBar extends JMenuBar
 		addMenuItem(actions, menu, ActionExportXmpz2.class);
 		menu.addSeparator();
 		addMenuItem(actions, menu, ActionExportCpmz.class, KeyEvent.VK_C);
-		//FIXME urgent - nmrd-5575: when mpz exporter is fixed, enable this action
-		//addMenuItem(actions, menu, ActionExportMpz.class);
+		addMenuItem(actions, menu, ActionExportMpz.class);
 		addMenuItem(actions, menu, ActionExportXmpz.class, KeyEvent.VK_X);
 		addMenuItem(actions, menu, ActionExportWcsProjectZip.class);
 		if(Miradi.isDemoMode())
