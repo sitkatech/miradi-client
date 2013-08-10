7ca0d8ced177f128b8eefd05e8ac9f9eaada9e36
nimaa <nimaa@localhost>
2013-08-10 16:58:22 -0400

    Removed unused methods

---
+++ source/org/miradi/dialogfields/AbstractEditorComponentWithHiearchies.java
@@ -0,0 +1,121 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogfields;
+
+import java.util.Vector;
+
+import javax.swing.Box;
+import javax.swing.JComponent;
+
+import org.miradi.dialogs.base.DataPanelSingleRowSelectionHandler;
+import org.miradi.dialogs.base.MiradiPanel;
+import org.miradi.dialogs.base.SingleRowSelectionHandler;
+import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
+import org.miradi.main.EAM;
+import org.miradi.questions.ChoiceItem;
+import org.miradi.questions.ChoiceQuestion;
+
+abstract public class AbstractEditorComponentWithHiearchies extends QuestionBasedEditorComponent
+{
+	public AbstractEditorComponentWithHiearchies(ChoiceQuestion questionToUse)
+	{
+		super(questionToUse);
+		
+		rowSelectionHandler = getSafeRowSelectionHandler();
+	}
+	
+	public SingleRowSelectionHandler getSafeRowSelectionHandler()
+	{
+		if (rowSelectionHandler == null)
+			rowSelectionHandler = new DataPanelSingleRowSelectionHandler();
+		
+		return rowSelectionHandler;
+	}
+	
+	@Override
+	public void becomeActive()
+	{
+		super.becomeActive();
+		
+		getSafeRowSelectionHandler().becomeActive();
+	}
+
+	@Override
+	public void becomeInactive()
+	{
+		getSafeRowSelectionHandler().becomeInactive();
+		
+		super.becomeInactive();
+	}
+	
+	@Override
+	protected void addComponentToRowPanel(MiradiPanel mainRowsPanel, JComponent leftColumnComponent, ChoiceItem rootChoiceItem)
+	{
+		mainRowsPanel.add(getSafeIconLabel(rootChoiceItem.getIcon()));
+		rootChoiceItem.setSelectable(isRootChoiceItemSelectable());
+		mainRowsPanel.add(createLeftColumnComponent(rootChoiceItem));
+		try
+		{
+			final int horizontalIndent = 0;
+			addRowComponents(mainRowsPanel, rootChoiceItem, horizontalIndent);
+		}
+		catch (Exception e)
+		{
+			EAM.alertUserOfNonFatalException(e);
+		}
+		
+		mainRowsPanel.add(createDescriptionComponent(rootChoiceItem));
+	}
+	
+	private void addRowComponents(MiradiPanel mainRowsPanel, ChoiceItem parentChoiceItem, int horizontalIndent) throws Exception
+	{
+		horizontalIndent += INDENT_PER_LEVEL;
+		Vector<ChoiceItem> children = parentChoiceItem.getChildren();
+		for(ChoiceItem childChoiceItem : children)
+		{
+			Box box = createHorizontalBoxWithIndents(INDENT_PER_LEVEL, horizontalIndent);
+			JComponent leftColumnComponent = createLeftColumnComponent(childChoiceItem);
+			PanelTitleLabel rightDescriptionComponent = new PanelTitleLabel(childChoiceItem.getAdditionalText());
+			box.add(leftColumnComponent);
+			mainRowsPanel.add(box);
+			mainRowsPanel.add(rightDescriptionComponent);
+			addRowComponents(mainRowsPanel, childChoiceItem, horizontalIndent);
+			
+			Vector<JComponent> selectableComponents = new Vector<JComponent>();
+			selectableComponents.add(leftColumnComponent);
+			selectableComponents.add(rightDescriptionComponent);
+
+			getSafeRowSelectionHandler().addSelectableRow(selectableComponents, childChoiceItem.getLongDescriptionProvider());
+		}
+	}
+
+	@Override
+	protected int calculateColumnCount()
+	{
+		return 1;
+	}
+
+	abstract protected boolean isRootChoiceItemSelectable();
+	
+	private SingleRowSelectionHandler rowSelectionHandler;
+	
+	private static final int INDENT_PER_LEVEL = 5;
+}
---
+++ source/org/miradi/dialogfields/TaxonomyFieldsPanel.java
@@ -0,0 +1,102 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogfields;
+
+import java.util.Collections;
+import java.util.Vector;
+
+import org.miradi.dialogs.base.MiradiPanel;
+import org.miradi.dialogs.base.ReadonlyPanelWithPopupEditor;
+import org.miradi.dialogs.base.ReadonlyPanelWithPopupEditorWithoutMainScrollPane;
+import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
+import org.miradi.layout.TwoColumnGridLayout;
+import org.miradi.objecthelpers.BaseObjectByNameSorter;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.TaxonomyHelper;
+import org.miradi.objectpools.TaxonomyAssociationPool;
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.MiradiShareTaxonomy;
+import org.miradi.objects.TaxonomyAssociation;
+import org.miradi.project.Project;
+import org.miradi.questions.MiradiShareTaxonomyQuestion;
+
+public class TaxonomyFieldsPanel extends MiradiPanel 
+{
+	public TaxonomyFieldsPanel(Project projectToUse)
+	{
+		super(new TwoColumnGridLayout());
+		
+		project = projectToUse;
+		clearFieldsToLabelMap();
+	}
+
+	public void setText(String newValue)
+	{
+		for(ReadonlyPanelWithPopupEditor field : taxonomyReadonlyWithPopupEditorPanels)
+		{
+			field.setText(newValue);
+		}
+	}
+
+	public void setObjectRef(ORef refToUse) throws Exception
+	{
+		clearFieldsToLabelMap();
+		rebuildFields(refToUse);
+	}
+
+	private void rebuildFields(ORef refToUse) throws Exception
+	{
+		removeAll();
+		if (refToUse.isInvalid())
+			return;
+		
+		BaseObject baseObject = BaseObject.find(getProject(), refToUse);
+		TaxonomyAssociationPool taxonomyAssociationPool = getProject().getTaxonomyAssociationPool();
+		Vector<TaxonomyAssociation> sortedTaxonomyAssociationsForType = taxonomyAssociationPool.findTaxonomyAssociationsForBaseObject(baseObject);
+		Collections.sort(sortedTaxonomyAssociationsForType, new BaseObjectByNameSorter());
+		for(TaxonomyAssociation taxonomyAssociation : sortedTaxonomyAssociationsForType)
+		{
+			MiradiShareTaxonomy miradiShareTaxonomy = TaxonomyHelper.getTaxonomyElementList(taxonomyAssociation);
+			final MiradiShareTaxonomyQuestion miradiShareTaxonomyQuestion = new MiradiShareTaxonomyQuestion(miradiShareTaxonomy, taxonomyAssociation);
+			final String taxonomyAssociationCode = taxonomyAssociation.getTaxonomyAssociationCode();
+			
+			TaxonomyReadonlyPanelWithPopupEditorProvider provider = new TaxonomyReadonlyPanelWithPopupEditorProvider(getProject(), refToUse, miradiShareTaxonomyQuestion, taxonomyAssociationCode);
+			ReadonlyPanelWithPopupEditor readonlyPanelPopupEditor = new ReadonlyPanelWithPopupEditorWithoutMainScrollPane(provider, taxonomyAssociation.getLabel(), miradiShareTaxonomyQuestion);
+			taxonomyReadonlyWithPopupEditorPanels.add(readonlyPanelPopupEditor);
+			
+			add(new PanelTitleLabel(taxonomyAssociation.getLabel()));
+			add(readonlyPanelPopupEditor);
+		}
+	}		
+	
+	Project getProject()
+	{
+		return project;
+	}
+	
+	private void clearFieldsToLabelMap()
+	{
+		taxonomyReadonlyWithPopupEditorPanels = new Vector<ReadonlyPanelWithPopupEditor>();
+	}
+	
+	private Project project;
+	private Vector<ReadonlyPanelWithPopupEditor> taxonomyReadonlyWithPopupEditorPanels;
+}
