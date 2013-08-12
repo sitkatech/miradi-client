ef040dc8e94715d32a27617f2ed7e6581fd987e7
nimaa <nimaa@localhost>
2013-08-12 10:15:08 -0400

    MRD-5631: text update

---
+++ source/org/miradi/migrations/forward/MigrationTo6.java
@@ -0,0 +1,117 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.migrations.forward;
+
+import java.util.Set;
+
+import org.miradi.migrations.AbstractMigrationVisitor;
+import org.miradi.migrations.AbstractSingleTypeMigration;
+import org.miradi.migrations.MigrationResult;
+import org.miradi.migrations.RawObject;
+import org.miradi.migrations.RawProject;
+import org.miradi.migrations.VersionRange;
+import org.miradi.objecthelpers.CodeToUserStringMap;
+import org.miradi.schemas.IndicatorSchema;
+import org.miradi.utils.XmlUtilities2;
+
+public class MigrationTo6 extends AbstractSingleTypeMigration
+{
+	public MigrationTo6(RawProject rawProjectToUse)
+	{
+		super(rawProjectToUse);
+	}
+	
+	@Override
+	protected MigrationResult migrateForward() throws Exception
+	{
+		final IndicatorVisitor visitor = new IndicatorVisitor();
+		visitAllObjectsInPool(visitor);
+		
+		return visitor.getMigrationResult();
+	}
+	
+	@Override
+	protected MigrationResult reverseMigrate() throws Exception
+	{
+		return MigrationResult.createSuccess();
+	}
+
+	@Override
+	protected VersionRange getMigratableVersionRange() throws Exception
+	{
+		return new VersionRange(VERSION_LOW, VERSION_HIGH);
+	}
+	
+	@Override
+	protected int getToVersion()
+	{
+		return 6;
+	}
+	
+	@Override
+	protected int getFromVersion() 
+	{
+		return 5;
+	}
+	
+	private class IndicatorVisitor extends AbstractMigrationVisitor
+	{
+		public int getTypeToVisit()
+		{
+			return IndicatorSchema.getObjectType();
+		}
+
+		@Override
+		public MigrationResult internalVisit(RawObject indicator) throws Exception
+		{
+			escapeThresholdValues(indicator, TAG_THRESHOLDS_MAP);
+			escapeThresholdValues(indicator, TAG_THRESHOLD_DETAILS_MAP);
+			
+			return MigrationResult.createSuccess();
+		}
+
+		private void escapeThresholdValues(RawObject indicator, final String tag) throws Exception
+		{
+			if (!indicator.containsKey(tag))
+				return;
+			
+			String codeToUserStringMapAsString = indicator.get(tag);
+			CodeToUserStringMap codeToUserStringMap = new CodeToUserStringMap(codeToUserStringMapAsString);
+			CodeToUserStringMap escapedMap = new CodeToUserStringMap();
+			Set<String> codes = codeToUserStringMap.getCodes();
+			for(String code : codes)
+			{
+				String userValue = codeToUserStringMap.getUserString(code);
+				String decodedValue = XmlUtilities2.getXmlDecoded(userValue);
+				String encodedValue = XmlUtilities2.getXmlEncoded(decodedValue);
+				escapedMap.putUserString(code, encodedValue);
+			}
+			
+			indicator.put(tag, escapedMap.toJsonString());
+		}
+	}
+	
+	private static final int VERSION_LOW = 5;
+	private static final int VERSION_HIGH = 5;
+	
+	private static final String TAG_THRESHOLDS_MAP = "IndicatorThresholds";
+	private static final String TAG_THRESHOLD_DETAILS_MAP = "ThresholdDetails";
+}
---
+++ source/org/miradi/objectdata/RefListData.java
@@ -0,0 +1,109 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.objectdata;
+
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objecthelpers.ORefList;
+import org.miradi.schemas.AbstractFieldSchema;
+import org.miradi.schemas.BaseObjectSchema;
+import org.miradi.schemas.FieldSchemaReflist;
+import org.miradi.xml.xmpz2.Xmpz2XmlImporter;
+import org.miradi.xml.xmpz2.Xmpz2XmlWriter;
+import org.miradi.xml.xmpz2.xmpz2schema.Xmpz2XmlSchemaCreator;
+import org.w3c.dom.Node;
+
+public class RefListData extends ObjectData
+{
+	public RefListData(String tagToUse)
+	{
+		super(tagToUse);
+		
+		refList = new ORefList();
+	}
+	
+	@Override
+	public boolean equals(Object rawOther)
+	{
+		if (! (rawOther instanceof RefListData))
+			return false;
+		
+		return rawOther.toString().equals(toString());
+	}
+
+	@Override
+	public String get()
+	{
+		return refList.toString();
+	}
+
+	@Override
+	public ORefList getRefList()
+	{
+		return new ORefList(refList);
+	}
+	
+	public ORefList getORefList(int objectTypeToFilterOn)
+	{
+		return refList.getFilteredBy(objectTypeToFilterOn);
+	}
+	
+	@Override
+	public boolean isRefListData()
+	{
+		return true;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return toString().hashCode();
+	}
+
+	@Override
+	public void set(String newValue) throws Exception
+	{
+		set(new ORefList(newValue));	
+	}
+
+	private void set(ORefList objectReferenceToUse)
+	{
+		refList = objectReferenceToUse;
+	}
+	
+	@Override
+	public void writeAsXmpz2XmlData(Xmpz2XmlWriter writer, BaseObjectSchema baseObjectSchema, AbstractFieldSchema fieldSchema) throws Exception
+	{
+		writer.writeRefList(baseObjectSchema, fieldSchema, refList);
+	}
+	
+	@Override
+	public void readAsXmpz2XmlData(Xmpz2XmlImporter importer, Node node, ORef detinationRef, BaseObjectSchema baseObjectSchema, AbstractFieldSchema fieldSchema) throws Exception
+	{
+		importer.importRefs(node, detinationRef, baseObjectSchema, fieldSchema, ((FieldSchemaReflist)fieldSchema).getTypeName());
+	}
+	
+	@Override
+	public String createXmpz2SchemaElementString(Xmpz2XmlSchemaCreator creator, BaseObjectSchema baseObjectSchema, AbstractFieldSchema fieldSchema) throws Exception
+	{
+		return creator.createRefListSchemaElement(baseObjectSchema, fieldSchema);
+	}
+	
+	private ORefList refList;
+}
