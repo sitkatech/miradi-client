2811370e003c844d2079e496344db28d8a6b5e6a
nimaa <nimaa@localhost>
2013-09-27 13:00:41 -0400

    Unit test for BiDirectionalHashMap.

---
+++ source/org/miradi/icons/AccountingCodeIcon.java
@@ -0,0 +1,49 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.icons;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+
+import javax.swing.Icon;
+
+public class AccountingCodeIcon implements Icon
+{
+	public int getIconHeight()
+	{
+		return HEIGHT;
+	}
+
+	public int getIconWidth()
+	{
+		return WIDTH;
+	}
+	public void paintIcon(Component c, Graphics g, int x, int y)
+	{
+		g.setColor(new Color(0xee, 0xdd, 0x22));
+		g.fillOval(x, y , WIDTH, HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawOval(x, y , WIDTH, HEIGHT);
+	}
+	
+	protected static final int WIDTH = 14;
+	protected static final int HEIGHT = 14;
+}
