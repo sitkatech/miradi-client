b8b3b049603156a9e519a34428e0d8053910d7df
nimaa <nimaa@localhost>
2013-10-04 15:19:27 -0400

    Changed to program url.

---
+++ source/org/miradi/project/threatrating/RatingValueSet.java
@@ -0,0 +1,98 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.project.threatrating;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.miradi.ids.BaseId;
+import org.miradi.utils.EnhancedJsonObject;
+
+public class RatingValueSet
+{
+	public RatingValueSet()
+	{
+		map = new HashMap<BaseId, BaseId>();
+	}
+	
+	public RatingValueSet(EnhancedJsonObject json)
+	{
+		this();
+		fillFrom(json);
+	}
+	
+	public void fillFrom(String data) throws ParseException
+	{
+		fillFrom(new EnhancedJsonObject(data));
+	}
+
+	public void fillFrom(EnhancedJsonObject json)
+	{
+		Iterator iter = json.keys();
+		while(iter.hasNext())
+		{
+			String key = (String)iter.next();
+			BaseId criterionId = new BaseId(Integer.parseInt(key));
+			BaseId valueId = new BaseId(json.getInt(key));
+			setValueId(criterionId, valueId);
+		}
+	}
+	
+	public BaseId getValueId(BaseId criterionId, BaseId defaultValueId)
+	{
+		BaseId valueObjectId = getRawValueOptionId(criterionId);
+		if(valueObjectId == null)
+			return defaultValueId;
+		return valueObjectId;
+	}
+
+	public void setValueId(BaseId criterionId, BaseId valueId)
+	{
+		map.put(criterionId, valueId);
+	}
+	
+	public EnhancedJsonObject toJson()
+	{
+		EnhancedJsonObject values = new EnhancedJsonObject();
+		Iterator iter = map.keySet().iterator();
+		while(iter.hasNext())
+		{
+			BaseId criterion = (BaseId)iter.next();
+			values.put(criterion.toString(), getRawValueOptionId(criterion).asInt());
+		}
+
+		return values;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return toJson().toString();
+	}
+
+	private BaseId getRawValueOptionId(BaseId criterionId)
+	{
+		return map.get(criterionId);
+	}
+	
+	HashMap<BaseId, BaseId> map;
+
+}
---
+++ source/org/miradi/xml/xmpz2/xmpz2schema/DashboardSchemaWriter.java
@@ -0,0 +1,44 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.xml.xmpz2.xmpz2schema;
+
+import java.util.Vector;
+
+import org.miradi.schemas.BaseObjectSchema;
+
+public class DashboardSchemaWriter extends BaseObjectSchemaWriter
+{
+	public DashboardSchemaWriter(Xmpz2XmlSchemaCreator creatorToUse, BaseObjectSchema baseObjectSchemaToUse)
+	{
+		super(creatorToUse, baseObjectSchemaToUse);
+	}
+	
+	@Override
+	public Vector<String> createFieldSchemas() throws Exception
+	{
+		Vector<String> schemaElements = new Vector<String>();
+		
+		schemaElements.add("attribute " + ID + " "+ "{xsd:integer}");
+		schemaElements.add(getXmpz2XmlSchemaCreator().getSchemaWriter().createOptionalSchemaElement(getXmpz2ElementName() + DASHBOARD_STATUS_ENTRIES, (DASHBOARD_STATUS_ENTRY + ".element*")));
+		
+		return schemaElements;
+	}
+}
