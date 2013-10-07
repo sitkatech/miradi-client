aac9d24b96a663b829e0bf2063797effb4e85c9e
nimaa <nimaa@localhost>
2013-10-07 17:18:33 -0400

    MRD-5561: Changed Details and Comments to be multi line.

---
+++ source/org/miradi/views/map/MapView.java
@@ -0,0 +1,92 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.views.map;
+
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import org.miradi.main.MainWindow;
+import org.miradi.main.MiradiToolBar;
+import org.miradi.main.ResourcesHandler;
+import org.miradi.project.Project;
+import org.miradi.utils.MiradiScrollPane;
+import org.miradi.views.TabbedView;
+import org.miradi.wizard.WizardPanel;
+
+public class MapView extends TabbedView
+{
+	public MapView(MainWindow mainWindowToUse)
+	{
+		super(mainWindowToUse);
+	}
+
+	@Override
+	public String cardName() 
+	{
+		return getViewName();
+	}
+	
+	static public String getViewName()
+	{
+		return Project.MAP_VIEW_NAME;
+	}
+
+	@Override
+	public MiradiToolBar createToolBar()
+	{
+		return new MapToolBar(getActions());
+	}
+
+	@Override
+	public void createTabs() throws Exception
+	{
+		String[] demoMaps = 
+		{
+			"base",
+			"scope",
+			"targets",
+			"threats",
+		};
+		
+		for(int i = 0; i < demoMaps.length; ++i)
+		{
+			URL imageURL = ResourcesHandler.getResourceURL("demo/map-" + demoMaps[i] + ".jpg");
+			JLabel image = new JLabel(new ImageIcon(imageURL));
+			image.setName(demoMaps[i]);
+			addTab(image.getName(), new MiradiScrollPane(image));
+		}
+	}
+
+	@Override
+	public void deleteTabs() throws Exception
+	{
+		// lightweight tabs...nothing to dispose yet
+		super.deleteTabs();
+	}
+
+	public WizardPanel createWizardPanel() throws Exception
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
