d51ed379764a8501c4bb8130c0b6031818573664
nimaa <nimaa@localhost>
2013-10-11 16:22:30 -0400

    Pulled methods up

---
+++ source/org/miradi/dialogfields/AbstractReadonlyChoiceComponent.java
@@ -0,0 +1,101 @@
+/* 
+Copyright 2005-2011, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogfields;
+
+import java.util.Vector;
+
+import org.miradi.dialogs.base.MiradiPanel;
+import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
+import org.miradi.icons.RatingIcon;
+import org.miradi.main.EAM;
+import org.miradi.questions.ChoiceItem;
+import org.miradi.questions.ChoiceQuestion;
+import org.miradi.utils.CodeList;
+
+import com.jhlabs.awt.BasicGridLayout;
+
+abstract public class AbstractReadonlyChoiceComponent extends MiradiPanel
+{
+	public AbstractReadonlyChoiceComponent(ChoiceQuestion questionToUse)
+	{
+		this(questionToUse, SINGLE_COLUMN_COUNT);
+	}
+
+	public AbstractReadonlyChoiceComponent(ChoiceQuestion questionToUse, int columnCount)
+	{
+		question = questionToUse;
+		setLayout(new BasicGridLayout(0, columnCount));
+		setBackground(EAM.READONLY_BACKGROUND_COLOR);
+		setForeground(EAM.READONLY_FOREGROUND_COLOR);
+	}		
+	
+	protected void createAndAddReadonlyLabels(final CodeList codeList)
+	{
+		removeAll();
+		if (codeList.isEmpty())
+			return;
+
+		getQuestion().reloadQuestion();
+		Vector<ChoiceItem> choiceItems = getQuestion().getChoicesAsVector();
+		createAndAddReadonlyLabelsRecursively(choiceItems, codeList);
+		if (getTopLevelAncestor() != null)
+		{
+			getTopLevelAncestor().validate();
+			getTopLevelAncestor().repaint();
+		}
+	}
+
+	private void createAndAddReadonlyLabelsRecursively(Vector<ChoiceItem> allChoiceItems, final CodeList selectedCodes)
+	{
+		try
+		{
+			for (ChoiceItem choiceItem : allChoiceItems)
+			{
+				if (selectedCodes.contains(choiceItem.getCode()))
+				{
+					PanelTitleLabel label = new PanelTitleLabel(choiceItem.getTextAsHtmlWrappedLabel());
+					if (choiceItem.getColor() != null)
+						label.setIcon(new RatingIcon(choiceItem));
+					
+					add(label);
+				}
+				
+				createAndAddReadonlyLabelsRecursively(choiceItem.getChildren(), selectedCodes);
+			}
+		}
+		catch(Exception e)
+		{
+			EAM.alertUserOfNonFatalException(e);
+		}
+	}
+	
+	protected ChoiceQuestion getQuestion()
+	{
+		return question;
+	}
+	
+	abstract public String getText();
+	
+	abstract public void setText(String text);
+
+	protected static final int SINGLE_COLUMN_COUNT = 1;
+	private ChoiceQuestion question;
+}
---
+++ source/org/miradi/migrations/forward/MigrationTo10.java
@@ -0,0 +1,81 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.migrations.forward;
+
+import org.miradi.main.EAM;
+import org.miradi.migrations.RawProject;
+import org.miradi.migrations.RenameMultipleFieldMigrator;
+import org.miradi.migrations.VersionRange;
+import org.miradi.schemas.TncProjectDataSchema;
+import org.miradi.utils.BiDirectionalHashMap;
+
+public class MigrationTo10 extends RenameMultipleFieldMigrator
+{
+	public MigrationTo10(RawProject rawProject)
+	{
+		super(rawProject, TncProjectDataSchema.getObjectType());
+
+		createLegacyToNewMap();
+	}
+
+	@Override
+	protected VersionRange getMigratableVersionRange() throws Exception
+	{
+		return new VersionRange(VERSION_FROM, VERSION_TO);
+	}
+	
+	@Override
+	protected int getToVersion()
+	{
+		return VERSION_TO;
+	}
+	
+	@Override
+	protected int getFromVersion() 
+	{
+		return VERSION_FROM;
+	}
+	
+	@Override
+	protected String getDescription()
+	{
+		return EAM.text("This migration renames 2 fields");
+	}
+
+	@Override
+	protected BiDirectionalHashMap createLegacyToNewMap()
+	{
+		BiDirectionalHashMap oldToNewTagMap = new BiDirectionalHashMap();
+		oldToNewTagMap.put(LEGACY_TAG_MAKING_THE_CASE, TAG_OVERALL_PROJECT_GOAL);
+		oldToNewTagMap.put(LEGACY_TAG_CAPACITY_AND_FUNDING, TAG_FINANCIAL_PLAN);
+		
+		return oldToNewTagMap;
+	}
+	
+	private static final int VERSION_FROM = 9;
+	public static final int VERSION_TO = 10;
+	
+	public final static String LEGACY_TAG_MAKING_THE_CASE = "MakingTheCase";
+	public final static String LEGACY_TAG_CAPACITY_AND_FUNDING = "CapacityAndFunding";
+	
+	public final static String TAG_OVERALL_PROJECT_GOAL = "OverallProjectGoal";
+	public final static String TAG_FINANCIAL_PLAN = "FinancialPlan";
+}
