39689ca0e102c77824a2b4d94e406d36eb58dd25
nimaa <nimaa@localhost>
2013-10-16 18:00:53 -0400

    Class rename

--- source/org/miradi/migrations/forward/MigrationTo11.java
+++ source/org/miradi/migrations/forward/MigrationTo11.java
@@ -24,11 +24,11 @@ import java.util.HashSet;
 
 import org.miradi.main.EAM;
 import org.miradi.migrations.RawProject;
-import org.miradi.migrations.RemoveMultipleFieldForwardMigration;
+import org.miradi.migrations.RemoveMultipleFieldsMigration;
 import org.miradi.migrations.VersionRange;
 import org.miradi.schemas.TncProjectDataSchema;
 
-public class MigrationTo11 extends RemoveMultipleFieldForwardMigration
+public class MigrationTo11 extends RemoveMultipleFieldsMigration
 {
 	public MigrationTo11(RawProject rawProjectToUse)
 	{
