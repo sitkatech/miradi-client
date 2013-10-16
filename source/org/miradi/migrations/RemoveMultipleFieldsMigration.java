517cd7ca467e09856c2873921d398dde679b883d
nimaa <nimaa@localhost>
2013-10-16 18:00:53 -0400

    Class rename

--- source/org/miradi/migrations/RemoveMultipleFieldsMigration.java
+++ source/org/miradi/migrations/RemoveMultipleFieldsMigration.java
@@ -21,9 +21,9 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.migrations;
 
 
-abstract public class RemoveMultipleFieldForwardMigration extends AbstractRemoveMultipleFieldMigration
+abstract public class RemoveMultipleFieldsMigration extends AbstractRemoveMultipleFieldMigration
 {
-	public RemoveMultipleFieldForwardMigration(RawProject rawProject, int typeToRemoveFieldsFrom)
+	public RemoveMultipleFieldsMigration(RawProject rawProject, int typeToRemoveFieldsFrom)
 	{
 		super(rawProject, typeToRemoveFieldsFrom);
 	}
