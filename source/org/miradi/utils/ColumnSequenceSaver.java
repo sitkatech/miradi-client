cb5445380d72f737b08bddf1b8385bbddb5c1b44
nimaa <nimaa@localhost>
2013-10-17 14:56:21 -0400

    Reduced method visibility

--- source/org/miradi/utils/ColumnSequenceSaver.java
+++ source/org/miradi/utils/ColumnSequenceSaver.java
@@ -131,7 +131,7 @@ public class ColumnSequenceSaver extends MouseAdapter
 		return storedColumnSequences;
 	}
 
-	public Project getProject()
+	private Project getProject()
 	{
 		return project;
 	}
