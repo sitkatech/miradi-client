e98d4806ae0e934b127934e20003887ef4fc34b3
nimaa <nimaa@localhost>
2013-10-18 13:19:23 -0400

    MRD-5717: Removing tnc elements that have been recently removed from the schema.

--- tests/org/miradi/xml/TestXmpz2ForwardMigration.java
+++ tests/org/miradi/xml/TestXmpz2ForwardMigration.java
@@ -7,9 +7,12 @@ import org.miradi.utils.HtmlUtilities;
 import org.miradi.utils.UnicodeXmlWriter;
 import org.miradi.xml.wcs.Xmpz2XmlValidator;
 import org.miradi.xml.xmpz2.Xmpz2ForwardMigration;
+import org.miradi.xml.xmpz2.Xmpz2XmlConstants;
 import org.miradi.xml.xmpz2.Xmpz2XmlSilentValidatorForTesting;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /* 
 Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
@@ -36,6 +39,40 @@ public class TestXmpz2ForwardMigration extends TestCaseWithProject
 	public TestXmpz2ForwardMigration(String name)
 	{
 		super(name);
+	}
+	
+	public void testImportingRemovedTncFields() throws Exception
+	{
+		UnicodeXmlWriter projectWriter = TestXmpz2XmlImporter.createWriter(getProject());
+		String exportedProjectXml = projectWriter.toString();
+		StringInputStreamWithSeek stringInputputStream = new StringInputStreamWithSeek(exportedProjectXml);
+		Document document = Xmpz2ForwardMigration.convertToDocument(stringInputputStream);
+		Element rootElement = document.getDocumentElement();
+		NodeList tncProjectDataNodes = rootElement.getElementsByTagName(Xmpz2XmlConstants.PREFIX + Xmpz2XmlConstants.TNC_PROJECT_DATA);
+		for (int index = 0; index < tncProjectDataNodes.getLength(); ++index)
+		{
+			Node node = tncProjectDataNodes.item(index);
+			
+			Element legacyOrganizationalPrioritiesNode = document.createElement(Xmpz2XmlConstants.PREFIX + Xmpz2ForwardMigration.createLegacyTncOrganizationlPrioritesElementName());
+			Element legacyOrganizationalPrioritiesCodeNode = document.createElement(Xmpz2XmlConstants.CODE_ELEMENT_NAME);
+			legacyOrganizationalPrioritiesCodeNode.setTextContent("Some value");
+			legacyOrganizationalPrioritiesNode.appendChild(legacyOrganizationalPrioritiesCodeNode);
+			node.appendChild(legacyOrganizationalPrioritiesNode);
+			
+			Element legacyProjectPlaceTypesNode = document.createElement(Xmpz2XmlConstants.PREFIX + Xmpz2ForwardMigration.createLegacyTncProjectPlaceTypesElementName());
+			Element legacyProjectPlaceTypesCodeNode = document.createElement(Xmpz2XmlConstants.CODE_ELEMENT_NAME);
+			legacyProjectPlaceTypesCodeNode.setTextContent("Some value");
+			legacyProjectPlaceTypesNode.appendChild(legacyProjectPlaceTypesCodeNode);
+			node.appendChild(legacyProjectPlaceTypesNode);
+		}
+		
+		String updatedXmlAsString = HtmlUtilities.toXmlString(document);
+		System.out.println(updatedXmlAsString);
+		Xmpz2ForwardMigration migration = new Xmpz2ForwardMigration();
+		InputStreamWithSeek inputStream = migration.migrate(new StringInputStreamWithSeek(updatedXmlAsString));
+		
+		if (!new Xmpz2XmlValidator().isValid(inputStream))
+			fail("Project should validate after xml has been migrated?");
 	}
 	
 	public void testImportingLegalOlderSchemaVersion() throws Exception
