153d01c6a005a0f4ecd1edc0d63bf4dbbc98b7d7
jenkins <jenkins@localhost.localdomain>
2013-10-28 15:02:37 -0500

    Updated PO files: 2013-10-28 15:01:39

---
+++ source/org/miradi/migrations/forward/MigrationTo12.java
@@ -0,0 +1,77 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.migrations.forward;
+
+import java.util.HashMap;
+
+import org.miradi.main.EAM;
+import org.miradi.migrations.AddMultipleFieldMigration;
+import org.miradi.migrations.RawProject;
+import org.miradi.migrations.VersionRange;
+import org.miradi.schemas.TncProjectDataSchema;
+
+public class MigrationTo12 extends AddMultipleFieldMigration
+{
+	public MigrationTo12(RawProject rawProjectToUse)
+	{
+		super(rawProjectToUse, TncProjectDataSchema.getObjectType());
+	}
+	
+	@Override
+	protected HashMap<String, String> createFieldsToRemove()
+	{
+		HashMap<String, String> fieldsToRemove = new HashMap<String, String>();
+		fieldsToRemove.put(TAG_PROJECT_FOCUS, EAM.text("Project Focus"));
+		fieldsToRemove.put(TAG_PROJECT_SCALE, EAM.text("Project Scale"));
+		
+		return fieldsToRemove;
+	}
+
+	@Override
+	protected VersionRange getMigratableVersionRange() throws Exception
+	{
+		return new VersionRange(VERSION_FROM, VERSION_TO);
+	}
+	
+	@Override
+	protected int getToVersion()
+	{
+		return VERSION_TO;
+	}
+	
+	@Override
+	protected int getFromVersion() 
+	{
+		return VERSION_FROM;
+	}
+	
+	@Override
+	protected String getDescription()
+	{
+		return EAM.text("This migration adds 2 new TNC fields.");
+	}
+	
+	private static final int VERSION_FROM = 11;
+	public static final int VERSION_TO = 12;
+	
+	public final static String TAG_PROJECT_FOCUS = "ProjectFocus";
+	public final static String TAG_PROJECT_SCALE = "ProjectScale";
+}
---
+++ source/org/miradi/views/planning/doers/AbstractPlanningViewConfigurationCodeListEditorDoer.java
@@ -0,0 +1,74 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.views.planning.doers;
+
+import org.miradi.dialogs.base.CodeListEditorPanel;
+import org.miradi.dialogs.base.ModelessDialogWithClose;
+import org.miradi.dialogs.base.ObjectDataInputPanel;
+import org.miradi.exceptions.CommandFailedException;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objects.ViewData;
+import org.miradi.questions.ChoiceQuestion;
+import org.miradi.views.ObjectsDoer;
+
+abstract public class AbstractPlanningViewConfigurationCodeListEditorDoer extends ObjectsDoer
+{
+	@Override
+	public boolean isAvailable()
+	{
+		return true;
+	}
+	
+	@Override
+	protected void doIt() throws Exception
+	{
+		if (!isAvailable())
+			return;
+		
+		try
+		{
+			ORef currentViewDataRef = getProject().getCurrentViewData().getRef();
+			ViewData viewData = ViewData.find(getProject(), currentViewDataRef);
+			ORef planningConfigurationRef = viewData.getORef(ViewData.TAG_TREE_CONFIGURATION_REF);
+
+			ChoiceQuestion configurationQuestion = getConfigurationQuestion();
+			ObjectDataInputPanel codeListPanel = createCodeListPanel(planningConfigurationRef, configurationQuestion);
+			ModelessDialogWithClose dialog = new ModelessDialogWithClose(EAM.getMainWindow(), EAM.text("Selection Dialog"));
+			dialog.setScrollableMainPanel(codeListPanel);
+			getView().showFloatingPropertiesDialog(dialog);
+		}
+		catch (Exception e)
+		{
+			throw new CommandFailedException(e);
+		}
+	}
+
+	protected CodeListEditorPanel createCodeListPanel(ORef planningConfigurationRef, ChoiceQuestion configurationQuestion)
+	{
+		return new CodeListEditorPanel(getProject(), planningConfigurationRef, getConfigurationTag(), configurationQuestion, getGridColumnCount());
+	}
+
+	abstract protected int getGridColumnCount();
+
+	abstract protected String getConfigurationTag();
+
+	abstract protected ChoiceQuestion getConfigurationQuestion();
+}
