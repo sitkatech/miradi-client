77bf140ed69223f5aa3ce5e782613317fe401419
jenkins <jenkins@localhost.localdomain>
2013-11-04 14:32:26 -0600

    Updated PO files: 2013-11-04 14:31:43

---
+++ tests/org/miradi/migrations/TestMigrationManager.java
@@ -0,0 +1,53 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.migrations;
+
+import org.miradi.main.TestCaseWithProject;
+import org.miradi.migrations.forward.MigrationManager;
+
+public class TestMigrationManager extends TestCaseWithProject
+{
+	public TestMigrationManager(String name)
+	{
+		super(name);
+	}
+	
+	public void testGetMigrationType() throws Exception
+	{
+		VersionRange tenTwentyRange = new VersionRange(10, 20);
+		VersionRange twentyFourtyRange = new VersionRange(20, 40);
+		VersionRange fourtyFourtyRange = new VersionRange(40);
+		VersionRange fifteenFourtyRange = new VersionRange(15, 40);
+		verifyType(MigrationManager.NO_MIGRATION, tenTwentyRange, twentyFourtyRange);
+		verifyType(MigrationManager.TOO_NEW_TO_MIGRATE, tenTwentyRange, fourtyFourtyRange);
+		verifyType(MigrationManager.NO_MIGRATION, tenTwentyRange, fifteenFourtyRange);
+		verifyType(MigrationManager.NO_MIGRATION, tenTwentyRange, tenTwentyRange);
+		
+		VersionRange FifteenFifteenRange = new VersionRange(15);
+		verifyType(MigrationManager.NO_MIGRATION, tenTwentyRange, FifteenFifteenRange);
+		verifyType(MigrationManager.MIGRATION, fourtyFourtyRange, FifteenFifteenRange);
+	}
+
+	private void verifyType(int expectedMigrationType, VersionRange miradiVersionRange, VersionRange mpfVersionRange) throws Exception
+	{
+		assertEquals("incorrect migration type?", expectedMigrationType, MigrationManager.getMigrationType(miradiVersionRange, mpfVersionRange));
+	}
+}
