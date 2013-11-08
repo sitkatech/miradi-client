04736b5aa1ea91d64a90d1120e025985aa1f49b2
nimaa <nimaa@localhost>
2013-11-08 16:56:31 -0500

    Readme update for upcoming beta release

---
+++ source/org/miradi/dialogfields/AbstractEditableCodeListField.java
@@ -0,0 +1,92 @@
+/* 
+Copyright 2005-2010, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogfields;
+
+import javax.swing.JComponent;
+
+import org.miradi.dialogs.base.DisposablePanel;
+import org.miradi.dialogs.base.ReadonlyPanelWithPopupEditor;
+import org.miradi.dialogs.base.ReadonlyPanelWithPopupEditorWithoutMainScrollPane;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.project.Project;
+import org.miradi.questions.ChoiceQuestion;
+import org.miradi.utils.FastScrollPane;
+import org.miradi.utils.Translation;
+
+abstract public class AbstractEditableCodeListField extends ObjectDataInputField implements ReadonlyPanelAndPopupEditorProvider
+{
+	public AbstractEditableCodeListField(Project projectToUse, ORef refToUse, String tagToUse, ChoiceQuestion questionToUse)
+	{
+		super(projectToUse, refToUse, tagToUse);
+		
+		question = questionToUse;
+	}
+	
+	@Override
+	protected boolean shouldBeEditable()
+	{
+		return isValidObject();
+	}
+	
+	@Override
+	public JComponent getComponent()
+	{
+		if (readonlyPanelPopupEditor == null)
+		{
+			final String dialogTitle = Translation.fieldLabel(getObjectType(), getTag());
+			readonlyPanelPopupEditor = new ReadonlyPanelWithPopupEditorWithoutMainScrollPane(this, dialogTitle, question);
+		}
+		
+		return readonlyPanelPopupEditor;
+	}
+
+	@Override
+	public String getText()
+	{
+		return readonlyPanelPopupEditor.getText();
+	}
+
+	@Override
+	public void setText(String newValue)
+	{
+		readonlyPanelPopupEditor.setText(newValue);
+	}
+	
+	protected ChoiceQuestion getQuestion()
+	{
+		return question;
+	}
+	
+	protected DisposablePanel createScrollingPanel(DisposablePanel panel)
+	{
+		final DisposablePanel scrollingPanelContainer = new DisposablePanel();
+		scrollingPanelContainer.add(new FastScrollPane(panel));
+		
+		return scrollingPanelContainer;
+	}
+	
+	abstract public DisposablePanel createEditorPanel() throws Exception;
+	
+	abstract public AbstractReadonlyChoiceComponent createReadOnlyComponent(ChoiceQuestion questionToUse, int columnCount);
+	
+	protected ChoiceQuestion question;
+	private ReadonlyPanelWithPopupEditor readonlyPanelPopupEditor;
+}
---
+++ source/org/miradi/views/diagram/doers/CreateFutureStatusDoer.java
@@ -0,0 +1,53 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.views.diagram.doers;
+
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.Indicator;
+import org.miradi.schemas.FutureStatusSchema;
+import org.miradi.views.planning.doers.AbstractTreeCreateAnnotationDoer;
+
+public class CreateFutureStatusDoer extends AbstractTreeCreateAnnotationDoer
+{
+	@Override
+	protected boolean isCorrectOwner(BaseObject selectedObject)
+	{
+		return Indicator.is(selectedObject);
+	}
+	
+	@Override
+	protected int getAnnotationType()
+	{
+		return FutureStatusSchema.getObjectType();
+	}
+
+	@Override
+	protected String getAnnotationTag()
+	{
+		return Indicator.TAG_FUTURE_STATUS_REFS;
+	}
+
+	@Override
+	protected String getObjectName()
+	{
+		return FutureStatusSchema.OBJECT_NAME;
+	}
+}
