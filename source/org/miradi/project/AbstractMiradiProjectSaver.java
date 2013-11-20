d662ad5c0632cda58519c511b25ae4fb9968713a
nimaa <nimaa@localhost>
2013-11-20 12:21:26 -0500

    Moved core MPF writing methods and related vars into a helper class.

--- source/org/miradi/project/AbstractMiradiProjectSaver.java
+++ source/org/miradi/project/AbstractMiradiProjectSaver.java
@@ -46,12 +46,12 @@ abstract public class AbstractMiradiProjectSaver
 	{
 		writer = writerToUse;
 	}
-
+		
 	protected UnicodeWriter getWriter()
 	{
 		return writer;
 	}
-	
+
 	protected void ensureNoNonHtmlNewlinesExists(String data)
 	{
 		if (data.contains(StringUtilities.NEW_LINE))
@@ -75,63 +75,15 @@ abstract public class AbstractMiradiProjectSaver
 		flushWriter();
 	}
 
-	protected String createSimpleRefString(final ORef ref)
-	{
-		return Integer.toString(ref.getObjectType()) +  ":" + ref.getObjectId().toString();
-	}
-	
-	protected void writeRefTagValue(final String actionCode, ORef ref, final String tag, final String value) throws Exception
-	{
-		writeLabelTagValue(actionCode, createSimpleRefString(ref), tag, value);
-	}
-
-	protected void writeLabelTagValue(final String actionCode, final String lineKey, final String tag, final String value) throws Exception, IOException
-	{
-		write(actionCode);
-		write(TAB);
-		
-		write(lineKey);
-		write(TAB);
-		
-		write(tag);
-		write(EQUALS);
-		write(value);
-		
-		getWriter().writeln();
-	}
-	
-	protected void writeTagValue(final String actionCode, final String tag, final String value) throws Exception
-	{
-		write(actionCode);
-		write(TAB);
-		write(tag);
-		write(EQUALS);
-		write(value);
-		getWriter().writeln();
-	}
-
-	protected void writeValue(final String actionCode, final String value) throws Exception
-	{
-		write(actionCode);
-		write(TAB);
-		write(value);
-		getWriter().writeln();
-	}
-	
-	protected void writeSimpleThreatRatingBundle(int threatId, int targetId, int defaultValueId, String ratings) throws Exception, IOException
+	protected void writeSimpleThreatRatingBundle(int threatId, int targetId, int defaultValueId, String ratings) throws Exception
 	{
 		String defaultValueIdString = Integer.toString(defaultValueId);
 		String bundleName = SimpleThreatRatingFramework.getBundleKey(threatId, targetId);
-		writeValue(CREATE_SIMPLE_THREAT_RATING_BUNDLE_CODE, bundleName);
-		writeLabelTagValue(UPDATE_SIMPLE_THREAT_RATING_BUNDLE_CODE, bundleName, ThreatRatingBundle.TAG_DEFAULT_VALUE_ID, defaultValueIdString);
-		writeLabelTagValue(UPDATE_SIMPLE_THREAT_RATING_BUNDLE_CODE, bundleName, ThreatRatingBundle.TAG_VALUES, ratings);
+		ProjectSaverHelper.writeValue(getWriter(), ProjectSaverHelper.CREATE_SIMPLE_THREAT_RATING_BUNDLE_CODE, bundleName);
+		ProjectSaverHelper.writeLabelTagValue(getWriter(), ProjectSaverHelper.UPDATE_SIMPLE_THREAT_RATING_BUNDLE_CODE, bundleName, ThreatRatingBundle.TAG_DEFAULT_VALUE_ID, defaultValueIdString);
+		ProjectSaverHelper.writeLabelTagValue(getWriter(), ProjectSaverHelper.UPDATE_SIMPLE_THREAT_RATING_BUNDLE_CODE, bundleName, ThreatRatingBundle.TAG_VALUES, ratings);
 	}
 
-	protected void write(final String data) throws Exception
-	{
-		getWriter().write(data);
-	}
-	
 	protected void writeFileHeader() throws Exception
 	{
 		getWriter().writeln(createLowHighVersionHeaderLine(getLowVersion(), getHighVersion()));
@@ -147,7 +99,7 @@ abstract public class AbstractMiradiProjectSaver
 		Date lastModified = new Date(lastModifiedMillis);
 		long lastModifiedForComputers = lastModified.getTime();
 		String lastModifiedForHumans = DateFormat.getDateTimeInstance().format(lastModified);
-		getWriter().writeln(STOP_MARKER + " " + lastModifiedForComputers + " " + lastModifiedForHumans);
+		getWriter().writeln(ProjectSaverHelper.STOP_MARKER + " " + lastModifiedForComputers + " " + lastModifiedForHumans);
 	}
 	
 	protected void flushWriter() throws IOException
@@ -157,7 +109,7 @@ abstract public class AbstractMiradiProjectSaver
 
 	protected void writeSchemaVersion() throws Exception
 	{
-		writeTagValue(UPDATE_PROJECT_VERSION_CODE, "Version", Integer.toString(LegacyProjectUtilities.DATA_VERSION));
+		ProjectSaverHelper.writeTagValue(getWriter(), ProjectSaverHelper.UPDATE_PROJECT_VERSION_CODE, "Version", Integer.toString(LegacyProjectUtilities.DATA_VERSION));
 	}
 
 	public static String getBasicFileHeader()
@@ -168,13 +120,13 @@ abstract public class AbstractMiradiProjectSaver
 	
 	protected void writeProjectInfo() throws Exception
 	{
-		writeTagValue(UPDATE_PROJECT_INFO_CODE, ProjectInfo.TAG_HIGHEST_OBJECT_ID, Integer.toString(getHighestAssignedId()));
-		writeTagValue(UPDATE_PROJECT_INFO_CODE, ProjectInfo.TAG_PROJECT_METADATA_ID, getProjectMetadataId());
+		ProjectSaverHelper.writeTagValue(getWriter(), ProjectSaverHelper.UPDATE_PROJECT_INFO_CODE, ProjectInfo.TAG_HIGHEST_OBJECT_ID, Integer.toString(getHighestAssignedId()));
+		ProjectSaverHelper.writeTagValue(getWriter(), ProjectSaverHelper.UPDATE_PROJECT_INFO_CODE, ProjectInfo.TAG_PROJECT_METADATA_ID, getProjectMetadataId());
 	}
 
 	protected void writeLastModified() throws Exception
 	{
-		writeTagValue(UPDATE_LAST_MODIFIED_TIME_CODE, LAST_MODIFIED_TAG, LegacyProjectUtilities.timestampToString(getLastModifiedTime()));
+		ProjectSaverHelper.writeTagValue(getWriter(), ProjectSaverHelper.UPDATE_LAST_MODIFIED_TIME_CODE, ProjectSaverHelper.LAST_MODIFIED_TAG, LegacyProjectUtilities.timestampToString(getLastModifiedTime()));
 	}
 	
 	protected void writeAllSimpleThreatRatings() throws Exception
@@ -202,7 +154,7 @@ abstract public class AbstractMiradiProjectSaver
 	{
 		String quarantineFileContents = getQuarantineData();
 		ensureNoNonHtmlNewlinesExists(quarantineFileContents);
-		writeTagValue(UPDATE_QUARANTINE_CODE, QUARANTINE_DATA_TAG, quarantineFileContents);
+		ProjectSaverHelper.writeTagValue(getWriter(), ProjectSaverHelper.UPDATE_QUARANTINE_CODE, ProjectSaverHelper.QUARANTINE_DATA_TAG, quarantineFileContents);
 	}
 	
 	protected void writeExceptionsLog() throws Exception
@@ -211,7 +163,7 @@ abstract public class AbstractMiradiProjectSaver
 		exceptions = HtmlUtilities.replaceNonHtmlNewlines(exceptions);
 		ensureNoNonHtmlNewlinesExists(exceptions);
 		exceptions = truncate(exceptions);
-		writeTagValue(UPDATE_EXCEPTIONS_CODE, EXCEPTIONS_DATA_TAG, exceptions);
+		ProjectSaverHelper.writeTagValue(getWriter(), ProjectSaverHelper.UPDATE_EXCEPTIONS_CODE, ProjectSaverHelper.EXCEPTIONS_DATA_TAG, exceptions);
 	}
 	
 	private String truncate(String fileContent)
@@ -239,21 +191,11 @@ abstract public class AbstractMiradiProjectSaver
 		for (int index = 0; index < sortedObjectRefs.size(); ++index)
 		{
 			final ORef ref = sortedObjectRefs.get(index);
-			writeNewObjectEntry(ref);
+			ProjectSaverHelper.writeNewObjectEntry(getWriter(), ref);
 			writeObjectUpdateEntries(ref);
 		}
 	}
 	
-	protected void writeNewObjectEntry(ORef ref) throws Exception
-	{
-		writeValue(CREATE_OBJECT_CODE, createSimpleRefString(ref));
-	}
-	
-	protected void writeUpdateObjectLine(ORef ref, String tag, String data) throws Exception
-	{
-		writeRefTagValue(UPDATE_OBJECT_CODE, ref, tag, data);
-	}
-
 	abstract protected ORefList getSortedRefsForType(int type) throws Exception;
 	
 	abstract protected String getExceptionLog() throws Exception;
@@ -274,24 +216,5 @@ abstract public class AbstractMiradiProjectSaver
 
 	abstract protected int getLowVersion();
 	
-	public static final String TAB = "\t";
-	public static final String EQUALS = "=";
-
-	public static final String UPDATE_LAST_MODIFIED_TIME_CODE = "UL";
-	public static final String UPDATE_PROJECT_INFO_CODE = "UP";
-	public static final String UPDATE_PROJECT_VERSION_CODE = "UV";
-	public static final String CREATE_OBJECT_CODE = "CO";
-	public static final String UPDATE_OBJECT_CODE = "UO";
-	public static final String CREATE_SIMPLE_THREAT_RATING_BUNDLE_CODE = "CT";
-	public static final String UPDATE_SIMPLE_THREAT_RATING_BUNDLE_CODE = "UT";
-	public static final String UPDATE_QUARANTINE_CODE = "UQ";
-	public static final String UPDATE_EXCEPTIONS_CODE = "UE";
-	
-	public static final String LAST_MODIFIED_TAG = "LastModified";
-	public static final String QUARANTINE_DATA_TAG = "Data";
-	public static final String EXCEPTIONS_DATA_TAG = "Data";
-
-	public static final String STOP_MARKER = "--";
-
 	private UnicodeWriter writer;
 }
