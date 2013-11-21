ab297431ba398fa29cf4cead407b10e82f804901
nimaa <nimaa@localhost>
2013-11-21 16:13:38 -0500

    Var renamed

--- source/org/miradi/questions/ProjectFocusQuestion.java
+++ source/org/miradi/questions/ProjectFocusQuestion.java
@@ -34,12 +34,12 @@ public class ProjectFocusQuestion extends StaticChoiceQuestion
 		return new ChoiceItem[] {
 			new ChoiceItem("", EAM.text("Not Specified")),
 			new ChoiceItem(PLACE_CODE, EAM.text("Place")),	
-			new ChoiceItem(POLICY_AND_PRACTIVE_CODE, EAM.text("Policy and Practice")),
+			new ChoiceItem(POLICY_AND_PRACTICE_CODE, EAM.text("Policy and Practice")),
 			new ChoiceItem(PERFORMANCE_CODE, EAM.text("Performance")),
 		};
 	}
 	
 	private static final String PLACE_CODE = "Place";
-	public static final String POLICY_AND_PRACTIVE_CODE = "PolicyAndPractice";
+	public static final String POLICY_AND_PRACTICE_CODE = "PolicyAndPractice";
 	private static final String PERFORMANCE_CODE = "Performance";
 }
