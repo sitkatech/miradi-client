6bb828180708eafa00ce78937fe136f83ee82d6e
nimaa <nimaa@localhost>
2013-12-01 12:14:43 -0500

    Pushed xmpz1 specific confirmation to leaf classes.

--- source/org/miradi/views/umbrella/XmlExporterDoer.java
+++ source/org/miradi/views/umbrella/XmlExporterDoer.java
@@ -37,7 +37,6 @@ import org.miradi.objecthelpers.ORefList;
 import org.miradi.objects.ConceptualModelDiagram;
 import org.miradi.objects.DiagramObject;
 import org.miradi.utils.BufferedImageFactory;
-import org.miradi.utils.ConstantButtonNames;
 import org.miradi.utils.MiradiFileSaveChooser;
 import org.miradi.utils.ProgressInterface;
 import org.miradi.utils.UnicodeXmlWriter;
@@ -45,30 +44,6 @@ import org.miradi.xml.XmlExporter;
 
 abstract public class XmlExporterDoer extends AbstractFileSaverDoer
 {
-	@Override
-	protected boolean doesUserConfirm() throws Exception
-	{
-		String title = EAM.text("Export Project");
-		String[] body = new String[] {
-			EAM.text("The XMPZ format is only supported for compatibility with Miradi 3.3. " +
-					"It does not support some of the newer features of Miradi 4.0, so exporting " +
-					"your project to the XMPZ format may lose data. If you just " +
-					"want a backup copy of your project, or if you want to send a copy to " +
-					"someone who uses Miradi 4.0, you should either export to the XMPZ2 format, " +
-					"or just use the .Miradi project file directly.  " +
-					"NOTE: This data format can be used for sending to other systems or creating reports. " +
-					"It cannot be used to transfer data from one copy or version of Miradi to another.\n" +
-					"Do you want to continue with XMPZ export that may lose data?"),
-		};
-
-		String[] buttons = new String[] {
-			EAM.text("Export"),
-			ConstantButtonNames.CANCEL,
-		};
-		
-		return EAM.confirmDialog(title, body, buttons);
-	}
-
 	@Override
 	protected boolean doWork(File destinationFile, ProgressInterface progressInterface) throws Exception
 	{
