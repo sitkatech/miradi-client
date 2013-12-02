f0724c70e8ce80d0917b9811827f7a43ee3f54cc
jenkins <jenkins@localhost.localdomain>
2013-12-02 16:32:23 -0600

    Updated PO files: 2013-12-02 16:31:39

---
+++ source/org/miradi/dialogs/base/WorkPlanProjectResourceFilterEditorPanel.java
@@ -0,0 +1,49 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.base;
+
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.project.Project;
+import org.miradi.questions.ChoiceQuestion;
+
+public class WorkPlanProjectResourceFilterEditorPanel extends ObjectDataInputPanel
+{
+	public WorkPlanProjectResourceFilterEditorPanel(Project projectToUse, ORef orefToUse, String tagToUse, ChoiceQuestion question)
+	{
+		super(projectToUse, orefToUse);
+		
+		addHtmlWrappedLabel("");
+		addHtmlWrappedLabel(EAM.text("Selecting one or more resources below<br>" +
+				"will filter the display to only include <br>" +
+				"work unit and budget total values for <br>" +
+				"items that are assigned to those resources."));
+		addFieldWithCustomLabel(createStringMapProjectResourceFilterCodeListEditor(orefToUse.getObjectType(), tagToUse, question), EAM.text("Resources:"));
+		
+		updateFieldsFromProject();
+	}
+
+	@Override
+	public String getPanelDescription()
+	{
+		return EAM.text("Project Resource Filter Editor");
+	}
+}
---
+++ source/org/miradi/utils/XslImportFileChooser.java
@@ -0,0 +1,59 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.utils;
+
+import javax.swing.filechooser.FileFilter;
+
+import org.miradi.main.EAM;
+import org.miradi.main.MainWindow;
+
+public class XslImportFileChooser extends MiradiFileOpenChooser
+{
+	public XslImportFileChooser(MainWindow mainWindow)
+	{
+		super(mainWindow);
+	}
+
+	@Override
+	public FileFilter[] getFileFilter()
+	{
+		return new FileFilter[] {new XsltFileFilter(), new XslFileFilter(), };
+	}
+
+	@Override
+	protected String getApproveButtonText()
+	{
+		return EAM.text("Import");
+	}
+
+	@Override
+	protected String getApproveButtonToolTipText()
+	{
+		return EAM.text("TT|Import Xsl File");
+	}
+
+	@Override
+	protected String getDialogTitleText()
+	{
+		return EAM.text("Select File to Import");
+	}
+}
+
