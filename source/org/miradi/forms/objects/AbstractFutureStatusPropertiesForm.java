9d39ec4baeda47e57a1273f019d6efc618e39a63
nimaa <nimaa@localhost>
2013-12-02 15:55:49 -0500

    MRD-5737: Referencing new JRE 1.7

---
+++ source/org/miradi/forms/objects/AbstractFutureStatusPropertiesForm.java
@@ -0,0 +1,45 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.forms.objects;
+
+import org.miradi.forms.FieldPanelSpec;
+import org.miradi.objects.Indicator;
+import org.miradi.questions.StatusQuestion;
+import org.miradi.schemas.FutureStatusSchema;
+import org.miradi.schemas.IndicatorSchema;
+
+abstract public class AbstractFutureStatusPropertiesForm extends FieldPanelSpec
+{
+	public AbstractFutureStatusPropertiesForm()
+	{
+		final int type = FutureStatusSchema.getObjectType();
+		addLabelAndField(type, FutureStatusSchema.TAG_FUTURE_STATUS_SUMMARY, TYPE_SINGLE_LINE_STRING);
+		if (isViabilityFutureStatus())
+			addChoiceField(type, FutureStatusSchema.TAG_FUTURE_STATUS_RATING, StatusQuestion.class);
+		
+		addLabelAndReadOnlySingeLineField(IndicatorSchema.getObjectType(), Indicator.TAG_UNIT);
+		addDateField(type, FutureStatusSchema.TAG_FUTURE_STATUS_DATE);
+		addLabelAndField(type, FutureStatusSchema.TAG_FUTURE_STATUS_DETAIL, TYPE_MULTILINE_STRING);
+		addLabelAndField(type, FutureStatusSchema.TAG_FUTURE_STATUS_COMMENTS, TYPE_MULTILINE_STRING);
+	}
+
+	abstract protected boolean isViabilityFutureStatus();
+}
---
+++ tests/org/miradi/ratings/TestRatingQuestion.java
@@ -0,0 +1,45 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+package org.miradi.ratings;
+
+import java.awt.Color;
+
+import org.miradi.main.MiradiTestCase;
+import org.miradi.questions.ChoiceItem;
+import org.miradi.questions.ChoiceQuestion;
+import org.miradi.questions.StaticChoiceQuestion;
+
+public class TestRatingQuestion extends MiradiTestCase
+{
+	public TestRatingQuestion(String name)
+	{
+		super(name);
+	}
+
+	public void testBasics()
+	{
+		ChoiceItem[] choices = {
+			new ChoiceItem("", "None", Color.BLACK),
+			new ChoiceItem("1", "Low", Color.GREEN),
+		};
+		ChoiceQuestion question = new StaticChoiceQuestion(choices);
+		assertEquals(2, question.getChoices().length);
+	}
+}
