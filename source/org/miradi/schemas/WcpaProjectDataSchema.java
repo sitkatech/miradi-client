46e2fab56bfc7bcba487fe9e06bd0e4b3281bf5d
nimaa <nimaa@localhost>
2013-12-02 16:31:03 -0500

    Reverted name change, since it did not fix build.

---
+++ source/org/miradi/schemas/WcpaProjectDataSchema.java
@@ -0,0 +1,69 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.schemas;
+
+import org.miradi.objecthelpers.ObjectType;
+import org.miradi.objects.WcpaProjectData;
+
+public class WcpaProjectDataSchema extends BaseObjectSchema
+{
+	public WcpaProjectDataSchema()
+	{
+		super();
+	}
+	
+	@Override
+	protected void fillFieldSchemas()
+	{
+		super.fillFieldSchemas();
+		
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_LEGAL_STATUS);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_LEGISLATIVE);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_PHYSICAL_DESCRIPTION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_BIOLOGICAL_DESCRIPTION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_SOCIO_ECONOMIC_INFORMATION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_HISTORICAL_DESCRIPTION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_CULTURAL_DESCRIPTION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_ACCESS_INFORMATION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_VISITATION_INFORMATION);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_CURRENT_LAND_USES);
+		createFieldSchemaMultiLineUserText(WcpaProjectData.TAG_MANAGEMENT_RESOURCES);
+	}
+	
+	public static int getObjectType()
+	{
+		return ObjectType.WCPA_PROJECT_DATA;
+	}
+	
+	@Override
+	public int getType()
+	{
+		return getObjectType();
+	}
+
+	@Override
+	public String getObjectName()
+	{
+		return OBJECT_NAME;
+	}
+	
+	public static final String OBJECT_NAME = "WCPAProjectData";
+}
---
+++ source/org/miradi/views/diagram/doers/AbstractPasteDoer.java
@@ -0,0 +1,92 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.views.diagram.doers;
+
+import java.awt.datatransfer.Transferable;
+
+import org.miradi.diagram.DiagramModel;
+import org.miradi.dialogs.diagram.DiagramPanel;
+import org.miradi.main.AbstractTransferableMiradiList;
+import org.miradi.main.EAM;
+import org.miradi.main.TransferableMiradiList;
+import org.miradi.main.TransferableMiradiListVersion4;
+import org.miradi.views.diagram.DiagramClipboard;
+import org.miradi.views.diagram.LocationDoer;
+
+abstract public class AbstractPasteDoer extends LocationDoer
+{
+	@Override
+	public boolean isAvailable()
+	{
+		if(!getProject().isOpen())
+			return false;
+		
+		if (! isInDiagram())
+			return false;
+		
+		try
+		{
+			Transferable contents = getDiagramClipboardContents();
+			if(contents == null)
+				return false;
+			
+			if (contents.isDataFlavorSupported(TransferableMiradiList.miradiListDataFlavor))
+				return true;
+			
+			return contents.isDataFlavorSupported(TransferableMiradiListVersion4.miradiListDataFlavor);
+		}
+		catch (IllegalStateException e)
+		{
+			EAM.logException(e);
+			return false;
+		}
+	}
+	
+	protected AbstractTransferableMiradiList getTransferableMiradiList() throws Exception
+	{
+		Transferable contents = getDiagramClipboardContents();
+		if(contents.isDataFlavorSupported(TransferableMiradiListVersion4.miradiListDataFlavor))
+			return (AbstractTransferableMiradiList)contents.getTransferData(TransferableMiradiListVersion4.miradiListDataFlavor);
+		
+		if (contents.isDataFlavorSupported(TransferableMiradiList.miradiListDataFlavor))
+			return (AbstractTransferableMiradiList)contents.getTransferData(TransferableMiradiList.miradiListDataFlavor); 
+
+		return null;
+	}
+
+	private Transferable getDiagramClipboardContents()
+	{
+		DiagramClipboard clipboard = getProject().getDiagramClipboard();
+		
+		return clipboard.getContents();
+	}
+	
+	protected DiagramModel getDiagramModel()
+	{
+		return getDiagramView().getDiagramModel();
+	}
+	
+	protected DiagramPanel getDiagramPanel()
+	{
+		return getDiagramView().getDiagramPanel();
+	}
+	
+	protected final String CANCEL_BUTTON = EAM.text("Button|Cancel");
+}
