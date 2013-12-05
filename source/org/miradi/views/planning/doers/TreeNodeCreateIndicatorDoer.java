8f00226f0a4898991b6fd30121db7f14289896cb
nimaa <nimaa@localhost>
2013-12-05 16:07:51 -0500

    MRD-5778: added a null test since retrieved data flavor can be null

---
+++ source/org/miradi/views/planning/doers/TreeNodeCreateIndicatorDoer.java
@@ -0,0 +1,54 @@
+/* 
+Copyright 2005-2009, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+package org.miradi.views.planning.doers;
+
+import org.miradi.objects.BaseObject;
+import org.miradi.objects.Factor;
+import org.miradi.schemas.IndicatorSchema;
+
+public class TreeNodeCreateIndicatorDoer extends AbstractTreeCreateAnnotationDoer
+{
+	@Override
+	protected int getAnnotationType()
+	{
+		return IndicatorSchema.getObjectType();
+	}
+	
+	@Override
+	protected String getAnnotationTag()
+	{
+		return Factor.TAG_INDICATOR_IDS;
+	}
+
+	@Override
+	protected boolean isCorrectOwner(BaseObject selectedObject)
+	{
+		if (!Factor.isFactor(selectedObject))
+			return false;
+		
+		return ((Factor) selectedObject).canDirectlyOwnIndicators();
+	}
+
+	@Override
+	protected String getObjectName()
+	{
+		return IndicatorSchema.OBJECT_NAME;
+	}
+}
