9f7c66e703f5133f77731db08cb90d399c9bd2dd
jenkins <jenkins@localhost.localdomain>
2014-02-18 15:01:33 -0600

    Updated PO files: 2014-02-18 15:00:52

---
+++ source/org/miradi/dialogs/diagram/WorkPlanPanelPropertiesPanel.java
@@ -0,0 +1,56 @@
+/* 
+Copyright 2005-2012, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.dialogs.diagram;
+
+import org.miradi.dialogs.base.ObjectDataInputPanel;
+import org.miradi.dialogs.fieldComponents.PanelTitleLabel;
+import org.miradi.main.EAM;
+import org.miradi.objecthelpers.ORef;
+import org.miradi.objects.BaseObject;
+import org.miradi.project.Project;
+import org.miradi.utils.FillerLabel;
+
+public class WorkPlanPanelPropertiesPanel extends ObjectDataInputPanel
+{
+	public WorkPlanPanelPropertiesPanel(Project projectToUse, ORef orefToUse) throws Exception
+	{
+		super(projectToUse, orefToUse);
+		
+		add(new PanelTitleLabel(EAM.text("Who")));
+		addFieldWithoutLabel(createWhoEditorField(orefToUse));
+
+		add(new FillerLabel());
+		add(new FillerLabel());
+		addField(createLeaderDropDownField(orefToUse.getObjectType(), BaseObject.TAG_LEADER_RESOURCE));
+		addField(createReadonlyTextField(BaseObject.PSEUDO_TAG_WHEN_TOTAL));
+		
+		add(new FillerLabel());
+		add(new PanelTitleLabel(EAM.text("Dates can be set in the Work Plan view")));
+		
+		updateFieldsFromProject();
+	}
+
+	@Override
+	public String getPanelDescription()
+	{
+		return EAM.text("Who and When");
+	}
+}
---
+++ source/org/miradi/questions/CustomPlanningAllRowsQuestion.java
@@ -0,0 +1,103 @@
+/* 
+Copyright 2005-2013, Foundations of Success, Bethesda, Maryland 
+(on behalf of the Conservation Measures Partnership, "CMP") and 
+Beneficent Technology, Inc. ("Benetech"), Palo Alto, California. 
+
+This file is part of Miradi
+
+Miradi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3, 
+as published by the Free Software Foundation.
+
+Miradi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Miradi.  If not, see <http://www.gnu.org/licenses/>. 
+*/ 
+
+package org.miradi.questions;
+
+import java.util.Vector;
+
+import org.miradi.icons.ActivityIcon;
+import org.miradi.icons.ContributingFactorIcon;
+import org.miradi.icons.DirectThreatIcon;
+import org.miradi.icons.MethodIcon;
+import org.miradi.icons.TaskIcon;
+import org.miradi.objects.Cause;
+import org.miradi.schemas.CauseSchema;
+import org.miradi.schemas.ConceptualModelDiagramSchema;
+import org.miradi.schemas.HumanWelfareTargetSchema;
+import org.miradi.schemas.ResultsChainDiagramSchema;
+import org.miradi.schemas.TargetSchema;
+import org.miradi.schemas.TaskSchema;
+import org.miradi.xml.xmpz2.Xmpz2XmlConstants;
+
+public class CustomPlanningAllRowsQuestion extends AbstractCustomPlanningRowsQuestion
+{
+	@Override
+	protected boolean shouldIncludeHumanWellbeignTargetRow()
+	{
+		return true;
+	}
+	
+	@Override
+	protected Vector<ChoiceItem> createCauseChoiceItems()
+	{
+		Vector<ChoiceItem> choiceItems = new Vector<ChoiceItem>();
+		choiceItems.add(createChoiceItem(CauseSchema.getObjectType(), Cause.OBJECT_NAME_THREAT, new DirectThreatIcon()));
+		choiceItems.add(createChoiceItem(CauseSchema.getObjectType(), Cause.OBJECT_NAME_CONTRIBUTING_FACTOR, new ContributingFactorIcon())); 
+
+		return choiceItems;
+	}
+	
+	@Override
+	protected Vector<ChoiceItem> createTaskChoiceItems()
+	{
+		Vector<ChoiceItem> choiceItems = new Vector<ChoiceItem>();
+		choiceItems.add(createChoiceItem(TaskSchema.getObjectType(), TaskSchema.ACTIVITY_NAME, new ActivityIcon()));
+		choiceItems.add(createChoiceItem(TaskSchema.getObjectType(), TaskSchema.METHOD_NAME, new MethodIcon()));
+		choiceItems.add(createChoiceItem(TaskSchema.getObjectType(), TaskSchema.OBJECT_NAME, new TaskIcon()));
+
+		return choiceItems;
+	}
+	
+	@Override
+	public String convertToReadableCode(String code)
+	{
+		if (code.equals(ConceptualModelDiagramSchema.OBJECT_NAME))
+			return Xmpz2XmlConstants.CONCEPTUAL_MODEL;
+		
+		if (code.equals(ResultsChainDiagramSchema.OBJECT_NAME))
+			return Xmpz2XmlConstants.RESULTS_CHAIN;
+		
+		if (code.equals(TargetSchema.OBJECT_NAME))
+			return Xmpz2XmlConstants.BIODIVERSITY_TARGET;
+		
+		if (code.equals(HumanWelfareTargetSchema.OBJECT_NAME))
+			return HumanWelfareTargetSchema.HUMAN_WELLBEING_TARGET;
+			
+		return super.convertToReadableCode(code);
+	}
+	
+	@Override
+	public String convertToInternalCode(String code)
+	{
+		if (code.equals(Xmpz2XmlConstants.CONCEPTUAL_MODEL))
+			return ConceptualModelDiagramSchema.OBJECT_NAME;
+		
+		if (code.equals(Xmpz2XmlConstants.RESULTS_CHAIN))
+			return ResultsChainDiagramSchema.OBJECT_NAME;
+		
+		if (code.equals(Xmpz2XmlConstants.BIODIVERSITY_TARGET))
+			return TargetSchema.OBJECT_NAME;
+		
+		if (code.equals(HumanWelfareTargetSchema.HUMAN_WELLBEING_TARGET))
+			return HumanWelfareTargetSchema.OBJECT_NAME;
+
+		return super.convertToInternalCode(code);
+	}
+}
