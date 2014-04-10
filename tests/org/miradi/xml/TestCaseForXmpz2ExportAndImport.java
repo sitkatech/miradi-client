afd5505ef5cebbfdb076aed1520148436dd1074c
kevins <kevins@localhost>
2014-04-10 09:49:32 -0700

    Refactor pull up test helper methods

--- tests/org/miradi/xml/TestCaseForXmpz2ExportAndImport.java
+++ tests/org/miradi/xml/TestCaseForXmpz2ExportAndImport.java
@@ -20,13 +20,75 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml;
 
+import java.io.IOException;
+
+import org.martus.util.UnicodeStringWriter;
+import org.martus.util.inputstreamwithseek.StringInputStreamWithSeek;
 import org.miradi.main.TestCaseWithProject;
+import org.miradi.project.Project;
+import org.miradi.project.ProjectForTesting;
+import org.miradi.project.ProjectSaver;
+import org.miradi.utils.NullProgressMeter;
+import org.miradi.utils.UnicodeXmlWriter;
+import org.miradi.xml.xmpz2.Xmpz2XmlExporter;
+import org.miradi.xml.xmpz2.Xmpz2XmlImporter;
 
 public class TestCaseForXmpz2ExportAndImport extends TestCaseWithProject
 {
 	public TestCaseForXmpz2ExportAndImport(String name)
 	{
 		super(name);
+	}
+
+	protected void verifyRoundTripExportImport() throws Exception,
+			IOException
+	{
+		Project roundTrip = exportAndImportProject(getProject());
+		String originalMpf = getProjectMpfWithoutTimestamp(getProject());
+		String roundTripMpf = getProjectMpfWithoutTimestamp(roundTrip);
+		assertEquals(originalMpf, roundTripMpf);
+	}
+
+	private String getProjectMpfWithoutTimestamp(Project roundTrip)
+			throws IOException, Exception
+	{
+		UnicodeStringWriter roundTripWriter = UnicodeStringWriter.create();
+		ProjectSaver.saveProject(roundTrip, roundTripWriter);
+		String roundTripMpf = roundTripWriter.toString();
+		int roundTripEndLine = roundTripMpf.indexOf("-- ");
+		roundTripMpf = roundTripMpf.substring(0, roundTripEndLine);
+		return roundTripMpf;
+	}
+
+	private Project exportAndImportProject(Project projectToExport) throws Exception
+	{
+		String xml = exportProject(projectToExport);
+		return importIntoNewProject(xml);
+	}
+
+	private Project importIntoNewProject(String xml) throws Exception
+	{
+		ProjectForTesting projectToImportInto = ProjectForTesting.createProjectWithoutDefaultObjects("ProjectToImportInto");
+		Xmpz2XmlImporter xmlImporter = new Xmpz2XmlImporter(projectToImportInto, new NullProgressMeter());
+		StringInputStreamWithSeek stringInputputStream = new StringInputStreamWithSeek(xml);
+		try
+		{
+			xmlImporter.importProject(stringInputputStream);
+			return xmlImporter.getProject();
+		}
+		finally
+		{
+			stringInputputStream.close();	
+		}
+		
+	}
+
+	private String exportProject(Project projectToExport) throws Exception
+	{
+		final UnicodeXmlWriter writer = UnicodeXmlWriter.create();
+		new Xmpz2XmlExporter(projectToExport).exportProject(writer);
+		writer.close();
+		return writer.toString();
 	}
 
 }
--- tests/org/miradi/xml/TestXmpz2XmlExporter.java
+++ tests/org/miradi/xml/TestXmpz2XmlExporter.java
@@ -20,9 +20,6 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 
 package org.miradi.xml;
 
-import java.io.IOException;
-
-import org.martus.util.UnicodeStringWriter;
 import org.martus.util.inputstreamwithseek.InputStreamWithSeek;
 import org.martus.util.inputstreamwithseek.StringInputStreamWithSeek;
 import org.miradi.exceptions.ValidationException;
@@ -38,9 +35,7 @@ import org.miradi.objects.ProjectMetadata;
 import org.miradi.objects.Strategy;
 import org.miradi.objects.Target;
 import org.miradi.objects.Task;
-import org.miradi.project.Project;
 import org.miradi.project.ProjectForTesting;
-import org.miradi.project.ProjectSaver;
 import org.miradi.schemas.CauseSchema;
 import org.miradi.schemas.GroupBoxSchema;
 import org.miradi.schemas.StrategySchema;
@@ -181,56 +176,6 @@ public class TestXmpz2XmlExporter extends TestCaseForXmpz2ExportAndImport
 		verifyRoundTripExportImport();
 	}
 
-	private void verifyRoundTripExportImport() throws Exception, IOException
-	{
-		Project roundTrip = exportAndImportProject(getProject());
-		String originalMpf = getProjectMpfWithoutTimestamp(getProject());
-		String roundTripMpf = getProjectMpfWithoutTimestamp(roundTrip);
-		assertEquals(originalMpf, roundTripMpf);
-	}
-
-	private String getProjectMpfWithoutTimestamp(Project roundTrip)
-			throws IOException, Exception
-	{
-		UnicodeStringWriter roundTripWriter = UnicodeStringWriter.create();
-		ProjectSaver.saveProject(roundTrip, roundTripWriter);
-		String roundTripMpf = roundTripWriter.toString();
-		int roundTripEndLine = roundTripMpf.indexOf("-- ");
-		roundTripMpf = roundTripMpf.substring(0, roundTripEndLine);
-		return roundTripMpf;
-	}
-	
-	private Project exportAndImportProject(Project projectToExport) throws Exception
-	{
-		String xml = exportProject(projectToExport);
-		return importIntoNewProject(xml);
-	}
-	
-	private Project importIntoNewProject(String xml) throws Exception
-	{
-		ProjectForTesting projectToImportInto = ProjectForTesting.createProjectWithoutDefaultObjects("ProjectToImportInto");
-		Xmpz2XmlImporter xmlImporter = new Xmpz2XmlImporter(projectToImportInto, new NullProgressMeter());
-		StringInputStreamWithSeek stringInputputStream = new StringInputStreamWithSeek(xml);
-		try
-		{
-			xmlImporter.importProject(stringInputputStream);
-			return xmlImporter.getProject();
-		}
-		finally
-		{
-			stringInputputStream.close();	
-		}
-		
-	}
-	
-	private String exportProject(Project projectToExport) throws Exception
-	{
-		final UnicodeXmlWriter writer = UnicodeXmlWriter.create();
-		new Xmpz2XmlExporter(projectToExport).exportProject(writer);
-		writer.close();
-		return writer.toString();
-	}
-	
 	private void create(final String externalAppCope, String xenoDataProjectId) throws Exception
 	{
 		final String xenodataAsString = getProject().createConproXenodata(externalAppCope, xenoDataProjectId);
