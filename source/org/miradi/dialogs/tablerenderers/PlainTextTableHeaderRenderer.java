f0d080d71031cd0eae7d08ad973540cbfdb7d35c
Andy Phenix <andy.phenix@gmail.com>
2015-06-23 11:42:44 +0100

    MRD-5345 - Target names with html tags show up as raw tags in threat rating view's target name column headers.
    * modified plain text header renderer to strip html tags from underlying value on display (similar to how cell values are handled)

--- source/org/miradi/dialogs/tablerenderers/PlainTextTableHeaderRenderer.java
+++ source/org/miradi/dialogs/tablerenderers/PlainTextTableHeaderRenderer.java
@@ -21,13 +21,38 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.dialogs.tablerenderers;
 
 import org.miradi.utils.HtmlUtilities;
+import sun.swing.table.DefaultTableCellHeaderRenderer;
+
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
+import java.awt.*;
 
 
-public class PlainTextTableHeaderRenderer extends MiradiDefaultTableCellHeaderRenderer
+public class PlainTextTableHeaderRenderer extends DefaultTableCellHeaderRenderer
 {
 	@Override
 	public void setText(String text)
 	{
 		super.setText(HtmlUtilities.convertHtmlToPlainText(text));
 	}
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	{
+		if (table != null)
+		{
+			JTableHeader header = table.getTableHeader();
+			if (header != null) {
+				setForeground(header.getForeground());
+				setBackground(header.getBackground());
+				setFont(header.getFont());
+			}
+		}
+
+		JLabel label = (JLabel) table.getTableHeader().getDefaultRenderer().getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		label.setText((value == null) ? "" : HtmlUtilities.convertHtmlToPlainText(value.toString()));
+
+		return label;
+	}
+
 }
