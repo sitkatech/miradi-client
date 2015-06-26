ebad7b9226e35876632bbbeecf1987f8a8011d83
Andy Phenix <andy.phenix@gmail.com>
2015-06-26 15:53:46 +0100

    MRD-5901 - Custom Monitoring Plan View does not display comments or details for Desired Future Value
    * added mapping of future status tags

--- source/org/miradi/dialogs/planning/upperPanel/PlanningViewMainTableModel.java
+++ source/org/miradi/dialogs/planning/upperPanel/PlanningViewMainTableModel.java
@@ -40,26 +40,7 @@ import org.miradi.objecthelpers.ObjectType;
 import org.miradi.objecthelpers.ProjectResourceLeaderAtTopSorter;
 import org.miradi.objecthelpers.TimePeriodCosts;
 import org.miradi.objecthelpers.TimePeriodCostsMap;
-import org.miradi.objects.AbstractTarget;
-import org.miradi.objects.BaseObject;
-import org.miradi.objects.Cause;
-import org.miradi.objects.ConceptualModelDiagram;
-import org.miradi.objects.Desire;
-import org.miradi.objects.Factor;
-import org.miradi.objects.Goal;
-import org.miradi.objects.Indicator;
-import org.miradi.objects.Measurement;
-import org.miradi.objects.Objective;
-import org.miradi.objects.PlanningTreeRowColumnProvider;
-import org.miradi.objects.ProjectMetadata;
-import org.miradi.objects.ProjectResource;
-import org.miradi.objects.ResourceAssignment;
-import org.miradi.objects.ResultsChainDiagram;
-import org.miradi.objects.Strategy;
-import org.miradi.objects.SubTarget;
-import org.miradi.objects.Target;
-import org.miradi.objects.Task;
-import org.miradi.objects.ThreatReductionResult;
+import org.miradi.objects.*;
 import org.miradi.project.Project;
 import org.miradi.questions.ChoiceItem;
 import org.miradi.questions.ChoiceQuestion;
@@ -72,6 +53,7 @@ import org.miradi.questions.StaticQuestionManager;
 import org.miradi.questions.StatusQuestion;
 import org.miradi.questions.TaglessChoiceItem;
 import org.miradi.questions.WorkPlanColumnConfigurationQuestion;
+import org.miradi.schemas.FutureStatusSchema;
 import org.miradi.schemas.ProjectResourceSchema;
 import org.miradi.schemas.ResourceAssignmentSchema;
 import org.miradi.utils.CodeList;
@@ -805,7 +787,14 @@ public class PlanningViewMainTableModel extends PlanningViewAbstractTreeTableSyn
 			if (isDetailsColumn(column))
 				return SubTarget.TAG_DETAIL;
 		}
-		
+		if (FutureStatus.is(nodeType))
+		{
+			if (isCommentsColumn(column))
+				return FutureStatusSchema.TAG_FUTURE_STATUS_COMMENTS;
+			if (isDetailsColumn(column))
+				return FutureStatusSchema.TAG_FUTURE_STATUS_DETAIL;
+		}
+
 		return columnTag;
 	}
 	
