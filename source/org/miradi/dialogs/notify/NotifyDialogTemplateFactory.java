a0f7ac4bd0a2b9c8dc21e6471dd1fb93ac2fe122
Andy Phenix <andy.phenix@gmail.com>
2015-08-19 07:41:38 +0100

    MRD-5924 - Cut & paste of Biophysical Factors / Results between CM and RC diagrams incorrect
    * modified paste shared factors notification dialog content

--- source/org/miradi/dialogs/notify/NotifyDialogTemplateFactory.java
+++ source/org/miradi/dialogs/notify/NotifyDialogTemplateFactory.java
@@ -30,19 +30,27 @@ public class NotifyDialogTemplateFactory
 	{
 		String title = EAM.text("Pasted Shared Factors");
 		String notificationTextToUse = "<html><div class='WizardText'>" + EAM.text(
-				"The pasted factors are now shared, " +
-				"meaning that any changes made in this diagram will also be reflected " +
-				"in other diagrams that these factors appear. " +
-				"In other words, each factor will only exist once in this project, " +
-				"but will be visible in multiple diagram pages. " +
-				"Normally, this is what you want.<br><br>" +
-				"If you really wanted to create duplicate copies of the factors, " +
-				"separate from the originals, " +
-				"you must undo the paste, " +
-				"return to the diagram where these factors originated, " +
-				"and paste them there. " +
-				"Then use the &lt;Cut&gt; command to move them to the clipboard, " +
-				"return to this diagram, and paste the new copies here.");
+				"Pasted factors take two forms - shared factors and complementary factors " +
+				"<br/><br/> " +
+				"Pasted Shared Factors " +
+				"<ul> " +
+				"<li>When pasting to the same type of diagram: all factors</li> " +
+				"<li>When pasting from Conceptual Model to Result Chain or vice-versa: targets and strategies only</li> " +
+				"</ul> " +
+				"<br/> " +
+				"The pasted factors are (in the above instances) <b>shared</b>, meaning that any changes made in this diagram will also be reflected in other diagrams that these factors appear. " +
+				"In other words, each factor will only exist once in this project, but will be visible in multiple diagram pages. " +
+				"<br/><br/> " +
+				"Normally, this is what you want. If you really wanted to create duplicate copies of the factors, separate from the originals, you must undo the paste, " +
+				"return to the diagram where these factors originated, and paste them there. Then use the &lt;Cut&gt; command to move them to the clipboard, return to this diagram, and paste the new copies here. " +
+				"<br/><br/> " +
+				"Pasted Complementary Factors " +
+				"<ul> " +
+				"<li>When pasting from Conceptual Model to Result Chain or vice-versa: Threats and Threat Reduction Results, Contributing Factors and Intermediate Results, Biophysical Factors and Biophysical Results</li> " +
+				"</ul> " +
+				"<br/> " +
+				"The pasted complementary factors are <b>not</b> shared, meaning that any changes made to the pasted factor in this diagram will not affect the original factor, and vice-versa. " +
+				"This also means that taxonomy or classification selections made in the original factor will not carry over to its pasted complementary factor.");
 		return new NotifyDialogTemplate("PastedSharedFactors", title, notificationTextToUse);
 	}
 	
