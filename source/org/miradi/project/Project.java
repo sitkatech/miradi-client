fb7c9fdd45a1d907f9de1a1654177ddfe24b832a
Andy Phenix <andy.phenix@gmail.com>
2016-05-26 16:35:08 +0100

    MRD-6013 - Work Plan: Overridden numbers are identified
    * draft migration to remove overridden assignments for strategy hierarchy (non-destructive)

--- source/org/miradi/project/Project.java
+++ source/org/miradi/project/Project.java
@@ -1458,6 +1458,6 @@ public class Project implements ProjectInterface
 	
 	public CommandExecutor commandExecutor;
 	
-	public static final int VERSION_LOW = 31;
-	public static final int VERSION_HIGH = 31;
+	public static final int VERSION_LOW = 32;
+	public static final int VERSION_HIGH = 32;
 }
