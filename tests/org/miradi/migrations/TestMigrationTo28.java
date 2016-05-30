dc43dd82d2859303c36ae605d294b41172cde3ae
Andy Phenix <andy.phenix@gmail.com>
2016-05-30 10:12:47 +0100

    MRD-6013 - Work Plan: Overridden numbers are identified
    * added unit tests

--- tests/org/miradi/migrations/TestMigrationTo28.java
+++ tests/org/miradi/migrations/TestMigrationTo28.java
@@ -21,6 +21,10 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 package org.miradi.migrations;
 
 import org.miradi.migrations.forward.MigrationTo28;
+import org.miradi.objecthelpers.DateUnit;
+import org.miradi.objecthelpers.ObjectType;
+import org.miradi.objects.*;
+import org.miradi.project.Project;
 
 public class TestMigrationTo28 extends AbstractTestMigration
 {
@@ -28,7 +32,211 @@ public class TestMigrationTo28 extends AbstractTestMigration
 	{
 		super(name);
 	}
-	
+
+	public void testForwardMigrationStrategyNoResourceAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		ResourceAssignment strategyResourceAssignment = getProject().addResourceAssignment(strategy, 1.0, 2005, 2005);
+
+		Task activity = getProject().createActivity(strategy);
+		ResourceAssignment activityResourceAssignment = getProject().addResourceAssignment(activity, 2.0, 2006, 2006);
+
+		Task subTask = getProject().createTask(activity);
+		ResourceAssignment subTaskResourceAssignment = getProject().addResourceAssignment(subTask, 3.0, 2007, 2007);
+
+		assertFalse(strategyResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(activityResourceAssignment.isAssignmentDataSuperseded(dateUnit2006));
+		assertFalse(subTaskResourceAssignment.isAssignmentDataSuperseded(dateUnit2007));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.RESOURCE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 3);
+	}
+
+	public void testForwardMigrationStrategyResourceAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		ResourceAssignment strategyResourceAssignment = getProject().addResourceAssignment(strategy, 1.0, 2005, 2005);
+
+		Task activity = getProject().createActivity(strategy);
+		ResourceAssignment activityResourceAssignment = getProject().addResourceAssignment(activity, 2.0, 2005, 2005);
+
+		Task subTask = getProject().createTask(activity);
+		ResourceAssignment subTaskResourceAssignment = getProject().addResourceAssignment(subTask, 3.0, 2005, 2005);
+
+		assertTrue(strategyResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertTrue(activityResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(subTaskResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.RESOURCE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 1);
+	}
+
+	public void testForwardMigrationStrategyNoExpenseAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		ExpenseAssignment strategyExpenseAssignment = getProject().addExpenseAssignment(strategy, dateUnit2005, 1.0);
+
+		Task activity = getProject().createActivity(strategy);
+		ExpenseAssignment activityExpenseAssignment = getProject().addExpenseAssignment(activity, dateUnit2006, 2.0);
+
+		Task subTask = getProject().createTask(activity);
+		ExpenseAssignment subTaskExpenseAssignment = getProject().addExpenseAssignment(subTask, dateUnit2007, 3.0);
+
+		assertFalse(strategyExpenseAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(activityExpenseAssignment.isAssignmentDataSuperseded(dateUnit2006));
+		assertFalse(subTaskExpenseAssignment.isAssignmentDataSuperseded(dateUnit2007));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.EXPENSE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 3);
+	}
+
+	public void testForwardMigrationStrategyExpenseAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		ExpenseAssignment strategyExpenseAssignment = getProject().addExpenseAssignment(strategy, dateUnit2005, 1.0);
+
+		Task activity = getProject().createActivity(strategy);
+		ExpenseAssignment activityExpenseAssignment = getProject().addExpenseAssignment(activity, dateUnit2005, 2.0);
+
+		Task subTask = getProject().createTask(activity);
+		ExpenseAssignment subTaskExpenseAssignment = getProject().addExpenseAssignment(subTask, dateUnit2005, 3.0);
+
+		assertTrue(strategyExpenseAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertTrue(activityExpenseAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(subTaskExpenseAssignment.isAssignmentDataSuperseded(dateUnit2005));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.EXPENSE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 1);
+	}
+
+	public void testForwardMigrationIndicatorNoResourceAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		Indicator indicator = getProject().createIndicator(strategy);
+		ResourceAssignment indicatorResourceAssignment = getProject().addResourceAssignment(indicator, 1.0, 2005, 2005);
+
+		Task method = getProject().createMethod(indicator);
+		ResourceAssignment methodResourceAssignment = getProject().addResourceAssignment(method, 2.0, 2006, 2006);
+
+		Task subTask = getProject().createTask(method);
+		ResourceAssignment subTaskResourceAssignment = getProject().addResourceAssignment(subTask, 3.0, 2007, 2007);
+
+		assertFalse(indicatorResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(methodResourceAssignment.isAssignmentDataSuperseded(dateUnit2006));
+		assertFalse(subTaskResourceAssignment.isAssignmentDataSuperseded(dateUnit2007));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.RESOURCE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 3);
+	}
+
+	public void testForwardMigrationIndicatorResourceAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		Indicator indicator = getProject().createIndicator(strategy);
+		ResourceAssignment indicatorResourceAssignment = getProject().addResourceAssignment(indicator, 1.0, 2005, 2005);
+
+		Task method = getProject().createMethod(indicator);
+		ResourceAssignment methodResourceAssignment = getProject().addResourceAssignment(method, 2.0, 2005, 2005);
+
+		Task subTask = getProject().createTask(method);
+		ResourceAssignment subTaskResourceAssignment = getProject().addResourceAssignment(subTask, 3.0, 2005, 2005);
+
+		assertTrue(indicatorResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertTrue(methodResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(subTaskResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.RESOURCE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 1);
+	}
+
+	public void testForwardMigrationIndicatorNoExpenseAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		Indicator indicator = getProject().createIndicator(strategy);
+		ExpenseAssignment indicatorResourceAssignment = getProject().addExpenseAssignment(indicator, dateUnit2005, 1.0);
+
+		Task method = getProject().createMethod(indicator);
+		ExpenseAssignment methodResourceAssignment = getProject().addExpenseAssignment(method, dateUnit2006, 2.0);
+
+		Task subTask = getProject().createTask(method);
+		ExpenseAssignment subTaskExpenseAssignment = getProject().addExpenseAssignment(subTask, dateUnit2007, 3.0);
+
+		assertFalse(indicatorResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(methodResourceAssignment.isAssignmentDataSuperseded(dateUnit2006));
+		assertFalse(subTaskExpenseAssignment.isAssignmentDataSuperseded(dateUnit2007));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.EXPENSE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 3);
+	}
+
+	public void testForwardMigrationIndicatorExpenseAssignmentsRemoved() throws Exception
+	{
+		getProject().setProjectStartDate(2005);
+		getProject().setProjectEndDate(2007);
+
+		Strategy strategy = getProject().createStrategy();
+		Indicator indicator = getProject().createIndicator(strategy);
+		ExpenseAssignment indicatorResourceAssignment = getProject().addExpenseAssignment(indicator, dateUnit2005, 1.0);
+
+		Task method = getProject().createMethod(indicator);
+		ExpenseAssignment methodResourceAssignment = getProject().addExpenseAssignment(method, dateUnit2005, 2.0);
+
+		Task subTask = getProject().createTask(method);
+		ExpenseAssignment subTaskExpenseAssignment = getProject().addExpenseAssignment(subTask, dateUnit2005, 3.0);
+
+		assertTrue(indicatorResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertTrue(methodResourceAssignment.isAssignmentDataSuperseded(dateUnit2005));
+		assertFalse(subTaskExpenseAssignment.isAssignmentDataSuperseded(dateUnit2005));
+
+		RawProject migratedProject = reverseMigrate(new VersionRange(MigrationTo28.VERSION_TO));
+		migrateProject(migratedProject, new VersionRange(Project.VERSION_HIGH));
+
+		RawPool rawPoolForType = migratedProject.getRawPoolForType(ObjectType.EXPENSE_ASSIGNMENT);
+		assertEquals(rawPoolForType.size(), 1);
+	}
+
 	@Override
 	protected int getFromVersion()
 	{
@@ -40,4 +248,8 @@ public class TestMigrationTo28 extends AbstractTestMigration
 	{
 		return MigrationTo28.VERSION_TO;
 	}
+
+	private final static DateUnit dateUnit2007 = new DateUnit("YEARFROM:2007-01");
+	private final static DateUnit dateUnit2006 = new DateUnit("YEARFROM:2006-01");
+	private final static DateUnit dateUnit2005 = new DateUnit("YEARFROM:2005-01");
 }
