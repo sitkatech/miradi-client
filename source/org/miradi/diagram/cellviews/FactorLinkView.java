d1ceaaf5b3400c19e4651f24db07abd7e376ac28
Andy Phenix <andy.phenix@gmail.com>
2018-06-07 13:17:34 +0100

    MRD-5961 - Mac: Diagram issue - bend point in connectors can only be selected when drawing a box around it, not via directly clicking it
    * added work-around for selecting diagram links with bend points on os x

--- source/org/miradi/diagram/cellviews/FactorLinkView.java
+++ source/org/miradi/diagram/cellviews/FactorLinkView.java
@@ -19,12 +19,16 @@ along with Miradi.  If not, see <http://www.gnu.org/licenses/>.
 */ 
 package org.miradi.diagram.cellviews;
 
-
-import org.jgraph.graph.CellHandle;
-import org.jgraph.graph.CellViewRenderer;
-import org.jgraph.graph.EdgeView;
-import org.jgraph.graph.GraphContext;
+import org.jdesktop.swingx.util.OS;
+import org.jgraph.JGraph;
+import org.jgraph.graph.*;
 import org.miradi.diagram.renderers.ArrowLineRenderer;
+
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class FactorLinkView extends EdgeView
 {
@@ -42,10 +46,69 @@ public class FactorLinkView extends EdgeView
 	@Override
 	public CellHandle getHandle(GraphContext context)
 	{
-		edgeHandleWithBendPointSelection = new EdgeHandleWithBendPointSelection(this, context);
-		return edgeHandleWithBendPointSelection;
+		return new EdgeHandleWithBendPointSelection(this, context);
 	}
-	
-	EdgeHandleWithBendPointSelection edgeHandleWithBendPointSelection;
-	protected static ArrowLineRenderer arrowLineRenderer = new ArrowLineRenderer();
+
+	@Override
+	public boolean intersects(JGraph graph, Rectangle2D rect)
+	{
+		boolean intersects = super.intersects(graph, rect);
+		if (!intersects)
+			intersects = checkForBendPointsIntersect(graph, rect);
+
+		return intersects;
+	}
+
+	private boolean checkForBendPointsIntersect(JGraph graph, Rectangle2D rect)
+	{
+		boolean intersects = false;
+
+		// MRD-5961 - work-around for not being able to select links with bend points on OS X
+		Graphics2D g2 = (Graphics2D) graph.getGraphics();
+		if (g2 != null && OS.isMacOSX() && this.getPointCount() > 2)
+		{
+			PathIterator pi = this.getShape().getPathIterator(null);
+			Point2D.Double start = null;
+			Point2D.Double point1 = null;
+			Point2D.Double point2 = null;
+
+			while (!pi.isDone())
+			{
+				double[] coordinates = new double[6];
+
+				switch (pi.currentSegment(coordinates))
+				{
+					case PathIterator.SEG_MOVETO:
+						point2 = new Point2D.Double(coordinates[0], coordinates[1]);
+						point1 = null;
+						start = (Point2D.Double) point2.clone();
+						break;
+					case PathIterator.SEG_LINETO:
+						point1 = point2;
+						point2 = new Point2D.Double(coordinates[0], coordinates[1]);
+						break;
+					case PathIterator.SEG_CLOSE:
+						point1 = point2;
+						point2 = start;
+						break;
+				}
+
+				if (point1 != null)
+				{
+					Line2D segment = new Line2D.Double(point1, point2);
+					if (segment.intersects(rect))
+					{
+						intersects = true;
+						break;
+					}
+				}
+
+				pi.next();
+			}
+		}
+
+		return intersects;
+	}
+
+	private static ArrowLineRenderer arrowLineRenderer = new ArrowLineRenderer();
 }
