8671967c9870a9f18fe9fc3e039316b01924da61
andyp <andy.phenix@gmail.com>
2021-10-25 16:42:40 +0100

    MS-2565 - Desktop: adopt library to improve look & feel
    * set default theme (must be empty code selection)

--- source/org/miradi/questions/LookAndFeelThemeQuestion.java
+++ source/org/miradi/questions/LookAndFeelThemeQuestion.java
@@ -39,7 +39,8 @@ public class LookAndFeelThemeQuestion extends StaticChoiceQuestion
     private static ChoiceItem[] getLookAndFeelThemeChoices()
     {
         return new ChoiceItem[] {
-                new ChoiceItem(DEFAULT_SYSTEM_LOOK_AND_FEEL_THEME_CLASS_NAME, EAM.text("System default")),
+                new ChoiceItem(DEFAULT_SYSTEM_LOOK_AND_FEEL_THEME_CLASS_NAME, EAM.text("System")),
+                new ChoiceItem("", EAM.substituteSingleString(EAM.text("%s (default)"), FlatGitHubIJTheme.NAME)),
                 new ChoiceItem(FlatLightLaf.class.getName(), EAM.text(FlatLightLaf.NAME)),
                 new ChoiceItem(FlatIntelliJLaf.class.getName(), EAM.text(FlatIntelliJLaf.NAME)),
                 new ChoiceItem(FlatArcIJTheme.class.getName(), EAM.text(FlatArcIJTheme.NAME)),
@@ -48,7 +49,6 @@ public class LookAndFeelThemeQuestion extends StaticChoiceQuestion
                 new ChoiceItem(FlatLightFlatIJTheme.class.getName(), EAM.text(FlatLightFlatIJTheme.NAME)),
                 new ChoiceItem(FlatSolarizedLightIJTheme.class.getName(), EAM.text(FlatSolarizedLightIJTheme.NAME)),
                 new ChoiceItem(FlatAtomOneLightIJTheme.class.getName(), EAM.text(FlatAtomOneLightIJTheme.NAME)),
-                new ChoiceItem(DEFAULT_LOOK_AND_FEEL_THEME_CLASS_NAME, EAM.substituteSingleString(EAM.text("%s (default)"), FlatGitHubIJTheme.NAME)),
                 new ChoiceItem(FlatLightOwlIJTheme.class.getName(), EAM.text(FlatLightOwlIJTheme.NAME)),
                 new ChoiceItem(FlatSolarizedLightIJTheme.class.getName(), EAM.text(FlatSolarizedLightIJTheme.NAME)),
         };
