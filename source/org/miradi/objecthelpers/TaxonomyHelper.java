f6a7618d62de7a3dded619b0e46d8d2f84a0524d
andyp <andy.phenix@gmail.com>
2022-01-04 13:46:24 +0000

    MS-2584 - Create "Analytical Question" factor
    * ensure analytical question / assumption taxonomy associations imported

--- source/org/miradi/objecthelpers/TaxonomyHelper.java
+++ source/org/miradi/objecthelpers/TaxonomyHelper.java
@@ -157,6 +157,12 @@ public class TaxonomyHelper implements Xmpz2XmlConstants
 		if (ExpenseAssignment.is(objectType))
 			return EXPENSE_ASSIGNMENT_TAXONOMY_ASSOCIATION_POOL;
 
+		if (AnalyticalQuestion.is(objectType))
+			return ANALYTICAL_QUESTION_TAXONOMY_ASSOCIATION_POOL;
+
+		if (Assumption.is(objectType))
+			return ASSUMPTION_TAXONOMY_ASSOCIATION_POOL;
+
 		return null;
 	}
 
