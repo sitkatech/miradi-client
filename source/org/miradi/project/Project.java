a353fde906ee1671a2e6ff36dd4e69f771086432
andyp <andy.phenix@gmail.com>
2022-02-14 11:08:11 +0000

    MS-2673 - Add Evidence Fields to Analytical Question / Assumption factor types
    * updated project version number range
    * fixed longstanding issue with migrations not correctly handling no-ops

--- source/org/miradi/project/Project.java
+++ source/org/miradi/project/Project.java
@@ -1474,6 +1474,6 @@ public class Project implements ProjectInterface
 	
 	public CommandExecutor commandExecutor;
 	
-	public static final int VERSION_LOW = 79;
+	public static final int VERSION_LOW = 80;
 	public static final int VERSION_HIGH = 80;
 }
