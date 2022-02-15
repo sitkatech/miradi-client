9539d9fcd112aa26b637abf7a33ea1df0b7cd1e8
andyp <andy.phenix@gmail.com>
2022-02-15 14:36:02 +0000

    MS-2464 - bug: Leader in UX isn't recognized in Desktop
    * ensure strategy / activity / tasks leader cleaned up if project resource deleted

--- source/org/miradi/dialogs/base/AbstractObjectDataInputPanel.java
+++ source/org/miradi/dialogs/base/AbstractObjectDataInputPanel.java
@@ -689,11 +689,6 @@ abstract public class AbstractObjectDataInputPanel extends ModelessDialogPanel i
 		return new DropDownChoiceField(project, getRefForType(objectType), tagToUse, question);
 	}
 	
-	public ObjectDataInputField createPlannedLeaderDropDownField(int objectType, String tagToUse)
-	{
-		return new ResourceLeaderDropDownField(getProject(), getRefForType(objectType), tagToUse, new ResourceLeaderPlannedQuestionWithUnspecifiedChoice(getProject()));
-	}
-
 	public ObjectDataInputField createAssignedLeaderDropDownField(int objectType, String tagToUse)
 	{
 		return new ResourceLeaderDropDownField(getProject(), getRefForType(objectType), tagToUse, new ResourceLeaderAssignedQuestionWithUnspecifiedChoice(getProject()));
