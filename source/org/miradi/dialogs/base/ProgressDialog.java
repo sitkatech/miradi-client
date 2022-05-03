2fe2dbcb0dfc4ec6003ca6a6f8f71ba8f753207b
andyp <andy.phenix@gmail.com>
2022-05-03 14:46:30 +0100

    MS-2718 - Deprecate support for v4.4 format
    * update copyright dates, version in readme

--- source/org/miradi/dialogs/base/ProgressDialog.java
+++ source/org/miradi/dialogs/base/ProgressDialog.java
@@ -1,5 +1,5 @@
 /* 
-Copyright 2005-2021, Foundations of Success, Bethesda, Maryland
+Copyright 2005-2022, Foundations of Success, Bethesda, Maryland
 on behalf of the Conservation Measures Partnership ("CMP").
 Material developed between 2005-2013 is jointly copyright by Beneficent Technology, Inc. ("The Benetech Initiative"), Palo Alto, California.
 	
