5cc66493e25c9f5ce466cd55e18630daaed7dfa5
andyp <andy.phenix@gmail.com>
2022-11-15 12:10:41 +0000

    MS-2917 - 4.6 feedback (online & desktop): Group box arrange
    * constrain group box z-index wrt child diagram factors

--- source/org/miradi/objects/DiagramFactor.java
+++ source/org/miradi/objects/DiagramFactor.java
@@ -362,7 +362,7 @@ public class DiagramFactor extends AbstractDiagramObject
 		{
 			for (DiagramFactor childDiagramFactor : getChildDiagramFactors())
 			{
-				maxZIndex = Math.min(maxZIndex, childDiagramFactor.getZIndex());
+				maxZIndex = Math.min(maxZIndex, childDiagramFactor.getZIndex() - 1);
 
 				DiagramLink[] childDiagramFactorLinks = getProject().getToAndFromLinks(childDiagramFactor.getDiagramFactorId());
 				for (DiagramLink diagramLink : childDiagramFactorLinks)
